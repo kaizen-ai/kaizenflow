5aa205207e46e74f9bc904dfee4779501b425e50
paul <smith.paul.anthony@gmail.com>
2019-07-31 13:38:55 -0700

    Add basic time-frequency analysis plots.

---
+++ features/sig_proc.py
@@ -0,0 +1,109 @@
+import matplotlib
+import numpy as np
+import pandas as pd
+import pywt
+import scipy as sp
+import statsmodels as sm
+
+
+def plot_autocorrelation(signal, lags=40):
+    """
+    Plot autocorrelation and partial autocorrelation of series.
+    """
+    fig = plt.figure(figsize=(12,8))
+    ax1 = fig.add_subplot(211)
+    fig = sm.api.graphics.tsa.plot_acf(signal, lags=lags, ax=ax1)
+    ax2 = fig.add_subplot(212)
+    fig = sm.api.graphics.tsa.plot_pacf(signal, lags=lags, ax=ax2)
+
+
+def plot_power_spectral_density(signal):
+    """
+    Estimates the power spectral density using Welch's method.
+    """
+    freqs, psd = sp.signal.welch(signal)
+    plt.figure(figsize=(5, 4))
+    plt.semilogx(freqs, psd)
+    plt.title('PSD: power spectral density')
+    plt.xlabel('Frequency')
+    plt.ylabel('Power')
+    plt.tight_layout()
+
+
+def plot_spectrogram(signal):
+    freqs, times, spectrogram = sp.signal.spectrogram(signal)
+    plt.figure(figsize=(5, 4))
+    plt.imshow(spectrogram, aspect='auto', cmap='hot_r', origin='lower')
+    plt.title('Spectrogram')
+    plt.ylabel('Frequency band')
+    plt.xlabel('Time window')
+    plt.tight_layout()
+
+
+def plot_wavelet_levels(signal, wavelet_name, levels):
+    """
+    Wavelet level decomposition plot. Higher levels are smoother.
+    
+    :param signal: Series-like numerical object
+    :param wavelet_name: One of the names in pywt.wavelist()
+    :param levels: The number of levels to plot
+    """
+    fig, ax = plt.subplots(figsize=(6, 1))
+    ax.set_title("Original signal")
+    ax.plot(signal)
+    plt.show()
+    
+    data = signal.copy()
+    fig, axarr = plt.subplots(nrows=levels, ncols=2, figsize=(6,6))
+    for idx in range(levels):
+        (data, coeff_d) = pywt.dwt(data, wavelet_name)
+        axarr[idx, 0].plot(data, 'r')
+        axarr[idx, 1].plot(coeff_d, 'g')
+        axarr[idx, 0].set_ylabel("Level {}".format(idx + 1), fontsize=14, rotation=90)
+        axarr[idx, 0].set_yticklabels([])
+        if idx == 0:
+            axarr[idx, 0].set_title("Approximation coefficients", fontsize=14)
+            axarr[idx, 1].set_title("Detail coefficients", fontsize=14)
+        axarr[idx, 1].set_yticklabels([])
+    plt.tight_layout()
+    plt.show()
+
+
+def low_pass_filter(signal, wavelet_name, threshold):
+    """
+    Wavelet low-pass filtering using a threshold.
+
+    Currently configured to use 'periodic' mode.
+    See https://pywavelets.readthedocs.io/en/latest/regression/modes.html.
+    Uses soft thresholding.
+
+    :param signal: Signal as pd.Series
+    :param wavelet_name: One of the names in pywt.wavelist()
+    :param threshold: Coefficient threshold (>= passes)
+
+    :return: Smoothed signal as pd.Series, indexed like signal.index
+    """
+    threshold = threshold * np.nanmax(signal)
+    coeff = pywt.wavedec(signal, wavelet_name, mode="per" )
+    coeff[1:] = (pywt.threshold(i, value=threshold, mode="soft" ) for i in coeff[1:])
+    rec = pywt.waverec(coeff, wavelet_name, mode="per")
+    if rec.size > signal.size:
+        rec = rec[1:]
+    reconstructed_signal = pd.Series(rec, index=signal.index)
+    return reconstructed_signal
+
+
+def plot_low_pass(signal, wavelet_name, threshold):
+    """
+    Overlays signal with result of low_pass_filter()
+    """
+    fig, ax = plt.subplots(figsize=(12,8))
+    ax.plot(signal, color="b", alpha=0.5, label='original signal')
+    rec = low_pass_filter(signal, wavelet_name, threshold)
+    ax.plot(rec, 'k', label='DWT smoothing}', linewidth=2)
+    ax.legend()
+    ax.set_title('Removing High Frequency Noise with DWT', fontsize=18)
+    ax.set_ylabel('Signal Amplitude', fontsize=16)
+    ax.set_xlabel('Sample', fontsize=16)
+    plt.show()
+
