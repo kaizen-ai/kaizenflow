7b0134bbd355fc7f5adb851cc3d193a1f6c60c79
paul <smith.paul.anthony@gmail.com>
2019-08-12 11:32:10 -0700

    Add starting values for sampling.

--- event_study/best.py
+++ event_study/best.py
@@ -17,14 +17,18 @@ def best(y1, y2, prior_tau=1e-6, samples=1000):
         #   2. We typically expect the precision to be set very low so that the
         #      prior is very vague. The center is not so important provided the
         #      prior is sufficiently vague.
-        group1_mean = pm.Normal('group1_mean', mu=0, tau=prior_tau)
-        group2_mean = pm.Normal('group2_mean', mu=0, tau=prior_tau)
+        group1_mean = pm.Normal('group1_mean', mu=0, tau=prior_tau,
+                                testval=y1.mean())
+        group2_mean = pm.Normal('group2_mean', mu=0, tau=prior_tau,
+                                testval=y2.mean())
         # BDA3 suggests log std ~ Uniform as a diffuse (improper) prior
         # (See Sec. 2.8).
         # Here, we opt for the "super-vague" but proper analogue by default.
         # (see https://github.com/stan-dev/stan/wiki/Prior-Choice-Recommendations)
-        group1_log_std = pm.Normal('group1_log_std', mu=0, tau=prior_tau)
-        group2_log_std = pm.Normal('group2_log_std', mu=0, tau=prior_tau)
+        group1_log_std = pm.Normal('group1_log_std', mu=0, tau=prior_tau,
+                                   testval=pm.math.log(y1.std()))
+        group2_log_std = pm.Normal('group2_log_std', mu=0, tau=prior_tau,
+                                   testval=pm.math.log(y2.std()))
         # Convert from log_std to std
         group1_std = pm.Deterministic('group1_std', pm.math.exp(group1_log_std))
         group2_std = pm.Deterministic('group2_std', pm.math.exp(group2_log_std))
