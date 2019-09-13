66e2e4cda6180e4f93ac8e4e23e3b2058487af32
saggese <saggese@gmail.com>
2019-09-13 09:37:11 -0400

    Leftover from previous commit

--- dev_scripts/infra/ssh_tunnels.py
+++ dev_scripts/infra/ssh_tunnels.py
@@ -19,7 +19,7 @@ import os
 import signal
 
 import helpers.dbg as dbg
-import helpers.system_interaction as hsi
+import helpers.system_interaction as si
 import infra.ssh_config as ssh_cfg
 
 _LOG = logging.getLogger(__name__)
@@ -27,57 +27,66 @@ _LOG = logging.getLogger(__name__)
 # #############################################################################
 
 # Servers
-DEV_SERVER = '54.172.40.4'
-DB_SERVER = ''
+DEV_SERVER = "104.248.187.204"
+DB_SERVER = ""
 
 # Server ports
 MAPPING = {
-    "NGINX": ("Publish notebook server", DEV_SERVER, 8181),
-    "NETDATA": ("System performance", DEV_SERVER, 19999),
-    "JENKINS": ("Continuous integration", DEV_SERVER, 8111),
-    "UPSOURCE": ("Code review", DEV_SERVER, 8080),
+    # "JENKINS": ("Continuous integration", DEV_SERVER, 8111),
+    # "REVIEWBOARD": ("Code review", DEV_SERVER, 8080),
+    # "NGINX": ("Publish notebook server", DEV_SERVER, 8181),
+    # "NETDATA": ("System performance", DEV_SERVER, 19999),
 }
 
 DEFAULT_PORTS = sorted(MAPPING.keys())
 
 # Users
-DEFAULT_RSA_KEY = "~/.ssh/id_rsa"
-
-USERS = {
-    'gp': {
-        'key_path':
-        DEFAULT_RSA_KEY,
-        # Jupyter notebooks running at /data/gp_wd/src/particle{1, 2}
-        'ports': [("Jupyter1", DEV_SERVER, 9185),
-                  ("Jupyter2", DEV_SERVER, 9186)]
-}
+_DEFAULT_RSA_KEY = "~/.ssh/id_rsa"
 
 # ##############################################################################
+
 
 def _create_tunnel(server, port, user_name):
     _LOG.debug("server=%s port=%s user_name=%s", server, port, user_name)
     dbg.dassert_in(user_name, ssh_cfg.USERS)
-    key_path = ssh_cfg.USERS[user_name]['key_path']
+    key_path = ssh_cfg.USERS[user_name]["key_path"]
     key_path = os.path.expanduser(key_path)
     _LOG.debug("key_path=%s", key_path)
     dbg.dassert_exists(key_path)
-    cmd = "ssh -i {key_path} -f -nNT -L {port}:localhost:{port} {user_name}@" + server
+    cmd = (
+        "ssh -i {key_path} -f -nNT -L {port}:localhost:{port} {user_name}@"
+        + server
+    )
     cmd = cmd.format(user_name=user_name, key_path=key_path, port=port)
-    hsi.system(cmd, blocking=False)
+    si.system(cmd, blocking=False)
+
+
+# def _get_tunnel_info(user_name):
+#     """
+#     Return the list of services for a user. Each service is represented
+#     as a triple (symbolic name, IP, port).
+#     """
+#     info = [ssh_cfg.MAPPING[n] for n in ssh_cfg.DEFAULT_PORTS]
+#     info.extend(ssh_cfg.USERS[user_name]['ports'])
+#     return info
 
 
 def _get_tunnel_info(user_name):
-    """
-    Return the list of services for a user. Each service is represented
-    as a triple (symbolic name, IP, port).
-    """
-    info = [ssh_cfg.MAPPING[n] for n in ssh_cfg.DEFAULT_PORTS]
-    info.extend(ssh_cfg.USERS[user_name]['ports'])
+    server_name = si.get_server_name()
+    git_repo_name = git.get_repo_symbolic_name(super_module=True)
+    info = None
+    if user_name in ("gp", "saggese"):
+        if server_name == "":
+            info = [
+                ("Jupyter1", DEV_SERVER, 10001),
+                ("Mongodb", DEV_SERVER, 27017),
+            ]
+    dbg.dassert_is_not(info, None)
     return info
 
 
 def _parse_ps_output(cmd):
-    rc, txt = hsi.system_to_string(cmd, abort_on_error=False)
+    rc, txt = si.system_to_string(cmd, abort_on_error=False)
     _LOG.debug("txt=\n%s", txt)
     pids = []
     if rc == 0:
@@ -100,7 +109,8 @@ def _parse_ps_output(cmd):
 def _get_ssh_tunnel_process(port):
     _LOG.debug("port=%s", port)
     cmd = "ps ax | grep 'ssh -i' | grep localhost:{port} | grep -v grep".format(
-        port=port)
+        port=port
+    )
     pids, txt = _parse_ps_output(cmd)
     if len(pids) > 1:
         _LOG.debug("Expect a single process, instead got:\n%s", txt)
@@ -123,21 +133,31 @@ def _start_tunnels(user_name):
     for sym_name, server, port in info:
         pids = _get_ssh_tunnel_process(port)
         if not pids:
-            _LOG.info("Starting tunnel for service '%s' server=%s port=%s",
-                      sym_name, server, port)
+            _LOG.info(
+                "Starting tunnel for service '%s' server=%s port=%s",
+                sym_name,
+                server,
+                port,
+            )
             _create_tunnel(server, port, user_name)
         else:
             _LOG.warning(
                 "Tunnel for service '%s' on port %s already exist: skipping",
-                sym_name, port)
+                sym_name,
+                port,
+            )
 
 
 def _stop_tunnels(user_name):
     _LOG.debug("user_name=%s", user_name)
     info = _get_tunnel_info(user_name)
     for sym_name, server, port in info:
-        _LOG.info("Stopping tunnel for service '%s' server=%s port=%s",
-                  sym_name, server, port)
+        _LOG.info(
+            "Stopping tunnel for service '%s' server=%s port=%s",
+            sym_name,
+            server,
+            port,
+        )
         _kill_ssh_tunnel_process(port)
 
 
@@ -150,8 +170,9 @@ def _check_tunnels(user_name):
             msg = "exists with pid=%s" % pids
         else:
             msg = "doesn't exist"
-        _LOG.info("Service='%s' server=%s port=%s %s", sym_name, server, port,
-                  msg)
+        _LOG.info(
+            "Service='%s' server=%s port=%s %s", sym_name, server, port, msg
+        )
 
 
 def _kill_all_tunnel_processes():
@@ -174,39 +195,35 @@ def _kill_all_tunnel_processes():
 
 def _main():
     parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--user', type=str, action='store')
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("--user", type=str, action="store")
     parser.add_argument(
-        '--action',
+        "--action",
         required=True,
-        action='store',
-        choices="start stop check killall".split())
+        action="store",
+        choices="start stop check killall".split(),
+    )
     parser.add_argument(
         "-v",
         dest="log_level",
         default="INFO",
-        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-        help="Set the logging level")
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level",
+    )
     #
     args = parser.parse_args()
     dbg.init_logger(verb=args.log_level, use_exec_path=True)
     if args.user:
-        user = args.user
+        user_name = args.user
     else:
-        _, user = hsi.system_to_string('whoami')
-    _LOG.info("user=%s", user)
-    dbg.dassert_in(
-        user,
-        ssh_cfg.USERS,
-        msg='There is no user with name %s. Please add it to infra/ssh_config.py'
-        % user)
+        user_name = si.get_user_name()
     if args.action == "start":
-        _start_tunnels(user)
+        _start_tunnels(user_name)
     elif args.action == "stop":
-        _stop_tunnels(user)
+        _stop_tunnels(user_name)
     elif args.action == "check":
-        _check_tunnels(user)
+        _check_tunnels(user_name)
     elif args.action == "killall":
         _kill_all_tunnel_processes()
     else:
