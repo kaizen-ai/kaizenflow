0f1490378eca17877e209ac3d82294ef443872e3
saggese <saggese@gmail.com>
2019-09-15 09:40:37 -0400

    Add draft of notebooks for demonstrating some ARIMA investigation

---
+++ core/notebooks/gallery_examples.py
@@ -0,0 +1,45 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: Python [conda env:develop] *
+#     language: python
+#     name: conda-env-develop-py
+# ---
+
+# %% [markdown]
+# ## Imports
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+# %matplotlib inline
+
+import numpy as np
+import pandas as pd
+
+import core.explore as exp
+import helpers.unit_test as hut
+
+# %% [markdown]
+# ## display_df
+
+# %%
+np.random.seed(100)
+
+x = 5 * np.random.randn(100)
+y = x + np.random.randn(*x.shape)
+df = pd.DataFrame()
+df["x"] = x
+df["y"] = y
+
+df.head()
+
+exp.regress_series(df["x"], df["y"], use_intercept=True)
+
+# %%
