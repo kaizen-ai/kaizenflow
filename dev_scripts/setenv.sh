05b13dcb4637bb607e67b941971a0484f2473ed6
saggese <saggese@gmail.com>
2019-09-23 11:41:49 -0400

    PartTask329: check for conda and python version

--- dev_scripts/setenv.sh
+++ dev_scripts/setenv.sh
@@ -11,8 +11,39 @@ echo "Starting $EXEC_NAME ..."
 DIR=$(dirname "$EXEC_NAME")
 EXEC_PATH=$(cd $DIR ; pwd -P)
 
+echo "rc=$rc"
+
+# Make sure conda works.
+conda -V
+rc=$?
+if [[ $rc != 0 ]]; then
+  echo "conda not working: exiting"
+  return $rc
+fi;
+
 # Make sure python works.
 python -V
+rc=$?
+if [[ $rc != 0 ]]; then
+  echo "python not working: exiting"
+  return $rc
+fi;
+
+# Check python version.
+python - <<END
+import sys
+pyv = sys.version_info[:]
+pyv_as_str = ".".join(map(str, pyv[:3]))
+print("python version='%s'" % pyv_as_str)
+is_too_old = pyv_as_str < "3.6"
+print("is_too_old=%s" % is_too_old)
+sys.exit(is_too_old);
+END
+rc=$?
+if [[ $rc != 0 ]]; then
+  echo "python is too old: exiting"
+  return $rc
+fi;
 
 # Create the script to execute calling python.
 DATETIME=$(date "+%Y%m%d-%H%M%S")
@@ -21,7 +52,7 @@ SCRIPT_FILE=/tmp/setenv.${DATETIME}.sh
 echo "SCRIPT_FILE=$SCRIPT_FILE"
 $EXEC_PATH/_setenv.py --output_file $SCRIPT_FILE $*
 
-# Execute the script
+# Execute the newly generated script.
 source $SCRIPT_FILE
 
 echo "... $EXEC_NAME done"
