5923b13b922725484f105fab077a9662df37e4b6
Sergey <gad26032@gmail.com>
2019-09-25 17:16:21 +0300

    commodity_research #302 filler added

---
+++ vendors/eurostat/filler_versions.py
@@ -0,0 +1,16 @@
+from vendors.eurostat.base_clases import EurostatFileSaver
+from data import data_config
+
+EUROSTAT_FILLERS = {
+    'TASK302_EUROSTAT_RAW_DATA': {
+        'class': EurostatFileSaver,
+        'settings': {
+            'dst_dir': data_config.TASK302_RAW_DATA,
+            'paranoid': True,
+        },
+        'optional': {
+            'root_url': 'https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?dir=data&sort=1&sort=2&start=all',
+            }
+    },
+
+}
