b902adcdb560dc1b0631947b4eecc3b1e991757f
paul <smith.paul.anthony@gmail.com>
2019-09-26 17:47:07 -0700

    LemTask54 clean-up.

--- core/dataflow.py
+++ core/dataflow.py
@@ -3,10 +3,14 @@ import copy
 import itertools
 import logging
 
+import matplotlib
+import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 from sklearn import linear_model
 
+# Import style usually to be avoided, but in this case it may make sense.
+from core.dataflow_core import *
 import core.features as ftrs
 import core.finance as fin
 import helpers.dbg as dbg
@@ -17,241 +21,18 @@ _LOG = logging.getLogger(__name__)
 
 
 # #############################################################################
-# Core node classes
+# Visualization
 # #############################################################################
 
 
-class AbstractNode:
-    """
-    Abstract node class for creating DAG pipelines of functions.
-
-    Common use case: Nodes wrap functions with a common method (e.g., `fit`).
-
-    This class provides some convenient introspection (input/output names)
-    accessors and, importantly, a unique identifier (`nid`) for building
-    graphs of nodes. The `nid` is also useful for config purposes.
-
-    For nodes requiring fit/transform, we can subclass / provide a mixin with
-    the desired methods.
-    """
-
-    def __init__(self, nid, inputs=None, outputs=None):
-        """
-        :param nid: node identifier. Should be unique in a graph.
-        :param inputs: list-like string names of input_names.
-        """
-        dbg.dassert_isinstance(nid, str)
-        if not nid:
-            _LOG.warning("Empty string chosen for unique nid!")
-        self._nid = nid
-        self._inputs = self._init_validation_helper(inputs)
-        self._outputs = self._init_validation_helper(outputs)
-
-    def _init_validation_helper(self, l):
-        if l is None:
-            return []
-        for item in l:
-            dbg.dassert_isinstance(item, str)
-        return l
-
-    @property
-    def nid(self):
-        return self._nid
-
-    @property
-    def input_names(self):
-        return self._inputs
-
-    @property
-    def output_names(self):
-        return self._outputs
-
-    def _info(self, **kwargs):
-        _LOG.info("input_names: %s", self.input_names)
-        _LOG.info("output_names: %s", self.output_names)
-        _LOG.info("nid: %s", self._nid)
-        dummy_output = {}
-        for output in self.output_names:
-            dummy_output[output] = None
-        return dummy_output
+def draw(graph):
+    nx.draw_networkx(graph,
+                     pos=nx.spectral_layout(graph),
+                     node_size=3000,
+                     arrowsize=30,
+                     width=1.5)
 
 
-class Node(AbstractNode):
-    """
-    Concrete node that also stores its output when run.
-    """
-
-    def __init__(self, nid, inputs=None, outputs=None):
-        """
-        :param nid: node identifier. Should be unique in a graph.
-        :param inputs: list-like string names of input_names.
-        :param outputs: list-like string names of output_names. The node is assumed
-            to store the last output.
-        """
-        super().__init__(nid=nid, inputs=inputs, outputs=outputs)
-        self._output_vals = {}
-
-    def store_output(self, method, name, value):
-        dbg.dassert_in(
-            name,
-            self.output_names,
-            "%s is not an output of node %s!",
-            name,
-            self.nid,
-        )
-        if method not in self._output_vals:
-            self._output_vals[method] = {}
-        self._output_vals[method][name] = value
-
-    def get_output(self, method, name):
-        dbg.dassert_in(
-            name,
-            self.output_names,
-            "%s is not an output of node %s!",
-            name,
-            self.nid,
-        )
-        dbg.dassert_in(
-            method,
-            self._output_vals.keys(),
-            "%s of node %s has no output!",
-            method,
-            self.nid,
-        )
-        return self._output_vals[method][name]
-
-    def get_outputs(self, method):
-        dbg.dassert_in(method, self._output_vals.keys())
-        return self._output_vals[method]
-
-
-# #############################################################################
-# Graph class for creating and executing a DAG of nodes.
-# #############################################################################
-
-
-# TODO(Paul): Consider renaming `DAG` once we enforce the invariant.
-class DAG:
-    """
-    Class for building pipeline graphs using Nodes.
-
-    The Graph is directed and should be a DAG.
-    TODO(Paul): enforce this when trying to added edges.
-
-    The Graph manages node execution.
-    """
-
-    def __init__(self):
-        self._dag = nx.DiGraph()
-
-    @property
-    def dag(self):
-        return self._dag
-
-    def add_node(self, node):
-        """
-        Adds `node` to the graph.
-
-        Relies upon the unique nid for identifying the node.
-
-        :param node: Node object
-        """
-        dbg.dassert_isinstance(
-            node, Node, "Only graphs of class `Node` are supported!"
-        )
-        self._dag.add_node(node.nid, stage=node)
-
-    def get_node(self, nid):
-        """
-        Convenience node accessor.
-
-        :param nid: unique string node id
-        :return: Node object
-        """
-        return self._dag.nodes[nid]["stage"]
-
-    # TODO(Paul): Automatically infer edge labels when possible (e.g., SISO).
-    def connect(self, parent, child):
-        """
-        Adds a directed edge from parent node output to child node input.
-
-        Raises if the requested edge is invalid.
-
-        If this is called multiple times on the same nid's but with different
-        output/input pairs, the additional input/output pairs are simply added
-        to the existing edge (the previous ones are not overwritten).
-
-        :param parent: tuple of the form (nid, output)
-        :param child: tuple of the form (nid, input)
-        """
-        dbg.dassert_in(parent[1], self.get_node(parent[0]).output_names)
-        dbg.dassert_in(child[1], self.get_node(child[0]).input_names)
-        kwargs = {child[1]: parent[1]}
-        self._dag.add_edge(parent[0], child[0], **kwargs)
-        if not nx.is_directed_acyclic_graph(self.dag):
-            _LOG.warning(
-                "Creating edge %s -> %s failed because it creates a cycle!",
-                parent[0],
-                child[0],
-            )
-            self._dag.remove_edge(parent[0], child[0])
-
-    def _run_node(self, nid, method):
-        """
-        Runs a single node.
-
-        This method DOES NOT run (or re-run) ancestors of `nid`.
-        """
-        _LOG.debug("Node nid=`%s` executing method `%s`...", nid, method)
-        kwargs = {}
-        for pre in self._dag.predecessors(nid):
-            kvs = self._dag.edges[[pre, nid]]
-            pre_node = self.get_node(pre)
-            for k, v in kvs.items():
-                # Retrieve output from store.
-                kwargs[k] = pre_node.get_output(method, v)
-        _LOG.debug("kwargs are %s", kwargs)
-        node = self.get_node(nid)
-        output = getattr(node, method)(**kwargs)
-        for out in node.output_names:
-            node.store_output(method, out, output[out])
-        return self.get_node(nid).get_outputs(method)
-
-    def run_dag(self, method):
-        """
-        Executes entire pipeline.
-
-        Nodes are run according to a topological sort.
-
-        :param method: Method of class `Node` (or subclass) to be executed for
-            the entire DAG.
-        """
-        sinks = []
-        for nid in nx.topological_sort(self._dag):
-            if any(True for _ in self._dag.predecessors(nid)):
-                sinks.append(nid)
-            self._run_node(method=method, nid=nid)
-        return [self.get_node(sink).get_outputs(method) for sink in sinks]
-
-    def run_leq_node(self, nid, method):
-        """
-        Executes pipeline only up to (and including) `node` and returns output.
-
-        "leq" refers to the partial ordering on the vertices. This method
-        runs a node if and only if there is a directed path from the node to
-        `nid`. Nodes are run according to a topological sort.
-        """
-        ancestors = filter(
-            lambda x: x in nx.ancestors(self._dag, nid),
-            nx.topological_sort(self._dag),
-        )
-        nids = itertools.chain(ancestors, [nid])
-        for n in nids:
-            self._run_node(n, method)
-        return self.get_node(nid).get_outputs(method)
-
-
-# TODO(Paul): Move (most of) these to a separate library
 # #############################################################################
 # DataFrame manipulation nodes
 # #############################################################################
@@ -280,7 +61,6 @@ class ReadData(Node):
             train_df = self.df.iloc[self._train_idxs]
         else:
             train_df = self.df
-        self._output_vals
         return {self.output_names[0]: train_df}
 
     def set_test_idxs(self, test_idxs):
@@ -332,8 +112,11 @@ class ReadDataFromKibot(ReadData):
         return super().fit()
 
 
-# TODO(Paul): Rename SISO to Siso
-class StatelessSISONode(Node):
+# TODO(Paul): Abstract? Should we?
+class StatelessSisoNode(Node):
+    """
+    Stateless Single-Input Single-Output node.
+    """
     def __init__(self, nid):
         super().__init__(nid, inputs=["input"], outputs=["output"])
 
@@ -361,7 +144,7 @@ class StatelessSISONode(Node):
 # TODO(Paul): Write a Node builder to automatically generate these from
 # functions.
 # TODO(gp): Pass "ret_0" and "open" through constructor.
-class PctReturns(StatelessSISONode):
+class PctReturns(StatelessSisoNode):
     def __init__(self, nid):
         super().__init__(nid)
 
@@ -372,7 +155,7 @@ class PctReturns(StatelessSISONode):
         return df, info
 
 
-class Zscore(StatelessSISONode):
+class Zscore(StatelessSisoNode):
     def __init__(self, nid, style, com):
         super().__init__(nid)
         self.style = style
@@ -385,7 +168,7 @@ class Zscore(StatelessSISONode):
         return df_out, info
 
 
-class FilterAth(StatelessSISONode):
+class FilterAth(StatelessSisoNode):
     def __init__(self, nid):
         super().__init__(nid)
 
@@ -397,7 +180,7 @@ class FilterAth(StatelessSISONode):
 
 # TODO(Paul, GP): Confusing interface. If we only sent out the transformed
 # variables, then we wouldn't need `get_x_vars`.
-class ComputeLaggedFeatures(StatelessSISONode):
+class ComputeLaggedFeatures(StatelessSisoNode):
     def __init__(self, nid, y_var, delay_lag, num_lags):
         super().__init__(nid)
         self.y_var = y_var
@@ -421,7 +204,7 @@ class ComputeLaggedFeatures(StatelessSISONode):
 
 
 class Model(Node):
-    # NOTE: y_var before x_vars?
+    # TODO(GP): y_var before x_vars? Probably should switch.
     def __init__(self, nid, y_var, x_vars):
         super().__init__(nid, inputs=["input"], outputs=["output"])
         self.y_var = y_var
---
+++ core/dataflow_core.py
@@ -0,0 +1,247 @@
+import itertools
+import logging
+
+import networkx as nx
+
+import helpers.dbg as dbg
+
+_LOG = logging.getLogger(__name__)
+
+
+# #############################################################################
+# Core node classes
+# #############################################################################
+
+
+# TODO(Paul): Make this abstract with ABC
+class AbstractNode:
+    """
+    Abstract node class for creating DAG pipelines of functions.
+
+    Common use case: Nodes wrap functions with a common method (e.g., `fit`).
+
+    This class provides some convenient introspection (input/output names)
+    accessors and, importantly, a unique identifier (`nid`) for building
+    graphs of nodes. The `nid` is also useful for config purposes.
+
+    For nodes requiring fit/transform, we can subclass / provide a mixin with
+    the desired methods.
+    """
+
+    def __init__(self, nid, inputs=None, outputs=None):
+        """
+        :param nid: node identifier. Should be unique in a graph.
+        :param inputs: list-like string names of input_names.
+        """
+        dbg.dassert_isinstance(nid, str)
+        if not nid:
+            _LOG.warning("Empty string chosen for unique nid!")
+        self._nid = nid
+        self._inputs = self._init_validation_helper(inputs)
+        self._outputs = self._init_validation_helper(outputs)
+
+    def _init_validation_helper(self, l):
+        if l is None:
+            return []
+        for item in l:
+            dbg.dassert_isinstance(item, str)
+        return l
+
+    @property
+    def nid(self):
+        return self._nid
+
+    @property
+    def input_names(self):
+        return self._inputs
+
+    @property
+    def output_names(self):
+        return self._outputs
+
+    def _info(self, **kwargs):
+        _LOG.info("input_names: %s", self.input_names)
+        _LOG.info("output_names: %s", self.output_names)
+        _LOG.info("nid: %s", self._nid)
+        dummy_output = {}
+        for output in self.output_names:
+            dummy_output[output] = None
+        return dummy_output
+
+
+class Node(AbstractNode):
+    """
+    Concrete node that also stores its output when run.
+    """
+
+    def __init__(self, nid, inputs=None, outputs=None):
+        """
+        :param nid: node identifier. Should be unique in a graph.
+        :param inputs: list-like string names of input_names.
+        :param outputs: list-like string names of output_names. The node is assumed
+            to store the last output.
+        """
+        super().__init__(nid=nid, inputs=inputs, outputs=outputs)
+        self._output_vals = {}
+
+    def store_output(self, method, name, value):
+        dbg.dassert_in(
+            name,
+            self.output_names,
+            "%s is not an output of node %s!",
+            name,
+            self.nid,
+        )
+        if method not in self._output_vals:
+            self._output_vals[method] = {}
+        self._output_vals[method][name] = value
+
+    def get_output(self, method, name):
+        dbg.dassert_in(
+            name,
+            self.output_names,
+            "%s is not an output of node %s!",
+            name,
+            self.nid,
+        )
+        dbg.dassert_in(
+            method,
+            self._output_vals.keys(),
+            "%s of node %s has no output!",
+            method,
+            self.nid,
+        )
+        return self._output_vals[method][name]
+
+    def get_outputs(self, method):
+        dbg.dassert_in(method, self._output_vals.keys())
+        return self._output_vals[method]
+
+
+# #############################################################################
+# Graph class for creating and executing a DAG of nodes.
+# #############################################################################
+
+
+class DAG:
+    """
+    Class for building pipeline graphs using Nodes.
+
+    The DAG manages node execution and storage of outputs (within executed
+    nodes).
+
+    TODO(Paul): Think about how subgraphs should fit into this framework.
+    """
+    def __init__(self):
+        self._dag = nx.DiGraph()
+
+    @property
+    def dag(self):
+        return self._dag
+
+    def add_node(self, node):
+        """
+        Adds `node` to the graph.
+
+        Relies upon the unique nid for identifying the node.
+
+        :param node: Node object
+        """
+        dbg.dassert_isinstance(
+            node, Node, "Only graphs of class `Node` are supported!"
+        )
+        self._dag.add_node(node.nid, stage=node)
+
+    def get_node(self, nid):
+        """
+        Convenience node accessor.
+
+        :param nid: unique string node id
+        :return: Node object
+        """
+        return self._dag.nodes[nid]["stage"]
+
+    # TODO(Paul): Automatically infer edge labels when possible (e.g., SISO).
+    def connect(self, parent, child):
+        """
+        Adds a directed edge from parent node output to child node input.
+
+        Raises if the requested edge is invalid or forms a cycle.
+
+        If this is called multiple times on the same nid's but with different
+        output/input pairs, the additional input/output pairs are simply added
+        to the existing edge (the previous ones are not overwritten).
+
+        :param parent: tuple of the form (nid, output)
+        :param child: tuple of the form (nid, input)
+        """
+        dbg.dassert_in(parent[1], self.get_node(parent[0]).output_names)
+        dbg.dassert_in(child[1], self.get_node(child[0]).input_names)
+        kwargs = {child[1]: parent[1]}
+        self._dag.add_edge(parent[0], child[0], **kwargs)
+        if not nx.is_directed_acyclic_graph(self.dag):
+            _LOG.warning(
+                "Creating edge %s -> %s failed because it creates a cycle!",
+                parent[0],
+                child[0],
+            )
+            self._dag.remove_edge(parent[0], child[0])
+
+    def _run_node(self, nid, method):
+        """
+        Runs a single node.
+
+        This method DOES NOT run (or re-run) ancestors of `nid`.
+        """
+        _LOG.debug("Node nid=`%s` executing method `%s`...", nid, method)
+        kwargs = {}
+        for pre in self._dag.predecessors(nid):
+            kvs = self._dag.edges[[pre, nid]]
+            pre_node = self.get_node(pre)
+            for k, v in kvs.items():
+                # Retrieve output from store.
+                kwargs[k] = pre_node.get_output(method, v)
+        _LOG.debug("kwargs are %s", kwargs)
+        node = self.get_node(nid)
+        output = getattr(node, method)(**kwargs)
+        for out in node.output_names:
+            node.store_output(method, out, output[out])
+        # Convenient for experiments/debugging, but not needed for internal use.
+        # Perhaps we should expose a public `run_node` that just invokes
+        # `_run_node` and then returns the output as below.
+        return self.get_node(nid).get_outputs(method)
+
+    def run_dag(self, method):
+        """
+        Executes entire pipeline.
+
+        Nodes are run according to a topological sort.
+
+        :param method: Method of class `Node` (or subclass) to be executed for
+            the entire DAG.
+        """
+        sinks = []
+        for nid in nx.topological_sort(self._dag):
+            # Collect all sinks so that we can easily output their data after
+            # all nodes have been run.
+            if any(True for _ in self._dag.predecessors(nid)):
+                sinks.append(nid)
+            self._run_node(method=method, nid=nid)
+        return [self.get_node(sink).get_outputs(method) for sink in sinks]
+
+    def run_leq_node(self, nid, method):
+        """
+        Executes pipeline up to (and including) `node` and returns output.
+
+        "leq" refers to the partial ordering on the vertices. This method
+        runs a node if and only if there is a directed path from the node to
+        `nid`. Nodes are run according to a topological sort.
+        """
+        ancestors = filter(
+            lambda x: x in nx.ancestors(self._dag, nid),
+            nx.topological_sort(self._dag),
+        )
+        nids = itertools.chain(ancestors, [nid])
+        for n in nids:
+            self._run_node(n, method)
+        return self.get_node(nid).get_outputs(method)
