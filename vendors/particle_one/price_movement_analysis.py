1f23c6f64cfeb68dbf30e2cbf6fa1dacf6b073b1
Julia <julia@particle.one>
2019-10-14 23:21:04 +0000

    PartTask218 Compute returns instead of diffs, address other comments from the review

--- vendors/particle_one/price_movement_analysis.py
+++ vendors/particle_one/price_movement_analysis.py
@@ -1,31 +1,56 @@
+import logging
+from typing import Dict, Iterable
+
 import pandas as pd
 
-import core.signal_processing as sp
+import core.signal_processing as sigp
+import helpers.dbg as dbg
 
-TAU = 2
+import vendors.kibot.utils as kut
+
+_LOG = logging.getLogger(__name__)
 
 
-def get_zscored_prices_diff(price_dict_df, symbol, tau=TAU):
-    prices_symbol = price_dict_df[symbol]
-    prices_diff = prices_symbol["close"] - prices_symbol["open"]
-    zscored_prices_diff = sp.rolling_zscore(prices_diff, tau)
-    zscored_prices_diff.head()
-    abs_zscored_prices_diff = zscored_prices_diff.abs()
-    return abs_zscored_prices_diff
+TAU = 18
+
+
+def get_zscored_returns(
+    price_df_dict: Dict[str, pd.DataFrame],
+    symbol: str,
+    period: str, 
+    tau: int = TAU,
+    demean: bool = False,
+):
+    dbg.dassert_in(period, ['daily', 'minutely'])
+    prices_symbol = price_df_dict[symbol]
+    if period == 'minutely':
+        rets = kut.compute_ret_0_from_1min_prices(prices_symbol, 'pct_change')
+    else:
+        rets = kut.compute_ret_0_from_daily_prices(prices_symbol, 'open', 'pct_change')
+    zscored_rets = sigp.rolling_zscore(rets, tau, demean=demean)
+    _LOG.debug("zscored_rets=\n%s", zscored_rets.head())
+    abs_zscored_rets = zscored_rets.abs()
+    return abs_zscored_rets
 
 
 def get_top_movements_by_group(
-    price_dict_df, commodity_symbols_kibot, group, n_movements=100
+    price_df_dict: Dict[str, pd.DataFrame],
+    commodity_symbols_kibot: Dict[str, Iterable],
+    group: str,
+    period: str,
+    n_movements: int = 100,
 ):
-    zscored_diffs = []
+    zscored_returns = []
     for symbol in commodity_symbols_kibot[group]:
-        zscored_diff = get_zscored_prices_diff(price_dict_df, symbol)
-        zscored_diffs.append(zscored_diff)
-    zscored_diffs = pd.concat(zscored_diffs, axis=1)
-    mean_zscored_diffs = zscored_diffs.mean(axis=1, skipna=True)
-    return mean_zscored_diffs.sort_values(ascending=False).head(n_movements)
+        zscored_ret = get_zscored_returns(price_df_dict, symbol, period)
+        zscored_returns.append(zscored_ret)
+    zscored_returns = pd.concat(zscored_returns, axis=1)
+    mean_zscored_rets = zscored_returns.mean(axis=1, skipna=True)
+    return mean_zscored_rets.sort_values(ascending=False).head(n_movements)
 
 
-def get_top_movements_for_symbol(price_dict_df, symbol, tau=TAU, n_movements=100):
-    zscored_diffs = get_zscored_prices_diff(price_dict_df, symbol, tau=tau)
-    return zscored_diffs.sort_values(ascending=False).head(n_movements)
+def get_top_movements_for_symbol(
+    price_df_dict: Dict[str, pd.DataFrame], symbol: str, period: str, n_movements: int = 100
+):
+    zscored_rets = get_zscored_returns(price_df_dict, symbol, period)
+    return zscored_rets.sort_values(ascending=False).head(n_movements)
