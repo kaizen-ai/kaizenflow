89d91d33e5a8eecc82378b283e6463816a36b72b
gad26032 <malanin@particle.one>
2019-10-16 15:46:37 +0300

    PartTask302 review notes, comments, lint imports

--- vendors/eurostat/base_classes.py
+++ vendors/eurostat/base_classes.py
@@ -1,15 +1,30 @@
 import os
 from typing import Tuple, Generator, Dict, Union
+
 import bs4
 import requests
-import generalized_filler.base_classes as gf_bc
 from tqdm import tqdm
+
+import amp.helpers.dbg as dbg
+import generalized_filler.base_classes as gf_bc
 
 
 class EurostatFileFiller(gf_bc.FileFiller):
     """
-    Saves data using root_url.
-    Search all files from root_url using pattern *.tsv.gz
+    Saves data using FileFiller backend.
+    Search all files from root_url.
+
+    runnable script:
+    generalized_filler/filler.py
+
+    usage:
+    python generalized_filler/filler.py \
+    --filler_version SOME_FILLER_VERSION \
+    --class_version v2
+
+
+    - SOME_FILLER_VERSION can be found in filler_versions.py.
+    On the same module level where places module with current class.
     """
 
     def __init__(self,
@@ -30,13 +45,21 @@ class EurostatFileFiller(gf_bc.FileFiller):
         return status, msg
 
     def data_reader(self) -> Generator[Dict[str, Union[str, bytes]], None, None]:
+        """
+        # Find all the links in the main page called "Download" and retrieve the file which is a `.tsv.gz` file.
+        """
         r = requests.get(self.root_url)
         data = r.content
         soup = bs4.BeautifulSoup(data, 'html5lib')
         soup_a = soup.find_all('a')
+        dbg.dassert_lte(1, len(soup_a))
+        # We expect that all files that we interest in can be downloaded using hrefs.
+        # Text of each href must be equal to 'Download'.
+        # Check root_url to see how page looks like.
         list_a = [i for i in filter(lambda x: x.text == 'Download', soup_a)]
         for link in tqdm(list_a):
             url = link.get('href')
+            # url example: https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=data%2Faact_ali01.tsv.gz
             file_name = url.split('%2F')[-1]
             rr = requests.get(url)
             yield {"data": rr.content, 'file_name': file_name}
