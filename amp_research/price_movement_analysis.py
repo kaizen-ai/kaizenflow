50928fd9fedc7c11140ac15ea6b9b54230657800
Julia <julia@particle.one>
2019-10-17 19:54:27 +0300

    PartTask218: Remove assertion, remove quotes in error messages

--- amp_research/price_movement_analysis.py
+++ amp_research/price_movement_analysis.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 import core.explore as coex
 import core.signal_processing as sigp
-import helpers.dbg as dbg
 import vendors.kibot.utils as kut
 
 _LOG = logging.getLogger(__name__)
@@ -18,8 +17,7 @@ def compute_kibot_returns(prices: pd.DataFrame, period: str):
         rets = kut.compute_ret_0_from_daily_prices(prices, "open", "log_rets")
     else:
         raise ValueError(
-            'Only "daily" and "minutely" periods are supported, ' "passed %s",
-            period,
+            'Only daily and minutely"periods are supported, ' "passed %s", period
         )
     return rets
 
@@ -78,7 +76,6 @@ def get_top_movements_for_symbol(
 
 
 def _choose_movements(zscored_rets, sign):
-    dbg.dassert_in(sign, ["pos", "neg", "all"])
     if sign == "pos":
         zscored_rets = zscored_rets.loc[zscored_rets >= 0]
     elif sign == "neg":
@@ -87,7 +84,7 @@ def _choose_movements(zscored_rets, sign):
         zscored_rets = zscored_rets.abs()
     else:
         raise ValueError(
-            'Only ["pos", "neg", "all"] signs ' "are supported, passed %s", sign
+            "Only pos, neg, all signs are supported, passed %s", sign
         )
     return zscored_rets
 
@@ -99,6 +96,6 @@ def _get_order(sign: str):
         ascending = False
     else:
         raise ValueError(
-            'Only ["pos", "neg", "all"] signs ' "are supported, passed %s", sign
+            "Only pos, neg, all signs are supported, passed %s", sign
         )
     return ascending
