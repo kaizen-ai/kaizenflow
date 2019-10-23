1a17fb0533b5b35a6702d035536664a6c07dabc2
Julia <julia@particle.one>
2019-10-23 14:56:39 +0300

    PartTask418: Fix for some of the cases, add mode parameter to control recursiveness.

--- helpers/s3.py
+++ helpers/s3.py
@@ -3,7 +3,6 @@
 
 import helpers.s3 as hs3
 """
-import os
 from typing import List
 
 import boto3
@@ -20,14 +19,20 @@ def get_path():
     return s3_path
 
 
-def listdir(s3_path: str) -> List[str]:
+def listdir(s3_path: str, mode: str = "recursive") -> List[str]:
     """
     List files in s3 directory.
 
-    :param s3_path: The path to s3 directory
-    :return:
+    :param s3_path: the path to s3 directory
+    :param mode: recursive or non-recursive
+        If recursive, will list all files in the directory
+        recursively. If non-recursive, will list only the next level of
+        the directory.
+    :return: list of file names
     """
     AMAZON_MAX_INT = 2147483647
+    if not s3_path.endswith("/"):
+        s3_path = s3_path + "/"
     split_path = s3_path.split("/")
     s3_bucket = split_path[2]
     dir_path = "/".join(split_path[3:])
@@ -38,8 +43,18 @@ def listdir(s3_path: str) -> List[str]:
     contents = s3_objects["Contents"]
     file_names = [cont["Key"] for cont in contents]
     file_names = [
-        os.path.basename(file_name)
+        file_name.replace(dir_path, "", 1)
         for file_name in file_names
-        if os.path.dirname(file_name) == dir_path
+        if file_name.startswith(dir_path)
     ]
+    if mode == "recursive":
+        file_names = [file_name.split("/")[0] for file_name in file_names]
+        file_names = list(set(file_names))
+    elif mode == "non-recursive":
+        pass
+    else:
+        raise ValueError(
+            "Only recursive and non-recursive modes are " "supported, passed %s",
+            mode,
+        )
     return file_names
