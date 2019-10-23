6d705f0447d7fbfe057b1e7baac43f6f598972d5
Julia <julia@particle.one>
2019-10-23 21:45:38 +0300

    PartTask269: Remove resampling and aggregation

--- vendors/kibot/PartTask269_liquidity_analysis_utils.py
+++ vendors/kibot/PartTask269_liquidity_analysis_utils.py
@@ -15,45 +15,6 @@ _LOG = logging.getLogger(__name__)
 
 KIBOT_VOL = "vol"
 
-# Offset descriptions from
-# https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
-_PANDAS_OFFSET_DESCRIPTIONS = {
-    "B": "business day",
-    "C": "custom business day",
-    "D": "calendar day",
-    "W": "weekly",
-    "M": "month end",
-    "SM": "semi-month end",
-    "BM": "business month end",
-    "CBM": "custom business month end",
-    "MS": "month start",
-    "SMS": "semi-month start",
-    "BMS": "business month start",
-    "CBMS": "custom business month start",
-    "Q": "quarter end",
-    "BQ": "business quarter end",
-    "QS": "quarter start",
-    "BQS": "business quarter start",
-    "A": "year end",
-    "Y": "year end",
-    "BA": "business year end",
-    "BY": "business year end",
-    "AS": "year start",
-    "YS": "year start",
-    "BAS": "business year start",
-    "BYS": "business year start",
-    "BH": "business hour",
-    "H": "hourly",
-    "T": "minutely",
-    "min": "minutely",
-    "S": "secondly",
-    "L": "milliseconds",
-    "ms": "milliseconds",
-    "U": "microseconds",
-    "us": "microseconds",
-    "N": "nanoseconds",
-}
-
 
 def _get_kibot_reader(
     frequency: str, symbol: str, n_rows: Optional[int] = None
@@ -123,8 +84,6 @@ class TimeSeriesStudy:
         time_series: pd.Series,
         freq_name: str,
         data_name: Optional[str] = None,
-        resample_freq: Optional[str] = None,
-        resample_agg_func: Optional[str] = None,
     ):
         """
         :param time_series: pd.Series for which the study needs to be
@@ -133,23 +92,11 @@ class TimeSeriesStudy:
             titles (for example, 'daily').
         :param data_name: The name of the data to add to plot titles
             (for example, symbol).
-        :param resample_freq: pandas frequency passed to the resample
-            function. The list of supported frequencies:
-            https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
-        :param resample_agg_func: Aggregation function applied to the
-            resample
         """
         self._time_series = time_series
         self._ts_name = time_series.name
         self._data_name = data_name
         self._freq_name = freq_name
-        self._resample_freq = resample_freq
-        self._agg_func = resample_agg_func
-        if self._resample_freq:
-            self._time_series = getattr(
-                self._time_series.resample(resample_freq), self._agg_func
-            )()
-            self._check_freq_name()
 
     def plot_time_series(self,):
         self._time_series.plot()
@@ -211,19 +158,6 @@ class TimeSeriesStudy:
             self._time_series.index, "The index should be monotonic increasing"
         )
 
-    def _check_freq_name(self):
-        if self._resample_freq:
-            freq_name = _PANDAS_OFFSET_DESCRIPTIONS[self._resample_freq]
-            if freq_name.lower() != self._freq_name.lower():
-                _LOG.warning(
-                    "You are resampling in %s frequency. "
-                    "Changing frequency name on plots from %s to %s.",
-                    self._resample_freq,
-                    self._freq_name,
-                    freq_name,
-                )
-                self._freq_name = freq_name
-
     @property
     def _title_suffix(self):
         if self._data_name is not None:
@@ -238,17 +172,11 @@ class TimeSeriesDailyStudy(TimeSeriesStudy):
         time_series: pd.Series,
         freq_name: Optional[str] = None,
         data_name: Optional[str] = None,
-        resample_freq: Optional[str] = None,
-        resample_agg_func: Optional[str] = None,
     ):
         if not freq_name:
             freq_name = "daily"
         super(TimeSeriesDailyStudy, self).__init__(
-            time_series=time_series,
-            freq_name=freq_name,
-            data_name=data_name,
-            resample_freq=resample_freq,
-            resample_agg_func=resample_agg_func,
+            time_series=time_series, freq_name=freq_name, data_name=data_name
         )
 
     def execute(self):
@@ -264,17 +192,11 @@ class TimeSeriesMinuteStudy(TimeSeriesStudy):
         time_series: pd.Series,
         freq_name: Optional[str] = None,
         data_name: Optional[str] = None,
-        resample_freq: Optional[str] = None,
-        resample_agg_func: Optional[str] = None,
     ):
         if not freq_name:
             freq_name = "minutely"
         super(TimeSeriesMinuteStudy, self).__init__(
-            time_series=time_series,
-            freq_name=freq_name,
-            data_name=data_name,
-            resample_freq=resample_freq,
-            resample_agg_func=resample_agg_func,
+            time_series=time_series, freq_name=freq_name, data_name=data_name
         )
 
     def execute(self):
