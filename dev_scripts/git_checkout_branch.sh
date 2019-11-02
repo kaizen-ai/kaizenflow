8a09d21ab4cb6dcb18b8ab3fc29c6edd53dd2756
saggese <saggese@gmail.com>
2019-11-02 11:00:46 -0400

    Improve script to create branch

--- dev_scripts/git_checkout_branch.sh
+++ dev_scripts/git_checkout_branch.sh
@@ -7,6 +7,15 @@
 source helpers.sh
 
 execute "git checkout -b $1"
-execute "git branch -u origin/$1"
+
+# This doesn't work since we are creating the branch locally and there is no
+# remote branch to track.
+#execute "git branch --set-upstream-to origin/$1"
+
+# Push the branch remotely.
 execute "git commit"
-execute "git push --set-upstream origin $1"
+execute "git push -u origin $1"
+
+# To delete a branch just created:
+# > git checkout master
+# > git branch -d <BRANCH_NAME>
