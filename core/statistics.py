2d3d2ee722ca7921198241863dbf5dd747f8dfe8
paul <smith.paul.anthony@gmail.com>
2019-11-08 08:32:54 -0800

    Add type hints and more documentation

--- core/statistics.py
+++ core/statistics.py
@@ -1,6 +1,11 @@
+import logging
+from typing import Optional
+
 import pandas as pd
 import scipy as sp
 import statsmodels as sm
+
+_LOG = logging.getLogger(__name__)
 
 MEAN_COL = "mean"
 STD_COL = "std"
@@ -11,7 +16,17 @@ TVAL_COL = "tvals"
 ADJ_PVAL_COL = "adj_pvals"
 
 
-def moments(df):
+# #############################################################################
+# Descriptive statistics
+# #############################################################################
+
+
+# TODO(Paul): Double-check axes in used in calculation.
+# Consider exposing `nan_policy`.
+def moments(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculates, mean, standard deviation, skew, and kurtosis.
+    """
     mean = df.mean()
     std = df.std()
     skew = sp.stats.skew(df, nan_policy="omit")
@@ -23,7 +38,14 @@ def moments(df):
     return result
 
 
-def ttest_1samp(df, popmean=0, nan_policy="omit"):
+# #############################################################################
+# Hypothesis testing
+# #############################################################################
+
+
+def ttest_1samp(df: pd.DataFrame,
+                popmean: Optional[float] = None,
+                nan_policy: Optional[str] = None) -> pd.DataFrame:
     """
     Thin wrapper around scipy's ttest.
 
@@ -32,8 +54,14 @@ def ttest_1samp(df, popmean=0, nan_policy="omit"):
     and instead use `nan_policy='omit'`.
 
     :param df: DataFrame with samples along rows, groups along columns.
+    :param popmean: assumed population mean for test
+    :param nan_policy: `nan_policy` for scipy's ttest_1samp
     :return: DataFrame with t-value and p-value columns, rows like df's columns
     """
+    if popmean is None:
+        popmean = 0
+    if nan_policy is None:
+        nan_policy = "omit"
     tvals, pvals = sp.stats.ttest_1samp(
         df, popmean=popmean, nan_policy=nan_policy
     )
@@ -41,7 +69,8 @@ def ttest_1samp(df, popmean=0, nan_policy="omit"):
     return result
 
 
-def multipletests(srs, method="fdr_bh"):
+def multipletests(srs: pd.Series,
+                  method: Optional[str] = None) -> pd.Series:
     """
     Thin wrapper around statsmodel's multipletests.
 
@@ -51,23 +80,22 @@ def multipletests(srs, method="fdr_bh"):
     https://www.statsmodels.org/stable/generated/statsmodels.stats.multitest.multipletests.html
 
     :param srs: Series with pvalues
-    :param method: Passed to statsmodel's
-    :return:
+    :param method: `method` for scipy's multipletests
+    :return: Series of adjusted p-values
     """
+    if method is None:
+        method = "fdr_bd"
     pvals_corrected = sm.stats.multitest.multipletests(srs, method=method)[1]
     return pd.Series(pvals_corrected, index=srs.index, name=ADJ_PVAL_COL)
 
 
-def multi_ttest(df, popmean=0, nan_policy="omit", method="fdr_bh"):
+def multi_ttest(df: pd.DataFrame,
+                popmean: Optional[float] = None,
+                nan_policy: Optional[str] = None,
+                method: Optional[str] = None) -> pd.DataFrame:
     """
     Combines ttest and multitest pvalue adjustment.
-
-    :param df: DataFrame with samples along rows, groups along columns.
-    :param pop_mean:
-    :param nan_policy:
-    :param method:
-    :return:
     """
     ttest = ttest_1samp(df, popmean=popmean, nan_policy=nan_policy)
-    ttest[ADJ_PVAL_COL] = multipletests(ttest[PVAL_COL])
+    ttest[ADJ_PVAL_COL] = multipletests(ttest[PVAL_COL], method=method)
     return ttest
