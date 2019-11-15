3a29065088e4047a47c7ff044db04071269a03fa
paul <smith.paul.anthony@gmail.com>
2019-11-14 17:52:55 -0800

    PartTask574: checkpoint

--- core/dataflow_builder.py
+++ core/dataflow_builder.py
@@ -9,10 +9,15 @@ import helpers.dbg as dbg
 class DagBuilder(abc.ABC):
     """
     Abstract class for creating DAGs.
+
+    Concrete classes must specify
+      - a default configuration (which may depend upon variables used in class
+        initialization)
+      - the construction of a dag
     """
 
     def __init__(self,
-                 config: Optional[cfg.Config] = None
+                 config: Optional[cfg.Config] = None,
                  nid_prefix: Optional[str] = None
                  ) -> None:
         """
@@ -21,7 +26,7 @@ class DagBuilder(abc.ABC):
         """
         self._nid_prefix = nid_prefix or ""
         # TODO(Paul): Defined in concrete class. Make static too?
-        self._config = config or self._get_default_config()
+        self._config = config or self.get_default_config()
 
     @property
     def config(self) -> cfg.Config:
@@ -33,15 +38,15 @@ class DagBuilder(abc.ABC):
 
     # TODO(Paul): Add setters for `nid_prefix`, `config`.
 
-    def get_nid_and_config(stage_name: str) -> Tuple[str, cfg.Config]:
+    def _get_nid_and_config(self, stage_name: str) -> Tuple[str, cfg.Config]:
         nid = self._nid_prefix + stage_name
         config = self._config[stage_name]
-        return nid, config
+        return nid, config.copy()
 
     @abc.abstractmethod
-    def _get_default_config() -> cfg.Config:
+    def get_default_config(self) -> cfg.Config:
         pass
 
     @abc.abstractmethod
-    def get_dag(dtf.DAG) -> dtf.DAG:
+    def get_dag(self, dag: Optional[dtf.DAG] = None) -> dtf.DAG:
         pass
