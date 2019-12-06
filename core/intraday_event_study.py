f5dca3924b3067648bb1048ce054a1415f8f32b3
paul <smith.paul.anthony@gmail.com>
2019-12-06 11:29:51 -0800

    Checkout from master

--- core/intraday_event_study.py
+++ core/intraday_event_study.py
@@ -1,66 +1,14 @@
-"""
-Import as:
-
-import core.intraday_event_study as ies
-
-TODO(Paul): Reorganize if we add additional event study files
-
-We make a distinction between the following different types of events (with
-respect to active trading hours):
-
-1.  Intraday events
-    a.  We focus on events strictly within active trading hours
-    b.  We may further restrict around the open / close, because response
-        windows of uniform time are easy to work with
-2.  At-the-open
-    a.  We focus on the effect on the market of information that has
-        accumulated outside of active-trading-hours
-    b.  These can be handled together with intraday events provided we treat
-        the event time as the market-open
-3.  At-the-close
-    a.  Distinguished from vanilla intraday in that the timing of the close
-        may restrict the response windows of interest
-4.  Multi-day
-    a.  Of interest if events are sparse on the scale of days; otherwise the
-        proper multi-day setting is a continuous one
-"""
-
 import logging
-from typing import Tuple, Union
 
 import numpy as np
 import pandas as pd
 
-import helpers.dbg as dbg
-
 _LOG = logging.getLogger(__name__)
 
 
-def generate_pre_and_post_event_split(
-    x_vars: pd.DataFrame, y_vars: Union[pd.Series, pd.DataFrame],
-        num_shifts: int, mode: str
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def generate_aligned_response(x_df, y_df, resp_col_name, num_shifts):
     """
-    TODO: Implement
-
-    :param x_vars:
-    :param y_vars:
-    :param num_shifts:
-    :param mode:
-        `panel`: y_var
-    :return:
-    """
-    pass
-
-
-# TODO(Paul): Really, we don't want to call these x_vars and y_vars. We just
-#     want 1) a series of event times and 2) a DataFrame of data (maybe with
-#     both signals and responses as separate columns).
-def generate_aligned_response(
-    x_vars: pd.DataFrame, y_vars: Union[pd.Series, pd.DataFrame], num_shifts: int,
-) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """
-    Align responses of a y_var with x_vars according to index
+    Align responses of a particular column of y_df with x_df.
 
     Restricting to the intraday setting greatly simplifies the alignment
     process.
@@ -70,66 +18,44 @@ def generate_aligned_response(
     response of US equities with this implementation would require a separate
     function call for each equity.
 
-    :param x_vars: pd.DataFrame of signal, indexed by datetimes aligned with
-        response time grid frequency.
-    :param y_vars: pd.DataFrame of response
+    :param x_df: pd.DataFrame of signal, indexed by datetimes aligned with
+        response time grid.
+    :param y_df: pd.DataFrame of response
+    :param resp_col_name: y_df column to subselect
     :param num_shifts: Number of response time shifts to grab both before and
         after the event time
+    :param y_resp_cols: Subselect response column(s) by providing name or list
+        of names. If default `None` is used, all available columns are kept.
 
     :return: pre_event_df, post_event_df
     """
-    if isinstance(y_vars, pd.Series):
-        y_vars = y_vars.to_frame()
-    dbg.dassert_monotonic_index(y_vars)
-    # TODO(Paul): Make sure everything in the big dataframe has a monotonic
-    #     index and a frequency. 
-    # TODO(Paul): Maybe assert `y_var` has a `freq` specified.
-    # TODO(Paul): Check offsets of x_vars (though this may be slow...)
     pre_event = []
     post_event = []
     for i in range(-num_shifts, num_shifts + 1, 1):
         # Times go -num_shifts, ..., -1, 0, 1, ..., num_shifts
-        # To get the response at time j, we call .shift(-j) on y_vars
-        # TODO(Paul): Restrict back to col names?
-        resp = x_vars.join(y_vars.shift(-i))
+        # To get the response at time j, we call .shift(-j) on y_df
+        resp = x_df.join(y_df.shift(-i))[resp_col_name]
         resp.name = i
-        # TODO(Paul): Placing the event time in the "post-event" group matches
-        #     how we treat time intervals. OTOH, the event itself (assuming its
-        #     time is a knowledge time) doesn't impact ret_0, which suggests
-        #     including the event time in the "pre-event" group.
         if i < 0:
             pre_event.append(resp)
         else:
             post_event.append(resp)
-    dbg.dassert_lt(0, len(pre_event), "No pre-event response data!")
-    dbg.dassert_lt(0, len(post_event), "No post-event response data!")
-    # datetime index contains event times, columns indicate relative time
-    #     shifts
     pre_event_df = pd.concat(pre_event, axis=1)
     post_event_df = pd.concat(post_event, axis=1)
     return pre_event_df, post_event_df
 
 
-def tile_x_flatten_y(
-    x_vars: pd.DataFrame, y_var: pd.DataFrame
-) -> Tuple[np.array, np.array]:
+def tile_x_flatten_y(x_vars, y_var):
     """
-    Reshape x_vars, y_var into equal-length series.
+    Reshape x_vars, y_var for analysis from event time forward.
 
-    Values of `x_vars` are repeated in the tiling for each column of `y_var`.
-    WARNING: May be non-casual if cols
-
-    :param x_vars: signal at event time
-    :param y_var: a single response variable in n x k form
-        - `n` (rows) represents number of events
-        - `k` (cols) represents time offsets
-    :return:
-        - `x` tiled `k` times resulting in n x k rows
-        - `y` flattened into an (n x k)-length array
+    :param x_var: signal at event time
+    :param y_var: a single response variable in an nxk form, from event time
+        and forward
     """
     y = y_var.values.transpose().flatten()
     # Tile x values to match flattened y
-    _LOG.debug("x_vars=`%s`", x_vars.columns.values)
+    _LOG.info("regressors: %s", x_vars.columns.values)
     x = np.tile(x_vars.values, (y_var.shape[1], 1))
     return x, y
 
@@ -200,16 +126,13 @@ def estimate_event_effect(x_vars, pre_resp, post_resp):
     :param post_resp: pd.DataFrame of response post-event (cols are pos
         offsets)
     """
-    # Estimate level pre-event.
+    # Estimate level pre-event
     _LOG.info("Estimating pre-event response level...")
-    # Create a constant term to regress against.
     x_ind = pd.DataFrame(
         index=x_vars.index, data=np.ones(x_vars.shape[0]), columns=["const"]
     )
     x_lvl, y_pre_resp = tile_x_flatten_y(x_ind, pre_resp)
-    # Regression against const.
     alpha_hat, alpha_hat_var, alpha_hat_z_score = regression(x_lvl, y_pre_resp)
-    # Estimate post-event response.
     _LOG.info("Regressing level-adjusted post-event response against x_vars...")
     # Adjust post-event values by pre-event-estimated level
     x, y_post_resp = tile_x_flatten_y(x_vars, post_resp - alpha_hat[0])
