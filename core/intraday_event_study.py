a986db9bfc2afbab092b8bc2d241d552e2a99627
paul <smith.paul.anthony@gmail.com>
2019-12-06 17:47:15 -0800

    Checkpoint

--- core/intraday_event_study.py
+++ core/intraday_event_study.py
@@ -3,7 +3,127 @@ import logging
 import numpy as np
 import pandas as pd
 
+from typing import Optional, Union
+
 _LOG = logging.getLogger(__name__)
+
+
+def _shift_and_select(idx: pd.Index, data: Union[pd.Series, pd.DataFrame],
+                      periods: int,
+                      freq: Optional[Union[pd.DateOffset, pd.Timedelta, str]] = None,
+                      mode: Optional[str] = None,
+                      info: Optional[dict] = None) -> Union[pd.Series, pd.DataFrame]:
+    """
+    Shift by `periods` and select `idx`.
+
+    :param idx: reference index (e.g., of datetimes of events)
+    :param data: tabular data
+    :param periods: as in pandas `shift` functions
+    :param freq: as in pandas `shift` functions
+    :param mode:
+        -   "shift_data" applies `.shift()` to `data`
+        -   "shift_idx" applies `.shift()` to `idx`
+    :param info: optional empty dict-like object to be populated with stats
+        about the operation performed
+    """
+    if mode is None:
+        mode = "shift_data"
+    # Shift.
+    if mode == "shift_data":
+        data = data.copy()
+        data = data.shift(periods, freq)
+    elif mode == "shift_idx":
+        idx = idx.copy()
+        idx = idx.shift(periods, freq)
+    else:
+        raise ValueError("Unrecognized mode=`%s`", mode)
+    # Select.
+    intersection = idx.intersection(data.index)
+    dbg.dassert(not intersection.empty)
+    selected = data.loc[intersection]
+    # Maybe add info.
+    if info is not None:
+        dbg.dassert_isinstance(info, dict)
+        dbg.dassert(not info)
+        info["indices_with_no_data"] = intersection.difference(idx)
+        info["periods"] = periods
+        if freq is not None:
+            info["freq"] = freq
+        info["mod"] = mode
+    return selected
+
+
+def _compute_relative_series(
+        idx: pd.Index,
+        data: Union[pd.Series, pd.DataFrame],
+        first_period: int,
+        last_period: int,
+        freq: Optional[Union[pd.DateOffset, pd.Timedelta, str]] = None,
+        shift_mode: Optional[str] = None,
+        info: Optional[dict] = None) -> Dict[int, pd.DataFrame]:
+    """
+    Compute series [first_period, last_period] relative to idx.
+
+    :param idx: reference index (e.g., of datetimes of events)
+    :param data: tabular data
+    :param first_period: first period offset, inclusive
+    :param last_period: last period offset, inclusive
+    :param periods: as in pandas `shift` functions
+    :param freq: as in pandas `shift` functions
+    :param shift_mode:
+        -   "shift_data" applies `.shift()` to `data`
+        -   "shift_idx" applies `.shift()` to `idx`
+    :return: a dict
+        -   keyed by period (from [first_period, last_period])
+        -   values series / dataframe, obtained from shifts and idx selection
+    """
+    dbg.dassert_lte(first_period, last_period)
+    period_to_data = {}
+    for period in range(first_period, last_period + 1):
+        if info is not None:
+            period_info = {}
+        else:
+            period_info = None
+        data = _shift_and_select(idx, data, period, freq, shift_mode, period_info)
+        if isinstance(data, pd.Series):
+            data = data.to_frame()
+        if info is not None:
+            info[period] = period_info
+        period_to_data[period] = data
+    return period_to_data
+
+
+def _add_indicator(
+    data: pd.DataFrame,
+    period: int,
+    name: str = EVENT_INDICATOR) -> pd.DataFrame:
+    """
+
+    :param data:
+    :param mode:
+    :return:
+    """
+    data = data.copy()
+    dbg.dassert_not_in(name, data.columns)
+    if period >= 0:
+        val = 1
+    else:
+        val = 0
+    srs = pd.Series(data=val, index=data.index, name=name)
+    data.insert(loc=0, column=name, value=srs)
+    return data
+
+
+def _stack_data(
+    data: Dict[int, Union[pd.Series, pd.DataFrame]],
+) -> Union[pd.Series, pd.DataFrame]:
+    """
+
+    :param data:
+    :return:
+    """
+    stacked = pd.concat(data.values(), axis=0, ignore_index=True)
+    return stacked
 
 
 def generate_aligned_response(x_df, y_df, resp_col_name, num_shifts):
