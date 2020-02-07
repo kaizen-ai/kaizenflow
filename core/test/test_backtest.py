a71c01c6c8fd9a89f4fbb68fd2644c608fabff1f
paul <smith.paul.anthony@gmail.com>
2020-02-07 21:55:41 +0000

    Lint

--- core/test/test_backtest.py
+++ core/test/test_backtest.py
@@ -1,13 +1,13 @@
 import logging
 from typing import Iterable
 
-import mxnet
 import gluonts
 
 # TODO(*): gluon needs these imports to work properly.
 import gluonts.model.deepar as gmd  # isort: skip # noqa: F401 # pylint: disable=unused-import
 import gluonts.model.predictor as gmp  # isort: skip # noqa: F401 # pylint: disable=unused-import
 import gluonts.trainer as gt  # isort: skip # noqa: F401 # pylint: disable=unused-import
+import mxnet
 import numpy as np
 import pandas as pd
 import pytest
@@ -86,7 +86,7 @@ class TestGeneratePredictions(hut.TestCase):
         """
         Generate y from a shift of an ARIMA series.
         """
-        mxnet.random.seed(0, ctx='all')
+        mxnet.random.seed(0, ctx="all")
         num_x_vars = 1
         df = TestGeneratePredictions._generate_input_data(
             num_x_vars=num_x_vars, base_random_state=42
@@ -127,7 +127,7 @@ class TestGeneratePredictions(hut.TestCase):
         """
         Generate y from a shift of a linear combination of ARIMA series.
         """
-        mxnet.random.seed(0, ctx='all')
+        mxnet.random.seed(0, ctx="all")
         num_x_vars = 2
         df = TestGeneratePredictions._generate_input_data(
             num_x_vars=num_x_vars, base_random_state=42
@@ -168,7 +168,7 @@ class TestGeneratePredictions(hut.TestCase):
         """
         Generate y from a shift of an ARIMA series. Ignore x.
         """
-        mxnet.random.seed(0, ctx='all')
+        mxnet.random.seed(0, ctx="all")
         df = TestGeneratePredictions._generate_input_data(
             num_x_vars=1, base_random_state=42
         )
@@ -206,7 +206,7 @@ class TestGeneratePredictions(hut.TestCase):
         """
         Generate y using `m4_hourly` Gluon dataset. No `x_vars`.
         """
-        mxnet.random.seed(0, ctx='all')
+        mxnet.random.seed(0, ctx="all")
         train_length = 500
         test_length = 100
         train_df, test_df = sig_gen.get_gluon_dataset(
