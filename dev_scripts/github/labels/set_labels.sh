6524904c297c0448c810bdbb7f837e4d4c1efc78
saggese <saggese@gmail.com>
2020-02-16 16:33:35 -0500

    PartTask1075: Add script to create standard labels

---
+++ dev_scripts/github/labels/set_labels.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+
+# """
+# Backup and then update labels of several GitHub repos.
+# """
+
+EXEC="/Users/saggese/src/github/github-label-maker/github-label-maker.py"
+
+SRC_DIR="./dev_scripts/github/labels"
+SRC_NAME="$SRC_DIR/gh_tech_labels.json"
+#SRC_NAME="$SRC_DIR/gh_org_labels.json"
+DST_DIR="$SRC_DIR/backup"
+
+
+function label() {
+    FULL_OPTS="$OPTS -o $OWNER -r $REPO"
+    CMD="python $EXEC $FULL_OPTS -t $GH_TOKEN"
+    echo "> $CMD"
+    eval $CMD
+    echo "Done"
+}
+
+
+function backup_and_update() {
+    # Backup.
+    FILE_NAME="$DST_DIR/labels.$OWNER.$REPO.json"
+    OPTS="-d $FILE_NAME"
+    label
+
+    # Update.
+    OPTS="-m $SRC_NAME"
+    label
+}
+
+
+# ParticleDev/commodity_research
+OWNER="ParticleDev"
+REPO="commodity_research"
+if [[ 1 == 1 ]]; then
+    backup_and_update
+fi;
+
+# ParticleDev/infra
+OWNER="ParticleDev"
+REPO="infra"
+if [[ 1 == 1 ]]; then
+    backup_and_update
+fi;
+
+# alphamatic/amp
+OWNER="alphamatic"
+REPO="amp"
+if [[ 1 == 1 ]]; then
+    backup_and_update
+fi;
