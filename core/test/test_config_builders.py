cad1f9f52848704316c55669c36454ff93c415b3
Daniil <daniil@particle.one>
2020-02-28 17:53:43 +0300

    Fix test bugs

--- core/test/test_config_builders.py
+++ core/test/test_config_builders.py
@@ -89,28 +89,6 @@ class TestBuildMultipleConfigs(hut.TestCase):
 
 
 class Test_config_comparisons(hut.TestCase):
-    @staticmethod
-    def get_test_config_1():
-        config = cfg.Config()
-        tmp_config = config.add_subconfig("build_model")
-        tmp_config["activation"] = "sigmoid"
-        tmp_config = config.add_subconfig("build_targets")
-        tmp_config["target_asset"] = "Crude Oil"
-        tmp_config = config["build_targets"].add_subconfig("preprocessing")
-        tmp_config["preprocessor"] = "tokenizer"
-        return config
-
-    @staticmethod
-    def get_test_config_2():
-        config = cfg.Config()
-        tmp_config = config.add_subconfig("build_model")
-        tmp_config["activation"] = "sigmoid"
-        tmp_config = config.add_subconfig("build_targets")
-        tmp_config["target_asset"] = "Gold"
-        tmp_config = config["build_targets"].add_subconfig("preprocessing")
-        tmp_config["preprocessor"] = "tokenizer"
-        return config
-
     def test_check_same_configs_error(self):
         # Create list of configs with duplicates.
         configs = [
@@ -126,30 +104,31 @@ class Test_config_comparisons(hut.TestCase):
         # Create expected intersection config.
         intersection_config = cfg.Config()
         tmp_config = intersection_config.add_subconfig("build_model")
-        tmp_config["build_model"] = "sigmoid"
+        tmp_config["activation"] = "sigmoid"
         tmp_config = intersection_config.add_subconfig("build_targets")
-        tmp_config = intersection_config["build_targets"].add_subconfig(
-            "preprocessing"
-        )
+        tmp_config = intersection_config["build_targets"].add_subconfig("preprocessing")
         tmp_config["preprocessor"] = "tokenizer"
+        tmp_config = intersection_config.add_subconfig("meta")
+        tmp_config["result_file_name"] = "results.pkl"
         config_1 = ConfigTestHelper.get_test_config_1()
         config_2 = ConfigTestHelper.get_test_config_2()
         actual_intersection = ccfgbld.get_config_intersection(
             [config_1, config_2]
         )
-        self.assertEqual(str(intersection_config), str(actual_intersection))
+        self.assertEqual(str(intersection_config), str(actual_intersection), msg=f"\n***\n{str(intersection_config)}\n\n***\n{str(actual_intersection)}")
+
 
     def test_varying_config_difference(self):
         # Create two different configs.
-        config_1 = Test_config_comparisons.get_test_config_1()
-        config_2 = Test_config_comparisons.get_test_config_2()
+        config_1 = ConfigTestHelper.get_test_config_1()
+        config_2 = ConfigTestHelper.get_test_config_2()
         # Define expected variation.
         expected_difference = {"build_targets.target_asset": ["Crude Oil", "Gold"]}
         actual_difference = ccfgbld.get_config_difference([config_1, config_2])
         self.assertEqual(expected_difference, actual_difference)
 
     def test_same_config_difference(self):
-        config = Test_config_comparisons.get_test_config_1()
+        config = ConfigTestHelper.get_test_config_1()
         actual_difference = ccfgbld.get_config_difference([config, config])
         # Check that the difference is empty.
         self.assertFalse(actual_difference)
@@ -162,7 +141,8 @@ class TestGetConfigDataframe(hut.TestCase):
 
         expected_result = pd.DataFrame({'build_model.activation': ["sigmoid", "sigmoid"],
                                         'build_targets.target_asset': ['Crude Oil', 'Gold'],
-                                        'build_targets.preprocessing.preprocessor': ['tokenizer', 'tokenizer']})
+                                        'build_targets.preprocessing.preprocessor': ['tokenizer', 'tokenizer'],
+                                        "meta.result_file_name": ["results.pkl", "results.pkl"]})
         actual_result = ccfgbld.get_configs_dataframe([config_1, config_2])
         self.assertTrue(expected_result.equals(actual_result))
 
@@ -188,8 +168,7 @@ class TestParameterAddition(hut.TestCase):
         # Modify test config manually.
         expected_config = ConfigTestHelper.get_test_config_1()
         result_dir = "test/results"
-        tmp_config = expected_config.add_subconfig("meta")
-        tmp_config["result_dir"] = result_dir
+        expected_config[("meta", "result_dir")] = result_dir
         # Pass test config as one-item list and apply function.
         actual_config = [ConfigTestHelper.get_test_config_1()]
         actual_config = ccfgbld.add_result_dir(result_dir, actual_config)
@@ -201,7 +180,7 @@ class TestParameterAddition(hut.TestCase):
         expected_config = ConfigTestHelper.get_test_config_1()
         sim_dir = "/data/tests/test_results/"
         # Set result file name manually.
-        expected_config["result_file_name"] = "/data/tests/test_results/results.pkl"
+        expected_config[("meta", "result_file_name")] = "/data/tests/test_results/results.pkl"
 
         actual_config = ConfigTestHelper.get_test_config_1()
         actual_config = ccfgbld.set_absolute_result_file_path(sim_dir, actual_config)
@@ -219,6 +198,3 @@ class TestParameterAddition(hut.TestCase):
         actual_configs = ccfgbld.add_config_idx(actual_configs)
         actual_configs = [str(config) for config in actual_configs]
         self.assertEqual(expected_configs, actual_configs)
-
-
-
