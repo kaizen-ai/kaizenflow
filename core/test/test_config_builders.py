e1390f3ba0480f4a2c46d08d3bbd726624c9a7cf
Daniil <daniil@particle.one>
2020-02-28 19:14:40 +0300

    Move tests to different classes

--- core/test/test_config_builders.py
+++ core/test/test_config_builders.py
@@ -88,7 +88,7 @@ class TestBuildMultipleConfigs(hut.TestCase):
             )
 
 
-class Test_config_comparisons(hut.TestCase):
+class TestCheckSameConfigs(hut.TestCase):
     def test_check_same_configs_error(self):
         # Create list of configs with duplicates.
         configs = [
@@ -96,10 +96,12 @@ class Test_config_comparisons(hut.TestCase):
             ConfigTestHelper.get_test_config_1(),
             ConfigTestHelper.get_test_config_2(),
         ]
-        # Check
+        # Check if function raises an error.
         with self.assertRaises(AssertionError):
             ccfgbld.check_same_configs(configs)
 
+
+class TestConfigIntersection(hut.TestCase):
     def test_config_intersection(self):
         # Create expected intersection config.
         intersection_config = cfg.Config()
@@ -115,8 +117,10 @@ class Test_config_comparisons(hut.TestCase):
         actual_intersection = ccfgbld.get_config_intersection(
             [config_1, config_2]
         )
-        self.assertEqual(str(intersection_config), str(actual_intersection), msg=f"\n***\n{str(intersection_config)}\n\n***\n{str(actual_intersection)}")
+        self.assertEqual(str(intersection_config), str(actual_intersection))
 
+
+class TestConfigDifference(hut.TestCase):
     def test_varying_config_difference(self):
         # Create two different configs.
         config_1 = ConfigTestHelper.get_test_config_1()
@@ -134,10 +138,16 @@ class Test_config_comparisons(hut.TestCase):
 
 
 class TestGetConfigDataframe(hut.TestCase):
+    """
+    Compare manually constructed dfs and dfs created by `ccfgbld.get_configs_dataframe`
+    using `pd.DataFrame.equals()`
+    """
     def test_all_params(self):
+        # Get two test configs.
         config_1 = ConfigTestHelper.get_test_config_1()
         config_2 = ConfigTestHelper.get_test_config_2()
 
+        # Create expected dataframe and one with function.
         expected_result = pd.DataFrame({'build_model.activation': ["sigmoid", "sigmoid"],
                                         'build_targets.target_asset': ['Crude Oil', 'Gold'],
                                         'build_targets.preprocessing.preprocessor': ['tokenizer', 'tokenizer'],
@@ -146,27 +156,30 @@ class TestGetConfigDataframe(hut.TestCase):
         self.assertTrue(expected_result.equals(actual_result))
 
     def test_different_params_subset(self):
+        # Get two test configs.
         config_1 = ConfigTestHelper.get_test_config_1()
         config_2 = ConfigTestHelper.get_test_config_2()
-
+        # Create expected dataframe and one with function.
         expected_result = pd.DataFrame({"build_targets.target_asset": ["Crude Oil", "Gold"]})
         actual_result = ccfgbld.get_configs_dataframe([config_1, config_2], params_subset="difference")
         self.assertTrue(expected_result.equals(actual_result))
 
     def test_custom_params_subset(self):
+        # Get two test configs.
         config_1 = ConfigTestHelper.get_test_config_1()
         config_2 = ConfigTestHelper.get_test_config_2()
 
+        # Create expected dataframe and one with function.
         expected_result = pd.DataFrame({"build_model.activation": ["sigmoid", "sigmoid"]})
         actual_result = ccfgbld.get_configs_dataframe([config_1, config_2], params_subset=["build_model.activation"])
         self.assertTrue(expected_result.equals(actual_result))
 
 
-class TestParameterAddition(hut.TestCase):
+class TestAddResultDir(hut.TestCase):
     def test_result_dir(self):
+        result_dir = "test/results"
         # Modify test config manually.
         expected_config = ConfigTestHelper.get_test_config_1()
-        result_dir = "test/results"
         expected_config[("meta", "result_dir")] = result_dir
         # Pass test config as one-item list and apply function.
         actual_config = [ConfigTestHelper.get_test_config_1()]
@@ -175,26 +188,32 @@ class TestParameterAddition(hut.TestCase):
         actual_config = actual_config[0]
         self.assertEqual(str(expected_config), str(actual_config))
 
+
+class TestSetAbsoluteResultFilePath(hut.TestCase):
     def test_set_absolute_result_file_path(self):
-        expected_config = ConfigTestHelper.get_test_config_1()
         sim_dir = "/data/tests/test_results/"
+        expected_config = ConfigTestHelper.get_test_config_1()
         # Set result file name manually.
         expected_config[("meta", "result_file_name")] = "/data/tests/test_results/results.pkl"
-
+        # Set using function.
         actual_config = ConfigTestHelper.get_test_config_1()
         actual_config = ccfgbld.set_absolute_result_file_path(sim_dir, actual_config)
         self.assertEqual(str(expected_config), str(actual_config))
 
+
+class TestAddConfigIdx(hut.TestCase):
     def test_add_config_idx(self):
+        # Assign id parameters manually.
         expected_config_1 = ConfigTestHelper.get_test_config_1()
         expected_config_1[("meta", "id")] = 0
         expected_config_2 = ConfigTestHelper.get_test_config_1()
         expected_config_2[("meta", "id")] = 1
-        # Convert to strings for comparison.
-        expected_configs = [str(expected_config_1), str(expected_config_2)]
+        # Assign id parameters through function.
         actual_configs = [ConfigTestHelper.get_test_config_1(),
                           ConfigTestHelper.get_test_config_1()]
         actual_configs = ccfgbld.add_config_idx(actual_configs)
+        # Convert configs to string for comparison.
+        expected_configs = [str(expected_config_1), str(expected_config_2)]
         actual_configs = [str(config) for config in actual_configs]
         self.assertEqual(expected_configs, actual_configs)
 
@@ -202,11 +221,13 @@ class TestParameterAddition(hut.TestCase):
 class TestGenerateDefaultConfigVariants(hut.TestCase):
     def test_add_var_params(self):
         params_variants = {("build_targets", "target_asset"): ["Gasoil", "Soy"]}
+        # Manually add varying params to test configs.
         expected_config_1 = ConfigTestHelper.get_test_config_1()
         expected_config_1[("build_targets", "target_asset")] = "Gasoil"
-        expected_config_2 = ConfigTestHelper.get_test_config_2()
+        expected_config_2 = ConfigTestHelper.get_test_config_1()
         expected_config_2[("build_targets", "target_asset")] = "Soy"
-        expected_configs = [str(expected_config_1), str(expected_config_2)]
+        # Pass test config builder to generating function.
         actual_configs = ccfgbld.generate_default_config_variants(ConfigTestHelper.get_test_config_1, params_variants)
+        expected_configs = [str(expected_config_1), str(expected_config_2)]
         actual_configs = [str(config) for config in actual_configs]
         self.assertEqual(expected_configs, actual_configs)
