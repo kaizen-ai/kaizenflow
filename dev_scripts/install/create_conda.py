8aef43a0e7a1ac6691337b9712ace9344dfc6518
saggese <saggese@gmail.com>
2020-03-03 20:56:45 -0500

    PartTask1352: Disable mxnet hack

--- dev_scripts/install/create_conda.py
+++ dev_scripts/install/create_conda.py
@@ -333,7 +333,8 @@ def _main(parser: argparse.ArgumentParser) -> None:
     if args.skip_pip_install:
         _LOG.warning("Skip pip install")
     else:
-        _run_pip_install(args, conda_env_name)
+        #_run_pip_install(args, conda_env_name)
+        pass
     #
     if args.skip_test_env:
         _LOG.warning("Skip test conda env")
