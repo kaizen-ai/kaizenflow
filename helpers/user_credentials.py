c0d79cc0c6ff41c44b4d7b466dcb9a8ac68e06be
gad26032 <malanin@particle.one>
2020-03-27 13:16:01 +0300

    PartTask1522: update root user.

--- helpers/user_credentials.py
+++ helpers/user_credentials.py
@@ -233,12 +233,11 @@ def get_credentials() -> Dict[str, Any]:
         conda_env_path = "/var/lib/jenkins/.conda/envs"
     # We use this for #1522
     elif user_name == "root":
-        # Jenkins.
-        # Jenkins should not commit so it doesn't neet Git credentials.
-        git_user_name = ""
-        git_user_email = ""
+        # Docker user.
+        git_user_name = "infraparticleone"
+        git_user_email = "infra@particle.one"
         conda_sh_path = "/opt/conda/etc/profile.d/conda.sh"
-        conda_env_path = "/root/.conda/envs/"
+        conda_env_path = "~/.conda/envs/"
     # Check.
     for var_name, val_name in [
         ("git_user_name", git_user_name),
