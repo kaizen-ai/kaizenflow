046c9e774a260303798b60ea03f3f84f22b57334
gad26032 <malanin@particle.one>
2020-04-10 16:25:58 +0300

    PartTask1831: add switch for tests.

--- dev_scripts/test/test_amp_dev_scripts.py
+++ dev_scripts/test/test_amp_dev_scripts.py
@@ -130,12 +130,14 @@ class Test_jack1(ut.TestCase):
     # TODO(gp): Not clear why it's broken.
     @pytest.mark.skipif('si.get_user_name() == "jenkins"')
     @pytest.mark.skipif('si.get_user_name() == "root"', reason="Issue #1522")
+    @pytest.mark.skipif('os.environ.get("IS_IN_DOCKER") == 1', reason="Issue #1831")
     def test_jack(self) -> None:
         cmd = 'jack -r "def dassert"'
         si.system(cmd)
 
     @pytest.mark.skipif('si.get_user_name() == "jenkins"')
     @pytest.mark.skipif('si.get_user_name() == "root"', reason="Issue #1522")
+    @pytest.mark.skipif('os.environ.get("IS_IN_DOCKER") == 1', reason="Issue #1831")
     def test_jackpy(self) -> None:
         cmd = 'jackpy -r "def dassert"'
         si.system(cmd)
@@ -363,6 +365,7 @@ if __name__ == "main":
         return txt
 
     @pytest.mark.skipif('si.get_user_name() == "root"', reason="Issue #1522")
+    @pytest.mark.skipif('os.environ.get("IS_IN_DOCKER") == 1', reason="Issue #1831")
     def test_linter1(self) -> None:
         """
         Run linter.py as executable on some text.
@@ -376,6 +379,7 @@ if __name__ == "main":
         self.check_string(output)
 
     @pytest.mark.skipif('si.get_user_name() == "root"', reason="Issue #1522")
+    @pytest.mark.skipif('os.environ.get("IS_IN_DOCKER") == 1', reason="Issue #1831")
     def test_linter2(self) -> None:
         """
         Run linter.py as library on some text.
