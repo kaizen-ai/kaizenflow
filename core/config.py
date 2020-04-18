16b9930bce6b5fd694c1a165d89e1c2a04eb65dc
paul <smith.paul.anthony@gmail.com>
2020-04-18 10:44:23 -0400

    Checkpoint

--- core/config.py
+++ core/config.py
@@ -269,6 +269,11 @@ class Config:
         )
 
 
+# #############################################################################
+# Config utils
+# #############################################################################
+
+
 def flatten_config(config: Config) -> Dict[str, collections.abc.Hashable]:
     """
     Flatten config by joining nested keys with "." and making val hashable.
@@ -282,3 +287,19 @@ def flatten_config(config: Config) -> Dict[str, collections.abc.Hashable]:
         if not isinstance(v, collections.abc.Hashable):
             flattened[k] = tuple(v)
     return flattened
+
+
+def intersect_configs(configs: Iterable[Config]) -> Config:
+    """
+    Return a config formed by taking the intersection of configs.
+    """
+    # Flatten configs and convert to sets for intersection.
+    flattened = map(flatten_config, configs)
+    sets = [set(c.items()) for c in flattened]
+    intersection_of_flattened = set.intersection(*sets)
+    # Create intersection. Rely on the fact that Config keys are of type `str`.
+    intersection = Config()
+    for (k, v) in intersection_of_flattened:
+        intersection[tuple(k.split("."))] = v
+    return intersection
+
