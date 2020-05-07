90a094e43697d71829b2f5c73a615bc7d547020b
Julia <julia@particle.one>
2020-05-07 20:58:25 +0000

    PartTask546: Re-run the notebook

--- core/notebooks/gallery_signal_processing.py
+++ core/notebooks/gallery_signal_processing.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.4.1
+#       jupytext_version: 1.3.4
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
