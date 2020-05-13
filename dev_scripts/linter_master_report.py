e8654f20cc31640d950823ee4050bb90185260df
gad26032 <malanin@particle.one>
2020-05-13 21:55:37 +0300

    PartTask2217: PR notes.

--- dev_scripts/linter_master_report.py
+++ dev_scripts/linter_master_report.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+"""
+Compute the change of lints of a branch with respect to master.
+"""
 
 import argparse
 import logging
 import os
 import sys
-from typing import Optional, Tuple
+from typing import Tuple, Optional, List
 
 import helpers.git as git
 import helpers.io_ as io_
@@ -14,65 +17,14 @@ import helpers.system_interaction as si
 _LOG = logging.getLogger(__name__)
 
 
-def _calculate_stats(
-    base_commit_sha: str,
-    head_commit_sha: str,
-    head_branch_name: str,
-    build_url: Optional[str] = None,
-) -> Tuple[int, str]:
+def _calculate_exit_status(branch_dirty_status: bool,
+                           master_lints: int,
+                           branch_lints: int, ) -> Tuple[int, str]:
     """
-
-    :param base_commit_sha: Respect to this commit or branch will be compared.
-    :param head_commit_sha:
-    :param head_branch_name:
-    :param build_url:
+    Calculate status and error message.
     """
-    # Calculate stats
-    dir_name = "."
-    # TODO: Think about it.
-    remove_files_non_present = False
-    mod_files = git.get_modified_files_in_branch(
-        dir_name,
-        base_commit_sha,
-        remove_files_non_present=remove_files_non_present,
-    )
-    # _LOG.info("modirty: %s", master_dirty)
-    cmd = f"linter.py -t {base_commit_sha} --post_check"
-    branch_dirty = si.system(cmd, abort_on_error=False)
-    _LOG.info("Branch dirty: %s", branch_dirty)
-    #
-    cmd = "git reset --hard"
-    si.system(cmd)
-    #
-    cmd = f"linter.py -t {base_commit_sha}"
-    branch_lints = si.system(cmd, abort_on_error=False)
-    _LOG.info("Branch lints: %s", branch_lints)
-    #
-    linter_output_filename = "./linter_warnings.txt"
-    # TODO: Rename -> linter_message
-    lints_message = io_.from_file(linter_output_filename)
-    lints_message = "```\n" + lints_message + "\n```\n"
-
-    # # Calculate "Before*" stats
-    cmd = "git reset --hard"
-    si.system(cmd)
-    cmd = f"git checkout {base_commit_sha} --recurse-submodules"
-    si.system(cmd)
-    mod_files_as_str = " ".join(mod_files)
-    cmd = f"linter.py --files {mod_files_as_str} --post_check"
-    master_dirty = si.system(cmd, abort_on_error=False)
-    _LOG.info("Master dirty: %s", master_dirty)
-    #
-    cmd = "git reset --hard"
-    si.system(cmd)
-    cmd = f"linter.py --files {mod_files_as_str}"
-    master_lints = si.system(cmd, abort_on_error=False)
-    _LOG.info("Master lints: %s", master_lints)
-    # Prepares a message and exit status
-    master_dirty_status = master_dirty > 0
     exit_status = 0
     errors = []
-    branch_dirty_status = branch_dirty > 0
     if branch_dirty_status:
         errors.append("**ERROR**: Run `linter.py. -b` locally before merging.")
         exit_status = 1
@@ -81,6 +33,97 @@ def _calculate_stats(
     if branch_lints > master_lints:
         exit_status = 1
         errors.append("**ERROR**: You introduced more lints. Please fix them.")
+    return exit_status, "\n".join(errors)
+
+
+def _compute_stats(master_dirty: int,
+                   branch_dirty: int,
+                   master_lints: int,
+                   branch_lints: int, ) -> Tuple[bool, bool, int, str]:
+    # Prepares a message and exit status
+    master_dirty_status = master_dirty > 0
+    branch_dirty_status = branch_dirty > 0
+    exit_status, errors = _calculate_exit_status(branch_dirty_status,
+                                                 master_lints,
+                                                 branch_lints)
+    return master_dirty_status, branch_dirty_status, exit_status, errors
+
+
+def _perform_linter_for_test_branch(base_commit_sha: str) -> Tuple[int,
+                                                                   int,
+                                                                   str]:
+    cmd = "git reset --hard"
+    # Clean up the client from all linter artifacts.
+    si.system(cmd)
+    cmd = f"linter.py -t {base_commit_sha} --post_check"
+    # We run the same comment twice since we need to get 2 different information
+    # from the linter.
+    # With `--post_check` we get information about whether any file needed to be
+    # linted. Without we receive the number of lints.
+    # TODO(Sergey): Pass both values with one execution of the linter and stop
+    #  this insanity.
+    branch_dirty = si.system(cmd, abort_on_error=False)
+    _LOG.info("Branch dirty: %s", branch_dirty)
+    #
+    cmd = "git reset --hard"
+    # Clean up the client from all linter artifacts.
+    si.system(cmd)
+    #
+    cmd = f"linter.py -t {base_commit_sha}"
+    branch_lints = si.system(cmd, abort_on_error=False)
+    _LOG.info("Branch lints: %s", branch_lints)
+    # Read the lints reported from the linter.
+    linter_output_filename = "./linter_warnings.txt"
+    linter_message = io_.from_file(linter_output_filename)
+    linter_message = "```\n" + linter_message + "\n```\n"
+    cmd = "git reset --hard"
+    # Clean up the client from all linter artifacts.
+    si.system(cmd)
+    return branch_lints, branch_dirty, linter_message
+
+
+def _perform_linter_for_reference_branch(base_commit_sha: str,
+                                         mod_files: List[str]) -> int:
+    # # Calculate "Before*" stats
+    cmd = "git reset --hard"
+    si.system(cmd)
+    cmd = f"git checkout {base_commit_sha} --recurse-submodules"
+    # Check out master at the requested hash.
+    si.system(cmd)
+    mod_files_as_str = " ".join(mod_files)
+    cmd = f"linter.py --files {mod_files_as_str} --post_check"
+    # We run the same comment twice since we need to get 2 different information
+    # from the linter.
+    # With `--post_check` we get information about whether any file needed to be
+    # linted. Without we receive the number of lints.
+    # TODO(Sergey): Pass both values with one execution of the linter and stop
+    #  this insanity.
+    # Lint the files that are modified.
+    master_dirty = si.system(cmd, abort_on_error=False)
+    _LOG.info("Master dirty: %s", master_dirty)
+    # Clean up the client.
+    cmd = "git reset --hard"
+    si.system(cmd)
+    cmd = f"linter.py --files {mod_files_as_str}"
+    master_lints = si.system(cmd, abort_on_error=False)
+    _LOG.info("Master lints: %s", master_lints)
+    # Clean up the client.
+    cmd = "git reset --hard"
+    si.system(cmd)
+    return master_lints, master_dirty
+
+
+def _prepare_message(
+        base_commit_sha,
+        master_lints,
+        master_dirty_status,
+        head_branch_name,
+        head_commit_sha,
+        branch_lints,
+        branch_dirty_status,
+        errors,
+        linter_message,
+        build_url: Optional[str]) -> str:
     # Message
     message = list()
     message.append("# Results of the linter build")
@@ -92,24 +135,71 @@ def _calculate_stats(
     message.append(console_message)
     message.append(f"- Master (sha: {base_commit_sha})")
     message.append(f"\t- Number of lints: {master_lints}")
-    message.append(f"\t- Dirty (i.e., linter was not run): {master_dirty_status}")
+    message.append(
+        f"\t- Dirty (i.e., linter was not run): {master_dirty_status}")
     message.append(f"- Branch ({head_branch_name}: {head_commit_sha})")
     message.append(f"\t- Number of lints: {branch_lints}")
-    message.append(f"\t- Dirty (i.e., linter was not run): {branch_dirty_status}")
+    message.append(
+        f"\t- Dirty (i.e., linter was not run): {branch_dirty_status}")
     diff_lints = branch_lints - master_lints
     message.append(
         f"\nThe number of lints introduced with this change: {diff_lints}"
     )
     message = "\n".join(message)
-    message += "\n\n" + "\n".join(errors)
-    message += "\n" + lints_message
+    message += "\n\n" + errors
+    message += "\n" + linter_message
 
+    return message
+
+
+def _calculate_stats(
+        base_commit_sha: str,
+        head_commit_sha: str,
+        head_branch_name: str,
+        build_url: Optional[str] = None
+) -> Tuple[int, str]:
+    """
+    Compute the statistics from the linter when run on a branch vs master.
+
+    :param base_commit_sha: hash of the branch to compare
+    :param: head_branch_name: name of the branch to be compared
+    :param: branch_name: branch name for the report
+    :param: build_url: jenkins build url
+    :return: an integer representing the exit status and an error message.
+    """
+    dir_name = "."
+    # TODO(Sergey): Not sure what to do with files that are not present in the
+    #  branch.
+    remove_files_non_present = False
+    # Find the files that are modified in the branch.
+    mod_files = git.get_modified_files_in_branch(
+        dir_name,
+        base_commit_sha,
+        remove_files_non_present=remove_files_non_present,
+    )
+    branch_lints, branch_dirty, linter_message = _perform_linter_for_test_branch(
+        base_commit_sha)
+    master_lints, master_dirty = _perform_linter_for_reference_branch(
+        base_commit_sha, mod_files)
+    master_dirty_status, branch_dirty_status, exit_status, errors = _compute_stats(
+        master_dirty, branch_dirty, master_lints, branch_lints)
+    message = _prepare_message(base_commit_sha,
+                               master_lints,
+                               master_dirty_status,
+                               head_branch_name,
+                               head_commit_sha,
+                               branch_lints,
+                               branch_dirty_status,
+                               errors,
+                               linter_message,
+                               build_url)
     return exit_status, message
 
 
 def _parse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
     )
     # Select files.
     parser.add_argument(
