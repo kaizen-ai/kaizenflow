c99442462bc629eb1dba39b858689b1f16655193
gad26032 <malanin@particle.one>
2020-05-14 00:57:59 +0300

    PartTask2217: actualize pipelines.

--- dev_scripts/jenkins/amp.run_slow_tests.sh
+++ dev_scripts/jenkins/amp.run_slow_tests.sh
@@ -1,33 +1,12 @@
 #!/bin/bash -e
-
-# """
-# - No conda env is built, but we rely on `develop` being already build
-# - Run the slow tests
-# """
-
-EXEC_NAME=`basename "$0"`
-CONDA_ENV="amp_develop.daily_build"
-
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-# Init.
+# Set env.
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-CMD="source ./dev_scripts/jenkins/amp.jenkins_helpers.sh"
-echo "+ $CMD"
-eval $CMD
-
-source_scripts
-
-# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-# Setenv.
-# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-setenv "$AMP/dev_scripts/setenv_amp.sh" $CONDA_ENV
+source dev_scripts/setenv_amp.jenkins.sh
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # Run tests.
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-# Run tests.
-OPTS="--test slow --coverage -v $VERBOSITY"
-run_tests $AMP $OPTS
+source dev_scripts/jenkins/test_runners/run_slow_tests.sh
