9c78355c023a4d040ebd33527f2a51269baad31c
DanilYachmenev <dan@particle.one>
2020-05-28 12:06:01 +0300

    PartTask2386: apply_nan_mode function is made and unit tested

--- helpers/test/test_dataframe.py
+++ helpers/test/test_dataframe.py
@@ -1,8 +1,10 @@
 import collections
 import logging
 
+import numpy as np
 import pandas as pd
 
+import core.artificial_signal_generators as sig_gen
 import helpers.dataframe as hdf
 import helpers.printing as prnt
 import helpers.unit_test as hut
@@ -76,3 +78,43 @@ class Test_filter_data_by_comparison(hut.TestCase):
             f"{hut.convert_info_to_string(info)}"
         )
         self.check_string(str_output)
+
+
+class Test_apply_nan_mode(hut.TestCase):
+    @staticmethod
+    def _get_series_with_nans(seed: int) -> pd.Series:
+        arparams = np.array([0.75, -0.25])
+        maparams = np.array([0.65, 0.35])
+        arma_process = sig_gen.ArmaProcess(arparams, maparams)
+        date_range = {"start": "1/1/2010", "periods": 40, "freq": "M"}
+        series = arma_process.generate_sample(
+            date_range_kwargs=date_range, seed=seed
+        )
+        series[:3] = np.nan
+        series[-3:] = np.nan
+        series[5:7] = np.nan
+        return series
+
+    def test1(self) -> None:
+        series = self._get_series_with_nans(1)
+        actual = hdf.apply_nan_mode(series)
+        actual_string = hut.convert_df_to_string(actual, index=True)
+        self.check_string(actual_string)
+
+    def test2(self) -> None:
+        series = self._get_series_with_nans(1)
+        actual = hdf.apply_nan_mode(series, nan_mode="ignore")
+        actual_string = hut.convert_df_to_string(actual, index=True)
+        self.check_string(actual_string)
+
+    def test3(self) -> None:
+        series = self._get_series_with_nans(1)
+        actual = hdf.apply_nan_mode(series, nan_mode="ffill")
+        actual_string = hut.convert_df_to_string(actual, index=True)
+        self.check_string(actual_string)
+
+    def test4(self) -> None:
+        series = self._get_series_with_nans(1)
+        actual = hdf.apply_nan_mode(series, nan_mode="fill_with_zero")
+        actual_string = hut.convert_df_to_string(actual, index=True)
+        self.check_string(actual_string)
