692a70ec30313f72f22367e8d9b4e33f35fe13b5
paul <smith.paul.anthony@gmail.com>
2020-06-05 21:11:36 -0400

    Checkpoint

---
+++ core/test/test_finance.py
@@ -0,0 +1,29 @@
+import logging
+
+import numpy as np
+import pandas as pd
+
+import core.artificial_signal_generators as sig_gen
+import core.finance as fin
+import helpers.unit_test as hut
+
+_LOG = logging.getLogger(__name__)
+
+class Test_compute_drawdown(hut.TestCase):
+    @staticmethod
+    def _get_series(seed: int) -> pd.Series:
+        arparams = np.array([0.75, -0.25])
+        maparams = np.array([0.65, 0.35])
+        arma_process = sig_gen.ArmaProcess(arparams, maparams)
+        date_range = {"start": "1/1/2010", "periods": 40, "freq": "M"}
+        series = arma_process.generate_sample(
+            date_range_kwargs=date_range, seed=seed
+        )
+        return series
+
+    def test1(self) -> None:
+        series = self._get_series(1)
+        actual = fin.compute_drawdown(series)
+        actual_string = hut.convert_df_to_string(actual, index=True)
+        self.check_string(actual_string)
+
