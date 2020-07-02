8eb8d6ad8eee22188b4e2d2d0d3757244b9ba0d5
Julia <julia@particle.one>
2020-07-02 17:20:01 +0300

    PartTask3201: Remove sorting

--- core/plotting.py
+++ core/plotting.py
@@ -12,7 +12,6 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 import matplotlib as mpl
 import matplotlib.colors as mpl_col
 import matplotlib.pyplot as plt
-import natsort
 import numpy as np
 import pandas as pd
 import scipy as sp
@@ -757,8 +756,7 @@ def multipletests_plot(
     )
     if not isinstance(ax, np.ndarray):
         ax = [ax]
-    sorted_cols = natsort.natsorted(adj_pvals.columns)
-    for i, col in enumerate(sorted_cols):
+    for i, col in enumerate(adj_pvals.columns):
         mask = adj_pvals[col].notna()
         adj_pval = adj_pvals.loc[mask, col].sort_values().reset_index(drop=True)
         ax[i].plot(
