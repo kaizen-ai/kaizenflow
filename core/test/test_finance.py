0439f511555710058acd93f097b52ad15382ddb0
Julia <julia@particle.one>
2020-07-02 20:20:24 +0300

    PartTask3161: Add basic tests

--- core/test/test_finance.py
+++ core/test/test_finance.py
@@ -109,3 +109,50 @@ class Test_compute_average_holding_period(hut.TestCase):
         actual = fin.compute_average_holding_period(series, unit="M")
         expected = 0.05001
         np.testing.assert_almost_equal(actual, expected, decimal=3)
+
+
+class Test_compute_signed_bet_lengths(hut.TestCase):
+    @staticmethod
+    def _get_series(seed: int) -> pd.Series:
+        arparams = np.array([0.75, -0.25])
+        maparams = np.array([0.65, 0.35])
+        arma_process = sig_gen.ArmaProcess(arparams, maparams)
+        date_range = {"start": "1/1/2010", "periods": 40, "freq": "M"}
+        series = arma_process.generate_sample(
+            date_range_kwargs=date_range, seed=seed
+        )
+        return series
+
+    def test1(self) -> None:
+        positions = Test_compute_signed_bet_lengths._get_series(42)
+        actual = fin.compute_signed_bet_lengths(positions, "fill_with_zero")
+        self.check_string(hut.convert_df_to_string(actual, index=True))
+
+    def test2(self) -> None:
+        positions = Test_compute_signed_bet_lengths._get_series(42)
+        positions.iloc[:4] = np.nan
+        positions.iloc[10:15] = np.nan
+        positions.iloc[-4:] = np.nan
+        actual = fin.compute_signed_bet_lengths(positions, "fill_with_zero")
+        self.check_string(hut.convert_df_to_string(actual, index=True))
+
+    def test3(self) -> None:
+        positions = pd.Series(
+            [1, 1, 1, 2, -1, -4, -0.5, 0, 0, -1, 0, 1],
+            index=pd.date_range(start="2010-01-01", periods=12, freq="D"),
+        )
+        expected_bet_starts = pd.to_datetime(
+            [
+                "2010-01-01",
+                "2010-01-05",
+                "2010-01-08",
+                "2010-01-10",
+                "2010-01-11",
+                "2010-01-12",
+            ]
+        )
+        expected = pd.Series(
+            [4, -3, 0, -1, 0, 1], index=expected_bet_starts, dtype=float
+        )
+        actual = fin.compute_signed_bet_lengths(positions, "fill_with_zero")
+        pd.testing.assert_series_equal(actual, expected)
