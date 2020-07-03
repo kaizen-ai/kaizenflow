288c539a4dc8224ac15a3f7862d792270351b2f4
DanilYachmenev <dan@particle.one>
2020-07-03 13:22:32 +0300

    PartTask3205: fix axes use when 2 or more axes pair

--- core/plotting.py
+++ core/plotting.py
@@ -278,7 +278,7 @@ def plot_cols(
     nrows = len(data.columns)
     if axes == [[None, None]]:
         _, axes = plt.subplots(nrows=nrows * 2, ncols=1, figsize=(20, 5 * nrows))
-        axes = [axes]
+        axes = [[axes[i], axes[i + 1]] for i in range(0, len(axes), 2)]
     if mode is None:
         mode = "default"
     elif mode == "renormalize":
@@ -287,9 +287,9 @@ def plot_cols(
     else:
         raise ValueError(f"Unsupported mode `{mode}`")
     for idx, col in enumerate(data.columns):
-        ax1 = axes[0][idx * 2]
+        ax1 = axes[idx][0]
         data[col].plot(kind="density", colormap=colormap, ax=ax1)
-        ax2 = axes[0][idx * 2 + 1]
+        ax2 = axes[idx][1]
         data[col].plot(colormap=colormap, ax=ax2)
 
 
