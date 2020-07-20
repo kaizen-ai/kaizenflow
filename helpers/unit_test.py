8b0e701f8a37f4b5bbe19b0dd0ad4523e58c9e2b
Daniil <daniil@particle.one>
2020-07-20 14:18:43 +0300

    PartTask3554: Remove default values

--- helpers/unit_test.py
+++ helpers/unit_test.py
@@ -140,7 +140,7 @@ def convert_info_to_string(info: Mapping) -> str:
     return output_str
 
 
-def convert_df_to_json_string(df: pd.DataFrame, n_head: int = 10, n_tail: int = 10) -> str:
+def convert_df_to_json_string(df: pd.DataFrame, n_head: int, n_tail: int) -> str:
     """
     Convert dataframe to pretty-printed json string.
 
