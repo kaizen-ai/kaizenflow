09fac6e17008207628ada93bb065af986decb776
gad26032 <malanin@particle.one>
2020-07-31 18:21:54 +0300

    PartTask3667: update

--- docker_build/gh_action_entrypoint.sh
+++ docker_build/gh_action_entrypoint.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-#docker_build/entrypoint/run_make.sh
-cd amp
 source docker_build/entrypoint/patch_environment_variables.sh
-
-cd ..
 source docker_build/entrypoint/gh_action_aws_credentials.sh
-source docker_build/entrypoint/patch_environment_variables.sh
 
 mount -a
 
