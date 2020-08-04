3b9896ebeaa05a931c4fc0f98d9a3a67bfa24464
DanilYachmenev <dan@particle.one>
2020-08-04 20:20:29 +0300

    PartTask3805: remove assert that one index is a subset of another

--- core/finance.py
+++ core/finance.py
@@ -508,12 +508,6 @@ def compute_returns_per_bet(
         bet
     """
     dbg.dassert_eq(positions.index.freq, log_rets.index.freq)
-    # Assert that one index is a subset of the other.
-    dbg.dassert(
-        (positions.index.intersection(log_rets.index).equals(log_rets.index))
-        or (positions.index.intersection(log_rets.index).equals(positions.index)),
-        msg="One input's index should be a subset of the other.",
-    )
     dbg.dassert_strictly_increasing_index(log_rets)
     bet_starts = compute_bet_starts(positions, nan_mode)
     bet_ends = compute_bet_ends(positions, nan_mode)
