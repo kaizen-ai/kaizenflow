5b8d55f772ab262100194d03a356dd5d443685b7
stanvanrooy <stan@rooy.dev>
2020-08-22 17:37:17 +0200

    Make sure trailing newlines aren't removed. #541

--- documentation/scripts/lint_txt.py
+++ documentation/scripts/lint_txt.py
@@ -162,7 +162,7 @@ def _postprocess(txt: str, in_file_name: str) -> str:
         txt_new.append(line)
     if in_triple_tick_block:
         print("%s:%s: A ``` block was not ending" % (in_file_name, 1))
-    txt_new_as_str = "\n".join(txt_new).rstrip("\n")
+    txt_new_as_str = "\n".join(txt_new)
     return txt_new_as_str
 
 
