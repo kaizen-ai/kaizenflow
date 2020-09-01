3afe84ce6e32cfe64b5eb4e7c0cb63853bbc5384
stanvanrooy <stan@rooy.dev>
2020-09-02 00:34:30 +0200

    Seperate out tests for check_import.

--- dev_scripts/linter2/test/test_p1_specific_lints.py
+++ dev_scripts/linter2/test/test_p1_specific_lints.py
@@ -5,6 +5,7 @@ import helpers.unit_test as hut
 
 from typing import List
 
+
 class Test_warn_incorrectly_formatted_todo(hut.TestCase):
     def test1(self) -> None:
         """Test warning for missing assignee.
@@ -165,46 +166,6 @@ class Test_check_test_file_dir(hut.TestCase):
     def _helper_check_test_file_dir(self, file_name: str, exp: str) -> None:
         msg = pslints._check_test_file_dir(file_name)
         self.assert_equal(msg, exp)
-
-
-class Test_check_import(hut.TestCase):
-    def test1(self) -> None:
-        """Test long import shortcut: invalid."""
-        shortcut = "very_long_name"
-        line = f"import test as {shortcut}"
-        exp = f"the import shortcut '{shortcut}' in '{line}' is longer than 8 characters"
-        self._helper_check_import(line, exp, file_name="test.py")
-
-    def test2(self) -> None:
-        """Test from lib import something: invalid."""
-        line = "from pandas import DataFrame"
-        exp = f"do not use '{line}' use 'import foo.bar " "as fba'"
-        self._helper_check_import(line, exp, file_name="test.py")
-
-    def test3(self) -> None:
-        """Test from typing import something: valid."""
-        line = "from typing import List"
-        exp = ""
-        self._helper_check_import(line, exp, file_name="test.py")
-
-    def test4(self) -> None:
-        """Test wild import in __init__.py: valid."""
-        line = "from test import *"
-        exp = ""
-        self._helper_check_import(line, exp, file_name="__init__.py")
-
-    def test5(self) -> None:
-        """Test import test.ab as tab: valid."""
-        line = "import test.ab as tab"
-        exp = ""
-        self._helper_check_import(line, exp, file_name="test.py")
-
-    def _helper_check_import(self, line: str, exp: str, file_name: str) -> None:
-        file_name = file_name or "test.py"
-        line_num = 1
-        exp = f"{file_name}:{line_num}: {exp}" if exp else exp
-        msg = pslints._check_import(file_name, line_num, line)
-        self.assertEqual(exp, msg)
 
 
 class Test_format_separating_lines(hut.TestCase):
