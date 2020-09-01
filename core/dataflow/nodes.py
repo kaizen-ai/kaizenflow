114065234d648cbafb22a54bdae920eab0882cbf
Julia <julia@particle.one>
2020-09-01 16:39:28 +0300

    PartTask4225: Rename parameter, add scale factor to info

--- core/dataflow/nodes.py
+++ core/dataflow/nodes.py
@@ -1597,7 +1597,7 @@ class VolatilityNormalizer(FitPredictNode):
     def __init__(
         self,
         nid: str,
-        y_var_hat: str,
+        col: str,
         target_volatility: float,
         col_mode: Optional[str] = None,
     ) -> None:
@@ -1605,14 +1605,14 @@ class VolatilityNormalizer(FitPredictNode):
         Normalize series to target annual volatility.
 
         :param nid: node identifier
-        :param y_var_hat: name of column to rescale
+        :param col: name of column to rescale
         :param target_volatility: target volatility as a proportion
         :param col_mode: `merge_all` or `replace_all`. If `replace_all`, return
             only the rescaled column, if `merge_all`, append the rescaled
             column to input dataframe
         """
         super().__init__(nid)
-        self._y_var_hat = y_var_hat
+        self._col = col
         self._target_volatility = target_volatility
         self._col_mode = col_mode or "merge_all"
         dbg.dassert_in(
@@ -1624,17 +1624,21 @@ class VolatilityNormalizer(FitPredictNode):
         self._scale_factor: Optional[float] = None
 
     def fit(self, df_in: pd.DataFrame) -> Dict[str, pd.DataFrame]:
-        dbg.dassert_in(self._y_var_hat, df_in.columns)
+        dbg.dassert_in(self._col, df_in.columns)
         self._scale_factor = fin.compute_volatility_normalization_factor(
-            df_in[self._y_var_hat], self._target_volatility
+            df_in[self._col], self._target_volatility
         )
-        rescaled_y_hat = self._scale_factor * df_in[self._y_var_hat]
+        rescaled_y_hat = self._scale_factor * df_in[self._col]
         df_out = self._form_output_df(df_in, rescaled_y_hat)
+        # Store info.
+        info = collections.OrderedDict()
+        info["scale_factor"] = self._scale_factor
+        self._set_info("fit", info)
         return {"df_out": df_out}
 
     def predict(self, df_in: pd.DataFrame) -> Dict[str, pd.DataFrame]:
-        dbg.dassert_in(self._y_var_hat, df_in.columns)
-        rescaled_y_hat = self._scale_factor * df_in[self._y_var_hat]
+        dbg.dassert_in(self._col, df_in.columns)
+        rescaled_y_hat = self._scale_factor * df_in[self._col]
         df_out = self._form_output_df(df_in, rescaled_y_hat)
         return {"df_out": df_out}
 
