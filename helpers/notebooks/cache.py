d9965aaa7548132dc31c4a61cc9979dab4f44ef8
Vitalii Rakovtsii <mfthemon@gmail.com>
2020-09-08 11:38:46 +0300

    Sync cache notebook py file

--- helpers/notebooks/cache.py
+++ helpers/notebooks/cache.py
@@ -1,26 +1,22 @@
 # ---
 # jupyter:
-# jupytext:
-# formats: ipynb,py:percent
-# text_representation:
-# extension: .py
-# format_name: percent
-# format_version: '1.3'
-# jupytext_version: 1.5.2
-# kernelspec:
-# display_name: Python 3
-# language: python
-# name: python3
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 # %% [markdown] pycharm={"name": "#%% md\n"}
 # # Test Cache in Jupyter Notebook
 
 # %%
-import joblib
-
-print(joblib)
-
 import helpers.cache as hcac
 
 # %%
@@ -30,19 +26,6 @@ import helpers.dbg as dbg
 # # Define computation function
 #
 # This function will be subjected to cache.
-
-# %% pycharm={"name": "#%%\n"}
-def computation_function(a, b):
-    out = a + b
-    print("Sum: %s + %s = %s" % (a, b, out))
-    return out
-
-
-inputs = (1, 2)
-exp_output = 3
-
-computation_function(*inputs)
-
 
 # %%
 def computation_function(a, b):
@@ -58,131 +41,15 @@ exp_output = 2
 
 computation_function(*inputs)
 
-
-# %% [markdown]
-# ## LRU Cache
-
-# %%
-def hello():
-    print("hello")
-
-
-print(id(hello))
-
-# %%
-import functools
-
-mem_func = functools.lru_cache(maxsize=None)(hello)
-
-mem_func()
-
-# %%
-mem_func = functools.lru_cache(maxsize=None)(hello)
-
-mem_func()
-
-
-# %%
-def hello():
-    print("hello")
-
-
-print(id(hello))
-
-
-# %%
-def hello():
-    assert 0
-    print("good bye")
-
-
-print(id(hello))
-
-# %%
-# hello()
-print(id(hello))
-mem_func()
-
-# %%
-mem_func = functools.lru_cache(maxsize=None)(hello)
-
-mem_func()
-
-# %% [markdown]
-# ## Joblib cache
-
-# %%
-import joblib
-
-disk_cache = joblib.Memory("./joblib.cache", verbose=0, compress=1)
-disk_cache.clear()
-
-
-# %%
-def hello():
-    print("hello")
-
-
-print(id(hello))
-
-cached_hello = disk_cache.cache(hello)
-
-print(id(cached_hello))
-
-# %%
-cached_hello()
-
-# %%
-cached_hello()
-
-
-# %%
-def hello():
-    print("hello")
-
-
-# %%
-cached_hello()
-
-
-# %%
-def hello():
-    print("good bye")
-
-
-cached_hello = disk_cache.cache(hello)
-
-# %%
-cached_hello()
-
-
-# %%
-def hello():
-    print("good bye")
-
-
-cached_hello = disk_cache.cache(hello)
-
-cached_hello()
-
-
 # %% [markdown] pycharm={"name": "#%% md\n"}
 # ## Memory cache
-
-# %%
-def computation_function(a, b):
-    out = a + b
-    print("Sum: %s + %s = %s" % (a, b, out))
-    return out
-
-
-inputs = (1, 2)
-exp_output = 3
 
 # %% pycharm={"name": "#%%\n"}
 memory_cached_computation = hcac.Cached(
     computation_function, use_mem_cache=True, use_disk_cache=False
 )
+
+memory_cached_computation.clear_cache()
 
 dbg.dassert_eq(memory_cached_computation(*inputs), exp_output)
 dbg.dassert_eq(memory_cached_computation.get_last_cache_accessed(), "no_cache")
@@ -206,16 +73,7 @@ inputs = (1, 2)
 exp_output = 2
 
 dbg.dassert_eq(memory_cached_computation(*inputs), exp_output)
-print(memory_cached_computation.get_last_cache_accessed())
 dbg.dassert_eq(memory_cached_computation.get_last_cache_accessed(), "mem")
-
-# %%
-memory_cached_computation._func.__code__
-memory_cached_computation._func.co_filename
-
-
-# %%
-computation_function.__code__
 
 # %% [markdown]
 # ## Disk cache
@@ -235,17 +93,6 @@ dbg.dassert_eq(disk_cached_computation.get_last_cache_accessed(), "disk")
 
 print("disk caching checks passed")
 
-# %%
-print(disk_cached_computation._execute_func_from_disk_cache.func)
-
-from joblib.func_inspect import get_func_code
-
-print(
-    get_func_code(disk_cached_computation._execute_func_from_disk_cache.func)[0]
-)
-dbg.dassert_eq(disk_cached_computation(*inputs), exp_output)
-dbg.dassert_eq(disk_cached_computation.get_last_cache_accessed(), "disk")
-
 # %% [markdown]
 # ## Full cache
 
@@ -254,7 +101,7 @@ fully_cached_computation = hcac.Cached(
     computation_function, use_mem_cache=True, use_disk_cache=True
 )
 
-fully_cached_computation.clear_cache("disk")
+fully_cached_computation.clear_cache()
 
 dbg.dassert_eq(fully_cached_computation(*inputs), exp_output)
 dbg.dassert_eq(fully_cached_computation.get_last_cache_accessed(), "no_cache")
