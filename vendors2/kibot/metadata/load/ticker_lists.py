3ef10be4180d1fd6098786da2d359e30b21a663a
Amr Awad <amro.khir@gmail.com>
2020-09-08 06:22:40 +0200

    feat: fix review comments #573

--- vendors2/kibot/metadata/load/ticker_lists.py
+++ vendors2/kibot/metadata/load/ticker_lists.py
@@ -9,17 +9,38 @@ import vendors2.kibot.metadata.types as types
 
 
 class ParsingState(enum.Enum):
-    # started parsing the file, no keywords encountered
+    # Started parsing the file, no keywords encountered.
     Started = 0
-    # A listed section header was found
+    # A listed section header was found.
     ListedSectionStarted = 1
-    # The column headers were skipped
+    # The column headers were skipped.
     HeaderSkipped = 2
-    # A delisted section header was found
+    # A delisted section header was found.
     DelistedSectionStarted = 3
 
 
 class TickerListsLoader:
+    # pylint: disable=line-too-long
+    """Parse text in the following form:
+
+    ```
+    Listed: 43 symbols and 4 gigabytes
+    Delisted: 3 symbols and 204 megabytes
+    List last updated on: 8/31/2020
+
+    Listed:
+
+    #    Symbol    StartDate    Size(MB)    Description    Exchange    Industry    Sector
+    1    AA    4/27/2007    68    "Alcoa Corporation"    NYSE    "Aluminum"    "Basic Industries"
+
+    Delisted:
+
+    1    XOM    12/1/1999    102    "Exxon Mobil Corporation"    NYSE    "Integrated oil Companies"    "Energy"
+    ```
+    """
+
+    # pylint: enable=line-too-long
+
     def get(self, ticker_list: str, listed: bool = True) -> List[types.Ticker]:
         s3_path = os.path.join(
             config.S3_PREFIX, config.TICKER_LISTS_SUB_DIR, f"{ticker_list}.txt",
@@ -49,19 +70,20 @@ class TickerListsLoader:
                 # Skip empty lines.
                 continue
 
-            if state == ParsingState.ListedSectionStarted:
+            if state == ParsingState.Started:
+                if line.strip() == "Listed:":
+                    state = ParsingState.ListedSectionStarted
+
+            elif state == ParsingState.ListedSectionStarted:
+                # First non-empty line after 'listed' section header is always the header.
                 state = ParsingState.HeaderSkipped
-                continue
 
-            if line.strip() == "Listed:":
-                state = ParsingState.ListedSectionStarted
-                continue
-            if line.strip() == "Delisted:":
-                state = ParsingState.DelistedSectionStarted
-                continue
+            elif state == ParsingState.HeaderSkipped:
+                if line.strip() == "Delisted:":
+                    state = ParsingState.DelistedSectionStarted
+                else:
+                    listed_tickers.append(self._get_ticker_from_line(line))
 
-            if state == ParsingState.HeaderSkipped:
-                listed_tickers.append(self._get_ticker_from_line(line))
             elif state == ParsingState.DelistedSectionStarted:
                 delisted_tickers.append(self._get_ticker_from_line(line))
 
@@ -69,12 +91,13 @@ class TickerListsLoader:
 
     @staticmethod
     def _get_ticker_from_line(line: str) -> types.Ticker:
+        # pylint: disable=line-too-long
         """Get a ticker from a line.
 
-        Example line:
-        #       Symbol  StartDate       Size(MB)    Description     Exchange        Industry        Sector 1
-        AA      4/27/2007       68      "Alcoa Corporation"     NYSE    "Aluminum"      "Basic Industries"
+        - Example line:
+        1    AA     4/27/2007    68    "Alcoa Corporation"    NYSE    "Aluminum"    "Basic Industries"
         """
+        # pylint: enable=line-too-long
         args = line.split("\t")
         # Remove new line from last element. Note: if we strip before splitting, the tab
         # delimiters would be removed as well if a column is empty.
--- vendors2/kibot/metadata/test/test_load.py
+++ vendors2/kibot/metadata/test/test_load.py
@@ -2,6 +2,7 @@ import os
 
 import pytest
 
+import helpers.io_ as io_
 import helpers.unit_test as hut
 import vendors2.kibot.metadata.load as load
 import vendors2.kibot.metadata.types as types
@@ -9,13 +10,51 @@ import vendors2.kibot.metadata.types as types
 
 class TestTickerListLoader(hut.TestCase):
     def test_parsing_logic(self) -> None:
-        with open(os.path.join(self.get_input_dir(), "test.txt"), "r") as fh:
-            lines = fh.readlines()
+        lines = io_.from_file(
+            file_name=os.path.join(self.get_input_dir(), "test.txt")
+        ).split("\n")
 
         loader = load.TickerListsLoader()
         listed_tickers, delisted_tickers = loader._parse_lines(lines=lines)
 
-        assert listed_tickers == [
+        self.assertEqual(
+            listed_tickers,
+            [
+                types.Ticker(
+                    Symbol="AA",
+                    StartDate="4/27/2007",
+                    Size="68",
+                    Description='"Alcoa Corporation"',
+                    Exchange="NYSE",
+                    Industry='"Aluminum"',
+                    Sector='"Basic Industries"',
+                )
+            ],
+        )
+
+        self.assertEqual(
+            delisted_tickers,
+            [
+                types.Ticker(
+                    Symbol="XOM",
+                    StartDate="12/1/1999",
+                    Size="102",
+                    Description='"Exxon Mobil Corporation"',
+                    Exchange="NYSE",
+                    Industry='"Integrated oil Companies"',
+                    Sector='"Energy"',
+                )
+            ],
+        )
+
+    @pytest.mark.skip("Disabled waiting for PartTask4139")
+    def test_real_call(self) -> None:
+        tickers = load.TickerListsLoader().get(ticker_list="dow_30_intraday")
+
+        self.assertEqual(len(tickers), 43)
+
+        self.assertEqual(
+            tickers[0],
             types.Ticker(
                 Symbol="AA",
                 StartDate="4/27/2007",
@@ -24,32 +63,5 @@ class TestTickerListLoader(hut.TestCase):
                 Exchange="NYSE",
                 Industry='"Aluminum"',
                 Sector='"Basic Industries"',
-            )
-        ]
-
-        assert delisted_tickers == [
-            types.Ticker(
-                Symbol="XOM",
-                StartDate="12/1/1999",
-                Size="102",
-                Description='"Exxon Mobil Corporation"',
-                Exchange="NYSE",
-                Industry='"Integrated oil Companies"',
-                Sector='"Energy"',
-            )
-        ]
-
-    @pytest.mark.xfail(reason="Requires S3 access")
-    def test_real_call(self) -> None:
-        tickers = load.TickerListsLoader().get(ticker_list="dow_30_intraday")
-        assert len(tickers) == 43
-
-        assert tickers[0] == types.Ticker(
-            Symbol="AA",
-            StartDate="4/27/2007",
-            Size="68",
-            Description='"Alcoa Corporation"',
-            Exchange="NYSE",
-            Industry='"Aluminum"',
-            Sector='"Basic Industries"',
+            ),
         )
