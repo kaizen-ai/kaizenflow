d12be2f1e239137671630cf262371b9c1545259f
paul <smith.paul.anthony@gmail.com>
2020-09-16 14:00:41 -0400

    PartTask4541: Add target volatility, in-sample, out-of-sample capability.

--- core/model_evaluator.py
+++ core/model_evaluator.py
@@ -1,9 +1,15 @@
+"""Import as:
+
+import core.model_evaluator as modeval
+"""
+
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 from tqdm.auto import tqdm
 
+import core.finance as fin
 import helpers.dbg as dbg
 
 _LOG = logging.getLogger(__name__)
@@ -31,6 +37,46 @@ class ModelEvaluator:
         # TODO(*): Allow configurable strategies.
         # TODO(*): Maybe required that this be called instead of always doing it.
         self.pnls = self._calculate_pnls()
+
+
+    def get_pnls(self,
+                 keys: Optional[List[Any]] = None,
+                 target_volatility: Optional[float] = None,
+                 oos_start: Optional[pd.datetime] = None,
+                 mode: Optional[str] = None):
+        """
+        Calculate PnLs from `returns` and `predictions`
+
+        :param keys: Use all available if `None`
+        :param target_volatility: Rescale PnLs to value over in-sample period
+        :param oos_start: Start of out-of-sample window (end of in-sample).
+            This parameter has no effect if `target_volatility` is `None`.
+        :return: Dictionary of rescaled PnL curves
+        """
+        keys = keys or self.valid_keys
+        dbg.dassert_isinstance(keys, list)
+        dbg.dassert_is_subset(keys, self.valid_keys)
+        mode = mode or "all_available"
+        if oos_start is not None:
+            insample_pnls = {k: self.pnls[k].loc[:oos_start] for k in keys}
+        else:
+            insample_pnls = self.pnls
+        if target_volatility is not None:
+           scale_factors = {k: fin.compute_volatility_normalization_factor(
+               srs=insample_pnls[k], target_volatility=target_volatility
+           ) for k in keys}
+        else:
+            scale_factors = {k: 1.0 for k in keys}
+        rescaled_pnls = {k: scale_factors[k] * self.pnls[k] for k in keys}
+        # NOTE: ins/oos overlap by one point as-is (consider changing).
+        if mode == "all_available":
+            return rescaled_pnls
+        elif mode == "ins":
+            return {k: v.loc[:oos_start] for k, v in rescaled_pnls.items()}
+        elif mode == "oos":
+            return {k: v.loc[oos_start:] for k, v in rescaled_pnls.items()}
+        else:
+            raise ValueError(f"Unrecognized mode {mode}.")
 
     def _calculate_pnls(self) -> Dict[Any, pd.Series]:
         """
