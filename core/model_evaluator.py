c45e64b7d200ebf08a4a558b4ba559b5b61c2a8a
paul <smith.paul.anthony@gmail.com>
2020-09-16 14:50:01 -0400

    PartTask4541: Add basic stats functionality to model evaluator.

--- core/model_evaluator.py
+++ core/model_evaluator.py
@@ -10,6 +10,7 @@ import pandas as pd
 from tqdm.auto import tqdm
 
 import core.finance as fin
+import core.statistics as stats
 import helpers.dbg as dbg
 
 _LOG = logging.getLogger(__name__)
@@ -21,7 +22,9 @@ class ModelEvaluator:
     """
 
     def __init__(
-        self, returns: Dict[Any, pd.Series], predictions: Dict[Any, pd.Series]
+        self, returns: Dict[Any, pd.Series], predictions: Dict[Any, pd.Series],
+        target_volatility: Optional[float] = None,
+        oos_start: Optional[float] = None,
     ) -> None:
         """
 
@@ -30,67 +33,145 @@ class ModelEvaluator:
         :param returns:
         :param prediction:
         """
-        self.valid_keys = self._get_valid_keys(returns, predictions)
+        self.oos_start = oos_start or None
+        self.valid_keys = list(self._get_valid_keys(returns, predictions))
         self.rets = {k: returns[k] for k in self.valid_keys}
         self.preds = {k: predictions[k] for k in self.valid_keys}
+        self.target_volatility = target_volatility or None
+        # Calculate positions
+        self.pos = self._calculate_positions()
         # Calculate pnl streams.
         # TODO(*): Allow configurable strategies.
         # TODO(*): Maybe required that this be called instead of always doing it.
-        self.pnls = self._calculate_pnls()
+        self.pnls = self._calculate_pnls(self.rets, self.pos)
+
+    def _calculate_positions(self) -> Dict[Any, pd.Series]:
+        """
+        Calculate positions from returns and predictions.
+
+        Rescales to target volatility over in-sample period (if provided).
+        """
+        pnls = self._calculate_pnls(self.rets, self.preds)
+        if self.oos_start is not None:
+            insample_pnls = {k: pnls[k].loc[:self.oos_start] for k in self.valid_keys}
+        else:
+            insample_pnls = pnls
+        if self.target_volatility is not None:
+            scale_factors = {
+                k: fin.compute_volatility_normalization_factor(
+                    srs=insample_pnls[k], target_volatility=self.target_volatility
+                )
+                for k in self.valid_keys
+            }
+        else:
+            scale_factors = {k: 1.0 for k in self.valid_keys}
+        return {k: scale_factors[k] * self.preds[k] for k in self.valid_keys}
 
     def get_pnls(
         self,
         keys: Optional[List[Any]] = None,
-        target_volatility: Optional[float] = None,
-        oos_start: Optional[pd.datetime] = None,
         mode: Optional[str] = None,
-    ):
+    ) -> Dict[Any, pd.Series]:
         """
-        Calculate PnLs from `returns` and `predictions`
+        Returns pnls for requested keys over requested range.
 
         :param keys: Use all available if `None`
-        :param target_volatility: Rescale PnLs to value over in-sample period
-        :param oos_start: Start of out-of-sample window (end of in-sample).
-            This parameter has no effect if `target_volatility` is `None`.
+        :param mode: "all_available", "ins", or "oos"
         :return: Dictionary of rescaled PnL curves
         """
         keys = keys or self.valid_keys
         dbg.dassert_isinstance(keys, list)
         dbg.dassert_is_subset(keys, self.valid_keys)
         mode = mode or "all_available"
-        if oos_start is not None:
-            insample_pnls = {k: self.pnls[k].loc[:oos_start] for k in keys}
-        else:
-            insample_pnls = self.pnls
-        if target_volatility is not None:
-            scale_factors = {
-                k: fin.compute_volatility_normalization_factor(
-                    srs=insample_pnls[k], target_volatility=target_volatility
-                )
-                for k in keys
-            }
-        else:
-            scale_factors = {k: 1.0 for k in keys}
-        rescaled_pnls = {k: scale_factors[k] * self.pnls[k] for k in keys}
         # NOTE: ins/oos overlap by one point as-is (consider changing).
         if mode == "all_available":
-            return rescaled_pnls
+            return self.pnls
         elif mode == "ins":
-            return {k: v.loc[:oos_start] for k, v in rescaled_pnls.items()}
+            return {k: v.loc[:self.oos_start] for k, v in self.pnls.items()}
         elif mode == "oos":
-            return {k: v.loc[oos_start:] for k, v in rescaled_pnls.items()}
+            return {k: v.loc[self.oos_start:] for k, v in self.pnls.items()}
         else:
             raise ValueError(f"Unrecognized mode {mode}.")
 
-    def _calculate_pnls(self) -> Dict[Any, pd.Series]:
+    def calculate_stats(self,
+        keys: Optional[List[Any]] = None,
+        mode: Optional[str] = None,
+    ) -> pd.DataFrame:
         """
-        TODO(*): Add volatility adjustment over period.
 
+        :param keys:
+        :param mode:
         :return:
         """
+        keys = keys or self.valid_keys
+        mode = mode or "all_available"
+        if mode == "all_available":
+            pnl = {k: self.pnls[k] for k in keys}
+            pos = {k: self.pos[k] for k in keys}
+            rets = {k: self.rets[k] for k in keys}
+        elif mode == "ins":
+            pnl = {k: self.pnls[k].loc[:self.oos_start] for k in keys}
+            pos = {k: self.pos[k].loc[:self.oos_start] for k in keys}
+            rets = {k: self.rets[k].loc[:self.oos_start] for k in keys}
+        elif mode == "oos":
+            pnl = {k: self.pnls[k].loc[self.oos_start:] for k in keys}
+            pos = {k: self.pos[k].loc[self.oos_start:] for k in keys}
+            rets = {k: self.rets[k].loc[self.oos_start:] for k in keys}
+        else:
+            raise ValueError(f"Unrecognized mode {mode}.")
+        stats_dict = {}
+        for key in keys:
+            stats_val = self._calculate_stats(returns=rets[key],
+                                              positions=pos[key],
+                                              pnl=pnl[key])
+            stats_dict[key] = stats_val
+        return pd.concat(stats_dict, axis=1)
+
+    def _calculate_stats(self,
+        returns: pd.Series,
+        positions: pd.Series,
+        pnl: pd.Series,
+    ) -> pd.DataFrame:
+        """
+
+        :param keys:
+        :param mode:
+        :return:
+        """
+        # Calculate stats.
+        stats_dict = {}
+        stats_dict[0] = stats.summarize_sharpe_ratio(pnl)
+        stats_dict[1] = stats.ttest_1samp(pnl)
+        stats_dict[2] = pd.Series(
+            fin.compute_kratio(pnl), index=["kratio"], name=pnl.name
+        )
+        stats_dict[3] = stats.compute_annualized_return_and_volatility(pnl)
+        stats_dict[4] = stats.compute_max_drawdown(pnl)
+        stats_dict[5] = stats.summarize_time_index_info(pnl)
+        stats_dict[6] = stats.calculate_hit_rate(pnl)
+        stats_dict[7] = pd.Series(
+            pnl.corr(returns), index=["corr_to_underlying"], name=returns.name
+        )
+        stats_dict[8] = stats.compute_bet_stats(positions, returns[positions.index])
+        stats_dict[9] = stats.compute_avg_turnover_and_holding_period(positions)
+        stats_dict[10] = stats.compute_jensen_ratio(pnl)
+        stats_dict[11] = stats.compute_forecastability(pnl)
+        stats_dict[12] = stats.compute_moments(pnl)
+        stats_dict[13] = stats.compute_special_value_stats(pnl)
+        # Sort dict by integer keys.
+        stats_dict = dict(sorted(stats_dict.items()))
+        # Combine stats into one series indexed by stats names.
+        stats_srs = pd.concat(stats_dict).droplevel(0)
+        return stats_srs
+
+    def _calculate_pnls(self,
+                        returns: Dict[Any, pd.Series],
+                        positions: Dict[Any, pd.Series]) -> Dict[Any, pd.Series]:
+        """
+        """
         pnls = {}
-        for key in tqdm(self.valid_keys):
-            pnl = self.rets[key].multiply(self.preds[key])
+        for key in tqdm(returns.keys()):
+            pnl = returns[key].multiply(positions[key])
             dbg.dassert(pnl.index.freq)
             pnls[key] = pnl
         return pnls
@@ -124,6 +205,9 @@ class ModelEvaluator:
         for k, v in input_dict.items():
             if v.empty:
                 _LOG.warning("Empty series for `k`=%s", str(k))
+                continue
+            if v[:self.oos_start].dropna().empty:
+                _LOG.warning("All-NaN in-sample for `k`=%s", str(k))
                 continue
             if v.index.freq is None:
                 _LOG.warning("No `freq` for series for `k`=%s", str(k))
