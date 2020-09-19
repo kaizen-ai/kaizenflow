4a238d052e50ccace6a7a0bac0836836fa361a3d
paul <smith.paul.anthony@gmail.com>
2020-09-19 15:37:56 -0400

    PartTask4611: Initial checkpoint of ModelPlotter class.

---
+++ core/model_plotter.py
@@ -0,0 +1,131 @@
+"""
+Import as:
+
+import core.model_plotter as modplot
+"""
+
+import logging
+from typing import Any, Dict, List, Optional, Tuple
+
+import pandas as pd
+from tqdm.auto import tqdm
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import core.model_evaluator as modeval
+import core.plotting as plot
+import helpers.dbg as dbg
+
+_LOG = logging.getLogger(__name__)
+
+
+class ModelPlotter:
+    """
+
+    """
+    def __init__(
+        self,
+        model_evaluator: modeval.ModelEvaluator,
+    ) -> None:
+        """
+
+        :param model_evaluator:
+        """
+        self.model_evaluator = model_evaluator
+
+    def plot_rets_signal_analysis(self,
+        keys: Optional[List[Any]] = None,
+        weights: Optional[List[Any]] = None,
+        mode: Optional[str] = None,
+        resample_rule: Optional[str] = None
+    ) -> None:
+        rets, _ = self.model_evaluator.aggregate_models(
+            keys=keys, weights=weights, mode=mode
+        )
+        if resample_rule is not None:
+            rets = rets.resample(rule=resample_rule).sum(min_count=1)
+        num_rows = 6
+        fig = plt.figure(constrained_layout=True, figsize=(20, 5 * num_rows))
+        gs = mpl.gridspec.GridSpec(num_rows, 2, figure=fig)
+        # qq-plot against normal.
+        plot.plot_qq(rets, ax=fig.add_subplot(gs[0, :]))
+        # Plot lineplot and density plot.
+        plot.plot_cols(
+            rets, axes=[fig.add_subplot(gs[1, :]), fig.add_subplot(gs[2, :])]
+        )
+        # Plot pnl.
+        plot.plot_pnl({"rets pnl": rets}, ax=fig.add_subplot(gs[3, :]))
+        # Plot ACF and PACF.
+        plot.plot_autocorrelation(
+            rets,
+            axes=[[fig.add_subplot(gs[4, 0]), fig.add_subplot(gs[4, -1])]],
+            fft=True,
+        )
+        # Plot power spectral density and spectrogram.
+        plot.plot_spectrum(
+            rets, axes=[[fig.add_subplot(gs[5, 0]), fig.add_subplot(gs[5, -1])]]
+        )
+
+
+    def plot_rets_and_vol(
+        self,
+        keys: Optional[List[Any]] = None,
+        weights: Optional[List[Any]] = None,
+        mode: Optional[str] = None,
+        resample_rule: Optional[str] = None,
+        plot_yearly_barplot_kwargs: Optional[dict] = None,
+        plot_monthly_heatmap_kwargs: Optional[dict] = None,
+        plot_rolling_annualized_volatility_kwargs: Optional[dict] = None,
+    ) -> None:
+        """
+        Plot returns by year and month, plot rolling volatility.
+        """
+        plot_yearly_barplot_kwargs = plot_yearly_barplot_kwargs or \
+                                     {"unit": "%"}
+        plot_monthly_heatmap_kwargs = plot_monthly_heatmap_kwargs or \
+                                      {"unit" : "%"}
+        plot_rolling_annualized_volatility_kwargs = plot_rolling_annualized_volatility_kwargs or \
+                                                    {"tau": 52, "max_depth": 1, "unit": "%"}
+        rets, _ = self.model_evaluator.aggregate_models(
+            keys=keys, weights=weights, mode=mode
+        )
+        if resample_rule is not None:
+            rets = rets.resample(rule=resample_rule).sum(min_count=1)
+        num_plots = 3
+        _, axs = plt.subplots(
+            num_plots, 1, figsize=(20, 5 * num_plots), constrained_layout=True
+        )
+        # Plot yearly returns.
+        plot.plot_yearly_barplot(
+            rets,
+            ax=axs[0],
+            figsize=(20, 5 * num_plots),
+            **plot_yearly_barplot_kwargs
+        )
+        # Plot monthly returns.
+        plot.plot_monthly_heatmap(
+            rets, ax=axs[1], **plot_monthly_heatmap_kwargs
+        )
+        events = None
+        if mode == "all_available" and self.model_evaluator.oos_start is not None:
+            events = [(self.model_evaluator.oos_start, "OOS start")]
+        # Plot volatility.
+        plot.plot_rolling_annualized_volatility(
+            rets,
+            ax=axs[2],
+            events=events,
+            **plot_rolling_annualized_volatility_kwargs,
+        )
+
+
+    def plot_sharpe_ratio_panel(self,
+                                  keys: Optional[List[Any]] = None,
+                                  weights: Optional[List[Any]] = None,
+                                  mode: Optional[str] = None,
+                                  frequencies: Optional[List[str]] = None,
+                                  ) -> None:
+        rets, _ = self.model_evaluator.aggregate_models(
+            keys=keys, weights=weights, mode=mode
+        )
+        plot.plot_sharpe_ratio_panel(rets, frequencies=frequencies)
