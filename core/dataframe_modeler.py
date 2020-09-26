2b05888f80d78f85ce30214a55b4669ed06f2600
paul <smith.paul.anthony@gmail.com>
2020-09-26 15:21:39 -0400

    PartTask4686: Collapse two classes into one and reorganize.

--- core/dataframe_modeler.py
+++ core/dataframe_modeler.py
@@ -1,3 +1,10 @@
+"""
+Import as:
+
+import core.dataframe_modeler as dfmod
+"""
+
+
 from __future__ import annotations
 
 import collections
@@ -7,20 +14,13 @@ from typing import Any, Callable, Dict, Iterable, List, Optional, Union
 
 import pandas as pd
 
+import core.dataflow as dtf
 import core.finance as fin
+import core.plotting as plot
 import core.signal_processing as sigp
 import core.statistics as stats
+import core.timeseries_study as tss
 import helpers.dbg as dbg
-from core.dataflow.nodes import (
-    ColumnTransformer,
-    ContinuousSkLearnModel,
-    FitPredictNode,
-    Resample,
-    Residualizer,
-    SmaModel,
-    UnsupervisedSkLearnModel,
-    VolatilityModel,
-)
 
 _LOG = logging.getLogger(__name__)
 
@@ -36,10 +36,10 @@ class DataFrameModeler:
     """
 
     def __init__(
-        self,
-        df: pd.DataFrame,
-        oos_start: Optional[float] = None,
-        info: Optional[Dict[str, Any]] = None,
+            self,
+            df: pd.DataFrame,
+            oos_start: Optional[float] = None,
+            info: Optional[Dict[str, Any]] = None,
     ) -> None:
         """
         Initialize by supplying a dataframe of time series.
@@ -73,21 +73,21 @@ class DataFrameModeler:
     # #########################################################################
 
     def apply_column_transformer(
-        self,
-        transformer_func: Callable[..., pd.DataFrame],
-        # TODO(Paul): Tighten this type annotation.
-        transformer_kwargs: Optional[Any] = None,
-        # TODO(Paul): May need to assume `List` instead.
-        cols: Optional[Iterable[str]] = None,
-        col_rename_func: Optional[Callable[[Any], Any]] = None,
-        col_mode: Optional[str] = None,
-        nan_mode: Optional[str] = None,
-        method: str = "fit",
+            self,
+            transformer_func: Callable[..., pd.DataFrame],
+            # TODO(Paul): Tighten this type annotation.
+            transformer_kwargs: Optional[Any] = None,
+            # TODO(Paul): May need to assume `List` instead.
+            cols: Optional[Iterable[str]] = None,
+            col_rename_func: Optional[Callable[[Any], Any]] = None,
+            col_mode: Optional[str] = None,
+            nan_mode: Optional[str] = None,
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Apply a function a a select of columns.
         """
-        model = ColumnTransformer(
+        model = dtf.ColumnTransformer(
             nid="column_transformer",
             transformer_func=transformer_func,
             transformer_kwargs=transformer_kwargs,
@@ -99,18 +99,18 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     def apply_resampler(
-        self,
-        rule: str,
-        agg_func: str,
-        resample_kwargs: Optional[Dict[str, Any]] = None,
-        agg_func_kwargs: Optional[Dict[str, Any]] = None,
-        method: str = "fit",
+            self,
+            rule: str,
+            agg_func: str,
+            resample_kwargs: Optional[Dict[str, Any]] = None,
+            agg_func_kwargs: Optional[Dict[str, Any]] = None,
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Resample the dataframe (causally, by default).
         """
         agg_func_kwargs = agg_func_kwargs or {"min_count": 1}
-        model = Resample(
+        model = dtf.Resample(
             nid="resample",
             rule=rule,
             agg_func=agg_func,
@@ -120,17 +120,17 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     def apply_residualizer(
-        self,
-        model_func: Callable[..., Any],
-        x_vars: Union[List[str], Callable[[], List[str]]],
-        model_kwargs: Optional[Any] = None,
-        nan_mode: Optional[str] = "drop",
-        method: str = "fit",
+            self,
+            model_func: Callable[..., Any],
+            x_vars: Union[List[str], Callable[[], List[str]]],
+            model_kwargs: Optional[Any] = None,
+            nan_mode: Optional[str] = "drop",
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Apply an unsupervised model and residualize.
         """
-        model = Residualizer(
+        model = dtf.Residualizer(
             nid="sklearn_residualizer",
             model_func=model_func,
             x_vars=x_vars,
@@ -140,22 +140,22 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     def apply_sklearn_model(
-        self,
-        model_func: Callable[..., Any],
-        x_vars: Union[List[str], Callable[[], List[str]]],
-        y_vars: Union[List[str], Callable[[], List[str]]],
-        steps_ahead: int,
-        model_kwargs: Optional[Any] = None,
-        col_mode: Optional[str] = "merge_all",
-        nan_mode: Optional[str] = "drop",
-        method: str = "fit",
+            self,
+            model_func: Callable[..., Any],
+            x_vars: Union[List[str], Callable[[], List[str]]],
+            y_vars: Union[List[str], Callable[[], List[str]]],
+            steps_ahead: int,
+            model_kwargs: Optional[Any] = None,
+            col_mode: Optional[str] = "merge_all",
+            nan_mode: Optional[str] = "drop",
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Apply a supervised sklearn model.
 
         Both x and y vars should be indexed by knowledge time.
         """
-        model = ContinuousSkLearnModel(
+        model = dtf.ContinuousSkLearnModel(
             nid="sklearn",
             model_func=model_func,
             x_vars=x_vars,
@@ -168,17 +168,17 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     def apply_sma_model(
-        self,
-        col: str,
-        steps_ahead: int,
-        tau: Optional[float] = None,
-        nan_mode: Optional[str] = "drop",
-        method: str = "fit",
+            self,
+            col: str,
+            steps_ahead: int,
+            tau: Optional[float] = None,
+            nan_mode: Optional[str] = "drop",
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Apply a smooth moving average model.
         """
-        model = SmaModel(
+        model = dtf.SmaModel(
             nid="sma_model",
             col=[col],
             steps_ahead=steps_ahead,
@@ -188,18 +188,18 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     def apply_unsupervised_sklearn_model(
-        self,
-        model_func: Callable[..., Any],
-        x_vars: Union[List[str], Callable[[], List[str]]],
-        model_kwargs: Optional[Any] = None,
-        col_mode: Optional[str] = "merge_all",
-        nan_mode: Optional[str] = "drop",
-        method: str = "fit",
+            self,
+            model_func: Callable[..., Any],
+            x_vars: Union[List[str], Callable[[], List[str]]],
+            model_kwargs: Optional[Any] = None,
+            col_mode: Optional[str] = "merge_all",
+            nan_mode: Optional[str] = "drop",
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Apply an unsupervised model, e.g., PCA.
         """
-        model = UnsupervisedSkLearnModel(
+        model = dtf.UnsupervisedSkLearnModel(
             nid="unsupervised_sklearn",
             model_func=model_func,
             x_vars=x_vars,
@@ -210,18 +210,18 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     def apply_volatility_model(
-        self,
-        col: str,
-        steps_ahead: int,
-        p_moment: float = 2,
-        tau: Optional[float] = None,
-        nan_mode: Optional[str] = "drop",
-        method: str = "fit",
+            self,
+            col: str,
+            steps_ahead: int,
+            p_moment: float = 2,
+            tau: Optional[float] = None,
+            nan_mode: Optional[str] = "drop",
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Model volatility.
         """
-        model = VolatilityModel(
+        model = dtf.VolatilityModel(
             nid="volatility_model",
             col=[col],
             steps_ahead=steps_ahead,
@@ -236,20 +236,20 @@ class DataFrameModeler:
     # #########################################################################
 
     def compute_ret_0(
-        self,
-        rets_mode: str = "log_rets",
-        cols: Optional[Iterable[str]] = None,
-        col_rename_func: Optional[Callable[[Any], Any]] = None,
-        col_mode: Optional[str] = None,
-        nan_mode: Optional[str] = None,
-        method: str = "fit",
+            self,
+            rets_mode: str = "log_rets",
+            cols: Optional[Iterable[str]] = None,
+            col_rename_func: Optional[Callable[[Any], Any]] = None,
+            col_mode: Optional[str] = None,
+            nan_mode: Optional[str] = None,
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Calculate returns (realized at timestamp).
         """
         col_rename_func = col_rename_func or (lambda x: str(x) + "_ret_0")
         col_mode = col_mode or "replace_all"
-        model = ColumnTransformer(
+        model = dtf.ColumnTransformer(
             nid="compute_ret_0",
             transformer_func=fin.compute_ret_0,
             transformer_kwargs={"mode": rets_mode},
@@ -261,15 +261,15 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     def set_non_ath_to_nan(
-        self,
-        start_time: Optional[datetime.time] = None,
-        end_time: Optional[datetime.time] = None,
-        method: str = "fit",
+            self,
+            start_time: Optional[datetime.time] = None,
+            end_time: Optional[datetime.time] = None,
+            method: str = "fit",
     ) -> DataFrameModeler:
         """
         Replace values at non active trading hours with NaNs.
         """
-        model = ColumnTransformer(
+        model = dtf.ColumnTransformer(
             nid="set_non_ath_to_nan",
             transformer_func=fin.set_non_ath_to_nan,
             col_mode="replace_all",
@@ -281,7 +281,7 @@ class DataFrameModeler:
         """
         Replace values over weekends with NaNs.
         """
-        model = ColumnTransformer(
+        model = dtf.ColumnTransformer(
             nid="set_weekends_to_nan",
             transformer_func=fin.set_weekends_to_nan,
             col_mode="replace_all",
@@ -289,68 +289,151 @@ class DataFrameModeler:
         return self._run_model(model, method)
 
     # #########################################################################
-    # Dataframe no-ops
+    # Dataframe stats and plotting
     # #########################################################################
 
     def calculate_stats(self,
                         cols: Optional[Iterable[Any]] = None,
-                        method: str = "fit") -> DataFrameModeler:
+                        mode: str = "ins") -> pd.DataFrame:
         """
         Calculate stats for selected columns.
         """
-        # TODO(*): Factor out this piece.
-        # Validate column selection.
-        cols = cols or self._df.columns
-        dbg.dassert_is_subset(cols, self._df.columns)
-        # Slice dataframe according to `fit` or `predict`.
-        if method == "fit":
-            df = self.ins_df[cols]
-        elif method == "predict":
-            df = self.df[cols]
-        else:
-            raise ValueError(f"Unrecognized method `{method}`.")
+        df = self._get_df(cols=cols, mode=mode)
+        # Calculate stats.
         stats_dict = {}
         for col in df.columns:
             stats_val = self._calculate_series_stats(df[col])
             stats_dict[col] = stats_val
         stats_df = pd.concat(stats_dict, axis=1)
-        # Add info.
-        method_info = collections.OrderedDict()
-        method_info["stats"] = stats_df
-        info = collections.OrderedDict()
-        info[method] = method_info
-        return DataFrameModeler(self.df, oos_start=self.oos_start, info=info)
-
+        return stats_df
 
     def correlate_with_lag(
-        self, lag: int, cols: Optional[Iterable[Any]] = None, method: str = "fit"
-    ) -> DataFrameModeler:
+            self, lag: int, cols: Optional[Iterable[Any]] = None, mode: str = "ins"
+    ) -> pd.DataFrame:
         """
         Calculate correlation of `cols` with lags of `cols`.
         """
-        # Validate column selection.
-        cols = cols or self._df.columns
-        dbg.dassert_is_subset(cols, self._df.columns)
-        # Slice dataframe according to `fit` or `predict`.
-        if method == "fit":
-            df = self.ins_df[cols]
-        elif method == "predict":
-            df = self.df[cols]
-        else:
-            raise ValueError(f"Unrecognized method `{method}`.")
+        df = self._get_df(cols=cols, mode=mode)
         # Calculate correlation.
         corr_df = sigp.correlate_with_lag(df, lag=lag)
-        # Add info.
-        method_info = collections.OrderedDict()
-        method_info["n_samples"] = df.index.size
-        method_info["correlation_matrix"] = corr_df
-        info = collections.OrderedDict()
-        info[method] = method_info
-        return DataFrameModeler(self.df, oos_start=self.oos_start, info=info)
+        return corr_df
+
+    def plot_autocorrelation(
+            self,
+            cols: Optional[List[Any]] = None,
+            plot_auto_correlation_kwargs: Optional[dict] = None,
+            mode: str = "ins",
+    ) -> None:
+        df = self._get_df(cols=cols, mode=mode)
+        plot_auto_correlation_kwargs = plot_auto_correlation_kwargs or {}
+        plot.plot_autocorrelation(df, **plot_auto_correlation_kwargs)
+
+    def plot_sequence_and_density(
+            self,
+            cols: Optional[List[Any]] = None,
+            plot_cols_kwargs: Optional[dict] = None,
+            mode: str = "ins",
+    ) -> None:
+        df = self._get_df(cols=cols, mode=mode)
+        plot_cols_kwargs = plot_cols_kwargs or {}
+        plot.plot_cols(df, **plot_cols_kwargs)
+
+    def plot_spectrum(
+            self,
+            cols: Optional[List[Any]] = None,
+            plot_spectrum_kwargs: Optional[dict] = None,
+            mode: str = "ins",
+    ) -> None:
+        df = self._get_df(cols=cols, mode=mode)
+        plot_spectrum_kwargs = plot_spectrum_kwargs or {}
+        plot.plot_spectrum(df, **plot_spectrum_kwargs)
+
+    def plot_correlation_matrix(
+            self,
+            cols: Optional[List[Any]] = None,
+            plot_correlation_matrix_kwargs: Optional[dict] = None,
+            mode: str = "ins",
+    ) -> None:
+        df = self._get_df(cols=cols, mode=mode)
+        plot_correlation_matrix_kwargs = plot_correlation_matrix_kwargs or {}
+        plot.plot_correlation_matrix(
+            df, **plot_correlation_matrix_kwargs
+        )
+
+    def plot_dendrogram(
+            self,
+            cols: Optional[List[Any]] = None,
+            figsize: Optional[Tuple[int, int]] = None,
+            mode: str = "ins",
+    ) -> None:
+        df = self._get_df(cols=cols, mode=mode)
+        plot.plot_dendrogram(df, figsize)
+
+    def plot_pca_components(
+            self,
+            cols: Optional[List[Any]] = None,
+            num_components: Optional[int] = None,
+            mode: str = "ins",
+    ) -> None:
+        df = self._get_df(cols=cols, mode=mode)
+        pca = plot.PCA(mode="standard")
+        pca.fit(df.fillna(0))
+        pca.plot_components(num_components)
+
+    def plot_explained_variance(self, cols: Optional[List[Any]] = None,
+                                mode: str = "ins") -> None:
+        df = self._get_df(cols=cols, mode=mode)
+        pca = plot.PCA(mode="standard")
+        pca.fit(df.fillna(0))
+        pca.plot_explained_variance()
+
+    def plot_qq(
+            self,
+            col: Any,
+            ax: Optional[mpl.axes.Axes] = None,
+            dist: Optional[str] = None,
+            nan_mode: Optional[str] = None,
+            mode: str = "ins",
+    ) -> None:
+        srs = self._get_df(cols=[col], mode=mode).squeeze()
+        plot.plot_qq(srs, ax=ax, dist=dist, nan_mode=nan_mode)
+
+    def plot_rolling_correlation(
+            self,
+            col1: Any,
+            col2: Any,
+            tau: float,
+            plot_rolling_correlation_kwargs: Optional[dict] = None,
+            mode: str = "ins",
+    ) -> None:
+        df = self._get_df(cols=[col1, cols2], mode=mode)
+        plot_rolling_correlation_kwargs = plot_rolling_correlation_kwargs or {}
+        plot.plot_rolling_correlation(
+            df[col1],
+            df[col2],
+            tau=tau,
+            **plot_rolling_correlation_kwargs,
+        )
+
+    def plot_time_series_study(self, col: Any, mode: str = "ins") -> None:
+        srs = self._get_df(cols=[col], mode=mode).squeeze()
+        tsds = tss.TimeSeriesDailyStudy(srs, data_name=str(col))
+        tsds.execute()
 
     # #########################################################################
     # Private helpers
     # #########################################################################
+
+    def _get_df(self, cols: Iterable[Any], mode: str) -> pd.DataFrame:
+        cols = cols or self._df.columns
+        dbg.dassert_is_subset(cols, self._df.columns)
+        if mode == "ins":
+            return self.ins_df[cols]
+        if mode == "oos":
+            return self.oos_df[cols]
+        if mode == "all_available":
+            return self.df[cols]
+        raise ValueError(f"Unrecognized mode `{mode}`")
 
     def _run_model(self, model: FitPredictNode, method: str) -> DataFrameModeler:
         info = collections.OrderedDict()
