c09e379535184d51295096a19908ffe795517355
paul <smith.paul.anthony@gmail.com>
2020-09-26 16:18:25 -0400

    PartTask4686: Remove obsolete import

--- core/dataflow/__init__.py
+++ core/dataflow/__init__.py
@@ -6,7 +6,6 @@ import core.dataflow as dtf
 
 from core.dataflow.builder import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.core import *  # pylint: disable=unused-import # NOQA
-from core.dataflow.explorer import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.nodes import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.pipelines import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.visualization import *  # pylint: disable=unused-import # NOQA
