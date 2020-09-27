4052b21f490b09d7c932da02bd8826e2f715642b
Amr Awad <amro.khir@gmail.com>
2020-09-27 09:04:08 +0200

    refactor: move normalizers to a separate module #729

--- vendors2/kibot/data/transform/convert_csv_to_pq.py
+++ vendors2/kibot/data/transform/convert_csv_to_pq.py
@@ -18,7 +18,7 @@ r"""Converts Kibot data on S3 from .csv.gz to Parquet.
 import argparse
 import logging
 import os
-from typing import Any, Callable, List, Optional
+from typing import Any, Callable, List
 
 import joblib
 import pandas as pd
@@ -31,88 +31,12 @@ import helpers.parser as prsr
 import helpers.s3 as hs3
 import helpers.system_interaction as si
 import vendors2.kibot.data.config as config
+import vendors2.kibot.data.transform.normalizers as nls
 
 _LOG = logging.getLogger(__name__)
 
 _JOBLIB_NUM_CPUS = 10
 _JOBLIB_VERBOSITY = 1
-
-
-# #############################################################################
-
-
-# TODO(gp): Call the column datetime_ET suffix.
-def _normalize_1_min(df: pd.DataFrame) -> pd.DataFrame:
-    """Convert a df with 1 min Kibot data into our internal format.
-
-    - Combine the first two columns into a datetime index
-    - Add column names
-    - Check for monotonic index
-
-    :param df: kibot raw dataframe as it is in .csv.gz files
-    :return: a dataframe with `datetime` index and `open`, `high`,
-        `low`, `close`, `vol` columns. If the input dataframe
-        has only one column, the column name will be transformed to
-        string format.
-    """
-    # There are cases in which the dataframes consist of only one column,
-    # with the first row containing a `405 Data Not Found` string, and
-    # the second one containing `No data found for the specified period
-    # for BTSQ14.`
-    if df.shape[1] > 1:
-        # According to Kibot the columns are:
-        #   Date,Time,Open,High,Low,Close,Volume
-        # Convert date and time into a datetime.
-        df[0] = pd.to_datetime(df[0] + " " + df[1], format="%m/%d/%Y %H:%M")
-        df.drop(columns=[1], inplace=True)
-        # Rename columns.
-        df.columns = "datetime open high low close vol".split()
-        df.set_index("datetime", drop=True, inplace=True)
-    else:
-        df.columns = df.columns.astype(str)
-        _LOG.warning("The dataframe has only one column:\n%s", df)
-    dbg.dassert(df.index.is_monotonic_increasing)
-    dbg.dassert(df.index.is_unique)
-    return df
-
-
-def _normalize_daily(df: pd.DataFrame) -> pd.DataFrame:
-    """Convert a df with daily Kibot data into our internal format.
-
-    - Convert the first column to datetime and set is as index
-    - Add column names
-    - Check for monotonic index
-
-    :param df: kibot raw dataframe as it is in .csv.gz files
-    :return: a dataframe with `datetime` index and `open`, `high`,
-        `low`, `close`, `vol` columns.
-    """
-    # Convert date and time into a datetime.
-    df[0] = pd.to_datetime(df[0], format="%m/%d/%Y")
-    # Rename columns.
-    df.columns = "datetime open high low close vol".split()
-    df.set_index("datetime", drop=True, inplace=True)
-    # TODO(gp): Turn date into datetime using EOD timestamp. Check on Kibot.
-    dbg.dassert(df.index.is_monotonic_increasing)
-    dbg.dassert(df.index.is_unique)
-    return df
-
-
-def _get_normalizer(dataset: str) -> Optional[Callable]:
-    """Choose a normalizer function based on a dataset name.
-
-    :param dataset: dataset name
-    """
-    ret = None
-    if dataset.endswith("1min"):
-        ret = _normalize_1_min
-    elif dataset.endswith("daily"):
-        ret = _normalize_daily
-    elif dataset.endswith("tick"):
-        dbg.dfatal("Support for dataset '%s' not implemented yet")
-    else:
-        dbg.dfatal("Unexpected dataset '%s'", dataset)
-    return ret
 
 
 # #############################################################################
@@ -180,7 +104,7 @@ def _convert_kibot_csv_gz_to_pq(
     # Convert the CSV file to PQ.
     pq_filepath = os.path.join(converted_dir, pq_filename)
     _LOG.debug("Converting '%s' file into '%s'", csv_filepath, pq_filepath)
-    normalizer = _get_normalizer(dataset)
+    normalizer = nls.get_normalizer(dataset)
     compression = "gzip"
     csv.convert_csv_to_pq(
         csv_filepath, pq_filepath, normalizer, compression=compression
@@ -219,7 +143,7 @@ def _compare_kibot_csv_gz_to_pq(
     csv_filepath = _download_from_s3(csv_gz_filename, aws_csv_gz_dir, source_dir)
     # Read the CSV file.
     csv_df = pd.read_csv(csv_filepath, header=None)
-    normalizer = _get_normalizer(dataset)
+    normalizer = nls.get_normalizer(dataset)
     if normalizer is not None:
         csv_df = normalizer(csv_df)
     # Copy the PQ file from S3 to local.
@@ -318,7 +242,9 @@ def _parse() -> argparse.ArgumentParser:
         help="Do not skip if it exists on S3",
     )
     parser.add_argument(
-        "--skip_compare", action="store_true", help="Skip compare step",
+        "--skip_compare",
+        action="store_true",
+        help="Skip compare step",
     )
     parser.add_argument(
         "--max_num_assets",
@@ -382,16 +308,17 @@ def _main(parser: argparse.ArgumentParser) -> None:
             si.system(cmd)
         # Get the symbols.
         _LOG.info(
-            "# Look for list of symbols to process for the dataset '%s'", dataset,
+            "# Look for list of symbols to process for the dataset '%s'",
+            dataset,
         )
         # TODO(gp): Replace the functions from AmpTask573.
         symbols = _get_symbols_to_process(aws_csv_gz_dir)
         # symbols = ["AAPL"]
-        #symbols = (
-        #    "AAPL AMGN AMZN BAC BIDU BRK.A C CAT COP CSCO CVX DIS EMC F FCX GE GILD "
-        #    "GMCR GOOG GS HAL HD HPQ IBM INTC JNJ JPM KO LOW LVS MCD MRK MS MSFT NFLX "
-        #    "ORCL OXY PBR PCLN PFE PG PM PSE QCOM SLB T VZ WFC WMT XOM GOOGL"
-        #).split()
+        # symbols = (
+        # "AAPL AMGN AMZN BAC BIDU BRK.A C CAT COP CSCO CVX DIS EMC F FCX GE GILD "
+        # "GMCR GOOG GS HAL HD HPQ IBM INTC JNJ JPM KO LOW LVS MCD MRK MS MSFT NFLX "
+        # "ORCL OXY PBR PCLN PFE PG PM PSE QCOM SLB T VZ WFC WMT XOM GOOGL"
+        # ).split()
         if args.max_num_assets is not None:
             dbg.dassert_lte(1, args.max_num_assets)
             symbols = symbols[: args.max_num_assets]
