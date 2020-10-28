5eb5a2f9a8dbe9e354ca497d10ba7b1d275ef4f9
Alina Khay <alina@particle.one>
2020-10-28 22:10:14 +0000

    Adressing comments

--- core/test/test_dataframe_modeler.py
+++ core/test/test_dataframe_modeler.py
@@ -11,69 +11,58 @@ _LOG = logging.getLogger(__name__)
 
 
 class TestDataFrameModeler(hut.TestCase):
-    def test_sklearnmodel_fit_with_oos(self) -> None:
+    def test_continioussklearnmodel_fit_with_oos(self) -> None:
         pred_lag = 2
-        # Load test data.
         data = self._get_data(pred_lag)
-        # Load sklearn config.
         config = self._get_config(pred_lag)
-        # Create DataFrame Modeler object.
         df_modeler = dfmod.DataFrameModeler(df=data, oos_start="2010-03-01")
-        # Apply sklearn model with fit method.
         output = df_modeler.apply_sklearn_model(**config.to_dict())
         output_df = output.df
         info_fit = output.info["fit"]
-        _LOG.info("Info Fit %s", info_fit)
         self.check_string(output_df.to_string())
+        self.check_string(info_fit.to_string())
 
-    def test_sklearnmodel_predict_with_oos(self) -> None:
+    def test_continioussklearnmodel_predict_with_oos(self) -> None:
         pred_lag = 2
-        # Load test data.
         data = self._get_data(pred_lag)
-        # Load sklearn config.
         config = self._get_config(pred_lag)
-        # Create DataFrame Modeler object.
         df_modeler = dfmod.DataFrameModeler(df=data, oos_start="2010-03-01")
-        # Apply sklearn model with predict method.
-        output = df_modeler.apply_sklearn_model(**config.to_dict(), method = "predict")
+        output = df_modeler.apply_sklearn_model(
+            **config.to_dict(), method="predict"
+        )
         output_df = output.df
         info_fit = output.info["fit"]
         info_predict = output.info["predict"]
-        _LOG.info("Info Fit %s", info_fit)
-        _LOG.info("Info Predict %s", info_predict)
         self.check_string(output_df.to_string())
+        self.check_string(info_fit.to_string())
+        self.check_string(info_predict.to_string())
 
-    def test_sklearnmodel_fit_without_oos(self) -> None:
+    def test_continioussklearnmodel_fit_without_oos(self) -> None:
         pred_lag = 2
-        # Load test data.
         data = self._get_data(pred_lag)
-        # Load sklearn config.
         config = self._get_config(pred_lag)
-        # Create DataFrame Modeler object.
         df_modeler = dfmod.DataFrameModeler(df=data)
-        # Apply sklearn model with fit method.
         output = df_modeler.apply_sklearn_model(**config.to_dict())
         output_df = output.df
         info_fit = output.info["fit"]
-        _LOG.info("Info Fit %s", info_fit)
         self.check_string(output_df.to_string())
+        self.check_string(info_fit.to_string())
 
-    def test_sklearnmodel_predict_without_oos(self) -> None:
+    def test_continioussklearnmodel_predict_without_oos(self) -> None:
         pred_lag = 2
-        # Load test data.
         data = self._get_data(pred_lag)
-        # Load sklearn config.
         config = self._get_config(pred_lag)
-        # Create DataFrame Modeler object.
         df_modeler = dfmod.DataFrameModeler(df=data)
-        # Apply sklearn model with predict method.
-        output = df_modeler.apply_sklearn_model(**config.to_dict(), method = "predict")
-        output_df = output.df
-        info_fit = output.info["fit"]
-        info_predict = output.info["predict"]
-        _LOG.info("Info Fit %s", info_fit)
-        _LOG.info("Info Predict %s", info_predict)
-        self.check_string(output_df.to_string())
+        with self.assertRaises(AssertionError):
+            output = df_modeler.apply_sklearn_model(
+                **config.to_dict(), method="predict"
+            )
+            output_df = output.df
+            info_fit = output.info["fit"]
+            info_predict = output.info["predict"]
+            self.check_string(output_df.to_string())
+            self.check_string(info_fit.to_string())
+            self.check_string(info_predict.to_string())
 
     def _get_config(self, steps_ahead: int) -> cfg.Config:
         config = cfg.Config()
