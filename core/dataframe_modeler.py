09bafbc607bf0f03d468de8baaaeec7d1e94247c
daberezina <dberezina@particle.one>
2020-10-28 11:05:26 +0300

    PartTask5123: add mode support

--- core/dataframe_modeler.py
+++ core/dataframe_modeler.py
@@ -487,16 +487,21 @@ class DataFrameModeler:
     def plot_histograms_and_lagged_scatterplot(
         self,
         lag: int,
+        mode: str = "ins",
         cols: Optional[List[Any]] = None,
         hist_kwargs: Optional[Any] = None,
         scatter_kwargs: Optional[Any] = None,
     ) -> None:
-        df = self._get_df(cols=cols, mode="all_available")
+        df = self._get_df(cols=cols, mode=mode)
+        if mode == "all_available":
+            oos_start = self.oos_start
+        else:
+            oos_start = None
         for col_name in df.columns:
             plot.plot_histograms_and_lagged_scatterplot(
                 df[col_name],
                 lag=lag,
-                oos_start=self.oos_start,
+                oos_start=oos_start,
                 title=col_name,
                 hist_kwargs=hist_kwargs,
                 scatter_kwargs=scatter_kwargs,
