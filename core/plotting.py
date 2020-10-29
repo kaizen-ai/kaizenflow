c58ebc7211a305e9ced95642c9b010890f1a28e8
Julia <julia@particle.one>
2020-10-29 20:26:51 +0300

    PartTask4840: Remove function, rename function, add default param

--- core/plotting.py
+++ core/plotting.py
@@ -198,26 +198,9 @@ def get_multiple_plots(
     return fig, ax
 
 
-def plot_projection(df: pd.DataFrame, ax: mpl.axes.Axes) -> None:
-    """
-    Plot lines above index where each column is not `NaN`.
-
-    :param df: dataframe
-    :param ax: axis on which to plot
-    """
-    for i, (_, srs) in enumerate(df.items()):
-        # Bottom `ylim` gets shifted with each new line.
-        ylim = ax.get_ylim()
-        # Replace non-nan values with bottom `ylim`.
-        ylim_srs = srs.mask(srs.notna(), ylim[0])
-        # Plot line above the index.
-        color = ax.lines[i].get_color()
-        ylim_srs.plot(ax=ax, legend=None, color=color, linewidth=1, alpha=0.8)
-
-
-def plot_special_values_indicator(
+def plot_projection(
     df: pd.DataFrame,
-    special_values: List[Any],
+    special_values: Optional[List[Any]] = None,
     mode: Optional[str] = None,
     ax: Optional[mpl.axes.Axes] = None,
     colormap: Optional[_COLORMAP] = None,
@@ -226,14 +209,15 @@ def plot_special_values_indicator(
     Plot lines where each column is not in special values.
 
     :param df: dataframe
-    :param special_values: values to omit from plot
+    :param special_values: values to omit from plot. If `None`, omit `NaN`s
     :param mode: "scatter" or "no-scatter"; whether to add a scatter plot
     :param ax: axis on which to plot. If `None`, create an axis and plot there
     :param colormap: matplotlib colormap or colormap name
     """
+    special_values = special_values or [None]
+    mode = mode or "scatter"
     ax = ax or plt.gca()
     ax.set_yticklabels([])
-    mode = mode or "scatter"
     dbg.dassert_strictly_increasing_index(df)
     dbg.dassert_no_duplicates(df.columns.tolist())
     df = df.copy()
