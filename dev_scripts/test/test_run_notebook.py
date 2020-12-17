e4edc5866f25784bc876448e305bb81e32ff3e2b
Julia <julia@particle.one>
2020-12-17 03:20:01 +0300

    PartTask5959: Update tests, add test for error

--- dev_scripts/test/test_run_notebook.py
+++ dev_scripts/test/test_run_notebook.py
@@ -1,6 +1,8 @@
 import logging
 from typing import List
 
+import pytest
+
 import core.config as cfg
 import core.config_builders as cfgb
 import helpers.system_interaction as si
@@ -9,30 +11,43 @@ import helpers.unit_test as hut
 _LOG = logging.getLogger(__name__)
 
 
+@pytest.mark.slow
 class TestRunNotebook(hut.TestCase):
     def test_main1(self) -> None:
-        dst_dir = self.get_output_dir()
+        dst_dir = self.get_scratch_space()
         cmd = (
             "/commodity_research/amp/dev_scripts/notebooks/run_notebook.py "
             f"--dst_dir {dst_dir} "
             "--notebook /commodity_research/amp/dev_scripts/notebooks/test_run_notebook.ipynb "
             "--function 'amp.dev_scripts.test.test_run_notebook.build_configs()' "
-            "--abort_on_error False "
+            "--no_abort_on_error "
             "--num_threads 1"
         )
         si.system(cmd)
 
     def test_main2(self) -> None:
-        dst_dir = self.get_output_dir()
+        dst_dir = self.get_scratch_space()
         cmd = (
             "/commodity_research/amp/dev_scripts/notebooks/run_notebook.py "
             f"--dst_dir {dst_dir} "
             "--notebook /commodity_research/amp/dev_scripts/notebooks/test_run_notebook.ipynb "
             "--function 'amp.dev_scripts.test.test_run_notebook.build_configs()' "
-            "--abort_on_error False "
+            "--no_abort_on_error "
             "--num_threads 3"
         )
         si.system(cmd)
+
+    def test_main3(self) -> None:
+        dst_dir = self.get_scratch_space()
+        cmd = (
+            "/commodity_research/amp/dev_scripts/notebooks/run_notebook.py "
+            f"--dst_dir {dst_dir} "
+            "--notebook /commodity_research/amp/dev_scripts/notebooks/test_run_notebook.ipynb "
+            "--function 'amp.dev_scripts.test.test_run_notebook.build_configs()' "
+            "--num_threads 3"
+        )
+        with self.assertRaises(RuntimeError):
+            si.system(cmd)
 
 
 def build_configs() -> List[cfg.Config]:
