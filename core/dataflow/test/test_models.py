546b9c066eebfcaba61b47d1d8d562bf423fd447
Julia <julia@particle.one>
2020-12-21 20:43:52 +0300

    PartTask6353: Stop shifting test col

--- core/dataflow/test/test_models.py
+++ core/dataflow/test/test_models.py
@@ -680,8 +680,7 @@ class TestSmaModel(hut.TestCase):
 
 class TestVolatilityModulator(hut.TestCase):
     def test_modulate1(self) -> None:
-        steps_ahead = 2
-        df_in = self._get_signal_and_fwd_vol(steps_ahead)
+        df_in = self._get_signal_and_fwd_vol()
         # Get mock returns prediction 1 step ahead indexed by knowledge time.
         y_hat = sigp.compute_smooth_moving_average(df_in["ret_0"], 4).shift(-1)
         df_in["ret_1_hat"] = y_hat
@@ -706,8 +705,7 @@ class TestVolatilityModulator(hut.TestCase):
         self.check_string(output_str)
 
     def test_demodulate1(self) -> None:
-        steps_ahead = 2
-        df_in = self._get_signal_and_fwd_vol(steps_ahead)
+        df_in = self._get_signal_and_fwd_vol()
         config = cfgb.get_config_from_nested_dict(
             {
                 "signal_cols": ["ret_0"],
@@ -729,8 +727,7 @@ class TestVolatilityModulator(hut.TestCase):
         self.check_string(output_str)
 
     def test_col_mode1(self) -> None:
-        steps_ahead = 2
-        df_in = self._get_signal_and_fwd_vol(steps_ahead)
+        df_in = self._get_signal_and_fwd_vol()
         config = cfgb.get_config_from_nested_dict(
             {
                 "signal_cols": ["ret_0"],
@@ -754,8 +751,7 @@ class TestVolatilityModulator(hut.TestCase):
         self.check_string(output_str)
 
     def test_col_mode2(self) -> None:
-        steps_ahead = 2
-        df_in = self._get_signal_and_fwd_vol(steps_ahead)
+        df_in = self._get_signal_and_fwd_vol()
         config = cfgb.get_config_from_nested_dict(
             {
                 "signal_cols": ["ret_0"],
@@ -779,18 +775,15 @@ class TestVolatilityModulator(hut.TestCase):
         self.check_string(output_str)
 
     @staticmethod
-    def _get_signal_and_fwd_vol(
-        steps_ahead: int,
-    ) -> pd.DataFrame:
+    def _get_signal_and_fwd_vol() -> pd.DataFrame:
         arma_process = sig_gen.ArmaProcess([0.45], [0])
         date_range_kwargs = {"start": "2010-01-01", "periods": 40, "freq": "B"}
         signal = arma_process.generate_sample(
             date_range_kwargs=date_range_kwargs, scale=0.1, seed=42
         )
         vol = sigp.compute_smooth_moving_average(signal, 16)
-        fwd_vol = vol.shift(-steps_ahead)
         return pd.concat(
-            [signal.rename("ret_0"), fwd_vol.rename("vol_2_hat")], axis=1
+            [signal.rename("ret_0"), vol.rename("vol_2_hat")], axis=1
         )
 
 
