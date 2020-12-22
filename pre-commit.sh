4631bcd39e8456c3b0d8e61eb31b268d131f11f8
gad26032 <gad26032@gmail.com>
2020-12-22 16:59:25 +0300

    PartTask6447: update pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,27 @@
+#!/usr/bin/env bash
+# When we run pre-commit in a Git submodule we can't mount the submodule directory in Docker.
+# This is because pre-commit gets confused by the fact that the directory looks like a submodule, but
+# there is no including Git repository. To work around this problem we mount the entire Git super-project
+# repository and then we tweak the `WORK_DIR` to change the working directory to the submodule.
+# See PartTask6350 for more context.
+# We file an issue in pre-commit official repo, but they won't going to fix this.
+# https://github.com/pre-commit/pre-commit/issues/1734
+
+if [ $(git rev-parse --show-superproject-working-tree) ]; then
+    SUBMODULE_NAME=$(basename "$(pwd)")
+    echo "Running linter for '$SUBMODULE_NAME' submodule."
+    WORK_DIR="/src/$SUBMODULE_NAME"
+    REPO_ROOT=$(git rev-parse --show-superproject-working-tree)
+  else
+    echo "Running linter for the repository."
+    WORK_DIR="/src"
+    REPO_ROOT="$(pwd)"
+fi
+
 docker run --rm -t \
     --env "PRE_COMMIT_HOME=$HOME/.cache/pre-commit" \
     --env "SKIP=$SKIP" \
     -v "$HOME/.cache:$HOME/.cache:rw" \
-    -v "$(pwd)":/src \
-    --workdir /src \
+    -v "$REPO_ROOT":/src \
+    --workdir "$WORK_DIR" \
     083233266530.dkr.ecr.us-east-2.amazonaws.com/dev_tools:prod "$@"
