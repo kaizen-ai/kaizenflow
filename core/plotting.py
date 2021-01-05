fed866d09992b72897ce80d5e2eb675da1631279
Daria Berezina <dberezina@particle.one>
2021-01-05 12:22:15 +0000

    PartTask5253: None instead value by default for ylim

--- core/plotting.py
+++ core/plotting.py
@@ -1981,7 +1981,7 @@ def plot_rolling_correlation(
     ax: Optional[mpl.axes.Axes] = None,
     events: Optional[List[Tuple[str, Optional[str]]]] = None,
     plot_zero_line: bool = True,
-    ylim: Optional[str] = "scalable",
+    ylim: Optional[str] = None,
 ) -> None:
     """
     Return rolling correlation between 2 series and plot rolling correlation.
@@ -1997,7 +1997,7 @@ def plot_rolling_correlation(
     :param mode: corr or zcorr
     :param ax: axis
     :param events: list of tuples with dates and labels to point out on the plot
-    :param ylim: either "fixed" or "scalable". If ylim is set to "fixed", 
+    :param ylim: either "fixed" or "scalable" (if None). If ylim is set to "fixed", 
         the y-axis limits are (-1, 1).
     """
     mode = mode or "corr"
@@ -2027,10 +2027,13 @@ def plot_rolling_correlation(
     )
     # Plot rolling correlation.
     roll_corr.plot(ax=ax, title=title, label=label)
+    ylim = ylim or "scalable"
     if ylim == "fixed":
         ax.set_ylim(-1, 1)
-    elif ylim != "scalable":
-        raise ValueError("Invalid ylim='%s'" % ylim)
+    elif ylim == "scalable":
+        pass
+    else:
+        raise ValueError("Invalid `ylim`='%s'" % ylim)
     # Calculate correlation whole period.
     whole_period = srs1.corr(srs2)
     # Plot correlation whole period.
