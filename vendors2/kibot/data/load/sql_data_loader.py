ba29848bc20aade540ab9690ba3b52ad5a7fab68
Vitalii Rakovtsii <mfthemon@gmail.com>
2021-01-11 20:50:18 +0200

    Add SQLKibotDataLoader

---
+++ vendors2/kibot/data/load/sql_data_loader.py
@@ -0,0 +1,156 @@
+from typing import Optional
+
+import pandas as pd
+import psycopg2
+import psycopg2.extensions as pexten
+
+import helpers.dbg as dbg
+import vendors2.kibot.data.types as vkdtyp
+
+
+class SQLKibotDataLoader:
+    def __init__(self, dbname: str, user: str, password: str, host: str):
+        self.conn: psycopg2.extensions.connection = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+        )
+
+    def read_data(
+        self,
+        exchange: str,
+        symbol: str,
+        frequency: vkdtyp.Frequency,
+        nrows: Optional[int] = None,
+    ) -> pd.DataFrame:
+        """
+        Read kibot data.
+
+        :param exchange:
+        :param symbol: symbol to get the data for
+        :param frequency: `D` or `T` for daily or minutely data respectively
+        :param nrows: if not None, return only the first nrows of the data
+        :return: a dataframe with the symbol data
+        """
+        return self._read_data(
+            exchange=exchange,
+            symbol=symbol,
+            frequency=frequency,
+            nrows=nrows,
+        )
+
+    def get_exchange_id(
+        self,
+        exchange: str,
+    ) -> int:
+        """
+        Get primary key (id) of the Exchange entry by its name.
+
+        :param exchange: Name of the Exchange entry as defined in DB.
+        :return: primary key (id)
+        """
+        exchange_id = -1
+        with self.conn:
+            with self.conn.cursor() as curs:
+                curs.execute(
+                    "SELECT id FROM Exchange WHERE name = %s", [exchange]
+                )
+                if curs.rowcount:
+                    (_exchange_id,) = curs.fetchone()
+                    exchange_id = _exchange_id
+        if exchange_id == 0:
+            dbg.dfatal(f"Could not find Exchange ${exchange}")
+        return exchange_id
+
+    def get_symbol_id(
+        self,
+        symbol: str,
+    ) -> int:
+        """
+        Get primary key (id) of the Symbol entry by its symbol code.
+
+        :param symbol: symbol code, e.g. GOOGL
+        :return: primary key (id)
+        """
+        symbol_id = -1
+        with self.conn:
+            with self.conn.cursor() as curs:
+                curs.execute("SELECT id FROM Symbol WHERE code = %s", [symbol])
+                if curs.rowcount:
+                    (_symbol_id,) = curs.fetchone()
+                    symbol_id = _symbol_id
+        if symbol_id == -1:
+            dbg.dfatal(f"Could not find Symbol ${symbol}")
+        return symbol_id
+
+    def get_trade_symbol_id(
+        self,
+        symbol_id: int,
+        exchange_id: int,
+    ) -> int:
+        """
+        Get primary key (id) of the TradeSymbol entry by its respective Symbol
+        and Exchange ids.
+
+        :param symbol_id: id of Symbol
+        :param exchange_id: id of Exchange
+        :return: primary key (id)
+        """
+        trade_symbol_id = -1
+        with self.conn:
+            with self.conn.cursor() as curs:
+                curs.execute(
+                    "SELECT id FROM TradeSymbol "
+                    "WHERE symbol_id = %s AND exchange_id = %s",
+                    [symbol_id, exchange_id],
+                )
+                if curs.rowcount:
+                    (_trade_symbol_id,) = curs.fetchone()
+                    trade_symbol_id = _trade_symbol_id
+        if trade_symbol_id == -1:
+            dbg.dfatal(
+                f"Could not find Trade Symbol with "
+                f"symbol_id=${symbol_id} and exchange_id=${exchange_id}"
+            )
+        return trade_symbol_id
+
+    @staticmethod
+    def get_table_name_by_frequency(frequency: vkdtyp.Frequency) -> str:
+        """
+        Get table name by predefined frequency.
+
+        :param frequency: a predefined frequency
+        :return: table name in DB
+        """
+        table_name = ""
+        if frequency == vkdtyp.Frequency.Minutely:
+            table_name = "MinuteData"
+        elif frequency == vkdtyp.Frequency.Daily:
+            table_name = "DailyData"
+        elif frequency == vkdtyp.Frequency.Tick:
+            table_name = "TickData"
+        dbg.dassert(table_name, f"Unknown frequency {frequency}")
+        return table_name
+
+    def _read_data(
+        self,
+        exchange: str,
+        symbol: str,
+        frequency: vkdtyp.Frequency,
+        nrows: Optional[int] = None,
+    ) -> pd.DataFrame:
+        exchange_id = self.get_exchange_id(exchange)
+        symbol_id = self.get_symbol_id(symbol)
+        trade_symbol_id = self.get_trade_symbol_id(symbol_id, exchange_id)
+        table_name = self.get_table_name_by_frequency(frequency)
+        limit = pexten.AsIs("ALL")
+        if nrows:
+            limit = nrows
+        query = "select * from %s where trade_symbol_id = %s limit %s"
+        df = pd.read_sql_query(
+            query,
+            self.conn,
+            params=[pexten.AsIs(table_name), trade_symbol_id, limit],
+        )
+        return df
