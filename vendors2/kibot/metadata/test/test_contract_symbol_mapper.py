d767b2468cb444ddc734fb891bb9d3be7dff6b71
Stan van Rooy <stan@rooy.dev>
2021-01-12 17:17:43 +0100

    update according to review

--- vendors2/kibot/metadata/test/test_contract_symbol_mapper.py
+++ vendors2/kibot/metadata/test/test_contract_symbol_mapper.py
@@ -5,54 +5,56 @@ import helpers.unit_test as hut
 import vendors2.kibot.metadata.load.contract_symbol_mapping as csm
 
 
-def mock_test_contract_symbol_mapping_init(self: csm.ContractSymbolMapping):
-    md = {
-        'Kibot_symbol': ['test'],
-        'Exchange_abbreviation': ['tt'],
-        'Exchange_symbol': ['TT']
-     }
-    self._metadata = pd.DataFrame.from_dict(md)
-
-
 class TestContractSymbolMapper(hut.TestCase):
     @staticmethod
     def _mock_csm():
+        def mock_test_contract_symbol_mapping_init(self: csm.ContractSymbolMapping):
+            md = {
+                'Kibot_symbol': ['test_symbol'],
+                'Exchange_abbreviation': ['test_exch_abbreviation'],
+                'Exchange_symbol': ['test_exch_symbol']
+            }
+            self._metadata = pd.DataFrame.from_dict(md)
         return patch.object(csm.ContractSymbolMapping, '__init__', mock_test_contract_symbol_mapping_init)
 
     def test_get_contract1(self):
         """Valid input returns a valid output."""
         with self._mock_csm():
             cls = csm.ContractSymbolMapping()
-            inp = 'test'
-            exp = 'tt:TT'
-            self.assertEqual(exp, cls.get_contract(inp))
+            inp = 'test_symbol'
+            exp = 'test_exch_abbreviation:test_exch_symbol'
+            act = cls.get_contract(inp)
+            self.assertEqual(exp, act)
 
     def test_get_contract2(self):
-        """No input returns null"""
+        """No input returns null."""
         with self._mock_csm():
             cls = csm.ContractSymbolMapping()
             inp = ''
             exp = None
-            self.assertEqual(exp, cls.get_contract(inp))
+            act = cls.get_contract(inp)
+            self.assertEqual(exp, act)
 
     def test_get_contract3(self):
-        """Invalid input returns null"""
+        """Invalid input returns null."""
         with self._mock_csm():
             cls = csm.ContractSymbolMapping()
-            inp = 'test2'
+            inp = 'non-existent'
             exp = None
-            self.assertEqual(exp, cls.get_contract(inp))
+            act = cls.get_contract(inp)
+            self.assertEqual(exp, act)
 
     def test_get_kibot_symbol1(self):
-        """Valid input returns valid output"""
+        """Valid input returns valid output."""
         with self._mock_csm():
             cls = csm.ContractSymbolMapping()
-            inp = 'tt:TT'
-            exp = 'test'
-            self.assertEqual(exp, cls.get_kibot_symbol(inp))
+            inp = 'test_exch_abbreviation:test_exch_symbol'
+            exp = 'test_symbol'
+            act = cls.get_kibot_symbol(inp)
+            self.assertEqual(exp, act)
 
     def test_get_kibot_symbol2(self):
-        """No input raises an error"""
+        """No input raises an error."""
         with self._mock_csm():
             cls = csm.ContractSymbolMapping()
             inp = ''
@@ -60,7 +62,7 @@ class TestContractSymbolMapper(hut.TestCase):
                 cls.get_kibot_symbol(inp)
 
     def test_get_kibot_symbol3(self):
-        """Incorrect input raises an error"""
+        """Incorrect input raises an error."""
         with self._mock_csm():
             cls = csm.ContractSymbolMapping()
             inp = 'test2'
@@ -68,9 +70,10 @@ class TestContractSymbolMapper(hut.TestCase):
                 cls.get_kibot_symbol(inp)
 
     def test_get_kibot_symbol4(self):
-        """Invalid input raises an error"""
+        """Invalid input raises an error."""
         with self._mock_csm():
             cls = csm.ContractSymbolMapping()
-            inp = 'test:test'
+            inp = 'non-existent:non-existent'
             exp = None
-            self.assertEqual(exp, cls.get_kibot_symbol(inp))
+            act = cls.get_kibot_symbol(inp)
+            self.assertEqual(exp, act)
