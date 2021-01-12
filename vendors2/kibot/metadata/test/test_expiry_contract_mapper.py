8b244012ad0038733e63024a27c7f24141f019f9
Stan van Rooy <stan@rooy.dev>
2021-01-12 17:22:10 +0100

    update according to review

--- vendors2/kibot/metadata/test/test_expiry_contract_mapper.py
+++ vendors2/kibot/metadata/test/test_expiry_contract_mapper.py
@@ -4,99 +4,110 @@ import vendors2.kibot.metadata.load.expiry_contract_mapper as ecm
 
 class TestExpiryContractMapper(hut.TestCase):
     def test_parse_expiry_contract1(self) -> None:
-        """Valid input returns valid output"""
+        """Valid input returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = 'TTH10'
         exp = ('TT', 'H', '10')
-        self.assertTupleEqual(exp, cls.parse_expiry_contract(inp))
+        act = cls.parse_expiry_contract(inp)
+        self.assertEqual(exp, act)
 
     def test_parse_expiry_contract2(self) -> None:
-        """Valid input with 1 letter contract returns valid input"""
+        """Valid input with 1 letter contract returns valid input."""
         cls = ecm.ExpiryContractMapper()
         inp = "TU10"
         exp = ("T", "U", "10")
-        self.assertTupleEqual(exp, cls.parse_expiry_contract(inp))
+        act = cls.parse_expiry_contract(inp)
+        self.assertEqual(exp, act)
 
     def test_parse_expiry_contract3(self) -> None:
-        """Incorrect input returns an error"""
+        """Incorrect input returns an error."""
         cls = ecm.ExpiryContractMapper()
         inp = 'TTH1'
         with self.assertRaises(AssertionError):
             cls.parse_expiry_contract(inp)
 
     def test_compare_expiry_contract1(self) -> None:
-        """Valid input with x > y based on months returns valid output"""
+        """Valid input with x > y based on months returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ('TUG10', 'TUF10')
         exp = 1
-        self.assertEqual(exp, cls.compare_expiry_contract(*inp))
+        act = cls.compare_expiry_contract(*inp)
+        self.assertEqual(exp, act)
 
     def test_compare_expiry_contract2(self) -> None:
-        """Valid input with x < y based on months returns valid output"""
+        """Valid input with x < y based on months returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ('TUF10', 'TUG10')
         exp = -1
-        self.assertEqual(exp, cls.compare_expiry_contract(*inp))
+        act = cls.compare_expiry_contract(*inp)
+        self.assertEqual(exp, act)
 
     def test_compare_expiry_contract3(self) -> None:
-        """Valid input with x == y returns valid output"""
+        """Valid input with x == y returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ('TUF10', 'TUF10')
         exp = 0
-        self.assertEqual(exp, cls.compare_expiry_contract(*inp))
+        act = cls.compare_expiry_contract(*inp)
+        self.assertEqual(exp, act)
 
     def test_compare_expiry_contract4(self) -> None:
-        """Valid input with x > y based on months returns valid output"""
+        """Valid input with x > y based on months returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ('TUF11', 'TUF10')
         exp = 1
-        self.assertEqual(exp, cls.compare_expiry_contract(*inp))
+        act = cls.compare_expiry_contract(*inp)
+        self.assertEqual(exp, act)
 
     def test_compare_expiry_contract5(self) -> None:
-        """Valid input with x < y based on months returns valid output"""
+        """Valid input with x < y based on months returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ('TUF10', 'TUF11')
         exp = -1
-        self.assertEqual(exp, cls.compare_expiry_contract(*inp))
+        act = cls.compare_expiry_contract(*inp)
+        self.assertEqual(exp, act)
 
     def test_compare_expiry_contract6(self) -> None:
-        """Invalid input raises an error"""
+        """Invalid input raises an error."""
         cls = ecm.ExpiryContractMapper()
         inp = ('TU10', 'TTU10')
         with self.assertRaises(AssertionError):
             cls.compare_expiry_contract(*inp)
 
     def test_compare_expiry_contract7(self) -> None:
-        """Empty input raises an error"""
+        """Empty input raises an error."""
         cls = ecm.ExpiryContractMapper()
         inp = ('', '')
         with self.assertRaises(AssertionError):
             cls.compare_expiry_contract(*inp)
 
     def test_sort_expiry_contract1(self) -> None:
-        """Valid input with differing months returns valid output"""
+        """Valid input with differing months returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ['TTZ10', 'TTV10', 'TTX10', 'TTN10', 'TTQ10']
         exp = ['TTN10', 'TTQ10', 'TTV10', 'TTX10', 'TTZ10']
-        self.assertEqual(exp, cls.sort_expiry_contract(inp))
+        act = cls.sort_expiry_contract(inp)
+        self.assertEqual(exp, act)
 
     def test_sort_expiry_contract2(self) -> None:
-        """Valid input with differing years returns valid output"""
+        """Valid input with differing years returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ['TTF15', 'TTF13', 'TTF14', 'TTF17', 'TTF11']
         exp = ['TTF11', 'TTF13', 'TTF14', 'TTF15', 'TTF17']
-        self.assertEqual(exp, cls.sort_expiry_contract(inp))
+        act = cls.sort_expiry_contract(inp)
+        self.assertEqual(exp, act)
 
     def test_sort_expiry_contract3(self) -> None:
-        """Valid input with differing months & years returns valid output"""
+        """Valid input with differing months & years returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ['TTK15', 'TTG13', 'TTJ14', 'TTK17', 'TTF11']
         exp = ['TTF11', 'TTG13', 'TTJ14', 'TTK15', 'TTK17']
-        self.assertEqual(exp, cls.sort_expiry_contract(inp))
+        act = cls.sort_expiry_contract(inp)
+        self.assertEqual(exp, act)
 
     def test_sort_expiry_contract4(self) -> None:
-        """Valid input with duplicate dates returns valid output"""
+        """Valid input with duplicate dates returns valid output."""
         cls = ecm.ExpiryContractMapper()
         inp = ['TTK15', 'TTG13', 'TTG15', 'TTG17', 'TTF11']
         exp = ['TTF11', 'TTG13', 'TTG15', 'TTK15', 'TTG17']
-        self.assertEqual(exp, cls.sort_expiry_contract(inp))
+        act = cls.sort_expiry_contract(inp)
+        self.assertEqual(exp, act)
