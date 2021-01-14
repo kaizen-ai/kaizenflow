77588174f0c09617e86523da3d0faed108c474d5
DanilYachmenev <dan@particle.one>
2021-01-14 16:54:33 +0300

    PartTask6831: refactor test

--- helpers/test/test_timer.py
+++ helpers/test/test_timer.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import helpers.timer as timer
 import helpers.unit_test as hut
@@ -7,12 +8,12 @@ import helpers.unit_test as hut
 class TestTimedScope(hut.TestCase):
     def test_1(self) -> None:
         """
-        Test that elapsed time is correctly extracted.
+        Test that elapsed time is correctly computed.
         """
-        # Run the function on test calculations.
-        with timer.TimedScope(logging.INFO, "Test calculations") as ts:
-            _ = 2 ** 10 ** 8
+        # Run the function to test.
+        with timer.TimedScope(logging.INFO, "Test") as ts:
+            time.sleep(1)
         # Round actual time up to 1 decimal and compare it with expected.
         actual_rounded_time = round(ts.elapsed_time[-1], 1)
-        expected_rounded_time = 0.4
+        expected_rounded_time = 1.0
         self.assertEqual(actual_rounded_time, expected_rounded_time)
