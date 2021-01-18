5ddda0f5315edb697975fee2fd32270a63ed2a3f
paul <smith.paul.anthony@gmail.com>
2021-01-18 10:02:43 -0500

    Task6786: Fix module import.

--- core/dataflow/__init__.py
+++ core/dataflow/__init__.py
@@ -6,9 +6,9 @@ import core.dataflow as dtf
 
 from core.dataflow.builder import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.core import *  # pylint: disable=unused-import # NOQA
-from core.dataflow.dag_runner import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.models import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.nodes import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.pipelines import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.result_bundle import *  # pylint: disable=unused-import # NOQA
+from core.dataflow.runner import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.visualization import *  # pylint: disable=unused-import # NOQA
