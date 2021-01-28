81190770aff22b6365634467b1dfb0ace6cdf25a
paul <smith.paul.anthony@gmail.com>
2021-01-28 15:06:42 +0000

    Refactor notebook.

--- vendors2/kibot/notebooks/gallery_kibot.py
+++ vendors2/kibot/notebooks/gallery_kibot.py
@@ -13,24 +13,63 @@
 # ---
 
 # %%
+import pandas as pd
+
+import vendors2.kibot.data.load.futures_forward_contracts as ffc
 import vendors2.kibot.metadata.load.kibot_metadata as kmd
-
-lfc = kmd.KibotHardcodedContractLifetimeComputer(365, 7)
-
-lfc.compute_lifetime("CLJ17")
+import vendors2.kibot.metadata.load.expiry_contract_mapper as ecm
+import vendors2.kibot.data.load.s3_data_loader as vkdls3
 
 # %%
-lfc = kmd.KibotTradingActivityContractLifetimeComputer()
+lfc_hc = kmd.KibotHardcodedContractLifetimeComputer(365, 7)
 
-lfc.compute_lifetime("CLJ17")
+lfc_hc.compute_lifetime("CLJ17")
+
+# %%
+lfc_ta = kmd.KibotTradingActivityContractLifetimeComputer()
+
+lfc_ta.compute_lifetime("CLJ17")
 
 # %%
 ##
 
 # %%
 symbols = ["ES", "CL"]
-file = "./contracts.csv"
+file = "../contracts.csv"
 
-lfc = kmd.KibotHardcodedContractLifetimeComputer(365, 7)
 
-kmd = kmd.ContractsLoader(symbols, file, lfc, refresh=True)
+fcl = kmd.FuturesContractLifetimes(file, lfc_hc)
+
+# %%
+fcl.save(["CL"])
+
+# %%
+cl_data = fcl.load(["CL"])
+
+# %%
+cl_data["CL"].head()
+
+# %%
+
+# %%
+fcem = kmd.FuturesContractExpiryMapper(cl_data)
+
+# %%
+fcem.get_nth_contract("CL", "2010-01-01", 1)
+
+# %%
+srs = fcem.get_nth_contracts("CL", "2010-01-10", "2010-01-20", freq="B", n=1)
+
+# %%
+srs
+
+# %%
+kdl = vkdls3.S3KibotDataLoader()
+
+# %%
+ffc_obj = ffc.FuturesForwardContracts(kdl)
+
+# %%
+ffc_obj._replace_contracts_with_data(srs)
+
+# %%
