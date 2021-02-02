eadb28517268ba0e48803c1975be3e6debc5d385
gad26032 <gad26032@gmail.com>
2021-02-02 16:17:53 +0300

    AmpTask942: fix

--- docker_build/gh_action_entrypoint.sh
+++ docker_build/gh_action_entrypoint.sh
@@ -2,7 +2,6 @@
 
 source docker_build/entrypoint/patch_environment_variables.sh
 source docker_build/entrypoint/gh_action_aws_credentials.sh
-source docker_build/entrypoint/gh_action_google_secret_credentials.sh
 
 mount -a
 
@@ -13,5 +12,5 @@ umask 000
 
 ./docker_build/test/test_mount_fsx.sh
 ./docker_build/test/test_mount_s3.sh
-./docker_build/test/test_volumes.sh
+
 exec "$@"
