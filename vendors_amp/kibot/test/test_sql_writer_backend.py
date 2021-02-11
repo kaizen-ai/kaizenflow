2211bdda369c87603f21c5f530653bb005cd9cb2
Artem <amashkovskii@particle.one>
2021-02-11 22:15:30 +0000

    AmpTask953: refactored test

--- vendors_amp/kibot/test/test_sql_writer_backend.py
+++ vendors_amp/kibot/test/test_sql_writer_backend.py
@@ -1,90 +1,111 @@
 import os
-
+import time
 import pandas as pd
-
+import pytest
 import helpers.unit_test as hut
 import vendors_amp.kibot.data.types as vkdtyp
 import vendors_amp.kibot.sql_writer_backend as vksqlw
+import helpers.io_ as hio
+import psycopg2
+import psycopg2.sql as sql
+
+DB_SCHEMA_FILE = os.path.join(os.path.dirname(__file__),
+                              "../compose/init_sql/db.sql")
 
 
+class TestDbSchemaFile(hut.TestCase):
+    """
+    Test SQL initialization file existence.
+    """
+    def test_exist1(self) -> None:
+        """
+        Test that schema SQL file exists.
+        """
+        self.assertTrue(os.path.exists(DB_SCHEMA_FILE))
+
+
+@pytest.mark.skip(reason="Testable only inside kibot container")
 class TestSqlWriterBackend1(hut.TestCase):
     """
     Test writing operation to Postgresql kibot db.
     """
-
     def setUp(self) -> None:
         super().setUp()
+        # Get postgresql connection parameters.
         host = os.environ["POSTGRES_HOST"]
         port = os.environ["POSTGRES_PORT"]
         user = os.environ["POSTGRES_USER"]
-        pwrd = os.environ["POSTGRES_PASSWORD"]
-        dbnm = os.environ["POSTGRES_DB"]
-        self._writer = vksqlw.SQLWriterBackend(dbnm, user, pwrd, host, port)
+        password = os.environ["POSTGRES_PASSWORD"]
+        self._dbname = self._get_test_string()
+        # Create database for each test.
+        create_database(self._dbname)
+        # Initialize writer class to test.
+        self._writer = vksqlw.SQLWriterBackend(self._dbname, user, password,
+                                               host, port)
+        # Apply production schema to created database.
+        with self._writer.conn as conn:
+            with conn.cursor() as curs:
+                curs.execute(hio.from_file(DB_SCHEMA_FILE))
+        # Define constant id-s for records across the test.
+        self._symbol_id = 10
+        self._exchange_id = 20
+        self._trade_symbol_id = 30
 
     def tearDown(self) -> None:
-        # Remove data from key tables.
-        with self._writer.conn:
-            with self._writer.conn.cursor() as curs:
-                curs.execute(
-                    "DELETE FROM TradeSymbol WHERE id = %s",
-                    [self._get_test_number()],
-                )
-                curs.execute(
-                    "DELETE FROM Exchange WHERE id = %s",
-                    [self._get_test_number()],
-                )
-                curs.execute(
-                    "DELETE FROM Symbol WHERE id = %s", [self._get_test_number()]
-                )
         # Close connection.
         self._writer.close()
+        # Remove created database.
+        remove_database(self._dbname)
         super().tearDown()
 
     def test_ensure_symbol_exist1(self) -> None:
+        """
+        Test adding a new symbol to Symbol table.
+        """
         self._writer.ensure_symbol_exists(
-            symbol=self._get_test_string(), asset_class=vkdtyp.AssetClass.Futures
-        )
-        self._check_saved_data(
-            table="Symbol", test_id_field="code", test_id_type=str
-        )
+            symbol=self._get_test_string(),
+            asset_class=vkdtyp.AssetClass.Futures)
+        self._check_saved_data(table="Symbol")
 
     def test_ensure_trade_symbol_exist1(self) -> None:
-        self._prepare_test(
-            insert_symbol=True, insert_exchange=True, insert_trade_symbol=False
-        )
-        self._writer.ensure_trade_symbol_exists(
-            symbol_id=self._get_test_number(), exchange_id=self._get_test_number()
-        )
-        self._check_saved_data(
-            table="TradeSymbol", test_id_field="symbol_id", test_id_type=int
-        )
+        """
+        Test adding a new symbol to TradeSymbol table.
+        """
+        self._prepare_tables(insert_symbol=True,
+                             insert_exchange=True,
+                             insert_trade_symbol=False)
+        self._writer.ensure_trade_symbol_exists(symbol_id=self._symbol_id,
+                                                exchange_id=self._exchange_id)
+        self._check_saved_data(table="TradeSymbol")
 
     def test_insert_bulk_daily_data1(self) -> None:
-        self._prepare_test(
-            insert_symbol=True, insert_exchange=True, insert_trade_symbol=True
-        )
-        df = pd.DataFrame(
-            {
-                "trade_symbol_id": [self._get_test_number()] * 3,
-                "date": ["2021-01-01", "2021-01-02", "2021-01-03"],
-                "open": [10.0] * 3,
-                "high": [15] * 3,
-                "low": [9] * 3,
-                "close": [12.5] * 3,
-                "volume": [1000] * 3,
-            }
-        )
+        """
+        Test adding a dataframe to DailyData table.
+        """
+        self._prepare_tables(insert_symbol=True,
+                             insert_exchange=True,
+                             insert_trade_symbol=True)
+        df = pd.DataFrame({
+            "trade_symbol_id": [self._trade_symbol_id] * 3,
+            "date": ["2021-01-01", "2021-01-02", "2021-01-03"],
+            "open": [10.0] * 3,
+            "high": [15] * 3,
+            "low": [9] * 3,
+            "close": [12.5] * 3,
+            "volume": [1000] * 3,
+        })
         self._writer.insert_bulk_daily_data(df=df)
-        self._check_saved_data(
-            table="DailyData", test_id_field="trade_symbol_id", test_id_type=int
-        )
+        self._check_saved_data(table="DailyData")
 
     def test_insert_daily_data1(self) -> None:
-        self._prepare_test(
-            insert_symbol=True, insert_exchange=True, insert_trade_symbol=True
-        )
+        """
+        Test adding a one bar data to DailyData table.
+        """
+        self._prepare_tables(insert_symbol=True,
+                             insert_exchange=True,
+                             insert_trade_symbol=True)
         self._writer.insert_daily_data(
-            trade_symbol_id=self._get_test_number(),
+            trade_symbol_id=self._trade_symbol_id,
             date="2021-01-01",
             open_val=10.0,
             high_val=15,
@@ -92,40 +113,40 @@ class TestSqlWriterBackend1(hut.TestCase):
             close_val=12.5,
             volume_val=1000,
         )
-        self._check_saved_data(
-            table="DailyData", test_id_field="trade_symbol_id", test_id_type=int
-        )
+        self._check_saved_data(table="DailyData")
 
     def test_insert_bulk_minute_data1(self) -> None:
-        self._prepare_test(
-            insert_symbol=True, insert_exchange=True, insert_trade_symbol=True
-        )
-        df = pd.DataFrame(
-            {
-                "trade_symbol_id": [self._get_test_number()] * 3,
-                "datetime": [
-                    "2021-02-10T13:50:00Z",
-                    "2021-02-10T13:51:00Z",
-                    "2021-02-10T13:52:00Z",
-                ],
-                "open": [10.0] * 3,
-                "high": [15] * 3,
-                "low": [9] * 3,
-                "close": [12.5] * 3,
-                "volume": [1000] * 3,
-            }
-        )
+        """
+        Test adding a dataframe to MinuteData table.
+        """
+        self._prepare_tables(insert_symbol=True,
+                             insert_exchange=True,
+                             insert_trade_symbol=True)
+        df = pd.DataFrame({
+            "trade_symbol_id": [self._trade_symbol_id] * 3,
+            "datetime": [
+                "2021-02-10T13:50:00Z",
+                "2021-02-10T13:51:00Z",
+                "2021-02-10T13:52:00Z",
+            ],
+            "open": [10.0] * 3,
+            "high": [15] * 3,
+            "low": [9] * 3,
+            "close": [12.5] * 3,
+            "volume": [1000] * 3,
+        })
         self._writer.insert_bulk_minute_data(df=df)
-        self._check_saved_data(
-            table="MinuteData", test_id_field="trade_symbol_id", test_id_type=int
-        )
+        self._check_saved_data(table="MinuteData")
 
     def test_insert_minute_data1(self) -> None:
-        self._prepare_test(
-            insert_symbol=True, insert_exchange=True, insert_trade_symbol=True
-        )
+        """
+        Test adding a one bar data to MinuteData table.
+        """
+        self._prepare_tables(insert_symbol=True,
+                             insert_exchange=True,
+                             insert_trade_symbol=True)
         self._writer.insert_minute_data(
-            trade_symbol_id=self._get_test_number(),
+            trade_symbol_id=self._trade_symbol_id,
             date_time="2021-02-10T13:50:00Z",
             open_val=10.0,
             high_val=15,
@@ -133,25 +154,24 @@ class TestSqlWriterBackend1(hut.TestCase):
             close_val=12.5,
             volume_val=1000,
         )
-        self._check_saved_data(
-            table="MinuteData", test_id_field="trade_symbol_id", test_id_type=int
-        )
+        self._check_saved_data(table="MinuteData")
 
     def test_insert_tick_data1(self) -> None:
-        self._prepare_test(
-            insert_symbol=True, insert_exchange=True, insert_trade_symbol=True
-        )
+        """
+        Test adding a one tick data to TickData table.
+        """
+        self._prepare_tables(insert_symbol=True,
+                             insert_exchange=True,
+                             insert_trade_symbol=True)
         self._writer.insert_tick_data(
-            trade_symbol_id=self._get_test_number(),
+            trade_symbol_id=self._trade_symbol_id,
             date_time="2021-02-10T13:50:00Z",
             price_val=10.0,
             size_val=15,
         )
-        self._check_saved_data(
-            table="TickData", test_id_field="trade_symbol_id", test_id_type=int
-        )
+        self._check_saved_data(table="TickData")
 
-    def _prepare_test(
+    def _prepare_tables(
         self,
         insert_symbol: bool,
         insert_exchange: bool,
@@ -159,36 +179,41 @@ class TestSqlWriterBackend1(hut.TestCase):
     ) -> None:
         """
         Insert Symbol, Exchange and TradeSymbol entries to make test work.
+        
+        See `DB_SCHEMA_FILE` for more info. 
         """
         with self._writer.conn:
             with self._writer.conn.cursor() as curs:
+                # Fill Symbol table.
                 if insert_symbol:
                     curs.execute(
                         "INSERT INTO Symbol (id, code, asset_class) "
                         "VALUES (%s, %s, %s) ON CONFLICT DO NOTHING",
                         [
-                            self._get_test_number(),
-                            self._get_test_string(),
+                            self._symbol_id,
+                            "ZS1M",
                             "Futures",
                         ],
                     )
+                # Fill Exchange table.
                 if insert_exchange:
                     curs.execute(
                         "INSERT INTO Exchange (id, name) "
                         "VALUES (%s, %s) ON CONFLICT DO NOTHING",
                         [
-                            self._get_test_number(),
-                            self._get_test_string(),
+                            self._exchange_id,
+                            "CME",
                         ],
                     )
+                # Fill TradeSymbol table.
                 if insert_trade_symbol:
                     curs.execute(
                         "INSERT INTO TradeSymbol (id, exchange_id, symbol_id) "
                         "VALUES (%s, %s, %s) ON CONFLICT DO NOTHING",
                         [
-                            self._get_test_number(),
-                            self._get_test_number(),
-                            self._get_test_number(),
+                            self._trade_symbol_id,
+                            self._exchange_id,
+                            self._symbol_id,
                         ],
                     )
 
@@ -196,37 +221,85 @@ class TestSqlWriterBackend1(hut.TestCase):
         string: str = self._get_test_name().replace("/", "").replace(".", "")
         return string
 
-    def _get_test_number(self) -> int:
-        string = self._get_test_string()
-        return sum(bytes(string, "utf-8"))
-
-    def _check_saved_data(
-        self, table: str, test_id_field: str, test_id_type: type
-    ) -> None:
+    def _check_saved_data(self, table: str) -> None:
         """
         Check data saved in Postgresql by test.
 
         :param table: table name
-        :param test_id_field: field in table to identify from which test
-            data was generated
-        :param test_id_type: type of id field: str or int.
         """
-        condition = ""
-        if test_id_type == int:
-            condition = "WHERE %s = %s" % (test_id_field, self._get_test_number())
-        elif test_id_type == str:
-            condition = "WHERE %s = '%s'" % (
-                test_id_field,
-                self._get_test_string(),
-            )
-        query = "SELECT * FROM %s %s;" % (table, condition)
+        # Construct query to retrieve the data.
+        query = "SELECT * FROM %s;" % table
+        # Get data in pandas format based on query.
         res = pd.read_sql(query, self._writer.conn)
-        remove_query = "DELETE FROM %s %s" % (table, condition)
-        with self._writer.conn:
-            with self._writer.conn.cursor() as curs:
-                curs.execute(remove_query)
+        # Exclude changeable columns.
         columns_to_check = list(res.columns)
         for column_to_remove in ["id", "start_date"]:
             if column_to_remove in columns_to_check:
                 columns_to_check.remove(column_to_remove)
-        self.check_string(hut.convert_df_to_string(res[columns_to_check]))
+        # Convert dataframe to string.
+        txt = hut.convert_df_to_string(res[columns_to_check])
+        # Check the output against the golden.
+        self.check_string(txt)
+
+
+# TODO(plyq): Move it to common place, e.g. helpers.
+def create_database(dbname: str) -> None:
+    """
+    Create database in current environment.
+    """
+    # Initialize connection.
+    host = os.environ["POSTGRES_HOST"]
+    port = os.environ["POSTGRES_PORT"]
+    user = os.environ["POSTGRES_USER"]
+    password = os.environ["POSTGRES_PASSWORD"]
+    default_dbname = os.environ["POSTGRES_DB"]
+    connection = psycopg2.connect(
+        dbname=default_dbname,
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+    )
+    # Make DROP/CREATE DATABASE executable from transaction block.
+    connection.set_isolation_level(
+        psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    # Create a database from scratch.
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                sql.SQL("DROP DATABASE IF EXISTS {};").format(
+                    sql.Identifier(dbname)))
+            cursor.execute(
+                sql.SQL("CREATE DATABASE {};").format(sql.Identifier(dbname)))
+    # Close connection.
+    connection.close()
+
+
+# TODO(plyq): Move it to common place, e.g. helpers.
+def remove_database(dbname: str) -> None:
+    """
+    Remove database in current environment.
+    """
+    # Initialize connection.
+    host = os.environ["POSTGRES_HOST"]
+    port = os.environ["POSTGRES_PORT"]
+    user = os.environ["POSTGRES_USER"]
+    password = os.environ["POSTGRES_PASSWORD"]
+    default_dbname = os.environ["POSTGRES_DB"]
+    connection = psycopg2.connect(
+        dbname=default_dbname,
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+    )
+    # Make DROP DATABASE executable from transaction block.
+    connection.set_isolation_level(
+        psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    # Drop database.
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                sql.SQL("DROP DATABASE {};").format(sql.Identifier(dbname)))
+    # Close connection.
+    connection.close()
