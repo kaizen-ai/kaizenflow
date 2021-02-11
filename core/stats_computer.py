75f646b2a2107db60f23f80e32466dcf53af1a15
Daria Berezina <dberezina@particle.one>
2021-02-11 09:58:48 +0000

    add properties and linter

--- core/stats_computer.py
+++ core/stats_computer.py
@@ -4,8 +4,9 @@ Import as:
 import core.stats_computer as cstats
 """
 
+import collections
 import logging
-from typing import Iterable, List, Optional, Union
+from typing import Callable, Dict, Iterable, List, Optional, Union
 
 import pandas as pd
 
@@ -19,32 +20,17 @@ _LOG = logging.getLogger(__name__)
 class StatsComputer:
     """
     Allows to get particular piece of stats instead of the whole stats table.
-
-    Available methods for series stats:
-        - summarize_time_index_info
-        - compute_jensen_ratio
-        - compute_forecastability
-        - compute_moments
-        - compute_special_value_stats
-        - apply_normality_test
-        - apply_stationarity_test
-    Available methods for model stats:
-        - summarize_sharpe_ratio
-        - ttest_1samp
-        - compute_kratio
-        - compute_annualized_return_and_volatility
-        - compute_max_drawdown
-        - summarize_time_index_info
-        - calculate_hit_rate
-        - calculate_corr_to_underlying
-        - compute_bet_stats
-        - compute_avg_turnover_and_holding_period
-        - compute_jensen_ratio
-        - compute_forecastability
-        - compute_prediction_corr
-        - compute_moments
-        - compute_special_value_stats
     """
+
+    @property
+    def series_stats_methods(self) -> List[str]:
+        methods = self._map_name_to_method("series").keys()
+        return list(methods)
+
+    @property
+    def model_stats_methods(self) -> List[str]:
+        methods = self._map_name_to_method("model").keys()
+        return list(methods)
 
     # #########################################################################
     # Series stats methods
@@ -128,7 +114,7 @@ class StatsComputer:
             mode=mode,
         )
 
-    def apply_stationarity_test(
+    def apply_stationarity_tests(
         self,
         srs: pd.Series,
         oos_start: Optional[Union[str, pd.Timestamp]] = None,
@@ -136,7 +122,7 @@ class StatsComputer:
     ) -> pd.Series:
         return self._calculate_series_stats(
             srs=srs,
-            stats_names=["apply_stationarity_test"],
+            stats_names=["apply_stationarity_tests"],
             oos_start=oos_start,
             mode=mode,
         )
@@ -147,48 +133,15 @@ class StatsComputer:
         oos_start: Optional[Union[str, pd.Timestamp]] = None,
         mode: str = "ins",
     ) -> pd.Series:
+        """
+        Calculate all available stats for a series as in
+        `dataframe_modeler.calculate_stats`.
+        """
         return self._calculate_series_stats(
             srs=srs,
             oos_start=oos_start,
             mode=mode,
         )
-    
-    @staticmethod
-    def _apply_stationarity_test(srs: pd.Series) -> pd.Series:
-        return pd.concat(
-            [
-                cstati.apply_adf_test(srs, prefix="adf_"),
-                cstati.apply_kpss_test(srs, prefix="kpss_"),
-            ]
-        )
-
-    def _calculate_series_stats(
-        self,
-        srs: pd.Series,
-        stats_names: Optional[List[str]] = None,
-        oos_start: Optional[Union[str, pd.Timestamp]] = None,
-        mode: str = "ins",
-    ) -> pd.Series:
-        dbg.dassert_isinstance(srs, pd.Series)
-        srs = self._get_srs(srs, mode=mode, oos_start=oos_start)
-        # Map `stats_names` to corresponding functions.
-        stats_names_dict = {
-            "summarize_time_index_info": cstati.summarize_time_index_info,
-            "compute_jensen_ratio": cstati.compute_jensen_ratio,
-            "compute_forecastability": cstati.compute_forecastability,
-            "compute_moments": cstati.compute_moments,
-            "compute_special_value_stats": cstati.compute_special_value_stats,
-            "apply_normality_test": lambda x: cstati.apply_normality_test(
-                x, prefix="normality_"
-            ),
-            "apply_stationarity_test": self._apply_stationarity_test,
-        }
-        stats_names = self._get_stats_names(stats_names_dict.keys(), stats_names)
-        stats_vals = []
-        for stat_name in stats_names:
-            stats_vals.append(stats_names_dict[stat_name](srs))
-        return pd.concat(stats_vals)
-    
 
     # #########################################################################
     # Model stats methods
@@ -259,19 +212,6 @@ class StatsComputer:
             mode=mode,
         )
 
-    def summarize_time_index_info(
-        self,
-        srs: pd.Series,
-        oos_start: Optional[Union[str, pd.Timestamp]] = None,
-        mode: str = "ins",
-    ) -> pd.Series:
-        return self._calculate_model_stats(
-            pnl=srs,
-            stats_names=["summarize_time_index_info"],
-            oos_start=oos_start,
-            mode=mode,
-        )
-
     def calculate_hit_rate(
         self,
         srs: pd.Series,
@@ -328,19 +268,6 @@ class StatsComputer:
             mode=mode,
         )
 
-    def compute_jensen_ratio(
-        self,
-        srs: pd.Series,
-        oos_start: Optional[Union[str, pd.Timestamp]] = None,
-        mode: str = "ins",
-    ) -> pd.Series:
-        return self._calculate_model_stats(
-            pnl=srs,
-            stats_names=["compute_jensen_ratio"],
-            oos_start=oos_start,
-            mode=mode,
-        )
-
     def compute_prediction_corr(
         self,
         returns: pd.Series,
@@ -356,32 +283,6 @@ class StatsComputer:
             mode=mode,
         )
 
-    def compute_moments(
-        self,
-        srs: pd.Series,
-        oos_start: Optional[Union[str, pd.Timestamp]] = None,
-        mode: str = "ins",
-    ) -> pd.Series:
-        return self._calculate_model_stats(
-            pnl=srs,
-            stats_names=["compute_moments"],
-            oos_start=oos_start,
-            mode=mode,
-        )
-
-    def compute_special_value_stats(
-        self,
-        srs: pd.Series,
-        oos_start: Optional[Union[str, pd.Timestamp]] = None,
-        mode: str = "ins",
-    ) -> pd.Series:
-        return self._calculate_model_stats(
-            pnl=srs,
-            stats_names=["compute_special_value_stats"],
-            oos_start=oos_start,
-            mode=mode,
-        )
-
     def calculate_model_stats(
         self,
         returns: Optional[pd.Series] = None,
@@ -390,13 +291,135 @@ class StatsComputer:
         oos_start: Optional[Union[str, pd.Timestamp]] = None,
         mode: str = "ins",
     ) -> pd.Series:
-        return self._calculate_series_stats(
+        """
+        Calculate all available stats for a model as in
+        `model_evaluator.calculate_stats`.
+        """
+        return self._calculate_model_stats(
             returns=returns,
             positions=positions,
             pnl=pnl,
             oos_start=oos_start,
             mode=mode,
         )
+
+    # #########################################################################
+    # Helpers
+    # #########################################################################
+
+    @staticmethod
+    def _apply_stationarity_tests(srs: pd.Series) -> pd.Series:
+        return pd.concat(
+            [
+                cstati.apply_adf_test(srs, prefix="adf_"),
+                cstati.apply_kpss_test(srs, prefix="kpss_"),
+            ]
+        )
+
+    def _map_name_to_method(self, stats_type: str) -> Dict[str, Callable]:
+        """
+        Map `stats_names` to corresponding methods.
+
+        :param stats_type: "series" or "model"
+        :return: dict with stats names as keys and corresponding methods as values
+        """
+        if stats_type == "series":
+            stats_names_dict = collections.OrderedDict(
+                {
+                    "summarize_time_index_info": cstati.summarize_time_index_info,
+                    "compute_jensen_ratio": cstati.compute_jensen_ratio,
+                    "compute_forecastability": cstati.compute_forecastability,
+                    "compute_moments": cstati.compute_moments,
+                    "compute_special_value_stats": cstati.compute_special_value_stats,
+                    "apply_normality_test": lambda x: cstati.apply_normality_test(
+                        x, prefix="normality_"
+                    ),
+                    "apply_stationarity_tests": self._apply_stationarity_tests,
+                }
+            )
+        if stats_type == "model":
+            stats_names_dict = collections.OrderedDict(
+                {
+                    "summarize_sharpe_ratio": cstati.summarize_sharpe_ratio,
+                    "ttest_1samp": cstati.ttest_1samp,
+                    "compute_kratio": lambda x: pd.Series(
+                        cfinan.compute_kratio(x), index=["kratio"]
+                    ),
+                    "compute_annualized_return_and_volatility": (
+                        cstati.compute_annualized_return_and_volatility
+                    ),
+                    "compute_max_drawdown": cstati.compute_max_drawdown,
+                    "summarize_time_index_info": cstati.summarize_time_index_info,
+                    "calculate_hit_rate": cstati.calculate_hit_rate,
+                    "calculate_corr_to_underlying": lambda x: pd.Series(
+                        x[0].corr(x[1]), index=["corr_to_underlying"]
+                    ),
+                    "compute_bet_stats": lambda x: cstati.compute_bet_stats(
+                        x[0], x[1][x[0].index]
+                    ),
+                    "compute_avg_turnover_and_holding_period": (
+                        cstati.compute_avg_turnover_and_holding_period
+                    ),
+                    "compute_jensen_ratio": cstati.compute_jensen_ratio,
+                    "compute_forecastability": cstati.compute_forecastability,
+                    "compute_prediction_corr": lambda x: pd.Series(
+                        x[0].corr(x[1]), index=["prediction_corr"]
+                    ),
+                    "compute_moments": cstati.compute_moments,
+                    "compute_special_value_stats": cstati.compute_special_value_stats,
+                }
+            )
+        return stats_names_dict
+
+    @staticmethod
+    def _get_srs(
+        srs: pd.Series,
+        oos_start: Optional[Union[str, pd.Timestamp]] = None,
+        mode: str = "ins",
+    ) -> pd.Series:
+        if mode == "ins":
+            return srs[:oos_start].copy()
+        if mode == "oos":
+            return srs[oos_start:].copy()
+        if mode == "all_available":
+            return srs.copy()
+        raise ValueError(f"Unrecognized mode `{mode}`")
+
+    @staticmethod
+    def _validate_stats_names(
+        stats_names_available: Iterable[str],
+        stats_names_requested: Optional[List[str]] = None,
+    ) -> pd.Series:
+        """
+        Validate `stats_names` and sort according to the whole stats table
+        logic.
+        """
+        stats_names = stats_names_requested or stats_names_available
+        stats_names_diff = set(stats_names) - set(stats_names_available)
+        if stats_names_diff:
+            raise ValueError(f"Unsupported stats names: {stats_names_diff}")
+        stats_names = [
+            name for name in stats_names_available if name in stats_names
+        ]
+        return stats_names
+
+    def _calculate_series_stats(
+        self,
+        srs: pd.Series,
+        stats_names: Optional[List[str]] = None,
+        oos_start: Optional[Union[str, pd.Timestamp]] = None,
+        mode: str = "ins",
+    ) -> pd.Series:
+        dbg.dassert_isinstance(srs, pd.Series)
+        srs = self._get_srs(srs, mode=mode, oos_start=oos_start)
+        stats_names_dict = self._map_name_to_method("series")
+        stats_names = self._validate_stats_names(
+            stats_names_dict.keys(), stats_names
+        )
+        stats_vals = []
+        for stat_name in stats_names:
+            stats_vals.append(stats_names_dict[stat_name](srs))
+        return pd.concat(stats_vals)
 
     def _calculate_model_stats(
         self,
@@ -407,48 +430,23 @@ class StatsComputer:
         oos_start: Optional[Union[str, pd.Timestamp]] = None,
         mode: str = "ins",
     ) -> pd.Series:
+        inputs = [returns, positions, pnl]
         dbg.dassert(
-            not pd.isna([pnl, positions, returns]).all(),
-            "At least one series should be not `None`.",
+            not pd.isna(inputs).all(), "At least one series should be not `None`."
         )
-        freqs = {
-            srs.index.freq for srs in [pnl, positions, returns] if srs is not None
-        }
+        freqs = {srs.index.freq for srs in inputs if srs is not None}
         dbg.dassert_eq(len(freqs), 1, "Series have different frequencies.")
-        returns = self._get_srs(returns, mode=mode, oos_start=oos_start)
-        positions = self._get_srs(positions, mode=mode, oos_start=oos_start)
-        pnl = self._get_srs(pnl, mode=mode, oos_start=oos_start)
-        # Map `stats_names` to corresponding functions.
-        stats_names_dict = {
-            "summarize_sharpe_ratio": cstati.summarize_sharpe_ratio,
-            "ttest_1samp": cstati.ttest_1samp,
-            "compute_kratio": lambda x: pd.Series(
-                cfinan.compute_kratio(x), index=["kratio"]
-            ),
-            "compute_annualized_return_and_volatility": (
-                cstati.compute_annualized_return_and_volatility
-            ),
-            "compute_max_drawdown": cstati.compute_max_drawdown,
-            "summarize_time_index_info": cstati.summarize_time_index_info,
-            "calculate_hit_rate": cstati.calculate_hit_rate,
-            "calculate_corr_to_underlying": lambda x: pd.Series(
-                x.corr(returns), index=["corr_to_underlying"]
-            ),
-            "compute_bet_stats": lambda x: cstati.compute_bet_stats(
-                x, returns[x.index]
-            ),
-            "compute_avg_turnover_and_holding_period": (
-                cstati.compute_avg_turnover_and_holding_period
-            ),
-            "compute_jensen_ratio": cstati.compute_jensen_ratio,
-            "compute_forecastability": cstati.compute_forecastability,
-            "compute_prediction_corr": lambda x: pd.Series(
-                x.corr(returns), index=["prediction_corr"]
-            ),
-            "compute_moments": cstati.compute_moments,
-            "compute_special_value_stats": cstati.compute_special_value_stats,
-        }
-        stats_names = self._get_stats_names(stats_names_dict.keys(), stats_names)
+        inputs = [
+            self._get_srs(srs, mode=mode, oos_start=oos_start)
+            if srs is not None
+            else None
+            for srs in inputs
+        ]
+        returns, positions, pnl = inputs
+        stats_names_dict = self._map_name_to_method("model")
+        stats_names = self._validate_stats_names(
+            stats_names_dict.keys(), stats_names
+        )
         stats_vals = []
         positions_stats_names = [
             "compute_bet_stats",
@@ -461,45 +459,13 @@ class StatsComputer:
             "calculate_corr_to_underlying",
         ]
         for stat_name in stats_names:
-            if (returns is not None) or (stat_name not in returns_stats_names):
-                srs = positions if stat_name in positions_stats_names else pnl
-                if srs is not None:
+            srs = positions if stat_name in positions_stats_names else pnl
+            if srs is not None:
+                if stat_name in returns_stats_names:
+                    if returns is not None:
+                        stats_vals.append(
+                            stats_names_dict[stat_name]([srs, returns])
+                        )
+                else:
                     stats_vals.append(stats_names_dict[stat_name](srs))
         return pd.concat(stats_vals)
-
-    # #########################################################################
-    # Helpers
-    # #########################################################################
-
-    @staticmethod
-    def _get_srs(
-        srs: pd.Series,
-        oos_start: Optional[Union[str, pd.Timestamp]] = None,
-        mode: str = "ins",
-    ) -> pd.Series:
-        if srs is None:
-            return srs
-        if mode == "ins":
-            return srs[:oos_start].copy()
-        if mode == "oos":
-            return srs[oos_start:].copy()
-        if mode == "all_available":
-            return srs.copy()
-        raise ValueError(f"Unrecognized mode `{mode}`")
-
-    @staticmethod
-    def _get_stats_names(
-        stats_names_dict_keys: Iterable[str],
-        stats_names: Optional[List[str]] = None,
-    ) -> pd.Series:
-        """
-        Check `stats_names` and sort according to the whole stats table logic.
-        """
-        stats_names = stats_names or stats_names_dict_keys
-        stats_names_diff = set(stats_names) - set(stats_names_dict_keys)
-        if stats_names_diff:
-            raise ValueError(f"Unsupported stats names: {stats_names_diff}")
-        stats_names = [
-            name for name in stats_names_dict_keys if name in stats_names
-        ]
-        return stats_names
