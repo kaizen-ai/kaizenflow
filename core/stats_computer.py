1aba6264bf548ddb0e5ef4ca75a83b835620195e
Daria Berezina <dberezina@particle.one>
2021-02-26 07:41:27 +0000

    simplified class with mixins

--- core/stats_computer.py
+++ core/stats_computer.py
@@ -4,9 +4,8 @@ Import as:
 import core.stats_computer as cstats
 """
 
-import collections
 import logging
-from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple
+from typing import Optional
 
 import pandas as pd
 
@@ -21,11 +20,6 @@ class StatsComputer:
     """
     Allows to get particular piece of stats instead of the whole stats table.
     """
-
-    @property
-    def get_stats_methods(self) -> List[str]:
-        methods = self._map_name_to_method().keys()
-        return list(methods) + ["calculate_stats"]
 
     @staticmethod
     def summarize_time_index_info(srs: pd.Series) -> pd.Series:
@@ -47,63 +41,6 @@ class StatsComputer:
     def compute_special_value_stats(srs: pd.Series) -> pd.Series:
         return cstati.compute_special_value_stats(srs)
 
-    def calculate_stats(self, *args: Any, **kwargs: Any) -> pd.Series:
-        """
-        Calculate all available stats as in dataframe_modeler/model_evaluator.
-        """
-        return self._calculate_stats(stats_names=None, *args, **kwargs)
-
-    @property
-    def _map_name_to_method(self) -> Dict[str, Callable]:
-        """
-        Map `stats_names` to corresponding methods.
-        """
-        stats_methods_dict = collections.OrderedDict(
-            {
-                "summarize_time_index_info": self.summarize_time_index_info,
-                "compute_jensen_ratio": self.compute_jensen_ratio,
-                "compute_forecastability": self.compute_forecastability,
-                "compute_moments": self.compute_moments,
-                "compute_special_value_stats": self.compute_special_value_stats,
-            }
-        )
-        return stats_methods_dict
-
-    @staticmethod
-    def _validate_stats_names(
-        stats_names_available: Iterable[str],
-        stats_names_requested: Optional[Iterable[str]] = None,
-    ) -> Iterable[str]:
-        stats_names = stats_names_requested or stats_names_available
-        stats_names_diff = set(stats_names) - set(stats_names_available)
-        if stats_names_diff:
-            raise ValueError(f"Unsupported stats names: {stats_names_diff}")
-        return stats_names
-
-    def _calculate_stats(
-        self,
-        srs: pd.Series,
-        stats_names: Optional[List[str]] = None,
-    ) -> pd.Series:
-        """
-        Calculate stats specified in `stats_names`, if `None` - all available.
-        """
-        dbg.dassert_isinstance(srs, pd.Series)
-        stats_methods_dict = self._map_name_to_method
-        stats_names = self._validate_stats_names(
-            stats_methods_dict.keys(), stats_names
-        )
-        stats_vals = []
-        for stat_name in stats_names:
-            stats_vals.append(stats_methods_dict[stat_name](srs))
-        return pd.concat(stats_vals)
-
-
-class SeriesStatsComputer(StatsComputer):
-    """
-    Class for series stats only.
-    """
-
     @staticmethod
     def apply_normality_test(srs: pd.Series) -> pd.Series:
         return cstati.apply_normality_test(srs, prefix="normality_")
@@ -116,25 +53,6 @@ class SeriesStatsComputer(StatsComputer):
                 cstati.apply_kpss_test(srs, prefix="kpss_"),
             ]
         )
-
-    @property
-    def _map_name_to_method(self) -> Dict[str, Callable]:
-        stats_methods_dict = super()._map_name_to_method
-        stats_methods_dict.update(
-            collections.OrderedDict(
-                {
-                    "apply_normality_test": self.apply_normality_test,
-                    "apply_stationarity_tests": self.apply_stationarity_tests,
-                }
-            )
-        )
-        return stats_methods_dict
-
-
-class ModelStatsComputer(StatsComputer):
-    """
-    Class for model stats only.
-    """
 
     @staticmethod
     def summarize_sharpe_ratio(srs: pd.Series) -> pd.Series:
@@ -161,20 +79,44 @@ class ModelStatsComputer(StatsComputer):
         return cstati.calculate_hit_rate(srs)
 
     @staticmethod
+    def compute_avg_turnover_and_holding_period(srs: pd.Series) -> pd.Series:
+        return cstati.compute_avg_turnover_and_holding_period(srs)
+
+
+class PnlReturnsMixin:
+    """
+    Add methods with pnl and returns inputs.
+    """
+
+    @staticmethod
     def calculate_corr_to_underlying(
         pnl: pd.Series, returns: pd.Series
     ) -> pd.Series:
         return pd.Series(pnl.corr(returns), index=["corr_to_underlying"])
 
+    def _calculate_pnl_returns_stats(
+        self,
+        pnl: pd.Series,
+        returns: pd.Series,
+    ) -> pd.Series:
+        """
+        Calculate stats for methods with pnl and returns inputs.
+        """
+        if pnl is None or returns is None:
+            return None
+        dbg.dassert_isinstance(pnl, pd.Series)
+        dbg.dassert_isinstance(returns, pd.Series)
+        return self.calculate_corr_to_underlying(pnl, returns)
+
+
+class PositionsReturnsMixin:
+    """
+    Add methods with positions and returns inputs.
+    """
+
     @staticmethod
     def compute_bet_stats(positions: pd.Series, returns: pd.Series) -> pd.Series:
         return cstati.compute_bet_stats(positions, returns[positions.index])
-
-    @staticmethod
-    def compute_avg_turnover_and_holding_period(
-        srs: pd.Series,
-    ) -> pd.Series:
-        return cstati.compute_avg_turnover_and_holding_period(srs)
 
     @staticmethod
     def compute_prediction_corr(
@@ -182,87 +124,64 @@ class ModelStatsComputer(StatsComputer):
     ) -> pd.Series:
         return pd.Series(positions.corr(returns), index=["prediction_corr"])
 
-    @property
-    def _map_name_to_method(self) -> Dict[str, Callable]:
-        stats_methods_dict = collections.OrderedDict(
-            {
-                "summarize_sharpe_ratio": self.summarize_sharpe_ratio,
-                "ttest_1samp": self.ttest_1samp,
-                "compute_kratio": self.compute_kratio,
-                "compute_annualized_return_and_volatility": self.compute_annualized_return_and_volatility,
-                "compute_max_drawdown": self.compute_max_drawdown,
-                "summarize_time_index_info": self.summarize_time_index_info,
-                "calculate_hit_rate": self.calculate_hit_rate,
-                "calculate_corr_to_underlying": self.calculate_corr_to_underlying,
-                "compute_bet_stats": self.compute_bet_stats,
-                "compute_avg_turnover_and_holding_period": self.compute_avg_turnover_and_holding_period,
-                "compute_jensen_ratio": self.compute_jensen_ratio,
-                "compute_forecastability": self.compute_forecastability,
-                "compute_prediction_corr": self.compute_prediction_corr,
-                "compute_moments": self.compute_moments,
-                "compute_special_value_stats": self.compute_special_value_stats,
-            }
-        )
-        return stats_methods_dict
-
-    @property
-    def _map_name_to_params(self) -> Dict[str, Tuple[str]]:
-        stats_params_dict = collections.OrderedDict(
-            {
-                "summarize_sharpe_ratio": ("pnl",),
-                "ttest_1samp": ("pnl",),
-                "compute_kratio": ("pnl",),
-                "compute_annualized_return_and_volatility": ("pnl",),
-                "compute_max_drawdown": ("pnl",),
-                "summarize_time_index_info": ("pnl",),
-                "calculate_hit_rate": ("pnl",),
-                "calculate_corr_to_underlying": ("pnl", "returns"),
-                "compute_bet_stats": ("positions", "returns"),
-                "compute_avg_turnover_and_holding_period": ("positions",),
-                "compute_jensen_ratio": ("pnl",),
-                "compute_forecastability": ("pnl",),
-                "compute_prediction_corr": ("positions", "returns"),
-                "compute_moments": ("pnl",),
-                "compute_special_value_stats": ("pnl",),
-            }
-        )
-        return stats_params_dict
-
-    @staticmethod
-    def _run_func(
-        func: Callable,
-        param_names: Tuple[str],
-        pnl: Optional[pd.Series] = None,
-        positions: Optional[pd.Series] = None,
-        returns: Optional[pd.Series] = None,
+    def _calculate_positions_returns_stats(
+        self,
+        positions: pd.Series,
+        returns: pd.Series,
     ) -> pd.Series:
         """
-        Apply a function to corresponding input series.
+        Calculate stats for methods with positions and returns inputs.
         """
-        param_names_diff = set(param_names) - set(["pnl", "positions", "returns"])
-        if param_names_diff:
-            raise ValueError(f"Unsupported param names: {param_names_diff}")
-        args = []
-        for param_name in param_names:
-            args.append(eval(param_name))
-        if pd.isna(args).any():
+        if positions is None or returns is None:
             return None
-        return func(*args)
+        dbg.dassert_isinstance(positions, pd.Series)
+        dbg.dassert_isinstance(returns, pd.Series)
+        stats_vals = pd.concat(
+            [
+                self.compute_bet_stats(positions, returns),
+                self.compute_prediction_corr(positions, returns),
+            ]
+        )
+        return stats_vals
 
-    def _calculate_stats(
+
+class SeriesStatsComputer(StatsComputer):
+    """
+    Class for series stats only.
+    """
+
+    def calculate_stats(self, srs: pd.Series) -> pd.Series:
+        """
+        Calculate all available stats as in dataframe_modeler.
+        """
+        dbg.dassert_isinstance(srs, pd.Series)
+        stats_vals = pd.concat(
+            [
+                self.summarize_time_index_info(srs),
+                self.compute_jensen_ratio(srs),
+                self.compute_forecastability(srs),
+                self.compute_moments(srs),
+                self.compute_special_value_stats(srs),
+                self.apply_normality_test(srs),
+                self.apply_stationarity_tests(srs),
+            ]
+        )
+        return stats_vals
+
+
+class ModelStatsComputer(StatsComputer, PnlReturnsMixin, PositionsReturnsMixin):
+    """
+    Class for model stats only.
+    """
+
+    def calculate_stats(
         self,
         pnl: Optional[pd.Series] = None,
         positions: Optional[pd.Series] = None,
         returns: Optional[pd.Series] = None,
-        stats_names: Optional[List[str]] = None,
-        stats_params_dict: Optional[Dict[str, Tuple[str]]] = None,
     ) -> pd.Series:
         """
-        :param stats_names: list of stats to calculate, if `None`, return all
-            available stats
-        :param stats_params_dict: dict `stats_name`: `param` specifying what
-            stats apply to what input series, if not specified, use default
-            specification
+        Calculate all available stats as in model_evaluator.
         """
         dbg.dassert(
             not pd.isna([pnl, positions, returns]).all(),
@@ -272,25 +191,35 @@ class ModelStatsComputer(StatsComputer):
             srs.index.freq for srs in [pnl, positions, returns] if srs is not None
         }
         dbg.dassert_eq(len(freqs), 1, "Series have different frequencies.")
-        stats_methods_dict = self._map_name_to_method
-        stats_names = self._validate_stats_names(
-            stats_methods_dict.keys(), stats_names
-        )
-        stats_params_dict = stats_params_dict or {}
-        if set(stats_params_dict.keys()) - set(stats_names):
-            _LOG.warning("Stats with specified parameters are not requested.")
-        stats_params_dict = collections.OrderedDict(
-            self._map_name_to_params, **stats_params_dict
-        )
-        stats_vals = []
-        for stat_name in stats_names:
-            stats_vals.append(
-                self._run_func(
-                    stats_methods_dict[stat_name],
-                    stats_params_dict[stat_name],
-                    pnl,
-                    positions,
-                    returns,
-                )
+        stats_vals = pd.Series()
+        if pnl is not None:
+            stats_vals = pd.concat(
+                [
+                    self.summarize_sharpe_ratio(pnl),
+                    self.ttest_1samp(pnl),
+                    self.compute_kratio(pnl),
+                    self.compute_annualized_return_and_volatility(pnl),
+                    self.compute_max_drawdown(pnl),
+                    self.summarize_time_index_info(pnl),
+                    self.calculate_hit_rate(pnl),
+                    self.compute_jensen_ratio(pnl),
+                    self.compute_forecastability(pnl),
+                    self.compute_moments(pnl),
+                    self.compute_special_value_stats(pnl),
+                ]
             )
-        return pd.concat(stats_vals)
+        if positions is not None:
+            stats_vals = pd.concat(
+                [
+                    stats_vals,
+                    self.compute_avg_turnover_and_holding_period(positions),
+                ]
+            )
+        stats_vals = pd.concat(
+            [
+                stats_vals,
+                self._calculate_pnl_returns_stats(pnl, returns),
+                self._calculate_positions_returns_stats(positions, returns),
+            ]
+        )
+        return stats_vals
--- core/test/test_stats_computer.py
+++ core/test/test_stats_computer.py
@@ -13,7 +13,7 @@ class TestStatsComputer(hut.TestCase):
     def test_all_stats(self) -> None:
         srs = _get_srs()["returns"]
         stats_comp = cstats.StatsComputer()
-        stats = stats_comp.calculate_stats(srs)
+        stats = stats_comp.summarize_time_index_info(srs)
         str_output = hut.convert_df_to_string(stats, index=True)
         self.check_string(str_output)
 
@@ -50,28 +50,6 @@ class TestModelStatsComputer(hut.TestCase):
         stats = stats_comp.calculate_stats(positions=srs["positions"])
         str_output = hut.convert_df_to_string(stats, index=True)
         self.check_string(str_output)
-
-    def test_specified_params(self) -> None:
-        srs = _get_srs()
-        stats_comp = cstats.ModelStatsComputer()
-        stats = stats_comp._calculate_stats(
-            positions=srs["positions"],
-            stats_names=["compute_moments", "compute_special_value_stats"],
-            stats_params_dict={"compute_moments": ["positions"]},
-        )
-        str_output = hut.convert_df_to_string(stats, index=True)
-        self.check_string(str_output)
-
-    def test_stand_alone_methods(self) -> None:
-        srs = _get_srs()
-        stats_comp = cstats.ModelStatsComputer()
-        actual = stats_comp._calculate_stats(
-            positions=srs["positions"],
-            stats_names=["ttest_1samp"],
-            stats_params_dict={"ttest_1samp": ["positions"]},
-        )
-        expected = stats_comp.ttest_1samp(srs["positions"])
-        pd.testing.assert_series_equal(actual, expected)
 
 
 def _get_srs() -> pd.DataFrame:
