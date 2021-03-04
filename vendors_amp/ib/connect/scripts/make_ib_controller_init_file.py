984863049f4f24d49bbae1f1bbfb94c5be2d7150
Artem <amashkovskii@particle.one>
2021-03-05 04:16:09 +0500

    PartTask7672: added connection to ib

---
+++ vendors_amp/ib/connect/scripts/make_ib_controller_init_file.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+"""
+Create IB controller config.
+
+Based on:
+https://github.com/ib-controller/ib-controller/blob/master/resources/IBController.ini
+
+Usage:
+    1. Create config with IB user/password:
+    > make_ib_controller_init_file.py --user username --password password
+"""
+import logging
+import argparse
+import helpers.dbg as dbg
+import helpers.parser as hparse
+import helpers.io_ as hio
+from typing import Dict, Any
+
+DEFAULT_CONFIG = dict(
+    LogToConsole="yes",
+    FIX="no",
+    IbLoginId="edemo",
+    IbPassword="demouser",
+    PasswordEncrypted="no",
+    FIXLoginId=None,
+    FIXPassword=None,
+    FIXPasswordEncrypted="yes",
+    TradingMode="paper",
+    IbDir=None,
+    StoreSettingsOnServer="no",
+    MinimizeMainWindow="no",
+    ExistingSessionDetectedAction="primary",
+    AcceptIncomingConnectionAction="accept",
+    ShowAllTrades="no",
+    ForceTwsApiPort=4001,
+    ReadOnlyLogin="no",
+    AcceptNonBrokerageAccountWarning="yes",
+    IbAutoClosedown="no",
+    ClosedownAt=None,
+    AllowBlindTrading="no",
+    DismissPasswordExpiryWarning="yes",
+    DismissNSEComplianceNotice="yes",
+    SaveTwsSettingsAt=None,
+    IbControllerPort=7462,
+    IbControlFrom=None,
+    IbBindAddress=None,
+    CommandPrompt=None,
+    SuppressInfoMessages="yes",
+    LogComponents="yes",
+)
+PATH_TO_CONFIG = "/root/IBController/IBController.ini"
+_LOG = logging.getLogger(__name__)
+
+
+def _save_config_to_file(config: Dict[str, Any]) -> None:
+    # Get text to save.
+    string = "\n".join(["%s=%s" % (param, value) for param, value in config.items() if value is not None])
+    # Save text to file.
+    hio.create_enclosing_dir(PATH_TO_CONFIG, incremental=True)
+    hio.to_file(PATH_TO_CONFIG, string)
+
+
+def _parse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--user",
+        type=str,
+        help="IB user",
+        action="store",
+    )
+    parser.add_argument(
+        "--password",
+        type=str,
+        help="IB password",
+        action="store",
+    )
+    hparse.add_verbosity_arg(parser)
+    return parser
+
+
+def _main(parser: argparse.ArgumentParser) -> None:
+    args = parser.parse_args()
+    dbg.init_logger(verbosity=args.log_level, use_exec_path=True)
+    dbg.shutup_chatty_modules()
+    # Set up config to save.
+    params = DEFAULT_CONFIG.copy()
+    if args.user is not None:
+        params["user"] = args.user
+    if args.password is not None:
+        params["password"] = args.password
+    # Save to file.
+    _save_config_to_file(params)
+
+
+if __name__ == "__main__":
+    _main(_parse())
+
---
+++ vendors_amp/ib/connect/scripts/make_jts_init_file.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+"""
+Create jts config file.
+
+Based on:
+https://github.com/mvberg/ib-gateway-docker/blob/master/ib/jts.ini
+
+Usage:
+    1. Create default config file:
+    > make_jts_init_file.py
+    
+    2. Create config on server (use your local IP instead of 33.3.33.3):
+    > make_jts_init_file.py --trusted_ips 127.0.0.1,33.3.33.3
+"""
+import logging
+import argparse
+import helpers.dbg as dbg
+import helpers.parser as hparse
+import helpers.io_ as hio
+from typing import Dict, Any
+
+PATH_TO_CONFIG = "/root/Jts/jts.ini"
+_LOG = logging.getLogger(__name__)
+DEFAULT_CONFIG = {
+    "IBGateway": {
+        "WriteDebug": "false",
+        "TrustedIPs": "127.0.0.1",
+        "MainWindow.Height": 550,
+        "RemoteHostOrderRouting": "hdc1.ibllc.com",
+        "RemotePortOrderRouting": 4000,
+        "LocalServerPort": 4000,
+        "ApiOnly": "true",
+        "MainWindow.Width": 700,
+    },
+    "Logon": {
+        "useRemoteSettings": "false",
+        "TimeZone": "UTC",
+        "tradingMode": "p",
+        "colorPalletName": "dark",
+        "Steps": 6,
+        "Locale": "en",
+        "SupportsSSL": "gdc1.ibllc.com:4000,true,20210304,false",
+        "UseSSL": "true",
+        "os_titlebar": "false",
+        "s3store": "true",
+    },
+    "ns" : {
+        "darykq": 1,
+    },
+    "Communication": {
+        "SettingsDir": "/root/Jts",
+        "Peer": "gdc1.ibllc.com:4001",
+        "Region": "us",
+    },
+}
+
+
+def _save_config_to_file(config: Dict[str, Dict[str, Any]]) -> None:
+    """
+    Save config from dictionary.
+
+    Format:
+        [Label_1]
+        Var=Value
+        [Label_2]
+        ...
+    """
+    # Get text to save.
+    string = ""
+    for section in config:
+        # I.e. "[Label]".
+        string += "[%s]\n" % section
+        for item, value in config[section].items():
+            string += "%s=%s\n" % (item, value)
+    # Save text to file.
+    hio.create_enclosing_dir(PATH_TO_CONFIG, incremental=True)
+    hio.to_file(PATH_TO_CONFIG, string)
+
+
+def _parse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--trusted_ips",
+        type=str,
+        help="Trusted IP-s split by comma",
+        action="store",
+    )
+    hparse.add_verbosity_arg(parser)
+    return parser
+
+
+def _main(parser: argparse.ArgumentParser) -> None:
+    args = parser.parse_args()
+    dbg.init_logger(verbosity=args.log_level, use_exec_path=True)
+    dbg.shutup_chatty_modules()
+    # Set up config to save.
+    params = DEFAULT_CONFIG.copy()
+    # Add trusted IP-s.
+    if args.trusted_ips is not None:
+        params["IBGateway"]["TrustedIPs"] = args.trusted_ips
+    # Save to file.
+    _save_config_to_file(params)
+
+
+if __name__ == "__main__":
+    _main(_parse())
+
