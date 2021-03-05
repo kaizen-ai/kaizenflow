99f83294eda82ad626a864bd9c8bf4f15ea9286d
k0nsta <3219704+KostaNau@users.noreply.github.com>
2021-03-05 19:47:52 +0300

    WIP: finish research and method for region and exchange layers

--- vendors_amp/ib/metadata/extract/ib_crawler/items.py
+++ vendors_amp/ib/metadata/extract/ib_crawler/items.py
@@ -1,18 +1,17 @@
+from urllib.parse import SplitResult
 import scrapy
 
 
 class ExchangeItem(scrapy.Item):
+    region = scrapy.Field()
     country = scrapy.Field()
     market = scrapy.Field()
     link = scrapy.Field()
-    products = scrapy.Field()
-    hours = scrapy.Field()
-
-
-class ProductItem(scrapy.Item):
-    category = scrapy.Field()
-    title = scrapy.Field()
+    products = scrapy.Field()   # TODO: implement postprocessor for list -> ",".join method
+    product = scrapy.Field()
+    hours = scrapy.Field()  # TODO: implement postprocessor for list -> " ".join()
     ib_symbol = scrapy.Field()
+    symbol_title = scrapy.Field()
     symbol = scrapy.Field()
     currency = scrapy.Field()
 
--- vendors_amp/ib/metadata/extract/ib_crawler/spiders/ibroker.py
+++ vendors_amp/ib/metadata/extract/ib_crawler/spiders/ibroker.py
@@ -1,20 +1,109 @@
+from typing import DefaultDict, List
+import collections as col
+
 import scrapy
+import scrapy.http as http
+import scrapy.loader as ldr
+import scrapy.loader.processors as pr
 import scrapy.linkextractors as le
+
+import extract.ib_crawler.items as it
+
+
+class ExchangeLoader(ldr.ItemLoader):
+    default_output_processor = pr.Identity()
+
+    products_in = pr.Join(",")
+    hours_in = pr.Join(" ")
 
 
 class IbrokerSpider(scrapy.Spider):
     name = 'ibroker'
     allowed_domains = ['interactivebrokers.com']
-    start_urls = ['https://ndcdyn.interactivebrokers.com/en/index.php?f=46390']
+    start_urls = ['https://ndcdyn.interactivebrokers.com/en/index.php?f=1562']
 
-    lx_categories = le.LinkExtractor(
+    lx_regions = le.LinkExtractor(
         restrict_css="#overview div.container div.row"
     )
+    lx_exchanges = le.LinkExtractor(
+        restrict_css="#exchange-listings table"
+    )
+    lx_products = le.LinkExtractor(
+        restrict_css='#exchange-products div.row div.btn-selectors',
+    )
 
-    def parse(self, response):
-        for category in self.lx_categories.extract_links(response):
-            yield scrapy.Request(category.url, self.parse_category_root)
+    def parse(self, response: http.HtmlResponse):
+        for link in self.lx_regions.extract_links(response):
+            yield scrapy.Request(
+                link.url,
+                self.parse_region,
+                cb_kwargs={"region": link.text},
+            )
 
-    def parse_category_root(self, response):
-        cat_title = response.css("#contents h1.Title::text").get()
+    def parse_region(self, response: http.HtmlResponse, region: str):
+        for link in self.lx_exchanges.extract_links(response):
+            loader = ExchangeLoader(item=it.ExchangeItem)
+            yield scrapy.Request(
+                link.url,
+                self.parse_exchanges, cb_kwargs={"item": loader},
+            )
+
+    def parse_exchanges(self, response: http.HtmlResponse, item: ExchangeLoader):
+        # header = response.css(
+        #     '#exchange-listings div.col-xs-12 table.table thead tr th::text'
+        # ).getall()
+        country = None
+        table = response.css(
+            '#exchange-listings div.col-xs-12 table.table tbody tr'
+        )
+        for t in table:
+            row = t.css("td")
+            if row.attrib.get("rowspan"):
+                country = row.css("td")[0].css("::text").get()
+                row = row.css("td")[1:]
+            url = response.urljoin(row[0].css("a").attrib["href"])
+            market = row[0].css("a::text").get()
+            products = row[1].css("td::text").getall()
+            hours = row[2].css("*::text").getall()
+            item.add_value("country", country)
+            item.add_value("market", market)
+            item.add_value("products", products)
+            item.add_value("hours", hours)
+            yield scrapy.Request(
+                url, self.parse_category, cb_kwargs={"item": item}
+            )
+
+    def parse_category(self, response: http.HtmlResponse, item: ExchangeLoader):
+        link = response.xpath(
+            '//*[@id="exchange-info"]/div/div/div/div[1]/table/tbody/tr/td[2]/a/@href'
+        ).get() or "N/A"
+        item.add_value("link", link)
+        if not self.lx_products.extract_links(response):
+            return self.parse_product(response, item)
+        for link in self.lx_products.extract_links(response):
+            yield scrapy.Request(
+                link.url, self.parse_product, cb_kwargs={"item": item}
+            )
+
+    def parse_product(self, response: http.HtmlResponse, item: ExchangeLoader):
+        product = response.css(
+            '#exchange-products div.btn-selectors p a.btn.btn-default::text'
+        ).get() or "N/A"
+        # hours = response.xpath(
+        #     '//*[@id="exchange-info"]/div/div/div/div[1]/table/tbody/tr/td[1]/text()'
+        # ).get() or "N/A"
+        symbol_title = c
+        symbols = self._parse_symbols(
+            response.css('#exchange-products div.col-xs-12 tbody tr')
+        )
+
+    def _parse_symbols(self, symbol_tb: List[scrapy.Selector]):
+        symbols = list()
+        for row in symbol_tb:
+            data = row.css('td::text').getall()
+            if len(data) < 4:
+                product = row.css('td a::text')
+                data.insert(1, product)
+            symbols.append(data)
+
 
