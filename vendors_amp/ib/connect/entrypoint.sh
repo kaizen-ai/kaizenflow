252e57eef6231e8aebf01f1b2eccdf42f084526b
saggese <saggese@gmail.com>
2021-03-05 16:19:53 -0500

    Cosmetic changes

--- vendors_amp/ib/connect/entrypoint.sh
+++ vendors_amp/ib/connect/entrypoint.sh
@@ -3,21 +3,21 @@
 source docker_build/entrypoint.sh
 
 # Create configurations for controller.
-CMD="python vendors_amp/ib/connect/scripts/make_ib_controller_init_file.py
-       --user ${TWSUSERID}
-             --password ${TWSPASSWORD}"
+CMD="python vendors_amp/ib/connect/scripts/make_ib_controller_init_file.py \
+     --user ${TWSUSERID} \
+     --password ${TWSPASSWORD}"
 echo $CMD
 eval $CMD
 
-CMD="python vendors_amp/ib/connect/scripts/make_jts_init_file.py
-       --trusted_ips 127.0.0.1,${VNC_TRUSTED_IPS}"
+CMD="python vendors_amp/ib/connect/scripts/make_jts_init_file.py \
+    --trusted_ips 127.0.0.1,${VNC_TRUSTED_IPS}"
 echo $CMD
 eval $CMD
 
 # Run controller.
 xvfb-daemon-run /opt/IBController/Scripts/DisplayBannerAndLaunch.sh &
 
-# Tail latest in log dir
+# Tail latest in log dir.
 sleep 1
 tail -f $(find $LOG_PATH -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f 2-) &
 
