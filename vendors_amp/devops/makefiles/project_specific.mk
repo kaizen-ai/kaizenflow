92346bebd6d6060ce60a70fac03c0ef55d833c63
saggese <saggese@gmail.com>
2021-03-22 18:15:47 -0400

    Checkpoint

--- vendors_amp/devops/makefiles/project_specific.mk
+++ vendors_amp/devops/makefiles/project_specific.mk
@@ -2,68 +2,41 @@
 # Setup
 # #############################################################################
 
-ifdef $(GITHUB_SHA)
-IMAGE_RC_SHA:=$(GITHUB_SHA)
-else
-# GITHUB_SHA not found. Setting up IMAGE_RC_SHA form HEAD.
-IMAGE_RC_SHA:=$(shell git rev-parse HEAD)
-endif
-KIBOT_REPO_BASE_PATH=083233266530.dkr.ecr.us-east-2.amazonaws.com/kibot
-KIBOT_IMAGE=$(KIBOT_REPO_BASE_PATH):latest
+ECR_BASE_PATH=083233266530.dkr.ecr.us-east-2.amazonaws.com
+
+KIBOT_REPO_BASE_PATH=$(ECR_BASE_PATH)/kibot
+KIBOT_IMAGE_DEV=$(KIBOT_REPO_BASE_PATH):latest
 KIBOT_IMAGE_RC=$(KIBOT_REPO_BASE_PATH):rc
-KIBOT_IMAGE_RC_SHA=$(KIBOT_REPO_BASE_PATH):$(IMAGE_RC_SHA)
+
+NO_SUPERSLOW_TESTS='True'
+
+im.print_setup:
+	@echo "KIBOT_REPO_BASE_PATH=$(KIBOT_REPO_BASE_PATH)"
+	@echo "KIBOT_IMAGE_DEV=$(KIBOT_IMAGE_DEV)"
+	@echo "KIBOT_IMAGE_RC=$(KIBOT_IMAGE_RC)"
 
 # #############################################################################
-# PostgreSQL server workflow.
+# Development.
 # #############################################################################
 
-# Start local postgres server.
-docker_kibot_postgres_local_up:
+im.docker_bash:
 	docker-compose \
-		-f compose/docker-compose.yml \
-		-f compose/docker-compose.local.yml \
-		up \
-		-d \
-		kibot_postgres_local
-
-# Stop local postgres server.
-docker_kibot_postgres_local_down:
-	docker-compose \
-		-f compose/docker-compose.yml \
-		-f compose/docker-compose.local.yml \
-		down
-
-# Stop local postgres server and remove all data.
-docker_kibot_postgres_local_rm:
-	docker-compose \
-		-f compose/docker-compose.yml \
-		-f compose/docker-compose.local.yml \
-		down \
-		-v
-
-
-# #############################################################################
-# Kibot workflow (including PostgreSQL server).
-# #############################################################################
-
-# Run container with local database.
-# Use for development.
-docker_kibot_local_run:
-	docker-compose \
-		-f compose/docker-compose.yml \
-		-f compose/docker-compose.local.yml \
-		run --rm \
+		-f devops/compose/docker-compose.yml \
+		-f devops/compose/docker-compose.local.yml \
+		run \
+		--rm \
 		-l user=$(USER) \
-		kibot_app bash
+		kibot_app \
+		bash
 
+im.docker_pull:
+	docker pull $(KIBOT_IMAGE_DEV)
 
 # #############################################################################
 # Test kibot workflow (including PostgreSQL server).
 # #############################################################################
 
-# Run container with local database.
-# Run fast tests.
-docker_kibot_test_fast_tests:
+im.run_fast_tests:
 	docker-compose \
 		-f compose/docker-compose.yml \
 		-f compose/docker-compose.test.yml \
@@ -72,9 +45,7 @@ docker_kibot_test_fast_tests:
 		kibot_app \
 		bash run_fast_tests.sh
 
-# Run container with local database.
-# Run slow tests.
-docker_kibot_test_slow_tests:
+im.run_slow_tests:
 	docker-compose \
 		-f compose/docker-compose.yml \
 		-f compose/docker-compose.test.yml \
@@ -83,9 +54,7 @@ docker_kibot_test_slow_tests:
 		kibot_app \
 		bash run_slow_tests.sh
 
-# Run container with local database.
-# Run superslow tests.
-docker_kibot_test_superslow_tests:
+im.run_superslow_tests:
 	docker-compose \
 		-f compose/docker-compose.yml \
 		-f compose/docker-compose.test.yml \
@@ -95,55 +64,95 @@ docker_kibot_test_superslow_tests:
 		bash run_superslow_tests.sh
 
 # #############################################################################
-# Docker workflow.
+# Services.
 # #############################################################################
-kibot_setup_print:
-	@echo "KIBOT_REPO_BASE_PATH=$(KIBOT_REPO_BASE_PATH)"
-	@echo "KIBOT_IMAGE=$(KIBOT_IMAGE)"
-	@echo "KIBOT_IMAGE_RC=$(KIBOT_IMAGE_RC)"
-	@echo "KIBOT_IMAGE_RC_SHA=$(KIBOT_IMAGE_RC_SHA)"
 
-# Pull images from ecr
-docker_kibot_pull:
-	docker pull $(KIBOT_IMAGE)
+# Start local postgres server.
+im.docker_postgres_up.local:
+	docker-compose \
+		-f compose/docker-compose.yml \
+		-f compose/docker-compose.local.yml \
+		up \
+		-d \
+		kibot_postgres_local
 
-# Build images
-docker_kibot_build_rc_image:
+# Stop local postgres server.
+im.docker_postgres_down.local:
+	docker-compose \
+		-f compose/docker-compose.yml \
+		-f compose/docker-compose.local.yml \
+		down
+
+# Stop local postgres server and remove all data.
+im.docker_postgres_rm.local:
+	docker-compose \
+		-f compose/docker-compose.yml \
+		-f compose/docker-compose.local.yml \
+		down \
+		-v
+
+# #############################################################################
+# Images workflows.
+# #############################################################################
+
+ifdef GITHUB_SHA
+IMAGE_RC_SHA:=$(GITHUB_SHA)
+else
+# GITHUB_SHA not found. Setting IMAGE_RC_SHA from HEAD.
+IMAGE_RC_SHA:=$(shell git rev-parse HEAD)
+endif
+
+# Use Docker buildkit or not.
+# DOCKER_BUILDKIT=1
+DOCKER_BUILDKIT=0
+
+im.docker_build_image.rc:
+	DOCKER_BUILDKIT=$(DOCKER_BUILDKIT) \
 	docker build \
 		--progress=plain \
 		--no-cache \
 		-t $(KIBOT_IMAGE_RC) \
-		-t $(KIBOT_IMAGE_RC_SHA) \
-		--file ./Dockerfile .
+		-t $(KIBOT_REPO_BASE_PATH):$(IMAGE_RC_SHA) \
+		--file devops/docker_build/Dockerfile \
+		.
 
-# Push release candidate images
-docker_kibot_push_rc_image:
+im.docker_build_image_with_cache.rc:
+	DOCKER_BUILDKIT=$(DOCKER_BUILDKIT) \
+	docker build \
+		--progress=plain \
+		-t $(KIBOT_IMAGE_RC) \
+		-t $(KIBOT_REPO_BASE_PATH):$(IMAGE_RC_SHA) \
+		--file devops/docker_build/Dockerfile \
+		.
+
+# Push the "rc" image to the registry.
+im.docker_push_image.rc:
 	docker push $(KIBOT_IMAGE_RC)
-	docker push $(KIBOT_IMAGE_RC_SHA)
+	docker push $(KIBOT_REPO_BASE_PATH):$(IMAGE_RC_SHA)
 
-# Tag :rc image with :latest tag
-docker_kibot_tag_rc_latest:
-	docker tag $(KIBOT_IMAGE_RC) $(KIBOT_IMAGE)
+# Mark the "rc" image as "latest".
+im.docker_tag_rc_image.latest:
+	docker tag $(KIBOT_IMAGE_RC) $(KIBOT_IMAGE_DEV)
 
-# Push image with :latest tag
-docker_kibot_push_latest_image:
-	docker push $(KIBOT_IMAGE)
+# Push the "latest" image to the registry.
+im.docker_push_image.latest:
+	docker push $(KIBOT_IMAGE_DEV)
 
 
-BASE_IMAGE?=$(KIBOT_IMAGE)
-VERSION?=
-# Tag :latest image with specific tag
-docker_kibot_tag_latest_version:
-ifeq ($(VERSION),)
-	@echo "You need to provide VERSION parameter. Example: 'make docker_tag_kibot_rc_version VERSION=0.1'"
-else
-	docker tag $(BASE_IMAGE) $(KIBOT_REPO_BASE_PATH):$(VERSION)
-endif
-
-# Push image wish specific tag
-docker_kibot_push_version_image:
-ifeq ($(VERSION),)
-	@echo "You need to provide VERSION parameter. Example: 'make docker_push_kibot_version_image VERSION=0.1'"
-else
-	docker push $(KIBOT_REPO_BASE_PATH):$(VERSION)
-endif
+#BASE_IMAGE?=$(KIBOT_IMAGE)
+#VERSION?=
+## Tag :latest image with specific tag
+#docker_kibot_tag_latest_version:
+#ifeq ($(VERSION),)
+#	@echo "You need to provide VERSION parameter. Example: 'make docker_tag_kibot_rc_version VERSION=0.1'"
+#else
+#	docker tag $(BASE_IMAGE) $(KIBOT_REPO_BASE_PATH):$(VERSION)
+#endif
+#
+## Push image wish specific tag
+#docker_kibot_push_version_image:
+#ifeq ($(VERSION),)
+#	@echo "You need to provide VERSION parameter. Example: 'make docker_push_kibot_version_image VERSION=0.1'"
+#else
+#	docker push $(KIBOT_REPO_BASE_PATH):$(VERSION)
+#endif
