e5c2125f84d83b18b900fc79cc2f50395876eb4d
Artem <amashkovskii@particle.one>
2021-03-24 16:30:00 +0000

    AmpTask1143: correct names in makefile, added entrypoint for no-postgresql container

---
+++ vendors_amp/devops/docker_build/entrypoints/entrypoint_app_only.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "STAGE: $STAGE"
+echo "POSTGRES_HOST: $POSTGRES_HOST"
+echo "POSTGRES_PORT: $POSTGRES_PORT"
+echo "PostgreSQL will not be started..."
+
+umask 000
+
+export PYTHONPATH=/amp:$PYTHONPATH
+
+eval "$@"
