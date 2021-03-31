f1e15b46e03445982d55d68983dc901e3ad60798
Vitaliy Vrublevskiy <vitaliy@particle.one>
2021-03-31 23:55:30 +0300

    AmpTask1161: Refactor

--- apps/im/init.sql
+++ apps/im/init.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE im_db_dev;
-CREATE USER im_dev WITH PASSWORD '***REMOVED***';
-GRANT ALL PRIVILEGES ON DATABASE im_dev TO im_db_dev;
 CREATE DATABASE im_db_pre_prod;
-CREATE USER im_pre_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
-GRANT ALL PRIVILEGES ON DATABASE im_pre_prod TO im_db_pre_prod;
 CREATE DATABASE im_db_prod;
+CREATE USER im_dev WITH PASSWORD '***REMOVED***';
+CREATE USER im_pre_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
 CREATE USER im_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
+GRANT ALL PRIVILEGES ON DATABASE im_dev TO im_db_dev;
+GRANT ALL PRIVILEGES ON DATABASE im_pre_prod TO im_db_pre_prod;
 GRANT ALL PRIVILEGES ON DATABASE im_prod TO im_db_prod;
