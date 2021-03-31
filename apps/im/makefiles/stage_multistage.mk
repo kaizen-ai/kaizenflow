d281daf0e576791408e169389267a35db371e113
Vitaliy Vrublevskiy <vitaliy@particle.one>
2021-03-31 22:14:18 +0300

    AmpTask1161: Refactor

--- apps/im/makefiles/stage_multistage.mk
+++ apps/im/makefiles/stage_multistage.mk
@@ -3,11 +3,11 @@
 # #############################################################################
 im-app.docker_up.multistage:
 	docker-compose \
-		-f apps/im/compose/docker-stack.multistage.yml
+		-f apps/im/compose/docker-stack.multistage.yml \
 		up -d
 
 # Stop multistage container including all dependencies.
 im-app.docker_down.multistage.:
 	docker-compose \
-		-f apps/im/compose/docker-stack.multistage.yml
+		-f apps/im/compose/docker-stack.multistage.yml \
 		down
