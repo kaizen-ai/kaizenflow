768a5372256fb23ef18e53d7ffd9a47208b5ed46
Vitaliy Vrublevskiy <vitaliy@particle.one>
2021-03-31 23:15:21 +0300

    AmpTask1161: Add db init script

---
+++ apps/im/init.sql
@@ -0,0 +1,11 @@
+CREATE USER im_dev WITH PASSWORD '***REMOVED***';
+CREATE DATABASE im_db_dev;
+GRANT ALL PRIVILEGES ON DATABASE im_dev TO im_db_dev;
+
+CREATE USER im_pre_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
+CREATE DATABASE im_db_pre_prod;
+GRANT ALL PRIVILEGES ON DATABASE im_pre_prod TO im_db_pre_prod;
+
+CREATE USER im_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
+CREATE DATABASE im_db_prod;
+GRANT ALL PRIVILEGES ON DATABASE im_prod TO im_db_prod;
