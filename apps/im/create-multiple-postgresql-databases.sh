b8c7b1c1dcc7bb5d82ae8b36cb23733d9fdf8ae0
Vitaliy Vrublevskiy <vitaliy@particle.one>
2021-04-01 11:56:06 +0300

    AmpTask1161: Refactor

--- apps/im/create-multiple-postgresql-databases.sh
+++ apps/im/create-multiple-postgresql-databases.sh
@@ -3,20 +3,15 @@
 set -e
 set -u
 
-function create_user_and_database() {
-	local database=$1
-	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
-EOSQL
-}
 
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_user_and_database $db
-	done
-	echo "Multiple databases created"
-fi
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER im_dev WITH PASSWORD '***REMOVED***';
+    CREATE DATABASE "im_db_dev";
+    GRANT ALL PRIVILEGES ON DATABASE im_dev TO im_db_dev;
+    CREATE USER im_pre_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
+    CREATE DATABASE "im_db_pre_prod";
+    GRANT ALL PRIVILEGES ON DATABASE im_pre_prod TO im_db_pre_prod;
+    CREATE DATABASE "im_db_prod";
+    CREATE USER im_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
+    GRANT ALL PRIVILEGES ON DATABASE im_prod TO im_db_prod;
+EOSQL
