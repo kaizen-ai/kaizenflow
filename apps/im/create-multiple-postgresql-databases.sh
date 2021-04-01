3832c1dac2792093186a7c8eed37345389689354
Vitaliy Vrublevskiy <vitaliy@particle.one>
2021-04-01 12:20:21 +0300

    AmpTask1161: Refactor

--- apps/im/create-multiple-postgresql-databases.sh
+++ apps/im/create-multiple-postgresql-databases.sh
@@ -3,15 +3,17 @@
 set -e
 set -u
 
-
+echo "Create im_db_dev,im_db_pre_prod,im_db_prod databases with users..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE im_db_dev;
     CREATE USER im_dev WITH PASSWORD '***REMOVED***';
-    CREATE DATABASE "im_db_dev";
-    GRANT ALL PRIVILEGES ON DATABASE im_dev TO im_db_dev;
-    CREATE USER im_pre_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
-    CREATE DATABASE "im_db_pre_prod";
-    GRANT ALL PRIVILEGES ON DATABASE im_pre_prod TO im_db_pre_prod;
-    CREATE DATABASE "im_db_prod";
-    CREATE USER im_prod WITH PASSWORD 'hjadhnuh2193nbodwaWa';
-    GRANT ALL PRIVILEGES ON DATABASE im_prod TO im_db_prod;
+    GRANT ALL PRIVILEGES ON DATABASE im_db_dev TO im_dev;
+
+    CREATE DATABASE im_db_pre_prod;
+    CREATE USER im_pre_prod WITH PASSWORD 'adwaw98dyaw9dWAD0';
+    GRANT ALL PRIVILEGES ON DATABASE im_db_pre_prod TO im_pre_prod;
+
+    CREATE DATABASE im_db_prod;
+    CREATE USER im_prod WITH PASSWORD 'awdhaw98dyAWPD(WDpa';
+    GRANT ALL PRIVILEGES ON DATABASE im_db_prod TO im_prod;
 EOSQL
