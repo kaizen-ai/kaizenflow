d12352358fe061a3fe7f187ba5dc9d52e21c4d61
Vitaliy Vrublevskiy <vitaliy@particle.one>
2021-04-01 14:18:25 +0300

    AmpTask1161: Refactor stages

--- apps/im/makefiles/stage_local.mk
+++ apps/im/makefiles/stage_local.mk
@@ -5,21 +5,16 @@
 # Run app container, start a local PostgreSQL DB.
 im-app.docker_up.local:
 	IMAGE=$(IM_IMAGE_DEV) \
-	POSTGRES_PORT=${IM_PG_PORT_LOCAL} \
 	docker-compose \
-		-f amp/apps/im/compose/docker-compose.local.yml
-		run \
-		--rm \
-		-l user=$(USER) \
-		app \
-		bash
+		-f apps/im/compose/docker-compose.local.yml \
+		up
 
 # Run app container w/o PostgreSQL.
 im-app.docker_bash:
 	IMAGE=$(IM_IMAGE_DEV) \
 	POSTGRES_PORT=${IM_PG_PORT_LOCAL} \
 	docker-compose \
-		-f amp/apps/im/compose/docker-compose.local.yml
+		-f apps/im/compose/docker-compose.local.yml \
 		run \
 		--rm \
 		-l user=$(USER) \
@@ -32,7 +27,7 @@ im-app.docker_down.local:
 	IMAGE=$(IM_IMAGE_DEV) \
 	POSTGRES_PORT=${IM_PG_PORT_LOCAL} \
 	docker-compose \
-		-f amp/apps/im/compose/docker-compose.local.yml
+		-f apps/im/compose/docker-compose.local.yml \
 		down \
 		--remove-orphans
 
@@ -41,11 +36,7 @@ im-app.docker_rm.local:
 	IMAGE=$(IM_IMAGE_DEV) \
 	POSTGRES_PORT=${IM_PG_PORT_LOCAL} \
 	docker-compose \
-		-f amp/apps/im/compose/docker-compose.local.yml
+		-f apps/im/compose/docker-compose.local.yml \
 		down \
 		--remove-orphans; \
 	docker volume rm compose_im_postgres_data_local
-
-# Pull docker image from AWS ECR
-im-app.docker_pull:
-	docker pull $(IM_IMAGE_DEV)
