0371608165968db8fb144ff2d4be244ada21407a
saggese <saggese@gmail.com>
2021-04-11 08:04:32 -0400

    Add unit tests

--- test/test_task.py
+++ test/test_task.py
@@ -1,7 +1,10 @@
 import os
+import re
+import unittest
 
 import pytest
 
+import invoke
 from invoke import MockContext, Result
 
 import helpers.unit_test as hut
@@ -12,46 +15,62 @@ from tasks import get_platform, replace
 
 class TestTasks(hut.TestCase):
 
-    def _helper(self, target:str) -> None:
+    def _test_dry_run(self, target:str) -> None:
         cmd = "invoke --dry " + target
         _, txt = hsi.system_to_string(cmd)
         self.check_string(txt)
 
-    def test_print_setup(self) -> None:
+    def test_print_setup1(self) -> None:
         target = "print_setup"
-        self._helper(target)
+        self._test_dry_run(target)
+        #
+        self._test_dry_run(target)
 
-    def test_git_pull(self) -> None:
+    def test_git_pull1(self) -> None:
         target = "git_pull"
-        self._helper(target)
+        self._test_dry_run(target)
 
-    def test_git_pull_master(self) -> None:
+    def test_git_pull_master1(self) -> None:
         target = "git_pull_master"
-        self._helper(target)
+        self._test_dry_run(target)
 
-    def test_git_clean(self) -> None:
+    def test_git_clean1(self) -> None:
         target = "git_clean"
-        self._helper(target)
+        self._test_dry_run(target)
+
+    def _build_mock_context_returning_ok(self):
+        ctx = MockContext(run={
+            re.compile(".*"): Result(exited=0)
+        })
+        return ctx
+
+    def _check_calls(self, ctx: invoke.MockContext) -> None:
+        act = "\n".join(map(str, ctx.run.mock_calls))
+        self.check_string(act)
 
     def test_docker_login(self) -> None:
-        #stdout = "aws-cli/1.19.49 Python/3.7.6 Darwin/19.6.0 botocore/1.20.49\n"
-        #ctx = MockContext(run=Result(stdout))
-        #tasks.docker_login(ctx).run()
-        pass
-
-    def test_get_platform(self):
-        c = MockContext(run=Result("Darwin\n"))
-        print(get_platform(c))
-        assert 0
-        assert "Apple" in get_platform(c)
-        c = MockContext(run=Result("Linux\n"))
-        assert "desktop" in get_platform(c)
-
-    def test_regular_sed(self):
-        expected_sed = "sed -e s/foo/bar/g file.txt"
-        c = MockContext(run={
-            "which gsed": Result(exited=1),
-            #expected_sed: Result(),
+        stdout = "aws-cli/1.19.49 Python/3.7.6 Darwin/19.6.0 botocore/1.20.49\n"
+        ctx = MockContext(run={
+            "aws --version": Result(stdout),
+            re.compile("^docker login"): Result(exited=0)
         })
-        replace(c, 'file.txt', 'foo', 'bar')
-        c.run.assert_called_with(expected_sed)
+        tasks.docker_login(ctx)
+        # Check the outcome.
+        self._check_calls(ctx)
+
+    # def test_get_platform(self):
+    #     c = MockContext(run=Result("Darwin\n"))
+    #     print(get_platform(c))
+    #     assert 0
+    #     assert "Apple" in get_platform(c)
+    #     c = MockContext(run=Result("Linux\n"))
+    #     assert "desktop" in get_platform(c)
+    #
+    # def test_regular_sed(self):
+    #     expected_sed = "sed -e s/foo/bar/g file.txt"
+    #     c = MockContext(run={
+    #         "which gsed": Result(exited=1),
+    #         #expected_sed: Result(),
+    #     })
+    #     replace(c, 'file.txt', 'foo', 'bar')
+    #     c.run.assert_called_with(expected_sed)
