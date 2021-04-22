a540f81798506d804ff8199f493d4d4afbc6468b
paul <smith.paul.anthony@gmail.com>
2021-04-22 16:10:54 -0400

    AmpTask1247: Add comments and change order of classes.

--- core/dataflow_source_nodes.py
+++ core/dataflow_source_nodes.py
@@ -14,6 +14,68 @@ _LOG = logging.getLogger(__name__)
 _PANDAS_DATE_TYPE = Union[str, pd.Timestamp, datetime.datetime]
 
 
+class ArmaGenerator(cdataf.DataSource):
+    def __init__(
+            self,
+            nid: str,
+            frequency: str,
+            start_date: _PANDAS_DATE_TYPE,
+            end_date: _PANDAS_DATE_TYPE,
+            ar_coeffs: Optional[List[float]] = None,
+            ma_coeffs: Optional[List[float]] = None,
+            scale: Optional[float] = None,
+            burnin: Optional[float] = None,
+            seed: Optional[float] = None,
+    ) -> None:
+        super().__init__(nid)
+        self._frequency = frequency
+        self._start_date = start_date
+        self._end_date = end_date
+        self._ar_coeffs = ar_coeffs or [0]
+        self._ma_coeffs = ma_coeffs or [0]
+        self._scale = scale or 1
+        self._burnin = burnin or 0
+        self._seed = seed
+        self._arma_process = sig_gen.ArmaProcess(
+            ar_coeffs=self._ar_coeffs,
+            ma_coeffs=self._ma_coeffs
+        )
+
+    def fit(self) -> Optional[Dict[str, pd.DataFrame]]:
+        """
+        :return: training set as df
+        """
+        self._lazy_load()
+        return super().fit()
+
+    def predict(self) -> Optional[Dict[str, pd.DataFrame]]:
+        self._lazy_load()
+        return super().predict()
+
+    def _lazy_load(self) -> None:
+        if self.df is not None:
+            return
+        rets = self._arma_process.generate_sample(
+            date_range_kwargs={
+                "start": self._start_date,
+                "end": self._end_date,
+                "freq": self._frequency,
+            },
+            scale=self._scale,
+            burnin=self._burnin,
+            seed=self._seed
+        )
+        # Cumulatively sum to generate a price series (implicitly assumes the
+        # returns are log returns; at small enough scales and short enough
+        # times this is practically interchangeable with percentage returns).
+        prices = rets.cumsum()
+        prices.name = "close"
+        self.df = prices.to_frame()
+        self.df = self.df.loc[self._start_date : self._end_date]
+        # Use constant volume (for now).
+        self.df["vol"] = 100
+
+
 def DataSourceNodeFactory(
     nid: str, source_node_name: str, source_node_kwargs: Dict[str, Any]
 ) -> cdataf.DataSource:
@@ -166,61 +228,3 @@ class KibotColumnReader(cdataf.DataSource):
             data = data.loc[self._start_date : self._end_date]
             dict_df[s] = data
         self.df = pd.DataFrame.from_dict(dict_df)
-
-
-class ArmaGenerator(cdataf.DataSource):
-    def __init__(
-        self,
-        nid: str,
-        frequency: str,
-        start_date: _PANDAS_DATE_TYPE,
-        end_date: _PANDAS_DATE_TYPE,
-        ar_coeffs: Optional[List[float]] = None,
-        ma_coeffs: Optional[List[float]] = None,
-        scale: Optional[float] = None,
-        burnin: Optional[float] = None,
-        seed: Optional[float] = None,
-    ) -> None:
-        super().__init__(nid)
-        self._frequency = frequency
-        self._start_date = start_date
-        self._end_date = end_date
-        self._ar_coeffs = ar_coeffs or [0]
-        self._ma_coeffs = ma_coeffs or [0]
-        self._scale = scale or 1
-        self._burnin = burnin or 0
-        self._seed = seed
-        self._arma_process = sig_gen.ArmaProcess(
-            ar_coeffs=self._ar_coeffs,
-            ma_coeffs=self._ma_coeffs
-        )
-
-    def fit(self) -> Optional[Dict[str, pd.DataFrame]]:
-        """
-        :return: training set as df
-        """
-        self._lazy_load()
-        return super().fit()
-
-    def predict(self) -> Optional[Dict[str, pd.DataFrame]]:
-        self._lazy_load()
-        return super().predict()
-
-    def _lazy_load(self) -> None:
-        if self.df is not None:
-            return
-        rets = self._arma_process.generate_sample(
-            date_range_kwargs={
-                "start": self._start_date,
-                "end": self._end_date,
-                "freq": self._frequency,
-            },
-            scale=self._scale,
-            burnin=self._burnin,
-            seed=self._seed
-        )
-        prices = rets.cumsum()
-        prices.name = "close"
-        self.df = prices.to_frame()
-        self.df = self.df.loc[self._start_date : self._end_date]
-        self.df["vol"] = 100
