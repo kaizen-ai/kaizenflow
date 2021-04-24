538f060bb47c49521658224c8172fb9b6f1e6b0b
saggese <saggese@gmail.com>
2021-04-24 14:21:14 -0400

    Fix bug

--- core/dataflow/test/test_models.py
+++ core/dataflow/test/test_models.py
@@ -1083,7 +1083,7 @@ class TestContinuousSarimaxModel(hut.TestCase):
         csm = cdataf.ContinuousSarimaxModel("model", **config.to_dict())
         df_out = csm.fit(data)["df_out"]
         # Package results.
-        act = self._package_results(config, df_out, decimals=5)
+        act = self._package_results(config, df_out, decimals=3)
         self.check_string(act)
 
     def test_compare_to_linear_regression1(self) -> None:
@@ -1185,7 +1185,7 @@ class TestContinuousSarimaxModel(hut.TestCase):
         csm.fit(data_fit)
         df_out = csm.predict(data_predict)["df_out"]
         # Package results.
-        act = self._package_results(config, df_out, decimals=4)
+        act = self._package_results(config, df_out, decimals=3)
         self.check_string(act)
 
     def test_predict2(self) -> None:
@@ -1264,7 +1264,7 @@ class TestContinuousSarimaxModel(hut.TestCase):
         csm.fit(data_fit)
         df_out = csm.predict(data_predict)["df_out"]
         # Package results.
-        act = self._package_results(config, df_out, decimals=4)
+        act = self._package_results(config, df_out, decimals=3)
         self.check_string(act)
 
     def test_predict_different_intervals_no_x1(self) -> None:
