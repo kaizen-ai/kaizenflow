1c4731976957a707cde0ebd2864cf1ec5096dbed
paul <smith.paul.anthony@gmail.com>
2021-04-27 14:59:02 -0400

    Rename builder to builders

--- core/dataflow/__init__.py
+++ core/dataflow/__init__.py
@@ -4,7 +4,7 @@ Import as:
 import core.dataflow as cdataf
 """
 
-from core.dataflow.builder import *  # pylint: disable=unused-import # NOQA
+from core.dataflow.builders import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.core import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.models import *  # pylint: disable=unused-import # NOQA
 from core.dataflow.nodes import *  # pylint: disable=unused-import # NOQA
