649cda663787693142e95d5bd7373e035f2db28a
paul <smith.paul.anthony@gmail.com>
2021-04-27 14:52:11 -0400

    LemTask134: Add pipeline for generating fake filtered returns

--- core/dataflow/builder.py
+++ core/dataflow/builder.py
@@ -3,7 +3,10 @@ import logging
 from typing import Any, Dict, List, Optional
 
 import core.config as cconfi
-from core.dataflow.core import DAG
+import core.finance as fin
+from core.dataflow.core import DAG, Node
+from core.dataflow.nodes import ArmaGenerator, ColumnTransformer, TimeBarResampler, TwapVwapComputer
+from core.dataflow.models import VolatilityModel
 
 _LOG = logging.getLogger(__name__)
 
@@ -91,3 +94,143 @@ class DagBuilder(abc.ABC):
     def _get_nid(self, stage_name: str) -> str:
         nid = self._nid_prefix + stage_name
         return nid
+
+
+class ArmaReturnsBuilder(DagBuilder):
+    """
+    Pipeline for generating filtered returns from an ARMA process.
+    """
+
+    def get_config_template(self) -> cconfi.Config:
+        """
+        Return a reference configuration.
+
+        :return: reference config
+        """
+
+        config = cfgb.get_config_from_nested_dict(
+            {
+                # Load prices.
+                self._get_nid("rets/read_data"): {
+                    "frequency": "T",
+                    "start_date": "2010-01-04 09:00:00",
+                    "end_date": "2010-01-04 16:30:00",
+                    "ar_coeffs": [0],
+                    "ma_coeffs": [0],
+                    "scale": 0.1,
+                    "burnin": 0,
+                    "seed": 0,
+                },
+                # Filter ATH.
+                self._get_nid("rets/filter_ath"): {
+                    "col_mode": "replace_all",
+                    "transformer_kwargs": {
+                        "start_time": datetime.time(9, 30),
+                        "end_time": datetime.time(16, 00),
+                    },
+                },
+                # Resample returns.
+                self._get_nid("rets/resample"): {
+                    "rule": "1T",
+                    "price_cols": ["close"],
+                    "volume_cols": ["vol"],
+                },
+                # Compute TWAP and VWAP.
+                self._get_nid("rets/compute_wap"): {
+                    "rule": "5T",
+                    "price_col": "close",
+                    "volume_col": "vol",
+                },
+                # Calculate rets.
+                self._get_nid("rets/compute_ret_0"): {
+                    "cols": ["twap", "vwap"],
+                    "col_mode": "merge_all",
+                    "transformer_kwargs": {"mode": "pct_change",},
+                },
+                # Model volatility.
+                self._get_nid("rets/model_volatility"): {
+                    "cols": ["vwap_ret_0"],
+                    "steps_ahead": 2,
+                    "nan_mode": "leave_unchanged",
+                },
+                # Clip rets.
+                self._get_nid("rets/clip"): {
+                    "cols": ["vwap_ret_0_zscored"],
+                    "col_mode": "replace_selected",
+                },
+            }
+        )
+        return config
+
+    def get_dag(self, config: cconfi.Config, mode: str = "strict") -> DAG:
+        """
+        Generate pipeline DAG.
+
+        :param config: config object used to configure DAG
+        :param mode: "strict" (e.g., for production) or "loose" (e.g., for
+            interactive jupyter notebooks)
+        :return: initialized DAG
+        """
+        dag = DAG(mode=mode)
+        _LOG.debug("%s", config)
+        # Read data.
+        stage = "rets/read_data"
+        nid = self._get_nid(stage)
+        node = ArmaGenerator(nid, **config[nid].to_dict())
+        tail_nid = self._append(dag, None, node)
+        # Set weekends to Nan.
+        stage = "rets/filter_weekends"
+        nid = self._get_nid(stage)
+        node = ColumnTransformer(
+            nid, transformer_func=fin.set_weekends_to_nan, col_mode="replace_all",
+        )
+        tail_nid = self._append(dag, tail_nid, node)
+        # Set non-ATH to NaN.
+        stage = "rets/filter_ath"
+        nid = self._get_nid(stage)
+        node = ColumnTransformer(
+            nid, transformer_func=fin.set_non_ath_to_nan, **config[nid].to_dict(),
+        )
+        tail_nid = self._append(dag, tail_nid, node)
+        # Resample.
+        stage = "rets/resample"
+        nid = self._get_nid(stage)
+        node = TimeBarResampler(nid, **config[nid].to_dict())
+        tail_nid = self._append(dag, tail_nid, node)
+        # Compute TWAP and VWAP.
+        stage = "rets/compute_wap"
+        nid = self._get_nid(stage)
+        node = TwapVwapComputer(nid, **config[nid].to_dict(),)
+        tail_nid = self._append(dag, tail_nid, node)
+        # Compute returns.
+        stage = "rets/compute_ret_0"
+        nid = self._get_nid(stage)
+        node = dtf.ColumnTransformer(
+            nid,
+            transformer_func=fin.compute_ret_0,
+            col_rename_func=lambda x: x + "_ret_0",
+            **config[nid].to_dict(),
+        )
+        tail_nid = self._append(dag, tail_nid, node)
+        # Model volatility.
+        stage = "rets/model_volatility"
+        nid = self._get_nid(stage)
+        node = VolatilityModel(nid, **config[nid].to_dict())
+        tail_nid = self._append(dag, tail_nid, node)
+        # Clip rets.
+        stage = "rets/clip"
+        nid = self._get_nid(stage)
+        node = ColumnTransformer(
+            nid,
+            transformer_func=lambda x: x.clip(lower=-3, upper=3),
+            **config[nid].to_dict(),
+        )
+        tail_nid = self._append(dag, tail_nid, node)
+        return dag
+
+    @staticmethod
+    def _append(dag: DAG, tail_nid: Optional[str], node: Node) -> str:
+        dag.add_node(node)
+        if tail_nid is not None:
+            dag.connect(tail_nid, node.nid)
+        return node.nid
