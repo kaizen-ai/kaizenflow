fba7c57ceb3bbf0b366a3786ae15ff1ca6360379
saggese <saggese@gmail.com>
2021-05-14 17:09:32 -0400

    Fix test

--- helpers/git.py
+++ helpers/git.py
@@ -824,14 +824,17 @@ def git_add_update(
 def fetch_origin_master_if_needed():
     if hsinte.is_inside_ci():
         _LOG.warning("Running inside CI so fetching master")
-        # See AmpTask1321 and AmpTask1338 for details.
-        cmd = "git fetch origin master:refs/remotes/origin/master"
-        hsinte.system(cmd)
-        cmd = "git branch --track master origin/master"
-        hsinte.system(cmd)
-        cmd = "git branch -a"
+        cmd = 'git branch -a | egrep "^\s*master\s*$" | wc -l'
         # * (HEAD detached at pull/1337/merge)
         # master
         # remotes/origin/master
         # remotes/pull/1337/merge
-        hsinte.system(cmd)
+        _, num = hsinte.system_to_one_line(cmd)
+        num = int(num)
+        _LOG.debug("num=%s", num)
+        if num == 0:
+            # See AmpTask1321 and AmpTask1338 for details.
+            cmd = "git fetch origin master:refs/remotes/origin/master"
+            hsinte.system(cmd)
+            cmd = "git branch --track master origin/master"
+            hsinte.system(cmd)
