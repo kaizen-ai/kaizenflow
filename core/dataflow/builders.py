a1aa79c611ab843b50b6e8651e76bdf0a519a1cf
paul <smith.paul.anthony@gmail.com>
2021-05-17 17:05:16 -0400

    Move remaining volatility models

--- core/dataflow/builders.py
+++ core/dataflow/builders.py
@@ -7,7 +7,7 @@ import core.config as cconfi
 import core.config_builders as cfgb
 import core.finance as fin
 from core.dataflow.core import DAG, Node
-from core.dataflow.models import VolatilityModel
+from core.dataflow.nodes.volatility_models import VolatilityModel
 from core.dataflow.nodes.sources import ArmaGenerator
 from core.dataflow.nodes.transformers import (
     ColumnTransformer,
