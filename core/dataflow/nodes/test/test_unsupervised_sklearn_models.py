556982012a1277cd6b0ba1492c264f2278950283
paul <smith.paul.anthony@gmail.com>
2021-05-19 10:12:50 -0400

    Checkpoint

--- core/dataflow/nodes/test/test_unsupervised_sklearn_models.py
+++ core/dataflow/nodes/test/test_unsupervised_sklearn_models.py
@@ -6,8 +6,6 @@ import sklearn.decomposition as sdecom
 import core.artificial_signal_generators as casgen
 import core.config_builders as ccbuild
 import helpers.unit_test as hut
-from core.dataflow.core import DAG
-from core.dataflow.nodes.sources import ReadDataFromDf
 from core.dataflow.nodes.unsupervised_sklearn_models import (
     Residualizer,
     UnsupervisedSkLearnModel,
@@ -80,14 +78,6 @@ class TestResidualizer(hut.TestCase):
     def test_predict_dag1(self) -> None:
         # Load test data.
         data = self._get_data()
-        data_source_node = ReadDataFromDf("data", data)
-        fit_interval = ("2000-01-03", "2000-01-31")
-        predict_interval = ("2000-02-01", "2000-02-25")
-        data_source_node.set_fit_intervals([fit_interval])
-        data_source_node.set_predict_intervals([predict_interval])
-        # Create DAG and test data node.
-        dag = DAG(mode="strict")
-        dag.add_node(data_source_node)
         # Load sklearn config and create modeling node.
         config = ccbuild.get_config_from_nested_dict(
             {
@@ -97,11 +87,9 @@ class TestResidualizer(hut.TestCase):
             }
         )
         node = Residualizer("sklearn", **config.to_dict())
-        dag.add_node(node)
-        dag.connect("data", "sklearn")
-        #
-        dag.run_leq_node("sklearn", "fit")
-        df_out = dag.run_leq_node("sklearn", "predict")["df_out"]
+        node.fit(data.loc["2000-01-03": "2000-01-31"])
+        # Predict.
+        df_out = node.predict(data.loc["2000-02-01": "2000-02-25"])["df_out"]
         self.check_string(df_out.to_string())
 
     def _get_data(self) -> pd.DataFrame:
