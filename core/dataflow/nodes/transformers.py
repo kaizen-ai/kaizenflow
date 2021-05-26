037e31273775db0576b96ff808ab7ee8d1dbec44
paul <smith.paul.anthony@gmail.com>
2021-05-26 11:53:01 -0400

    Fix comment

--- core/dataflow/nodes/transformers.py
+++ core/dataflow/nodes/transformers.py
@@ -370,8 +370,7 @@ class SeriesToSeriesTransformer(Transformer):
         nan_mode: Optional[str] = None,
     ) -> None:
         """
-        For reference, let.
-
+        For reference, let
           - N = df.columns.nlevels
           - leaf_cols = df[in_col_group].columns
 
