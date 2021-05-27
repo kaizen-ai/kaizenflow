5832e31877f1d5cb3aab9168dbe14c88cc0de2e1
paul <smith.paul.anthony@gmail.com>
2021-05-27 11:36:42 -0400

    Checkpoint

--- core/dataflow/nodes/test/test_sklearn_models.py
+++ core/dataflow/nodes/test/test_sklearn_models.py
@@ -181,104 +181,32 @@ class TestMultiindexSkLearnModel(hut.TestCase):
         df_str = hut.convert_df_to_string(df_out.round(3), index=True, decimals=3)
         self.check_string(df_str)
 
-    # def test2(self) -> None:
-    #     data = self._get_data(2)
-    #     config = cfgb.get_config_from_nested_dict(
-    #         {
-    #             "x_vars": ["x"],
-    #             "y_vars": ["y"],
-    #             "steps_ahead": 2,
-    #             "model_kwargs": {
-    #                 "alpha": 0.5,
-    #             },
-    #             "col_mode": "merge_all",
-    #         }
-    #     )
-    #     node = ContinuousSkLearnModel(
-    #         "sklearn",
-    #         model_func=slmode.Ridge,
-    #         **config.to_dict(),
-    #     )
-    #     df_out = node.fit(data)["df_out"]
-    #     df_str = hut.convert_df_to_string(df_out, index=True, decimals=3)
-    #     self.check_string(df_str)
-    #
-    # def test3(self) -> None:
-    #     """
-    #     Test `slmode.Lasso` model.
-    #
-    #     `Lasso` returns a one-dimensional array for a two-dimensional
-    #     input.
-    #     """
-    #     data = self._get_data(1)
-    #     config = cfgb.get_config_from_nested_dict(
-    #         {
-    #             "x_vars": ["x"],
-    #             "y_vars": ["y"],
-    #             "steps_ahead": 1,
-    #             "model_kwargs": {
-    #                 "alpha": 0.5,
-    #             },
-    #         }
-    #     )
-    #     node = ContinuousSkLearnModel(
-    #         "sklearn",
-    #         model_func=slmode.Lasso,
-    #         **config.to_dict(),
-    #     )
-    #     df_out = node.fit(data)["df_out"]
-    #     df_str = hut.convert_df_to_string(df_out, index=True, decimals=3)
-    #     self.check_string(df_str)
-    #
-    # def test4(self) -> None:
-    #     data = self._get_data(1)
-    #     data_fit = data.loc[:"2010-01-01 00:29:00"]
-    #     data_predict = data.loc["2010-01-01 00:30:00":]
-    #     config = cfgb.get_config_from_nested_dict(
-    #         {
-    #             "x_vars": ["x"],
-    #             "y_vars": ["y"],
-    #             "steps_ahead": 1,
-    #             "model_kwargs": {
-    #                 "alpha": 0.5,
-    #             },
-    #             "col_mode": "merge_all",
-    #         }
-    #     )
-    #     node = ContinuousSkLearnModel(
-    #         "sklearn",
-    #         model_func=slmode.Ridge,
-    #         **config.to_dict(),
-    #     )
-    #     node.fit(data_fit)
-    #     df_out = node.predict(data_predict)["df_out"]
-    #     df_str = hut.convert_df_to_string(df_out, index=True, decimals=3)
-    #     self.check_string(df_str)
-    #
-    # def test5(self) -> None:
-    #     data = self._get_data(2)
-    #     data_fit = data.loc[:"2010-01-01 00:29:00"]
-    #     data_predict = data.loc["2010-01-01 00:30:00":]
-    #     config = cfgb.get_config_from_nested_dict(
-    #         {
-    #             "x_vars": ["x"],
-    #             "y_vars": ["y"],
-    #             "steps_ahead": 2,
-    #             "model_kwargs": {
-    #                 "alpha": 0.5,
-    #             },
-    #         }
-    #     )
-    #     node = ContinuousSkLearnModel(
-    #         "sklearn",
-    #         model_func=slmode.Ridge,
-    #         **config.to_dict(),
-    #     )
-    #     node.fit(data_fit)
-    #     df_out = node.predict(data_predict)["df_out"]
-    #     df_str = hut.convert_df_to_string(df_out, index=True, decimals=3)
-    #     self.check_string(df_str)
-    #
+    def test2(self) -> None:
+        data = self._get_data()
+        data_fit = data.loc[:"2000-01-31"]
+        data_predict = data.loc["2000-01-31":]
+        config = cfgb.get_config_from_nested_dict(
+            {
+                "in_col_groups": [("ret_0",),],
+                "out_col_group": (),
+                "x_vars": ["ret_0"],
+                "y_vars": ["ret_0"],
+                "steps_ahead": 1,
+                "model_kwargs": {
+                    "alpha": 0.5,
+                },
+            }
+        )
+        node = MultiindexSkLearnModel(
+            "sklearn",
+            model_func=slmode.Ridge,
+            **config.to_dict(),
+        )
+        node.fit(data_fit)
+        df_out = node.predict(data_predict)["df_out"]
+        df_str = hut.convert_df_to_string(df_out.round(3), index=True, decimals=3)
+        self.check_string(df_str)
+
     def _get_data(self) -> pd.DataFrame:
         """
         Generate multivariate normal returns.
