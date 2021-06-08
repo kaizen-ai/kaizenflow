e715bb00688ad4876f02d006c933871d293a9053
saggese <saggese@gmail.com>
2021-06-08 16:03:52 -0400

    Rename files

---
+++ core/config/__init__.py
@@ -0,0 +1,9 @@
+"""
+Import as:
+
+import core.config as cconfig
+"""
+
+from core.config.config import *  # pylint: disable=unused-import # NOQA
+from core.config.builder import *  # pylint: disable=unused-import # NOQA
+from core.config.utils import *  # pylint: disable=unused-import # NOQA
