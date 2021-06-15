26d42bd8c8574d59169805ea6a0c9263230d6652
saggese <saggese@gmail.com>
2021-06-15 08:10:44 -0400

    Checkpoint

---
+++ helpers/notebooks/gallery_s3.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# https://s3fs.readthedocs.io/en/latest/
+
+# %%
+import s3fs
+
+import helpers.s3 as hs3
+
+# %%
+profile = "am"
+aws_access_key_id, aws_secret_access_key, aws_region = hs3.get_aws_credentials(
+    profile=profile
+)
+
+# %%
+s3 = s3fs.S3FileSystem(anon=False, key=aws_access_key_id, secret=aws_secret_access_key)
+
+# %%
+bucket = hs3.get_bucket()
+print("bucket=%s" % bucket)
+s3.ls(bucket)
