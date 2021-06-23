c8fc30b77b71b506e24beef60ac92cee0301b3b6
saggese <saggese@gmail.com>
2021-06-23 16:16:45 -0400

    Update repo_config to latest standard

--- repo_config.py
+++ repo_config.py
@@ -1,5 +1,13 @@
-def get_repo_map():
+"""
+This file contains info specific of this repo.
+"""
+
+from typing import Dict
+ 
+def get_repo_map() -> Dict[str, str]:
+    """
+    Return a mapping of long repo name -> short repo name.
+    """
     repo_map = {
-        
     }
     return repo_map
