c4b318ddfa124c665bf939c8509d4583ac762d87
paul <smith.paul.anthony@gmail.com>
2021-06-23 12:16:56 -0400

    Add comments, fix bug.

--- core/statistics.py
+++ core/statistics.py
@@ -256,32 +256,43 @@ def compute_local_level_model_stats(
     """
     Compute statistics assuming a steady-state local level model.
 
-    This is equivalent to modeling `srs` as ARIMA(0,1,1).
+    E.g.,
+       y_t = \mu_t + \epsilon_t
+       \mu_{t + 1} = \mu_t + \eta_t
+
+    This is equivalent to modeling `srs` of {y_t} as ARIMA(0,1,1).
     """
     prefix = prefix or ""
     dbg.dassert_isinstance(srs, pd.Series)
-    gamma0 = srs.multiply(srs).mean()
-    gamma1 = srs.multiply(srs.shift(1)).mean()
+    diff = srs.diff()
+    gamma0 = diff.multiply(diff).mean()
+    gamma1 = diff.multiply(diff.shift(1)).mean()
     if gamma1 > 0:
         _LOG.warning(f"gamma1=`{gamma1}` should be negative if a local level model is appropriate.")
-    var_eps = -1 * gamma1
-    var_eta = gamma0 - 2 * var_eps
+    # This is the variance of \epsilon.
+    var_epsilon = -1 * gamma1
+    # This is the variance of \eta.
+    var_eta = gamma0 - 2 * var_epsilon
+    # This is the first autocorrelation coefficient.
     rho1 = gamma1 / gamma0
-    snr = -1 / rho1 - 2
-    p = 0.5 * (np.sqrt(snr ** 2 + 4 * snr))
+    # The signal-to-noise ratio `snr` is commonly denoted `q`.
+    snr = var_eta / var_epsilon
+    p = 0.5 * (snr + np.sqrt(snr ** 2 + 4 * snr))
+    # Equivalent to EMA lambda in m_t = (1 - \lambda) m_{t - 1} + \lambda y_t.
     kalman_gain = p / (p + 1)
+    # Convert to center-of-mass.
     com = 1 / kalman_gain - 1
     result_index = [
         prefix + "gamma0",
         prefix + "gamma1",
-        prefix + "var_eps",
+        prefix + "var_epsilon",
         prefix + "var_eta",
         prefix + "rho1",
         prefix + "snr",
         prefix + "kalman_gain",
         prefix + "com",
         ]
-    result_values = [gamma0, gamma1, var_eps, var_eta, rho1, snr, kalman_gain, com]
+    result_values = [gamma0, gamma1, var_epsilon, var_eta, rho1, snr, kalman_gain, com]
     result = pd.Series(data=result_values, index=result_index, name=srs.name)
     return result
 
