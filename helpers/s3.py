6f758370ba0e79f7c9f17ec52655f8c0fd4c9e50
saggese <saggese@gmail.com>
2021-07-16 14:49:58 -0400

    Checkpoint

--- helpers/s3.py
+++ helpers/s3.py
@@ -9,6 +9,7 @@ import datetime
 import functools
 import logging
 import os
+import pprint
 from typing import Any, Dict, List, Optional, Tuple
 
 import boto3
@@ -33,51 +34,75 @@ def _get_aws_config(file_name: str) -> configparser.RawConfigParser:
 @functools.lru_cache()
 def get_aws_credentials(
     aws_profile: Optional[str] = None,
-) -> Tuple[str, str, str]:
+    force_use_aws_profile: bool = False,
+) -> Dict[str, Optional[str]]:
     """
     Read the AWS credentials for a given profile.
 
-    :return: access_key_id, aws_secret_access_key, aws_region
+    :return: a dictionary with `access_key_id`, `aws_secret_access_key`,
+        `aws_region` and optionally `aws_session_token`
     """
-    # If the AWS credentials are passed through env vars, they override the config
-    # file.
-    # TODO(gp): AWS_DEFAULT_REGION -> AWS_REGION
-    env_vars = [
-        "AWS_ACCESS_KEY_ID",
-        "AWS_SECRET_ACCESS_KEY",
-        "AWS_DEFAULT_REGION",
-    ]
-    env_var_override = any(
-        env_var in os.environ and os.environ[env_var] != ""
-        for env_var in env_vars
-    )
+    result: Dict[str, Optional[str]] = {}
+    key_to_env_var: Dict[str, str] = {
+        "aws_access_key_id": "AWS_ACCESS_KEY_ID",
+        "aws_secret_access_key": "AWS_SECRET_ACCESS_KEY",
+        # TODO(gp): AWS_DEFAULT_REGION -> AWS_REGION?
+        "aws_region": "AWS_DEFAULT_REGION",
+    }
+    if force_use_aws_profile:
+        env_var_override = False
+    else:
+        # If the AWS credentials are passed through env vars, they override the
+        # config file.
+        # Check if one env var is defined.
+        env_var_override = any(
+            env_var in os.environ and os.environ[env_var] != ""
+            for env_var in key_to_env_var.keys()
+        )
     if env_var_override:
         _LOG.warning("Using AWS credentials from env vars")
         # If one variable is defined all should be defined.
-        for env_var in env_vars:
+        for key, env_var in key_to_env_var.items():
             _LOG.debug("'%s' in env vars=%s", env_var, env_var in os.environ)
             _LOG.debug(
                 "'%s' != ''=%s", env_var, os.environ.get(env_var, None) != ""
             )
             dbg.dassert_in(env_var, os.environ)
-        return tuple(os.environ[env_var] for env_var in env_vars)  # type: ignore
-    aws_profile = aws_profile or "am"
-    _LOG.debug("Getting credentials for aws_profile=%s", aws_profile)
-    # > more ~/.aws/credentials
-    # [am]
-    # aws_access_key_id=AKI...
-    # aws_secret_access_key=mhg..
-    file_name = "credentials"
-    config = _get_aws_config(file_name)
-    aws_access_key_id = config.get(aws_profile, "aws_access_key_id")
-    aws_secret_access_key = config.get(aws_profile, "aws_secret_access_key")
-    # > more ~/.aws/config
-    # [am]
-    # region = us-east-1
-    file_name = "config"
-    config = _get_aws_config(file_name)
-    aws_region = config.get(aws_profile, "region")
-    return aws_access_key_id, aws_secret_access_key, aws_region
+            result[key] = os.environ[env_var]
+        # TODO(gp): We don't pass this through env var for now.
+        result["aws_session_token"] = None
+    else:
+        aws_profile = aws_profile or "am"
+        _LOG.debug("Getting credentials for aws_profile='%s'", aws_profile)
+        # > more ~/.aws/credentials
+        # [am]
+        # aws_access_key_id=AKI...
+        # aws_secret_access_key=mhg..
+        # aws_session_token = Fwo...
+        file_name = "credentials"
+        config = _get_aws_config(file_name)
+        #
+        key = "aws_access_key_id"
+        result[key] = config.get(aws_profile, key)
+        #
+        key = "aws_secret_access_key"
+        result[key] = config.get(aws_profile, key)
+        #
+        key = "aws_session_token"
+        if config.has_option(aws_profile, key):
+            result[key] = config.get(aws_profile, key)
+        else:
+            result[key] = None
+        # > more ~/.aws/config
+        # [am]
+        # region = us-east-1
+        file_name = "config"
+        config = _get_aws_config(file_name)
+        key = "aws_region"
+        result[key] = config.get(aws_profile, "region")
+    #
+    dbg.dassert_is_subset(key_to_env_var.keys(), result.keys())
+    return result
 
 
 def get_bucket() -> str:
@@ -143,43 +168,40 @@ def get_fsx_root_path() -> str:
 # #############################################################################
 
 
-def _get_boto3_resource(aws_profile: Optional[str] = None) -> boto3.resource:
-    aws_access_key_id, aws_secret_access_key, aws_region = get_aws_credentials(
-        aws_profile=aws_profile
-    )
+def _get_boto3_resource(*args: Any, **kwargs: Any) -> boto3.resource:
+    aws_credentials = get_aws_credentials(*args, **kwargs)
+    # _LOG.debug("%s", pprint.pformat(aws_credentials))
     s3 = boto3.resource(
         "s3",
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name=aws_region,
+        aws_access_key_id=aws_credentials["aws_access_key_id"],
+        aws_secret_access_key=aws_credentials["aws_secret_access_key"],
+        region_name=aws_credentials["aws_region"],
     )
     return s3
 
 
-def _get_boto3_client(aws_profile: Optional[str] = None) -> boto3.client:
-    aws_access_key_id, aws_secret_access_key, aws_region = get_aws_credentials(
-        aws_profile=aws_profile
-    )
+def _get_boto3_client(*args: Any, **kwargs: Any) -> boto3.client:
+    aws_credentials = get_aws_credentials(*args, **kwargs)
+    # _LOG.debug("%s", pprint.pformat(aws_credentials))
     s3 = boto3.client(
         "s3",
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name=aws_region,
+        aws_access_key_id=aws_credentials["aws_access_key_id"],
+        aws_secret_access_key=aws_credentials["aws_secret_access_key"],
+        region_name=aws_credentials["aws_region"],
     )
     return s3
 
 
-def get_s3fs(aws_profile: Optional[str] = None) -> s3fs.core.S3FileSystem:
+def get_s3fs(*args: Any, **kwargs: Any) -> s3fs.core.S3FileSystem:
     # From https://stackoverflow.com/questions/62562945
-    aws_access_key_id, aws_secret_access_key, aws_region = get_aws_credentials(
-        aws_profile=aws_profile
-    )
-    _LOG.debug("aws_region=%s", aws_region)
+    aws_credentials = get_aws_credentials(*args, **kwargs)
+    _LOG.debug("%s", pprint.pformat(aws_credentials))
     s3 = s3fs.core.S3FileSystem(
         anon=False,
-        key=aws_access_key_id,
-        secret=aws_secret_access_key,
-        client_kwargs={"region_name": aws_region},
+        key=aws_credentials["aws_access_key_id"],
+        secret=aws_credentials["aws_secret_access_key"],
+        token=aws_credentials["aws_session_token"],
+        client_kwargs={"region_name": aws_credentials["aws_region"]},
     )
     return s3
 
