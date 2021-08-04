08b3053213ad9f02989567dda2cf52c9ae5a65c2
saggese <saggese@gmail.com>
2021-08-04 08:53:37 -0400

    Select only one test

--- helpers/lib_tasks.py
+++ helpers/lib_tasks.py
@@ -395,8 +395,6 @@ def git_clean(ctx, dry_run=False):  # type: ignore
     git_clean_cmd = "git clean -fd"
     if dry_run:
         git_clean_cmd += " --dry-run"
-    cmd = git_clean_cmd
-    _run(ctx, cmd)
     cmd = f"git submodule foreach '{git_clean_cmd}'"
     _run(ctx, cmd)
     # Delete other files.
