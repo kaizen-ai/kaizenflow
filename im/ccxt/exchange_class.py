a33a9a3fc8cead54ea4906d26b8284c03a082e36
danya <notextinhere@gmail.com>
2021-09-05 22:28:57 +0200

    CMTask17: Factor out credentials loading, add comments

--- im/ccxt/exchange_class.py
+++ im/ccxt/exchange_class.py
@@ -10,24 +10,36 @@ class CCXTExchange:
     def __init__(self, exchange_id: str, api_keys_path: Optional[str]) -> None:
         """
         Create a class for accessing ccxt exchange data.
+
         :param: exchange_id: ccxt exchange id
-        :param: api_keys_path: path to json
+        :param: api_keys_path: path to json file with API credentials
         """
         self.exchange_id = exchange_id
         self.api_keys_path = api_keys_path or API_KEYS_PATH
         self.exchange = self.log_into_exchange()
 
+    def load_api_credentials(self):
+        """
+        Load JSON file with available ccxt credentials
+        :return: JSON file with API credentials
+        """
+        dbg.dassert_file_extension(self.api_keys_path, "json")
+        all_credentials = hio.from_json(self.api_keys_path)
+        return all_credentials
+
     def log_into_exchange(self):
         """
         Log into exchange via ccxt.
         """
-        # TODO (Danya): Update path to JSON API keys, factor out into separate loading function.
-        all_credentials = hio.from_json(self.api_keys_path)
+        # Load all exchange credentials.
+        all_credentials = self.load_api_credentials()
         dbg.dassert_in(self.exchange_id, all_credentials, msg="'%s' exchange ID is incorrect.")
+        # Select credentials for provided exchange.
         credentials = all_credentials[self.exchange_id]
         # Enable rate limit.
         credentials["rateLimit"] = True
         exchange_class = getattr(ccxt, self.exchange_id)
+        # Create a `ccxt` exchange class.
         exchange = exchange_class(credentials)
         dbg.dassert(exchange.checkRequiredCredentials(), msg="Required credentials not passed")
         return exchange
