7ca96dc47792cd6853802ced9053a2dd6f5bae17
paul <smith.paul.anthony@gmail.com>
2021-09-16 10:19:00 -0400

    Update tests

--- core/dataflow/nodes/test/test_transformers.py
+++ core/dataflow/nodes/test/test_transformers.py
@@ -744,6 +744,58 @@ datetime,MN0,MN1,MN0,MN1
         return df
 
 
+class TestSeriesToSeriesTransformer2(hut.TestCase):
+    def test1(self) -> None:
+        """
+        Test `fit()` call.
+        """
+        data = self._get_data()
+        config = cconfig.get_config_from_nested_dict(
+            {
+                "in_col_group": ("close",),
+                "out_col_group": ("ret_0",),
+                "transformer_func": lambda x: x.pct_change(),
+            }
+        )
+        node = cdnt.SeriesToSeriesTransformer("sklearn", **config.to_dict())
+        df_out = node.fit(data)["df_out"]
+        df_str = hut.convert_df_to_string(df_out.round(3), index=True, decimals=3)
+        self.check_string(df_str)
+
+    def test2(self) -> None:
+        """
+        Test `predict()` call.
+        """
+        data = self._get_data()
+        config = cconfig.get_config_from_nested_dict(
+            {
+                "in_col_group": ("close",),
+                "out_col_group": ("ret_0",),
+                "transformer_func": lambda x: x.pct_change(),
+            }
+        )
+        node = cdnt.SeriesToSeriesTransformer("sklearn", **config.to_dict())
+        expected, actual = cdnth.get_fit_predict_outputs(data, node)
+        self.assert_equal(actual, expected)
+
+    def _get_data(self) -> pd.DataFrame:
+        """
+        Generate multivariate normal returns.
+        """
+        mn_process = casgen.MultivariateNormalProcess()
+        mn_process.set_cov_from_inv_wishart_draw(dim=4, seed=342)
+        realization = mn_process.generate_sample(
+            {"start": "2000-01-01", "periods": 40, "freq": "B"}, seed=134
+        )
+        realization = realization.rename(columns=lambda x: "MN" + str(x))
+        realization = np.exp(0.1 * realization.cumsum())
+        volume = pd.DataFrame(
+            index=realization.index, columns=realization.columns, data=100
+        )
+        data = pd.concat([realization, volume], axis=1, keys=["close", "volume"])
+        return data
+
+
 class TestFunctionWrapper(hut.TestCase):
     def test1(self) -> None:
         """
