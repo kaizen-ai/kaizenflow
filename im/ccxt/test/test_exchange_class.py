063146a3c5c9e3feb752ecf573f2514cfca651b2
DanY <dyachmenev@nes.ru>
2021-09-16 09:40:16 +0200

    CMTask33: implement requested changes

--- im/ccxt/test/test_exchange_class.py
+++ im/ccxt/test/test_exchange_class.py
@@ -5,56 +5,37 @@ import pandas as pd
 import pytest
 
 import helpers.unit_test as hut
-# TODO(Dan): return to code after https://github.com/cryptomtc/cmamp/issues/43 is fixed.
+# TODO(Dan): return to code after CmTask43 is fixed.
 # import im.ccxt.exchange_class as iccexcl
 
 _LOG = logging.getLogger(__name__)
 
 
 class Test_CCXTExchange(hut.TestCase):
-    @staticmethod
-    def _conduct_asserts(
-        actual: Any,
-        exp_n_rows: int,
-        # TODO(Dan): return to code after https://github.com/cryptomtc/cmamp/issues/43 is fixed.
-        # exchange_class: Optional[iccexcl.CCXTExchange] = None,
-        exp_first_date: Optional[str] = None,
-        exp_last_date: Optional[str] = None,
-    ) -> None:
-        # Assert that the output is a dataframe and assert its size.
-        dbg.dassert_isinstance(actual, pd.DataFrame)
-        dbg.dassert_eq(tuple([exp_n_rows, 6]), actual.shape)
-        # Assert column names.
-        exp_col_names = ["timestamp", "open", "high", "close", "volume"]
-        dbg.dassert_eq(list(actual.columns), exp_col_names)
-        # Assert corner datetimes if output is not empty.
-        # TODO(Dan): return to code after https://github.com/cryptomtc/cmamp/issues/43 is fixed.
-        # if not actual.empty:
-            # exchange = exchange_class._exchange
-            # first_date = exchange.iso8601(int(actual["timestamp"].iloc[0]))
-            # last_date = exchange.iso8601(int(actual["timestamp"].iloc[-1]))
-            # dbg.dassert_eq(exp_first_date, first_date)
-            # dbg.dassert_eq(exp_last_date, last_date)
+    @pytest.mark.skip()
+    def test_initialize_class(self) -> None:
+        """
+        Smoke test that the class is being initialized correctly.
+        """
+        _ = iccexcl.CCXTExchange("binance")
 
     @pytest.mark.skip()
     def test_get_exchange_currencies(self) -> None:
         """
         Test that a non-empty list of exchange currencies is loaded.
         """
-        # Set a list of exchange ids to test and iterate over them.
-        exchange_ids = ["binance", "kucoin"]
-        for exchange_id in exchange_ids:
-            # Extract a list of currencies.
-            exchange_class = iccexcl.CCXTExchange(exchange_id)
-            curr_list = exchange_class.get_exchange_currencies()
-            # Assert that the output is a non-empty list.
-            dbg.dassert_isinstance(curr_list, List)
-            dbg.dassert_lt(0, len(curr_list))
+        # Extract a list of currencies.
+        exchange_class = iccexcl.CCXTExchange("binance")
+        curr_list = exchange_class.get_exchange_currencies()
+        # Verify that the output is a non-empty list with only string values.
+        dbg.dassert_isinstance(curr_list, list)
+        dbg.dassert_lt(0, len(curr_list))
+        dbg.dassert_eq(all(isinstance(curr, str) for curr in curr_list)
 
-    @pytest.mark.skip()
-    def test_download_ohlcv_data_binance1(self) -> None:
+    @ pytest.mark.skip()
+    def test_download_ohlcv_data(self) -> None:
         """
-        Test that Binance data is loaded correctly.
+        Test that historical data is being loaded correctly.
         """
         # Initiate class and set date parameters.
         exchange_class = iccexcl.CCXTExchange("binance")
@@ -66,124 +47,21 @@ class Test_CCXTExchange(hut.TestCase):
             end_datetime=pd.Timestamp(end_date),
             curr_symbol="BTC/USDT",
         )
-        # Conduct asserts.
-        # Last date is going beyond end date due to step value.
-        self._conduct_asserts(
-            actual, 1500, exchange_class, start_date, "2021-09-10T00:59:00Z",
-        )
+        # Verify that the output is a dataframe and verify its length.
+        dbg.dassert_isinstance(actual, pd.DataFrame)
+        dbg.dassert_eq(1500, actual.shape[0])
+        # Verify column names.
+        exp_col_names = ["timestamp", "open", "high", "close", "volume"]
+        dbg.dassert_eq(list(actual.columns), exp_col_names)
+        # Verify types inside each column.
+        col_types = [col_type.name for col_type in actual.dtypes]
+        exp_col_types = ["int64"] + ["float64"] * 5
+        dbg.dassert_eq(exp_col_types, col_types)
+        # Verify corner datetimes if output is not empty.
+        first_date = int(actual["timestamp"].iloc[0])
+        last_date = int(actual["timestamp"].iloc[-1])
+        dbg.dassert_eq(1631145600000, first_date)
+        dbg.dassert_eq(1631235540000, last_date)
         # Check the output values.
-        actual_string = hut.convert_df_to_string(actual, index=True)
+        actual_string = hut.convert_df_to_json_string(actual, n_tail=None)
         self.check_string(actual_string)
-
-    @pytest.mark.skip()
-    def test_download_ohlcv_data_binance2(self) -> None:
-        """
-        Test that Binance data is loaded correctly with non-default step.
-        """
-        # Initiate class and set date parameters.
-        exchange_class = iccexcl.CCXTExchange("binance")
-        start_date = "2021-09-09T00:00:00Z"
-        end_date = "2021-09-10T00:00:00Z"
-        # Extract data.
-        actual = exchange_class.download_ohlcv_data(
-            start_datetime=pd.Timestamp(start_date),
-            end_datetime=pd.Timestamp(end_date),
-            curr_symbol="BTC/USDT",
-            step=1000,
-        )
-        # Conduct asserts.
-        # Last date is going beyond end date due to step value.
-        self._conduct_asserts(
-            actual, 2000, exchange_class, start_date, "2021-09-10T09:19:00Z",
-        )
-        # Check the output values.
-        actual_string = hut.convert_df_to_string(actual, index=True)
-        self.check_string(actual_string)
-
-    @pytest.mark.skip()
-    def test_download_ohlcv_data_binance3(self) -> None:
-        """
-        Test that empty dataframe is loaded for period with no data.
-        """
-        # Initiate class.
-        exchange_class = iccexcl.CCXTExchange("binance")
-        # Set dates of a period with no data.
-        start_date = "2011-01-09T00:00:00Z"
-        end_date = "2011-01-10T00:00:00Z"
-        # Extract data.
-        actual = exchange_class.download_ohlcv_data(
-            start_datetime=pd.Timestamp(start_date),
-            end_datetime=pd.Timestamp(end_date),
-            curr_symbol="BTC/USDT",
-        )
-        # Conduct asserts.
-        # Last date is going beyond end date due to step value.
-        self._conduct_asserts(actual, 0)
-
-    @pytest.mark.skip()
-    def test_download_ohlcv_data_kucoin1(self) -> None:
-        """
-        Test that Kucoin data is loaded correctly.
-        """
-        # Initiate class and set date parameters.
-        exchange_class = iccexcl.CCXTExchange("kucoin")
-        start_date = "2021-09-09T00:00:00Z"
-        end_date = "2021-09-10T00:00:00Z"
-        # Extract data.
-        actual = exchange_class.download_ohlcv_data(
-            start_datetime=pd.Timestamp(start_date),
-            end_datetime=pd.Timestamp(end_date),
-            curr_symbol="BTC/USDT",
-        )
-        # Conduct asserts.
-        # Last date is going beyond end date due to step value.
-        self._conduct_asserts(
-            actual, 1500, exchange_class, start_date, "2021-09-10T00:59:00Z",
-        )
-        # Check the output values.
-        actual_string = hut.convert_df_to_string(actual, index=True)
-        self.check_string(actual_string)
-
-    @pytest.mark.skip()
-    def test_download_ohlcv_data_kucoin2(self) -> None:
-        """
-        Test that Kucoin data is loaded correctly with non-default step.
-        """
-        # Initiate class and set date parameters.
-        exchange_class = iccexcl.CCXTExchange("kucoin")
-        start_date = "2021-09-09T00:00:00Z"
-        end_date = "2021-09-10T00:00:00Z"
-        # Extract data.
-        actual = exchange_class.download_ohlcv_data(
-            start_datetime=pd.Timestamp(start_date),
-            end_datetime=pd.Timestamp(end_date),
-            curr_symbol="BTC/USDT",
-            step=1000,
-        )
-        # Conduct asserts.
-        # Last date is going beyond end date due to step value.
-        self._conduct_asserts(
-            actual, 2000, exchange_class, start_date, "2021-09-10T09:19:00Z",
-        )
-        # Check the output values.
-        actual_string = hut.convert_df_to_string(actual, index=True)
-        self.check_string(actual_string)
-
-    @pytest.mark.skip()
-    def test_download_ohlcv_data_kucoin3(self) -> None:
-        """
-        Test that empty dataframe is loaded for period with no data.
-        """
-        # Initiate class.
-        exchange_class = iccexcl.CCXTExchange("kucoin")
-        # Set dates of a period with no data.
-        start_date = "2011-01-09T00:00:00Z"
-        end_date = "2011-01-10T00:00:00Z"
-        # Extract data.
-        actual = exchange_class.download_ohlcv_data(
-            start_datetime=pd.Timestamp(start_date),
-            end_datetime=pd.Timestamp(end_date),
-            curr_symbol="BTC/USDT",
-        )
-        # Conduct asserts.
-        self._conduct_asserts(actual, 0)
