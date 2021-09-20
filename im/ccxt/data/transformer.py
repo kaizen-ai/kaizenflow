b32c6bb3bb00fac3b0010bcec04d14c56f56109e
danya <notextinhere@gmail.com>
2021-09-20 17:06:11 +0200

    CMTask56: Sketch `transform` class method

--- im/ccxt/data/transformer.py
+++ im/ccxt/data/transformer.py
@@ -1,15 +1,24 @@
 import pandas as pd
 import im.common.data.types as vcdtyp
 
+_LOG = logging.getLogger(__name__)
+
 
 class CcxtTransformer:
     def __init__(self) -> None:
         return
-
-    def transform(self,
+    @classmethod
+    def transform(cls,
                   data: pd.DataFrame,
-                  exchange: str
+                  exchange: str,
+                  currency: str,
                   frequency: vcdtyp.Frequency):
-    
+        """
+        Transform IB data loaded from S3 to load to SQL.
 
-    return transformed_data
+        :param df: dataframe with data from S3
+        :param trade_symbol_id: symbol id in SQL database
+        :param frequency: dataframe frequency
+        :return: processed dataframe
+        """
+        return transformed_data
