0af1300faac6cec8ce628177dc9118a8a389e449
danya <notextinhere@gmail.com>
2021-09-20 16:28:17 +0200

    CMTask56: Create class skeleton

---
+++ im/ccxt/data/transformer.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import im.common.data.types as vcdtyp
+
+
+class CcxtTransformer:
+    def __init__(self) -> None:
+        return
+
+    def transform(self,
+                  data: pd.DataFrame,
+                  exchange: str
+                  frequency: vcdtyp.Frequency):
+    
+
+    return transformed_data
