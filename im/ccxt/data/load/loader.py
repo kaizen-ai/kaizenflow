f1eadeeee556dd0c00912e37f2a35a8a606ce7eb
PomazkinG <pomazking@gmail.com>
2021-09-21 15:50:36 +0200

    init commit

--- im/ccxt/data/load/loader.py
+++ im/ccxt/data/load/loader.py
@@ -1,18 +1,63 @@
 import logging
+import os
 
 import pandas as pd
 
 import helpers.dbg as dbg
+import helpers.s3 as hs3
 
 _LOG = logging.getLogger(__name__)
 
 
+def get_file_name(exchange: str, currency: str) -> str:
+    """
+    Get name for a file with CCXT data.
+
+    File name is constructed in the following way:
+    "<exchange>_<currency1>_<currency2>.csv.gz.
+
+    :param exchange: CCXT exchange id
+    :param currency: currency pair "<currency1>/<currency2>" (e.g. "BTC/USDT")
+    :return: name for a file with CCXT data
+    """
+    file_name = f"{exchange}_{currency.replace('/', '_')}.csv.gz"
+    return file_name
+
+
 class CcxtLoader:
     """
-    Class to load CCXT data.
+    Load CCXT data.
     """
-    # TODO(*): To merge into `load_data` method.
-    #  Remove this class afterwards.
+
+    def read_data(
+        self, exchange: str, currency: str, data_type: str
+    ) -> pd.DataFrame:
+        """
+        Load data from s3 and process it.
+
+        :param exchange: CCXT exchange id
+        :param currency: currency pair (e.g. "BTC/USDT")
+        :param data_type: OHLCV or trade, bid/ask data
+        :return: processed CCXT data
+        """
+        # Get file path for a CCXT file.
+        file_name = get_file_name(exchange, currency)
+        s3_bucket = hs3.get_path()
+        file_path = os.path.join(s3_bucket, file_name)
+        # Read raw CCXT data from s3.
+        _LOG.info("Reading file '%s'...", file_path)
+        data = pd.read_csv(file_path)
+        # Apply transformation to raw data.
+        _LOG.info(
+            "Processing CCXT data for exchange='%s', currencies='%s'...",
+            exchange,
+            currency,
+        )
+        transformed_data = self._transform(
+            data, exchange, currency, data_type
+        )
+        return transformed_data
+
     def _transform(
         self, data: pd.DataFrame, exchange: str, currency: str, data_type: str
     ):
