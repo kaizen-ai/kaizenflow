f3ee24341be3312b7cf2baf28602c9e2a4d8e8b5
danya <notextinhere@gmail.com>
2021-09-21 15:53:53 +0200

    CMTask56: Fill methods with code

--- im/ccxt/data/load/loader.py
+++ im/ccxt/data/load/loader.py
@@ -1,7 +1,8 @@
 import logging
 
+import ccxt
 import pandas as pd
-
+import helpers.datetime_ as hdatet
 import helpers.dbg as dbg
 
 _LOG = logging.getLogger(__name__)
@@ -47,7 +48,21 @@ class CcxtLoader:
         - Adding exchange_id and currency_pair columns
         :return:
         """
+        dbg.dassert_container_type(data["timestamp"], container_type=None, elem_type=int)
+        transformed_data = data.copy()
+        transformed_data = transformed_data.rename({"timestamp": "epoch"}, axis=1)
+        transformed_data["timestamp"] = transformed_data["epoch"].apply(ccxt.)
+        transformed_data["exchange"] = exchange
+        transformed_data["currency_pair"] = currency
         return transformed_data
+
+    @staticmethod
+    def _convert_epochs_to_timestamp(epoch_col: pd.Series, exchange: str):
+        exchange_class = getattr(ccxt, exchange)
+        timestamp_col = epoch_col.apply(exchange_class.iso8601)
+        # Convert to timestamp.
+        timestamp_col = hdatet.to_generalized_datetime(timestamp_col)
+        return timestamp_col
 
     @staticmethod
     def _apply_ohlcv_transformation(transformed_data: pd.DataFrame):
@@ -69,4 +84,7 @@ class CcxtLoader:
              "exchange"]
         :return:
         """
+        ohlcv_columns = ["timestamp", "open", "high", "low", "close", "volume", "epoch", "currency_pair", "exchange"]
+        transformed_ohlcv = transformed_data.copy()
+        dbg.dassert_is_subset(transformed_ohlcv.columns, ohlcv_columns)
         return transformed_ohlcv
