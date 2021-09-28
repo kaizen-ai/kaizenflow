f7da6489586a75f332ce6edf8a9febd04e25495b
PomazkinG <pomazking@gmail.com>
2021-09-28 02:09:57 +0200

    prettify the install requirements

--- im/devops/install_requirements.sh
+++ im/devops/install_requirements.sh
@@ -5,20 +5,24 @@
 
 set -ex
 
-FILE_NAME="devops/docker_build/install_requirements.sh"
+FILE_NAME="devops/install_requirements.sh"
 echo "##> $FILE_NAME"
 
 echo "# Installing ${ENV_NAME}"
 
-# Install with poetry inside a venv.
+# Install with 'poetry' inside a 'venv'.
 echo "# Install with venv + poetry"
 
+# Activate the virtual environment.
 python3 -m ${ENV_NAME} /${ENV_NAME}
 source /${ENV_NAME}/bin/activate
 
+# Install 'wheel'.
 pip3 install wheel
 
+# Install the Python packages with 'poetry'
 poetry install
 
+# List the virtual environments.
 poetry env list
 
