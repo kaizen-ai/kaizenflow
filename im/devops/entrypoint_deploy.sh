dd1e7d442464af196a5eb97743680f3151800a0c
saggese <saggese@gmail.com>
2021-10-01 16:08:04 -0400

    Checkpoint

--- im/devops/entrypoint_deploy.sh
+++ im/devops/entrypoint_deploy.sh
@@ -3,21 +3,9 @@
 # Entrypoint for an app that runs together with PostgreSQL.
 #
 
+# TODO(Grisha): remove this file and use the amp entrypoint #106.
+
 set -e
-
-postgres_ready() {
-  pg_isready -d $POSTGRES_DB -p $POSTGRES_PORT -h $POSTGRES_HOST
-}
-
-echo "STAGE: $STAGE"
-echo "POSTGRES_HOST: $POSTGRES_HOST"
-echo "POSTGRES_PORT: $POSTGRES_PORT"
-
-until postgres_ready; do
-  >&2 echo 'Waiting for PostgreSQL to become available...'
-  sleep 1
-done
->&2 echo 'PostgreSQL is available'
 
 umask 000
 
