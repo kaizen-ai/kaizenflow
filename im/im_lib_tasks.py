a733122faa78e767f53e159d1d25dd0608d50a6a
PomazkinG <pomazking@gmail.com>
2021-10-07 22:11:23 +0200

    introduce stage to invoke tasks

--- im/im_lib_tasks.py
+++ im/im_lib_tasks.py
@@ -7,6 +7,7 @@ import im.im_lib_tasks as imimlitas
 """
 
 import os
+from typing import Optional
 
 from invoke import task
 
@@ -14,14 +15,16 @@ import helpers.dbg as hdbg
 import helpers.lib_tasks as hlitas
 
 
-def _get_im_docker_compose_path() -> str:
+def _get_im_docker_compose_path(stage: Optional[str] = None) -> str:
     """
-    Return path to the docker-compose file `im/devops/compose/docker-
+    Return path to the docker-compose file, e.g., `im/devops/compose/docker-
     compose.yml`.
     """
     # Get `docker-compose` file path.
     docker_compose_dir = "im/devops/compose"
     compose_file_name = "docker-compose.yml"
+    if stage is not None:
+        compose_file_name = f"docker-compose.{stage}.yml"
     docker_compose_path = os.path.join(docker_compose_dir, compose_file_name)
     # Get absolute version of a file path.
     docker_compose_abs_path = os.path.abspath(docker_compose_path)
@@ -30,14 +33,15 @@ def _get_im_docker_compose_path() -> str:
     return docker_compose_abs_path
 
 
-def _get_im_docker_cmd(cmd: str) -> str:
+def _get_im_docker_cmd(stage: str, cmd: str) -> str:
     """
     Construct `im docker-compose' command.
 
-    E.g, to run the `im/devops/set_schema_im_db.py`:
+    E.g, to run the `im/devops/set_schema_im_db.py` on the `local` stage:
     ```
     docker-compose \
         --file /app/im/devops/compose/docker-compose.yml \
+        --file /app/im/devops/compose/docker-compose.local.yml \
         run --rm app \
         im/devops/set_schema_im_db.py
     ```
@@ -49,6 +53,9 @@ def _get_im_docker_cmd(cmd: str) -> str:
     # Add `docker-compose` file path.
     docker_compose_file_path = _get_im_docker_compose_path()
     docker_cmd.append(f"--file {docker_compose_file_path}")
+    # Add stage-specific `docker-compose` override file path.
+    docker_compose_stage_file_path = _get_im_docker_compose_path(stage)
+    docker_cmd.append(f"--file {docker_compose_stage_file_path}")
     # Add `run`.
     service_name = "app"
     docker_cmd.append(f"run --rm {service_name}")
@@ -58,7 +65,7 @@ def _get_im_docker_cmd(cmd: str) -> str:
     return multiline_docker_cmd
 
 
-def _get_im_docker_down(volumes_remove: bool) -> str:
+def _get_im_docker_down(stage: str, volumes_remove: bool) -> str:
     """
     Construct `im docker-compose down' command.
 
@@ -76,6 +83,9 @@ def _get_im_docker_down(volumes_remove: bool) -> str:
     # Add `docker-compose` file path.
     docker_compose_file_path = _get_im_docker_compose_path()
     docker_compose_down.append(f"--file {docker_compose_file_path}")
+    # Add stage-specific `docker-compose` override file path.
+    docker_compose_stage_file_path = _get_im_docker_compose_path(stage)
+    docker_compose_down.append(f"--file {docker_compose_stage_file_path}")
     # Add `down` command.
     docker_compose_down.append("down")
     if volumes_remove:
@@ -86,7 +96,7 @@ def _get_im_docker_down(volumes_remove: bool) -> str:
 
 
 @task
-def im_docker_cmd(ctx, cmd):  # type: ignore
+def im_docker_cmd(ctx, stage, cmd):  # type: ignore
     """
     Execute the command `cmd` inside a container attached to the `im app`.
 
@@ -95,13 +105,13 @@ def im_docker_cmd(ctx, cmd):  # type: ignore
     """
     hdbg.dassert_ne(cmd, "")
     # Get docker cmd.
-    docker_cmd = _get_im_docker_cmd(cmd)
+    docker_cmd = _get_im_docker_cmd(stage, cmd)
     # Execute the command.
     hlitas._run(ctx, docker_cmd, pty=True)
 
 
 @task
-def im_docker_down(ctx, volumes_remove=False):  # type: ignore
+def im_docker_down(ctx, stage, volumes_remove=False):  # type: ignore
     """
     Remove containers and volumes attached to the `im app`.
 
@@ -112,6 +122,6 @@ def im_docker_down(ctx, volumes_remove=False):  # type: ignore
     :param ctx: `context` object
     """
     # Get docker down command.
-    docker_clean_up_cmd = _get_im_docker_down(volumes_remove)
+    docker_clean_up_cmd = _get_im_docker_down(stage, volumes_remove)
     # Execute the command.
     hlitas._run(ctx, docker_clean_up_cmd, pty=True)
