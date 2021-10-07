d81680d3e000ca3df04b52f70bd91e033314b53b
danya <notextinhere@gmail.com>
2021-10-07 16:00:31 +0200

    CMTask136: Checkpoint

---
+++ im/ccxt/db/insert_data.py
@@ -0,0 +1,27 @@
+"""
+Utilities for inserting data into DB table.
+
+
+"""
+
+import logging
+
+import helpers.dbg as hdbg
+import helpers.parser as hparser
+import helpers.sql as hsql
+import im.common.db.create_schema as imcodbcrsch
+import psycopg2
+from typing import List, Tuple
+
+
+def get_insert_rows_sql_command(values: List[Tuple], table_name: str):
+    """
+    Get command to insert given values into CCXT table.
+
+    :param values:
+    :param table_name:
+    :return:
+    """
+    return command
+
+_LOG = logging.getLogger(__name__)
