1af6b57a3737d98d9340eeb8c2f984bccb4fd63f
danya <notextinhere@gmail.com>
2021-10-11 15:50:38 +0200

    CMTask136: PR changes

--- im/ccxt/db/insert_data.py
+++ im/ccxt/db/insert_data.py
@@ -18,64 +18,63 @@ import helpers.sql as hsql
 _LOG = logging.getLogger(__name__)
 
 
-def copy_rows_from_buffer(
-    connection: hsql.DbConnection, rows: pd.DataFrame, table_name: str
+def _copy_rows_with_copy_from(
+    connection: hsql.DbConnection, df: pd.DataFrame, table_name: str
 ) -> None:
     """
     Copy dataframe contents into DB directly from buffer.
 
-    Works much faster for larger dataframes (>10000 rows).
+    This function works much faster for large dataframes (>10000 rows).
 
     :param connection: DB connection
-    :param rows: data to insert
+    :param df: data to insert
     :param table_name: name of the table for insertion
     """
-    hdbg.dassert(
-        table_name in hsql.get_table_names(connection),
-        "Table %s is not present in the database!",
-        table_name,
+    hdbg.dassert_in(
+        table_name, hsql.get_table_names(connection)
     )
     buffer = io.StringIO()
-    rows.to_csv(buffer, index=False, header=False)
+    df.to_csv(buffer, index=False, header=False)
     buffer.seek(0)
     cur = connection.cursor()
     cur.copy_from(buffer, table_name, header=False)
     connection.commit()
 
 
-def create_insert_query(data: pd.DataFrame, table_name: str) -> str:
+def _create_insert_query(df: pd.DataFrame, table_name: str) -> str:
     """
     Create an INSERT query.
 
-    :param data: rows to insert into DB
+    Example:
+
+    INSERT INTO ccxt_ohlcv(timestamp,open,high,low,close) VALUES %s
+
+    :param df: data to insert into DB
     :param table_name: name of the table for insertion
     :return: INSERT command
     """
-    columns = ",".join(list(data.columns))
+    columns = ",".join(list(df.columns))
     query = f"INSERT INTO {table_name}({columns}) VALUES %s"
-    _LOG.debug("Executing %s", query)
+    _LOG.debug("query=%s", query)
     return query
 
 
 def execute_insert_query(
-    connection: hsql.DbConnection, data: pd.DataFrame, table_name: str
+    connection: hsql.DbConnection, df: pd.DataFrame, table_name: str
 ) -> None:
     """
     Insert multiple rows into the database.
 
     :param connection: connection to the DB
-    :param data: rows to insert
+    :param df: data to insert
     :param table_name: name of the table for insertion
     """
-    hdbg.dassert(
-        table_name in hsql.get_table_names(connection),
-        "Table %s is not present in the database!",
-        table_name,
-    )
+    hdbg.dassert_in(
+        table_name, hsql.get_table_names(connection))
     # Transform dataframe into list of tuples.
-    values = [tuple(v) for v in data.to_numpy()]
+    values = [tuple(v) for v in df.to_numpy()]
     # Generate a query for multiple rows.
-    query = create_insert_query(data, table_name)
+    query = _create_insert_query(df, table_name)
     # Execute query for each provided row.
     cur = connection.cursor()
     extras.execute_values(cur, query, values)
