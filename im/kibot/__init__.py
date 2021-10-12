aa6a0f6392e57ea9626e6515a2ffbe4eb674e416
PomazkinG <pomazking@gmail.com>
2021-10-12 22:38:37 +0200

    fix __init__.py files

--- im/kibot/__init__.py
+++ im/kibot/__init__.py
@@ -1,18 +1,1 @@
-"""
-Specify public methods and classes.
-
-Import as: import im.kibot as ikibot
-"""
-# Types
 from im.common.data.types import *  # pylint: disable=unused-import # NOQA
-
-# Data
-#from im.kibot.data.load.kibot_s3_data_loader import *  # pylint: disable=unused-import disable=unused-variable # NOQA
-
-# Metadata
-#from im.kibot.metadata.load.adjustments import *  # pylint: disable=unused-import # NOQA
-#from im.kibot.metadata.load.contract_symbol_mapping import *  # pylint: disable=unused-import # NOQA
-#from im.kibot.metadata.load.expiry_contract_mapper import *  # pylint: disable=unused-import # NOQA
-#from im.kibot.metadata.load.kibot_metadata import *  # pylint: disable=unused-import # NOQA
-#from im.kibot.metadata.load.ticker_lists import *  # pylint: disable=unused-import # NOQA
-#from im.kibot.metadata.types import *  # pylint: disable=unused-import # NOQA
