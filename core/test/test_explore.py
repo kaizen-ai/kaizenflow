15a2accd5df7a816e3fd810d50960fb24945813f
PomazkinG <pomazking@gmail.com>
2021-10-22 01:49:52 +0200

    add tests

--- core/test/test_explore.py
+++ core/test/test_explore.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pytest
 
 import core.explore as exp
+import helpers.datetime_ as hdatetim
 import helpers.unit_test as hut
 
 _LOG = logging.getLogger(__name__)
@@ -35,3 +36,104 @@ class Test_explore1(hut.TestCase):
             + "eigvec_df=\n%s\n" % eigvec_df.to_string()
         )
         self.check_string(txt)
+
+
+class TestFilterByTime(hut.TestCase):
+    @staticmethod
+    def _get_test_data() -> pd.DataFrame:
+        """
+        Get data for testing.
+
+        :return: data for testing
+        """
+        df = pd.DataFrame({
+            "col1": [1, 2, 3, 4],
+            "col2": [
+                hdatetim.to_datetime("2018-04-05"),
+                hdatetim.to_datetime("2018-04-06"),
+                hdatetim.to_datetime("2018-04-07"),
+                hdatetim.to_datetime("2018-04-08"),
+            ]
+        })
+        df.index = pd.date_range("2017-01-01", periods=4)
+        return df
+
+    def test_filter_by_index1(self) -> None:
+        """
+        Verify that lower bound is included.
+        """
+        df = self._get_test_data()
+        lower_bound = hdatetim.to_datetime("2017-01-02")
+        upper_bound = hdatetim.to_datetime("2017-01-10")
+        actual = exp.filter_by_time(
+            df=df,
+            ts_col_name="index",
+            lower_close_interval=lower_bound,
+            upper_close_interval=upper_bound,
+        )
+        expected = df[1::]
+        self.assert_equal(actual.to_string(), expected.to_string())
+
+    def test_filter_by_index2(self) -> None:
+        """
+        Verify that upper bound is not included.
+        """
+        df = self._get_test_data()
+        lower_bound = hdatetim.to_datetime("2017-01-02")
+        upper_bound = hdatetim.to_datetime("2017-01-04")
+        actual = exp.filter_by_time(
+            df=df,
+            ts_col_name="index",
+            lower_close_interval=lower_bound,
+            upper_close_interval=upper_bound,
+        )
+        expected = df[1:3]
+        self.assert_equal(actual.to_string(), expected.to_string())
+
+    def test_filter_by_column1(self) -> None:
+        """
+        Verify that lower bound is included.
+        """
+        df = self._get_test_data()
+        lower_bound = hdatetim.to_datetime("2018-04-06")
+        upper_bound = hdatetim.to_datetime("2018-04-10")
+        actual = exp.filter_by_time(
+            df=df,
+            ts_col_name="col2",
+            lower_close_interval=lower_bound,
+            upper_close_interval=upper_bound,
+        )
+        expected = df[1::]
+        self.assert_equal(actual.to_string(), expected.to_string())
+
+    def test_filter_by_column2(self) -> None:
+        """
+        Verify that upper bound is not included.
+        """
+        df = self._get_test_data()
+        lower_bound = hdatetim.to_datetime("2018-04-06")
+        upper_bound = hdatetim.to_datetime("2018-04-08")
+        actual = exp.filter_by_time(
+            df=df,
+            ts_col_name="col2",
+            lower_close_interval=lower_bound,
+            upper_close_interval=upper_bound,
+        )
+        expected = df[1:3]
+        self.assert_equal(actual.to_string(), expected.to_string())
+
+    def test_no_intersection(self) -> None:
+        """
+        Verify that if time interval is not covered by data then empty DataFrame is returned.
+        """
+        df = self._get_test_data()
+        lower_bound = hdatetim.to_datetime("2021-04-06")
+        upper_bound = hdatetim.to_datetime("2021-04-08")
+        actual = exp.filter_by_time(
+            df=df,
+            ts_col_name="index",
+            lower_close_interval=lower_bound,
+            upper_close_interval=upper_bound,
+        )
+        self.assertEqual(actual.shape[0], 0)
+
