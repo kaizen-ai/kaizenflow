8e0606656f1b9e16842cc642bd5441283925358d
Vitalii Rakovtsii <mfthemon@gmail.com>
2021-10-26 17:48:58 +0200

    DevToolsTask288: disable failing tests

--- im/ib/data/load/test/test_file_path_generator.py
+++ im/ib/data/load/test/test_file_path_generator.py
@@ -6,6 +6,8 @@ import im.ib.data.load.test.test_file_path_generator as tfpgen
 
 import os
 
+import pytest
+
 import helpers.s3 as hs3
 import helpers.unit_test as hut
 import im.common.data.types as mcdtyp
@@ -25,6 +27,7 @@ class TestIbFilePathGenerator(hut.TestCase):
         super().setUp()
         self._file_path_generator = ifpgen.IbFilePathGenerator()
 
+    @pytest.mark.skip(msg="See alphamatic/dev_tools#288")
     def test_get_latest_symbols_file1(self) -> None:
         """
         Get the latest file with the info.
