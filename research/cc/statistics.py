d774d7c1284e6d55e1c785b99be1a75403799633
PomazkinG <pomazking@gmail.com>
2021-11-01 21:04:30 +0100

    checkpoint

--- research/cc/statistics.py
+++ research/cc/statistics.py
@@ -1,7 +1,6 @@
-from typing import List, Union
-
 import pandas as pd
 
+import core.config.config_ as ccocon
 import core.statistics as cstati
 import helpers.dbg as hdbg
 import helpers.hpandas as hpandas
@@ -9,8 +8,7 @@ import helpers.hpandas as hpandas
 
 def compute_start_end_table(
     price_data: pd.DataFrame,
-    group_by_cols: Union[str, List[str]],
-    close_price_col: str,
+    config: ccocon.Config,
 ) -> pd.DataFrame:
     """
     Compute start-end table on exchange-currency level.
@@ -28,25 +26,32 @@ def compute_start_end_table(
           as percentage
 
     :param price_data: crypto price data
-    :param group_by_cols:
-    :param close_price_col:
+    :param config:
     :return: start-end table
     """
-    # hdbg.dassert_is_subset(group_by_cols, price_data.columns)
-    hdbg.dassert_in(close_price_col, price_data.columns)
+    hdbg.dassert_in(
+        [
+            config["column_names"]["close_price"],
+            config["column_names"]["currency_pair"],
+            config["column_names"]["exchange"],
+        ],
+        price_data.columns
+    )
     # ....
     hdbg.dassert_isinstance(price_data.index, pd.DatetimeIndex)
     hpandas.dassert_monotonic_index(price_data.index)
     # ....
-    price_data[group_by_cols] = price_data[group_by_cols].fillna(method="ffill")
+    group_by_columns = [config["column_names"]["exchange"], config["column_names"]["currency_pair"]]
+    price_data[group_by_columns] = price_data[group_by_columns].fillna(method="ffill")
     # Group by........
-    price_data_grouped = price_data.groupby(group_by_cols, dropna=False)
+    price_data_grouped = price_data.groupby(group_by_columns, dropna=False)
     # Compute the stats.
     start_end_table = (
-        price_data_grouped[close_price_col]
+        price_data_grouped[config["column_names"]["close_price"]]
         .agg(
             min_timestamp="idxmin",
             max_timestamp="idxmax",
+            # Count only not NaN observations.
             n_data_points="count",
             coverage=lambda x: round((1 - cstati.compute_frac_nan(x)) * 100, 2),
         )
@@ -55,7 +60,7 @@ def compute_start_end_table(
     start_end_table["days_available"] = (
         start_end_table["max_timestamp"] - start_end_table["min_timestamp"]
     ).dt.days
-    start_end_table["avg_data_points_per_day"] = (
+    start_end_table["avg_data_points_per_day"] = round((
         start_end_table["n_data_points"] / start_end_table["days_available"]
-    )
+    ), 2)
     return start_end_table
