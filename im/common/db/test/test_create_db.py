271278c98ea8f7612ffb231923c1266e06330b9f
saggese <saggese@gmail.com>
2021-11-04 20:55:41 +0100

    Checkpoint

--- im/common/db/test/test_create_db.py
+++ im/common/db/test/test_create_db.py
@@ -15,14 +15,23 @@ class Test1(huntes.TestCase):
         """
         
         super().setUp()
-        dbname = "im_postgres_db_local"
-        host = "localhost"
-        port = 5432
-        user = "aljsdalsd"
-        password = "alsdkqoen"
+        # TODO: Find a way to pass the envfile to docker-compose.
+        # Pass --env-path ../env/local.im_db_config.env
         cmd = "sudo docker-compose --file im/devops/compose/docker-compose.yml up -d im_postgres_local"
         hsyint.system(cmd, suppress_output=False)
-        self.connection, _ = hsql.get_connection( dbname, host, user, port,
+        #
+        #dbname = "im_postgres_db_local"
+        #host = "localhost"
+        #port = 5432
+        #user = "aljsdalsd"
+        #password = "alsdkqoen"
+        # TODO: Read the env file.
+        print("Waiting...")
+        # TODO: This guy needs to accept the dbname, ...
+        imcodbuti.check_db_connection(self.connection)
+        _LOG.info("done...")
+        #
+        self.connection, _ = hsql.get_connection(dbname, host, user, port,
                 password, autocommit=True,)
 
 
@@ -38,19 +47,13 @@ class Test1(huntes.TestCase):
     def test_create_all_tables1(self):
 #        cmd = "sudo docker-compose --file im/devops/compose/docker-compose.yml up -d im_postgres_local"
 #        hsyint.system(cmd, suppress_output=False)
-
-        dbname = "im_postgres_db_local"
-        host = "localhost"
-        port = 5432
-        user = "aljsdalsd"
-        password = "alsdkqoen"
         # Create a connection to the local DB which runs on localhost on a certain port.
 
       #  connection, _ = hsql.get_connection( dbname, host, user, port,
       #          password, autocommit=True,)
-        _LOG.info("Waiting...")
-        imcodbuti.check_db_connection(self.connection)
-        _LOG.info("done...")
+        #print("Waiting...")
+        #imcodbuti.check_db_connection(self.connection)
+        #_LOG.info("done...")
         _LOG.info("before")
         _LOG.info(hsql.get_db_names(self.connection))
         _LOG.info(hsql.get_table_names(self.connection))
@@ -65,4 +68,3 @@ class Test1(huntes.TestCase):
         _LOG.info("after")
         _LOG.info(hsql.get_db_names(self.connection))
         _LOG.info(hsql.get_table_names(self.connection))
-        assert 0
