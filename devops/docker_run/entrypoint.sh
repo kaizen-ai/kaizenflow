e748b740f90f410ba180d33aee7f6d4285fa13fd
defimov <elfore@yandex.ru>
2021-11-07 17:48:48 +0100

    create and fill /etc/docker/daemon.json

--- devops/docker_run/entrypoint.sh
+++ devops/docker_run/entrypoint.sh
@@ -18,7 +18,12 @@ source devops/docker_run/setenv.sh
 # Allow working with files outside a container.
 #umask 000
 
+# Need for database tests.
+sudo mkdir /etc/docker
+sudo echo '{ "storage-driver": "vfs" }' | sudo tee -a /etc/docker/daemon.json
+
 # Start Docker Engine.
+
 sudo /etc/init.d/docker start
 sudo /etc/init.d/docker status
 
