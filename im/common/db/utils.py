8c2f68baf238904f4581c8826ec41d579fcd8fe2
defimov <elfore@yandex.ru>
2021-11-08 12:06:05 +0100

    rewrote and moved db_connection_to_str to helpers/sql.py

--- im/common/db/utils.py
+++ im/common/db/utils.py
@@ -4,37 +4,9 @@ Import as:
 import im.common.db.utils as imcodbuti
 """
 
-import logging
 import os
 
-import helpers.sql as hsql
-
 _LOG = logging.getLogger(__name__)
-
-
-def db_connection_to_str(connection: hsql.DbConnection) -> str:
-    """
-    Get database connection details using environment variables. Connection
-    details include:
-
-        - Database name
-        - Host
-        - Port
-        - Username
-        - Password
-
-    :param connection: a database connection
-    :return: database connection details
-    """
-    info = connection.info
-    txt = []
-    txt.append("dbname='%s'" % info.dbname)
-    txt.append("host='%s'" % info.host)
-    txt.append("port='%s'" % info.port)
-    txt.append("user='%s'" % info.user)
-    txt.append("password='%s'" % info.password)
-    txt = "\n".join(txt)
-    return txt
 
 
 def is_inside_im_container() -> bool:
