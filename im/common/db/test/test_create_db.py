30cfb64ee51833d447042b5bfe820bdf908fafab
defimov <elfore@yandex.ru>
2021-11-08 18:27:57 +0100

    fix

--- im/common/db/test/test_create_db.py
+++ im/common/db/test/test_create_db.py
@@ -90,10 +90,12 @@ class TestCreateDB(huntes.TestCase):
         """
         Create database 'test_db_to_remove_xqw' and removing it.
         """
-        imcodbcrdb.create_database(self.connection, new_db="test_db_to_remove_xqw")
+        db_list_initial = hsql.get_db_names(self.connection)
+        if "test_db_to_remove_xqw" not in db_list_initial
+            imcodbcrdb.create_database(self.connection, new_db="test_db_to_remove_xqw")
         imcodbcrdb.remove_database(self.connection, "test_db_to_remove_xqw")
-        db_list = hsql.get_db_names(self.connection)
-        self.assertNotIn("test_db_to_remove_xqw", db_list)
+        db_list_updated = hsql.get_db_names(self.connection)
+        self.assertNotIn("test_db_to_remove_xqw", db_list_updated)
 
     def test_remove_database_invalid(self):
         """
