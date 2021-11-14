39604186d6273634ef574a7b8c93f28e4bd99a5e
Maksim Sergeychikov <sergeychikovm@gmail.com>
2021-11-14 15:42:51 +0100

    CMTask323: Rewrite the code to add general functions

--- research/cc/notebooks/CMTask323_Exchange_volatility_analysis.py
+++ research/cc/notebooks/CMTask323_Exchange_volatility_analysis.py
@@ -16,6 +16,7 @@
 # # Imports
 
 # %%
+import logging
 import os
 
 import pandas as pd
@@ -23,10 +24,18 @@ import seaborn as sns
 from statsmodels.tsa.stattools import adfuller
 
 import core.config.config_ as ccocon
-import core.plotting as cplot
+import helpers.dbg as hdbg
+import helpers.env as henv
+import helpers.printing as hprintin
 import helpers.s3 as hs3
-import im.ccxt.data.load.loader as imccdaloloa
 import im.data.universe as imdauni
+import research.cc.statistics as rccsta
+import research.cc.volume as rccvol
+import im.ccxt.data.load.loader as imccdaloloa
+
+import core.plotting as cplot
+import numpy as np
+
 
 # %% [markdown]
 # # Config
@@ -45,7 +54,7 @@ def get_cmtask323_config() -> ccocon.Config:
     config.add_subconfig("data")
     config["data"]["data_type"] = "OHLCV"
     config["data"]["universe_version"] = "v0_3"
-    #        config["data"]["universe_version"] = "v0_1"
+#        config["data"]["universe_version"] = "v0_1"
     config["data"]["vendor"] = "CCXT"
     # Column names.
     config.add_subconfig("column_names")
@@ -64,88 +73,77 @@ print(config)
 # # Functions
 
 # %%
-def get_initial_df_with_close_prices(coins, exchange, is_notional_volume):
+def compute_volatility_for_each_coin(data, freq):
     """
-    Return DataFrame with the volume of all coins for exchange with initial timestamps
-    Parameters: list of coins, exchange name
+    Loads and transforms each (exchange-coin) dataframe to compute 18-period ema volatility.
+    Parameters: DataFrame, resampling frequency
     """
-    result = []
-    loader = imccdaloloa.CcxtLoader(
-        root_dir="s3://alphamatic-data/data", aws_profile="am"
-    )
-    for coin in coins:
-        df = loader.read_data_from_filesystem(
-            exchange_id=exchange, currency_pair=coin, data_type="OHLCV"
+    data["date"] = data.index
+    new_df = data.groupby(["currency_pair", "exchange_id", pd.Grouper(key="date", freq=freq)])["close"].last()
+    new_df = new_df.pct_change().transform(lambda x: x.ewm(span=18, adjust=False).std())
+    new_df = new_df.reset_index()
+    return new_df
+
+def daily_close(data, freq):
+    """
+    Loads and transforms each (exchange-coin) dataframe to compute volatility for the whole period.
+    Parameters: DataFrame, resampling frequency
+    """
+    data["date"] = data.index
+    new_df = data.groupby(["currency_pair", "exchange_id", pd.Grouper(key="date", freq=freq)])["close"].last()
+    new_df = new_df.reset_index()
+    return new_df
+
+def get_df_with_coin_price_volatility(data, display_plot):
+    """
+    Unifies volatility values for each coin and plot the graph.
+    Parameters: DataFrame with computed volatility, boolean value to plot the graph
+    """
+    vix_df = data.groupby(["currency_pair", pd.Grouper(key="date", freq=frequency)])["close"].mean()
+    vix_df = vix_df.to_frame()
+    vix_df.columns = ["ema_volatility"]
+    if display_plot:
+        sns.set(rc={'figure.figsize':(15,8)})
+        sns.lineplot(data=vix_df, x='date', y="ema_volatility", hue="currency_pair")
+    return vix_df
+
+def get_overall_returns_volatility(data, display_plot):
+    """
+    Unifies volatility values for each coin for the whole period and plot the barplot.
+    Parameters: DataFrame with computed volatility, boolean value to plot the graph
+    """
+    close_df = daily_close.groupby(["currency_pair", pd.Grouper(key="date", freq=frequency)])["close"].mean()
+    rets_df = close_df.groupby(["currency_pair"]).pct_change()
+    std_df = rets_df.groupby(["currency_pair"]).std()
+    if display_plot:
+        cplot.plot_barplot(
+            std_df.sort_values(ascending=False),
+            title="Volatility per coin for the whole period (1-day basis, log-scaled)",
+            figsize=[15, 7],
+            yscale="log",
         )
-        df = df[["close", "currency_pair", "exchange_id"]]
-        df["date"] = df.index
-        df = df.reset_index(drop=True)
-        result.append(df)
-    final_result = pd.concat(result)
-    return final_result
+    return std_df
 
-
-def get_df_with_price_volatility(df, freq):
-    resample = df.groupby(["currency_pair", pd.Grouper(key="date", freq=freq)])[
-        "close"
-    ].last()
-    resample = resample.to_frame()
-    resample["rets"] = resample.groupby(["currency_pair"]).close.pct_change()
-    resample["std_18_ema_simple"] = resample.groupby(["currency_pair"])[
-        "rets"
-    ].transform(lambda x: x.ewm(span=18, adjust=False).std())
-    rets_for_plot = resample.reset_index()
-    sns.set(rc={"figure.figsize": (15, 8)})
-    sns.lineplot(
-        data=rets_for_plot, x="date", y="std_18_ema_simple", hue="currency_pair"
-    )
-    return resample
-
-
-def perform_adf_test(df_daily, coin_list):
+def perform_adf_test(df_daily):
+    """
+    Performs ADF test to check the stationarity of volatility values
+    Parameters: Daily DataFrame with computed volatility
+    """
     final_result = []
+    coin_list = df_daily.reset_index()["currency_pair"].unique()
     for coin in coin_list:
-        result = pd.DataFrame()
-        df = df_daily.loc[[f"{coin}"]]
-        df[df["std_18_ema_simple"].notna()]
-        X = df[df["std_18_ema_simple"].notna()]["std_18_ema_simple"].values
-        test_result = adfuller(X)
-        result.loc[f"{coin}", "ADF Statistic"] = test_result[0]
-        result.loc[f"{coin}", "p-value"] = test_result[1]
-        #            result.loc[f"{coin}", "is_unit_root_and_non-stationary (5% sign. level)"] = (result["p-value"]>0.05)
-        final_result.append(result)
+            result = pd.DataFrame()
+            df = df_daily.loc[[f"{coin}"]]
+            df[df["ema_volatility"].notna()]
+            X = df[df["ema_volatility"].notna()]["ema_volatility"].values
+            test_result = adfuller(X)
+            result.loc[f"{coin}", "ADF Statistic"] = test_result[0]
+            result.loc[f"{coin}", "p-value"] = test_result[1]
+            final_result.append(result)
     final_result = pd.concat(final_result)
-    final_result["is_unit_root_and_non-stationary (5% sign. level)"] = (
-        final_result["p-value"] > 0.05
-    )
+    final_result["is_unit_root_and_non-stationary (5% sign. level)"] = (final_result["p-value"]>0.05)
     return final_result
 
-
-# %% [markdown]
-# # Load the data
-
-# %%
-# get the list of all coin paires for each exchange
-binance_coins = imdauni.get_trade_universe("v0_3")["CCXT"]["binance"]
-ftx_coins = imdauni.get_trade_universe("v0_3")["CCXT"]["ftx"]
-gateio_coins = imdauni.get_trade_universe("v0_3")["CCXT"]["gateio"]
-kucoin_coins = imdauni.get_trade_universe("v0_3")["CCXT"]["kucoin"]
-
-# load all the dataframes
-binance = get_initial_df_with_close_prices(
-    binance_coins, "binance", is_notional_volume=True
-)
-ftx = get_initial_df_with_close_prices(ftx_coins, "ftx", is_notional_volume=True)
-gateio = get_initial_df_with_close_prices(
-    gateio_coins, "gateio", is_notional_volume=True
-)
-kucoin = get_initial_df_with_close_prices(
-    kucoin_coins, "kucoin", is_notional_volume=True
-)
-
-# construct unique DataFrame
-prices_df = pd.concat([binance, ftx, gateio, kucoin])
-prices_df.head(3)
 
 # %% [markdown]
 # # Volatility Analysis
@@ -153,70 +151,48 @@ prices_df.head(3)
 # %% [markdown]
 # ## 1 day
 
-# %%
-ema_df_daily = get_df_with_price_volatility(prices_df, "1D")
-ema_df_daily
+# %% run_control={"marked": false}
+frequency = "1D"
+compute_daily_vix_ema = lambda data: compute_volatility_for_each_coin(data, freq=frequency)
+daily_vix_ema = rccsta.compute_stats_for_universe(config, compute_daily_vix_ema)
 
 # %%
-cplot.plot_barplot(
-    ema_df_daily.groupby(["currency_pair"])["rets"]
-    .std()
-    .sort_values(ascending=False),
-    title="Volatility per coin for the whole period (1-day basis, log-scaled)",
-    figsize=[15, 7],
-    yscale="log",
-)
+ema_df_daily = get_df_with_coin_price_volatility(daily_vix_ema, display_plot=True)
+print(ema_df_daily)
 
 # %% [markdown]
 # ## 5 min
 
 # %%
-ema_df_5min = get_df_with_price_volatility(prices_df, "5min")
-ema_df_5min
+frequency = "5min"
+compute_5min_vix_ema = lambda data: compute_volatility_for_each_coin(data, freq=frequency)
+vix_ema_5min = rccsta.compute_stats_for_universe(config, compute_5min_vix_ema)
 
 # %%
-cplot.plot_barplot(
-    ema_df_5min.groupby(["currency_pair"])["rets"]
-    .std()
-    .sort_values(ascending=False),
-    title="Volatility per coin for the whole period (5min basis, log-scaled)",
-    figsize=[15, 7],
-    yscale="log",
-)
+ema_df_5min = get_df_with_coin_price_volatility(vix_ema_5min, display_plot=True)
+print(ema_df_5min)
 
 # %% [markdown]
-# One can notice that volatility distribution across coins is pretty stable if comparing between 1-day and 5-min sampling periods.
-
-# %% [markdown]
-# ## 1min
+# ## Volatility for the whole period (1-day frequency)
 
 # %%
-# ema_df_1min = get_df_with_price_volatility(prices_df, "1min")
-# ema_df_1min
+frequency = "1D"
+compute_daily_close = lambda data: daily_close(data, freq=frequency)
+daily_close = rccsta.compute_stats_for_universe(config, compute_daily_close)
 
 # %%
-# cplot.plot_barplot(
-#    ema_df_1min.groupby(["currency_pair"])["rets"].std().sort_values(ascending=False),
-#    title="Volatility per coin for the whole period (1min basis, log-scaled)",
-#    figsize=[15, 7],
-#    yscale="log",
-# )
+std_df = get_overall_returns_volatility(daily_close, display_plot=True)
+print(std_df)
 
 # %% [markdown]
 # # Test for stationarity of volatility
 
 # %%
-coin_list = ema_df_daily.reset_index()["currency_pair"].unique()
-test_results = perform_adf_test(ema_df_daily, coin_list)
+test_results = perform_adf_test(ema_df_daily)
 test_results
 
 # %% [markdown]
 # After test results we see that __FIL/USDT__ volatility over 1-day is failed to pass the stationarity test. The graph below confirms the persistence of trend: seems like the coin was too volatile right after the listing and failed to keep the same levels during its trading lifetime.
 
 # %%
-sns.lineplot(
-    data=ema_df_daily.loc[["FIL/USDT"]].reset_index(),
-    x="date",
-    y="std_18_ema_simple",
-    hue="currency_pair",
-)
+sns.lineplot(data=ema_df_daily.loc[["FIL/USDT"]].reset_index(), x='date', y='ema_volatility', hue="currency_pair")
