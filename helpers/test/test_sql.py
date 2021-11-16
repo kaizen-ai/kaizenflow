ecd9000e28d4d860ebfcbf3061398f3eb3653493
danya <notextinhere@gmail.com>
2021-11-16 16:42:22 +0100

    CMTask445: Move credentials to attributes

--- helpers/test/test_sql.py
+++ helpers/test/test_sql.py
@@ -1,5 +1,5 @@
-import os
 import logging
+import os
 
 import pytest
 
@@ -7,21 +7,28 @@ import helpers.git as hgit
 import helpers.sql as hsql
 import helpers.system_interaction as hsyint
 import helpers.unit_test as huntes
-import im.common.db.create_db as imcodbcrdb
 
 _LOG = logging.getLogger(__name__)
 
 
 @pytest.mark.skipif(not hgit.is_amp(), reason="Only run in amp")
 class Test_sql(huntes.TestCase):
-    def setUp(self):
+    def _create_test_table(self) -> None:
+
+        query = """CREATE TABLE IF NOT EXISTS test_table(
+                    id SERIAL PRIMARY KEY,
+                    column_1 NUMERIC,
+                    column_2 VARCHAR(255) NOT NULL,
+                    )
+                    """
+
+    def setUp(self) -> None:
         """
         Initialize the test container.
         """
         super().setUp()
         self.docker_compose_file_path = os.path.join(
-            hgit.get_amp_abs_path(),
-            "im/devops/compose/docker-compose.yml"
+            hgit.get_amp_abs_path(), "im/devops/compose/docker-compose.yml"
         )
         cmd = (
             "sudo docker-compose "
@@ -29,13 +36,21 @@ class Test_sql(huntes.TestCase):
             "up -d im_postgres_local"
         )
         hsyint.system(cmd, suppress_output=False)
-        
-    def tearDown(self):
+        # Set DB credentials.
+        self.dbname = "im_postgres_db_local"
+        self.host = "localhost"
+        self.port = 5432
+        self.password = "alsdkqoen"
+        self.user = "aljsdalsd"
+
+    def tearDown(self) -> None:
         """
         Bring down the test container.
         """
-        cmd = ("sudo docker-compose "
-               f"--file {self.docker_compose_file_path} down -v")
+        cmd = (
+            "sudo docker-compose "
+            f"--file {self.docker_compose_file_path} down -v"
+        )
         hsyint.system(cmd, suppress_output=False)
 
         super().tearDown()
@@ -45,60 +60,46 @@ class Test_sql(huntes.TestCase):
         """
         Smoke test.
         """
-        #TODO(Dan3): change to env
-        dbname = "im_postgres_db_local"
-        host = "localhost"
-        port = 5432
-        hsql.wait_db_connection(dbname, port, host)
+        # TODO(Dan3): change to env
+        hsql.wait_db_connection(self.dbname, self.port, self.host)
 
     @pytest.mark.slow()
     def test_db_connection_to_tuple(self) -> None:
         """
         Verify that connection string is correct.
         """
-        dbname = "im_postgres_db_local"
-        host = "localhost"
-        port = 5432
-        password = "alsdkqoen"
-        user = "aljsdalsd"
-        hsql.wait_db_connection(dbname, port, host)
+        hsql.wait_db_connection(self.dbname, self.port, self.host)
         self.connection, _ = hsql.get_connection(
-            dbname,
-            host,
-            user,
-            port,
-            password,
+            self.dbname,
+            self.host,
+            self.user,
+            self.port,
+            self.password,
             autocommit=True,
         )
         actual_details = hsql.db_connection_to_tuple(self.connection)
-        expected = {'dbname': dbname,
-                    'host' : host,
-                    'port' : port,
-                    'user' : user,
-                    'password' :password}
-        self.assertEqual(actual_details._asdict(), expected) 
+        expected = {
+            "dbname": self.dbname,
+            "host": self.host,
+            "port": self.port,
+            "user": self.user,
+            "password": self.password,
+        }
+        self.assertEqual(actual_details._asdict(), expected)
 
     @pytest.mark.slow()
-    def test_create_database(self):
+    def test_create_database(self) -> None:
         """
         Verify that db is creating.
         """
-        dbname = "im_postgres_db_local"
-        host = "localhost"
-        port = 5432
-        password = "alsdkqoen"
-        user = "aljsdalsd"
-        hsql.wait_db_connection(dbname, port, host)
+        hsql.wait_db_connection(self.dbname, self.port, self.host)
         self.connection, _ = hsql.get_connection(
-            dbname,
-            host,
-            user,
-            port,
-            password,
+            self.dbname,
+            self.host,
+            self.user,
+            self.port,
+            self.password,
             autocommit=True,
         )
-        hsql.create_database(
-            self.connection,
-            dbname="test_db"
-        )
+        hsql.create_database(self.connection, dbname="test_db")
         self.assertIn("test_db", hsql.get_db_names(self.connection))
