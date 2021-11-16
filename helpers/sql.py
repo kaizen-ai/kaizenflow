d104a5f28b7508cdbda6a78421d22cec03dc1584
danya <notextinhere@gmail.com>
2021-11-16 15:32:25 +0100

    CMTask445: Movements from OMS PR

--- helpers/sql.py
+++ helpers/sql.py
@@ -13,22 +13,34 @@ from typing import List, NamedTuple, Optional, Tuple, Union
 import pandas as pd
 import psycopg2 as psycop
 import psycopg2.sql as psql
-
+import psycopg2.extras as extras
 
 import helpers.dbg as hdbg
+import helpers.printing as hprint
 import helpers.system_interaction as hsysinte
 import helpers.timer as htimer
 
 _LOG = logging.getLogger(__name__)
 
+# #############################################################################
+# Connection
+# #############################################################################
 
-# TODO(gp): mypy doesn't like this.
+# Invariant: keep the arguments in the interface in the same order as: host,
+#  dbname, port, user, password
+
+# TODO(gp): mypy doesn't like this. Understand why and / or inline.
 DbConnection = psycop.extensions.connection
 
+
+# TODO(gp): host, dbname, ...
 DbConnectionInfo = collections.namedtuple(
     "DbConnectionInfo", ["dbname", "host", "port", "user", "password"]
 )
 
+
+# TODO(gp): Return only the connection (CmampTask441).
+# TODO(gp): Reorg params -> host, dbname, user, port
 def get_connection(
     dbname: str,
     host: str,
@@ -40,8 +52,9 @@ def get_connection(
     """
     Create a connection and cursor for a SQL database.
     """
+    _LOG.debug(hprint.to_str("host dbname port user"))
     connection = psycop.connect(
-        dbname=dbname, host=host, user=user, port=port, password=password
+        host=host, dbname=dbname, port=port, user=user, password=password
     )
     cursor = connection.cursor()
     if autocommit:
@@ -49,20 +62,24 @@ def get_connection(
     return connection, cursor
 
 
+# TODO(gp): Return only the connection (CmampTask441).
 def get_connection_from_env_vars() -> Tuple[
     DbConnection, psycop.extensions.cursor
 ]:
     """
-    Create a SQL connection using environment variables.
+    Create a SQL connection with the information from the environment
+    variables.
     """
-    # Get environment variables
-    db_name = os.environ["POSTGRES_DB"]
+    # Get values from the environment variables.
+    # TODO(gp): -> POSTGRES_DBNAME
     host = os.environ["POSTGRES_HOST"]
-    port = int(os.environ["POSTGRES_PORT"])
+    dbname = os.environ["POSTGRES_DB"]
     user = os.environ["POSTGRES_USER"]
+    port = int(os.environ["POSTGRES_PORT"])
     password = os.environ["POSTGRES_PASSWORD"]
+    # Build the
     connection, cursor = get_connection(
-        dbname=db_name,
+        dbname=dbname,
         host=host,
         port=port,
         user=user,
@@ -77,6 +94,8 @@ def get_connection_from_string(
 ) -> Tuple[DbConnection, psycop.extensions.cursor]:
     """
     Create a connection from a string.
+
+    TODO(gp): E.g., add example
     """
     connection = psycop.connect(conn_as_str)
     cursor = connection.cursor()
@@ -85,26 +104,41 @@ def get_connection_from_string(
     return connection, cursor
 
 
+def check_db_connection(
+    host: str, dbname: str, port: int,
+) -> bool:
+    """
+    Check whether a connection to a DB exists, in a non-blocking way.
+    """
+    cmd = f"pg_isready -d {dbname} -p {port} -h {host}"
+    rc = hsysinte.system(cmd, abort_on_error=False)
+    conn_exists = rc == 0
+    return conn_exists
+
+
+# TODO(gp): Rearrange as host, dbname (instead of db_name), port.
 def wait_db_connection(
     db_name: str, port: int, host: str, timeout_in_secs: int = 10
 ) -> None:
     """
-    Verify that the database is available.
+    Wait until the database is available.
+
+    :param timeout_in_secs: secs before timing out with `RuntimeError`.
     """
     hdbg.dassert_lte(1, timeout_in_secs)
     _LOG.debug("db_name=%s, port=%s, host=%s", db_name, port, host)
-    timer = 0
+    elapsed_secs = 0
     while True:
         _LOG.info("Waiting for PostgreSQL to become available...")
-        # Note: credentials passed due to race condition.
-        cmd = f"pg_isready -d {db_name} -p {port} -h {host}"
-        rc = hsysinte.system(cmd, abort_on_error=False)
-        if rc == 0:
-            _LOG.info("PostgreSQL is available")
+        conn_exists = check_db_connection(host, db_name, port)
+        if conn_exists:
+            _LOG.info("PostgreSQL is available (after %s seconds)", elapsed_secs)
             break
-        if timer > timeout_in_secs:
-            raise RuntimeError(f"Cannot connect to db db_name: {db_name}")
-        timer += 1
+        if elapsed_secs > timeout_in_secs:
+            raise RuntimeError(
+                f"Cannot connect to db host={host} db_name={db_name} port={port}"
+            )
+        elapsed_secs += 1
         time.sleep(1)
 
 
@@ -133,7 +167,8 @@ def db_connection_to_tuple(connection: DbConnection) -> NamedTuple:
     return det
 
 
-
+# #############################################################################
+# State of the whole DB
 # #############################################################################
 
 
@@ -151,9 +186,51 @@ def get_engine_version(connection: DbConnection) -> str:
     return info
 
 
+# TODO(gp): Test / fix this.
+def get_indexes(connection: DbConnection) -> pd.DataFrame:
+    res = []
+    tables = get_table_names(connection)
+    cursor = connection.cursor()
+    for table in tables:
+        query = (
+            """SELECT * FROM pg_indexes WHERE tablename = '{table}' """.format(
+                table=table
+            )
+        )
+        cursor.execute(query)
+        z = cursor.fetchall()
+        res.append(pd.DataFrame(z))
+    tmp: pd.DataFrame = pd.concat(res)
+    tmp["index_type"] = tmp[4].apply(
+        lambda w: w.split("USING")[1].lstrip().split(" ")[0]
+    )
+    tmp.columns = [
+        "type: public/private",
+        "table_name",
+        "key_name",
+        "None",
+        "Statement",
+        "index_type",
+    ]
+    tmp["columns"] = tmp["Statement"].apply(lambda w: w.split("(")[1][:-1])
+
+    return tmp
+
+
+def disconnect_all_clients(dbname: str):
+    # From https://stackoverflow.com/questions/36502401
+    # Not sure this will work in our case, since it might kill our own connection.
+    cmd = f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{dbname}';"
+
+
+# #############################################################################
+# Database
+# #############################################################################
+
+
 def get_db_names(connection: DbConnection) -> List[str]:
     """
-    DbConnection  Return the names of the available DBs.
+    Return the names of the available DBs.
 
     E.g., ['postgres', 'rdsadmin', 'template0', 'template1']
     """
@@ -163,6 +240,53 @@ def get_db_names(connection: DbConnection) -> List[str]:
     dbs = list(zip(*cursor.fetchall()))[0]
     dbs = sorted(dbs)
     return dbs
+
+
+def create_database(
+    connection: DbConnection,
+    dbname: str,
+    overwrite: Optional[bool] = None,
+) -> None:
+    """
+    Create empty database.
+
+    :param connection: database connection
+    :param dbname: database to create
+    :param overwrite: overwrite existing database
+    """
+    _LOG.debug("connection=%s", connection)
+    with connection.cursor() as cursor:
+        if overwrite:
+            cursor.execute(
+                psql.SQL("DROP DATABASE IF EXISTS {} WITH (FORCE);").format(
+                    psql.Identifier(dbname)
+                )
+            )
+        else:
+            if dbname in get_table_names(connection):
+                raise ValueError(f"Database {dbname} already exists")
+        cursor.execute(
+            psql.SQL("CREATE DATABASE {};").format(psql.Identifier(dbname))
+        )
+
+
+def remove_database(connection: DbConnection, dbname: str) -> None:
+    """
+    Remove database in current environment.
+
+    :param connection: a database connection
+    :param dbname: database name to drop, e.g. `im_db_local`
+    """
+    # Drop database.
+    # From https://stackoverflow.com/questions/36502401
+    connection.cursor().execute(
+        psql.SQL("DROP DATABASE {} WITH (FORCE);").format(psql.Identifier(dbname))
+    )
+
+
+# #############################################################################
+# Tables
+# #############################################################################
 
 
 def get_table_names(connection: DbConnection) -> List[str]:
@@ -183,10 +307,11 @@ def get_table_names(connection: DbConnection) -> List[str]:
     return tables
 
 
+# TODO(gp): -> get_tables_size
 def get_table_size(
-    connection: DbConnection,
-    only_public: bool = True,
-    summary: bool = True,
+        connection: DbConnection,
+        only_public: bool = True,
+        summary: bool = True,
 ) -> pd.DataFrame:
     """
     Report the size of each table.
@@ -225,46 +350,50 @@ def get_table_size(
     return df
 
 
-# TODO(gp): Test / fix this.
-def get_indexes(connection: DbConnection) -> pd.DataFrame:
-    res = []
-    tables = get_table_names(connection)
-    cursor = connection.cursor()
+def head_table(
+    connection: DbConnection,
+    table: str,
+    limit: int = 5,
+) -> str:
+    """
+    Report the head of the table as str.
+    """
+    txt = []
+    query = "SELECT * FROM %s LIMIT %s " % (table, limit)
+    df = execute_query(connection, query)
+    # pd.options.display.max_columns = 1000
+    # pd.options.display.width = 130
+    txt.append(str(df))
+    txt = "\n".join(txt)
+    return txt
+
+
+def head_tables(
+        connection: DbConnection,
+        tables: Optional[List[str]] = None,
+        limit: int = 5,
+) -> str:
+    txt = []
+    if tables is None:
+        tables = get_table_names(connection)
     for table in tables:
-        query = (
-            """SELECT * FROM pg_indexes WHERE tablename = '{table}' """.format(
-                table=table
-            )
-        )
-        cursor.execute(query)
-        z = cursor.fetchall()
-        res.append(pd.DataFrame(z))
-    tmp: pd.DataFrame = pd.concat(res)
-    tmp["index_type"] = tmp[4].apply(
-        lambda w: w.split("USING")[1].lstrip().split(" ")[0]
-    )
-    tmp.columns = [
-        "type: public/private",
-        "table_name",
-        "key_name",
-        "None",
-        "Statement",
-        "index_type",
-    ]
-    tmp["columns"] = tmp["Statement"].apply(lambda w: w.split("(")[1][:-1])
-
-    return tmp
+        txt.append("\n" + "#" * 80 + "\n" + table + "\n" + "#" * 80)
+        txt_tmp = head_table(connection, table, limit=limit)
+        txt.append(txt_tmp)
+    txt = "\n".join(txt)
+    return txt
 
 
+# TODO(gp): -> get_table_columns
 def get_columns(connection: DbConnection, table_name: str) -> list:
     """
     Get column names for given table.
     """
     query = (
-        """SELECT column_name
-            FROM information_schema.columns
-            WHERE TABLE_NAME = '%s' """
-        % table_name
+            """SELECT column_name
+                FROM information_schema.columns
+                WHERE TABLE_NAME = '%s' """
+            % table_name
     )
     cursor = connection.cursor()
     cursor.execute(query)
@@ -272,41 +401,55 @@ def get_columns(connection: DbConnection, table_name: str) -> list:
     return columns
 
 
-# #############################################################################
-
-
-def create_database(
-    connection: DbConnection,
-    db: str,
-    overwrite: Optional[bool] = None,
-) -> None:
-    """
-    Create empty database.
-
-    :param connection: database connection
-    :param db: database to create
-    :param force: overwrite existing database
-    """
-    _LOG.debug("connection=%s", connection)
-    with connection.cursor() as cursor:
-        if overwrite:
-            cursor.execute(
-                psql.SQL("DROP DATABASE IF EXISTS {};").format(
-                    psql.Identifier(db)
+# TODO(gp): -> find_tables_common_columns
+def find_common_columns(
+        connection: DbConnection,
+        tables: List[str],
+        as_df: bool = False,
+) -> Union[None, pd.DataFrame]:
+    limit = 5
+    df = []
+    for i, table in enumerate(tables):
+        table = tables[i]
+        query = "SELECT * FROM %s LIMIT %s " % (table, limit)
+        df1 = execute_query(connection, query, verbose=False)
+        if df1 is None:
+            continue
+        for j in range(i + 1, len(tables)):
+            table = tables[j]
+            query = "SELECT * FROM %s LIMIT %s " % (table, limit)
+            df2 = execute_query(connection, query, verbose=False)
+            if df2 is None:
+                continue
+            common_cols = [c for c in df1 if c in df2]
+            if as_df:
+                df.append(
+                    (
+                        tables[i],
+                        tables[j],
+                        len(common_cols),
+                        " ".join(common_cols),
+                    )
                 )
-            )
-        else:
-            if db in get_table_names(connection):
-                raise ValueError(f"Database {db} already exists")
-        cursor.execute(
-            psql.SQL("CREATE DATABASE {};").format(psql.Identifier(db))
+            else:
+                print(("'%s' vs '%s'" % (tables[i], tables[j])))
+                print(
+                    ("    (%s): %s" % (len(common_cols), " ".join(common_cols)))
+                )
+    obj = None
+    if as_df:
+        obj = pd.DataFrame(
+            df, columns=["table1", "table2", "num_comm_cols", "common_cols"]
         )
+    return obj
 
 
 # #############################################################################
+# Query
+# #############################################################################
 
 
-# TODO(gp): Rename it execute_pandas_query
+# TODO(gp): -> execute_query_to_df
 def execute_query(
     connection: DbConnection,
     query: str,
@@ -349,77 +492,76 @@ def execute_query(
     return df
 
 
-def head_table(
-    connection: DbConnection,
-    table: str,
-    limit: int = 5,
-) -> str:
+# #############################################################################
+# Insert
+# #############################################################################
+
+
+def copy_rows_with_copy_from(
+    connection: DbConnection, df: pd.DataFrame, table_name: str
+) -> None:
     """
-    Report the head of the table as str.
+    Copy dataframe contents into DB directly from buffer.
+
+    This function works much faster for large dataframes (>10000 rows).
+
+    :param connection: DB connection
+    :param df: data to insert
+    :param table_name: name of the table for insertion
     """
-    txt = []
-    query = "SELECT * FROM %s LIMIT %s " % (table, limit)
-    df = execute_query(connection, query)
-    # pd.options.display.max_columns = 1000
-    # pd.options.display.width = 130
-    txt.append(str(df))
-    txt = "\n".join(txt)
-    return txt
+    # The target table needs to exist.
+    hdbg.dassert_in(table_name, hsql.get_table_names(connection))
+    # Read the data.
+    buffer = io.StringIO()
+    df.to_csv(buffer, index=False, header=False)
+    buffer.seek(0)
+    # Copy the data to the DB.
+    cur = connection.cursor()
+    cur.copy_from(buffer, table_name, sep=",")
+    # TODO(gp): CmampTask413, is this still needed because the autocommit.
+    connection.commit()
 
 
-def head_tables(
-    connection: DbConnection,
-    tables: Optional[List[str]] = None,
-    limit: int = 5,
-) -> str:
-    txt = []
-    if tables is None:
-        tables = get_table_names(connection)
-    for table in tables:
-        txt.append("\n" + "#" * 80 + "\n" + table + "\n" + "#" * 80)
-        txt_tmp = head_table(connection, table, limit=limit)
-        txt.append(txt_tmp)
-    txt = "\n".join(txt)
-    return txt
+def _create_insert_query(df: pd.DataFrame, table_name: str) -> str:
+    """
+    Create an INSERT query to insert data into a DB.
+
+    :param df: data to insert into DB
+    :param table_name: name of the table for insertion
+    :return: sql query, e.g.,
+        ```
+        INSERT INTO ccxt_ohlcv(timestamp,open,high,low,close) VALUES %s
+        ```
+    """
+    hdbg.dassert_isinstance(df, pd.DataFrame)
+    columns = ",".join(list(df.columns))
+    query = f"INSERT INTO {table_name}({columns}) VALUES %s"
+    _LOG.debug("query=%s", query)
+    return query
 
 
-def find_common_columns(
-    connection: DbConnection,
-    tables: List[str],
-    as_df: bool = False,
-) -> Union[None, pd.DataFrame]:
-    limit = 5
-    df = []
-    for i, table in enumerate(tables):
-        table = tables[i]
-        query = "SELECT * FROM %s LIMIT %s " % (table, limit)
-        df1 = execute_query(connection, query, verbose=False)
-        if df1 is None:
-            continue
-        for j in range(i + 1, len(tables)):
-            table = tables[j]
-            query = "SELECT * FROM %s LIMIT %s " % (table, limit)
-            df2 = execute_query(connection, query, verbose=False)
-            if df2 is None:
-                continue
-            common_cols = [c for c in df1 if c in df2]
-            if as_df:
-                df.append(
-                    (
-                        tables[i],
-                        tables[j],
-                        len(common_cols),
-                        " ".join(common_cols),
-                    )
-                )
-            else:
-                print(("'%s' vs '%s'" % (tables[i], tables[j])))
-                print(
-                    ("    (%s): %s" % (len(common_cols), " ".join(common_cols)))
-                )
-    obj = None
-    if as_df:
-        obj = pd.DataFrame(
-            df, columns=["table1", "table2", "num_comm_cols", "common_cols"]
-        )
-    return obj
+def execute_insert_query(
+    connection: DbConnection, obj: Union[pd.DataFrame, pd.Series], table_name: str
+) -> None:
+    """
+    Insert a DB as multiple rows into the database.
+
+    :param connection: connection to the DB
+    :param df: data to insert
+    :param table_name: name of the table for insertion
+    """
+    if isinstance(obj, pd.Series):
+        df = obj.to_frame().T
+    else:
+        df = obj
+    hdbg.dassert_isinstance(df, pd.DataFrame)
+    hdbg.dassert_in(table_name, get_table_names(connection))
+    _LOG.debug("df=\n%s", hprint.dataframe_to_str(df))
+    # Transform dataframe into list of tuples.
+    values = [tuple(v) for v in df.to_numpy()]
+    # Generate a query for multiple rows.
+    query = _create_insert_query(df, table_name)
+    # Execute query for each provided row.
+    cur = connection.cursor()
+    extras.execute_values(cur, query, values)
+    connection.commit()
