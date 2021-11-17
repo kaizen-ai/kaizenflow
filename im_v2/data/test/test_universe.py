e802585513feda53edce148d405bb216c7e7d998
PomazkinG <pomazking@gmail.com>
2021-11-17 16:19:31 +0100

    change short_import

--- im_v2/data/test/test_universe.py
+++ im_v2/data/test/test_universe.py
@@ -1,5 +1,5 @@
 import helpers.unit_test as hunitest
-import im_v2.data.universe as imtwodauni
+import im_v2.data.universe as imdatuniv
 
 
 class TestFilterVendorUniverseAsTuples(hunitest.TestCase):
@@ -9,16 +9,16 @@ class TestFilterVendorUniverseAsTuples(hunitest.TestCase):
         """
         exchange_ids = ["binance", "kucoin"]
         currency_pairs = ["BTC/USDT", "ETH/USDT", "FIL/USDT"]
-        ccxt_universe = imtwodauni.get_vendor_universe_as_tuples("v03", "CCXT")
-        actual = imtwodauni.filter_vendor_universe_as_tuples(
+        ccxt_universe = imdatuniv.get_vendor_universe_as_tuples("v03", "CCXT")
+        actual = imdatuniv.filter_vendor_universe_as_tuples(
             ccxt_universe, exchange_ids, currency_pairs
         )
         expected = [
-            imtwodauni.ExchangeCurrencyTuple("binance", "BTC/USDT"),
-            imtwodauni.ExchangeCurrencyTuple("binance", "ETH/USDT"),
-            imtwodauni.ExchangeCurrencyTuple("kucoin", "BTC/USDT"),
-            imtwodauni.ExchangeCurrencyTuple("kucoin", "ETH/USDT"),
-            imtwodauni.ExchangeCurrencyTuple("kucoin", "FIL/USDT"),
+            imdatuniv.ExchangeCurrencyTuple("binance", "BTC/USDT"),
+            imdatuniv.ExchangeCurrencyTuple("binance", "ETH/USDT"),
+            imdatuniv.ExchangeCurrencyTuple("kucoin", "BTC/USDT"),
+            imdatuniv.ExchangeCurrencyTuple("kucoin", "ETH/USDT"),
+            imdatuniv.ExchangeCurrencyTuple("kucoin", "FIL/USDT"),
         ]
         self.assert_equal(str(actual), str(expected))
 
@@ -28,11 +28,11 @@ class TestGetUniverse(hunitest.TestCase):
         """
         A smoke test to verify that universe loads correctly.
         """
-        _ = imtwodauni.get_trade_universe()
+        _ = imdatuniv.get_trade_universe()
 
     def test_get_universe2(self) -> None:
         """
         Verify that incorrect universe name is recognized.
         """
         with self.assertRaises(AssertionError):
-            _ = imtwodauni.get_trade_universe("non-existent")
+            _ = imdatuniv.get_trade_universe("non-existent")
--- im_v2/data/universe.py
+++ im_v2/data/universe.py
@@ -1,7 +1,7 @@
 """
 Import as:
 
-import im_v2.data.universe as imtwodauni
+import im_v2.data.universe as imdatuniv
 """
 
 import collections
