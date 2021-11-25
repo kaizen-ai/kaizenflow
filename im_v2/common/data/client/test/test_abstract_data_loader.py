e536317944125ceed4b7af3f914b299455d80d43
PomazkinG <pomazking@gmail.com>
2021-11-25 21:16:40 +0100

    add some tests

--- im_v2/common/data/client/test/test_abstract_data_loader.py
+++ im_v2/common/data/client/test/test_abstract_data_loader.py
@@ -3,6 +3,37 @@ import helpers.unit_test as hunitest
 import im_v2.common.data.client.abstract_data_loader as imvcdcadlo
 
 
-class TestGetFilePath(hunitest.TestCase):
-    def test1(self):
-        # TODO(Grisha): add tests.
+class TestDassertIsFullSymbolValid(hunitest.TestCase):
+    def test1(self) -> None:
+        full_symbol = "binance::BTC_USDT"
+        imvcdcadlo.dassert_is_full_symbol_valid(full_symbol)
+
+    def test2(self) -> None:
+        full_symbol = "binance::BTC/USDT"
+        with self.assertRaises(AssertionError):
+            imvcdcadlo.dassert_is_full_symbol_valid(full_symbol)
+
+    def test3(self) -> None:
+        full_symbol = "bi nance::BTC_USDT"
+        with self.assertRaises(AssertionError):
+            imvcdcadlo.dassert_is_full_symbol_valid(full_symbol)
+
+    def test4(self) -> None:
+        full_symbol = "bi1nance::BTC2USDT"
+        with self.assertRaises(AssertionError):
+            imvcdcadlo.dassert_is_full_symbol_valid(full_symbol)
+
+    def test5(self) -> None:
+        full_symbol = ""
+        with self.assertRaises(AssertionError):
+            imvcdcadlo.dassert_is_full_symbol_valid(full_symbol)
+
+    def test6(self) -> None:
+        full_symbol = 123
+        with self.assertRaises(AssertionError):
+            imvcdcadlo.dassert_is_full_symbol_valid(full_symbol)
+
+    def test7(self) -> None:
+        full_symbol = "binance;;BTC_USDT"
+        with self.assertRaises(AssertionError):
+            imvcdcadlo.dassert_is_full_symbol_valid(full_symbol)
