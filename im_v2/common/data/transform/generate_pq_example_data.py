1e55081e6232b700877d56a395cba23f11739715
postal_dude <cowardly.dog.solutions.oy@eesti.ee>
2021-11-25 20:21:53 +0100

    CMTask560: Update PR per review

--- im_v2/common/data/transform/generate_pq_example_data.py
+++ im_v2/common/data/transform/generate_pq_example_data.py
@@ -5,6 +5,7 @@ import im_v2.common.data.transform.generate_pq_example_data as imvcdtgped
 """
 
 import logging
+from typing import List
 
 import pandas as pd
 import pyarrow as pa
@@ -16,12 +17,15 @@ import helpers.timer as htimer
 _LOG = logging.getLogger(__name__)
 
 
-def _get_dummy_df(
-    start_date: str, end_date: str, assets: str, freq: str
+def _get_daily_df(
+    start_date: str, end_date: str, assets: List[str], freq: str
 ) -> pd.DataFrame:
     """
-    Create Pandas random data, like:
-
+    :param start_date: start of date range including start_date
+    :param end_date: end of date range excluding end_date
+    :param assets: list of desired assets
+    :param freq: frequency of steps between start and end date
+    :return: daily dataframe as presented below
     ```
                 idx asset  val1  val2
     2000-01-01    0     A    00    00
@@ -34,18 +38,18 @@ def _get_dummy_df(
     _LOG.debug("len(df_idx)=%s", len(df_idx))
     # For each asset generate random data.
     df = []
-    for idx, asset in enumerate(assets.split(",")):
+    for idx, asset in enumerate(assets):
         df_tmp = pd.DataFrame(
             {
                 "idx": idx,
                 "asset": asset,
-                "asset_for_check": asset,
-                "val1": [i for i in range(len(df_idx))],
-                "val2": [i for i in range(len(df_idx))],
+                "val1": list(range(len(df_idx))),
+                "val2": list(range(len(df_idx))),
             },
             index=df_idx,
         )
-        # drop last midnight
+        # Drop last midnight.
+        # TODO(Nikola): end_date - pd.DateOffset(days=1)
         df_tmp.drop(df_tmp.tail(1).index, inplace=True)
         _LOG.debug(hprint.df_to_short_str("df_tmp", df_tmp))
         df.append(df_tmp)
@@ -55,9 +59,17 @@ def _get_dummy_df(
     return df
 
 
-def _save_dummy_df_daily_data_as_pq(df: pd.DataFrame, dst_dir: str) -> None:
+def _save_daily_df_as_pq(df: pd.DataFrame, dst_dir: str) -> None:
     """
-    Mimics current daily parquet structure in production.
+    Mimics daily parquet structure as seen below.
+
+    ................/by_date
+    ................/by_date/date=20211230
+    ................/by_date/date=20211230/data.parquet
+    ................/by_date/date=20211231
+    ................/by_date/date=20211231/data.parquet
+    ................/by_date/date=20220101
+    ................/by_date/date=20220101/data.parquet
     """
     date_column_name = "date"
     with htimer.TimedScope(logging.DEBUG, "Create partition idxs"):
@@ -73,13 +85,13 @@ def _save_dummy_df_daily_data_as_pq(df: pd.DataFrame, dst_dir: str) -> None:
         )
 
 
-def generate(
+def generate_pq_daily_data(
     start_date: str,
     end_date: str,
-    assets: str = "A,B,C",
-    freq: str = "1H",
+    assets: List[str],
     *,
+    freq: str = "1H",
     dst_dir: str,
 ) -> None:
-    dummy_df = _get_dummy_df(start_date, end_date, assets, freq)
-    _save_dummy_df_daily_data_as_pq(dummy_df, dst_dir)
+    dummy_df = _get_daily_df(start_date, end_date, assets, freq)
+    _save_daily_df_as_pq(dummy_df, dst_dir)
--- im_v2/common/data/transform/test/test_convert_pq_by_date_to_by_asset.py
+++ im_v2/common/data/transform/test/test_convert_pq_by_date_to_by_asset.py
@@ -1,53 +1,46 @@
-import argparse
 import logging
 import os
 
 import helpers.hparquet as hparque
+import helpers.system_interaction as hsysinte
 import helpers.unit_test as hunitest
-from im_v2.common.data.transform.convert_pq_by_date_to_by_asset import _main
-from im_v2.common.data.transform.generate_pq_example_data import generate
+import im_v2.common.data.transform.generate_pq_example_data as imvcdtgped
 
 _LOG = logging.getLogger(__name__)
 
 
-# TODO(Nikola): test _parse()
-class TestPQByDateToByAsset(hunitest.TestCase):
+class TestPqByDateToByAsset1(hunitest.TestCase):
     def test_convert_pq_by_date_to_by_asset(self) -> None:
         test_dir = self.get_scratch_space()
         by_date_dir = os.path.join(test_dir, "by_date")
         by_asset_dir = os.path.join(test_dir, "by_asset")
 
-        # TODO(Nikola): properties are only visible with str()
-        #   Example: args.src_dir is returning mock object __repr__, instead str.
-        # dummy_argparse = mock.create_autospec(argparse.ArgumentParser)
-        # dummy_args = mock.MagicMock()
-        # dummy_args.configure_mock(
-        #     src_dir=by_date_dir, dst_dir=by_asset_dir, log_level="DEBUG")
-        # dummy_argparse.parse_args = lambda: dummy_args
+        assets = ["A", "B", "C"]
+        imvcdtgped.generate_pq_daily_data(
+            "2021-12-30", "2022-01-02", assets, dst_dir=by_date_dir
+        )
+        file_path = (
+            "im_v2/common/data/transform/convert_pq_by_date_to_by_asset.py"
+        )
+        cmd = (
+            f"python {file_path} --src_dir {by_date_dir} --dst_dir {by_asset_dir}"
+        )
+        hsysinte.system(cmd)
 
-        # temporary mock
-        class DummyArgs:
-            src_dir = by_date_dir
-            dst_dir = by_asset_dir
-            log_level = "DEBUG"
-
-        class DummyArgumentParser(argparse.ArgumentParser):
-            def parse_args(self) -> DummyArgs:
-                return DummyArgs()
-
-        generate("2021-12-30", "2022-01-02", dst_dir=by_date_dir)
-        # _main(dummy_argparse())
-        _main(DummyArgumentParser())
-
-        # check directory structure
-        by_date_signature = hunitest.get_dir_signature(by_date_dir, False)
-        by_asset_signature = hunitest.get_dir_signature(by_asset_dir, False)
+        # Check directory structure.
+        include_file_content = False
+        by_date_signature = hunitest.get_dir_signature(
+            by_date_dir, include_file_content
+        )
+        by_asset_signature = hunitest.get_dir_signature(
+            by_asset_dir, include_file_content
+        )
         self.check_string(by_date_signature, tag="by_date_signature")
         self.check_string(by_asset_signature, tag="by_asset_signature")
 
-        # check parquet files content
-        signatures = (
-            by_date_signature.split("\n")[1:] + by_asset_signature.split("\n")[1:]
+        # Check parquet files content.
+        signatures = by_date_signature.split("\n") + by_asset_signature.split(
+            "\n"
         )
         for signature in signatures:
             if signature.endswith(".parquet") or signature.endswith(".pq"):
