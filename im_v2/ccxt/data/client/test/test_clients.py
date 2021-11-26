0ccb0c56ac58108b1c4d2ab32d35ebe0f6db44a5
DanY <dyachmenev@nes.ru>
2021-11-26 14:27:32 +0100

    CmTask544: remove obsolete code

--- im_v2/ccxt/data/client/test/test_clients.py
+++ im_v2/ccxt/data/client/test/test_clients.py
@@ -1,19 +1,15 @@
 import os
-from typing import List
 
 import pandas as pd
 import pytest
 
-import helpers.dbg as hdbg
 import helpers.git as hgit
-import helpers.lib_tasks as hlibtask
 import helpers.s3 as hs3
 import helpers.sql as hsql
 import helpers.system_interaction as hsysinte
 import helpers.unit_test as hunitest
 import im.ccxt.db.utils as imccdbuti
 import im_v2.ccxt.data.client.clients as imcdaclcl
-import im_v2.common.universe.universe as imvcounun
 
 _AM_S3_ROOT_DIR = os.path.join(hs3.get_path(), "data")
 
@@ -184,95 +180,6 @@ class TestCcxtDbClient(hunitest.TestCase):
             ]
         )
         return test_data
-
-
-@pytest.mark.skip("Fix in #544.")
-class TestCcxtLoaderFromFileReadUniverseData(hunitest.TestCase):
-    def test1(self) -> None:
-        """
-        Test that all files from universe version are being read correctly.
-        """
-        # Initialize loader and get actual result.
-        ccxt_loader = imcdaclcl.CcxtLoaderFromFile(
-            root_dir=_AM_S3_ROOT_DIR, aws_profile="am"
-        )
-        actual = ccxt_loader.read_universe_data(
-            universe="small", data_type="OHLCV"
-        )
-        actual_json = hunitest.convert_df_to_json_string(actual)
-        self.check_string(actual_json)
-
-    @pytest.mark.slow("18 seconds.")
-    def test2(self) -> None:
-        """
-        Test that data for provided list of tuples is being read correctly.
-        """
-        # Set input universe.
-        input_universe = [
-            imvcounun.ExchangeCurrencyTuple("kucoin", "BTC_USDT"),
-            imvcounun.ExchangeCurrencyTuple("kucoin", "ETH_USDT"),
-        ]
-        # Initialize loader and get actual result.
-        ccxt_loader = imcdaclcl.CcxtLoaderFromFile(
-            root_dir=_AM_S3_ROOT_DIR, aws_profile="am"
-        )
-        actual = ccxt_loader.read_universe_data(
-            universe=input_universe, data_type="OHLCV"
-        )
-        actual_json = hunitest.convert_df_to_json_string(actual)
-        # Check output.
-        self.check_string(actual_json)
-
-    def test3(self) -> None:
-        """
-        Test that all files from small test universe are being read correctly.
-        """
-        # Initialize loader and get actual result.
-        ccxt_loader = imcdaclcl.CcxtLoaderFromFile(
-            root_dir=_AM_S3_ROOT_DIR, aws_profile="am"
-        )
-        actual = ccxt_loader.read_universe_data(
-            universe="small", data_type="OHLCV"
-        )
-        # Check output.
-        expected_length = 190046
-        expected_exchange_ids = ["gateio", "kucoin"]
-        expected_currency_pairs = ["SOL_USDT", "XRP_USDT"]
-        self._check_output(
-            actual,
-            expected_length,
-            expected_exchange_ids,
-            expected_currency_pairs,
-        )
-
-    def _check_output(
-        self,
-        actual: pd.DataFrame,
-        expected_length: int,
-        expected_exchange_ids: List[str],
-        expected_currency_pairs: List[str],
-    ) -> None:
-        """
-        Verify that actual outcome dataframe matches the expected one.
-
-        :param actual: actual outcome dataframe
-        :param expected_length: expected outcome dataframe length
-        :param expected_exchange_ids: list of expected exchange ids
-        :param expected_currency_pairs: list of expected currency pairs
-        """
-        # Check output df length.
-        self.assert_equal(str(expected_length), str(actual.shape[0]))
-        # Check unique exchange ids in the output df.
-        actual_exchange_ids = sorted(list(actual["exchange_id"].unique()))
-        self.assert_equal(str(actual_exchange_ids), str(expected_exchange_ids))
-        # Check unique currency pairs in the output df.
-        actual_currency_pairs = sorted(list(actual["currency_pair"].unique()))
-        self.assert_equal(
-            str(actual_currency_pairs), str(expected_currency_pairs)
-        )
-        # Check the output values.
-        actual_string = hunitest.convert_df_to_json_string(actual)
-        self.check_string(actual_string)
 
 
 # TODO(*): Consider to factor out the class calling in a `def _get_loader()`.
