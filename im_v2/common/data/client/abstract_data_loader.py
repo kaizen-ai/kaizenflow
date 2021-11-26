c106111a9a6d7cac1717a78315c1005b057de530
DanilYachmenev <31514660+DanilYachmenev@users.noreply.github.com>
2021-11-26 21:53:00 +0300

    Cm task544 process list of full symbols and unit test (#603)
    * CmTask544: refactor method and unit test files
    
    * CmTask544: put MultipleSymbolsClient in different file
    
    * CmTask544: fix bug and tests
    
    * CmTask544: fill missing values after resampling
    
    * CmTask544: fix slow test time marker
    
    * CmTask544: remove redundant code
    
    * CmTask544: update goldens
    
    * CmTask544: drop obsolete outcomes
    
    * CmTask544: fix imports
    
    * CmTask544: update goldens
    
    * CmTask544: remove obsolete code
    
    * CmTask544: add tests for db client
    
    * CmTask544: fix slow test markers
    
    * CmTask544: fix test and update goldens
    
    * CmTask544: implement requested changes and update goldens
    
    * CmTask544: remove string universe support and update tests
    
    * CmTask544: fix docstrings
    
    * CmTask544: fix docstrings
    
    * CmTask544: fix merge conflicts
    
    * CmTask544: remove obsolete goldens
    
    * CmTask544: add TODO
    
    * CmTask544: move tests to test_clients.py
    
    * CmTask544: implement requested nits

--- im_v2/common/data/client/abstract_data_loader.py
+++ im_v2/common/data/client/abstract_data_loader.py
@@ -7,7 +7,7 @@ import im_v2.common.data.client.abstract_data_loader as imvcdcadlo
 import abc
 import logging
 import re
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Tuple
 
 import pandas as pd
 
@@ -178,75 +178,3 @@ class AbstractImClient(abc.ABC):
             0,
             msg=f"There are {n_duplicated_rows} duplicated rows in data",
         )
-
-
-class MultipleSymbolsClient(AbstractImClient):
-    """
-    Object compatible with `AbstractImClient` interface which reads data for multiple full symbols.
-    """
-    def __init__(self, class_: AbstractImClient, mode: str) -> None:
-        """
-        :param class_: `AbstractImClient` object
-        :param mode: output mode
-            - concat: store data for multiple full symbols in one dataframe
-            - dict: store data in a dict of a type `Dict[full_symbol, data]`
-        """
-        # Store an object from `AbstractImClient`.
-        self._class = class_
-        # Specify output mode.
-        hdbg.dassert_in(mode, ("concat", "dict"))
-        self._mode = mode
-
-    @abc.abstractmethod
-    def read_data(
-        self,
-        full_symbols: Union[str, List[str]],
-        *,
-        full_symbol_col_name: str = "full_symbol",
-        start_ts: Optional[pd.Timestamp] = None,
-        end_ts: Optional[pd.Timestamp] = None,
-        **kwargs: Dict[str, Any],
-    ) -> Union[pd.DataFrame, Dict[str, pd.DataFrame]]:
-        """
-        Read data for multiple full symbols or a specified universe version.
-
-        None `start_ts` and `end_ts` means the entire period of time available.
-
-        :param full_symbols: universe version or a list of full symbols, e.g.
-            `['binance::BTC_USDT', 'kucoin::ETH_USDT']`
-        :param full_symbol_col_name: name of the column with full symbols
-        :param start_ts: the earliest date timestamp to load data for
-        :param end_ts: the latest date timestamp to load data for
-        :return: combined data for provided symbols or universe version
-        """
-        # TODO(Dan): Implement the case when `full_symbols` is string, e.g."v01".
-        # Verify that all the provided full symbols are unique.
-        hdbg.dassert_no_duplicates(full_symbols)
-        # Initialize results dict.
-        full_symbol_to_df = {}
-        for full_symbol in sorted(full_symbols):
-            # Read data for each given full symbol.
-            df = self._class.read_data(
-                full_symbol=full_symbol,
-                start_ts=start_ts,
-                end_ts=end_ts,
-                **kwargs,
-            )
-            # Insert column with full symbol to the dataframe.
-            df.insert(0, full_symbol_col_name, full_symbol)
-            # Add full symbol data to the results dict.
-            full_symbol_to_df[full_symbol] = df
-        if self._mode == "concat":
-            # Combine results dict in a dataframe if specified.
-            ret = pd.concat(full_symbol_to_df.values())
-            # Sort results dataframe by increasing index and full symbol.
-            ret = ret.sort_index().sort_values(by=full_symbol_col_name)
-        elif self._mode == "dict":
-            # Return results dict if specified.
-            ret = full_symbol_to_df
-        else:
-            raise ValueError("Invalid mode=%s", self._mode)
-        return ret
-
-    # TODO(Grisha/Dan): Decide if we want to also implement other methods of the base class.
-    # TODO(Grisha/Dan): Decide if we want to add get_start(end)_ts_available() methods.
