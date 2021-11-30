30e0af119cc5e66ba9d550b3c4d27c9c85bb4364
PomazkinG <pomazking@gmail.com>
2021-11-30 23:24:51 +0100

    add pylint disable

--- im_v2/ccxt/data/client/test/test_clients.py
+++ im_v2/ccxt/data/client/test/test_clients.py
@@ -68,7 +68,6 @@ class TestGetFilePath(hunitest.TestCase):
             )
 
 
-
 # #############################################################################
 
 
@@ -200,15 +199,17 @@ class TestCcxtDbClient(hunitest.TestCase):
                 "created_at",
             ],
             # fmt: off
+            # pylint: disable=line-too-long
             data=[
-                [1, 1631145600000, 30, 40, 50, 60, 70, "BTC_USDT", "binance", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [2, 1631145660000, 31, 41, 51, 61, 71, "BTC_USDT", "binance", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [3, 1631145720000, 32, 42, 52, 62, 72, "ETH_USDT", "binance", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [4, 1631145780000, 33, 43, 53, 63, 73, "BTC_USDT", "kucoin", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [5, 1631145840000, 34, 44, 54, 64, 74, "BTC_USDT", "binance", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [6, 1631145900000, 34, 44, 54, 64, 74, "BTC_USDT", "kucoin", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [7, 1631145960000, 34, 44, 54, 64, 74, "ETH_USDT", "binance", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
+                [1, 1631145600000, 30, 40, 50, 60, 70, "BTC_USDT", "binance", pd.Timestamp("2021-09-09")],
+                [2, 1631145660000, 31, 41, 51, 61, 71, "BTC_USDT", "binance", pd.Timestamp("2021-09-09")],
+                [3, 1631145720000, 32, 42, 52, 62, 72, "ETH_USDT", "binance", pd.Timestamp("2021-09-09")],
+                [4, 1631145780000, 33, 43, 53, 63, 73, "BTC_USDT", "kucoin", pd.Timestamp("2021-09-09")],
+                [5, 1631145840000, 34, 44, 54, 64, 74, "BTC_USDT", "binance", pd.Timestamp("2021-09-09")],
+                [6, 1631145900000, 34, 44, 54, 64, 74, "BTC_USDT", "kucoin", pd.Timestamp("2021-09-09")],
+                [7, 1631145960000, 34, 44, 54, 64, 74, "ETH_USDT", "binance", pd.Timestamp("2021-09-09")],
             ]
+            # pylint: enable=line-too-long
             # fmt: on
         )
         return test_data
@@ -465,7 +466,6 @@ class TestMultipleSymbolsCcxtFileSystemClient(hunitest.TestCase):
             self.check_string(actual_string)
 
 
-
 # #############################################################################
 
 
@@ -687,15 +687,17 @@ class TestMultipleSymbolsCcxtDbClient(hunitest.TestCase):
                 "created_at",
             ],
             # fmt: off
+            # pylint: disable=line-too-long
             data=[
-                [1, 1631145600000, 30, 40, 50, 60, 70, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [2, 1631145660000, 31, 41, 51, 61, 71, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [3, 1631145720000, 32, 42, 52, 62, 72, "SOL_USDT", "kucoin", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [4, 1631145780000, 33, 43, 53, 63, 73, "SOL_USDT", "kucoin", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [5, 1631145840000, 34, 44, 54, 64, 74, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [6, 1631145900000, 34, 44, 54, 64, 74, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
-                [7, 1631145960000, 34, 44, 54, 64, 74, "ETH_USDT", "binance", pd.Timestamp("2021-09-09")],  # pylint: disable=line-too-long
+                [1, 1631145600000, 30, 40, 50, 60, 70, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
+                [2, 1631145660000, 31, 41, 51, 61, 71, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
+                [3, 1631145720000, 32, 42, 52, 62, 72, "SOL_USDT", "kucoin", pd.Timestamp("2021-09-09")],
+                [4, 1631145780000, 33, 43, 53, 63, 73, "SOL_USDT", "kucoin", pd.Timestamp("2021-09-09")],
+                [5, 1631145840000, 34, 44, 54, 64, 74, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
+                [6, 1631145900000, 34, 44, 54, 64, 74, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
+                [7, 1631145960000, 34, 44, 54, 64, 74, "ETH_USDT", "binance", pd.Timestamp("2021-09-09")],
             ]
+            # pylint: enable=line-too-long
             # fmt: on
         )
         return test_data
