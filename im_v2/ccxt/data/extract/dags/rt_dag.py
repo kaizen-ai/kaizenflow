3767b45e9e7402942d7a1ab3f10ef11d3ef84bc4
danya <notextinhere@gmail.com>
2021-12-01 19:18:56 +0100

    CMTask525: Construct a bash command

--- im_v2/ccxt/data/extract/dags/rt_dag.py
+++ im_v2/ccxt/data/extract/dags/rt_dag.py
@@ -1,10 +1,26 @@
 import airflow
+from airflow.operators.bash import BashOperator
+import helpers.datetime_ as hdateti
 
 # Pass default parameters.
 args = {
     "dst_dir": "test/default_dir",
+    "data_type": "ccxt_ohlcv",
     "universe": "v3",
     "api_keys": "API_keys.json",
     "table_name": "ccxt_ohlcv",
 }
+
+end_datetime = hdateti.get_timestamp("UTC")
+start_datetime = hdateti.get_timestamp("UTC")
+
+bash_command = f"python im_v2/ccxt/data/extract/download_realtime.py" \
+               f"--start_datetime {start_datetime}" \
+               f"--end_datetime {end_datetime}" \
+               f"--dst_dir {args['dst_dir']}" \
+               f"--data_type {args['data_type']}" \
+               f"--table_name {args['table_name']}" \
+               f"--api_keys {args['api_keys']}" \
+               f"--universe {args['universe']}"
+
 
