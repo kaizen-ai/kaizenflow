1832f253e0d9fd044a459d72e9059bbdcc4a1d43
cryptomtc <89211724+cryptomtc@users.noreply.github.com>
2021-12-01 08:31:26 -0800

    Amp task1786 integrate 20211201 (#641)
    * Checkpoint
    
    * Checkpoint
    
    * Checkpoint
    
    * Checkpoint
    
    * Checkpoint
    
    * Fix bug
    
    * Fix bug
    
    * Fix bug
    
    * Fix bug
    
    Co-authored-by: saggese <saggese@gmail.com>

--- dev_scripts/repo_compare.sh
+++ dev_scripts/repo_compare.sh
@@ -5,10 +5,10 @@
 
 set -x
 
-GIT_CLIENT="lem1"
+GIT_CLIENT="lemonade1"
 echo "GIT_CLIENT=$GIT_CLIENT"
 
-if [[ 0 == 1 ]]; then
+if [[ 1 == 1 ]]; then
     # amp vs lm
     DIR1=$HOME/src/$GIT_CLIENT/amp
     DIR2=$HOME/src/$GIT_CLIENT
@@ -16,9 +16,13 @@ if [[ 0 == 1 ]]; then
     echo "DIR1=$DIR1"
     echo "DIR2=$DIR2"
 
-    vimdiff $DIR1/dev_scripts/go_amp.sh $DIR2/dev_scripts_lem/go_lem.sh
-    vimdiff $DIR1/dev_scripts/tmux_amp.sh $DIR2/dev_scripts_lem/tmux_lem.sh
-    vimdiff $DIR1/dev_scripts/setenv_amp.sh $DIR2/dev_scripts_lem/setenv.sh
+    if [[ 0 == 1 ]]; then
+        vimdiff $DIR1/dev_scripts/go_amp.sh $DIR2/dev_scripts_lem/go_lem.sh
+        vimdiff $DIR1/dev_scripts/tmux_amp.sh $DIR2/dev_scripts_lem/tmux_lem.sh
+        vimdiff $DIR1/dev_scripts/setenv_amp.sh $DIR2/dev_scripts_lem/setenv.sh
+    fi;
+
+    diff_to_vimdiff.py --dir1 $DIR1/.github --dir2 $DIR2/.github
 else
     # amp vs dev_tools
     DIR1=$HOME/src/$GIT_CLIENT/amp
