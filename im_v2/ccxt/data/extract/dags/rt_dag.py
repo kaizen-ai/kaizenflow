8a654a90d5a13636f45605c3bac1d768fbd36e98
danya <notextinhere@gmail.com>
2021-12-01 23:21:10 +0100

    CMTask525: Update DAG

--- im_v2/ccxt/data/extract/dags/rt_dag.py
+++ im_v2/ccxt/data/extract/dags/rt_dag.py
@@ -1,15 +1,19 @@
-import airflow
-from airflow.operators.bash import BashOperator
-import helpers.datetime_ as hdateti
 import datetime
 
+import airflow
+import pandas as pd
+from airflow.operators.bash import BashOperator
+
+import helpers.datetime_ as hdateti
 
 # Pass default parameters for the DAG.
-default_args = {"retries": 1,
-                "retry_delay": datetime.timedelta(minutes=1),
-                "email": ["d.tikhomirov@crypto-kaizen.com"],
-                "email_on_failure": True,
-                "owner": "test"}
+default_args = {
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=1),
+    "email": ["d.tikhomirov@crypto-kaizen.com"],
+    "email_on_failure": True,
+    "owner": "test",
+}
 
 with airflow.DAG(
     dag_id="realtime_ccxt",
@@ -20,7 +24,7 @@ with airflow.DAG(
     start_date=datetime.datetime.now(),
 ) as dag:
 
-    # #############################################################################
+    # #########################################################################
 
     # Pass default parameters for the script.
     script_args = {
@@ -32,18 +36,22 @@ with airflow.DAG(
     }
 
     end_datetime = hdateti.get_timestamp("UTC")
-    start_datetime = hdateti.get_timestamp("UTC")
+    start_datetime = (
+        pd.Timestamp(end_datetime) - pd.Timedelta("5 minutes")
+    ).strftime("%Y%m%d-%H%M%S")
 
     # TODO(Danya): Rewrite as a collection of PythonOperators.
-    bash_command = f"python im_v2/ccxt/data/extract/download_realtime.py" \
-                   f"--start_datetime {start_datetime}" \
-                   f"--end_datetime {end_datetime}" \
-                   f"--dst_dir {script_args['dst_dir']}" \
-                   f"--data_type {script_args['data_type']}" \
-                   f"--table_name {script_args['table_name']}" \
-                   f"--api_keys {script_args['api_keys']}" \
-                   f"--universe {script_args['universe']}"
+    bash_command = (
+        f"python im_v2/ccxt/data/extract/download_realtime.py"
+        f"--start_datetime {start_datetime}"
+        f"--end_datetime {end_datetime}"
+        f"--dst_dir {script_args['dst_dir']}"
+        f"--data_type {script_args['data_type']}"
+        f"--table_name {script_args['table_name']}"
+        f"--api_keys {script_args['api_keys']}"
+        f"--universe {script_args['universe']}"
+    )
 
-    downloading_task = BashOperator(task_id="run_script",
-                                    bash_command=bash_command,
-                                    )
+    downloading_task = BashOperator(
+        task_id="run_script", bash_command=bash_command, default_args=default_args
+    )
