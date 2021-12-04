0055780bb90c9cf749c0b091a40c7c14e55c09c2
Daniil Tikhomirov <notextinhere@gmail.com>
2021-12-04 20:00:52 +0300

    Cm task672 update command format (#677)
    * CMTask672: Change data format
    
    * CMTask672: Lint, change task id
    
    * CMTask672: Fix some bugs

--- im_v2/ccxt/data/extract/dags/rt_dag.py
+++ im_v2/ccxt/data/extract/dags/rt_dag.py
@@ -24,27 +24,25 @@ with airflow.DAG(
     # Pass default parameters for the script.
     script_args = {
         "dst_dir": "test/default_dir",
-        "data_type": "ccxt_ohlcv",
-        "universe": "v3",
+        "data_type": "ohlcv",
+        "universe": "v03",
         "api_keys": "API_keys.json",
-        "table_name": "ccxt_ohlcv",
         "period_length": "5 minutes",
     }
     # Build a bash command to execute.
-    bash_command = (
-        f"python im_v2/ccxt/data/extract/download_realtime.py"
+    bash_command = [
+        "python im_v2/ccxt/data/extract/download_realtime.py ",
         # Provide end of the time period as UTC timestamp.
-        f"--end_datetime {datetime.datetime.now(datetime.timezone.utc).strftime('%Y%m%d-%H%M%S')} "
-        f"--period_length {script_args['period_length']} "
-        f"--dst_dir {script_args['dst_dir']} "
-        f"--data_type {script_args['data_type']} "
-        f"--table_name {script_args['table_name']} "
-        f"--api_keys {script_args['api_keys']} "
-        f"--universe {script_args['universe']}"
-    )
+        f"--end_datetime {datetime.datetime.now(datetime.timezone.utc).strftime('%Y%m%d-%H%M%S')} ",
+        f"--period_length {script_args['period_length']} ",
+        f"--dst_dir {script_args['dst_dir']} ",
+        f"--data_type {script_args['data_type']} ",
+        f"--api_keys {script_args['api_keys']} ",
+        f"--universe {script_args['universe']}",
+    ]
     # Run the script.
     downloading_task = DockerOperator(
-        task_id="run_script",
+        task_id="run_ccxt_realtime",
         image="665840871993.dkr.ecr.us-east-1.amazonaws.com/cmamp:dev",
         command=bash_command,
         default_args=default_args,
