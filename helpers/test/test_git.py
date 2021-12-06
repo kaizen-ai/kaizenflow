cbff1766f4dbe5568d2ca669902da1bfe2bf6ecf
Grisha <64330743+PomazkinG@users.noreply.github.com>
2021-12-06 17:19:54 +0300

    CMTask683: fix_tests (#684)
    * init commit
    
    * mark tests as slow
    
    * mark slow the classes
    
    * disable
    
    * mark slow class
    
    * mark slow
    
    * another fix
    
    * mark the whole class
    
    * mark slow
    
    * skip if

--- helpers/test/test_git.py
+++ helpers/test/test_git.py
@@ -262,6 +262,7 @@ class Test_git_path1(hunitest.TestCase):
             )
 
 
+@pytest.mark.slow(reason="slow via gh actions #687.")
 class Test_git_modified_files1(hunitest.TestCase):
     def setUp(self) -> None:
         """
