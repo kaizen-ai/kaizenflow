11783548631fbba22f3b2cde1a67a6407e14ef27
PomazkinG <pomazking@gmail.com>
2021-12-10 18:51:19 +0100

    fix

--- core/dataflow/nodes/test/test_sarimax_models.py
+++ core/dataflow/nodes/test/test_sarimax_models.py
@@ -30,6 +30,7 @@ class TestContinuousSarimaxModel(hunitest.TestCase):
     Warning: SARIMAX can give slightly different outputs on different machines.
     """
 
+    @pytest.mark.superslow("~5 seconds.")
     def test_fit1(self) -> None:
         data = self._get_data([], [])
         config = self._get_config((1, 0, 1), (1, 0, 1, 3))
@@ -63,6 +64,7 @@ class TestContinuousSarimaxModel(hunitest.TestCase):
         # Check results.
         self._check_results(config, df_out)
 
+    @pytest.mark.superslow("~5 seconds.")
     def test_fit_no_x1(self) -> None:
         """
         Fit without providing an exogenous variable.
@@ -166,6 +168,7 @@ class TestContinuousSarimaxModel(hunitest.TestCase):
         )
         self.check_string(act)
 
+    @pytest.mark.superslow("~5 seconds.")
     def test_predict1(self) -> None:
         data = self._get_data([], [])
         data_fit = data.iloc[:70]
@@ -237,6 +240,7 @@ class TestContinuousSarimaxModel(hunitest.TestCase):
         )
         self.check_string(hunitest.convert_df_to_string(df_out, index=True))
 
+    @pytest.mark.superslow("~5 seconds.")
     def test_predict_no_x1(self) -> None:
         """
         Predict without providing an exogenous variable.
@@ -280,6 +284,7 @@ class TestContinuousSarimaxModel(hunitest.TestCase):
             df_out3.loc["2010-04-07":"2010-04-16", "ret_0_3_hat"],  # type: ignore
         )
 
+    @pytest.mark.superslow("~5 seconds.")
     def test_summary(self) -> None:
         data = self._get_data([], [])
         config = self._get_config((1, 0, 1), (1, 0, 1, 3))
