555ee1251928bf8cdc10ee4c1f29f44c59b98bd1
saggese <saggese@gmail.com>
2021-12-10 19:45:21 -0500

    Revert mistake

--- oms/test/test_oms_db.py
+++ oms/test/test_oms_db.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
-import os
-from typing import Any, Callable, List
+from typing import Any, Callable, Coroutine, List
 
 import pandas as pd
 import pytest
@@ -9,107 +8,33 @@ import pytest
 import helpers.datetime_ as hdateti
 import helpers.git as hgit
 import helpers.hasyncio as hasynci
+import helpers.hsql_test as hsqltest
 import helpers.printing as hprint
 import helpers.sql as hsql
-import helpers.system_interaction as hsysinte
-import helpers.unit_test as hunitest
 import oms.oms_db as oomsdb
 
 _LOG = logging.getLogger(__name__)
 
 
-# TODO(gp): Generalize and move this to hsql.py or hsql_test.py.
-class _TestOmsDbHelper(hunitest.TestCase):
-    """
-    This class allows to test code that interacts with DB.
+class TestOmsDbHelper(hsqltest.TestDbHelper):
+    @staticmethod
+    def _get_compose_file() -> str:
+        # TODO(gp): This information should be retrieved from oms_lib_tasks.py.
+        #  We can also use the invoke command.
+        return "oms/devops/compose/docker-compose.yml"
 
-    It creates / destroys a test DB during setup / teardown.
+    # TODO(Dan): Deprecate after #585.
+    @staticmethod
+    def _get_db_name() -> str:
+        return "oms_postgres_db_local"
 
-    A user can create a persistent local DB in the Docker container with:
-    ```
-    # Create an OMS DB inside Docker for local stage
-    docker> (cd oms; sudo docker-compose \
-        --file /app/oms/devops/compose/docker-compose.yml up \
-        -d \
-        oms_postgres_local)
-    # or
-    docker> invoke oms_docker_up
-    ```
-    and then the creation / destruction of the DB is skipped making the tests faster
-    and allowing easier debugging.
-    For this to work, tests should not assume that the DB is clean, but create tables
-    from scratch.
-    """
-
-    def setUp(self) -> None:
-        """
-        Initialize the test database inside test container.
-        """
-        _LOG.info("\n%s", hprint.frame("setUp"))
-        super().setUp()
-        # TODO(gp): Read the info from env.
-        host = "localhost"
-        dbname = "oms_postgres_db_local"
-        port = 5432
-        user = "aljsdalsd"
-        password = "alsdkqoen"
-        self.dbname = dbname
-        conn_exists = hsql.check_db_connection(
-            host, dbname, port, user, password
-        )[0]
-        if conn_exists:
-            _LOG.warning("DB is already up: skipping docker compose")
-            # Since we have found the DB already up, we assume that we need to
-            # leave it running after the tests.
-            self.bring_down_db = False
-        else:
-            # Start the service.
-            cmd = []
-            # TODO(gp): This information should be retrieved from oms_lib_tasks.py.
-            #  We can also use the invoke command.
-            self.docker_compose_file_path = os.path.join(
-                hgit.get_amp_abs_path(), "oms/devops/compose/docker-compose.yml"
-            )
-            cmd.append("sudo docker-compose")
-            cmd.append(f"--file {self.docker_compose_file_path}")
-            service = "oms_postgres_local"
-            cmd.append(f"up -d {service}")
-            cmd = " ".join(cmd)
-            hsysinte.system(cmd, suppress_output=False)
-            # Wait for the DB to be available.
-            hsql.wait_db_connection(host, dbname, port, user, password)
-            self.bring_down_db = True
-        # Save connection info.
-        self.connection = hsql.get_connection(
-            host,
-            self.dbname,
-            port,
-            user,
-            password,
-            autocommit=True,
-        )
-
-    def tearDown(self) -> None:
-        """
-        Bring down the test container.
-        """
-        _LOG.info("\n%s", hprint.frame("tearDown"))
-        if self.bring_down_db:
-            cmd = (
-                "sudo docker-compose "
-                f"--file {self.docker_compose_file_path} down -v"
-            )
-            hsysinte.system(cmd, suppress_output=False)
-        else:
-            _LOG.warning("Leaving DB up")
-        super().tearDown()
-
-
-# #############################################################################
+    @staticmethod
+    def _get_service_name() -> str:
+        return "oms_postgres_local"
 
 
 @pytest.mark.skip(reason="Run manually to clean up the DB")
-class TestOmsDbRemoveAllTables1(_TestOmsDbHelper):
+class TestOmsDbRemoveAllTables1(TestOmsDbHelper):
     """
     This is used to reset the state of the DB.
     """
@@ -158,11 +83,12 @@ def _test_create_table_helper(
 @pytest.mark.skipif(
     hgit.is_dev_tools() or hgit.is_lime(), reason="Need dind support"
 )
-class TestOmsDbSubmittedOrdersTable1(_TestOmsDbHelper):
+class TestOmsDbSubmittedOrdersTable1(TestOmsDbHelper):
     """
     Test operations on the submitted orders table.
     """
 
+    @pytest.mark.slow("9 seconds.")
     def test_create_table1(self) -> None:
         """
         Test creating the table.
@@ -243,11 +169,12 @@ def _get_row3() -> pd.Series:
 @pytest.mark.skipif(
     hgit.is_dev_tools() or hgit.is_lime(), reason="Need dind support"
 )
-class TestOmsDbAcceptedOrdersTable1(_TestOmsDbHelper):
+class TestOmsDbAcceptedOrdersTable1(TestOmsDbHelper):
     """
     Test operations on the accepted orders table.
     """
 
+    @pytest.mark.slow("8 seconds.")
     def test_create_table1(self) -> None:
         """
         Test creating the table.
@@ -258,6 +185,7 @@ class TestOmsDbAcceptedOrdersTable1(_TestOmsDbHelper):
             self, self.connection, table_name, create_table_func
         )
 
+    @pytest.mark.slow("8 seconds.")
     def test_insert1(self) -> None:
         """
         Test inserting in the table.
@@ -295,7 +223,7 @@ class TestOmsDbAcceptedOrdersTable1(_TestOmsDbHelper):
 @pytest.mark.skipif(
     hgit.is_dev_tools() or hgit.is_lime(), reason="Need dind support"
 )
-class TestOmsDbTableInteraction1(_TestOmsDbHelper):
+class TestOmsDbTableInteraction1(TestOmsDbHelper):
     """
     Test interactions through the DB.
     """
@@ -315,6 +243,7 @@ class TestOmsDbTableInteraction1(_TestOmsDbHelper):
         # Delete the table.
         hsql.remove_table(self.connection, oomsdb.ACCEPTED_ORDERS_TABLE_NAME)
 
+    @pytest.mark.slow("9 seconds.")
     def test_wait_for_table1(self) -> None:
         """
         Show that if the value doesn't show up in the DB there is a timeout.
@@ -325,6 +254,7 @@ class TestOmsDbTableInteraction1(_TestOmsDbHelper):
         with self.assertRaises(TimeoutError):
             self.wait_for_table_helper(coroutines)
 
+    @pytest.mark.slow("9 seconds.")
     def test_wait_for_table2(self) -> None:
         """
         Show that waiting on a value on the table works.
@@ -344,6 +274,7 @@ class TestOmsDbTableInteraction1(_TestOmsDbHelper):
         exp = r"""[[(3, None)], None]"""
         self.assert_equal(act, exp)
 
+    @pytest.mark.slow("9 seconds.")
     def test_wait_for_table3(self) -> None:
         """
         The data is written too late triggering a timeout.
