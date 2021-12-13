6f9ce562548aee3b4153af3179fdc417f257970c
defimov <elfore@yandex.ru>
2021-12-13 15:50:37 +0100

    fixed all except extension

--- im_v2/common/data/transform/csv_to_pq.py
+++ im_v2/common/data/transform/csv_to_pq.py
@@ -45,7 +45,7 @@ def _parse() -> argparse.ArgumentParser:
     parser.add_argument(
         "--incremental",
         action="store_true",
-        help="Skip files that have already being downloaded",
+        help="Skip files that have already being converted",
     )
     hparser.add_verbosity_arg(parser)
     return parser
@@ -56,22 +56,34 @@ def _main(parser: argparse.ArgumentParser) -> None:
     hdbg.init_logger(verbosity=args.log_level, use_exec_path=True)
     hio.create_dir(args.dst_dir, args.incremental)
     ext = (".csv", ".csv.gz")
+    # Find all the CSV files to convert.
     csv_files = [fn for fn in os.listdir(args.src_dir) if fn.endswith(ext)]
-    hdbg.dassert_ne(len(csv_files), 0,  "No .csv files inside '%s'", args.src_dir)
-    pq_filenames = []
-    for fn in os.listdir(args.dst_dir):
-        if fn.endswith(".pq"):
-            filename = fn[:-3]
-        elif fn.endswith(".parquet"):
-            filename = fn[:-8]
-        pq_filenames.append(filename)
+    hdbg.dassert_ne(len(csv_files), 0, "No .csv files inside '%s'", args.src_dir)
+    if args.incremental:
+        # Find pq files that already converted.
+        pq_filenames = []
+        for f in os.listdir(args.dst_dir):
+            if f.endswith(".pq"):
+                pq_filenames.append(f[:-3])
+            elif f.endswith(".parquet"):
+                pq_filenames.append(f[:-8])
+            else:
+                raise ValueError("Invalid file '{f}'")
+        # Remove csv files that do not need to be converted.
+        for f in csv_files:
+            filename = f[:-4] if f.endswith(".csv") else f[:-7]
+            if filename in pq_filenames:
+                csv_files.remove(f)
+                _LOG.warning(
+                    "Skipping the conversion of CSV file '%s' since '%s' already exists",
+                    f,
+                    f,
+                )
+    # Transform csv files.
     for f in csv_files:
         filename = f[:-4] if f.endswith(".csv") else f[:-7]  # for .csv.gz
-        if args.incremental:
-            if filename in pq_filenames:
-                continue
         csv_full_path = os.path.join(args.src_dir, f)
-        pq_full_path = os.path.join(args.dst_dir, f"{filename}.pq")
+        pq_full_path = os.path.join(args.dst_dir, f"{filename}.parquet")
         hcsv.convert_csv_to_pq(csv_full_path, pq_full_path)
 
 
--- im_v2/common/data/transform/test/test_csv_to_pq.py
+++ im_v2/common/data/transform/test/test_csv_to_pq.py
@@ -1,8 +1,6 @@
 import os
 
-import pytest
 import pandas as pd
-from typing import Tuple
 
 import helpers.io_ as hio
 import helpers.system_interaction as hsysinte
@@ -14,14 +12,18 @@ class TestCsvToPq(hunitest.TestCase):
         """
         Test that generated .pq file is correct.
         """
-        csv_dir_path, pq_dir_path = self._generate_files()
+        # Generate the files.
+        self._generate_files1()
+        pq_dir_path = os.path.join(self.get_scratch_space(), "pq_dir")
+        # Run command.
         cmd = (
             "im_v2/common/data/transform/csv_to_pq.py"
-            f" --src_dir {csv_dir_path}"
+            f" --src_dir {self.csv_dir_path}"
             f" --dst_dir {pq_dir_path}"
         )
         hsysinte.system(cmd)
-        df = pd.read_parquet(os.path.join(pq_dir_path, "test.pq"))
+        # Check output.
+        df = pd.read_parquet(os.path.join(pq_dir_path, "test.parquet"))
         actual = hunitest.convert_df_to_json_string(df, n_tail=None)
         self.check_string(actual)
 
@@ -29,52 +31,72 @@ class TestCsvToPq(hunitest.TestCase):
         """
         Test that --incremental option does not change the file.
         """
-        csv_dir_path, pq_dir_path = self._generate_files()
-        df = pd.read_parquet(os.path.join(pq_dir_path, "test.pq"))
+        # Generate the files.
+        self._generate_files1()
+        self._generate_files2()
+        # Read file before running command.
+        parquet_file_path = os.path.join(self.pq_dir_path, "test.parquet")
+        df = pd.read_parquet(parquet_file_path)
         before = hunitest.convert_df_to_json_string(df, n_tail=None)
+        # Run command.
         cmd = (
             "im_v2/common/data/transform/csv_to_pq.py"
-            f" --src_dir {csv_dir_path}"
-            f" --dst_dir {pq_dir_path}"
+            f" --src_dir {self.csv_dir_path}"
+            f" --dst_dir {self.pq_dir_path}"
             " --incremental"
         )
         hsysinte.system(cmd)
-        df = pd.read_parquet(os.path.join(pq_dir_path, "test.pq"))
+        # Read file after running command.
+        df = pd.read_parquet(parquet_file_path)
         after = hunitest.convert_df_to_json_string(df, n_tail=None)
+        # Check that file does not change.
         self.assert_equal(before, after)
 
-    def _generate_files(self) -> Tuple[str,str]:
+    def _generate_files1(self) -> None:
+        """
+        Create a CSV file in scratch directory.
+        """
         test_dir = self.get_scratch_space()
-        pq_dir_path = os.path.join(test_dir, "pq_dir")
-        csv_dir_path = os.path.join(test_dir, "csv_dir")
-        hio.create_dir(csv_dir_path, False)
-        hio.create_dir(pq_dir_path, False)
-        d = {        
-            'timestamp':[1638646800000,1638646860000],
-            'open':[49317.68,49330.63],
-            'high':[49346.95,49400.98],
-            'volume':[23.13681,61.99752],
-            'low':[49315.45,49322.78],
-            'close':[49330.63,49325.23],
-            'currency_pair':["BTC_USDT","BTC_USDT"],
-            'created_at':["2021-12-07 13:01:20.183463+00:00",
-            "2021-12-07 13:01:20.183463+00:00"],
-            'exchange_id':["binance","binance"]
-        }
-        d2 = {        
-            'timestamp':[1632646800000,1638646860000],
-            'open':[49666.18,49666.63],
-            'high':[49666.95,49666.98],
-            'volume':[23.12681,61.99752],
-            'low':[49666.45,49322.78],
-            'close':[49230.63,49225.23],
-            'currency_pair':["BTC_USDT","BTC_USDT"],
-            'created_at':["2021-12-07 13:01:20.183463+00:00",
-            "2021-12-07 13:01:20.183463+00:00"],
-            'exchange_id':["binance","binance"]
+        self.csv_dir_path = os.path.join(test_dir, "csv_dir")
+        hio.create_dir(self.csv_dir_path, False)
+        d = {
+            "timestamp": [1638646800000, 1638646860000],
+            "open": [49317.68, 49330.63],
+            "high": [49346.95, 49400.98],
+            "volume": [23.13681, 61.99752],
+            "low": [49315.45, 49322.78],
+            "close": [49330.63, 49325.23],
+            "currency_pair": ["BTC_USDT", "BTC_USDT"],
+            "created_at": [
+                "2021-12-07 13:01:20.183463+00:00",
+                "2021-12-07 13:01:20.183463+00:00",
+            ],
+            "exchange_id": ["binance", "binance"],
         }
         df = pd.DataFrame(data=d)
-        df.to_csv(os.path.join(csv_dir_path, "test.csv"), index=False)
-        df2 = pd.DataFrame(data=d2)
-        df2.to_parquet(os.path.join(pq_dir_path, "test.pq"), index=False)
-        return csv_dir_path, pq_dir_path
+        df.to_csv(os.path.join(self.csv_dir_path, "test.csv"), index=False)
+
+    def _generate_files2(self) -> None:
+        """
+        Create a PQ file in scratch directory.
+        """
+        test_dir = self.get_scratch_space()
+        self.pq_dir_path = os.path.join(test_dir, "pq_dir")
+        hio.create_dir(self.pq_dir_path, False)
+        # DF is different, because we want to check that it does not change.
+        d = {
+            "timestamp": [1632646800000, 1638646860000],
+            "open": [49666.18, 49666.63],
+            "high": [49666.95, 49666.98],
+            "volume": [23.12681, 61.99752],
+            "low": [49666.45, 49322.78],
+            "close": [49230.63, 49225.23],
+            "currency_pair": ["BTC_USDT", "BTC_USDT"],
+            "created_at": [
+                "2021-12-07 13:01:20.183463+00:00",
+                "2021-12-07 13:01:20.183463+00:00",
+            ],
+            "exchange_id": ["binance", "binance"],
+        }
+        df = pd.DataFrame(data=d)
+        df.to_parquet(os.path.join(self.pq_dir_path, "test.parquet"), index=False)
