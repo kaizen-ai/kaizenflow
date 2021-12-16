7eb6e02739511d10f6c3d1106edc088d1160ca0e
Nikola Jašek <n.jasek@icloud.com>
2021-12-16 16:48:29 +0000

    CMTask720: Use new abstract test helper in transform test (#743)
    * CMTask720: Update test to use json comparison
    
    * CMTask720: Use new test db helper class
    
    * CMTask720: Update docstring for reindex_on_unix_epoch
    
    * CMTask720: Use sql helper to execute queries
    
    * CMTask720: Use dotenv to obtain env vars
    
    * CMTask720: Docstring update in pandas helpers
    
    * CMTask720: Disable test without purify_text
    
    Co-authored-by: nikola.jasek <cowardly.dog.solutions.oy@eesti.ee>

--- helpers/hpandas.py
+++ helpers/hpandas.py
@@ -168,19 +168,24 @@ def drop_duplicates(
     return data_no_dups
 
 
-def reindex_on_unix_epoch(df: pd.DataFrame, in_col_name: str) -> pd.DataFrame:
+def reindex_on_unix_epoch(
+    df: pd.DataFrame, in_col_name: str, unit: str = "s"
+) -> pd.DataFrame:
     """
-    Transform the df so that `start_time` is converted into a datetime index.
-
-    `start_time` contains an Unix epoch (e.g., 1638194400) and it's
+    Transform the column `in_col_name` into a datetime index.
+    `in_col_name` contains Unix epoch (e.g., 1638194400) and it is
     converted into a UTC time.
+
+    :param df: dataframe with a unix epoch
+    :param in_col_name: column containing unix epoch
+    :param unit: the unit of unix epoch
     """
     # Convert.
     temp_col_name = in_col_name + "_tmp"
     hdbg.dassert_in(in_col_name, df.columns)
     hdbg.dassert_not_in(temp_col_name, df.columns)
     # Save.
-    df[temp_col_name] = pd.to_datetime(df[in_col_name], unit="s", utc=True)
+    df[temp_col_name] = pd.to_datetime(df[in_col_name], unit=unit, utc=True)
     df.set_index(temp_col_name, inplace=True, drop=True)
     df.index.name = None
     return df
@@ -190,9 +195,9 @@ def get_df_signature(df: pd.DataFrame, num_rows: int = 3) -> str:
     """
     Compute a simple signature of a dataframe in string format.
 
-    The signature contains metadata about dataframe size and certain amount of
-    rows from start and end of a dataframe.
-    It is used for testing purposes.
+    The signature contains metadata about dataframe size and certain
+    amount of rows from start and end of a dataframe. It is used for
+    testing purposes.
     """
     hdbg.dassert_isinstance(df, pd.DataFrame)
     txt: List[str] = []
