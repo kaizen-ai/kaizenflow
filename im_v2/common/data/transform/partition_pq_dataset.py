5360283adc457aa1b29e66934b7414ea6617b5dd
danya <notextinhere@gmail.com>
2021-12-20 17:25:12 +0100

    CMTask741: Add `convert_timestamp_column`

--- im_v2/common/data/transform/partition_pq_dataset.py
+++ im_v2/common/data/transform/partition_pq_dataset.py
@@ -17,9 +17,25 @@ import helpers.io_ as hio
 import helpers.joblib_helpers as hjoblib
 import helpers.parser as hparser
 import helpers.printing as hprint
+import pandas as pd
+
 
 _LOG = logging.getLogger(__name__)
 
+
+def convert_timestamp_column(datetime_col: pd.Series) -> pd.Series:
+    """
+
+    :param datetime_col:
+    :return:
+    """
+    if pd.api.types.is_integer_dtype(datetime_col):
+        converted_datetime_col = datetime_col.apply(hdateti.convert_unix_epoch_to_timestamp)
+    elif pd.api.types.is_string_dtype(datetime_col):
+        converted_datetime_col = hdateti.to_generalized_datetime(datetime_col)
+    else:
+        raise ValueError("Incorrect data format. Datetime column should be of integer or string type.")
+    return converted_datetime_col
 
 def _parse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
