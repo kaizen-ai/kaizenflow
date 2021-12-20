9e6275264615bb0f46c73170d552ae97a6f92f19
danya <notextinhere@gmail.com>
2021-12-20 18:54:04 +0100

    CMTask741: Finish the script

--- im_v2/common/data/transform/partition_pq_dataset.py
+++ im_v2/common/data/transform/partition_pq_dataset.py
@@ -1,35 +1,71 @@
-"""
+#!/usr/bin/env python
 
+"""
+# Partition parquet dataset by date or by asset.
+
+# Parquet dataset partitioned by date:
+```
+src_dir/
+    date=20211211/
+        data.parquet
+    date=20211212/
+        data.parquet
+```
+
+# A parquet file partitioned by assets:
+
+```
+dst_dir/
+    year=2021/
+        month=12/
+                asset=BTC_USDT/
+                    data.parquet
+                asset=ETH_USDT/
+                    data.parquet
+```
+
+# Use as:
+
+> im_v2/common/data/transform/partition_pq_dataset.py
+    --src_dir ccxt_realtime_raw_pq_example/ \
+    --dst_dir ccxt_realtime_partitioned_pq/ \
+    --by asset \
+    --datetime_col timestamp \
+    --asset_col currency_pair
 """
 
 import argparse
 import logging
 
-import helpers.datetime_ as hdateti
-import helpers.dbg as hdbg
-
 import pandas as pd
 import pyarrow as pa
 import pyarrow.dataset as ds
 import pyarrow.parquet as pq
-import helpers.parser as hparser
+
+import helpers.datetime_ as hdateti
+import helpers.dbg as hdbg
 import helpers.io_ as hio
+import helpers.parser as hparser
 
 _LOG = logging.getLogger(__name__)
 
 
+# TODO(*): Move as a general transform utility function.
 def convert_timestamp_column(datetime_col: pd.Series) -> pd.Series:
     """
-
     :param datetime_col:
     :return:
     """
     if pd.api.types.is_integer_dtype(datetime_col):
-        converted_datetime_col = datetime_col.apply(hdateti.convert_unix_epoch_to_timestamp)
+        converted_datetime_col = datetime_col.apply(
+            hdateti.convert_unix_epoch_to_timestamp
+        )
     elif pd.api.types.is_string_dtype(datetime_col):
         converted_datetime_col = hdateti.to_generalized_datetime(datetime_col)
     else:
-        raise ValueError("Incorrect data format. Datetime column should be of integer or string type.")
+        raise ValueError(
+            "Incorrect data format. Datetime column should be of integer or string type."
+        )
     return converted_datetime_col
 
 
@@ -49,7 +85,7 @@ def _parse() -> argparse.ArgumentParser:
         action="store",
         type=str,
         required=True,
-        help="Location to place partitioned parquet dataset"
+        help="Location to place partitioned parquet dataset",
     )
     parser.add_argument(
         "--by",
@@ -63,14 +99,14 @@ def _parse() -> argparse.ArgumentParser:
         action="store",
         type=str,
         required=True,
-        help="Name of column containing datetime information"
+        help="Name of column containing datetime information",
     )
     parser.add_argument(
         "--asset_col",
         action="store",
         type=str,
         default=None,
-        help="Name of column containing asset name for partitioning by asset"
+        help="Name of column containing asset name for partitioning by asset",
     )
     parser.add_argument("--incremental", action="store_true")
     hparser.add_verbosity_arg(parser)
@@ -90,7 +126,9 @@ def _main(parser: argparse.ArgumentParser) -> None:
         data["date"] = data.index.strftime("%Y%m%d")
         partition_cols = ["date"]
     elif args.by == "asset":
-        hdbg.dassert(args.asset_col, msg="Please probide the name of asset column.")
+        hdbg.dassert(
+            args.asset_col, msg="Please probide the name of asset column."
+        )
         data["year"] = data.index.year
         data["month"] = data.index.month
         partition_cols = ["year", "month", args.asset_col]
