3921cd57391b62bbd8b151b965a8a8d8d0e618db
danya <notextinhere@gmail.com>
2021-12-20 18:35:05 +0100

    CMTask741: Add partition cols

--- im_v2/common/data/transform/partition_pq_dataset.py
+++ im_v2/common/data/transform/partition_pq_dataset.py
@@ -4,21 +4,16 @@
 
 import argparse
 import logging
-import os
-from typing import Any, Dict, List
-
-import numpy as np
 
 import helpers.datetime_ as hdateti
 import helpers.dbg as hdbg
-import helpers.hpandas as hpandas
-import helpers.hparquet as hparque
-import helpers.io_ as hio
-import helpers.joblib_helpers as hjoblib
-import helpers.parser as hparser
-import helpers.printing as hprint
-import pandas as pd
 
+import pandas as pd
+import pyarrow as pa
+import pyarrow.dataset as ds
+import pyarrow.parquet as pq
+import helpers.parser as hparser
+import helpers.io_ as hio
 
 _LOG = logging.getLogger(__name__)
 
@@ -36,6 +31,7 @@ def convert_timestamp_column(datetime_col: pd.Series) -> pd.Series:
     else:
         raise ValueError("Incorrect data format. Datetime column should be of integer or string type.")
     return converted_datetime_col
+
 
 def _parse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -63,17 +59,40 @@ def _parse() -> argparse.ArgumentParser:
         help="Partition dataset by date or by asset",
     )
     parser.add_argument(
-        "--col_name",
+        "--datetime_col",
         action="store",
         type=str,
         required=True,
-        help="Name of column to parition dataset by"
+        help="Name of column containing datetime information"
     )
+    parser.add_argument(
+        "--asset_col",
+        action="store",
+        type=str,
+        default=None,
+        help="Name of column containing asset name for partitioning by asset"
+    )
+    parser.add_argument("--incremental", action="store_true")
+    hparser.add_verbosity_arg(parser)
+    return parser
 
 
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
     hdbg.init_logger(verbosity=args.log_level, use_exec_path=True)
+    hio.create_dir(args.dst_dir, incremental=args.incremental)
+    # Read files.
+    dataset = ds.dataset(args.src_dir, format="parquet", partitioning="hive")
+    data = dataset.to_table().to_pandas()
+    # Set datetime index.
+    data = data.set_index(convert_timestamp_column(args.datetime_col))
+    if args.by == "date":
+        data["date"] = data.index.strftime("%Y%m%d")
+    elif args.by == "asset":
+        data["year"] = data.index.year
+        data["month"] = data.index.month
+    else:
+        raise ValueError(f"Partition by {args.by} is not supported.")
 
 
 if __name__ == "__main__":
