000d5659774ed7becc0648531ca4a027537f017f
DanilYachmenev <31514660+DanilYachmenev@users.noreply.github.com>
2021-12-24 19:00:41 +0300

    CmTask817: Add mappings and functions to convert FullSymbols to num ids (#826)
    * CmTask817: Add mappings and functions to convert FullSymbols to numeric ids
    
    * CmTask817: refactor the approach
    
    * CmTask817: remove redundant code
    
    * CmTask817: add as_ids switch to get_vendor_universe()
    
    * CmTask817: implement requested changes
    
    * CmTask817: linter
    
    * CmTask817: fix comments and add TODO
    
    * CmTask817: implement requested nits
    
    * fix
    
    * fix
    
    * fix lints
    
    * fix todo
    
    Co-authored-by: PomazkinG <pomazking@gmail.com>
    Co-authored-by: Grisha <64330743+PomazkinG@users.noreply.github.com>

--- im_v2/ccxt/universe/test/test_universe.py
+++ im_v2/ccxt/universe/test/test_universe.py
@@ -20,9 +20,52 @@ class TestGetUniverse(hunitest.TestCase):
 class TestGetVendorUniverse(hunitest.TestCase):
     def test1(self) -> None:
         """
-        Test that universe is received correctly.
+        Test that universe as full symbols is received correctly.
         """
         universe_as_full_symbols = imvccunun.get_vendor_universe(version="small")
         self.assertEqual(len(universe_as_full_symbols), 2)
         self.assert_equal(universe_as_full_symbols[0], "gateio::XRP_USDT")
         self.assert_equal(universe_as_full_symbols[1], "kucoin::SOL_USDT")
+
+    def test2(self) -> None:
+        """
+        Test that universe as numeric ids is received correctly.
+        """
+        universe_as_numeric_ids = imvccunun.get_vendor_universe(
+            version="small", vendor="CCXT", as_ids=True
+        )
+        self.assertEqual(len(universe_as_numeric_ids), 2)
+        self.assertEqual(universe_as_numeric_ids[0], 2002879833)
+        self.assertEqual(universe_as_numeric_ids[1], 2568064341)
+
+
+class TestStringToNumId(hunitest.TestCase):
+    def test1(self) -> None:
+        """
+        Test that string id is converted to numeric correctly.
+        """
+        num_id = imvccunun.string_to_num_id("binance::BTC_USDT")
+        self.assertEqual(num_id, 1467591036)
+
+
+class TestBuildNumToStringIdMapping(hunitest.TestCase):
+    def test1(self) -> None:
+        """
+        Test that numeric to string ids mapping is being built correctly.
+        """
+        mapping = imvccunun.build_num_to_string_id_mapping(
+            ("gateio::XRP_USDT", "kucoin::SOL_USDT")
+        )
+        self.assertEqual(len(mapping), 2)
+        self.assert_equal(mapping[2002879833], "gateio::XRP_USDT")
+        self.assert_equal(mapping[2568064341], "kucoin::SOL_USDT")
+
+    def test2(self) -> None:
+        """
+        Smoke test that latest CCXT universe has no collisions in numeric ids.
+
+        Since the mapping is dict with numeric ids as keys, there are no
+        collisions if it is just built.
+        """
+        latest_universe = tuple(imvccunun.get_vendor_universe())
+        _ = imvccunun.build_num_to_string_id_mapping(latest_universe)
--- im_v2/ccxt/universe/universe.py
+++ im_v2/ccxt/universe/universe.py
@@ -3,8 +3,10 @@ Import as:
 
 import im_v2.ccxt.universe.universe as imvccunun
 """
+import functools
+import hashlib
 import os
-from typing import Dict, List
+from typing import Dict, List, Tuple, Union
 
 import helpers.dbg as hdbg
 import helpers.git as hgit
@@ -32,24 +34,74 @@ def get_trade_universe(
     return universe  # type: ignore[no-any-return]
 
 
+# TODO(Dan): remove default values for `vendor` and `as_ids` param #832.
 def get_vendor_universe(
     version: str = _LATEST_UNIVERSE_VERSION,
     vendor: str = "CCXT",
-) -> List[imvcdcadlo.FullSymbol]:
+    as_ids: bool = False,
+) -> Union[List[imvcdcadlo.FullSymbol], List[int]]:
     """
-    Load vendor universe as full symbols.
+    Load vendor universe as full symbols or numeric ids.
 
     :param version: release version
     :param vendor: vendor to load data for
-    :return: vendor universe as full symbols
+    :param as_ids: if True return universe as numeric ids, otherwise universe as full symbols
+    :return: vendor universe
     """
     # Get vendor universe.
     vendor_universe = get_trade_universe(version)[vendor]
     # Convert vendor universe dict to a sorted list of full symbols.
-    full_symbols = [
+    universe = [
         imvcdcadlo.construct_full_symbol(exchange_id, currency_pair)
         for exchange_id, currency_pairs in vendor_universe.items()
         for currency_pair in currency_pairs
     ]
-    sorted_full_symbols = sorted(full_symbols)
-    return sorted_full_symbols
+    if as_ids:
+        # Convert universe symbols to numeric ids.
+        universe_tuple = tuple(universe)
+        universe = list(build_num_to_string_id_mapping(universe_tuple).keys())
+    # Sort list of symbols in the universe.
+    universe = sorted(universe)
+    return universe
+
+
+def string_to_num_id(string_id: str) -> int:
+    """
+    Convert string id into a numeric one.
+
+    :param string_id: string id to convert
+    :return: numeric id
+    """
+    # Initialize MD5 algorithm converter and update it with string id.
+    converter = hashlib.md5()
+    converter.update(string_id.encode("utf-8"))
+    # Get hexadecimal numeric id.
+    num_id = converter.hexdigest()
+    # Convert hexadecimal id to decimal one.
+    num_id = int(num_id, 16)
+    # Shorten full numeric id to 10 symbols.
+    num_id = int(str(num_id)[:10])
+    return num_id
+
+
+@functools.lru_cache()
+def build_num_to_string_id_mapping(universe: Tuple[str, ...]) -> Dict[int, str]:
+    """
+    Build a mapping from numeric ids to string ones.
+
+    :param universe: universe of string ids to convert
+    :return: numeric to string ids mapping
+    """
+    mapping: Dict[int, str] = {}
+    for string_id in universe:
+        # Convert string id to a numeric one.
+        num_id = string_to_num_id(string_id)
+        hdbg.dassert_not_in(
+            num_id,
+            mapping,
+            "Collision: id %s for string `%s` already exists",
+            num_id,
+            string_id,
+        )
+        mapping[num_id] = string_id
+    return mapping
