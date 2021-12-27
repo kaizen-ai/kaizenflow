9016d29b510fbf4a4ecdfaa3478c55e15f002e65
PomazkinG <pomazking@gmail.com>
2021-12-27 21:52:18 +0000

    checkpoint

--- im_v2/ccxt/data/client/test/test_clients.py
+++ im_v2/ccxt/data/client/test/test_clients.py
@@ -122,10 +122,18 @@ class TestCcxtDbClient(imvcodbut.TestImDbHelper):
         hsql.copy_rows_with_copy_from(self.connection, test_data, "ccxt_ohlcv")
         # Load data with client and check if it is correct.
         ccxt_db_client = imvcdclcl.CcxtDbClient("ohlcv", self.connection)
-        df = ccxt_db_client.read_data(["binance::BTC_USDT"])
-        actual = hunitest.convert_df_to_json_string(df, n_tail=None)
-        self.check_string(actual)
-        # Delete the table.
+        actual = ccxt_db_client.read_data(["binance::BTC_USDT"])
+        # Check the output values.
+        expected_length = 5
+        expected_exchange_ids = ["binance"]
+        expected_currency_pairs = ["BTC_USDT"]
+        _check_output(
+            self,
+            actual,
+            expected_length,
+            expected_exchange_ids,
+            expected_currency_pairs,
+        )
         hsql.remove_table(self.connection, "ccxt_ohlcv")
 
     @pytest.mark.slow("8 seconds.")
@@ -139,20 +147,29 @@ class TestCcxtDbClient(imvcodbut.TestImDbHelper):
         hsql.copy_rows_with_copy_from(self.connection, test_data, "ccxt_ohlcv")
         # Load data with client and check if it is correct.
         ccxt_db_client = imvcdclcl.CcxtDbClient("ohlcv", self.connection)
-        df = ccxt_db_client.read_data(
+        actual = ccxt_db_client.read_data(
             ["binance::BTC_USDT"],
             start_ts=pd.Timestamp("2021-09-08T20:01:00-04:00"),
             end_ts=pd.Timestamp("2021-09-08T20:04:00-04:00"),
         )
-        actual = hunitest.convert_df_to_json_string(df, n_tail=None)
-        self.check_string(actual)
+        # Check the output values.
+        expected_length = 1
+        expected_exchange_ids = ["binance"]
+        expected_currency_pairs = ["BTC_USDT"]
+        _check_output(
+            self,
+            actual,
+            expected_length,
+            expected_exchange_ids,
+            expected_currency_pairs,
+        )
         # Delete the table.
         hsql.remove_table(self.connection, "ccxt_ohlcv")
 
     @pytest.mark.slow("8 seconds.")
     def test_read_data3(self) -> None:
         """
-        Verify that data from DB is read correctly without normalization.
+        Verify that data from DB is read correctly for multiple full symbols.
         """
         # Load test data.
         self._create_test_table()
@@ -160,12 +177,21 @@ class TestCcxtDbClient(imvcodbut.TestImDbHelper):
         hsql.copy_rows_with_copy_from(self.connection, test_data, "ccxt_ohlcv")
         # Load data with client and check if it is correct.
         ccxt_db_client = imvcdclcl.CcxtDbClient("ohlcv", self.connection)
-        df = ccxt_db_client.read_data(
-            ["binance::BTC_USDT"],
+        actual = ccxt_db_client.read_data(
+            ["binance::BTC_USDT", "binance::ETH_USDT"],
             normalize=False,
         )
-        actual = hunitest.convert_df_to_json_string(df, n_tail=None)
-        self.check_string(actual)
+        # Check the output values.
+        expected_length = 5
+        expected_exchange_ids = ["binance"]
+        expected_currency_pairs = ["BTC_USDT"]
+        _check_output(
+            self,
+            actual,
+            expected_length,
+            expected_exchange_ids,
+            expected_currency_pairs,
+        )
         # Delete the table.
         hsql.remove_table(self.connection, "ccxt_ohlcv")
 
@@ -446,237 +472,6 @@ class TestCcxtParquetFileSystemClientReadData(hunitest.TestCase):
         # Check the output values.
         actual_string = hunitest.convert_df_to_json_string(actual)
         self.check_string(actual_string)
-
-
-# class TestMultipleSymbolsCcxtDbClient(imvcodbut.TestImDbHelper):
-#     @pytest.mark.slow("10 seconds.")
-#     def test1(self) -> None:
-#         """
-#         Test that data for provided list of full symbols is being read
-#         correctly.
-#         """
-#         # Set input list of full symbols.
-#         full_symbols = ["kucoin::SOL_USDT", "gateio::XRP_USDT"]
-#         # Load test data.
-#         self._create_test_table()
-#         test_data = self._get_test_data()
-#         hsql.copy_rows_with_copy_from(self.connection, test_data, "ccxt_ohlcv")
-#         # Initialize CCXT DB client and pass it to multiple symbols client.
-#         ccxt_db_client = imvcdclcl.CcxtDbClient("ohlcv", self.connection)
-#         multiple_symbols_client = imvcdcli.MultipleSymbolsImClient(
-#             class_=ccxt_db_client, mode="concat"
-#         )
-#         # Check actual results.
-#         actual = multiple_symbols_client.read_data(full_symbols=full_symbols)
-#         expected_length = 8
-#         expected_exchange_ids = ["gateio", "kucoin"]
-#         expected_currency_pairs = ["SOL_USDT", "XRP_USDT"]
-#         self._check_output(
-#             actual,
-#             expected_length,
-#             expected_exchange_ids,
-#             expected_currency_pairs,
-#         )
-#         # Delete the table.
-#         hsql.remove_table(self.connection, "ccxt_ohlcv")
-#
-#     @pytest.mark.slow("9 seconds.")
-#     def test2(self) -> None:
-#         """
-#         Test that all files are being read and filtered correctly.
-#         """
-#         # Set input list of full symbols.
-#         full_symbols = ["kucoin::SOL_USDT", "gateio::XRP_USDT"]
-#         # Load test data.
-#         self._create_test_table()
-#         test_data = self._get_test_data()
-#         hsql.copy_rows_with_copy_from(self.connection, test_data, "ccxt_ohlcv")
-#         # Initialize CCXT DB client and pass it to multiple symbols client.
-#         ccxt_db_client = imvcdclcl.CcxtDbClient("ohlcv", self.connection)
-#         multiple_symbols_client = imvcdcli.MultipleSymbolsImClient(
-#             class_=ccxt_db_client, mode="concat"
-#         )
-#         # Check output.
-#         actual = multiple_symbols_client.read_data(
-#             full_symbols=full_symbols,
-#             start_ts=pd.Timestamp("2021-09-09T00:01:00"),
-#             end_ts=pd.Timestamp("2021-09-09T00:04:00"),
-#         )
-#         expected_length = 3
-#         expected_exchange_ids = ["gateio", "kucoin"]
-#         expected_currency_pairs = ["SOL_USDT", "XRP_USDT"]
-#         self._check_output(
-#             actual,
-#             expected_length,
-#             expected_exchange_ids,
-#             expected_currency_pairs,
-#         )
-#         # Delete the table.
-#         hsql.remove_table(self.connection, "ccxt_ohlcv")
-#
-#     @pytest.mark.slow("9 seconds.")
-#     def test3(self) -> None:
-#         """
-#         Test that all files are being read correctly without normalization.
-#         """
-#         # Set input list of full symbols.
-#         full_symbols = ["kucoin::SOL_USDT", "gateio::XRP_USDT"]
-#         # Load test data.
-#         self._create_test_table()
-#         test_data = self._get_test_data()
-#         hsql.copy_rows_with_copy_from(self.connection, test_data, "ccxt_ohlcv")
-#         # Initialize CCXT DB client and pass it to multiple symbols client.
-#         ccxt_db_client = imvcdclcl.CcxtDbClient("ohlcv", self.connection)
-#         multiple_symbols_client = imvcdcli.MultipleSymbolsImClient(
-#             class_=ccxt_db_client, mode="concat"
-#         )
-#         # Check output.
-#         actual = multiple_symbols_client.read_data(
-#             full_symbols=full_symbols,
-#             normalize=False,
-#         )
-#         expected_length = 6
-#         expected_exchange_ids = ["gateio", "kucoin"]
-#         expected_currency_pairs = ["SOL_USDT", "XRP_USDT"]
-#         self._check_output(
-#             actual,
-#             expected_length,
-#             expected_exchange_ids,
-#             expected_currency_pairs,
-#         )
-#         # Delete the table.
-#         hsql.remove_table(self.connection, "ccxt_ohlcv")
-#
-#     @pytest.mark.slow("9 seconds.")
-#     def test4(self) -> None:
-#         """
-#         Test that all files are being read correctly in dict output mode.
-#         """
-#         # Set input list of full symbols.
-#         full_symbols = ["gateio::XRP_USDT", "kucoin::SOL_USDT"]
-#         # Load test data.
-#         self._create_test_table()
-#         test_data = self._get_test_data()
-#         hsql.copy_rows_with_copy_from(self.connection, test_data, "ccxt_ohlcv")
-#         # Initialize CCXT DB client and pass it to multiple symbols client.
-#         ccxt_db_client = imvcdclcl.CcxtDbClient("ohlcv", self.connection)
-#         multiple_symbols_client = imvcdcli.MultipleSymbolsImClient(
-#             class_=ccxt_db_client, mode="dict"
-#         )
-#         # Check output.
-#         actual_dict = multiple_symbols_client.read_data(full_symbols=full_symbols)
-#         actual_dict_keys = sorted(list(actual_dict.keys()))
-#         actual_df1 = actual_dict[actual_dict_keys[0]]
-#         actual_df2 = actual_dict[actual_dict_keys[1]]
-#         self.assert_equal(str(actual_dict_keys), str(full_symbols))
-#         self._check_output(
-#             actual=actual_df1,
-#             expected_length=6,
-#             expected_exchange_ids=["gateio"],
-#             expected_currency_pairs=["XRP_USDT"],
-#             check_string=False,
-#         )
-#         self._check_output(
-#             actual=actual_df2,
-#             expected_length=2,
-#             expected_exchange_ids=["kucoin"],
-#             expected_currency_pairs=["SOL_USDT"],
-#             check_string=False,
-#         )
-#         # Create combined actual string and check it.
-#         actual_string_df1 = hunitest.convert_df_to_json_string(actual_df1)
-#         actual_string_df2 = hunitest.convert_df_to_json_string(actual_df2)
-#         actual_string = "\n".join(
-#             [
-#                 actual_dict_keys[0],
-#                 actual_string_df1,
-#                 "\n",
-#                 actual_dict_keys[1],
-#                 actual_string_df2,
-#             ]
-#         )
-#         self.check_string(actual_string)
-#         # Delete the table.
-#         hsql.remove_table(self.connection, "ccxt_ohlcv")
-#
-#     def _create_test_table(self) -> None:
-#         """
-#         Create a test CCXT OHLCV table in DB.
-#         """
-#         query = imvccdbut.get_ccxt_ohlcv_create_table_query()
-#         self.connection.cursor().execute(query)
-#
-#     @staticmethod
-#     def _get_test_data() -> pd.DataFrame:
-#         """
-#         Create a test CCXT OHLCV dataframe.
-#         """
-#         test_data = pd.DataFrame(
-#             columns=[
-#                 "id",
-#                 "timestamp",
-#                 "open",
-#                 "high",
-#                 "low",
-#                 "close",
-#                 "volume",
-#                 "currency_pair",
-#                 "exchange_id",
-#                 "created_at",
-#             ],
-#             # fmt: off
-#             # pylint: disable=line-too-long
-#             data=[
-#                 [1, 1631145600000, 30, 40, 50, 60, 70, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
-#                 [2, 1631145660000, 31, 41, 51, 61, 71, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
-#                 [3, 1631145720000, 32, 42, 52, 62, 72, "SOL_USDT", "kucoin", pd.Timestamp("2021-09-09")],
-#                 [4, 1631145780000, 33, 43, 53, 63, 73, "SOL_USDT", "kucoin", pd.Timestamp("2021-09-09")],
-#                 [5, 1631145840000, 34, 44, 54, 64, 74, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
-#                 [6, 1631145900000, 34, 44, 54, 64, 74, "XRP_USDT", "gateio", pd.Timestamp("2021-09-09")],
-#                 [7, 1631145960000, 34, 44, 54, 64, 74, "ETH_USDT", "binance", pd.Timestamp("2021-09-09")],
-#             ]
-#             # pylint: enable=line-too-long
-#             # fmt: on
-#         )
-#         return test_data
-#
-#     def _check_output(
-#         self,
-#         actual: pd.DataFrame,
-#         expected_length: int,
-#         expected_exchange_ids: List[str],
-#         expected_currency_pairs: List[str],
-#         check_string: bool = True,
-#     ) -> None:
-#         """
-#         Verify that actual outcome dataframe matches the expected one.
-#
-#         :param actual: actual outcome dataframe
-#         :param expected_length: expected outcome dataframe length
-#         :param expected_exchange_ids: list of expected exchange ids
-#         :param expected_currency_pairs: list of expected currency pairs
-#         :param check_string: whether to check with golden outcome or not
-#         """
-#         # Check output df length.
-#         self.assert_equal(str(expected_length), str(actual.shape[0]))
-#         # Check unique exchange ids in the output df.
-#         actual_exchange_ids = sorted(
-#             list(actual["exchange_id"].dropna().unique())
-#         )
-#         self.assert_equal(str(actual_exchange_ids), str(expected_exchange_ids))
-#         # Check unique currency pairs in the output df.
-#         actual_currency_pairs = sorted(
-#             list(actual["currency_pair"].dropna().unique())
-#         )
-#         self.assert_equal(
-#             str(actual_currency_pairs), str(expected_currency_pairs)
-#         )
-#         if check_string:
-#             # Check the output values.
-#             actual_string = hunitest.convert_df_to_json_string(
-#                 actual.reset_index()
-#             )
-#             self.check_string(actual_string)
 
 
 # #############################################################################
