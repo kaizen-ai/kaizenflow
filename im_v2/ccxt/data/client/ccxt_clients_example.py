4ebd6b86eed831bb3f0d26efb58c7d1181056ac8
DanilYachmenev <31514660+DanilYachmenev@users.noreply.github.com>
2022-01-14 15:33:39 +0300

    CmTask949: remove data_type from CCXT clients (#958)
    * CmTask949: remove data_type from CCXT clients
    
    * CMTask949: add docstring to get_test_data_dir

--- im_v2/ccxt/data/client/ccxt_clients_example.py
+++ im_v2/ccxt/data/client/ccxt_clients_example.py
@@ -11,8 +11,14 @@ import helpers.hgit as hgit
 import im_v2.ccxt.data.client.ccxt_clients as imvcdccccl
 
 
-# TODO(gp): @grisha, explain how was this file generated
-def get_test_data_dir():
+def get_test_data_dir() -> str:
+    """
+    Get dir with data files for the tests.
+
+    The files in the dir are copies of some CCXT data files from S3 that
+    were loaded for our research purposes. These copies are checked out
+    locally in order to test functions without dependencies on S3.
+    """
     test_data_dir = os.path.join(
         hgit.get_amp_abs_path(),
         "im_v2/ccxt/data/client/test/test_data",
@@ -36,14 +42,12 @@ def get_CcxtCsvFileSytemClient_example1() -> imvcdccccl.CcxtCsvFileSystemClient:
     # 1534464240000,285.400193,285.884638,285.400193,285.884638,0.074655
     # ```
     # Initialize client.
-    data_type = "ohlcv"
     root_dir = get_test_data_dir()
-    ccxt_file_client = imvcdccccl.CcxtCsvFileSystemClient(data_type, root_dir)
+    ccxt_file_client = imvcdccccl.CcxtCsvFileSystemClient(root_dir)
     return ccxt_file_client
 
 
 def get_CcxtParquetFileSytemClient_example1() -> imvcdccccl.CcxtParquetFileSystemClient:
-    data_type = "ohlcv"
     root_dir = get_test_data_dir()
-    ccxt_client = imvcdccccl.CcxtParquetFileSystemClient(data_type, root_dir)
+    ccxt_client = imvcdccccl.CcxtParquetFileSystemClient(root_dir)
     return ccxt_client
