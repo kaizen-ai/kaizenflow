1a6ba302e629e6c681bbafb544d75aaa262e5787
jsmerix <49269742+jsmerix@users.noreply.github.com>
2022-01-14 12:11:03 +0100

    Cm task846 implement centralized storage of api keys (#959)
    * Add auxiliary method to return secret manager client & add get_secret method to retrieve AWS secret
    
    * Fix typos and add store_secret function
    
    * Add profile parameter to boto session to support other AWS account
    
    * Add basic test for get_secrets_client and get_secret
    
    * Update docs
    
    * Fix decorator placement in tests and invalid calls in tests
    
    * Add missing import from typing
    
    * Rename helpers libs imports after naming convention change
    
    * Fix docstring typo
    
    * Change parameter for get_secrets_client to aws_profile, adapt test to change
    
    * Rename module to hsecrets according to naming convention
    
    * Skip tests, will enable in later PR
    
    * Code style changes, Enable tests to run after code style change
    
    * Refactor imports, disable tests
    
    * Refactor to use wildcard argument before default arguments
    
    * Clarify reason for skipping tests

---
+++ helpers/test/test_secrets.py
@@ -0,0 +1,60 @@
+import json
+import logging
+
+import boto3
+from botocore.client import BaseClient
+from moto import mock_secretsmanager
+import pytest
+import helpers.hsecrets as hsecret
+import helpers.hunit_test as hunitest
+
+_LOG = logging.getLogger(__name__)
+
+# mock_secretsmanager decorator ensures the calls to the AWS API are mocked
+
+
+class Test_Create_Client1(hunitest.TestCase):
+    """
+    Simple smoke test to verify connection to AWS.
+    """
+
+    @pytest.mark.skip(reason='Need to add ck profile to gh actions CmTask961, test has passed on dev stage')
+    def test_create_client1(self) -> None:
+        client = hsecret.get_secrets_client()
+        self.assertIsInstance(client, BaseClient)
+
+class Test_Get_Secret1(hunitest.TestCase):
+    """
+    Verify that the secret can be retrieved correctly.
+    """
+
+    @pytest.mark.skip(reason='Need to add ck profile to gh actions CmTask961, test has passed on dev stage')
+    @mock_secretsmanager
+    def test_get_secret(self) -> None:
+        # make sure the region name matches the one used in hsecret profile
+        client = boto3.client("secretsmanager", region_name="eu-north-1")
+        secret = {"testkey": "testvalue"}
+        secret_name = "Testsecret"
+
+        client.create_secret(Name=secret_name, SecretString=json.dumps(secret))
+        self.assertDictEqual(hsecret.get_secret(secret_name), secret)
+
+
+class Test_Store_Secret1(hunitest.TestCase):
+    """
+    Verify that a secret can be stored correctly.
+    """
+
+    @pytest.mark.skip(reason='Need to add ck profile to gh actions CmTask961, test has passed on dev stage')
+    @mock_secretsmanager
+    def test_store_secret(self) -> None:
+        secret = {"testkey": "testvalue"}
+        secret_name = "Testsecret"
+        hsecret.store_secret(secret_name, secret)
+
+        # make sure the region name matches the one used in hsecret
+        client = boto3.client("secretsmanager", region_name="eu-north-1")
+        test_secret_value = json.loads(
+            client.get_secret_value(SecretId=secret_name)["SecretString"]
+        )
+        self.assertDictEqual(test_secret_value, secret)
