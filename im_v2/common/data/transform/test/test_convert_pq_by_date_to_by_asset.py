b6b6850f3411392f518fa04d95fd2c954f4b29b6
nikola.jasek <cowardly.dog.solutions.oy@eesti.ee>
2022-01-14 12:03:25 +0000

    CMTask857: Update by date by asset tests

--- im_v2/common/data/transform/test/test_convert_pq_by_date_to_by_asset.py
+++ im_v2/common/data/transform/test/test_convert_pq_by_date_to_by_asset.py
@@ -9,41 +9,6 @@ import im_v2.common.data.transform.convert_pq_by_date_to_by_asset as imvcdtcpbdt
 
 
 class TestPqByDateToByAsset1(hunitest.TestCase):
-    @staticmethod
-    def get_dummy_script_arguments() -> Dict[str, Any]:
-        """
-        Arguments used in script run.
-        """
-        return {
-            "asset_col_name": "asset",
-            # parallelization args
-            "num_threads": "1",
-            "dry_run": True,
-            "no_incremental": True,
-            "skip_on_error": True,
-            "num_attempts": 1,
-        }
-
-    @staticmethod
-    def get_dummy_task_config(test_dir: str) -> Dict[str, Any]:
-        """
-        Specific config used for joblib task.
-
-        Along with regular arguments used in script run, there is
-        `parquet_file_name` which represents list of daily PQ file paths
-        that will be converted to by asset PQ files.
-        """
-        return {
-            "src_dir": f"{test_dir}/by_date",
-            "parquet_file_names": [
-                f"{test_dir}/by_date/date=20211230/data.parquet",
-                f"{test_dir}/by_date/date=20211231/data.parquet",
-                f"{test_dir}/by_date/date=20220101/data.parquet",
-            ],
-            "dst_dir": f"{test_dir}/by_asset",
-            "asset_col_name": "asset",
-        }
-
     def generate_test_data(self, verbose: bool) -> Tuple[str, str]:
         """
         Generate test data in form of daily PQ files.
@@ -93,17 +58,17 @@ class TestPqByDateToByAsset1(hunitest.TestCase):
         actual = "\n".join(actual)
         self.check_string(actual, purify_text=True)
 
-    def test_daily_data2(self) -> None:
+    def test_command_line(self) -> None:
         verbose = True
-        self._test_daily_data(verbose)
+        self._test_command_line(verbose)
 
-    def test_daily_data_direct_run1(self) -> None:
+    def test_function_call1(self) -> None:
         verbose = False
-        self._test_daily_data_direct_run(verbose)
+        self._test_function_call(verbose)
 
-    def test_daily_data_direct_run2(self) -> None:
+    def test_function_call2(self) -> None:
         verbose = True
-        self._test_daily_data_direct_run(verbose)
+        self._test_function_call(verbose)
 
     def test_process_chunk(self) -> None:
         verbose = True
@@ -120,13 +85,25 @@ class TestPqByDateToByAsset1(hunitest.TestCase):
         cmd.extend(["--num_threads", "1"])
         cmd.extend(["--asset_col_name", "ticker"])
         args = parser.parse_args(cmd)
-        args = str(args).split("(")[-1]
-        args = args.rstrip(")")
+        args = str(args).lstrip("Namespace").lstrip("(").rstrip(")")
         args = args.split(", ")
-        args = "\n".join(args)
-        self.check_string(args, purify_text=True)
+        actual = "\n".join(args)
+        expected = "\n".join(
+            [
+                "asset_col_name='ticker'",
+                "dry_run=False",
+                "dst_dir='dummy_by_asset_dir'",
+                "log_level='INFO'",
+                "no_incremental=False",
+                "num_attempts=1",
+                "num_threads='1'",
+                "skip_on_error=False",
+                "src_dir='dummy_by_date_dir'",
+            ]
+        )
+        self.assert_equal(actual, expected)
 
-    def _test_daily_data(self, verbose: bool) -> None:
+    def _test_command_line(self, verbose: bool) -> None:
         """
         Generate daily data for 3 days in a by-date format and then convert it
         to by-asset.
@@ -151,29 +128,32 @@ class TestPqByDateToByAsset1(hunitest.TestCase):
             by_date_dir, by_asset_dir
         )
 
-    def _test_daily_data_direct_run(self, verbose: bool) -> None:
+    def _test_function_call(self, verbose: bool) -> None:
         """
         Tests directly _run function for coverage increase.
 
         Flow is same as in _test_daily_data.
         """
+        # Prepare inputs.
         test_dir, by_date_dir = self.generate_test_data(verbose)
         by_asset_dir = os.path.join(test_dir, "by_asset")
-        kwargs = self.get_dummy_script_arguments()
-        kwargs.update(
-            {
-                "src_dir": by_date_dir,
-                "dst_dir": by_asset_dir,
-            }
-        )
+        kwargs = {
+            "src_dir": by_date_dir,
+            "dst_dir": by_asset_dir,
+            "asset_col_name": "asset",
+            # parallelization args
+            "num_threads": "1",
+            "dry_run": True,
+            "no_incremental": True,
+            "skip_on_error": True,
+            "num_attempts": 1,
+        }
         if verbose:
-            kwargs.update(
-                {
-                    "asset_col_name": "ticker",
-                }
-            )
+            kwargs.update({"asset_col_name": "ticker"})
+        # Run.
         args = argparse.Namespace(**kwargs)
         imvcdtcpbdtba._run(args)
+        # Check output.
         self.check_directory_structure_with_file_contents(
             by_date_dir, by_asset_dir
         )
@@ -187,18 +167,25 @@ class TestPqByDateToByAsset1(hunitest.TestCase):
         Because it is run as a separate process it is not caught by
         coverage.
         """
+        # Prepare inputs.
         test_dir, by_date_dir = self.generate_test_data(verbose)
         by_asset_dir = os.path.join(test_dir, "by_asset")
-        config = self.get_dummy_task_config(test_dir)
+        config = {
+            "dst_dir": by_asset_dir,
+            "parquet_file_names": [
+                f"{test_dir}/by_date/date=20211230/data.parquet",
+                f"{test_dir}/by_date/date=20211231/data.parquet",
+                f"{test_dir}/by_date/date=20220101/data.parquet",
+            ],
+            "asset_col_name": "asset",
+        }
         if verbose:
-            config.update(
-                {
-                    "asset_col_name": "ticker",
-                }
-            )
+            config.update({"asset_col_name": "ticker"})
         if config_update:
             config.update(config_update)
+        # Run.
         imvcdtcpbdtba._process_chunk(**config)
+        # Check output.
         self.check_directory_structure_with_file_contents(
             by_date_dir, by_asset_dir
         )
