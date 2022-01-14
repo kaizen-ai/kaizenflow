4394bb86a27a8f5fb059bfd20f4bcfb3494348a3
jsmerix <smeriga.juraj@gmail.com>
2022-01-14 10:47:21 +0000

    Refactor imports, disable tests

--- helpers/test/test_secrets.py
+++ helpers/test/test_secrets.py
@@ -2,9 +2,8 @@ import json
 import logging
 
 import boto3
-import botocore.client
-from botocore.client import botocore.clientBaseClient
-import moto.mock_secretsmanager
+from botocore.client import BaseClient
+from moto import mock_secretsmanager
 import pytest
 import helpers.hsecrets as hsecret
 import helpers.hunit_test as hunitest
@@ -14,9 +13,9 @@ _LOG = logging.getLogger(__name__)
 # mock_secretsmanager decorator ensures the calls to the AWS API are mocked
 
 
-class Test_Create_Client(hunitest.TestCase):
+class Test_Create_Client1(hunitest.TestCase):
     """
-    Simple smoke test to verify cconnection to AWS.
+    Simple smoke test to verify connection to AWS.
     """
 
     @pytest.mark.skip(reason='Need to unblock issues, test has passed on dev stage')
@@ -24,13 +23,13 @@ class Test_Create_Client(hunitest.TestCase):
         client = hsecret.get_secrets_client()
         self.assertIsInstance(client, BaseClient)
 
-class Test_Get_Secret(hunitest.TestCase):
+class Test_Get_Secret1(hunitest.TestCase):
     """
     Verify that the secret can be retrieved correctly.
     """
 
     @pytest.mark.skip(reason='Need to unblock issues, test has passed on dev stage')
-    @moto.mock_secretsmanager
+    @mock_secretsmanager
     def test_get_secret(self) -> None:
         # make sure the region name matches the one used in hsecret profile
         client = boto3.client("secretsmanager", region_name="eu-north-1")
@@ -41,13 +40,13 @@ class Test_Get_Secret(hunitest.TestCase):
         self.assertDictEqual(hsecret.get_secret(secret_name), secret)
 
 
-class Test_Store_Secret(hunitest.TestCase):
+class Test_Store_Secret1(hunitest.TestCase):
     """
     Verify that a secret can be stored correctly.
     """
 
     @pytest.mark.skip(reason='Need to unblock issues, test has passed on dev stage')
-    @moto.mock_secretsmanager
+    @mock_secretsmanager
     def test_store_secret(self) -> None:
         secret = {"testkey": "testvalue"}
         secret_name = "Testsecret"
