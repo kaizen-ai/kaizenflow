07ae0b71609d88e89ba3c9c9c8c4549e4bb5f844
dan <d.yachmenev@crypto-kaizen.com>
2022-01-18 03:09:45 +0000

    CmTask907: move code inside libs

--- im_v2/common/data/client/__init__.py
+++ im_v2/common/data/client/__init__.py
@@ -6,3 +6,4 @@ import im_v2.common.data.client as icdc
 
 from im_v2.common.data.client.clients import *  # pylint: disable=unused-import # NOQA
 from im_v2.common.data.client.full_symbol import *  # pylint: disable=unused-import # NOQA
+from im_v2.common.data.client.test.test_clients_helper import *  # pylint: disable=unused-import # NOQA
