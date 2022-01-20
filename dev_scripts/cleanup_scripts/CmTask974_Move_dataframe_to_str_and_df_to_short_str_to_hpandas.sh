be644e12b6fa390ae75bd8431aa8f38aa3eb1b6f
saggese <saggese@gmail.com>
2022-01-20 12:28:12 -0500

    Checkpoint

---
+++ dev_scripts/cleanup_scripts/CmTask974_Move_dataframe_to_str_and_df_to_short_str_to_hpandas.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -xe
+
+# This script should be invoked on a different repo so that we don't annihilate
+# this file.
+TARGET_REPO=/Users/saggese/src/cmamp2
+cd $TARGET_REPO
+
+# Clean up.
+git reset --hard
+
+# Apply changes.
+git merge CmTask972_Merge_dataframe_to_str_and_df_to_short_str_into_hpandas
+
+# Move dataframe_to_str from hprint to hpandas.
+./dev_scripts/replace_text.py \
+  --old "hprint.dataframe_to_str" \
+  --new "hpandas.dataframe_to_str" \
+  --ext "py"
+
+# Move df_to_short_str from hprint to hpandas.
+./dev_scripts/replace_text.py \
+  --old "hprint.df_to_short_str" \
+  --new "hpandas.df_to_short_str" \
+  --ext "py"
+
+# Add potential missing imports.
+./dev_scripts/replace_text.py \
+  --old "import helpers.hprint as hprint" \
+  --new "import helpers.hpandas as hpandas; import helpers.hprint as hprint" \
+  --ext "py"
+
+# Remove unused imports.
+invoke lint --files "$(find . -name '*.py')" --phases="amp_isort autoflake"
