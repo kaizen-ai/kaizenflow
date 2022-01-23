1fe79361bc965b4c03ec0869bf59c786fde04b83
nikola.jasek <cowardly.dog.solutions.oy@eesti.ee>
2022-01-23 19:42:32 +0100

    CMTask972: Add script

---
+++ dev_scripts/cleanup_scripts/CmTask972_Merge_dataframe_to_str_and_df_to_short_str_into_hpandas.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import re
+
+import dev_scripts.replace_text as dscretex
+import helpers.hdbg as hdbg
+import helpers.hio as hio
+import helpers.hparser as hparser
+
+_LOG = logging.getLogger(__name__)
+
+
+def _run() -> None:
+    dataframe_to_str = "hpandas.dataframe_to_str"
+    df_to_short_str = "hpandas.df_to_short_str"
+    df_to_str = "hpandas.df_to_str"
+    all_files = dscretex._get_all_files(["."], ["py"])
+    # Remove current script from the result.
+    all_files.remove(__file__)
+    for file_name in all_files:
+        file_string = hio.from_file(file_name, encoding=dscretex._ENCODING)
+        # Skip non relevant files.
+        if not any(
+            [
+                f"{dataframe_to_str}(" in file_string,
+                f"{df_to_short_str}(" in file_string,
+            ]
+        ):
+            continue
+        for function_ in [dataframe_to_str, df_to_short_str]:
+            # Find all possible patterns for function call.
+            function_args = re.findall(f"{function_}\\((.*?)\\)", file_string)
+            if function_args:
+                for args in function_args:
+                    # Skip multiline function call.
+                    if "(" in args:
+                        _LOG.info(
+                            f"Replace occurrences of `{args}` in `{file_name}` "
+                            f"manually or by custom pattern!"
+                        )
+                        continue
+                    # Create desired function call.
+                    new_args = ""
+                    if function_ == dataframe_to_str:
+                        # Append generic tag name.
+                        new_args = f'{args}, tag="df"'
+                    elif function_ == df_to_short_str:
+                        # Convert tag positional to keyword argument.
+                        new_args_list = args.split(", ")
+                        new_args_list.append(f"tag={new_args_list[0]}")
+                        new_args_list.pop(0)
+                        new_args = ", ".join(new_args_list)
+                    # Replace occurrences in file.
+                    old_regex = f"{function_}({args})"
+                    new_regex = f"{df_to_str}({new_args})"
+                    file_string = file_string.replace(old_regex, new_regex)
+        hio.to_file(file_name, file_string)
+
+
+def _parse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    hparser.add_verbosity_arg(parser)
+    return parser
+
+
+def _main(parser: argparse.ArgumentParser) -> None:
+    args = parser.parse_args()
+    hdbg.init_logger(verbosity=args.log_level, use_exec_path=True)
+    _run()
+
+
+if __name__ == "__main__":
+    _main(_parse())
