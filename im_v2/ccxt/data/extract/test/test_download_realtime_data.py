142d398a36ba8ebf80bed4e3ebf73997f333610f
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-01-25 11:36:33 +0000

    CMTask968: Add TODOs

--- im_v2/ccxt/data/extract/test/test_download_realtime_data.py
+++ im_v2/ccxt/data/extract/test/test_download_realtime_data.py
@@ -1,5 +1,4 @@
 import logging
-
 import helpers.hunit_test as hunitest
 import im_v2.common.db.db_utils as imvcddbut
 
@@ -7,29 +6,54 @@ _LOG = logging.getLogger(__name__)
 
 
 class TestDownloadRealtimeData(imvcddbut.TestImDbHelper):
-    def test_download_realtime_ohlcv(self) -> None:
+    def test_download_realtime_ohlcv_to_db(self) -> None:
         """
         Verify that OHLCV data is downloaded in correct format.
         """
+        # TODO(steysie): Check the parent class and its uses for extra methods.
+        #  Tests should check writing to temporary DB (implemented in the class).
+        pass
+
+    def test_download_realtime_ohlcv_to_disk(self) -> None:
+        """
+        Verify that OHLCV data is downloaded in correct format.
+        """
+        # TODO(steysie): Check the parent class for extra methods.
+        #  Tests should check writing to scratch space (.get_scratch_space()) method.
+        #  This is a different script call from previous method: previous saves to DB only,
+        #  this one to disk as well. Use `--universe "small"` for both.
+        pass
 
 
 class TestDownloadData(hunitest.TestCase):
-    class TestInstantiateExchange(hunitest.TestCase):
-        def test_instantiate_exchange(self) -> None:
+        def test_download_ohlcv(self) -> None:
             """
-            Verify that exchange tuple is generated correctly.
+            Verify that OHLCV is downloaded in a correct format.
             """
-    def test_download_ohlcv(self) -> None:
-        """
-        Verify that OHLCV is downloaded in a correct format.
-        """
+            # TODO(steysie): Provide fixed input parameters, instantiate exchange,
+            #  drop `created_at` column, convert to json and use check_string.
+            pass
 
-    def test_download_orderbook(self) -> None:
-        """
-        Verify that orderbook data is downloaded in a correct format.
-        """
+        def test_download_orderbook(self) -> None:
+            """
+            Verify that orderbook data is downloaded in a correct format.
+            """
+            # TODO(steysie): Note that there is no historical data.
+            #  Check data type and structure, not contents.
+            pass
 
-    def test_download_incorrect(self) -> None:
+        def test_download_incorrect(self) -> None:
+            """
+            Verify that unsupported data formats are handled correctly.
+            """
+            # TODO(steysie): Check that a correct error is raised with `self.assertRaises`.
+            pass
+
+
+class TestInstantiateExchange(hunitest.TestCase):
+    def test_instantiate_exchange(self) -> None:
         """
-        Verify that unsupported data formats are handled correctly.
+        Verify that exchange tuple is generated correctly.
         """
+        # TODO(steysie): One test to verify types of each of NamedTuple elements is sufficient.
+        pass
