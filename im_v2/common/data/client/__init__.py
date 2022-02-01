cf51592cd6509350135a648b7769daeedc531809
Max Sergeychikov <43182811+max-rsrch@users.noreply.github.com>
2022-02-01 20:16:42 +0300

    Cm task1072 rename files related to market data and im client 2 (#1075)
    * Rename file and lint for new import
    
    * Rename new imports
    
    * Rename test_clients_helper
    
    * Apply requested changes
    
    * Fix nit

--- im_v2/common/data/client/__init__.py
+++ im_v2/common/data/client/__init__.py
@@ -4,6 +4,6 @@ Import as:
 import im_v2.common.data.client as icdc
 """
 
-from im_v2.common.data.client.clients import *  # pylint: disable=unused-import # NOQA
+from im_v2.common.data.client.base_im_clients import *  # pylint: disable=unused-import # NOQA
 from im_v2.common.data.client.full_symbol import *  # pylint: disable=unused-import # NOQA
 from im_v2.common.data.client.historical_pq_clients import *  # pylint: disable=unused-import # NOQA
