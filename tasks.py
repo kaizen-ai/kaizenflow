fb15decdef262546d542b7c67b7e3c65626563a3
sonniki <son.nik@mail.ru>
2022-02-03 18:55:21 +0000

    DevToolsTask274: Add cycle detector to tasks

--- tasks.py
+++ tasks.py
@@ -72,6 +72,7 @@ from helpers.lib_tasks import (  # noqa: F401  # pylint: disable=unused-import
     integrate_find_files,
     lint,
     lint_create_branch,
+    lint_detect_cycles,
     print_setup,
     print_tasks,
     pytest_clean,
