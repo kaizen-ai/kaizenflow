1a11f4f429f4703fd7f20f7a7ade66e3f2adbabc
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-02-07 11:08:26 +0000

    CMTask1126: checkpoint

--- im_v2/ccxt/data/extract/compare_realtime_and_historical.py
+++ im_v2/ccxt/data/extract/compare_realtime_and_historical.py
@@ -1,3 +1,9 @@
+"""
+Import as:
+
+import im_v2.ccxt.data.extract.compare_realtime_and_historical as imvcdecrah
+"""
+
 # DF1 = reading from S3 bucket for 24 hours
 # DF2 = SELECT FROM over 24 hours
 
@@ -8,46 +14,84 @@
 
 # Comparison 2: take timestamps present in both, compare row-by-row.
 
-import pandas as pd
-import logging
-import helpers.hsql as hsql
 import argparse
+
+import pandas as pd
+
 import helpers.hdatetime as hdateti
-import im_v2.im_lib_tasks as imvimlita
 import helpers.hdbg as hdbg
+import helpers.hparser as hparser
+import helpers.hs3 as hs3
+import helpers.hsql as hsql
+import im_v2.im_lib_tasks as imvimlita
 
 
 def find_gaps(rt_data, daily_data) -> pd.DataFrame:
     """
-
+    
     """
-    rt_data_reindex = rt_data.drop(["ended_downloaded_at", "knowledge_time"], axis=1)
+    rt_data_reindex = rt_data.drop(
+        ["ended_downloaded_at", "knowledge_time"], axis=1
+    )
     rt_data_reindex = rt_data_reindex.set_index(["timestamp", "currency_pair"])
-    daily_data_reindex = daily_data.drop(["ended_downloaded_at", "knowledge_time"], axis=1)
-    daily_data_reindex = daily_data_reindex.set_index(["timestamp", "currency_pair"])
+    daily_data_reindex = daily_data.drop(
+        ["ended_downloaded_at", "knowledge_time"], axis=1
+    )
+    daily_data_reindex = daily_data_reindex.set_index(
+        ["timestamp", "currency_pair"]
+    )
     # Get data present in daily, but not present in rt.
-    rt_missing_indices = daily_data_reindex.index.difference(rt_data_reindex.index)
+    rt_missing_indices = daily_data_reindex.index.difference(
+        rt_data_reindex.index
+    )
     rt_missing_data = daily_data_reindex.loc[rt_missing_indices]
     # Get data present in rt, but not present in daily.
-    daily_missing_indices = rt_data_reindex.index.difference(rt_data_reindex.index)
+    daily_missing_indices = rt_data_reindex.index.difference(
+        rt_data_reindex.index
+    )
     daily_missing_data = rt_data_reindex.loc[daily_missing_indices]
     return rt_missing_data, daily_missing_data
 
 
 def compare_rows(rt_data, daily_data) -> pd.DataFrame:
     """
-
+    
     """
     #
-    rt_data_reindex = rt_data.drop(["ended_downloaded_at", "knowledge_time"], axis=1)
+    rt_data_reindex = rt_data.drop(
+        ["ended_downloaded_at", "knowledge_time"], axis=1
+    )
     rt_data_reindex = rt_data_reindex.set_index(["timestamp", "currency_pair"])
-    daily_data_reindex = daily_data.drop(["ended_downloaded_at", "knowledge_time"], axis=1)
-    daily_data_reindex = daily_data_reindex.set_index(["timestamp", "currency_pair"])
+    daily_data_reindex = daily_data.drop(
+        ["ended_downloaded_at", "knowledge_time"], axis=1
+    )
+    daily_data_reindex = daily_data_reindex.set_index(
+        ["timestamp", "currency_pair"]
+    )
     #
     idx_intersection = rt_data.index.intersection(daily_data.intersection)
     # Get difference between daily data and rt data.
-    data_difference = daily_data_reindex.loc[idx_intersection].compare(rt_data_reindex.loc[idx_intersection])
+    data_difference = daily_data_reindex.loc[idx_intersection].compare(
+        rt_data_reindex.loc[idx_intersection]
+    )
     return data_difference
+
+
+def _parse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--db_table",
+        action="store",
+        required=False,
+        default="ccxt_ohlcv",
+        type=str,
+        help="(Optional) DB table to use, default: 'ccxt_ohlcv'",
+    )
+    parser = hparser.add_verbosity_arg(parser)
+    parser = hs3.add_s3_args(parser)
 
 
 def _main(parser: argparse.ArgumentParser) -> None:
@@ -60,6 +104,9 @@ def _main(parser: argparse.ArgumentParser) -> None:
     env_file = imvimlita.get_db_env_path(args.db_stage)
     connection_params = hsql.get_connection_info_from_env_file(env_file)
     connection = hsql.get_connection(*connection_params)
+    # Connect to S3 filesystem, if provided.
+    hs3.get_s3fs(args.aws_profile)
+
     # Read DB realtime data.
     query = f"SELECT * FROM ccxt_ohlcv WHERE created_at >='{start_datetime}' and created_at <= {end_datetime}"
     rt_data = hsql.execute_query_to_df(connection, query)
@@ -70,5 +117,4 @@ def _main(parser: argparse.ArgumentParser) -> None:
     # Get missing data.
     rt_missing_data, daily_missing_data = find_gaps(rt_data, daily_data)
     # Compare dataframe contents.
-    data_difference = compare_rows(rt_data, daily_data)
-
+    compare_rows(rt_data, daily_data)
