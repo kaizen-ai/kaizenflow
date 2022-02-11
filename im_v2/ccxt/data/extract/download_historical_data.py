23139f05ee707c41193a7a082071d18e3515b1ae
jsmerix <smeriga.juraj@gmail.com>
2022-02-11 00:02:04 +0000

    Add knowledge timestamp to historical data.

--- im_v2/ccxt/data/extract/download_historical_data.py
+++ im_v2/ccxt/data/extract/download_historical_data.py
@@ -99,11 +99,14 @@ def _main(parser: argparse.ArgumentParser) -> None:
             start_datetime=start_datetime,
             end_datetime=end_datetime,
         )
+        # Get datetime of push to s3 in UTC.
+        knowledge_timestamp = hdateti.get_current_timestamp_as_string("UTC")
+        data["knowledge_timestamp"] = knowledge_timestamp
         # Get file name.
         file_name = (
             currency_pair
             + "_"
-            + hdateti.get_current_timestamp_as_string("UTC")
+            + knowledge_timestamp
             + ".csv"
         )
         path_to_file = os.path.join(args.s3_path, args.exchange_id, file_name)
