02ea9e064d47705160b766a6c12836c6595313b4
nikola.jasek <cowardly.dog.solutions.oy@eesti.ee>
2022-02-22 20:20:12 +0100

    CMTask815: Enable unit tests and increase coverage

--- im_v2/ccxt/data/extract/test/test_exchange_class.py
+++ im_v2/ccxt/data/extract/test/test_exchange_class.py
@@ -1,26 +1,25 @@
 import logging
+import unittest.mock as umock
+from typing import Optional
 
 import pandas as pd
 import pytest
 
 import helpers.hdbg as hdbg
 import helpers.hunit_test as hunitest
-
-# TODO(Dan): return to code after CmTask43 is fixed.
 import im_v2.ccxt.data.extract.exchange_class as imvcdeexcl
 
 _LOG = logging.getLogger(__name__)
 
 
-@pytest.mark.skip(reason="CMTask961")
-class Test_CcxtExchange(hunitest.TestCase):
+class TestCcxtExchange1(hunitest.TestCase):
     def test_initialize_class(self) -> None:
         """
         Smoke test that the class is being initialized correctly.
         """
         _ = imvcdeexcl.CcxtExchange("binance")
 
-    def test_get_exchange_currencies(self) -> None:
+    def test_get_exchange_currency_pairs(self) -> None:
         """
         Test that a non-empty list of exchange currencies is loaded.
         """
@@ -32,23 +31,58 @@ class Test_CcxtExchange(hunitest.TestCase):
         self.assertGreater(len(curr_list), 0)
 
     @pytest.mark.slow()
-    def test_download_ohlcv_data(self) -> None:
+    @umock.patch.object(imvcdeexcl.hdateti, "get_current_time")
+    def test_download_ohlcv_data1(self, mock_get_current_time: umock.MagicMock):
         """
-        Test that historical data is being loaded correctly.
+        Test download for historical data.
+        """
+        mock_get_current_time.return_value = "2021-09-09 00:00:00.000000+00:00"
+        start_timestamp = pd.Timestamp("2021-09-09T00:00:00Z")
+        end_timestamp = pd.Timestamp("2021-09-10T00:00:00Z")
+        actual = self._download_ohlcv_data(start_timestamp, end_timestamp)
+        # Verify dataframe length.
+        self.assertEqual(1500, actual.shape[0])
+        # Check number of calls and args for current time.
+        self.assertEqual(mock_get_current_time.call_count, 3)
+        self.assertEqual(mock_get_current_time.call_args.args, ("UTC",))
+        # Verify corner datetime if output is not empty.
+        first_date = int(actual["timestamp"].iloc[0])
+        last_date = int(actual["timestamp"].iloc[-1])
+        self.assertEqual(1631145600000, first_date)
+        self.assertEqual(1631235540000, last_date)
+        # Check the output values.
+        actual = hunitest.convert_df_to_json_string(actual, n_tail=None)
+        self.check_string(actual)
+
+    @pytest.mark.slow()
+    def test_download_ohlcv_data2(self):
+        """
+        Test download for latest bars when no timestamps are provided.
+        """
+        actual = self._download_ohlcv_data(None, None)
+        # Verify dataframe length. Only one bar is obtained.
+        self.assertEqual(500, actual.shape[0])
+
+    def _download_ohlcv_data(
+        self,
+        start_timestamp: Optional[pd.Timestamp],
+        end_timestamp: Optional[pd.Timestamp],
+    ) -> pd.DataFrame:
+        """
+        Test that data is being loaded correctly.
+
+        Data is returned for further checking in different tests.
         """
         # Initiate class and set date parameters.
         exchange_class = imvcdeexcl.CcxtExchange("binance")
-        start_date = "2021-09-09T00:00:00Z"
-        end_date = "2021-09-10T00:00:00Z"
         # Extract data.
         actual = exchange_class.download_ohlcv_data(
             currency_pair="BTC/USDT",
-            start_timestamp=pd.Timestamp(start_date),
-            end_timestamp=pd.Timestamp(end_date),
+            start_timestamp=start_timestamp,
+            end_timestamp=end_timestamp,
         )
-        # Verify that the output is a dataframe and verify its length.
+        # Verify that the output is a dataframe.
         hdbg.dassert_isinstance(actual, pd.DataFrame)
-        self.assertEqual(1500, actual.shape[0])
         # Verify column names.
         exp_col_names = [
             "timestamp",
@@ -71,15 +105,8 @@ class Test_CcxtExchange(hunitest.TestCase):
             "float64",
             "object",
         ]
-        self.assertEqual(exp_col_types, col_types)
-        # Verify corner datetimes if output is not empty.
-        first_date = int(actual["timestamp"].iloc[0])
-        last_date = int(actual["timestamp"].iloc[-1])
-        self.assertEqual(1631145600000, first_date)
-        self.assertEqual(1631235540000, last_date)
-        # Check the output values.
-        actual_string = hunitest.convert_df_to_json_string(actual, n_tail=None)
-        self.check_string(actual_string)
+        self.assertListEqual(exp_col_types, col_types)
+        return actual
 
     def test_download_order_book(self):
         """
