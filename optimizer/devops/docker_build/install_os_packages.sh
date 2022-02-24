16ee8700f71757f11f172fa12cdbb5ca06211268
PomazkinG <pomazking@gmail.com>
2022-02-24 15:41:31 +0000

    checkpoint

--- optimizer/devops/docker_build/install_os_packages.sh
+++ optimizer/devops/docker_build/install_os_packages.sh
@@ -5,7 +5,7 @@
 
 set -ex
 
-FILE_NAME="optimzer/devops/docker_build/install_os_packages.sh"
+FILE_NAME="optimizer/devops/docker_build/install_os_packages.sh"
 echo "#############################################################################"
 echo "##> $FILE_NAME"
 echo "#############################################################################"
@@ -20,6 +20,19 @@ apt-get -y upgrade
 
 APT_GET_OPTS="-y --no-install-recommends"
 
+## TODO(gp): We can remove lots of this.
+#apt-get install $APT_GET_OPTS cifs-utils
+#apt-get install $APT_GET_OPTS git
+#apt-get install $APT_GET_OPTS keyutils
+#apt-get install $APT_GET_OPTS make
+
+## Install vim.
+#apt-get install $APT_GET_OPTS vim
+
+## This is needed to compile ujson.
+## See https://github.com/alphamatic/lm/issues/155
+#apt-get install $APT_GET_OPTS build-essential autoconf libtool python3-dev
+
 # Install pip.
 apt-get install $APT_GET_OPTS python3-venv python3-pip
 echo "PYTHON VERSION="$(python3 --version)
@@ -29,5 +42,42 @@ echo "PIP VERSION="$(pip3 --version)
 pip3 install poetry
 echo "POETRY VERSION="$(poetry --version)
 
+# Install homebrew.
+#apt-get install $APT_GET_OPTS build-essential procps curl file git
+
+# Install github CLI.
+# From https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+#DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+#apt-get install $APT_GET_OPTS software-properties-common
+#apt-get install $APT_GET_OPTS dirmngr
+#apt-get install $APT_GET_OPTS gpg-agent
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+#apt-add-repository https://cli.github.com/packages
+#apt-get update
+#apt-get install $APT_GET_OPTS gh
+#echo "GH VERSION="$(gh --version)
+
+## This is needed to install pygraphviz.
+## See https://github.com/alphamatic/amp/issues/1311
+## It needs tzdata so it needs to go after installing tzdata.
+#apt-get install $APT_GET_OPTS libgraphviz-dev
+#
+## This is needed to install dot.
+#apt-get install $APT_GET_OPTS graphviz
+#
+## This is needed for Postgres DB.
+#apt-get install $APT_GET_OPTS postgresql-client
+
 # Install sudo.
 apt-get install $APT_GET_OPTS sudo
+
+# Clean up.
+if [[ $CLEAN_UP_INSTALLATION ]]; then
+    echo "Cleaning up installation..."
+    apt-get purge -y --auto-remove
+    DIRS="/root/.cache /tmp/*"
+    du -hs $DIRS | sort -h
+    rm -rf $DIRS
+else
+    echo "No clean up installation"
+fi;
