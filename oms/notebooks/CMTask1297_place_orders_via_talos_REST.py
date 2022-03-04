473a1d9cad897d40d49781a3444ab1c01ecd37d2
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-03-04 20:02:57 +0000

    CMTask1297: Finalize notebook

--- oms/notebooks/CMTask1297_place_orders_via_talos_REST.py
+++ oms/notebooks/CMTask1297_place_orders_via_talos_REST.py
@@ -40,6 +40,8 @@ import datetime
 import helpers.hsql as hsql
 import im_v2.im_lib_tasks as imvimlita
 import uuid
+import helpers.hsecrets as hsecret
+from urllib.parse import urlparse, urlencode
 
 # %%
 hdbg.init_logger(verbosity=logging.DEBUG)
@@ -57,7 +59,6 @@ def calculate_signature(api_secret, parts):
     """
     A signature required for some types of GET and POST requests.
 
-    Not required for historical data.
     """
     payload = "\n".join(parts)
     hash = hmac.new(
@@ -76,216 +77,125 @@ def timestamp_to_tz_naive_ISO_8601(timestamp: pd.Timestamp) -> str:
 
     Note: microseconds must be included.
     """
-    hdateti.dassert_is_tz_naive(timestamp)
+    #hdateti.dassert_is_tz_naive(timestamp)
     timestamp_iso_8601 = timestamp.isoformat(timespec="microseconds") + "Z"
     return timestamp_iso_8601
 
-def get_orders(host: str, path: str, query: str, signature: str) -> pd.DataFrame:
+def get_orders(endpoint: str, path: str, public_key: str, secret_key: str) -> pd.DataFrame:
     """
     Load data from given path.
+    
+    Loads all orders up to the moment of request
     """
-    headers = {"TALOS-KEY": key_talos}
-    # Example of full url:
-    #  https://sandbox.talostrading.com/v1/symbols/BTC-USDT/markets/binance/ohlcv/1m?startDate=2022-02-24T19:21:00.000000Z&startDate=2022-02-24T19:25:00.000000Z&limit=100
-    url = f"https://{host}{path}{query}"
-    r = requests.get(url=url, params={}, headers=headers)
-    if r.status_code == 200:
-        data = r.json()["data"]
-
-    return pd.DataFrame(data)
-
-
-# %% [markdown]
-# ### Small Q&A
-#
-# - How to get a list of order IDs?
-# - How to load all orders (not just IDs)?
-# - How to post an order to buy?
-# - How to post an order to sell?
-# - How to find out the status of the order?
-# - ** How to link the API to a wallet?
-
-# %% [markdown]
-# ### How to load orders?
-# https://docs.talostrading.com/#get-an-order-rest
-#
-# See the header for orders - how to pass a signature?
-#
-# `GET /v1/orders/`
-
-# %%
-def get_orders(host: str, path: str, query: str, signature: str) -> pd.DataFrame:
-    """
-    Load data from given path.
-    """
-    headers = {"TALOS-KEY": key_talos}
-    # Example of full url:
-    url = f"https://{host}{path}{query}"
-    r = requests.get(url=url, params={}, headers=headers)
-    if r.status_code == 200:
-        data = r.json()["data"]
-
-    return pd.DataFrame(data)
-
-
-# %%
-key_talos = "CRYEY4S913H3"
-secret_talos = "***REMOVED***"
-
-# %%
-from urllib.parse import urlparse, urlencode
-
-# %%
-utc_datetime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000000Z")
-endpoint = "tal-87.sandbox.talostrading.com"
-path = "/v1/orders"
-parts = [
+    utc_datetime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000000Z")
+    # TODO(Danya): Add time query (startDate and endDate)
+    # TODO(Danya): Factor out the general form of a GET request (common with OHLCV)
+    # TODO(Danya): Factor out the general part of TALOS authorization.
+    # Note: some kind of query is required.
+    query = {"EndDate": utc_datetime}
+    query_string = urlencode(query)
+    print(utc_datetime)
+    get_request_parts = [
     "GET",
     utc_datetime,
     endpoint,
     path,
+    query_string
 ]
-query = {"EndDate": "2022-03-01T19:30:10.000000Z"}
-query_string = urlencode(query)
-if query_string:
-    parts.append(query_string)
-signature = calculate_signature(secret_talos, parts)
-
-headers = {
-    "TALOS-KEY": key_talos,
-    "TALOS-SIGN": signature,
-    "TALOS-TS": utc_datetime,
-}
-url = f"https://{endpoint}{path}"
-if query_string:
-    url += "?" + query_string
-r = requests.get(url=url, headers=headers)
-if r.status_code == 200:
-    data = r.json()
-else:
-    raise Exception(f"{r.status_code}: {r.text}")
-
-# %%
-data
-
-# %%
-order = {
-  "type": "NewOrderSingle",
-  "data": [
-    {
-      "ClOrdID": "bffd1c40-dcc3-4817-b61c-4185912e99fa",
-      "Markets": ["coinbase"],
-      "OrdType": "Limit",
-      "OrderQty": "0.1000",
-      "Price": "11100.00",
-      "ExpectedFillQty": "0.1000",
-      "ExpectedFillPrice": "11100.00",
-      "Side": "Sell",
-      "Strategy": "Limit",
-      "Symbol": "BTC-USD",
-      "TimeInForce": "GoodTillCancel",
-      "TransactTime": "2019-09-17T17:46:28.000000Z",
-      "SessionID": "1109RQ13KXR00",
-      "CancelSessionID": "1109RQ13KXR00",
-      "SubAccount": "SubAccount",
-      "Group": "groupA"
+    signature = calculate_signature(secret_key, get_request_parts)
+    # TODO(*): Get secrets from hsecrets.
+    headers = {
+    "TALOS-KEY": public_key,  # API public key
+    "TALOS-SIGN": signature, # an encoded secret key + request
+    "TALOS-TS": utc_datetime, # Time of request UTC.
     }
-  ]
-}
+    # TODO(Danya): Factor out 
+    url = f"https://{endpoint}{path}?{query_string}"
+    print(url)
+    r = requests.get(url=url, headers=headers)
+    if r.status_code == 200:
+        data = r.json()
+    else:
+        raise Exception(f"{r.status_code}: {r.text}")
+    return data
 
-# %%
-"""
-Perform a signed POST request to the Talos API
-@param api_key: Talos API Key
-@param api_secret: Talos API Secret
-@param scheme: URL scheme, e.g. https
-@param endpoint: hostname to connect to, e.g. tal-1.sandbox.talostrading.com
-@param path: path for request, e.g. /v1/orders
-@param data: body to submit
-@return response as an object
-@raise an exception if the status code is not 200
-"""
-url = f"https://{endpoint}{path}"
-utc_datetime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000000Z")
-parts = [
-    "POST",
-    utc_datetime,
-    endpoint,
-    path,
-]
-if query:
-    query_string = urlencode(query)
-    url += f"?{query_string}"
-    parts.append(query_string)
+def get_talos_api_keys(mode: str = "sandbox"):
+    if mode=="sandbox":
+        api_keys = hsecret.get_secret("talos_sandbox")
+    return api_keys
 
-if data:
-    body = json.dumps(data)
-    parts.append(body)
-
-signature = calculate_signature(api_secret, parts)
-
-headers = {
-    "TALOS-KEY": api_key,
-    "TALOS-SIGN": signature,
-    "TALOS-TS": utc_datetime,
-}
-
-r = requests.post(url=url, data=body, headers=headers)
-if r.status_code == 200:
-    data = r.json()
-else:
-    raise Exception(f"{r.status_code}: {r.text}")
-
-
-# %%
-def new_cl_ord_id():
-    """ Returns a new ClOrdID. """
+def get_cl_ord_id():
+    """ Create a ClOrdID for the POST request. """
     return str(uuid.uuid4())
 
+def create_order(timestamp_ISO8601: str):
+    # TODO(Danya): Add arguments: quantity, markets (exchanges), order type, etc.
+    # TODO(Danya): required types of order: limit, VWAP, TWAP; TimeInForce should have "GoodUntil" passed.
+    order = {
+        "ClOrdID": get_cl_ord_id(),
+        "Markets": ["binance"],
+        "OrderQty": "1.0000",
+        "Symbol": "BTC-USDT",
+        "Currency": "BTC",
+        "TransactTime": timestamp_ISO8601, # Should always be the utcnow() with Talos date formatting.
+        "OrdType": "Limit",
+        "TimeInForce": "GoodTillCancel",
+        "Price": "5.81",
+        "Side": "Buy",
+    }
+    return order
+
+
+def post_order(endpoint: str, path: str, public_key: str, secret_key: str):
+    # TODO(Danya): Factor out the statement.
+    utc_datetime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000000Z")
+    parts = [
+        "POST",
+        utc_datetime,
+        "tal-87.sandbox.talostrading.com",
+        "/v1/orders",
+    ]
+    # TODO(Danya): Create order from outside with specific arguments
+    order = create_order(utc_datetime)
+    print(order)
+    body = json.dumps(order)
+    parts.append(body)
+    # Enciode request with secret key.
+    signature = calculate_signature(secret_key, parts)
+    headers = {
+        "TALOS-KEY": public_key,
+        "TALOS-SIGN": signature,
+        "TALOS-TS": utc_datetime,
+    }
+    # Create a POST request.
+    url = f"https://{endpoint}{path}"
+    r = requests.post(url=url, data=body, headers=headers)
+    if r.status_code != 200:
+        Exception(f"{r.status_code}: {r.text}")
+    return r.status_code
+
+
+# %% [markdown]
+# ### Setup
 
 # %%
-new_cl_ord_id()
+# Imitation of script input parameters.
+# Common elements of both GET and POST requests.
+api_keys = get_talos_api_keys()
+endpoint = "tal-87.sandbox.talostrading.com" # our sandbox endpoint
+path = "/v1/orders" # path for all data related to placin orders
+
+# %% [markdown]
+# ### How to load orders?
+# https://docs.talostrading.com/#get-an-order-rest
 
 # %%
-order_request = {
-    "ClOrdID": new_cl_ord_id(),
-    "Markets": ["binance"],
-    "OrderQty": "1.0000",
-    "Symbol": "BTC-USDT",
-    "Currency": "BTC",
-    "TransactTime": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000000Z"),
-    "OrdType": "Limit",
-    "TimeInForce": "GoodTillCancel",
-    "Price": str("43481.81"),
-    "Side": "Buy",
-}
+get_orders(endpoint, path, api_keys["apiKey"], api_keys["secret"])
+
+# %% [markdown]
+# ### Post an order
 
 # %%
-url = f"https://{endpoint}{path}"
-utc_datetime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.000000Z")
-parts = [
-    "POST",
-    utc_datetime,
-    "tal-87.sandbox.talostrading.com",
-    "/v1/orders",
-]
-body = json.dumps(order_request)
-parts.append(body)
-signature = calculate_signature(secret_talos, parts)
-
-headers = {
-    "TALOS-KEY": key_talos,
-    "TALOS-SIGN": signature,
-    "TALOS-TS": utc_datetime,
-}
-
-r = requests.post(url=url, data=body, headers=headers)
-if r.status_code == 200:
-    r.json()
-else:
-    Exception(f"{r.status_code}: {r.text}")
-
-# %%
-r.status_code
+post_order(endpoint, path, api_keys["apiKey"], api_keys["secret"])
 
 # %%
