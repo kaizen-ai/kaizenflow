458ca9716fea619b11c46ceeed6b39f7a2e18972
jsmerix <smeriga.juraj@gmail.com>
2022-03-04 20:56:03 +0000

    Fix minor issues after PR review

--- im_v2/talos/data/extract/exchange_class.py
+++ im_v2/talos/data/extract/exchange_class.py
@@ -1,4 +1,6 @@
 """
+This file contains a class for providing interface to download data from Talos broker.
+
 Import as:
 
 import im_v2.talos.data.extract.exchange_class as imvtdeexcl
@@ -17,8 +19,6 @@ import helpers.hsecrets as hsecret
 
 _LOG = logging.getLogger(__name__)
 
-_TALOS_HOST = "talostrading.com"
-
 
 class TalosExchange:
     """
@@ -33,8 +33,9 @@ class TalosExchange:
         Constructor.
 
         :param environment: specify if this instance should call the 'sandbox'
-        or 'prod' API
+          or 'prod' API
         """
+        _TALOS_HOST = "talostrading.com"
         hdbg.dassert_in(environment, ["sandbox", "prod"])
         keys = hsecret.get_secret(f"talos_{environment}")
         self._api_host = (
@@ -42,22 +43,6 @@ class TalosExchange:
         )
         self._api_key = keys["apiKey"]
         self._api_secret = keys["secret"]
-
-    # TODO(Juraj): not needed for now
-    # def calculate_signature(parts: Optional[List[str]]) -> str:
-    #     """
-    #     Compute a signature required for some types of GET and POST requests.
-    #     (Not required for historical data.)
-
-    #     :param parts: parts of the query to calculate signature from
-    #     """
-    #     payload = "\n".join(parts)
-    #     hash = hmac.new(
-    #         self._api_secret.encode("ascii"), payload.encode("ascii"), hashlib.sha256
-    #     )
-    #     hash.hexdigest()
-    #     signature = base64.urlsafe_b64encode(hash.digest()).decode()
-    #     return signature
 
     def build_talos_ohlcv_path(
         self, currency_pair: str, exchange: str, *, resolution: str = "1m"
@@ -73,9 +58,9 @@ class TalosExchange:
         )
         return data_path
 
-    def timestamp_to_tz_naive_ISO_8601(self, timestamp: pd.Timestamp) -> str:
+    def timestamp_to_talos_iso_8601(self, timestamp: pd.Timestamp) -> str:
         """
-        Transform Timestamp into a string in format accepted by Talos API.
+        Transform Timestamp into a string in the format accepted by Talos API.
 
         Example:
         2019-10-20T15:00:00.000000Z
@@ -101,16 +86,17 @@ class TalosExchange:
                     "endDate=2019-10-23:28:0.000000Z,
                     "limit": 10000
                  }
+
+        Note that endDate is an open interval, i.e. endDate is NOT included
+        in the response.
+
+        :param limit: number of records to return in request response
         """
-        # TODO(Danya): Note: end timestamp is NOT included.
-        # Start
         params: Dict[str, Union[str, int]] = {}
-        if start_timestamp:
-            start_date = self.timestamp_to_tz_naive_ISO_8601(start_timestamp)
-            params["startDate"] = start_date
-        if end_timestamp:
-            end_date = self.timestamp_to_tz_naive_ISO_8601(end_timestamp)
-            params["endDate"] = end_date
+        start_date = self.timestamp_to_talos_iso_8601(start_timestamp)
+        params["startDate"] = start_date
+        end_date = self.timestamp_to_talos_iso_8601(end_timestamp)
+        params["endDate"] = end_date
         params["limit"] = limit
         return params
 
@@ -118,8 +104,8 @@ class TalosExchange:
         self,
         currency_pair: str,
         exchange: str,
-        start_timestamp: Optional[pd.Timestamp],
-        end_timestamp: Optional[pd.Timestamp],
+        start_timestamp: pd.Timestamp,
+        end_timestamp: pd.Timestamp,
         *,
         bar_per_iteration: int = 10000,
     ) -> pd.DataFrame:
@@ -130,9 +116,9 @@ class TalosExchange:
         :param currency_pair: a currency pair, e.g. "BTC_USDT"
         :param exchange: crypto exchange, e.g. "binance"
         :param start_timestamp: starting point for data
-        :param end_timestamp: end point for data (included)
+        :param end_timestamp: end point for data (NOT included)
         :param bar_per_iteration: number of bars per iteration
-        :return: Dataframe with OHLCV data from Talos
+        :return: dataframe with OHLCV data
         """
         # TODO(Juraj): we can implement this check later if needed.
         # hdbg.dassert_in(
@@ -140,7 +126,32 @@ class TalosExchange:
         #     self.currency_pairs,
         #     "Currency pair is not present in exchange",
         # )
+        return self._fetch_ohlcv(
+            currency_pair,
+            exchange,
+            start_timestamp,
+            end_timestamp,
+            bar_per_iteration=bar_per_iteration,
+        )
 
+    def _fetch_ohlcv(
+        self,
+        currency_pair: str,
+        exchange: str,
+        start_timestamp: pd.Timestamp,
+        end_timestamp: pd.Timestamp,
+        *,
+        bar_per_iteration: int = 10000,
+    ) -> pd.DataFrame:
+        """
+        Fetch OHLCV data for given currency and time.
+
+         Example of full API request URL:
+          https://sandbox.talostrading.com/v1/symbols/BTC-USDT/markets/binance/ohlcv/1m?startDate=2022-02-24T19:21:00.000000Z&startDate=2022-02-24T19:25:00.000000Z&limit=100
+        url = f"https://{self._api_host}{path}{query}"
+
+        :return: Dataframe with OHLCV data
+        """
         # Verify that date parameters are of correct format.
         hdbg.dassert_isinstance(
             end_timestamp,
@@ -154,32 +165,6 @@ class TalosExchange:
             start_timestamp,
             end_timestamp,
         )
-        return self._fetch_ohlcv(
-            currency_pair,
-            exchange,
-            start_timestamp,
-            end_timestamp,
-            bar_per_iteration=bar_per_iteration,
-        )
-
-    def _fetch_ohlcv(
-        self,
-        currency_pair: str,
-        exchange: str,
-        start_timestamp: Optional[pd.Timestamp],
-        end_timestamp: Optional[pd.Timestamp],
-        *,
-        bar_per_iteration: int = 10000,
-    ) -> pd.DataFrame:
-        """
-        Fetch OHLCV data for given currency and time.
-
-         Example of full API request URL:
-          https://sandbox.talostrading.com/v1/symbols/BTC-USDT/markets/binance/ohlcv/1m?startDate=2022-02-24T19:21:00.000000Z&startDate=2022-02-24T19:25:00.000000Z&limit=100
-        url = f"https://{self._api_host}{path}{query}"
-
-        :return: Dataframe with OHLCV data from Talos
-        """
         headers = {"TALOS-KEY": self._api_key}
         params = self.build_talos_query_params(
             start_timestamp, end_timestamp, limit=bar_per_iteration
@@ -187,9 +172,9 @@ class TalosExchange:
         path = self.build_talos_ohlcv_path(currency_pair, exchange)
         url = f"https://{self._api_host}{path}"
 
-        hasNext = True
+        has_next = True
         dfs = []
-        while hasNext:
+        while has_next:
             r = requests.get(url=url, params=params, headers=headers)
             if r.status_code == 200:
                 data = r.json()["data"]
@@ -199,16 +184,16 @@ class TalosExchange:
                     hdateti.get_current_time("UTC")
                 )
                 dfs.append(df)
-                hasNext = "next" in r.json()
+                has_next = "next" in r.json()
                 # Handle pagination, details at:
                 # https://docs.talostrading.com/#historical-ohlcv-candlesticks-rest
-                if hasNext:
+                if has_next:
                     params["after"] = r.json()["next"]
             else:
                 raise ValueError(
                     f"Request: {r.url} \n Finished with code: {r.status_code}"
                 )
-
+        # Assemble the results in a dataframe.
         columns = [
             "timestamp",
             "open",
