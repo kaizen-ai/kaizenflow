b105169011541c2d9e99c6d8f85ab063deeed7b4
Juraj Smeriga <49269742+jsmerix@users.noreply.github.com>
2022-03-08 11:51:20 +0100

    Add funnction to return create query for talos ohlcv table (#1335)

---
+++ im_v2/talos/db/utils.py
@@ -0,0 +1,34 @@
+"""
+Utilities for working with Talos related database tables.
+
+Import as:
+
+import im_v2.talos.db.utils as imvtadbut
+"""
+
+import logging
+
+_LOG = logging.getLogger(__name__)
+
+
+def get_talos_ohlcv_create_table_query() -> str:
+    """
+    Get SQL query to create CCXT OHLCV table.
+    """
+    query = """
+    CREATE TABLE IF NOT EXISTS talos_ohlcv(
+            id SERIAL PRIMARY KEY,
+            timestamp BIGINT NOT NULL,
+            open NUMERIC,
+            high NUMERIC,
+            low NUMERIC,
+            close NUMERIC,
+            volume NUMERIC,
+            ticks NUMERIC,
+            currency_pair VARCHAR(255) NOT NULL,
+            exchange_id VARCHAR(255) NOT NULL,
+            end_download_timestamp TIMESTAMP,
+            knowledge_timestamp TIMESTAMP
+            )
+            """
+    return query
