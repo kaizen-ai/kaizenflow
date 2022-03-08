4db5ab1b897611339d364ba7af49ecfd865a5c40
Juraj Smeriga <49269742+jsmerix@users.noreply.github.com>
2022-03-08 17:02:24 +0100

    Add script for realtime download from talos (#1347)
    Co-authored-by: Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>

---
+++ im_v2/talos/data/extract/download_realtime_for_one_exchange.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+"""
+Script to download OHLCV data for a single exchange from Talos.
+
+Use as:
+
+# Download OHLCV data for binance 'v03', saving dev_stage:
+> im_v2/talos/data/extract/download_realtime_for_one_exchange.py \
+    --start_timestamp '20211110-101100' \
+    --end_timestamp '20211110-101200' \
+    --exchange_id 'binance' \
+    --universe 'v03' \
+    --db_stage 'dev' \
+    --api_stage 'sandbox' \
+    --aws_profile 'ck' \
+    --s3_path 's3://cryptokaizen-data/real_time/talos'
+
+Import as:
+
+import im_v2.talos.data.extract.download_realtime_for_one_exchange as imvtdedrfoe
+"""
+
+import argparse
+import logging
+import os
+
+import pandas as pd
+
+import helpers.hdatetime as hdateti
+import helpers.hdbg as hdbg
+import helpers.hparser as hparser
+import helpers.hs3 as hs3
+import helpers.hsql as hsql
+import im_v2.talos.data.extract.exchange_class as imvtdedrfoe
+import im_v2.ccxt.universe.universe as imvccunun
+import im_v2.im_lib_tasks as imvimlita
+
+_LOG = logging.getLogger(__name__)
+
+
+def _parse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--start_timestamp",
+        action="store",
+        required=True,
+        type=str,
+        help="Beginning of the downloaded period",
+    )
+    parser.add_argument(
+        "--end_timestamp",
+        action="store",
+        required=True,
+        type=str,
+        help="End of the downloaded period",
+    )
+    parser.add_argument(
+        "--exchange_id",
+        action="store",
+        required=True,
+        type=str,
+        help="Name of exchange to download data from",
+    )
+    parser.add_argument(
+        "--universe",
+        action="store",
+        required=True,
+        type=str,
+        help="Trade universe to download data for",
+    )
+    parser.add_argument(
+        "--db_stage",
+        action="store",
+        required=True,
+        type=str,
+        help="DB stage to use",
+    )
+    parser.add_argument(
+        "--db_table",
+        action="store",
+        required=False,
+        default="talos_ohlcv",
+        type=str,
+        help="(Optional) DB table to use, default: 'talos_ohlcv'",
+    )
+    parser.add_argument(
+        "--api_stage",
+        action="store",
+        required=False,
+        default="sandbox",
+        type=str,
+         help="(Optional) API 'stage' to use ('sandbox' or 'prod'), default: 'sandbox'",
+    )
+    parser.add_argument("--incremental", action="store_true")
+    parser = hparser.add_verbosity_arg(parser)
+    parser = hs3.add_s3_args(parser)
+    return parser  # type: ignore[no-any-return]
+
+def _run(args: argparse.Namespace):
+    # Connect to database.
+    env_file = imvimlita.get_db_env_path(args.db_stage)
+    connection_params = hsql.get_connection_info_from_env_file(env_file)
+    connection = hsql.get_connection(*connection_params)
+    # Connect to S3 filesystem, if provided.
+    if args.aws_profile:
+        fs = hs3.get_s3fs(args.aws_profile)
+    # Initialize exchange class.
+    exchange = imvtdedrfoe.TalosExchange(args.api_stage)
+    # Load currency pairs.
+    universe = imvccunun.get_trade_universe(args.universe)
+    currency_pairs = universe["CCXT"][args.exchange_id]
+    # Load DB table to work with
+    db_table = args.db_table
+    # Generate a query to remove duplicates.
+    dup_query = hsql.get_remove_duplicates_query(
+        table_name=db_table,
+        id_col_name="id",
+        column_names=["timestamp", "exchange_id", "currency_pair"],
+    )
+    # Convert timestamps.
+    start_timestamp = pd.Timestamp(args.start_timestamp)
+    end_timestamp = pd.Timestamp(args.end_timestamp)
+    # Download data for specified time period.
+    for currency_pair in currency_pairs:
+        data = exchange.download_ohlcv_data(
+            currency_pair,
+            args.exchange_id,
+            start_timestamp=start_timestamp,
+            end_timestamp=end_timestamp,
+        )
+        # Assign pair and exchange columns.
+        data["currency_pair"] = currency_pair
+        data["exchange_id"] = args.exchange_id
+        # Get timestamp of insertion in UTC.
+        data["knowledge_timestamp"] = hdateti.get_current_time("UTC")
+        # Insert data into the DB.
+        hsql.execute_insert_query(
+            connection=connection,
+            obj=data,
+            table_name=db_table,
+        )
+        # Save data to S3 bucket.
+        if args.s3_path:
+            # Get file name.
+            file_name = (
+                    currency_pair
+                    + "_"
+                    + hdateti.get_current_timestamp_as_string("UTC")
+                    + ".csv"
+            )
+            path_to_file = os.path.join(args.s3_path, args.exchange_id, file_name)
+            # Save data to S3 filesystem.
+            with fs.open(path_to_file, "w") as f:
+                data.to_csv(f, index=False)
+        # Remove duplicated entries.
+        connection.cursor().execute(dup_query)
+
+
+def _main(parser: argparse.ArgumentParser) -> None:
+    args = parser.parse_args()
+    hdbg.init_logger(verbosity=args.log_level, use_exec_path=True)
+    _run(args)
+
+
+if __name__ == "__main__":
+    _main(_parse())
