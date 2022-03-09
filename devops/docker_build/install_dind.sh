dff6e599746111e006270695ae37879ddcf4379a
PomazkinG <pomazking@gmail.com>
2022-03-09 19:03:03 +0000

    init commit

--- devops/docker_build/install_dind.sh
+++ devops/docker_build/install_dind.sh
@@ -20,5 +20,13 @@ apt update
 apt-cache policy docker-ce
 apt install -y docker-ce
 
-# Install docker-compose.
-apt install -y docker-compose
+# Install the latest version of `docker-compose`.
+# `https://docs.docker.com/compose/install/`.
+COMPOSE_LATEST_VERSION=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+COMPOSE_DIR="/usr/bin/docker-compose"
+curl -L https://github.com/docker/compose/releases/download/$COMPOSE_LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m) -o $COMPOSE_DIR
+# Apply executable permissions to the binary.
+chmod +x $COMPOSE_DIR
+# Check version.
+COMPOSE_VERSION=$(docker-compose --version)
+echo "docker-compose-version=$COMPOSE_VERSION"
