d6032b7a6841a2115f0412ced855822f687cc064
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-03-15 14:06:00 +0000

    CMTask1336: Finish the submit order methods

--- oms/talos_broker.py
+++ oms/talos_broker.py
@@ -13,6 +13,7 @@ import hmac
 import json
 import logging
 import uuid
+import urllib
 from typing import Any, Dict, List
 
 import requests
@@ -28,12 +29,23 @@ class TalosBroker(ombroker.AbstractBroker):
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.api_keys = hsecret.get_secret(self._account)
-        self.endpoint = self.get_endpoint()
+        self._api_keys = hsecret.get_secret(self._account)
+        self._endpoint = self.get_endpoint()
 
-    def calculate_signature(self, parts) -> str:
+    def calculate_signature(self, parts: List[str]) -> str:
         """
-        Encode the request.
+        Encode the request using Talos secret key.
+
+        Requires parts of the API request provided as a list, e.g.:
+        [
+        "POST",
+        str(utc_datetime),
+        "tal-87.sandbox.talostrading.com",
+        "/v1/orders",
+        ]
+
+        :param parts: parts of the GET or POST request
+        :returns: an encoded string
         """
         payload = "\n".join(parts)
         hash = hmac.new(
@@ -52,11 +64,12 @@ class TalosBroker(ombroker.AbstractBroker):
         Based on the 'account' value.
         """
         if self._account == "talos_sandbox":
-            return "tal-87.sandbox.talostrading.com"
+            endpoint = "tal-87.sandbox.talostrading.com"
         else:
             hdbg.dfatal(
                 "Incorrect account type. Supported account types: 'talos_sandbox'."
             )
+        return endpoint
 
     def create_order(
         self,
@@ -71,11 +84,15 @@ class TalosBroker(ombroker.AbstractBroker):
     ) -> Dict[str, Any]:
         """
         Create an order.
+
+        Note: Currently acts a placeholder to demonstrate
+        the format of Talos order.
         """
         # TODO(Danya): Adapt to `oms.order.Order` type,
         #  e.g. convert Order to a supported Talos format.
         # TODO(Danya): Add assertions for order types and trading strategies.
         # TODO(Danya): Connect to `strategy` parameter?
+        # TODO(Danya): Pass the order information as a config.
         order = {
             "ClOrdID": self.get_order_id(),
             # E.g. `["binance", "coinbase"]`.
@@ -95,15 +112,15 @@ class TalosBroker(ombroker.AbstractBroker):
         }
         return order
 
-    def submit_orders(
+    def submit_order(
         self,
         orders: List[Dict[str, Any]],
         *,
-        dry_run: bool = False,
     ) -> None:
         """
-        Submit and log orders given by the model.
+        Submit and log multiple orders given by the model.
         """
+        # TODO(Danya): Merge with `market_data` wall clock time
         wall_clock_timestamp = self.get_talos_current_utc_timestamp()
         _LOG.debug("Submitting %d orders", len(orders))
         for order in orders:
@@ -124,6 +141,7 @@ class TalosBroker(ombroker.AbstractBroker):
 
         Example: 2019-10-20T15:00:00.000000Z
         """
+        # TODO(Danya): Merge with wall_clock_timestamp method.
         utc_datetime = datetime.datetime.utcnow().strftime(
             "%Y-%m-%dT%H:%M:%S.000000Z"
         )
@@ -148,15 +166,30 @@ class TalosBroker(ombroker.AbstractBroker):
         # Enciode request with secret key.
         signature = self.calculate_signature(parts)
         headers = {
-            "TALOS-KEY": self.api_keys["apiKey"],
+            "TALOS-KEY": self._api_keys["apiKey"],
             "TALOS-SIGN": signature,
             "TALOS-TS": wall_clock_timestamp,
         }
         # Create a POST request.
-        url = f"https://{self.endpoint}{path}"
+        url = f"https://{self._endpoint}{path}"
         r = requests.post(url=url, data=body, headers=headers)
         # TODO(Danya): Return a receipt instead of a status code.
         if r.status_code != 200:
             # TODO(Danya): Remove Exception.
             Exception(f"{r.status_code}: {r.text}")
         return r.status_code
+
+    def get_orders(self, start_timestamp: str, end_timestamp: str,):
+        utc_datetime = datetime.datetime.utcnow().strftime(
+            "%Y-%m-%dT%H:%M:%S.000000Z"
+        )
+        # TODO(Danya): Add time query (startDate and endDate)
+        # TODO(Danya): Factor out the general form of a GET request (common with OHLCV)
+        # TODO(Danya): Factor out the general part of TALOS authorization.
+        # Note: some kind of query is required.
+        utc_datetime = datetime.datetime.utcnow().strftime(
+            "%Y-%m-%dT%H:%M:%S.000000Z"
+        )
+        # Note: some kind of query is required.
+        query = {"StartDate": start_timestamp, "EndDate": end_timestamp}
+        query_string = urllib.parse.urlencode(query)
