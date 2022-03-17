7565d54130036b7e8cb8edd4939a91736a2bb036
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-03-17 18:30:18 +0300

    Cm task1336 move talos to library (#1367)
    * CMTask1336: Initialize broker class lib
    
    * CMTask1336: Initialize broker class lib
    
    * CMTask1336: Inherit from AbstractBroker
    
    * CMTask1336: Add more basic methods
    
    * CMTask1336: Finish the submit order methods
    
    * CMTask1336: Finish the submit order methods
    
    * CMTask1336: Checkpoint (get_orders)
    
    * CMTask1336: Lint
    
    * CMTask1336: Checkpoint
    
    * CMTask1336: Checkpoint
    
    * CMTask1336: Move API-related functions to utils lib
    
    * CMTask1336: Add a line

--- oms/oms_utils.py
+++ oms/oms_utils.py
@@ -3,8 +3,13 @@ Import as:
 
 import oms.oms_utils as oomsutil
 """
+import base64
 import collections
+import datetime
+import hashlib
+import hmac
 import logging
+import uuid
 from typing import Dict, List
 
 import numpy as np
@@ -74,3 +79,70 @@ def _append_accounting_df(
     # Concat all the data together with the input.
     df_out = pd.concat([df] + dfs, axis=1)
     return df_out
+
+
+# #############################################################################
+# Accounting functions.
+# #############################################################################
+
+
+def calculate_signature(secret_key: str, parts: List[str]) -> str:
+    """
+    Encode the request using secret key.
+
+    Requires parts of the API request provided as a list, e.g.:
+
+    ```
+    [
+        "POST",
+        str(utc_datetime),
+        "tal-87.sandbox.talostrading.com",
+        "/v1/orders",
+    ]
+    ```
+
+    :param secret_key: secret key used for encoding
+    :param parts: parts of the GET or POST request
+    :return: an encoded string
+    """
+    payload = "\n".join(parts)
+    hash = hmac.new(
+        secret_key.encode("ascii"),
+        payload.encode("ascii"),
+        hashlib.sha256,
+    )
+    hash.hexdigest()
+    signature = base64.urlsafe_b64encode(hash.digest()).decode()
+    return signature
+
+
+def get_endpoint(account: str) -> str:
+    """
+    Get entrypoint to Talos API.
+    """
+    if account == "talos_sandbox":
+        endpoint = "tal-87.sandbox.talostrading.com"
+    else:
+        hdbg.dfatal(
+            "Incorrect account type. Supported account types: 'talos_sandbox'."
+        )
+    return endpoint
+
+
+def get_order_id() -> str:
+    """
+    Get an order ID in UUID4 format.
+    """
+    return str(uuid.uuid4())
+
+
+def get_talos_current_utc_timestamp() -> str:
+    """
+    Return the current UTC timestamp in Talos-acceptable format.
+
+    Example: 2019-10-20T15:00:00.000000Z
+    """
+    utc_datetime = datetime.datetime.utcnow().strftime(
+        "%Y-%m-%dT%H:%M:%S.000000Z"
+    )
+    return utc_datetime
