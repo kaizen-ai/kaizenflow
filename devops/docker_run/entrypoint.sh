3d43b54acbd5aa95eea4803cd5dd537a07fb3650
Viktor Andriichuk <vandriichuk@appen.com>
2022-03-19 19:49:28 +0200

    Use heredocs syntax

--- devops/docker_run/entrypoint.sh
+++ devops/docker_run/entrypoint.sh
@@ -45,20 +45,20 @@ if [[ $ENABLE_DIND == 1 ]]; then
     # TODO(Grisha): give permissions to the `docker` group only and not to everyone, i.e. `666`.
 
     python3 <<- 'EOF'
-	import pathlib
-	import subprocess
-	import asyncio
+    import pathlib
+    import subprocess
+    import asyncio
 
-	path = pathlib.Path('/var/run/docker.sock')
-	async def task():
+    path = pathlib.Path('/var/run/docker.sock')
+    async def task():
 	    while not path.exists():
-	        await asyncio.sleep(0.1)
+	      await asyncio.sleep(0.1)
 	    subprocess.call(['chmod', '0666', path])
 
-	loop = asyncio.get_event_loop()
-	cors = asyncio.wait([task()])
-	loop.run_until_complete(cors)
-	EOF
+    loop = asyncio.get_event_loop()
+    cors = asyncio.wait([task()])
+    loop.run_until_complete(cors)
+  EOF
 
 fi;
 
