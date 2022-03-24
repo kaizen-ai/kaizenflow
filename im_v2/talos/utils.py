e14e3c5dc7e74f269836d73d459497c17b867b9d
max-rsrch <sergeychikovm@gmail.com>
2022-03-24 09:52:29 +0000

    Apply changes

--- im_v2/talos/utils.py
+++ im_v2/talos/utils.py
@@ -6,6 +6,7 @@ import im_v2.talos.utils as imv2tauti
 import datetime
 import logging
 
+import helpers.hdatetime as hdateti
 import helpers.hdbg as hdbg
 
 _LOG = logging.getLogger(__name__)
@@ -14,23 +15,29 @@ _TALOS_HOST = "talostrading.com"
 
 def get_endpoint(environment: str) -> str:
     """
-    Get entrypoint to Talos.
+    Get entrypoint to Talos. The only environment we currently support is
+    `sandbox`.
+
+    :param environment: i.e., `sandbox`
+    :return:
     """
-    hdbg.dassert_in(environment, ["sandbox", "prod"])
     if environment == "sandbox":
         endpoint = f"sandbox.{_TALOS_HOST}"
     else:
-        hdbg.dfatal("Incorrect account type. Supported environment: 'sandbox'.")
+        hdbg.dfatal("Incorrect account type. Supported environments: 'sandbox'.")
     return endpoint
 
 
 def get_talos_current_utc_timestamp() -> str:
     """
-    Return the current UTC timestamp in Talos-acceptable format.
+    Return the current UTC timestamp in the format acceptable by Talos.
 
     Example: 2019-10-20T15:00:00.000000Z
     """
+    # Timestamp converter.
     utc_datetime = datetime.datetime.utcnow().strftime(
         "%Y-%m-%dT%H:%M:%S.000000Z"
     )
+    # Timezone check.
+    hdateti.dassert_has_UTC_tz(utc_datetime)
     return utc_datetime
