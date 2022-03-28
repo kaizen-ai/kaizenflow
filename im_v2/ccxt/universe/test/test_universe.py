e3bb8da7bbff71381d2b18b26696e62b886ef0ca
jsmerix <smeriga.juraj@gmail.com>
2022-03-28 19:49:19 +0000

    Refactor tests to include vendor specific tests inside their corresponding folders

---
+++ im_v2/ccxt/universe/test/test_universe.py
@@ -0,0 +1,54 @@
+import im_v2.common.universe.test.test_universe as imvcountt
+
+
+class TestGetUniverseFilePath1(imvcountt.TestGetUniverseFilePath1):
+
+    def test_get_universe_file_path(self) -> None:
+        """
+        A smoke test to test correct file path return when correct version is
+        provided.
+        """
+        self._test_get_universe_file_path("CCXT", "v1")
+        self._test_get_universe_file_path("CCXT", "v3")
+
+    def test_get_latest_file_version(self) -> None:
+        """
+        Verify that the max universe version is correctly detected and
+        returned.
+        """
+        self._test_get_latest_file_version("CCXT")
+
+
+class TestGetUniverse1(imvcountt.TestGetUniverse1):
+
+    def test_get_universe1(self) -> None:
+        """
+        A smoke test to verify that universe loads correctly.
+        """
+        self._test_get_universe1("CCXT")
+
+    def test_get_universe_invalid_version(self) -> None:
+        """
+        Verify that incorrect universe version is recognized.
+        """
+        self._test_get_universe_invalid_version("CCXT")
+
+
+class TestGetVendorUniverse1(imvcountt.TestGetVendorUniverse1):
+
+    def test_get_vendor_universe_small(self) -> None:
+        """
+        Test that vendor universe is loaded correctly as dict using small
+        universe file.
+        """
+        self._test_get_vendor_universe_small("CCXT", "gateio", "XRP_USDT")
+        self._test_get_vendor_universe_small("CCXT", "kucoin", "SOL_USDT")
+
+    def test_get_vendor_universe_as_full_symbol(self) -> None:
+        """
+        Test that universe as full symbols is received correctly from small
+        universal.
+        """
+        self._test_get_vendor_universe_as_full_symbol(
+            "CCXT", ["gateio::XRP_USDT", "kucoin::SOL_USDT"]
+        )
--- im_v2/common/universe/test/test_universe.py
+++ im_v2/common/universe/test/test_universe.py
@@ -1,4 +1,11 @@
+"""
+Import as:
+
+import im_v2.common.universe.test_universe as imvcounte
+"""
+
 import os
+from typing import List
 
 import pytest
 
@@ -11,84 +18,88 @@ import im_v2.common.universe.universe as imvcounun
 _VENDORS = ["CCXT", "Talos"]
 
 
-# TODO(Juraj): Factor out the body of the for loops in test methods
-# (after #1487 is done).
 class TestExtractUniverseVersion1(hunitest.TestCase):
 
     def test_extract_universe_version(self) -> None:
         """
         Verify function provides expected output on valid inputs.
         """
-        versions = [("01", 1), ("10", 10), ("25", 25)]
-        for s, n in versions:
-            fn = f"/app/im_v2/ccxt/universe/universe_v{s}.json"
-            self.assertEqual(imvcounun._extract_universe_version(fn), n)
+        versions = [
+            ("1.1", (1, 1)),
+            ("4", (4, 0)),
+            ("1.0", (1, 0)),
+            ("3.11", (3, 11)),
+            ("2.16", (2, 16)),
+            ("25.11", (25, 11)),
+        ]
+        for ver, num in versions:
+            with self.subTest(ver=ver, num=num):
+                fn = f"/app/im_v2/ccxt/universe/universe_v{ver}.json"
+                self.assertEqual(imvcounun._extract_universe_version(fn), num)
 
     def test_extract_universe_version_incorrect_format(self) -> None:
         """
         Verify function raises AssertionError on incorrect input format.
         """
-        file_names = ["incorrect", "universe_vxx.json", ""]
+        file_names = [
+            "incorrect",
+            "universe_vxx.json",
+            "universe_v.1.json",
+            "universe_11.json",
+        ]
         expected_fail = "Can't parse file"
         for fn in file_names:
-            with pytest.raises(AssertionError) as fail:
-                _ = imvcounun._extract_universe_version(fn)
-            self.assertIn(expected_fail, str(fail.value))
-
-
-# TODO(Juraj): Factor out the body of the for loops in test methods
-# (after #1487 is done).
+            with self.subTest(fn=fn):
+                with pytest.raises(AssertionError) as fail:
+                    _ = imvcounun._extract_universe_version(fn)
+                self.assertIn(expected_fail, str(fail.value))
 
 
 class TestGetUniverseFilePath1(hunitest.TestCase):
 
-    def test_get_universe_file_path(self) -> None:
+    def _test_get_universe_file_path(self, vendor: str, version: str) -> None:
         """
-        A smoke test to test correct file path return when version is provided.
+        A smoke test to test correct file path return when correct version is
+        provided.
+
+        :param vendor: vendor to apply test to, e.g. CCXT or Talos
+        :param version: version to test (should be present for a given vendor)
+         e.g. Talos -> v1, CCXT -> v1/v2/v3
         """
         # These should already exist in the filesystem.
-        test_vers = ["v03", "v01"]
         expected_part = "im_v2/{}/universe/universe_{}.json"
-        for vendor, version in zip(_VENDORS, test_vers):
-            actual = imvcounun._get_universe_file_path(vendor, version=version)
-            expected = os.path.join(
-                hgit.get_amp_abs_path(),
-                expected_part.format(vendor.lower(), version),
-            )
-            self.assertEqual(actual, expected)
+        actual = imvcounun._get_universe_file_path(vendor, version=version)
+        expected = os.path.join(
+            hgit.get_amp_abs_path(),
+            expected_part.format(vendor.lower(), version),
+        )
+        self.assertEqual(actual, expected)
 
-    def test_get_latest_file_version(self) -> None:
+    def _test_get_latest_file_version(self, vendor: str) -> None:
         """
         Verify that the max universe version is correctly detected and
         returned.
+
+        :param vendor: vendor to apply test to, e.g. CCXT or Talos
         """
         # Future proof this test when new versions are added.
         # Assuming we won't have more versions :).
         max_ver = 9999
         expected_part = "im_v2/{}/universe/universe_v{}.json"
-        for vendor in _VENDORS:
-            mock_universe = os.path.join(
-                hgit.get_amp_abs_path(),
-                expected_part.format(vendor.lower(), max_ver),
-            )
-            # Create tmp mock file as max version.
-            with open(mock_universe, mode="w", encoding="utf-8") as _:
-                pass
-            actual = imvcounun._get_universe_file_path(vendor)
-            # Delete tmp file.
-            hio.delete_file(mock_universe)
-            self.assertEqual(actual, mock_universe)
+        mock_universe = os.path.join(
+            hgit.get_amp_abs_path(),
+            expected_part.format(vendor.lower(), max_ver),
+        )
+        # Create tmp mock file as max version.
+        with open(mock_universe, mode="w", encoding="utf-8") as _:
+            pass
+        actual = imvcounun._get_universe_file_path(vendor)
+        # Delete tmp file.
+        hio.delete_file(mock_universe)
+        self.assertEqual(actual, mock_universe)
 
 
-class TestGetUniverse1(hunitest.TestCase):
-
-    def test_get_universe1(self) -> None:
-        """
-        A smoke test to verify that universe loads correctly.
-        """
-        for vendor in _VENDORS:
-            _ = imvcounun._get_trade_universe(vendor)
-            _ = imvcounun._get_trade_universe(vendor, version="v01")
+class TestGetUniverseGeneral1(hunitest.TestCase):
 
     def test_get_universe_invalid_vendor(self) -> None:
         """
@@ -97,24 +108,46 @@ class TestGetUniverse1(hunitest.TestCase):
         with self.assertRaises(AssertionError):
             _ = imvcounun._get_trade_universe("unknown")
 
-    def test_get_universe_invalid_version(self) -> None:
+
+class TestGetUniverse1(hunitest.TestCase):
+
+    def _test_get_universe1(self, vendor: str) -> None:
+        """
+        A smoke test to verify that universe loads correctly.
+
+        :param vendor: vendor to apply test to, e.g. CCXT or Talos
+        """
+        _ = imvcounun._get_trade_universe(vendor)
+        _ = imvcounun._get_trade_universe(vendor, version="v1")
+
+    def _test_get_universe_invalid_version(
+        self, vendor: str, *, version: str = "unknown"
+    ) -> None:
         """
         Verify that incorrect universe version is recognized.
+
+        :param vendor: vendor to apply test to, e.g. CCXT or Talos
         """
         with self.assertRaises(AssertionError):
-            _ = imvcounun._get_trade_universe("CCXT", version="unknown")
+            _ = imvcounun._get_trade_universe(vendor, version=version)
 
 
 class TestGetVendorUniverse1(hunitest.TestCase):
 
-    def test_get_vendor_universe(self) -> None:
+    def _test_get_vendor_universe_small(
+        self, vendor: str, exchange: str, currency_pair: str
+    ) -> None:
         """
-        Test that vendor universe is loaded correctly as dict.
-        """
-        self.get_vendor_universe_helper("CCXT", "gateio", "XRP_USDT")
-        self.get_vendor_universe_helper("Talos", "binance", "ADA_USDT")
+        Test that vendor universe is loaded correctly as dict using small
+        universe file.
 
-    def get_vendor_universe_helper(
+        :param vendor: vendor to apply test to, e.g. CCXT or Talos
+        :param exchange: exchange to load currency pairs for
+        :param currency_pair: currency pair in format: SYMBOL_SYMBOL
+        """
+        self._get_vendor_universe_small(vendor, exchange, currency_pair)
+
+    def _get_vendor_universe_small(
         self, vendor: str, exchange: str, currency_pair: str
     ) -> None:
         """
@@ -124,20 +157,21 @@ class TestGetVendorUniverse1(hunitest.TestCase):
         self.assertIn(exchange, universe)
         self.assertEqual([currency_pair], universe[exchange])
 
-    def test_get_vendor_universe_as_full_symbol(self) -> None:
+    def _test_get_vendor_universe_as_full_symbol(
+        self, vendor: str, universe_as_full_symbols: List[str]
+    ) -> None:
         """
-        Test that universe as full symbols is received correctly.
-        """
-        universe_as_full_symbols = imvcounun.get_vendor_universe(
-            "CCXT", version="small", as_full_symbol=True
-        )
-        self.assertEqual(len(universe_as_full_symbols), 2)
-        self.assertEqual(universe_as_full_symbols[0], "gateio::XRP_USDT")
-        self.assertEqual(universe_as_full_symbols[1], "kucoin::SOL_USDT")
+        Test that universe as full symbols is received correctly 
+        from small universe.
 
-        universe_as_full_symbols = imvcounun.get_vendor_universe(
-            "Talos", version="small", as_full_symbol=True
+        :param vendor: vendor to apply test to, e.g. CCXT or Talos
+        :param universe_as_full_symbols: list of currency pairs as 
+         full symbols in format exchange_id::SYMBOL_SYMBOL
+        """
+        actual = imvcounun.get_vendor_universe(
+            vendor, version="small", as_full_symbol=True
         )
         self.assertEqual(len(universe_as_full_symbols), 2)
-        self.assertEqual(universe_as_full_symbols[0], "binance::ADA_USDT")
-        self.assertEqual(universe_as_full_symbols[1], "ftx::BNB_USDT")
+        self.assertEqual(len(actual), 2)
+        self.assertEqual(actual[0], universe_as_full_symbols[0])
+        self.assertEqual(actual[1], universe_as_full_symbols[1])
