6f2aa7106cec4d31d80c5464f996022a8027bdb4
saggese <saggese@gmail.com>
2022-03-29 14:45:07 -0400

    Checkpoint

--- im_v2/common/data/client/test/test_historical_pq_clients.py
+++ im_v2/common/data/client/test/test_historical_pq_clients.py
@@ -51,7 +51,6 @@ def _generate_test_data(
 
 
 class MockHistoricalByTile(imvcdchpcl.HistoricalPqByTileClient):
-
     def get_universe(self) -> List[str]:
         return ["binance::BTC_USDT", "kucoin::FIL_USDT"]
 
