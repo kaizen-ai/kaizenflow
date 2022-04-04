6865286a5d23d00948b1c7bdcb1a704490ffabbd
PomazkinG <pomazking@gmail.com>
2022-04-04 11:08:53 +0000

    fix

--- optimizer/test/test_process_forecasts_opt.py
+++ optimizer/test/test_process_forecasts_opt.py
@@ -18,7 +18,7 @@ import oms
 _LOG = logging.getLogger(__name__)
 
 
-@pytest.mark.skip("CmTask #1580 Optimizer-related tests fail.")
+#@pytest.mark.skip("CmTask #1580 Optimizer-related tests fail.")
 class TestDataFrameProcessForecasts1(hunitest.TestCase):
 
     # TODO(gp): This can become an _example.
@@ -166,8 +166,9 @@ class TestDataFrameProcessForecasts1(hunitest.TestCase):
             predictions, volatility, portfolio, config, spread_df, restrictions_df
         )
         actual = str(portfolio)
+        print("ACTUAL \n", actual)
         expected = r"""
-# historical holdings=                                                                                                                     [15/30401]
+# historical holdings=                                                                                                                 
 asset_id                     100    200    -1
 2000-01-01 09:40:01-05:00 -50.00  50.00    0.00
 2000-01-01 09:45:01-05:00 -50.13  49.91  220.24
@@ -232,6 +233,7 @@ asset_id                      100     200
 2000-01-01 10:25:01-05:00  155.78         35.55       18.41   99887.56  155.82  338.24      494.07    202.17
 2000-01-01 10:30:01-05:00 -165.84     200091.94      268.09  100166.57  258.07   70.15      328.23    305.17
         """
+        print("EXPECTED \n", expected)
         self.assert_equal(actual, expected, fuzzy_match=True)
 
     # TODO(gp): -> test1
