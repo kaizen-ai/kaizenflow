52e3ebec00cb5f1d1f52e33cbc62d5f6c37195f6
nikola.jasek <cowardly.dog.solutions.oy@eesti.ee>
2022-04-05 22:30:05 +0200

    CMTask1320: Refactor to_file from_file tests

--- helpers/test/test_hs3.py
+++ helpers/test/test_hs3.py
@@ -1,7 +1,7 @@
-import gzip
 import unittest.mock as umock
 
 # Equivalent to `import moto`, but skip this module if the module is not present.
+# Also, `moto` must be imported before `boto3` to properly mock it.
 import pytest  # isort:skip # noqa: E402 # pylint: disable=wrong-import-position
 
 moto = pytest.importorskip("moto")
@@ -10,9 +10,10 @@ import helpers.hs3 as hs3  # noqa: E402 module level import not at top of file  
 import helpers.hunit_test as hunitest  # noqa: E402 module level import not at top of file  # pylint: disable=wrong-import-position
 
 
-# TODO(Nikola): Move to `hunitest.TestCase` or separate test class?
-class S3Mock(hunitest.TestCase):
+# TODO(Nikola): Consider moving this class to `hunit_test_case.py`, `hs3.py`, or `hmoto.py`.
+class S3TestCase(hunitest.TestCase):
     # Mocked AWS credentials.
+    # TODO(Nikola): Different behaviour if moved in `setUp`?
     mock_aws_credentials_patch = umock.patch.dict(
         hs3.os.environ,
         {
@@ -44,58 +45,45 @@ class S3Mock(hunitest.TestCase):
 
     def tearDown(self) -> None:
         super().tearDown()
-        # Stop boto3 mock.
+        # Stop moto.
         self.mock_s3.stop()
-        # Stop mock aws credentials.
         self.mock_aws_credentials_patch.stop()
 
 
 @pytest.mark.skip("Enable after CMTask1292 is resolved.")
-class TestToFile1(S3Mock):
+class TestToFileAndFromFile1(S3TestCase):
 
-    def test_to_file1(self) -> None:
+    def test_to_file_and_from_file1(self) -> None:
         """
-        Verify that regular `.txt` file is saved on S3.
+        Verify that regular `.txt` file is saved/read on S3.
         """
         # Prepare inputs.
         regular_file_name = "mock.txt"
-        regular_file_content = "line_mock1\nline_mock2\nline_mock3"
-        moto_s3fs = hs3.get_s3fs("ck")
-        s3_path = f"s3://{self.bucket_name}/{regular_file_name}"
-        # Save file.
-        hs3.to_file(regular_file_content, s3_path, aws_profile=moto_s3fs)
-        # Check output.
-        saved_files = moto_s3fs.ls(self.bucket_name)
-        self.assertListEqual(saved_files, [s3_path.lstrip("s3://")])
-        with moto_s3fs.open(s3_path.lstrip("s3://")) as saved_file:
-            saved_content = saved_file.read().decode()
-        expected = r"""line_mock1
-        line_mock2
-        line_mock3"""
-        self.assert_equal(saved_content, expected, fuzzy_match=True)
+        force_flush = False
+        self.write_read_helper(regular_file_name, force_flush)
 
-    def test_to_file2(self) -> None:
+    def test_to_file_and_from_file2(self) -> None:
         """
-        Verify that compressed (e.g,`.gz`,`gzip`) file is saved on S3.
-
-        Also try `force_flush`.
+        Verify that compressed (e.g,`.gz`,`gzip`) file is saved/read on S3.
         """
         # Prepare inputs.
         gzip_file_name = "mock.gzip"
-        gzip_file_content = "line_mock1\nline_mock2\nline_mock3"
+        force_flush = True
+        self.write_read_helper(gzip_file_name, force_flush)
+
+    def write_read_helper(self, file_name: str, force_flush: bool) -> None:
+        # Prepare inputs.
+        file_content = "line_mock1\nline_mock2\nline_mock3"
         moto_s3fs = hs3.get_s3fs("ck")
-        s3_path = f"s3://{self.bucket_name}/{gzip_file_name}"
+        s3_path = f"s3://{self.bucket_name}/{file_name}"
         # Save file.
         # TODO(Nikola): Is it possible to verify `force_flush`?
         hs3.to_file(
-            gzip_file_content, s3_path, aws_profile=moto_s3fs, force_flush=True
+            file_content, s3_path, aws_profile=moto_s3fs, force_flush=force_flush
         )
+        # Read file.
+        saved_content = hs3.from_file(s3_path, aws_profile=moto_s3fs)
         # Check output.
-        saved_files = moto_s3fs.ls(self.bucket_name)
-        self.assertListEqual(saved_files, [s3_path.lstrip("s3://")])
-        with moto_s3fs.open(s3_path.lstrip("s3://")) as saved_file:
-            with gzip.GzipFile(fileobj=saved_file) as gzip_file:
-                saved_content = gzip_file.read().decode()
         expected = r"""line_mock1
         line_mock2
         line_mock3"""
@@ -119,51 +107,6 @@ class TestToFile1(S3Mock):
         actual = str(fail.value)
         expected = r"S3 only allows binary mode!"
         self.assert_equal(actual, expected)
-
-
-@pytest.mark.skip("Enable after CMTask1292 is resolved.")
-class TestFromFile1(S3Mock):
-
-    def test_from_file1(self) -> None:
-        """
-        Verify that regular `.txt` file can be read from S3.
-        """
-        # Prepare inputs.
-        regular_file_name = "mock.txt"
-        moto_s3fs = hs3.get_s3fs("ck")
-        s3_path = f"s3://{self.bucket_name}/{regular_file_name}"
-        lines = [b"line_mock1\n", b"line_mock2\n", b"line_mock3"]
-        # Prepare test file.
-        with moto_s3fs.open(s3_path, "wb") as s3_file:
-            s3_file.writelines(lines)
-        # Read file.
-        file_content = hs3.from_file(s3_path, aws_profile=moto_s3fs)
-        # Check output.
-        expected = r"""line_mock1
-        line_mock2
-        line_mock3"""
-        self.assert_equal(file_content, expected, fuzzy_match=True)
-
-    def test_from_file2(self) -> None:
-        """
-        Verify that compressed (e.g,`.gz`,`gzip`) file can be read from S3.
-        """
-        # Prepare inputs.
-        gzip_file_name = "mock.gzip"
-        moto_s3fs = hs3.get_s3fs("ck")
-        s3_path = f"s3://{self.bucket_name}/{gzip_file_name}"
-        lines = [b"line_mock1\n", b"line_mock2\n", b"line_mock3"]
-        # Prepare test file.
-        with moto_s3fs.open(s3_path, "wb") as s3_file:
-            with gzip.GzipFile(fileobj=s3_file) as gzip_file:
-                gzip_file.writelines(lines)
-        # Read file.
-        file_content = hs3.from_file(s3_path, aws_profile=moto_s3fs)
-        # Check output.
-        expected = r"""line_mock1
-        line_mock2
-        line_mock3"""
-        self.assert_equal(file_content, expected, fuzzy_match=True)
 
     def test_from_file_invalid1(self) -> None:
         """
