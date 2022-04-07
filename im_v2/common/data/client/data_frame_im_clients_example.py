10bbe70b57cdb38680b0405814a331d317575f9d
DanilYachmenev <31514660+DanilYachmenev@users.noreply.github.com>
2022-04-07 15:07:32 +0300

    CmTask1618: fix circular import problem and implement requested changes (#1631)
    * CmTask1618: fix circular import problem and implement requested changes
    
    * CmTask1618: implement requested changes
    
    * CmTask1618: implement nits

--- im_v2/common/data/client/data_frame_im_clients_example.py
+++ im_v2/common/data/client/data_frame_im_clients_example.py
@@ -6,7 +6,7 @@ import im_v2.common.data.client.data_frame_im_clients_example as imvcdcdfimce
 
 
 import im_v2.common.data.client.data_frame_im_clients as imvcdcdfimc
-import core.finance.market_data_example as cfmadaex
+import core.finance as cofinanc
 
 
 def get_DataFrameImClient_example1() -> imvcdcdfimc.DataFrameImClient:
@@ -15,7 +15,7 @@ def get_DataFrameImClient_example1() -> imvcdcdfimc.DataFrameImClient:
     """
     # Generate input dataframe and universe for client initialization.
     universe = ["binance::BTC_USDT", "binance::ADA_USDT"]
-    df = cfmadaex.get_im_client_market_data_df1(universe)
+    df = cofinanc.get_im_client_market_data_df1(universe)
     # Init the client for testing.
     resample_1min = False
     im_client = imvcdcdfimc.DataFrameImClient(df, universe, resample_1min)
--- im_v2/talos/data/client/__init__.py
+++ im_v2/talos/data/client/__init__.py
@@ -5,3 +5,4 @@ import im_v2.talos.data.client as itdcl
 """
 
 from im_v2.talos.data.client.talos_clients import *  # pylint: disable=unused-import # NOQA
+from im_v2.talos.data.client.talos_clients_example import *  # pylint: disable=unused-import # NOQA
