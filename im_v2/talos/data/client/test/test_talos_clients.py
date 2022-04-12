3977f5ce0861e349a42884b3bf2a840c425bc2b6
Nina Lee <nina.tru94@gmail.com>
2022-04-12 15:56:44 +0000

    CMTask1646: added tests `TestTalosParquetByTileClient2`

--- im_v2/talos/data/client/test/test_talos_clients.py
+++ im_v2/talos/data/client/test/test_talos_clients.py
@@ -337,6 +337,160 @@ class TestTalosParquetByTileClient2(icdctictc.ImClientTestCase):
         ]
         return expected_column_names
 
+    def test_read_data1(self) -> None:
+        resample_1min = True
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbol = "binance::ADA_USDT"
+        #
+        expected_length = 44640
+        expected_column_names = self.get_expected_column_names()
+        expected_column_unique_values = {"full_symbol": ["binance::ADA_USDT"]}
+        # pylint: disable=line-too-long
+        expected_signature = r"""
+        # df=
+        index=[2022-01-01 00:00:00+00:00, 2022-01-31 23:59:00+00:00]
+        columns=full_symbol,open,high,low,close,volume
+        shape=(44640, 6)
+                                         full_symbol        open        high         low       close           volume
+        timestamp                                                                                                    
+        2022-01-01 00:00:00+00:00  binance::ADA_USDT  1.30800000  1.31000000  1.30700000  1.31000000   98266.80000000
+        2022-01-01 00:01:00+00:00  binance::ADA_USDT  1.31000000  1.31400000  1.30800000  1.31200000  132189.40000000
+        2022-01-01 00:02:00+00:00  binance::ADA_USDT  1.31200000  1.31800000  1.31100000  1.31700000  708964.20000000
+        ...
+        2022-01-31 23:57:00+00:00  binance::ADA_USDT  1.05300000  1.05300000  1.05200000  1.05200000   7768.30000000
+        2022-01-31 23:58:00+00:00  binance::ADA_USDT  1.05200000  1.05300000  1.05200000  1.05200000  42496.40000000
+        2022-01-31 23:59:00+00:00  binance::ADA_USDT  1.05200000  1.05200000  1.05100000  1.05100000  86742.50000000
+        """
+        # pylint: enable=line-too-long
+        self._test_read_data1(
+            talos_client,
+            full_symbol,
+            expected_length,
+            expected_column_names,
+            expected_column_unique_values,
+            expected_signature,
+        )
+
+    def test_read_data2(self) -> None:
+        resample_1min = True
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbols = ["binance::ADA_USDT", "binance::BTC_USDT"]
+        #
+        expected_length = 89280
+        expected_column_names = self.get_expected_column_names()
+        expected_column_unique_values = {
+            "full_symbol": ["binance::ADA_USDT", "binance::BTC_USDT"]
+        }
+        # pylint: disable=line-too-long
+        expected_signature = r"""
+        # df=
+        index=[2022-01-01 00:00:00+00:00, 2022-01-31 23:59:00+00:00]
+        columns=full_symbol,open,high,low,close,volume
+        shape=(89280, 6)
+                                         full_symbol            open            high             low           close           volume
+        timestamp                                                                                                                    
+        2022-01-01 00:00:00+00:00  binance::ADA_USDT      1.30800000      1.31000000      1.30700000      1.31000000   98266.80000000
+        2022-01-01 00:00:00+00:00  binance::BTC_USDT  46216.93000000  46271.08000000  46208.37000000  46250.00000000      40.57574000
+        2022-01-01 00:01:00+00:00  binance::ADA_USDT      1.31000000      1.31400000      1.30800000      1.31200000  132189.40000000
+        ...
+        2022-01-31 23:58:00+00:00  binance::BTC_USDT  38495.52000000  38510.28000000  38493.76000000  38500.98000000     14.57001000
+        2022-01-31 23:59:00+00:00  binance::ADA_USDT      1.05200000      1.05200000      1.05100000      1.05100000  86742.50000000
+        2022-01-31 23:59:00+00:00  binance::BTC_USDT  38500.98000000  38500.99000000  38460.78000000  38466.89000000     14.27622000
+        """
+        # pylint: enable=line-too-long
+        self._test_read_data2(
+            talos_client,
+            full_symbols,
+            expected_length,
+            expected_column_names,
+            expected_column_unique_values,
+            expected_signature,
+        )
+
+    def test_read_data3(self) -> None:
+        resample_1min = True
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbols = ["binance::ADA_USDT", "binance::BTC_USDT"]
+        start_ts = pd.Timestamp("2022-01-01T00:01:00-00:00")
+        #
+        expected_length = 89278
+        expected_column_names = self.get_expected_column_names()
+        expected_column_unique_values = {
+            "full_symbol": ["binance::ADA_USDT", "binance::BTC_USDT"]
+        }
+        # pylint: disable=line-too-long
+        expected_signature = r"""
+        # df=
+        index=[2022-01-01 00:01:00+00:00, 2022-01-31 23:59:00+00:00]
+        columns=full_symbol,open,high,low,close,volume
+        shape=(89278, 6)
+                                         full_symbol            open            high             low           close           volume
+        timestamp                                                                                                                    
+        2022-01-01 00:01:00+00:00  binance::ADA_USDT      1.31000000      1.31400000      1.30800000      1.31200000  132189.40000000
+        2022-01-01 00:01:00+00:00  binance::BTC_USDT  46250.01000000  46344.23000000  46234.39000000  46312.76000000      42.38106000
+        2022-01-01 00:02:00+00:00  binance::ADA_USDT      1.31200000      1.31800000      1.31100000      1.31700000  708964.20000000
+        ...
+        2022-01-31 23:58:00+00:00  binance::BTC_USDT  38495.52000000  38510.28000000  38493.76000000  38500.98000000     14.57001000
+        2022-01-31 23:59:00+00:00  binance::ADA_USDT      1.05200000      1.05200000      1.05100000      1.05100000  86742.50000000
+        2022-01-31 23:59:00+00:00  binance::BTC_USDT  38500.98000000  38500.99000000  38460.78000000  38466.89000000     14.27622000
+        """
+        # pylint: enable=line-too-long
+        self._test_read_data3(
+            talos_client,
+            full_symbols,
+            start_ts,
+            expected_length,
+            expected_column_names,
+            expected_column_unique_values,
+            expected_signature,
+        )
+
+    def test_read_data4(self) -> None:
+        resample_1min = True
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbols = ["binance::ADA_USDT", "binance::BTC_USDT"]
+        end_ts = pd.Timestamp("2022-01-01T00:05:00-00:00")
+        #
+        expected_length = 12
+        expected_column_names = self.get_expected_column_names()
+        expected_column_unique_values = {
+            "full_symbol": ["binance::ADA_USDT", "binance::BTC_USDT"]
+        }
+        # pylint: disable=line-too-long
+        expected_signature = r"""
+        # df=
+        index=[2022-01-01 00:00:00+00:00, 2022-01-01 00:05:00+00:00]
+        columns=full_symbol,open,high,low,close,volume
+        shape=(12, 6)
+                                         full_symbol            open            high             low           close           volume
+        timestamp                                                                                                                    
+        2022-01-01 00:00:00+00:00  binance::ADA_USDT      1.30800000      1.31000000      1.30700000      1.31000000   98266.80000000
+        2022-01-01 00:00:00+00:00  binance::BTC_USDT  46216.93000000  46271.08000000  46208.37000000  46250.00000000      40.57574000
+        2022-01-01 00:01:00+00:00  binance::ADA_USDT      1.31000000      1.31400000      1.30800000      1.31200000  132189.40000000
+        ...
+        2022-01-01 00:04:00+00:00  binance::BTC_USDT  46331.07000000  46336.10000000  46300.00000000  46321.34000000     20.96029000
+        2022-01-01 00:05:00+00:00  binance::ADA_USDT      1.31500000      1.31800000      1.31300000      1.31800000  75423.50000000
+        2022-01-01 00:05:00+00:00  binance::BTC_USDT  46321.34000000  46443.56000000  46280.00000000  46436.03000000     35.86682000
+        """
+        # pylint: enable=line-too-long
+        self._test_read_data4(
+            talos_client,
+            full_symbols,
+            end_ts,
+            expected_length,
+            expected_column_names,
+            expected_column_unique_values,
+            expected_signature,
+        )
+
     def test_read_data5(self) -> None:
         resample_1min = True
         talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
@@ -377,6 +531,83 @@ class TestTalosParquetByTileClient2(icdctictc.ImClientTestCase):
             expected_column_names,
             expected_column_unique_values,
             expected_signature,
+        )
+
+    def test_read_data6(self) -> None:
+        resample_1min = True
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbol = "unsupported_exchange::unsupported_currency"
+        self._test_read_data6(
+            talos_client,
+            full_symbol,
+        )
+
+    def test_read_data7(self) -> None:
+        resample_1min = False
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbols = ["binance::ADA_USDT", "binance::BTC_USDT"]
+        #
+        expected_length = 89280
+        expected_column_names = self.get_expected_column_names()
+        expected_column_unique_values = {
+            "full_symbol": ["binance::ADA_USDT", "binance::BTC_USDT"]
+        }
+        # pylint: disable=line-too-long
+        expected_signature = r"""
+        # df=
+        index=[2022-01-01 00:00:00+00:00, 2022-01-31 23:59:00+00:00]
+        columns=full_symbol,open,high,low,close,volume
+        shape=(89280, 6)
+                                         full_symbol            open            high             low           close           volume
+        timestamp                                                                                                                    
+        2022-01-01 00:00:00+00:00  binance::ADA_USDT      1.30800000      1.31000000      1.30700000      1.31000000   98266.80000000
+        2022-01-01 00:00:00+00:00  binance::BTC_USDT  46216.93000000  46271.08000000  46208.37000000  46250.00000000      40.57574000
+        2022-01-01 00:01:00+00:00  binance::ADA_USDT      1.31000000      1.31400000      1.30800000      1.31200000  132189.40000000
+        ...
+        2022-01-31 23:58:00+00:00  binance::BTC_USDT  38495.52000000  38510.28000000  38493.76000000  38500.98000000     14.57001000
+        2022-01-31 23:59:00+00:00  binance::ADA_USDT      1.05200000      1.05200000      1.05100000      1.05100000  86742.50000000
+        2022-01-31 23:59:00+00:00  binance::BTC_USDT  38500.98000000  38500.99000000  38460.78000000  38466.89000000     14.27622000
+        """
+        # pylint: enable=line-too-long
+        self._test_read_data7(
+            talos_client,
+            full_symbols,
+            expected_length,
+            expected_column_names,
+            expected_column_unique_values,
+            expected_signature,
+        )
+
+    # ////////////////////////////////////////////////////////////////////////
+
+    def test_get_start_ts_for_symbol1(self) -> None:
+        resample_1min = True
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbol = "binance::ADA_USDT"
+        expected_start_ts = pd.Timestamp("2022-01-01T00:00:00-00:00")
+        self._test_get_start_ts_for_symbol1(
+            talos_client,
+            full_symbol,
+            expected_start_ts,
+        )
+
+    def test_get_end_ts_for_symbol1(self) -> None:
+        resample_1min = True
+        talos_client = imvtdctcex.get_TalosHistoricalPqByTileClient_example2(
+            resample_1min
+        )
+        full_symbol = "binance::ADA_USDT"
+        expected_end_ts = pd.Timestamp("2022-01-31T23:59:00-00:00")
+        self._test_get_end_ts_for_symbol1(
+            talos_client,
+            full_symbol,
+            expected_end_ts,
         )
 
 
