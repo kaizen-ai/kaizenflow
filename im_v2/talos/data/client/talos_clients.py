60f22f2d287f9fa69f522afa69b54a160ecd368b
tamriq <tamriqua@gmail.com>
2022-04-14 11:27:10 +0000

    CmTask1651: small fix

--- im_v2/talos/data/client/talos_clients.py
+++ im_v2/talos/data/client/talos_clients.py
@@ -184,20 +184,6 @@ class RealTimeSqlTalosClient(icdc.ImClient):
         self._mode = mode
         super().__init__(vendor, resample_1min)
 
-    @staticmethod
-    def should_be_online() -> bool:
-        """
-        The real-time system for Talos should always be online.
-        """
-        return True
-
-    @staticmethod
-    def get_metadata() -> pd.DataFrame:
-        """
-        Return metadata.
-        """
-        raise NotImplementedError
-
     def get_universe(self) -> List[icdc.FullSymbol]:
         """
         See description in the parent class.
@@ -215,6 +201,20 @@ class RealTimeSqlTalosClient(icdc.ImClient):
         # Convert to list.
         full_symbols = full_symbols.to_list()
         return full_symbols
+
+    @staticmethod
+    def should_be_online() -> bool:
+        """
+        The real-time system for Talos should always be online.
+        """
+        return True
+
+    @staticmethod
+    def get_metadata() -> pd.DataFrame:
+        """
+        Return metadata.
+        """
+        raise NotImplementedError
 
     @staticmethod
     # TODO(Danya): Move up to hsql.
