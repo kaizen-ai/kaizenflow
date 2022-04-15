cdb42cb57ec15b930f2f1bfee37311b2b3441625
DanilYachmenev <31514660+DanilYachmenev@users.noreply.github.com>
2022-04-15 16:32:25 +0300

    CmTask1681: Refactor HistoricalPqByTileClient and Talos client (#1682)
    * CmTask1681: Refactor HistoricalPqByTileClient and Talos one to demonstrate approach
    
    * CmTask1681: implement requested nits
    
    * CmTask1681: Create CcxtHistoricalPqByTileClient
    
    * CmTask1681: fix docstrings
    
    * CmTask1681: untnangle class with CcxtCddClient
    
    * CmTask1681: fix bug
    
    * CmTask1681: add hack with exchange_id col
    
    * CmTask1681: fix bug
    
    * CmTask1681: fix bug
    
    * CmTask1681: fix errors
    
    * checkpoint
    
    * checkpoint
    
    * fix imports
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * add todos
    
    * lint
    
    * add a TODO
    
    * checkpoint
    
    * fix tests
    
    * fix todos
    
    * lint
    
    Co-authored-by: PomazkinG <pomazking@gmail.com>
    Co-authored-by: Grigorii Pomazkin <64330743+PomazkinG@users.noreply.github.com>

---
+++ im_v2/common/universe/__init__.py
@@ -0,0 +1,8 @@
+"""
+Import as:
+
+import im_v2.common.universe as icunv
+"""
+
+from im_v2.common.universe.universe import *  # pylint: disable=unused-import # NOQA
+from im_v2.common.universe.universe_utils import *  # pylint: disable=unused-import # NOQA
