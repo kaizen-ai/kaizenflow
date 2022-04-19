2d6673fa5aa6c54bd932eba28e8cf2ba409e3ccf
nikola.jasek <cowardly.dog.solutions.oy@eesti.ee>
2022-04-20 00:11:48 +0200

    CMTask1664: Update per review

--- dev_scripts/github/set_secrets.py
+++ dev_scripts/github/set_secrets.py
@@ -1,44 +1,36 @@
 #!/usr/bin/env python
 """
-Script to set batch of GitHub secrets in one go.
+Script to set batch of GitHub secrets from `.json` file in one go.
 
 Simple usage:
 
-> ./dev_scripts/github/set_secrets.py
+> ./dev_scripts/github/set_secrets.py \
+     --file 'dev_scripts/github/secrets.json' \
+     --repo 'cryptomtc/cmamp_test'
 """
 
 import argparse
 import logging
 
 import helpers.hdbg as hdbg
+import helpers.hio as hio
 import helpers.hparser as hparser
 import helpers.hsystem as hsystem
 
 _LOG = logging.getLogger(__name__)
 
 
-SECRETS = {
-    "GH_ACTION_ACCESS_TOKEN": "***",
-    "AM_AWS_ACCESS_KEY_ID": "***",
-    "AM_AWS_SECRET_ACCESS_KEY": "***",
-    "AM_AWS_DEFAULT_REGION": "us-east-1",
-    "AM_ECR_BASE_PATH": "***",
-    "AM_AWS_S3_BUCKET": "alphamatic-data",
-    "AM_TELEGRAM_TOKEN": "***",
-    "CK_AWS_ACCESS_KEY_ID": "***",
-    "CK_AWS_SECRET_ACCESS_KEY": "***",
-    "CK_AWS_DEFAULT_REGION": "eu-north-1",
-    "CK_AWS_S3_BUCKET": "cryptokaizen-data",
-    "CK_TELEGRAM_TOKEN": "***",
-}
-
-
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
     hdbg.init_logger(verbosity=args.log_level, use_exec_path=True)
-    for secret_key, secret_value in SECRETS.items():
-        command = f'gh secret set {secret_key} --body "{secret_value}"'
-        hsystem.system(command)
+    secrets = hio.from_json(args.file)
+    for secret_key, secret_value in secrets.items():
+        cmd = [
+            f"gh secret set {secret_key}",
+            f'--body "{secret_value}"',
+            f"--repo {args.repo}",
+        ]
+        hsystem.system(" ".join(cmd))
         _LOG.info("%s is set!", secret_key)
     _LOG.info("All secrets are set!")
 
@@ -48,6 +40,18 @@ def _parse() -> argparse.ArgumentParser:
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
     hparser.add_verbosity_arg(parser)
+    parser.add_argument(
+        "--file",
+        action="store",
+        type=str,
+        help="Location of `.json` file with desired secrets.",
+    )
+    parser.add_argument(
+        "--repo",
+        action="store",
+        type=str,
+        help="On which repository command will be applied.",
+    )
     return parser
 
 
