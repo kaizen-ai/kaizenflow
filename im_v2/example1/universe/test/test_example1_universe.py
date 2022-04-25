283ff5fc2b04e5a12b321e3cd0aff36ec4455bd0
Nina Lee <nina.tru94@gmail.com>
2022-04-25 13:45:37 +0000

    added tests

--- im_v2/example1/universe/test/test_example1_universe.py
+++ im_v2/example1/universe/test/test_example1_universe.py
@@ -18,8 +18,23 @@ class TestGetUniverseFilePath1(imvcountt.TestGetUniverseFilePath1_TestCase):
 
 
 class TestGetUniverse1(imvcountt.TestGetUniverse1_TestCase):
-    def test_get_universe_example1(self) -> None:
+    def test_get_universe1(self) -> None:
         """
         A smoke test to verify that example universe loads correctly.
         """
         self._test_get_universe1("example1")
+
+    def test_get_universe_invalid_version(self) -> None:
+        """
+        Verify that incorrect universe version is recognized.
+        """
+        self._test_get_universe_invalid_version("example1")
+
+    def test_get_vendor_universe_small(self) -> None:
+        vendor = "example1"
+        self._test_get_vendor_universe_small(vendor, "binance", "ADA_USDT")
+
+    def test_get_vendor_universe_as_full_symbol(self) -> None:
+        vendor = "example1"
+        universe_as_full_symbols = ["binance::ADA_USDT", "kucoin::ETH_USDT"]
+        self._test_get_vendor_universe_as_full_symbol(vendor, universe_as_full_symbols)
