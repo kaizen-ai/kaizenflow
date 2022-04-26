2fd00c52a794a063d1b5571e110850687f1d48c6
jsmerix <smeriga.juraj@gmail.com>
2022-04-26 17:05:02 +0000

    Run linter and rename DAG

--- im_v2/common/data/extract/airflow/data_reconciliation_dag.py
+++ im_v2/common/data/extract/airflow/data_reconciliation_dag.py
@@ -6,23 +6,23 @@
 # Make sure to set correct dag schedule `schedule_interval`` parameter.
 # Make sure to set the correct timedelta for `start_timestamp` cmd line
 # argument in all variables representing bash commands.
+import copy
 import datetime
+from itertools import product
+
 import airflow
 from airflow.contrib.operators.ecs_operator import ECSOperator
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.models import Variable
-from itertools import product
-import copy
+from airflow.operators.dummy_operator import DummyOperator
 
-
-# This variable will be propagated throughout DAG definition as a prefix to 
+# This variable will be propagated throughout DAG definition as a prefix to
 # names of Airflow configuration variables, allow to switch from test to prod
 # in one line (in best case scenario).
 _STAGE = "test"
-_DAG_ID = F"{_STAGE}_daily_data_reconciliation_multi_dag"
-_EXCHANGES = ["ftx", "binance"] 
+_DAG_ID = f"{_STAGE}_daily_data_reconciliation_multi_dag"
+_EXCHANGES = ["ftx", "binance"]
 _PROVIDERS = ["ccxt"]
-_UNIVERSES = { "ccxt": "v3" }
+_UNIVERSES = {"ccxt": "v3"}
 
 # E.g. DB table ccxt_ohlcv -> has an equivalent for testing ccxt_ohlcv_test
 # but production is ccxt_ohlcv.
@@ -31,11 +31,11 @@ _TABLE_SUFFIX = f"_{_STAGE}" if _STAGE == "test" else ""
 CONTAINER_SUFFIX = f"-{_STAGE}" if _STAGE == "test" else ""
 CONTAINER_NAME = f"cmamp{CONTAINER_SUFFIX}"
 
-ecs_cluster = Variable.get(f'{_STAGE}_ecs_cluster')
-# The naming convention is set such that this value is then reused 
-# in log groups, stream prefixes and container names to minimize 
+ecs_cluster = Variable.get(f"{_STAGE}_ecs_cluster")
+# The naming convention is set such that this value is then reused
+# in log groups, stream prefixes and container names to minimize
 # convolution and maximize simplicity.
-ecs_task_definition = Variable.get(f'{_STAGE}_ecs_task_definiton')
+ecs_task_definition = Variable.get(f"{_STAGE}_ecs_task_definiton")
 ecs_subnets = [Variable.get("ecs_subnet1"), Variable.get("ecs_subnet2")]
 ecs_security_group = [Variable.get("ecs_security_group")]
 ecs_awslogs_group = f"/ecs/{ecs_task_definition}"
@@ -45,9 +45,9 @@ s3_daily_staged_data_path = f"s3://{Variable.get(f'{_STAGE}_s3_data_bucket')}/{V
 # Pass default parameters for the DAG.
 default_args = {
     "retries": 1,
-    "email": [Variable.get(f'{_STAGE}_notification_email')],
+    "email": [Variable.get(f"{_STAGE}_notification_email")],
     "email_on_failure": True,
-    'email_on_retry': True,
+    "email_on_retry": True,
     "owner": "airflow",
 }
 
@@ -64,32 +64,31 @@ dag = airflow.DAG(
 
 download_command = [
     "/app/im_v2/{}/data/extract/download_historical_data.py",
-     "--end_timestamp '{{ execution_date }}'",
-     "--start_timestamp '{{ execution_date - macros.timedelta(minutes=30) }}'",
-     "--exchange_id '{}'",
-     "--universe '{}'",
-     "--aws_profile 'ck'",
-     "--s3_path '{}/{}'"
+    "--end_timestamp '{{ execution_date }}'",
+    "--start_timestamp '{{ execution_date - macros.timedelta(minutes=30) }}'",
+    "--exchange_id '{}'",
+    "--universe '{}'",
+    "--aws_profile 'ck'",
+    "--s3_path '{}/{}'",
 ]
 
-start_task = DummyOperator(task_id='start_download', dag=dag)
-end_download = DummyOperator(task_id='end_download', dag=dag)
+start_task = DummyOperator(task_id="start_download", dag=dag)
+end_download = DummyOperator(task_id="end_download", dag=dag)
 
-# Used for container overrides in task definition 
+# Used for container overrides in task definition
 container_suffix = f"-{_STAGE}" if _STAGE == "test" else ""
 container_name = f"cmamp{container_suffix}"
 
 for provider, exchange in product(_PROVIDERS, _EXCHANGES):
 
-    #TODO(Juraj): Make this code more readable.
+    # TODO(Juraj): Make this code more readable.
     # Do a deepcopy of the bash command list so we can reformat params on each iteration.
     curr_bash_command = copy.deepcopy(download_command)
     curr_bash_command[0] = curr_bash_command[0].format(provider)
     curr_bash_command[3] = curr_bash_command[3].format(exchange)
     curr_bash_command[4] = curr_bash_command[4].format(_UNIVERSES[provider])
     curr_bash_command[-1] = curr_bash_command[-1].format(
-        s3_daily_staged_data_path,
-        provider
+        s3_daily_staged_data_path, provider
     )
 
     downloading_task = ECSOperator(
@@ -107,18 +106,15 @@ for provider, exchange in product(_PROVIDERS, _EXCHANGES):
                 }
             ]
         },
-        placement_strategy= [
-                {
-                    'type': 'spread',
-                    'field': 'instanceId'
-                },
+        placement_strategy=[
+            {"type": "spread", "field": "instanceId"},
         ],
         awslogs_group=ecs_awslogs_group,
         awslogs_stream_prefix=ecs_awslogs_stream_prefix,
-        execution_timeout=datetime.timedelta(minutes=15)
+        execution_timeout=datetime.timedelta(minutes=15),
     )
     start_task >> downloading_task >> end_download
-    
+
 compare_command = [
     "/app/im_v2/{}/data/extract/compare_realtime_and_historical.py",
     "--end_timestamp '{{ execution_date }}'",
@@ -127,20 +123,19 @@ compare_command = [
     "--exchange_id '{}'",
     "--db_table '{}_ohlcv{}'",
     "--aws_profile 'ck'",
-    "--s3_path '{}/{}'"
+    "--s3_path '{}/{}'",
 ]
 
 for provider, exchange in product(_PROVIDERS, _EXCHANGES):
 
-    #TODO(Juraj): Make this code more readable.
+    # TODO(Juraj): Make this code more readable.
     # Do a deepcopy of the bash command list so we can reformat params on each iteration.
     curr_bash_command = copy.deepcopy(compare_command)
     curr_bash_command[0] = curr_bash_command[0].format(provider)
     curr_bash_command[4] = curr_bash_command[4].format(exchange)
     curr_bash_command[5] = curr_bash_command[5].format(provider, _TABLE_SUFFIX)
     curr_bash_command[-1] = curr_bash_command[-1].format(
-        s3_daily_staged_data_path,
-        provider
+        s3_daily_staged_data_path, provider
     )
 
     comparing_task = ECSOperator(
@@ -150,7 +145,7 @@ for provider, exchange in product(_PROVIDERS, _EXCHANGES):
         cluster=ecs_cluster,
         task_definition=ecs_task_definition,
         launch_type="EC2",
-        # Make sure to run even if some of 
+        # Make sure to run even if some of
         # the upstream tasks failed.
         trigger_rule="all_done",
         overrides={
@@ -161,14 +156,11 @@ for provider, exchange in product(_PROVIDERS, _EXCHANGES):
                 }
             ]
         },
-        placement_strategy= [
-                {
-                    'type': 'spread',
-                    'field': 'instanceId'
-                },
+        placement_strategy=[
+            {"type": "spread", "field": "instanceId"},
         ],
         awslogs_group=ecs_awslogs_group,
         awslogs_stream_prefix=ecs_awslogs_stream_prefix,
-        execution_timeout=datetime.timedelta(minutes=10)
+        execution_timeout=datetime.timedelta(minutes=10),
     )
     end_download >> comparing_task
