b38993999fb50ec52c061a613fae0e84d21e0bd3
Timur Galimov <tuimazy2@gmail.com>
2022-04-28 17:28:27 +0300

    CMTask1773: Changed script arguments

--- im_v2/ccxt/data/extract/download_realtime_for_one_exchange_periodically.py
+++ im_v2/ccxt/data/extract/download_realtime_for_one_exchange_periodically.py
@@ -4,8 +4,6 @@ Script to download OHLCV data for a single exchange from CCXT periodically.
 TODO(timurg): Move this to im_v2/common
 
 Use as:
-
-# Download OHLCV data for binance 'v3', saving dev_stage:
 > im_v2/ccxt/data/extract/download_realtime_for_one_exchange_periodically.py \
     --start_timestamp '20211110-101100' \
     --end_timestamp '20211110-101200' \
@@ -14,9 +12,10 @@ Use as:
     --db_stage 'dev' \
     --db_table 'ccxt_ohlcv_test' \
     --aws_profile 'ck' \
-    --s3_path 's3://cryptokaizen-data-test/realtime/'\
-    --run_for_min '10'\
-    --interval_min '1'
+    --s3_path 's3://cryptokaizen-data-test/realtime/' \
+    --trigger_time '2022-04-28 14:22:51' \
+    --interval_min '1' \
+    --stop_time '2022-04-28 17:23:51'
 """
 
 import argparse
@@ -39,11 +38,19 @@ def _parse() -> argparse.ArgumentParser:
         description=__doc__,
         formatter_class=argparse.RawTextHelpFormatter,
     )
-    parser.add_argument("--incremental", action="store_true")
     parser.add_argument(
-        "--run_for_min",
-        type=int,
-        help="Total running time, in minutes",
+        "--trigger_time",
+        action="store",
+        default=datetime.now().replace(second=0, microsecond=0) + timedelta(minutes=1),
+        type=str,
+        help="When downloads should start",
+    )
+    parser.add_argument(
+        "--stop_time",
+        action="store",
+        default=datetime.now().replace(second=0, microsecond=0) + timedelta(minutes=15),
+        type=str,
+        help="When script should stop",
     )
     parser.add_argument(
         "--interval_min",
@@ -61,27 +68,23 @@ def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
     # Time range for each download
     time_window = 5
-    #
-    run_for_min = args.run_for_min
-    interval_min = args.interval_min
     # Check values.
-    for value in run_for_min, interval_min:
-        hdbg.dassert_lte(1, value, msg="Value: '{value}' should be greater then 0")
-    # TODO(timurg): Align the use of "5" via a constant or argument throughout the scripts.
+    stop_time = datetime.strptime(args.stop_time, '%Y-%m-%d %H:%M:%S')
+    trigger_time = datetime.strptime(args.trigger_time, '%Y-%m-%d %H:%M:%S')
+    interval_min = args.interval_min
+    hdbg.dassert_lte(1, interval_min, "interval_min: %s should be greater then 0", interval_min)
+    hdbg.dassert_lt(datetime.now(), trigger_time, "trigger_time: %s should be greater then current time: %s", trigger_time, datetime.now())
+    hdbg.dassert_lt(trigger_time, stop_time, "stop_time: %s should be greater then trigger_time: %s", stop_time, trigger_time)
+    #
     # Error will be raised if we miss full 5 minute window of data,
     # even if the next download succeeds, we don't recover all of the previous data.
     failures_limit = time_window // interval_min + time_window % interval_min
     consecutive_failures_left = failures_limit
     # Delay start
-    iteration_start_time = datetime.now()
-    iteration_delay_sec = 0.0
-    if iteration_start_time.second != 0:
-        iteration_start_time = iteration_start_time.replace(second=0, microsecond=0) + timedelta(minutes=1)
-        iteration_delay_sec = (iteration_start_time - datetime.now()).total_seconds()
-        _LOG.info("Delay: %s seconds before first download", iteration_delay_sec)
-    # Save time limit.
-    script_stop_time = iteration_start_time + timedelta(minutes=run_for_min)
-    while datetime.now() < script_stop_time:
+    iteration_start_time = trigger_time
+    iteration_delay_sec = (iteration_start_time - datetime.now()).total_seconds()
+    _LOG.info("Delay: %s seconds before first download", iteration_delay_sec)
+    while datetime.now() < stop_time and consecutive_failures_left:
         # Wait until next download.
         time.sleep(iteration_delay_sec)
         # TODO(timurg): investigate why end_timestamp ignored, and returned data up to now.
@@ -92,7 +95,7 @@ def _main(parser: argparse.ArgumentParser) -> None:
             imvcdeexut.download_realtime_for_one_exchange(args, imvcdeexcl.CcxtExchange)
             # Reset failures counter.
             consecutive_failures_left = failures_limit
-        except Exception as e:
+        except (KeyboardInterrupt, Exception) as e:
             consecutive_failures_left -= 1
             _LOG.error("Download failed: %s, %s failures left", str(e), consecutive_failures_left)
             # Download failed.
@@ -115,7 +118,7 @@ def _main(parser: argparse.ArgumentParser) -> None:
             iteration_delay_sec = (iteration_start_time + timedelta(minutes=interval_min) - datetime.now()).total_seconds()
             # Add interval in order to get next download time.
             iteration_start_time = iteration_start_time + timedelta(minutes=interval_min)
-            _LOG.info("Succesfull download, took %s sec, delay %s sec until next iteration", download_duration_sec, iteration_delay_sec)
+            _LOG.info("Successfully completed, iteration took %s sec, delay %s sec until next iteration", download_duration_sec, iteration_delay_sec)
 
 
 if __name__ == "__main__":
