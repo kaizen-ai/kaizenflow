29c381a860ee623d8f39063c681460394f99715d
Nina Lee <nina.tru94@gmail.com>
2022-05-03 23:57:49 +0000

    added assert

--- helpers/hpandas.py
+++ helpers/hpandas.py
@@ -404,6 +404,7 @@ def drop_duplicates(
         str(*args),
         str(**kwargs),
     )
+    hdbg.dassert_not_in("index", data.columns.tolist())
     num_rows_before = data.shape[0]
     # Get all columns list for subset if no subset is passed.
     if subset is None:
