5d6662e7c687f621609a8eff96c0c3e04436849e
PomazkinG <pomazking@gmail.com>
2022-05-06 19:08:42 +0000

    checkpoint

--- dataflow/model/experiment_config.py
+++ dataflow/model/experiment_config.py
@@ -102,8 +102,8 @@ def get_period(period: str) -> Tuple[pd.Timestamp, pd.Timestamp]:
         start_datetime = datetime.datetime(2012, 1, 1)
         end_datetime = datetime.datetime(2022, 1, 1)
     elif period == "2018_2022":
-        start_datetime = datetime.datetime(2012, 1, 1)
-        end_datetime = datetime.datetime(2022, 3, 1)
+        start_datetime = datetime.datetime(2018, 1, 1)
+        end_datetime = datetime.datetime(2022, 5, 7)
     elif period == "2019_2022":
         start_datetime = datetime.datetime(2019, 1, 1)
         end_datetime = datetime.datetime(2022, 3, 1)
