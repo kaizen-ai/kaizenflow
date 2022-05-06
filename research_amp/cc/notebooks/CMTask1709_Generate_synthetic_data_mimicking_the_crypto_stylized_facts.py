31d96b799c3d3edac293bce209d352db74f6e7de
max-rsrch <sergeychikovm@gmail.com>
2022-05-06 17:49:43 +0000

    Checkpoint

--- research_amp/cc/notebooks/CMTask1709_Generate_synthetic_data_mimicking_the_crypto_stylized_facts.py
+++ research_amp/cc/notebooks/CMTask1709_Generate_synthetic_data_mimicking_the_crypto_stylized_facts.py
@@ -13,7 +13,8 @@
 # ---
 
 # %% [markdown]
-# Compute random returns as Gaussians or t-distributions
+# - Compute returns from the real data.
+# - Pre-define the hit rate and calculate predictions, hits and confidence intervals.
 
 # %% [markdown]
 # # Imports
@@ -25,222 +26,16 @@ import im_v2.common.universe as ivcu
 import requests
 import helpers.hdatetime as hdateti
 import statsmodels
-
+import random
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import core.finance as cofinanc
 import dataflow.core as dtfcore
 import dataflow.system.source_nodes as dtfsysonod
+import helpers.hdbg as hdbg
 import core.artificial_signal_generators as carsigen
-
-# %% [markdown]
-# # Generate the data
-
-# %% [markdown]
-# ## Using `generate_random_price_data` for multiple assets
-
-# %% [markdown]
-# Problems:
-# - Generating prices are the same for all assets
-# - Distribution of returns is not normal
-
-# %%
-start_datetime = pd.Timestamp("2021-01-01")
-end_datetime = pd.Timestamp("2021-01-31")
-columns = ["close"]
-asset_ids = list(range(2))
-freq = "1T"
-initial_price = 29000
-seed = 100,
-
-df = cfmadaex.generate_random_price_data(
-start_datetime,
-end_datetime,
-columns,
-asset_ids,
-freq=freq,
-initial_price=initial_price,
-seed=seed)
-
-df = df.set_index("timestamp_db").drop(columns=["start_datetime", "end_datetime"])
-df = dtfsysonod._convert_to_multiindex(df, "asset_id")
-df.head()
-
-# %%
-node_returns_config = {
-    "in_col_groups": [
-        ("close",),
-    ],
-    "out_col_group": (),
-    "transformer_kwargs": {
-        "mode": "pct_change",
-    },
-    "col_mapping": {
-        "close": "close.ret_0",
-    },
-}
-# Create the node that computes ret_0.
-nid = "ret0"
-node = dtfcore.GroupedColDfToDfTransformer(
-    nid,
-    transformer_func=cofinanc.compute_ret_0,
-    **node_returns_config,
-)
-# Compute the node on the data.
-rets = node.fit(df)
-
-# %%
-df = rets["df_out"]
-df.head(5)
-
-# %%
-df.swaplevel(axis=1)[0][["close"]].plot(figsize=(15,7))
-
-# %% [markdown]
-# ## Using `generate_random_price_data` for one asset
-
-# %% [markdown]
-# Problems:
-# - Distribution of returns is not normal
-
-# %%
-start_datetime = pd.Timestamp("2021-01-01")
-end_datetime = pd.Timestamp("2021-01-31")
-columns = ["close"]
-asset_ids = [1]
-freq = "1T"
-initial_price = 29000
-seed = 100,
-
-df = cfmadaex.generate_random_price_data(
-start_datetime,
-end_datetime,
-columns,
-asset_ids,
-freq=freq,
-initial_price=initial_price,
-seed=seed)
-
-df = df.set_index("timestamp_db").drop(columns=["start_datetime", "end_datetime", "asset_id"])
-df["rets"] = cofinanc.compute_ret_0(df[["close"]], "pct_change")
-df.head(3)
-
-# %%
-df[["close"]].plot(figsize=(15,7))
-df[["rets"]].plot(figsize=(15,7))
-
-# %%
-fig = plt.figure(figsize=(15, 7))
-ax1 = fig.add_subplot(1, 1, 1)
-df['rets'].hist(bins=50, ax=ax1)
-ax1.set_xlabel('Return')
-ax1.set_ylabel('Sample')
-ax1.set_title('Return distribution')
-plt.show()
-
-# %% [markdown]
-# ## Using `generate_random_bars`
-
-# %% [markdown]
-# Problems:
-# - Strange outcomes
-
-# %% run_control={"marked": false}
-start_datetime = pd.Timestamp("2021-01-01")
-end_datetime = pd.Timestamp("2021-01-31")
-
-df = cfmadaex.generate_random_bars(
-    start_datetime,
-    end_datetime,
-    asset_ids = [1,2],
-)
-
-df = df.set_index("timestamp_db").drop(columns=["start_datetime", "end_datetime"])
-df = dtfsysonod._convert_to_multiindex(df, "asset_id")
-df.head()
-
-# %%
-df["close"].plot(figsize=(15,7))
-
-# %%
-node_returns_config = {
-    "in_col_groups": [
-        ("close",),
-    ],
-    "out_col_group": (),
-    "transformer_kwargs": {
-        "mode": "pct_change",
-    },
-    "col_mapping": {
-        "close": "close.ret_0",
-    },
-}
-# Create the node that computes ret_0.
-nid = "ret0"
-node = dtfcore.GroupedColDfToDfTransformer(
-    nid,
-    transformer_func=cofinanc.compute_ret_0,
-    **node_returns_config,
-)
-# Compute the node on the data.
-rets = node.fit(df)
-rets["df_out"].head(3)
-
-# %%
-rets["df_out"]["close.ret_0"].plot(figsize=(15,7))
-
-# %% [markdown]
-# ## Using returns directly (`get_gaussian_walk`)
-
-# %% [markdown]
-# Seems correct
-
-# %%
-drift = 0
-vol = 0.2
-size = 252
-seed = 10
-rets = carsigen.get_gaussian_walk(drift, vol, size, seed=seed).diff()
-
-# %%
-rets.plot()
-
-# %%
-fig = plt.figure(figsize=(15, 7))
-ax1 = fig.add_subplot(1, 1, 1)
-rets.hist(bins=100, ax=ax1)
-ax1.set_xlabel('Return')
-ax1.set_ylabel('Sample')
-ax1.set_title('Returns distribution')
-plt.show()
-
-# %% [markdown]
-# For each bar, generate random predictions with a given hit rate
-# - Compute the hit rate to confirm
-# - def hit_rate(y, y_hat):
-#
-
-# %%
-rets_df = rets.to_frame().iloc[1:]
-rets_df.columns = ["rets"]
-
-# 0 -> price goes down
-# 1 -> price goes up
-rets_df["prediction"] = np.random.randint(0, 2, rets_df.shape[0])
-# Get the difference to estimate the validity of prediciton.
-rets_df["diff"] = rets_df.rets.diff()
-# Estimate the prediciton.
-rets_df["is_prediction_right"] = ((rets_df["diff"]>0)&(rets_df["prediction"]==1))|((rets_df["diff"]<0)&(rets_df["prediction"]==0))
-# Get rid of unnecessary columns
-rets_df = rets_df.drop(columns=["diff"])
-rets_df = rets_df.iloc[1:]
-
-# %%
-rets_df
-
-# %%
-print(f"Hit Rate: {round(rets_df.is_prediction_right.sum()/rets_df.shape[0], 4)*100}%")
+import helpers.hpandas as hpandas
 
 
 # %% [markdown]
@@ -351,11 +146,6 @@ btc_df = read_crypto_chassis_ohlcv(["binance::BTC_USDT"], pd.Timestamp("2021-01-
 # %%
 btc = btc_df.copy()
 
-# %%
-btc["rets"] = btc["close"].pct_change()
-
-btc.describe()
-
 # %% run_control={"marked": false}
 # Calculate returns.
 btc["rets"] = btc["close"].pct_change()
@@ -367,12 +157,12 @@ btc["rets_cleaned"] = btc["rets"] - btc["rets_sma"]
 btc["rets_cleaned"].plot()
 
 # %%
+# Exclude NaNs for the better analysis (at least one in the beginning because of `pct_change()`)
 rets = btc[["rets"]]
-
-# TODO(gp): Always use the dropna function that reports the number of nans removed
-rets = rets[rets.rets.notna()]
+rets = hpandas.dropna(rets, report_stats=True)
 
 # %% run_control={"marked": false}
+# Show the distribution.
 fig = plt.figure(figsize=(15, 7))
 ax1 = fig.add_subplot(1, 1, 1)
 rets.hist(bins=300, ax=ax1)
@@ -381,173 +171,70 @@ ax1.set_ylabel('Sample')
 ax1.set_title('Returns distribution')
 plt.show()
 
-# %%
-rets_df["rets"]
 
-# %%
-import helpers.hdbg as hdbg
-
-# TODO(Max): Pass hit rate and 
-def add_hit_rate(rets_df, hit_rate, seed):
-    hdbg.dassert_isinstance(rets_df, pd.DataFrame)
-    rets_df = rets_df.copy()
-    #rets_df.columns = ["rets"]
-
-    np.random.seed(seed)
-
-    # (-1) -> price goes down
-    # 1 -> price goes up
-    uniform = np.random.randint(0, 2, rets_df.shape[0])
-    #uniform = uniform - (2 * hit_rate - 1 - 0.5)
-    rets_df["prediction"] = 2 * uniform - 1
-    # Get the difference to estimate the validity of prediciton.
-    #rets_df["diff"] = rets_df.rets.diff()
-    # Estimate the prediciton.
-    #rets_df["hit"] = ((rets_df["diff"]>0)&(rets_df["prediction"]==1))|((rets_df["diff"]<0)&(rets_df["prediction"]==-1))
-    rets_df["hit"] = (rets_df["rets"] * rets_df["prediction"] >= 0)
-    # Get rid of unnecessary columns
-    #rets_df = rets_df.drop(columns=["diff"])
-    #rets_df = rets_df.iloc[1:]
-
-    #rets_df.head(3)
-    return rets_df
-
-
-# %%
-dd = add_hit_rate(rets_df, 0.7, 1)
-dd#print(rets_df["rets"])
-
-# %%
-dd["hit"].mean()
-
-# %%
-import random
-
+# %% [markdown]
+# # Pre-defined Predictions, Hit Rates and Confidence Interval
 
 # %% run_control={"marked": false}
-# X ~ U[-1, 1], E[X] = 0
-# hit_rate = 0
-
-#n = df.shape[0]
 def get_predictions(df, hit_rate, seed):
+    """
+    :param df: Desired sample with OHLCV data and calculated returns
+    :param hit_rate: Desired percantage of successful predictions
+    :param seed: Experiment stance
+    """
     n = df.shape[0]
-    #hit_rate = 0.0
-
     rets = df["rets"].values
-    #rets = rets[:n]
-    #print("rets=", rets)
-
-    # mask contains 1 for a desired hit and -1 for a miss.
+    # Mask contains 1 for a desired hit and -1 for a miss.
     num_hits = int((1 - hit_rate) * n)
     mask = pd.Series(([-1] * num_hits) + ([1] * (n - num_hits)))
-
-    
+    # Randomize the location of the outcomes.
     random.shuffle(mask)
-    #print("mask=", mask)
-    #print(mask.mean())
-
-    #print("sign(rets)=", np.sign(rets))
-    #pred = pd.Series(np.sign(rets) * mask, index=df.index)
+    # Construct predictions using the sign of the actual returns.
     pred = pd.Series(np.sign(rets) * mask)
-    #print("pred=", pred)
-
-    #hit_rate = (np.sign(pred * rets) >= 0).mean()
-    #print(hit_rate)
-
-    #print((pred * rets).mean())
+    # Change the index for easy attachment to initial DataFrame.
     pred.index = df.index
     return pred
 
-# %%
-hit_rate = 0.8
-dd = btc.head(1000)
-
-# %%
-dd["predictions"] = get_predictions(dd, hit_rate, 10)
-dd = dd[["rets", "predictions"]]
-dd["hit"] = (dd["rets"] * dd["predictions"] >= 0)
-dd
-
-# %%
-dd["hit"].mean()
-
-
-# %%
-
-# %%
-
-# %%
 def calculate_confidence_interval(hit_series, alpha, method):
     """
     :param alpha: Significance level
     :param method: "normal", "agresti_coull", "beta", "wilson", "binom_test"
     """
-    point_estimate = hit.mean()
+    point_estimate = hit_series.mean()
     hit_lower, hit_upper = statsmodels.stats.proportion.proportion_confint(
-        count=hit.sum(), nobs=hit.count(), alpha=alpha, method=method
+        count=hit_series.sum(), nobs=hit_series.count(), alpha=alpha, method=method
     )
     result_values_pct = [100 * point_estimate, 100 * hit_lower, 100 * hit_upper]
     conf_alpha = (1 - alpha / 2) * 100
     print(f"hit_rate: {result_values_pct[0]}")
     print(f"hit_rate_lower_CI_({conf_alpha}%): {result_values_pct[1]}")
     print(f"hit_rate_upper_CI_({conf_alpha}%): {result_values_pct[2]}")
+    
+def get_predictions_hits_and_stats(df, hit_rate, seed, alpha, method):
+    """
+    :param df: Desired sample with OHLCV data and calculated returns
+    :param hit_rate: Desired percantage of successful predictions
+    :param seed: Experiment stance
+    :param alpha: Significance level for CI
+    :param method: "normal", "agresti_coull", "beta", "wilson", "binom_test"
+    """
+    df= df.copy()
+    df["predictions"] = get_predictions(df, hit_rate, seed)
+    df = df[["rets", "predictions"]]
+    df= df.copy()
+    df["hit"] = (df["rets"] * df["predictions"] >= 0)
+    # D
+    df = df[1:]
+    calculate_confidence_interval(df["hit"], alpha, method)
+    return df
 
 
 # %%
-# Calculate confidence intervals.
-hit = rets_df["hit"]
+sample = btc.head(1000)
+hit_rate = 0.7
+seed = 20
 alpha = 0.05
 method = "normal"
-calculate_confidence_interval(hit, alpha, method)
 
-# %%
-# A model makes a prediction on the rets and then it's realized.
-pnl = (rets_df["prediction"] * rets_df["rets"]).cumsum()
-
-pnl.plot()
-
-
-# %%
-# Relationship between hit rate and pnl
-
-# %%
-def compute_pnl(rets_df):
-    return (rets_df["prediction"] * rets_df["rets"]).sum()
-
-
-# %%
-compute_pnl(rets_df)
-
-# %%
-print(rets_df)
-
-# %%
-# Number of experiments per value of hit rate.
-#n_experiment = 100
-n_experiment = 1
-# Every seed corresponds to a different "model".
-seed = 10
-results = {}
-for hit_rate in np.linspace(0.4, 0.6, num=3):
-    for i in range(n_experiment):
-        df_tmp = add_hit_rate(rets_df, hit_rate, seed)
-        hit_rate = df_tmp["hit"].mean()
-        pnl = compute_pnl(df_tmp)
-        results[hit_rate].append(pnl)
-        print(hit_rate, pnl)
-        seed += 1
-        
-        
-# hit_rate -> pnls
-# 0.4 -> [0.1, 0.2, 0.3]
-# 0.5 -> []
-
-
-# %%
-# Step 1: split the notebooks in gallery and not
-# - gallery notebook with the first 2-3 sessions showing the functions
-# - 1709 contains only the good stuff
-# Step 2: 
-# - clean up get_predictions() and add 1 unit test that checks the schema of df, freeze the output
-# Step 3:
-# - bootstrapping (montecarlo simulation) to compute pnl = f(hit_rate)
+hit_df = get_predictions_hits_and_stats(sample, hit_rate, seed, alpha, method)
+display(hit_df)
