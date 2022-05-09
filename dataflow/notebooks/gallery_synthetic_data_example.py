df41216293eef92ebd9b9d128fe1b6e5af9103fb
max-rsrch <sergeychikovm@gmail.com>
2022-05-09 10:32:19 +0000

    Checkpoint

--- dataflow/notebooks/gallery_synthetic_data_example.py
+++ dataflow/notebooks/gallery_synthetic_data_example.py
@@ -22,23 +22,16 @@
 # ## Imports
 
 # %%
-import core.finance.market_data_example as cfmadaex
-from typing import List
-import im_v2.common.universe as ivcu
-import requests
-import helpers.hdatetime as hdateti
-import statsmodels
-import random
-import pandas as pd
-import numpy as np
+import logging
+
 import matplotlib.pyplot as plt
+import pandas as pd
+
+import core.artificial_signal_generators as carsigen
 import core.finance as cofinanc
+import core.finance.market_data_example as cfmadaex
 import dataflow.core as dtfcore
 import dataflow.system.source_nodes as dtfsysonod
-import helpers.hdbg as hdbg
-import core.artificial_signal_generators as carsigen
-import logging
-import os
 import helpers.hdbg as hdbg
 import helpers.hprint as hprint
 
@@ -61,23 +54,24 @@ hprint.config_notebook()
 # - Distribution of returns is not normal
 
 # %%
-#Generate the initial price data.
+# Generate the initial price data.
 start_datetime = pd.Timestamp("2021-01-01")
 end_datetime = pd.Timestamp("2021-01-31")
 columns = ["close"]
 asset_ids = list(range(2))
 freq = "1T"
 initial_price = 29000
-seed = 100,
+seed = (100,)
 
 df = cfmadaex.generate_random_price_data(
-start_datetime,
-end_datetime,
-columns,
-asset_ids,
-freq=freq,
-initial_price=initial_price,
-seed=seed)
+    start_datetime,
+    end_datetime,
+    columns,
+    asset_ids,
+    freq=freq,
+    initial_price=initial_price,
+    seed=seed,
+)
 
 df = df.set_index("timestamp_db").drop(columns=["start_datetime", "end_datetime"])
 df = dtfsysonod._convert_to_multiindex(df, "asset_id")
@@ -113,11 +107,11 @@ df.head(5)
 
 # %%
 # Plot the prices.
-df[["close"]].plot(figsize=(15,7))
+df[["close"]].plot(figsize=(15, 7))
 
 # %%
 # Plot returns.
-df[["close.ret_0"]].plot(figsize=(15,7))
+df[["close.ret_0"]].plot(figsize=(15, 7))
 
 # %% [markdown]
 # ## Using `generate_random_price_data` for one asset
@@ -134,32 +128,35 @@ columns = ["close"]
 asset_ids = [1]
 freq = "1T"
 initial_price = 29000
-seed = 100,
+seed = (100,)
 
 df = cfmadaex.generate_random_price_data(
-start_datetime,
-end_datetime,
-columns,
-asset_ids,
-freq=freq,
-initial_price=initial_price,
-seed=seed)
+    start_datetime,
+    end_datetime,
+    columns,
+    asset_ids,
+    freq=freq,
+    initial_price=initial_price,
+    seed=seed,
+)
 
-df = df.set_index("timestamp_db").drop(columns=["start_datetime", "end_datetime", "asset_id"])
+df = df.set_index("timestamp_db").drop(
+    columns=["start_datetime", "end_datetime", "asset_id"]
+)
 df["rets"] = cofinanc.compute_ret_0(df[["close"]], "pct_change")
 df.head(3)
 
 # %%
-df[["close"]].plot(figsize=(15,7))
-df[["rets"]].plot(figsize=(15,7))
+df[["close"]].plot(figsize=(15, 7))
+df[["rets"]].plot(figsize=(15, 7))
 
 # %%
 fig = plt.figure(figsize=(15, 7))
 ax1 = fig.add_subplot(1, 1, 1)
-df['rets'].hist(bins=50, ax=ax1)
-ax1.set_xlabel('Return')
-ax1.set_ylabel('Sample')
-ax1.set_title('Return distribution')
+df["rets"].hist(bins=50, ax=ax1)
+ax1.set_xlabel("Return")
+ax1.set_ylabel("Sample")
+ax1.set_title("Return distribution")
 plt.show()
 
 # %% [markdown]
@@ -178,7 +175,7 @@ end_datetime = pd.Timestamp("2021-01-31")
 df = cfmadaex.generate_random_bars(
     start_datetime,
     end_datetime,
-    asset_ids = [1,2],
+    asset_ids=[1, 2],
 )
 
 df = df.set_index("timestamp_db").drop(columns=["start_datetime", "end_datetime"])
@@ -186,7 +183,7 @@ df = dtfsysonod._convert_to_multiindex(df, "asset_id")
 
 # %%
 # Show prices.
-df["close"].plot(figsize=(15,7))
+df["close"].plot(figsize=(15, 7))
 
 # %%
 # Calculate returns.
@@ -215,7 +212,7 @@ rets["df_out"].head(3)
 
 # %%
 # Show returns.
-rets["df_out"]["close.ret_0"].plot(figsize=(15,7))
+rets["df_out"]["close.ret_0"].plot(figsize=(15, 7))
 
 # %% [markdown]
 # ## Using returns directly (`get_gaussian_walk`)
@@ -234,7 +231,7 @@ rets.plot()
 fig = plt.figure(figsize=(15, 7))
 ax1 = fig.add_subplot(1, 1, 1)
 rets.hist(bins=50, ax=ax1)
-ax1.set_xlabel('Return')
-ax1.set_ylabel('Sample')
-ax1.set_title('Returns distribution')
+ax1.set_xlabel("Return")
+ax1.set_ylabel("Sample")
+ax1.set_title("Returns distribution")
 plt.show()
