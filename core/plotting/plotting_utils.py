309c70b6dfedc76ee67c4275843b1e32e4baa7c5
max-rsrch <sergeychikovm@gmail.com>
2022-05-09 15:59:45 +0000

    Checkpoint

--- core/plotting/plotting_utils.py
+++ core/plotting/plotting_utils.py
@@ -24,6 +24,7 @@ COLORMAP = Union[str, mpl.colors.Colormap]
 
 FIG_SIZE = (20, 5)
 
+mpl.rcParams['figure.dpi'] = 300
 
 def get_multiple_plots(
     num_plots: int,
@@ -149,6 +150,16 @@ def plot_barplot(
         if orientation == "horizontal":
             return x_ + max(width_, 0), y_
         raise ValueError("Invalid orientation='%s'" % orientation)
+
+    # Set the size of the font in the plot.
+    size = 15
+    plt.rc('font', size=size)  # controls default text sizes
+    plt.rc('axes', titlesize=size)  # fontsize of the axes title
+    plt.rc('axes', labelsize=size)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=size)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=size)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=size)  # legend fontsize
+    plt.rc('figure', titlesize=size)  # fontsize of the figure title
 
     # Get default figure size.
     if figsize is None:
