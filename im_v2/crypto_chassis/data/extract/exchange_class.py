76175dbcee2c6d6dbd36f4661d74e1d478fde9c8
tamriq <tamriqua@gmail.com>
2022-05-12 13:55:03 +0000

    CmTask1873: add examples

--- im_v2/crypto_chassis/data/extract/exchange_class.py
+++ im_v2/crypto_chassis/data/extract/exchange_class.py
@@ -82,7 +82,10 @@ class CryptoChassisExchange:
         :param currency_pair: the pair of currency to exchange, e.g. `btc-usd`
         :param start_timestamp: start of processing
         :param depth: allowed values: 1 to 10. Defaults to 1. 
-        :return: market depth data
+        :return: market depth data, e.g.
+             timestamp 	bid_price 	bid_size 	ask_price 	ask_size
+          0 	1641686400 	41678.35 	0.017939 	41686.97 	1.69712319
+          1 	1641686401 	41678.35 	0.017939 	41690.58 	0.04
         """
         # Verify that date parameters are of correct format.
         if start_timestamp:
@@ -158,8 +161,8 @@ class CryptoChassisExchange:
             endpoint is 1 request per second per public IP.
         :return: ohlcv data, e.g.
                 timestamp 	open 	high 	low 	close 	volume 	vwap 	number_of_trades 	twap
-            0 	1634011620 	56775.59 	56799.51 	56775.59 	56799.51 	0.184718 	56781.6130 	9 	56783.3033
-            1 	1634011680 	56822.35 	56832.25 	56815.59 	56815.59 	0.363495 	56828.9840 	16 	56828.9512
+          0 	1634011620 	56775.59 	56799.51 	56775.59 	56799.51 	0.184718 	56781.6130 	9 	56783.3033
+          1 	1634011680 	56822.35 	56832.25 	56815.59 	56815.59 	0.363495 	56828.9840 	16 	56828.9512
         """
         # Verify that date parameters are of correct format.
         if start_timestamp:
@@ -231,7 +234,10 @@ class CryptoChassisExchange:
         :param exchange: the name of exchange, e.g. `binance`, `coinbase`
         :param currency_pair: the pair of currency to download, e.g. `btc-usd`
         :param start_time: timestamp of start
-        :return: trade data
+        :return: trade data, e.g.
+                timestamp 	price 	size 	is_buyer_maker
+          0 	1641686404 	41692.50 	0.012473 	0
+          1 	1641686441 	41670.00 	0.001194 	0
         """
         # Verify that date parameters are of correct format.
         if start_timestamp:
