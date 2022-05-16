3f6c8e8528ad92f06c026f96f068123a5b703757
Timur Galimov <tuimazy2@gmail.com>
2022-05-16 17:56:53 +0300

    CMTask1889: moved functionality into common file

--- im_v2/ccxt/data/extract/download_realtime_for_one_exchange_periodically.py
+++ im_v2/ccxt/data/extract/download_realtime_for_one_exchange_periodically.py
@@ -12,28 +12,16 @@ Use as:
     --aws_profile 'ck' \
     --s3_path 's3://cryptokaizen-data-test/realtime/' \
     --interval_min '1' \
-    --start_time '2022-05-03 00:40:00' \
-    --stop_time '2022-05-03 00:30:00'
+    --start_time '2022-05-16 00:45:00' \
+    --stop_time '2022-05-16 00:55:00'
 """
 
 import argparse
-import logging
-import time
-from datetime import datetime, timedelta
-
-import pandas as pd
-
-import helpers.hdbg as hdbg
 import helpers.hparser as hparser
 import helpers.hs3 as hs3
 import im_v2.ccxt.data.extract.exchange_class as imvcdeexcl
 import im_v2.common.data.extract.extract_utils as imvcdeexut
 import im_v2.common.db.db_utils as imvcddbut
-from helpers.hthreading import timeout
-
-_LOG = logging.getLogger(__name__)
-# Time limit for each download execution.
-TIMEOUT_SEC = 60
 
 
 def _parse() -> argparse.ArgumentParser:
@@ -80,115 +68,9 @@ def _parse() -> argparse.ArgumentParser:
     return parser  # type: ignore[no-any-return]
 
 
-@timeout(TIMEOUT_SEC)
-def _download_realtime_for_one_exchange_with_timeout(
-    args: argparse.Namespace,
-    start_timestamp: datetime,
-    end_timestamp: datetime,
-) -> None:
-    """
-    Wrapper for download_realtime_for_one_exchange. Download data for given
-    time range, raise Interrupt in case if timeout occured.
-
-    :param args: arguments passed on script run
-    :param start_timestamp: beginning of the downloaded period
-    :param end_timestamp: end of the downloaded period
-    """
-    args.start_timestamp, args.end_timestamp = start_timestamp, end_timestamp
-    _LOG.info(
-        "Starting data download from: %s, till: %s",
-        start_timestamp,
-        end_timestamp,
-    )
-    imvcdeexut.download_realtime_for_one_exchange(args, imvcdeexcl.CcxtExchange)
-
-
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
-    # Time range for each download.
-    time_window_min = 5
-    # Check values.
-    start_time = (
-        pd.Timestamp(args.start_time).to_pydatetime().replace(tzinfo=None)
-    )
-    stop_time = pd.Timestamp(args.stop_time).to_pydatetime().replace(tzinfo=None)
-    interval_min = args.interval_min
-    hdbg.dassert_lte(
-        1, interval_min, "interval_min: %s should be greater than 0", interval_min
-    )
-    hdbg.dassert_lt(datetime.now(), start_time, "start_time is in the past")
-    hdbg.dassert_lt(start_time, stop_time, "stop_time is less than start_time")
-    # Error will be raised if we miss full 5 minute window of data,
-    # even if the next download succeeds, we don't recover all of the previous data.
-    num_failures = 0
-    max_num_failures = (
-        time_window_min // interval_min + time_window_min % interval_min
-    )
-    # Delay start.
-    iteration_start_time = start_time
-    iteration_delay_sec = (iteration_start_time - datetime.now()).total_seconds()
-    while (
-        datetime.now() + timedelta(seconds=iteration_delay_sec) < stop_time
-        and num_failures < max_num_failures
-    ):
-        # Wait until next download.
-        _LOG.info("Delay %s sec until next iteration", iteration_delay_sec)
-        time.sleep(iteration_delay_sec)
-        start_timestamp = iteration_start_time - timedelta(
-            minutes=time_window_min
-        )
-        end_timestamp = datetime.now()
-        try:
-            _download_realtime_for_one_exchange_with_timeout(
-                args, start_timestamp, end_timestamp
-            )
-            # Reset failures counter.
-            num_failures = 0
-        except (KeyboardInterrupt, Exception) as e:
-            num_failures += 1
-            _LOG.error("Download failed %s", str(e))
-            # Download failed.
-            if num_failures >= max_num_failures:
-                raise RuntimeError(
-                    f"{max_num_failures} consecutive downloads were failed"
-                ) from e
-        # if the download took more than expected, we need to align on the grid.
-        if datetime.now() > iteration_start_time + timedelta(
-            minutes=interval_min
-        ):
-            _LOG.error(
-                "The download was not finished in %s minutes.", interval_min
-            )
-            iteration_delay_sec = 0
-            # Download that will start after repeated one, should follow to the initial schedule.
-            while datetime.now() > iteration_start_time + timedelta(
-                minutes=interval_min
-            ):
-                iteration_start_time = iteration_start_time + timedelta(
-                    minutes=interval_min
-                )
-        # If download failed, but there is time before next download.
-        elif num_failures > 0:
-            _LOG.info("Start repeat download immediately.")
-            iteration_delay_sec = 0
-        else:
-            download_duration_sec = (
-                datetime.now() - iteration_start_time
-            ).total_seconds()
-            # Calculate delay before next download.
-            iteration_delay_sec = (
-                iteration_start_time
-                + timedelta(minutes=interval_min)
-                - datetime.now()
-            ).total_seconds()
-            # Add interval in order to get next download time.
-            iteration_start_time = iteration_start_time + timedelta(
-                minutes=interval_min
-            )
-            _LOG.info(
-                "Successfully completed, iteration took %s sec",
-                download_duration_sec,
-            )
+    imvcdeexut.download_realtime_for_one_exchange_periodically(args, imvcdeexcl.CcxtExchange)
 
 
 if __name__ == "__main__":
--- im_v2/common/data/extract/extract_utils.py
+++ im_v2/common/data/extract/extract_utils.py
@@ -10,7 +10,8 @@ import im_v2.common.data.extract.extract_utils as imvcdeexut
 import argparse
 import os
 import time
-from typing import Any
+from typing import Any, Union, Type
+from datetime import datetime, timedelta
 
 import pandas as pd
 import psycopg2
@@ -22,7 +23,16 @@ import helpers.hs3 as hs3
 import helpers.hsql as hsql
 import im_v2.common.data.transform.transform_utils as imvcdttrut
 import im_v2.common.universe as ivcu
+import im_v2.ccxt.data.extract.exchange_class as imvcdeexcl
+import im_v2.talos.data.extract.exchange_class as imvcdeexut
 import im_v2.im_lib_tasks as imvimlita
+import logging
+
+from helpers.hthreading import timeout
+
+_LOG = logging.getLogger(__name__)
+# Time limit for each download execution.
+TIMEOUT_SEC = 60
 
 
 def add_exchange_download_args(
@@ -165,6 +175,119 @@ def download_realtime_for_one_exchange(
                 data.to_csv(f, index=False)
         # Remove duplicated entries.
         connection.cursor().execute(dup_query)
+
+
+@timeout(TIMEOUT_SEC)
+def _download_realtime_for_one_exchange_with_timeout(
+    args: argparse.Namespace,
+    exchange_class: Type[Union[imvcdeexcl.CcxtExchange, imvcdeexut.TalosExchange]],
+    start_timestamp: datetime,
+    end_timestamp: datetime,
+) -> None:
+    """
+    Wrapper for download_realtime_for_one_exchange. Download data for given
+    time range, raise Interrupt in case if timeout occured.
+
+    :param args: arguments passed on script run
+    :param start_timestamp: beginning of the downloaded period
+    :param end_timestamp: end of the downloaded period
+    """
+    args.start_timestamp, args.end_timestamp = start_timestamp, end_timestamp
+    _LOG.info(
+        "Starting data download from: %s, till: %s",
+        start_timestamp,
+        end_timestamp,
+    )
+    download_realtime_for_one_exchange(args, exchange_class)
+
+
+def download_realtime_for_one_exchange_periodically(
+    args: argparse.Namespace, exchange_class: Any
+) -> None:
+    # Time range for each download.
+    time_window_min = 5
+    # Check values.
+    start_time = (
+        pd.Timestamp(args.start_time).to_pydatetime().replace(tzinfo=None)
+    )
+    stop_time = pd.Timestamp(args.stop_time).to_pydatetime().replace(tzinfo=None)
+    interval_min = args.interval_min
+    hdbg.dassert_lte(
+        1, interval_min, "interval_min: %s should be greater than 0", interval_min
+    )
+    hdbg.dassert_lt(datetime.now(), start_time, "start_time is in the past")
+    hdbg.dassert_lt(start_time, stop_time, "stop_time is less than start_time")
+    # Error will be raised if we miss full 5 minute window of data,
+    # even if the next download succeeds, we don't recover all of the previous data.
+    num_failures = 0
+    max_num_failures = (
+        time_window_min // interval_min + time_window_min % interval_min
+    )
+    # Delay start.
+    iteration_start_time = start_time
+    iteration_delay_sec = (iteration_start_time - datetime.now()).total_seconds()
+    while (
+        datetime.now() + timedelta(seconds=iteration_delay_sec) < stop_time
+        and num_failures < max_num_failures
+    ):
+        # Wait until next download.
+        _LOG.info("Delay %s sec until next iteration", iteration_delay_sec)
+        time.sleep(iteration_delay_sec)
+        start_timestamp = iteration_start_time - timedelta(
+            minutes=time_window_min
+        )
+        end_timestamp = datetime.now()
+        try:
+            _download_realtime_for_one_exchange_with_timeout(
+                args, exchange_class, start_timestamp, end_timestamp
+            )
+            # Reset failures counter.
+            num_failures = 0
+        except (KeyboardInterrupt, Exception) as e:
+            num_failures += 1
+            _LOG.error("Download failed %s", str(e))
+            # Download failed.
+            if num_failures >= max_num_failures:
+                raise RuntimeError(
+                    f"{max_num_failures} consecutive downloads were failed"
+                ) from e
+        # if the download took more than expected, we need to align on the grid.
+        if datetime.now() > iteration_start_time + timedelta(
+            minutes=interval_min
+        ):
+            _LOG.error(
+                "The download was not finished in %s minutes.", interval_min
+            )
+            iteration_delay_sec = 0
+            # Download that will start after repeated one, should follow to the initial schedule.
+            while datetime.now() > iteration_start_time + timedelta(
+                minutes=interval_min
+            ):
+                iteration_start_time = iteration_start_time + timedelta(
+                    minutes=interval_min
+                )
+        # If download failed, but there is time before next download.
+        elif num_failures > 0:
+            _LOG.info("Start repeat download immediately.")
+            iteration_delay_sec = 0
+        else:
+            download_duration_sec = (
+                datetime.now() - iteration_start_time
+            ).total_seconds()
+            # Calculate delay before next download.
+            iteration_delay_sec = (
+                iteration_start_time
+                + timedelta(minutes=interval_min)
+                - datetime.now()
+            ).total_seconds()
+            # Add interval in order to get next download time.
+            iteration_start_time = iteration_start_time + timedelta(
+                minutes=interval_min
+            )
+            _LOG.info(
+                "Successfully completed, iteration took %s sec",
+                download_duration_sec,
+            )
 
 
 def download_historical_data(
