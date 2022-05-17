c6c384c80aea2a89915debbff68480cd35c414e1
Timur Galimov <tuimazy2@gmail.com>
2022-05-17 14:44:27 +0300

    CMTask1889: fix for using timestamp with timezone, added script for talos, fixed command in docstring for talos

--- im_v2/talos/data/extract/download_realtime_for_one_exchange_periodically.py
+++ im_v2/talos/data/extract/download_realtime_for_one_exchange_periodically.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 """
-Script to download OHLCV data for a single exchange from CCXT periodically.
+Script to download OHLCV data for a single exchange from Talos periodically.
 
 Use as:
-> im_v2/ccxt/data/extract/download_realtime_for_one_exchange_periodically.py \
+> im_v2/talos/data/extract/download_realtime_for_one_exchange_periodically.py \
     --exchange_id 'binance' \
-    --universe 'v3' \
+    --universe 'v1' \
     --db_stage 'dev' \
-    --db_table 'ccxt_ohlcv_test' \
+    --db_table 'talos_ohlcv_test' \
     --aws_profile 'ck' \
     --s3_path 's3://cryptokaizen-data-test/realtime/' \
     --interval_min '1' \
-    --start_time '2022-05-16 00:45:00' \
-    --stop_time '2022-05-16 00:55:00'
+    --start_time '2022-05-17T10:30:00.000000Z' \
+    --stop_time '2022-05-17T10:35:00.000000Z'
 """
 
 import argparse
 
 import helpers.hparser as hparser
 import helpers.hs3 as hs3
-import im_v2.ccxt.data.extract.exchange_class as imvcdeexcl
 import im_v2.common.data.extract.extract_utils as imvcdeexut
 import im_v2.common.db.db_utils as imvcddbut
+import im_v2.talos.data.extract.exchange_class as imvtdeexcl
 
 
 def _parse() -> argparse.ArgumentParser:
@@ -62,6 +62,14 @@ def _parse() -> argparse.ArgumentParser:
         type=str,
         help="Trading universe to download data for",
     )
+    parser.add_argument(
+        "--api_stage",
+        action="store",
+        required=False,
+        default="sandbox",
+        type=str,
+        help="(Optional) API 'stage' to use ('sandbox' or 'prod'), default: 'sandbox'",
+    )
     parser = hparser.add_verbosity_arg(parser)
     parser = imvcddbut.add_db_args(parser)
     parser = hs3.add_s3_args(parser)
@@ -71,7 +79,7 @@ def _parse() -> argparse.ArgumentParser:
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
     imvcdeexut.download_realtime_for_one_exchange_periodically(
-        args, imvcdeexcl.CcxtExchange
+        args, imvtdeexcl.TalosExchange
     )
 
 
