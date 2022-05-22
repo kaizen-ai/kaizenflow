a59baadff6871f60b1f941416a921028f02e7b38
PomazkinG <pomazking@gmail.com>
2022-05-22 18:12:52 +0000

    remove hack

--- im_v2/common/data/client/base_im_clients.py
+++ im_v2/common/data/client/base_im_clients.py
@@ -296,8 +296,6 @@ class ImClient(abc.ABC):
         """
         Apply normalizations to IM data.
         """
-        # TODO(Grisha): remove temporary hack.
-        df = df[~df.index.duplicated(keep='first')]
         _LOG.debug(hprint.to_str("full_symbol_col_name start_ts end_ts"))
         # 1) Drop duplicates.
         use_index = True
