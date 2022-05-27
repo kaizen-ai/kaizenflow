5179da78e3f7abca6fff7d233f681f5e07c3ddbb
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-05-27 12:37:58 +0000

    CMTask2021: Update Talos scripts

--- im_v2/talos/data/extract/download_realtime_for_one_exchange_periodically.py
+++ im_v2/talos/data/extract/download_realtime_for_one_exchange_periodically.py
@@ -10,6 +10,7 @@ Use as:
     --db_table 'talos_ohlcv_test' \
     --aws_profile 'ck' \
     --s3_path 's3://cryptokaizen-data-test/realtime/' \
+    --data_type 'ohlcv' \
     --interval_min '1' \
     --start_time '2022-05-18T10:55:00.000000Z' \
     --stop_time '2022-05-18T10:59:00.000000Z'
@@ -37,6 +38,13 @@ def _parse() -> argparse.ArgumentParser:
         type=str,
         help="(Optional) API 'stage' to use ('sandbox' or 'prod'), default: 'sandbox'",
     )
+    parser.add_argument(
+        "--data_type",
+        action="store",
+        required=True,
+        type=str,
+        help="OHLCV, market_depth or trades data.",
+    )
     parser = imvcdeexut.add_periodical_download_args(parser)
     parser = hparser.add_verbosity_arg(parser)
     parser = imvcddbut.add_db_args(parser)
@@ -46,8 +54,11 @@ def _parse() -> argparse.ArgumentParser:
 
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
+    # Initialize the Talos Extractor class.
+    exchange = imvtdeexcl.TalosExtractor(args.api_stage)
+    args = vars(args)
     imvcdeexut.download_realtime_for_one_exchange_periodically(
-        args, imvtdeexcl.TalosExtractor
+        args, exchange
     )
 
 
