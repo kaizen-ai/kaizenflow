70166333a1dc431b4832850f00439d474fce4438
saggese <saggese@gmail.com>
2022-05-31 16:02:28 -0400

    Integrate

--- oms/test/test_broker.py
+++ oms/test/test_broker.py
@@ -129,11 +129,11 @@ class TestSimulatedBroker2(hunitest.TestCase):
 
 
 class TestDatabaseBroker1(omtodh.TestOmsDbHelper):
-    
+
     @classmethod
     def get_id(cls) -> int:
         return hash(cls.__name__) % 1000
-    
+
     def setUp(self) -> None:
         super().setUp()
         # Create OMS tables.
