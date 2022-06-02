990968c21259308a5c085bc426d0fddcb0f9ab95
dan <d.yachmenev@crypto-kaizen.com>
2022-06-02 11:19:26 +0000

    Cmtask2036: remove doge

--- dataflow/universe.py
+++ dataflow/universe.py
@@ -323,12 +323,8 @@ def get_universe(universe_str: str) -> List[Amid]:
     elif universe_version == "crypto_chassis_v2":
         version = "v2"
         ret = _get_crypto_chassis_universe(version, top_n)
-        # Remove after duplicates are dropped.
-        ret = [
-            full_symbol
-            for full_symbol in ret
-            if full_symbol not in ["binance::ADA_USDT"]
-        ]
+        # Remove DOGE data due to its bad quality.
+        ret.remove("coinbase::DOGE_USDT")
     else:
         raise ValueError(f"Invalid universe_str='{universe_str}'")
     return ret
