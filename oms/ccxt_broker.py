47ae53d1e7b43599aceda272574da8cac171ecdc
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-06-09 13:39:43 +0000

    CMTask1995: Lint and update exchange login

--- oms/ccxt_broker.py
+++ oms/ccxt_broker.py
@@ -7,28 +7,31 @@ import oms.ccxt_broker as occxbrok
 """
 
 import logging
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import ccxt
+import pandas as pd
+
+import helpers.hdbg as hdbg
 import helpers.hsecrets as hsecret
+import im_v2.common.universe.full_symbol as imvcufusy
+import im_v2.common.universe.universe as imvcounun
+import im_v2.common.universe.universe_utils as imvcuunut
 import oms.broker as ombroker
 import oms.order as omorder
-import pandas as pd
-import numpy as np
-import helpers.hdbg as hdbg
-import itertools
-import pytest
-
-import im_v2.common.universe.universe_utils as imvcuunut
-import im_v2.common.universe.universe as imvcounun
-import im_v2.common.universe.full_symbol as imvcufusy
 
 _LOG = logging.getLogger(__name__)
 
 
 class CcxtBroker(ombroker.Broker):
     def __init__(
-        self, exchange, universe_version: str, mode: str, portfolio_id: str, *args: Any, **kwargs: Any
+        self,
+        exchange: ccxt.Exchange,
+        universe_version: str,
+        mode: str,
+        portfolio_id: str,
+        *args: Any,
+        **kwargs: Any,
     ) -> None:
         super().__init__(*args, **kwargs)
         """
@@ -37,17 +40,17 @@ class CcxtBroker(ombroker.Broker):
         :param exchange: name of the exchange to initialize the broker for
         :param universe_version: version of the universe to use
         :param mode: supported values: "test", "prod", if "test", launches the broker
-         in sandbox environment (not supported for every exchange), 
+         in sandbox environment (not supported for every exchange),
          if "prod" launches with production API.
         """
         hdbg.dassert_in(mode, ["prod", "test"])
-        #self._exchange_id = exchange
-        self._exchange_id = exchange.id
-        #self._exchange = self._log_into_exchange()
         self._exchange = exchange
+        self._exchange_id = exchange.id
         self._assert_order_methods_presence()
         # Enable mapping back from asset ids when placing orders.
-        self._asset_id_to_symbol_mapping = self._build_asset_id_to_symbol_mapping(universe_version)
+        self._asset_id_to_symbol_mapping = self._build_asset_id_to_symbol_mapping(
+            universe_version
+        )
         # Will be used to determine timestamp since when to fetch orders.
         self.last_order_execution_ts: Optional[pd.Timestamp] = None
         # TODO(Juraj): not sure how to generalize this coinbasepro-specific parameter.
@@ -55,20 +58,6 @@ class CcxtBroker(ombroker.Broker):
         if mode == "test":
             self._exchange.set_sandbox_mode(True)
             _LOG.warning("Running in sandbox mode")
-
-    def _assert_order_methods_presence(self) -> None:
-        """
-        Assert that the requested exchange supports all methods
-        necessary to make placing/fetching orders possible.
-        """
-        methods = ['createOrder', 'fetchClosedOrders']
-        abort = False
-        for method in methods:
-            if not self._exchange.has[method]:
-                _LOG.error(f"Method {method} is unsupported for {self._exchange_id}.")
-                abort = True
-        if abort:
-            raise ValueError(f"The {self._exchange_id} exchange is not fully supported for placing orders.")
 
     def get_fills(self) -> List[ombroker.Fill]:
         """
@@ -79,6 +68,24 @@ class CcxtBroker(ombroker.Broker):
             _LOG.info("Inside get_fills")
             orders = exchange.fetch_orders(since=self.last_order_execution_ts)
         return fills
+
+    def _assert_order_methods_presence(self) -> None:
+        """
+        Assert that the requested exchange supports all methods necessary to
+        make placing/fetching orders possible.
+        """
+        methods = ["createOrder", "fetchClosedOrders"]
+        abort = False
+        for method in methods:
+            if not self._exchange.has[method]:
+                _LOG.error(
+                    f"Method {method} is unsupported for {self._exchange_id}."
+                )
+                abort = True
+        if abort:
+            raise ValueError(
+                f"The {self._exchange_id} exchange is not fully supported for placing orders."
+            )
 
     def _log_into_exchange(self) -> ccxt.Exchange:
         """
@@ -122,24 +129,36 @@ class CcxtBroker(ombroker.Broker):
                 # TODO(Juraj): maybe it is possible to somehow abstract this to a general behavior
                 # but most likely the method will need to be overriden per each exchange
                 # to accomodate endpoint specific behavior.
-                params={"portfolio_id": self._portfolio_id}
+                params={"portfolio_id": self._portfolio_id},
             )
             _LOG.info(order_resp)
 
-    def _build_asset_id_to_symbol_mapping(self, universe_version: str) -> Dict[int, str]:
+    def _build_asset_id_to_symbol_mapping(
+        self, universe_version: str
+    ) -> Dict[int, str]:
         """
         Build asset id to full symbol mapping.
         """
         # Get full symbol universe.
-        full_symbol_universe = imvcounun.get_vendor_universe("CCXT", version=universe_version, as_full_symbol=True)
+        full_symbol_universe = imvcounun.get_vendor_universe(
+            "CCXT", version=universe_version, as_full_symbol=True
+        )
         # Filter symbols of the exchange corresponding to this instance.
-        full_symbol_universe = list(filter(lambda s: s.startswith(self._exchange_id), full_symbol_universe))
+        full_symbol_universe = list(
+            filter(
+                lambda s: s.startswith(self._exchange_id), full_symbol_universe
+            )
+        )
         # Build mapping.
-        asset_id_to_full_symbol_mapping = imvcuunut.build_numerical_to_string_id_mapping(full_symbol_universe)
+        asset_id_to_full_symbol_mapping = (
+            imvcuunut.build_numerical_to_string_id_mapping(full_symbol_universe)
+        )
         # Change mapped values to be symbol only (more convevient when placing orders)
-        asset_id_to_symbol_mapping = {id_: imvcufusy.parse_full_symbol(fs)[1].replace("_", "/") for id_, fs in asset_id_to_full_symbol_mapping.items()}
-        return asset_id_to_symbol_mapping # type: ignore[no-any-return]
-
+        asset_id_to_symbol_mapping = {
+            id_: imvcufusy.parse_full_symbol(fs)[1].replace("_", "/")
+            for id_, fs in asset_id_to_full_symbol_mapping.items()
+        }
+        return asset_id_to_symbol_mapping
 
     async def _wait_for_accepted_orders(
         self,
--- oms/test/test_ccxt_broker.py
+++ oms/test/test_ccxt_broker.py
@@ -1,53 +1,46 @@
 import asyncio
-import logging
-from typing import List
-import helpers.hsecrets as hsecret
-import helpers.hdbg as hdbg
 
-import pandas as pd
 import pytest
 
 import helpers.hasyncio as hasynci
+import helpers.hdbg as hdbg
+import helpers.hsecrets as hsecret
 import helpers.hunit_test as hunitest
 import market_data as mdata
-import oms.broker as ombroker
-import oms.broker_example as obroexam
-import oms.oms_db as oomsdb
-import oms.order as omorder
-import oms.order_example as oordexam
-import oms.order_processor as oordproc
-import oms.test.oms_db_helper as omtodh
-
 import oms.ccxt_broker as occxbrok
+import ccxt
 import oms.order_example as oordexam
-import oms.coinbasepro_exchange as ocoiexch
 
 
 class TestCcxtBroker1(hunitest.TestCase):
-
-    async def run_coroutine1(
-        self, event_loop: asyncio.AbstractEventLoop
-    ) -> None:
+    async def run_coroutine1(self, event_loop: asyncio.AbstractEventLoop) -> None:
         """
         Submit orders to a CcxtBroker initialized for coinbase prime.
         """
         market_data, _ = mdata.get_ReplayedTimeMarketData_example3(None)
         strategy = "SAU1"
         # Instantiante a broker with specified exchange.
-        exchange =  self._log_into_coinbasepro_exchange()
+        exchange = self._log_into_coinbasepro_exchange()
         # TODO(Juraj) mock the API calls.
-        broker = occxbrok.CcxtBroker(exchange, "v5", "prod", "c27158ee-ac73-49bb-a1f3-ec022cac33c2", strategy_id=strategy, market_data=market_data)                                                             
+        broker = occxbrok.CcxtBroker(
+            exchange,
+            "v5",
+            "prod",
+            "c27158ee-ac73-49bb-a1f3-ec022cac33c2",
+            strategy_id=strategy,
+            market_data=market_data,
+        )
         # Submit an order.
         order = oordexam.get_order_example4()
         orders = [order]
         await broker.submit_orders(orders)
         # Check fills.
-        #fills = broker.get_fills()
-        #self.assertEqual(len(fills), 1)
-        #actual = str(fills[0])
-        #expected = r"""Fill: asset_id=101 fill_id=0 timestamp=2000-01-01 09:35:00-05:00 num_shares=100.0 price=1000.3449750508295
-        #"""
-        #self.assert_equal(actual, expected, fuzzy_match=True)
+        # fills = broker.get_fills()
+        # self.assertEqual(len(fills), 1)
+        # actual = str(fills[0])
+        # expected = r"""Fill: asset_id=101 fill_id=0 timestamp=2000-01-01 09:35:00-05:00 num_shares=100.0 price=1000.3449750508295
+        # """
+        # self.assert_equal(actual, expected, fuzzy_match=True)
 
     def test_submit_and_fill1(self) -> None:
         event_loop = None
@@ -56,28 +49,36 @@ class TestCcxtBroker1(hunitest.TestCase):
     @pytest.mark.skip(reason="Code in development.")
     def test_unsupported_exchange1(self) -> None:
         """
-        Test that ValueError is raised when CcxtBroker cannot be
-        instantiated with given exchange because of missing method 
-        implementation.
+        Test that ValueError is raised when CcxtBroker cannot be instantiated
+        with given exchange because of missing method implementation.
         """
         market_data, _ = mdata.get_ReplayedTimeMarketData_example3(None)
         strategy = "SAU1"
         with pytest.raises(ValueError) as fail:
-            broker = occxbrok.CcxtBroker("coinbase", "v3", "test", strategy_id=strategy, market_data=market_data) 
+            broker = occxbrok.CcxtBroker(
+                "coinbase",
+                "v3",
+                "test",
+                strategy_id=strategy,
+                market_data=market_data,
+            )
         actual = str(fail.value)
-        self.assertIn("The coinbase exchange is not fully supported for placing orders.", actual)
+        self.assertIn(
+            "The coinbase exchange is not fully supported for placing orders.",
+            actual,
+        )
 
-    def _log_into_coinbasepo_exchange(self) -> ocoiexch.coinbasepro:
+    def _log_into_coinbasepro_exchange(self) -> ccxt.Exchange:
         """
-        Log into coinbasepro and return the corresponding
-        `ccxt.Exchange` object.
+        Log into coinbasepro and return the corresponding `ccxt.Exchange`
+        object.
         """
         # Select credentials for provided exchange.
         credentials = hsecret.get_secret("coinbasepro")
         # Enable rate limit.
         credentials["rateLimit"] = True
         # Create a CCXT Exchange class object.
-        exchange = ocoiexch.coinbasepro(credentials)
+        exchange = ccxt.coinbasepro(credentials)
         hdbg.dassert(
             exchange.checkRequiredCredentials(),
             msg="Required credentials not passed",
