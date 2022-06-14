a7fc3d4bfc97d22738834ed47bb34300dc9b657b
max-rsrch <sergeychikovm@gmail.com>
2022-06-14 15:49:44 +0000

    Filter Tier-1

---
+++ research_amp/cc/notebooks/CMTask2080_Get_investors_from_Consensus_conference.py
@@ -0,0 +1,45 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Imports
+
+# %%
+import pandas as pd
+
+# %% [markdown]
+# # Load the data
+
+# %%
+data = pd.read_csv("/shared_data/consensus_conference.csv", index_col = "Unnamed: 0")
+data
+
+# %% [markdown]
+# # Tier 1 filter
+
+# %%
+# `Partner` in job, `Venture,Capital,VC` in company.
+tier1 = pd.concat([
+    data[data["job"].str.contains('Partner', case = True, na=False)],
+    data[data["company"].str.contains('venture|capital|VC', case = True, na=False)],
+])
+# Exclude LLP (lawyer firms).
+tier1 = tier1[~tier1["company"].str.contains('LLP', case = True, na=False)]
+tier1 = tier1.drop_duplicates()
+tier1
+
+# %%
+# `Phantombuster` format (name + company)
+tier1_phantombuster = tier1['name'].map(str) + ' ' + tier1['company'].map(str)
+tier1_phantombuster
