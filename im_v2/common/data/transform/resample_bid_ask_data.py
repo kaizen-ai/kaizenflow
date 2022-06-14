f43cfb71183838482a41f4ad486e7dd273dda4d9
Nina Lee <nina.tru94@gmail.com>
2022-06-14 14:53:08 +0000

    fix

--- im_v2/common/data/transform/resample_bid_ask_data.py
+++ im_v2/common/data/transform/resample_bid_ask_data.py
@@ -6,7 +6,7 @@ back.
 # Usage sample:
 > im_v2/common/data/transform/resample_bid_ask_data.py \
     --src_dir 's3://<ck-data>/bid_ask/crypto_chassis/ftx' \
-    --dst_dir 's3://<ck-data>/resampled_bid_ask/ftx/...' \
+    --dst_dir 's3://<ck-data>/resampled_bid_ask/ftx' \
 
 Import as:
 
@@ -28,6 +28,23 @@ import helpers.hs3 as hs3
 _LOG = logging.getLogger(__name__)
 
 
+def _resample_bid_ask_data(data: pd.DataFrame) -> pd.DataFrame:
+    resample_rule = "T"
+    df = cfinresa.resample(data, rule=resample_rule).agg(
+        {
+            "bid_size": "sum",
+            "ask_size": "sum",
+            "exchange_id": "last",
+        }
+    )
+    df_mean = df[["bid_size", "ask_size"]].groupby(pd.Grouper(
+        freq=resample_rule)
+    ).mean()
+    df.insert(0, "bid_price", df_mean["bid_size"])
+    df.insert(2, "ask_price", df_mean["ask_size"])
+    return df
+
+
 def _run(args: argparse.Namespace) -> None:
     pattern = "*"
     only_files = True
@@ -42,18 +59,22 @@ def _run(args: argparse.Namespace) -> None:
         aws_profile=aws_profile,
     )
     filesystem = hs3.get_s3fs(aws_profile)
+    columns = [
+        "bid_price",
+        "bid_size",
+        "ask_price",
+        "ask_size",
+        "currency_pair",
+        "exchange_id",
+    ]
     for file in files_to_read:
         file_path = os.path.join(args.dst_dir, file)
-        df = hparque.from_parquet(file_path, aws_profile=aws_profile)
-        df = cfinresa.resample(df, rule="T").agg(
-            {
-                "bid_price": "last",
-                "bid_size": "sum",
-                "ask_price": "last",
-                "ask_size": "last",
-                "exchange_id": "last",
-            }
+        df = hparque.from_parquet(
+            file_path,
+            columns=columns,
+            aws_profile=aws_profile
         )
+        df = _resample_bid_ask_data(df)
         pq.write_table(
             pa.Table.from_pandas(df),
             args.dst_dir + "/" + file,
