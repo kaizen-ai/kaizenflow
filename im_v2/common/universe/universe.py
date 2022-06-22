b866357b2b0c09aefdd932ef57d9697702a0557b
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-06-22 20:11:04 +0000

    CMTask2189: Separate a download mode

--- im_v2/common/universe/universe.py
+++ im_v2/common/universe/universe.py
@@ -17,7 +17,8 @@ import im_v2.common.universe.full_symbol as imvcufusy
 def _extract_universe_version(universe_file: str) -> Tuple[int, int]:
     """
     Extract version number from universe_vXX.json file. e.g.
-    'universe_v3.1.json' -> (3, 1) 'universe_v1.json' -> (1, 0)
+    'download_universe_v3.1.json' -> (3, 1), 'download_universe_v1.json' -> (1,
+    0)
 
     :param file_name: universe file to extract version part from
     :return: universe file version tuple in format (major, minor)
@@ -26,7 +27,7 @@ def _extract_universe_version(universe_file: str) -> Tuple[int, int]:
     m = re.search(r"v(\d+(\.\d+)?)$", basename)
     hdbg.dassert(
         m,
-        "Can't parse file '%s', correct format is e.g. 'universe_v03.json'.",
+        "Can't parse file '%s', correct format is e.g. 'download_universe_v03.json'.",
         basename,
     )
     # Groups return tuple.
@@ -36,28 +37,34 @@ def _extract_universe_version(universe_file: str) -> Tuple[int, int]:
     return major, minor
 
 
-def _get_universe_file_path(vendor: str, *, version: Optional[str] = None) -> str:
+def _get_universe_file_path(
+    vendor: str, mode: str, *, version: Optional[str] = None
+) -> str:
     """
     Get universe file path based on version.
 
     :param vendor: vendor to load data for (e.g., CCXT, Talos)
+    :param mode: download or trade universe
     :param version: universe release version (e.g. "v01"). If None it uses
       the latest version available
     :return: file path to the universe file corresponding to the specified version
     """
+    hdbg.dassert_in(mode, ["download", "trade"])
     vendor = vendor.lower()
     # Get path to vendor universe dir.
-    vendor_dir = os.path.join(hgit.get_amp_abs_path(), f"im_v2/{vendor}/universe")
+    vendor_dir = os.path.join(hgit.get_amp_abs_path(), f"im_v2/{vendor}/universe/{mode}")
     hdbg.dassert_dir_exists(vendor_dir)
     if version is None:
         # Find all universe files.
-        vendor_universe_pattern = os.path.join(vendor_dir, "universe_v*.json")
+        vendor_universe_pattern = os.path.join(
+            vendor_dir, f"universe_v*.json"
+        )
         universe_files = list(glob.glob(vendor_universe_pattern))
         hdbg.dassert_ne(len(universe_files), 0)
         file_path = max(universe_files, key=_extract_universe_version)
     else:
         # TODO(Juraj): #1487 Assert version format (include 'small').
-        file_name = "".join(["universe_", version, ".json"])
+        file_name = "".join([mode, "/universe_", version, ".json"])
         file_path = os.path.join(vendor_dir, file_name)
     hdbg.dassert_path_exists(file_path)
     return file_path
@@ -65,6 +72,7 @@ def _get_universe_file_path(vendor: str, *, version: Optional[str] = None) -> st
 
 def _get_trade_universe(
     vendor: str,
+    mode: str,
     *,
     version: Optional[str] = None,
 ) -> Dict[str, List[str]]:
@@ -72,6 +80,7 @@ def _get_trade_universe(
     Load trade universe for which we have historical data.
 
     :param vendor: vendor to load data for (e.g., CCXT/Talos)
+    :param mode: download or trade
     :param version: release version
     :return: trade universe as a nested dictionary of
       exchange name (e.g., binance) to list of symbols e.g.,
@@ -93,7 +102,7 @@ def _get_trade_universe(
         }
     """
     # TODO(Grisha): consider always converting a vendor to lowercase.
-    file_path = _get_universe_file_path(vendor, version=version)
+    file_path = _get_universe_file_path(vendor, mode, version=version)
     hdbg.dassert_path_exists(file_path)
     universe = hio.from_json(file_path)
     hdbg.dassert_in(vendor, universe, "Invalid vendor=`%s`", vendor)
@@ -102,13 +111,18 @@ def _get_trade_universe(
 
 
 def get_vendor_universe(
-    vendor: str, *, version: Optional[str] = None, as_full_symbol: bool = False
+    vendor: str,
+    mode: str,
+    *,
+    version: Optional[str] = None,
+    as_full_symbol: bool = False,
 ) -> Union[List[imvcufusy.FullSymbol], Dict[str, Dict[str, List[str]]]]:
     """
     Load vendor universe either as a list of currency pairs per each vendor or
     list of full symbols.
 
     :param vendor: vendor to load data for (e.g., CCXT, Talos)
+    :param mode: trade or download
     :param version: release version
     :param as_full_symbol: if True transform the universe into list of
         full symbols e.g. gateio::XRP_USDT
@@ -130,7 +144,7 @@ def get_vendor_universe(
         }
         or ["gateio::XRP_USDT", "kucoin::SOL_USDT"]
     """
-    vendor_universe = _get_trade_universe(vendor, version=version)
+    vendor_universe = _get_trade_universe(vendor, mode, version=version)
     if as_full_symbol:
         # Convert vendor universe dict to a sorted list of full symbols.
         vendor_universe = [
