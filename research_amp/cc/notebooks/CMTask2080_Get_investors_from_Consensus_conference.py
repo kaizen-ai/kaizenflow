1d20a29e27ff819a9d1ac22767e827a120baa29d
max-rsrch <sergeychikovm@gmail.com>
2022-06-22 12:50:22 +0000

    Filter new names

--- research_amp/cc/notebooks/CMTask2080_Get_investors_from_Consensus_conference.py
+++ research_amp/cc/notebooks/CMTask2080_Get_investors_from_Consensus_conference.py
@@ -22,29 +22,86 @@ import pandas as pd
 # # Load the data
 
 # %%
-data = pd.read_csv("/shared_data/consensus_conference.csv", index_col = "Unnamed: 0")
+data = pd.read_csv(
+    "/shared_data/consensus_conference.csv", index_col="Unnamed: 0"
+)
 data
 
 # %% [markdown]
 # # Tier 1 filter
 
-# %%
+# %% [markdown]
+# ## Either Partner or VC
+
+# %% run_control={"marked": false}
 # `Partner` in job.
-mask1 = data["job"].str.contains('Partner', case = False, na=False)
+mask1 = data["job"].str.contains("Partner", case=False, na=False)
 print("Selecting partner=", mask1.sum())
 # `Venture,Capital,VC` in company.
-mask2 = data["company"].str.contains('venture|capital|VC', case = False, na=False)
+mask2 = data["company"].str.contains("venture|capital|VC", case=False, na=False)
 print("Company count=", mask2.sum())
 # Exclude LLP (lawyer firms).
-mask3 = data["company"].str.contains('LLP', na=False)
+mask3 = data["company"].str.contains("LLP", na=False)
 print("Company count=", mask3.sum())
 
 # Collect all Tier-1 contacts.
-tier1 = data[(mask1|mask2) & ~mask3]
+tier1_ease = data[(mask1 | mask2) & ~mask3]
+tier1_ease = tier1_ease.drop_duplicates()
+tier1_ease
+
+# %% [markdown]
+# ## Partner and VC is obligatory
+
+# %%
+# `Partner` in job.
+mask1 = data["job"].str.contains("Partner", case=False, na=False)
+print("Selecting partner=", mask1.sum())
+# `Venture,Capital,VC` in company.
+mask2 = data["company"].str.contains("venture|capital|VC", case=False, na=False)
+print("Company count=", mask2.sum())
+
+# Collect all Tier-1 contacts.
+tier1 = data[(mask1 & mask2)]
 tier1 = tier1.drop_duplicates()
 tier1
 
+# %% [markdown]
+# ## Investment-related jobs
+
 # %%
-# `Phantombuster` format (name + company)
-tier1_phantombuster = tier1['name'].map(str) + ' ' + tier1['company'].map(str)
-tier1_phantombuster
+# `Investor` in job.
+mask1 = data["job"].str.contains("investor", case=False, na=False)
+print("Selecting Investor=", mask1.sum())
+# Exclude IR positions.
+mask2 = data["job"].str.contains("Relations", case=False, na=False)
+# More investor-related jobs.
+mask3 = data["job"].str.contains("investment", case=False, na=False)
+print("Selecting Investment=", mask3.sum())
+
+new_names = data[(mask1 | mask3) & ~mask2]
+# Exclude previously found Tier-1 names.
+new_names = new_names[~new_names["name"].isin(list(tier1["name"]))]
+# Drop those with missing `company` field.
+new_names = new_names[new_names["company"].notna()]
+# Drop duplicates.
+new_names = new_names.drop_duplicates(keep="last")
+new_names
+
+# %% [markdown]
+# ## Save results
+
+# %%
+new_names.to_csv("tier1.csv")
+
+
+# %% [markdown]
+# ## Phantombuster format converter
+
+# %%
+def conver_to_phantombuster(df):
+    ph_contacts = df["name"].map(str) + " " + df["company"].map(str)
+    return ph_contacts
+
+
+# %%
+conver_to_phantombuster(new_names)
