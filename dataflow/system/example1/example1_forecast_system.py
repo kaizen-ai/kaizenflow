919d8f90757faa1e17a8593fb9468962b3f2a122
cryptomtc <89211724+cryptomtc@users.noreply.github.com>
2022-06-29 15:16:41 -0700

    AmpTask1786_Integrate_20220629 (#2254)
    * Integrate
    
    * Integrate
    
    * Integrate
    
    * Integrate
    
    * Fix
    
    Co-authored-by: saggese <saggese@gmail.com>

--- dataflow/system/example1/example1_forecast_system.py
+++ dataflow/system/example1/example1_forecast_system.py
@@ -13,6 +13,7 @@ import dataflow.pipelines.example1.example1_pipeline as dtfpexexpi
 import dataflow.system.example1.example1_builders as dtfsexexbu
 import dataflow.system.real_time_dag_runner as dtfsrtdaru
 import dataflow.system.system as dtfsyssyst
+import dataflow.system.system_builder_utils as dtfssybuut
 import helpers.hdbg as hdbg
 import market_data as mdata
 import oms
@@ -36,15 +37,13 @@ class Example1_ForecastSystem(dtfsyssyst.ForecastSystem):
     """
 
     def get_dag_runner(self) -> dtfcore.DagRunner:
-        dag_runner = dtfcore.FitPredictDagRunner(self.config, self.dag)
+        dag_runner = dtfcore.FitPredictDagRunner(self.dag)
         return dag_runner
 
     def _get_system_config_template(self) -> cconfig.Config:
         _ = self
         dag_builder = dtfpexexpi.Example1_DagBuilder()
-        system_config = dtfsexexbu.get_system_config_template_example1(
-            dag_builder
-        )
+        system_config = dtfssybuut.get_system_config_template_instance1(dag_builder)
         return system_config
 
     def _get_market_data(self) -> mdata.ReplayedMarketData:
@@ -71,7 +70,7 @@ def get_Example1_ForecastSystem_example1(
     hdbg.dassert_in(trading_period_str, ("1T", "5T", "15T"))
     # TODO(gp): Use a setter.
     system.config[
-        "DAG", "resample", "transformer_kwargs", "rule"
+        "dag_config", "resample", "transformer_kwargs", "rule"
     ] = trading_period_str
     # TODO(gp): We pass dag_runner through the config since somebody
     #  outside needs this function to run.
@@ -107,13 +106,11 @@ class Example1_Time_ForecastSystem(dtfsyssyst.Time_ForecastSystem):
     def _get_system_config_template(self) -> cconfig.Config:
         _ = self
         dag_builder = dtfpexexpi.Example1_DagBuilder()
-        system_config = dtfsexexbu.get_system_config_template_example1(
-            dag_builder
-        )
+        system_config = dtfssybuut.get_system_config_template_instance1(dag_builder)
         return system_config
 
     def _get_market_data(self) -> mdata.ReplayedMarketData:
-        market_data = dtfsexexbu.get_Example1_market_data_example1(self)
+        market_data = dtfssybuut.get_event_loop_market_data_instance1(self)
         return market_data
 
     def _get_dag(self) -> dtfcore.DAG:
@@ -144,23 +141,22 @@ class Example1_Time_ForecastSystem_with_DataFramePortfolio(
     def _get_system_config_template(self) -> cconfig.Config:
         _ = self
         dag_builder = dtfpexexpi.Example1_DagBuilder()
-        system_config = dtfsexexbu.get_system_config_template_example1(
-            dag_builder
-        )
+        system_config = dtfssybuut.get_system_config_template_instance1(dag_builder)
         return system_config
 
     def _get_market_data(self) -> mdata.ReplayedMarketData:
-        market_data = dtfsexexbu.get_Example1_market_data_example1(self)
+        market_data = dtfssybuut.get_event_loop_market_data_instance1(self)
         return market_data
 
     def _get_dag(self) -> dtfcore.DAG:
         dag = dtfsexexbu.get_Example1_dag_example3(self)
         return dag
 
+    # TODO(gp): Extract this code in Example1_builders.py
     def _get_portfolio(self) -> oms.Portfolio:
         event_loop = self.config["event_loop"]
         market_data = self.market_data
-        asset_ids = self.config["market_data", "asset_ids"]
+        asset_ids = self.config["market_data_config", "asset_ids"]
         portfolio = oms.get_DataFramePortfolio_example1(
             event_loop,
             market_data=market_data,
@@ -180,11 +176,11 @@ class Example1_Time_ForecastSystem_with_DataFramePortfolio(
 
 
 # #############################################################################
-# Example1_Time_ForecastSystem_with_DatabasePortfolio
+# Example1_Time_ForecastSystem_with_DatabasePortfolio_and_OrderProcessor
 # #############################################################################
 
 
-class Example1_Time_ForecastSystem_with_DatabasePortfolio(
+class Example1_Time_ForecastSystem_with_DatabasePortfolio_and_OrderProcessor(
     dtfsyssyst.Time_ForecastSystem_with_DatabasePortfolio_and_OrderProcessor
 ):
     """
@@ -203,28 +199,27 @@ class Example1_Time_ForecastSystem_with_DatabasePortfolio(
     def _get_system_config_template(self) -> cconfig.Config:
         _ = self
         dag_builder = dtfpexexpi.Example1_DagBuilder()
-        system_config = dtfsexexbu.get_system_config_template_example1(
-            dag_builder
-        )
+        system_config = dtfssybuut.get_system_config_template_instance1(dag_builder)
         return system_config
 
     def _get_market_data(
         self,
     ):
-        market_data = dtfsexexbu.get_Example1_market_data_example1(self)
+        market_data = dtfssybuut.get_event_loop_market_data_instance1(self)
         return market_data
 
     def _get_dag(self) -> dtfcore.DAG:
         dag = dtfsexexbu.get_Example1_dag_example3(self)
         return dag
 
+    # TODO(gp): Extract this code in Example1_builders.py
     def _get_portfolio(
         self,
     ) -> oms.Portfolio:
         event_loop = self.config["event_loop"]
         market_data = self.market_data
         table_name = oms.CURRENT_POSITIONS_TABLE_NAME
-        asset_ids = self.config["market_data", "asset_ids"]
+        asset_ids = self.config["market_data_config", "asset_ids"]
         portfolio = oms.get_DatabasePortfolio_example1(
             event_loop,
             self._db_connection,
@@ -241,84 +236,3 @@ class Example1_Time_ForecastSystem_with_DatabasePortfolio(
             "price": "close",
         }
         return portfolio
-
-
-# #############################################################################
-# Example1_Time_ForecastSystem_with_Portfolio
-# #############################################################################
-
-# # TODO(gp): We should finish porting and re-activate this test.
-# class Example1_Time_ForecastSystem_with_Portfolio(
-#     dtfsyssyst.Time_ForecastSystem_with_Portfolio
-# ):
-#     """
-#
-#     - ReplayedTimeMarketData
-#     - Example1 DAG
-#     - Portfolio
-#     """
-#
-#     def _get_system_config_template(self) -> cconfig.Config:
-#         _ = self
-#         dag_builder = dtfpexexpi.Example1_DagBuilder()
-#         system_config = get_system_config_template_example1(dag_builder)
-#         return system_config
-#
-#     def _get_market_data(
-#         self,
-#     ):
-#         market_data = get_Example1_market_data_example1(self.config)
-#         return market_data
-#
-#     def _get_dag(
-#         self,
-#     ) -> dtfcore.DAG:
-#         portfolio = self.portfolio
-#         market_data = self.market_data
-#         # TODO(gp): It should be factor out.
-#         prediction_col = "feature1"
-#         volatility_col = "vwap.ret_0.vol"
-#         price_col = "vwap.ret_0"
-#         spread_col = None
-#         #
-#         market_data_history_lookback = pd.Timedelta("7D")
-#         asset_id_col = "asset_id"
-#         dag_builder = dtfpexexpi.Example1_DagBuilder()
-#         _LOG.debug("dag_builder=\n%s", dag_builder)
-#         config = dag_builder.get_config_template()
-#         _LOG.debug("config=\n%s", config)
-#         dag = dag_builder.get_dag(config)
-#         process_forecasts_dict = dtfsysinod.get_process_forecasts_dict_example1(
-#             portfolio,
-#             prediction_col,
-#             volatility_col,
-#             price_col,
-#             spread_col,
-#         )
-#         # Adapt DAG to real-time.
-#         dag = dtfsrtdaad.adapt_dag_to_real_time(
-#             dag,
-#             market_data,
-#             market_data_history_lookback,
-#             asset_id_col,
-#             process_forecasts_dict
-#         )
-#         return dag
-#
-#     def get_dag_runner(
-#         self,
-#         dag_builder: dtfcore.DagBuilder,
-#         config: cconfig.Config,
-#         get_wall_clock_time: hdateti.GetWallClockTime,
-#         *,
-#         sleep_interval_in_secs: int = 60 * 5,
-#         real_time_loop_time_out_in_secs: Optional[int] = None,
-#     ) -> dtfsys.RealTimeDagRunner:
-#         dag_runner = get_dag_runner(
-#             dag_builder,
-#             config,
-#             get_wall_clock_time,
-#             sleep_interval_in_secs=sleep_interval_in_secs,
-#             real_time_loop_time_out_in_secs=real_time_loop_time_out_in_secs,
-#         )
-#         return dag_runner
