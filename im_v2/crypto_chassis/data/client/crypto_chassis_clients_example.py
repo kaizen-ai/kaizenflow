217de2a1b5a1a929d08c8130e0eb3ae27fe95374
Nina Lee <nina.tru94@gmail.com>
2022-06-30 14:09:36 +0000

    fix

--- im_v2/crypto_chassis/data/client/crypto_chassis_clients_example.py
+++ im_v2/crypto_chassis/data/client/crypto_chassis_clients_example.py
@@ -111,31 +111,3 @@ def get_CryptoChassisHistoricalPqByTileClient_example3(
         )
     )
     return crypto_chassis_parquet_client
-
-
-def get_CryptoChassisHistoricalPqByTileClient_example4(
-    resample_1min: bool, contract_type: str, dataset: str, data_snapshot: str
-) -> imvccdcccc.CryptoChassisHistoricalPqByTileClient:
-    """
-    Get `CryptoChassisHistoricalPqByTileClient` object for unit test reading
-    actual futures historical data, which is stored on S3.
-
-    Client is initialized to process CryptoChassis data for:
-    - universe version: `v3`
-    """
-    universe_version = "v3"
-    aws_profile = "ck"
-    s3_bucket_path = hs3.get_s3_bucket_path(aws_profile)
-    root_dir = os.path.join(s3_bucket_path, "reorg", "historical.manual.pq")
-    partition_mode = "by_year_month"
-    crypto_chassis_client = imvccdcccc.CryptoChassisHistoricalPqByTileClient(
-        universe_version,
-        resample_1min,
-        root_dir,
-        partition_mode,
-        dataset,
-        contract_type,
-        data_snapshot=data_snapshot,
-        aws_profile=aws_profile,
-    )
-    return crypto_chassis_client
--- im_v2/crypto_chassis/data/client/test/test_crypto_chassis_clients.py
+++ im_v2/crypto_chassis/data/client/test/test_crypto_chassis_clients.py
@@ -1,86 +1,111 @@
+import os
+
 import pandas as pd
 import pytest
 
+import helpers.hs3 as hs3
 import helpers.hpandas as hpandas
 import helpers.hunit_test as hunitest
-import im_v2.crypto_chassis.data.client.crypto_chassis_clients_example as imvccdcccce
+import im_v2.crypto_chassis.data.client.crypto_chassis_clients as imvccdcccc
 
 
 class TestCryptoChassisHistoricalPqByTileClient1(hunitest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.resample_1min = True
+        self.aws_profile = "ck"
+        s3_bucket_path = hs3.get_s3_bucket_path(self.aws_profile)
+        self.root_dir = os.path.join(s3_bucket_path, "reorg", "historical.manual.pq")
+        self.partition_mode = "by_year_month"
+        self.dataset = "ohlcv"
+        self.filter_data_mode = "assert"
+
     def test1(self) -> None:
         """
-        `dataset = bid_ask`
         `contract_type = futures`
         """
-        resample1_min = True
+        universe_version = "v2"
         contract_type = "futures"
-        dataset = "bid_ask"
         data_snapshot = "20220620"
-        client = imvccdcccce.get_CryptoChassisHistoricalPqByTileClient_example4(
-            resample1_min, contract_type, dataset, data_snapshot
+        client = imvccdcccc.CryptoChassisHistoricalPqByTileClient(
+            universe_version,
+            self.resample_1min,
+            self.root_dir,
+            self.partition_mode,
+            self.dataset,
+            contract_type,
+            data_snapshot=data_snapshot,
+            aws_profile=self.aws_profile,
         )
         full_symbols = ["binance::BTC_USDT"]
-        start_ts = pd.Timestamp("2022-06-15 00:00:00+00:00")
+        start_ts = pd.Timestamp("2022-06-15 13:00:00+00:00")
         end_ts = pd.Timestamp("2022-06-15 16:00:00+00:00")
-        columns = None
-        filter_data_mode = "assert"
+        columns = ["full_symbol", "open", "high", "low"]
         df = client.read_data(
             full_symbols,
             start_ts,
             end_ts,
             columns,
-            filter_data_mode,
+            self.filter_data_mode,
         )
         df = hpandas.df_to_str(df)
-        expected_signature = r"""
-                                full_symbol             bid_price   bid_size     ask_price  ask_size
+        expected_signature = r"""                                
+                                        full_symbol     open     high      low
         timestamp
-        2022-06-15 00:00:00+00:00  binance::BTC_USDT  22112.989064    90.241  22108.061867   149.091
-        2022-06-15 00:01:00+00:00  binance::BTC_USDT  22063.260453    96.488  22075.549002   100.748
-        2022-06-15 00:02:00+00:00  binance::BTC_USDT  22034.594408    65.474  22028.264294   116.809
+        2022-06-15 13:00:00+00:00  binance::BTC_USDT  21183.7  21211.7  21156.0
+        2022-06-15 13:01:00+00:00  binance::BTC_USDT  21162.8  21178.9  21142.0
+        2022-06-15 13:02:00+00:00  binance::BTC_USDT  21158.0  21209.0  21158.0
         ...
-        2022-06-15 15:58:00+00:00  binance::BTC_USDT  21395.001921    95.367  21397.577884    69.930
-        2022-06-15 15:59:00+00:00  binance::BTC_USDT  21401.457408   110.456  21404.437116    79.228
-        2022-06-15 16:00:00+00:00  binance::BTC_USDT  21386.773491    73.926  21394.488583    57.439
+        2022-06-15 15:58:00+00:00  binance::BTC_USDT  21396.0  21408.8  21384.2
+        2022-06-15 15:59:00+00:00  binance::BTC_USDT  21402.0  21419.1  21393.0
+        2022-06-15 16:00:00+00:00  binance::BTC_USDT  21408.8  21429.0  21367.9
         """
         self.assert_equal(df, expected_signature, fuzzy_match=True)
 
-    @pytest.mark.slow("Slow via GH, fast on the server")
+    @pytest.mark.skipif(
+        not henv.execute_repo_config_code("is_CK_S3_available()"),
+        reason="Run only if CK S3 is available",
+    )
     def test2(self) -> None:
         """
-        `dataset = ohlcv`
         `contract_type = spot`
         """
-        resample1_min = True
+        universe_version = "v2"
         contract_type = "spot"
-        dataset = "ohlcv"
         data_snapshot = "20220530"
-        client = imvccdcccce.get_CryptoChassisHistoricalPqByTileClient_example4(
-            resample1_min, contract_type, dataset, data_snapshot
+        client = (
+            imvccdcccc.CryptoChassisHistoricalPqByTileClient(
+                universe_version,
+                self.resample_1min,
+                self.root_dir,
+                self.partition_mode,
+                self.dataset,
+                contract_type,
+                data_snapshot=data_snapshot,
+                aws_profile=self.aws_profile,
+            )
         )
         full_symbols = ["binance::BTC_USDT"]
-        start_ts = pd.Timestamp("2022-05-15 12:00:00+00:00")
+        start_ts = pd.Timestamp("2022-05-15 13:00:00+00:00")
         end_ts = pd.Timestamp("2022-05-15 16:00:00+00:00")
         columns = ["full_symbol", "open", "high", "low"]
-        filter_data_mode = "assert"
         df = client.read_data(
             full_symbols,
             start_ts,
             end_ts,
             columns,
-            filter_data_mode,
+            self.filter_data_mode,
         )
         df = hpandas.df_to_str(df)
         expected_signature = r"""
                                         full_symbol      open      high       low
         timestamp
-        2022-05-15 12:00:00+00:00  binance::BTC_USDT  30241.46  30293.32  30238.39
-        2022-05-15 12:01:00+00:00  binance::BTC_USDT  30293.31  30456.36  30291.60
-        2022-05-15 12:02:00+00:00  binance::BTC_USDT  30368.16  30393.57  30340.00
+        2022-05-15 13:00:00+00:00  binance::BTC_USDT  30309.99  30315.91  30280.00
+        2022-05-15 13:01:00+00:00  binance::BTC_USDT  30280.95  30299.57  30263.28
+        2022-05-15 13:02:00+00:00  binance::BTC_USDT  30263.31  30277.61  30234.97
         ...
         2022-05-15 15:58:00+00:00  binance::BTC_USDT  29960.20  29976.90  29941.20
         2022-05-15 15:59:00+00:00  binance::BTC_USDT  29976.90  30040.40  29968.33
         2022-05-15 16:00:00+00:00  binance::BTC_USDT  30013.46  30045.63  30007.45
         """
-
         self.assert_equal(df, expected_signature, fuzzy_match=True)
