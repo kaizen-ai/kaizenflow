6eb1be3fa04aa59de326d9d8858c12b239679190
Nina Lee <nina.tru94@gmail.com>
2022-06-30 17:22:52 +0000

    linter

--- im_v2/common/data/client/historical_pq_clients.py
+++ im_v2/common/data/client/historical_pq_clients.py
@@ -314,7 +314,12 @@ class HistoricalPqByCurrencyPairTileClient(HistoricalPqByTileClient):
             only_files = False
             use_relatives_paths = True
             dirs = hs3.listdir(
-                root_dir, pattern, only_files, use_relatives_paths, aws_profile=aws_profile)
+                root_dir,
+                pattern,
+                only_files,
+                use_relatives_paths,
+                aws_profile=aws_profile,
+            )
             dirs = [snapshot for snapshot in dirs if snapshot.isnumeric()]
             data_snapshot = max(dirs)
         self._data_snapshot = data_snapshot
