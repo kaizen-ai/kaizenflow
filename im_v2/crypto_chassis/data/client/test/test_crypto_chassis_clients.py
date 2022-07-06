342f8c555243df06a45d53b6603db1930172d273
Nina Lee <nina.tru94@gmail.com>
2022-07-06 13:16:34 +0000

    test

--- im_v2/crypto_chassis/data/client/test/test_crypto_chassis_clients.py
+++ im_v2/crypto_chassis/data/client/test/test_crypto_chassis_clients.py
@@ -57,7 +57,6 @@ class TestCryptoChassisHistoricalPqByTileClient1(icdctictc.ImClientTestCase):
             expected_column_unique_values=expected_column_unique_values,
         )
 
-    @pytest.mark.slow("Slow via GH, fast on the server")
     def test2(self) -> None:
         """
         `dataset = ohlcv`
@@ -72,22 +71,22 @@ class TestCryptoChassisHistoricalPqByTileClient1(icdctictc.ImClientTestCase):
         )
         full_symbols = ["binance::BTC_USDT", "binance::DOGE_USDT"]
         start_ts = pd.Timestamp("2022-05-15 13:00:00+00:00")
-        end_ts = pd.Timestamp("2022-05-15 16:00:00+00:00")
+        end_ts = pd.Timestamp("2022-05-15 13:15:00+00:00")
         expected_signature = r"""# df=
-        index=[2022-05-15 13:00:00+00:00, 2022-05-15 16:00:00+00:00]
+        index=[2022-05-15 13:00:00+00:00, 2022-05-15 13:15:00+00:00]
         columns=full_symbol,open,high,low,close,volume,vwap,number_of_trades,twap,knowledge_timestamp
-        shape=(362, 10)
+        shape=(32, 10)
                                           full_symbol        open        high         low       close        volume          vwap  number_of_trades          twap              knowledge_timestamp
         timestamp
         2022-05-15 13:00:00+00:00   binance::BTC_USDT  30309.9900  30315.9100  30280.0000  30280.9500      45.13335  30291.409100               607  30291.172400 2022-05-31 14:38:23.953697+00:00
         2022-05-15 13:00:00+00:00  binance::DOGE_USDT      0.0895      0.0895      0.0894      0.0894  606842.00000      0.089435                49      0.089469 2022-05-31 14:38:29.472387+00:00
         2022-05-15 13:01:00+00:00   binance::BTC_USDT  30280.9500  30299.5700  30263.2800  30263.3100      39.45772  30278.419700               577  30278.335000 2022-05-31 14:38:23.953697+00:00
         ...
-        2022-05-15 15:59:00+00:00  binance::DOGE_USDT      0.0888      0.0889      0.0886      0.0888  2.485897e+06      0.088763                56      0.088791 2022-05-31 14:38:29.472387+00:00
-        2022-05-15 16:00:00+00:00   binance::BTC_USDT  30013.4600  30045.6300  30007.4500  30015.1900  4.861676e+01  30024.202100               977  30024.244600 2022-05-31 14:38:23.953697+00:00
-        2022-05-15 16:00:00+00:00  binance::DOGE_USDT      0.0888      0.0889      0.0886      0.0887  4.286810e+05      0.088748                40      0.088750 2022-05-31 14:38:29.472387+00:00
+        2022-05-15 13:14:00+00:00  binance::DOGE_USDT      0.0895      0.0897      0.0894      0.0896  233260.00000      0.089547                30      0.089507 2022-05-31 14:38:29.472387+00:00
+        2022-05-15 13:15:00+00:00   binance::BTC_USDT  30363.5000  30400.0000  30324.2300  30387.2600      62.71008  30370.925000              1364  30374.435700 2022-05-31 14:38:23.953697+00:00
+        2022-05-15 13:15:00+00:00  binance::DOGE_USDT      0.0897      0.0898      0.0895      0.0898  460666.00000      0.089629                23      0.089626 2022-05-31 14:38:29.472387+00:00
         """
-        expected_length = 362
+        expected_length = 32
         expected_column_unique_values = {
             "full_symbol": ["binance::BTC_USDT", "binance::DOGE_USDT"]
         }
