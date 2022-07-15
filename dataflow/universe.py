4f60aa7757d83813a43ff90933d905ddf3bb6f53
max-rsrch <sergeychikovm@gmail.com>
2022-07-15 12:22:20 +0000

    Add comment

--- dataflow/universe.py
+++ dataflow/universe.py
@@ -314,6 +314,7 @@ def get_universe(universe_str: str) -> List[Amid]:
     elif universe_version == "ccxt_v7":
         version = "v7" 
         ret = _get_ccxt_universe(version, top_n)
+        # See CMTask2375 - Current exclusion of `binance::ADA_USDT` from universe `v7`.
         ret.remove("binance::ADA_USDT")
     elif universe_version == "crypto_chassis_v1":
         version = "v1"
