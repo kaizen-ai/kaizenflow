00ccde568b5be4c04429198cba6ea8b02a2e6a66
PomazkinG <pomazking@gmail.com>
2022-08-01 15:59:43 +0000

    checkpoint

--- dataflow/system/sink_nodes.py
+++ dataflow/system/sink_nodes.py
@@ -55,8 +55,8 @@ class ProcessForecasts(dtfcore.FitPredictNode):
         self._volatility_col = volatility_col
         self._spread_col = spread_col
         self._portfolio = portfolio
-        if isinstance(process_forecasts_config, dict):
-            process_forecasts_config = cconfig.get_config_from_nested_dict(process_forecasts_config)
+        # if isinstance(process_forecasts_config, dict):
+        #     process_forecasts_config = cconfig.get_config_from_nested_dict(process_forecasts_config)
         print("process_forecasts_config", process_forecasts_config)
         self._process_forecasts_config = process_forecasts_config
         self._forecast_evaluator_from_prices_dict = (
