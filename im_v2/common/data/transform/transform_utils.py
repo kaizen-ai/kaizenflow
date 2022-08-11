abf6839eb5d59dbc7ff3aa5a57590eaf1d5ddb51
tamriq <tamriqua@gmail.com>
2022-08-11 16:15:00 +0000

    CmTask2556: fix testd

--- im_v2/common/data/transform/transform_utils.py
+++ im_v2/common/data/transform/transform_utils.py
@@ -29,8 +29,8 @@ def convert_timestamp_column(
     :param unit: the unit of unix epoch
     :return: series containing datetime as `pd.Timestamp`
     """
-    if pd.to_numeric(datetime_col_name, errors="coerce").notnull().all():
-        # Check whether the column is numeric.
+    if pd.to_numeric(datetime_col_name, errors="coerce").notnull().all() and not pd.api.types.is_float_dtype(datetime_col_name):
+        # Check whether the column is numeric but not float typed.
         # Convert unix epoch into timestamp.
         kwargs = {"unit": unit}
         converted_datetime_col = datetime_col_name.apply(
