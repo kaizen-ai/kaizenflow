13672ae679a66bce6a406561761eaf141068afaa
Nina Lee <nina.tru94@gmail.com>
2022-08-16 06:25:08 +0000

    fixed calls

--- dataflow_amp/pipelines/mock1/test/run_Test_Mock1_TiledBacktest.sh
+++ dataflow_amp/pipelines/mock1/test/run_Test_Mock1_TiledBacktest.sh
@@ -9,6 +9,6 @@ if [[ 1 == 0 ]]; then
 # --dry_run
 fi;
 
-#/app/amp/dataflow/backtest/run_config_stub.py --experiment_builder 'amp.dataflow.backtest.master_backtest.run_tiled_backtest' --config_builder 'dataflow.pipelines.examples.example1_configs.build_tile_configs("kibot_v1-top1.5T.2020-01-01_2020-03-01")' --config_idx 0 --dst_dir /app/amp/dataflow/pipelines/examples/test/outcomes/Test_Example1_ForecastSystem.test1/tmp.scratch/run_model -v DEBUG
+#/app/amp/dataflow/backtest/run_config_stub.py --experiment_builder 'amp.dataflow.backtest.master_backtest.run_tiled_backtest' --config_builder 'dataflow_amp.system.mock1.mock1_tile_config_builders.build_Mock1_tile_config_list("kibot_v1-top1.5T.2020-01-01_2020-03-01")' --config_idx 0 --dst_dir /app/amp/dataflow/pipelines/examples/test/outcomes/Test_Example1_ForecastSystem.test1/tmp.scratch/run_model -v DEBUG
 
-#/app/dataflow/backtest/run_config_stub.py --experiment_builder 'dataflow.backtest.master_backtest.run_tiled_backtest' --config_builder 'dataflow.pipelines.examples.example1_configs.build_tile_configs("example1_v1-top2.1T.Jan2000")' --config_idx 0 --dst_dir /app/dataflow/pipelines/examples/test/outcomes/Test_Example1_ForecastSystem.test1/tmp.scratch/run_model -v INFO 2>&1 | tee log.txt
+#/app/dataflow/backtest/run_config_stub.py --experiment_builder 'dataflow.backtest.master_backtest.run_tiled_backtest' --config_builder 'dataflow_amp.system.mock1.mock1_tile_config_builders.build_Mock1_tile_config_list("example1_v1-top2.1T.Jan2000")' --config_idx 0 --dst_dir /app/dataflow/pipelines/examples/test/outcomes/Test_Example1_ForecastSystem.test1/tmp.scratch/run_model -v INFO 2>&1 | tee log.txt
