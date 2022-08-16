50a7b416d4c5e2167594fbd99ae17196f1ea5797
Nina Lee <nina.tru94@gmail.com>
2022-08-16 10:07:19 +0000

    fix'

--- dataflow_amp/pipelines/__init__.py
+++ dataflow_amp/pipelines/__init__.py
@@ -4,4 +4,4 @@ Import as:
 import dataflow_amp.pipelines as dtfapi
 """
 
-from dataflow_amp.pipelines.mock1 import *  # pylint: disable=unused-import # NOQA
+from dataflow_amp.pipelines.mock1.mock1_pipeline import *  # pylint: disable=unused-import # NOQA
