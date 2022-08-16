1d0443d7ed959873d95dfb7440fd1b45a1990d25
Nina Lee <nina.tru94@gmail.com>
2022-08-16 13:07:11 +0000

    fixes due to Max's changes in amp/core/config/config_list_builder.py:71

--- dataflow_amp/pipelines/mock1/test/run_Test_Mock1_TiledBacktest.sh
+++ dataflow_amp/pipelines/mock1/test/run_Test_Mock1_TiledBacktest.sh
@@ -3,7 +3,7 @@
 if [[ 1 == 0 ]]; then
 /app/dataflow/backtest/run_config_list.py \
     --experiment_builder dataflow.backtest.master_backtest.run_tiled_backtest \
-    --config_builder 'dataflow_amp.system.mock1.mock1_tile_config_builders.build_Mock1_tile_config_list("example1_v1-top2.1T.Jan2000")' \
+    --config_builder 'dataflow_amp.system.mock1.mock1_tile_config_builders.build_Mock1_tile_config_list("example1_v1-top2.1T.2022-01-01_2022-02-01")' \
     --dst_dir /app/amp/dataflow_amp/system/mock1/test/outcomes/Test_Mock1_ForecastSystem_TiledBacktest.test1/tmp.scratch/run_model \
     -v DEBUG --clean_dst_dir --no_confirm --num_threads serial
 # --dry_run
