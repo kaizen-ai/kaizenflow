7e1521def17bd9683482d4281d6783f2e43252aa
Nina Lee <68428519+rheenina@users.noreply.github.com>
2022-08-17 21:57:04 +0800

    Move `_build_C1b_tile_config_list` (#2640)
    * Move _build_C1b_tile_config_list to amp/dataflow/system/system_config_list.py
    
    * nits
    
    * removed asserts

--- dataflow_amp/system/mock1/mock1_tile_config_builders.py
+++ dataflow_amp/system/mock1/mock1_tile_config_builders.py
@@ -6,10 +6,8 @@ import dataflow_amp.system.mock1.mock1_tile_config_builders as dtfasmmtcbu
 
 import logging
 
-import core.config.config_list_builder as cccolibu
 import dataflow.system as dtfsys
 import dataflow_amp.system.mock1.mock1_forecast_system as dtfasmmfosy
-import helpers.hdbg as hdbg
 
 _LOG = logging.getLogger(__name__)
 
@@ -20,13 +18,5 @@ def build_Mock1_tile_config_list(
     system = dtfasmmfosy.get_Mock1_ForecastSystem_for_simulation_example1(
         backtest_config
     )
-    hdbg.dassert_isinstance(system, dtfsys.System)
-    system_config_list = dtfsys.SystemConfigList.from_system(system)
-    #
-    system_config_list = (
-        cccolibu.build_config_list_with_tiled_universe_and_periods(
-            system_config_list
-        )
-    )
-    hdbg.dassert_isinstance(system_config_list, dtfsys.SystemConfigList)
+    system_config_list = dtfsys.build_tile_config_list(system)
     return system_config_list
