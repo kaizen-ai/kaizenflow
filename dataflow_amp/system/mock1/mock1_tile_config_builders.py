a39ef68204e562b253af0b5f589b77e6b921f7d8
Nina Lee <nina.tru94@gmail.com>
2022-08-17 13:36:59 +0000

    removed asserts

--- dataflow_amp/system/mock1/mock1_tile_config_builders.py
+++ dataflow_amp/system/mock1/mock1_tile_config_builders.py
@@ -8,7 +8,6 @@ import logging
 
 import dataflow.system as dtfsys
 import dataflow_amp.system.mock1.mock1_forecast_system as dtfasmmfosy
-import helpers.hdbg as hdbg
 
 _LOG = logging.getLogger(__name__)
 
@@ -19,8 +18,5 @@ def build_Mock1_tile_config_list(
     system = dtfasmmfosy.get_Mock1_ForecastSystem_for_simulation_example1(
         backtest_config
     )
-    hdbg.dassert_isinstance(system, dtfsys.System)
-    #
     system_config_list = dtfsys.build_tile_config_list(system)
-    hdbg.dassert_isinstance(system_config_list, dtfsys.SystemConfigList)
     return system_config_list
