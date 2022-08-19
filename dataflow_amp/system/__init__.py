f24e7ba13172d7ebc47f59b6b9c22158f60e0318
Nina Lee <68428519+rheenina@users.noreply.github.com>
2022-08-19 22:36:51 +0800

    Update __init__.py

--- dataflow_amp/system/__init__.py
+++ dataflow_amp/system/__init__.py
@@ -5,3 +5,4 @@ import dataflow_amp.system as dtfamsys
 """
 
 import dataflow_amp.system.Cx # pylint: disable=unused-import # NOQA
+import dataflow_amp.system.mock1 # pylint: disable=unused-import # NOQA
