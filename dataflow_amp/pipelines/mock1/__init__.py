09d4347a0a112cf3926c85bdcd908336617aaaf9
Nina Lee <68428519+rheenina@users.noreply.github.com>
2022-08-19 23:47:31 +0800

    Update __init__.py

--- dataflow_amp/pipelines/mock1/__init__.py
+++ dataflow_amp/pipelines/mock1/__init__.py
@@ -4,4 +4,4 @@ Import as:
 import dataflow_amp.pipelines.mock1 as dtfapmo
 """
 
-from dataflow_amp.pipelines.mock1.mock1_pipeline.py import * # pylint: disable=unused-import # NOQA
+from dataflow_amp.pipelines.mock1.mock1_pipeline import * # pylint: disable=unused-import # NOQA
