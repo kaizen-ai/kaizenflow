b24325fed5e75bb4758c5b8cfed5876ab7192600
Grigorii Pomazkin <64330743+PomazkinG@users.noreply.github.com>
2022-08-24 00:04:38 +0400

    CmTask2679: module_has_no_attribute_error_in_C1b_prod_system_import (#2681)
    * checkpoint
    
    * lint init files
    
    * checkpoint

--- dataflow_amp/pipelines/__init__.py
+++ dataflow_amp/pipelines/__init__.py
@@ -4,4 +4,4 @@ Import as:
 import dataflow_amp.pipelines as dtfapi
 """
 
-import dataflow_amp.pipelines.mock1 # pylint: disable=unused-import # NOQA
+import dataflow_amp.pipelines.mock1  # pylint: disable=unused-import # NOQA
