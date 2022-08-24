a72c544cdc9e46b6fd68c5281d5ba097385cc2f9
Nina Lee <nina.tru94@gmail.com>
2022-08-24 11:16:32 +0000

    script

---
+++ dev_scripts/cleanup_scripts/CMTask_2669_Rename_real_time_loop_time_out_in_secs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/ -xe
+
+  script_name="dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_replayed_delay.sh"
+
+  replace_text.py \
+    --old "real_time_loop_time_out_in_secs: int" \
+    --new "rt_time_out_in_secs_or_timestamp: Union[int, pd.Timestamp]" \
+    --exclude_files $script_name \
+
+  replace_text.py \
+    --old "real_time_loop_time_out_in_secs" \
+    --new "rt_time_out_in_secs_or_timestamp" \
+    --exclude_files $script_name \
