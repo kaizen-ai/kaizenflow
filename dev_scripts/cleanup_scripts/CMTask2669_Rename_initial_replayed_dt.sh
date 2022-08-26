e1802e3013fb2aa22d654f1d38d1bd45e6c33348
Nina Lee <nina.tru94@gmail.com>
2022-08-26 09:59:22 +0000

    script

--- dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_replayed_dt.sh
+++ dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_replayed_dt.sh
@@ -3,6 +3,6 @@
   script_name="dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_replayed_dt.sh"
 
   replace_text.py \
-    --old "initial_replayed_dt" \
-    --new "initial_timestamp" \
+    --old "initial_timestamp" \
+    --new "initial_replayed_timestamp" \
     --exclude_files $script_name \
