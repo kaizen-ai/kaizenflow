0e7d74420c4f7cfe08adf9b0470481eeed812387
Nina Lee <68428519+rheenina@users.noreply.github.com>
2022-08-26 11:36:44 +0800

    Update CMTask2669_Rename_initial_replayed_dt.sh

--- dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_replayed_dt.sh
+++ dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_replayed_dt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-  script_name="dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_timestamp.sh"
+  script_name="dev_scripts/cleanup_scripts/CMTask2669_Rename_initial_replayed_dt.sh"
 
   replace_text.py \
     --old "initial_replayed_dt" \
