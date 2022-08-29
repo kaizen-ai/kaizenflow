772ded6a4aa99a6e18c941b43fb3e80381cfb651
Nina Lee <nina.tru94@gmail.com>
2022-08-29 08:38:55 +0000

    script

--- dev_scripts/cleanup_scripts/CMTask_2669_Rename_real_time_loop_time_out_in_secs.sh
+++ dev_scripts/cleanup_scripts/CMTask_2669_Rename_real_time_loop_time_out_in_secs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -xe
 
-script_name="dev_scripts/cleanup_scripts/CMTask_2669_Rename_real_time_loop_time_out_in_secs.sh"
+script_name="dev_scripts/cleanup_scripts/CMTask_2669_Rename_rt_timeout_in_secs_or_time.sh"
 
 replace_text.py \
-  --old "real_time_loop_time_out_in_secs: Optional[int]" \
+  --old "rt_timeout_in_secs_or_time: Optional[int]" \
   --new "rt_timeout_in_secs_or_time: Optional[Union[int, datetime.time]]" \
   --exclude_files $script_name 
 
 replace_text.py \
-  --old "real_time_loop_time_out_in_secs" \
+  --old "rt_timeout_in_secs_or_time" \
   --new "rt_timeout_in_secs_or_time" \
   --exclude_files $script_name 
