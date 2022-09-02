9c06852c1eb38247e5e89e6a361c09633bf53504
saggese <saggese@gmail.com>
2022-09-02 17:44:38 -0400

    Remove white spaces

--- im_v2/crypto_chassis/data/extract/test/test_chassis_download_historical_data.py
+++ im_v2/crypto_chassis/data/extract/test/test_chassis_download_historical_data.py
@@ -85,4 +85,3 @@ class TestDownloadHistoricalData1(hunitest.TestCase):
         actual_args = tuple(chassis_extractor_mock.call_args)
         expected_args = (("spot",), {})
         self.assertEqual(actual_args, expected_args)
-        
