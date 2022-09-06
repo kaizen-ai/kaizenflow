45a1b74734fb0cd975765d478a8b829657092d14
Nina Lee <nina.tru94@gmail.com>
2022-09-06 13:30:13 +0000

    dev_scripts/cleanup_scripts/CMTask2739_Introduce_NonTime_ForecastSystem.sh

--- dev_scripts/cleanup_scripts/CMTask2739_Introduce_NonTime_ForecastSystem.sh
+++ dev_scripts/cleanup_scripts/CMTask2739_Introduce_NonTime_ForecastSystem.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -xe
 
-dir_names="dev_scripts/cleanup_scripts dev_scripts/cleanup_scripts dataflow/system dataflow_amp/system/mock1"
+dir_names="dev_scripts/cleanup_scripts dataflow/system dataflow_amp/system/mock1 dataflow/system/test dataflow_amp/system/mock1/test"
+
+replace_text.py \
+  --old "ForecastSystem" \
+  --new "NonTime_ForecastSystem" \
+  --exclude_dirs "$dir_names" 
 
 replace_text.py \
   --old "ForecastSystem" \
