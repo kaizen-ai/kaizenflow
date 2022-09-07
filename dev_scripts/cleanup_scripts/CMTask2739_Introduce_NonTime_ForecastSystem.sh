f6450295e3e67dbc795729f13a65f657e2030f55
Nina Lee <68428519+rheenina@users.noreply.github.com>
2022-09-07 22:16:28 +0500

    Update CMTask2739_Introduce_NonTime_ForecastSystem.sh

--- dev_scripts/cleanup_scripts/CMTask2739_Introduce_NonTime_ForecastSystem.sh
+++ dev_scripts/cleanup_scripts/CMTask2739_Introduce_NonTime_ForecastSystem.sh
@@ -3,6 +3,6 @@
 dir_names="amp/dev_scripts/cleanup_scripts"
 
 replace_text.py \
-  --old "(?=(?!Time_)ForecastSystem)" \
+  --old "(?<!Time_)ForecastSystem" \
   --new "NonTime_ForecastSystem" \
   --exclude_dirs "$dir_names" 
