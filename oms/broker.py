4821bbd00db1f6e405a57e8360144869ffa76ec0
Nina Lee <nina.tru94@gmail.com>
2022-09-21 11:38:55 +0000

    oms

--- oms/broker.py
+++ oms/broker.py
@@ -111,13 +111,12 @@ class Broker(abc.ABC, hobject.PrintableMixin):
     """
     Represent a broker to which we can place orders and receive fills back.
 
-    The broker:
-    1) keeps an internal book keeping of orders submitted and deadlines when they
-       are supposed to be executed
-    2) passes the orders to the actual Order Management System (OMS) through an
-       interface (e.g., DB, file system)
-    3) waits for an acknowledgement of orders being submitted successfully by the OMS
-    4) reports the order fills from the market
+    The broker: 1) keeps an internal book keeping of orders submitted
+    and deadlines when they    are supposed to be executed 2) passes the
+    orders to the actual Order Management System (OMS) through an
+    interface (e.g., DB, file system) 3) waits for an acknowledgement of
+    orders being submitted successfully by the OMS 4) reports the order
+    fills from the market
     """
 
     _submitted_order_id: int = 0
@@ -188,11 +187,12 @@ class Broker(abc.ABC, hobject.PrintableMixin):
         # Load last low price from market data.
         col_name = "low"
         low_price = self.market_data.get_last_price(col_name, [asset_id])
-        low_price = low_price.loc[asset_id]
+        if len(low_price) == 0:
+            # Return empty value if there is no price.
+            low_price = np.nan
+            # Select topmost price if there are multiple entries.
+            _LOG.warning("Length of price series is >1: %s", low_price)
         if isinstance(low_price, pd.Series):
-            if len(low_price) == 0:
-                # Return empty value if there is no price.
-                low_price = np.nan
             # Select topmost price if there are multiple entries.
             _LOG.warning("Length of price series is >1: %s", low_price)
             low_price = low_price.iloc[0]
@@ -395,10 +395,9 @@ class Broker(abc.ABC, hobject.PrintableMixin):
 
 class SimulatedBroker(Broker):
     """
-    Represent a broker to which we place orders and receive back fills:
-    1) completely
-    2) as soon as their deadline comes
-    3) at the price from the Market
+    Represent a broker to which we place orders and receive back fills: 1)
+    completely 2) as soon as their deadline comes 3) at the price from the
+    Market.
 
     There is no interaction with an OMS (e.g., no need to waiting for
     acceptance and execution).
