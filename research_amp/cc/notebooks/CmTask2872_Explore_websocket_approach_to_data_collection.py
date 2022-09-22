0ba643c8031d8401dc0dd3851150ac2fb6e8c38a
jsmerix <smeriga.juraj@gmail.com>
2022-09-22 20:32:38 +0000

    Add unicorn-binance-websocket library example

--- research_amp/cc/notebooks/CmTask2872_Explore_websocket_approach_to_data_collection.py
+++ research_amp/cc/notebooks/CmTask2872_Explore_websocket_approach_to_data_collection.py
@@ -13,10 +13,10 @@
 # ---
 
 # %% [markdown]
-# ## Setup
+# # Python-Binance lib approach
 
 # %% [markdown]
-# We need to install python binance library, since it is not currently a part of dev image
+# As a first approach we will try python-binance lib. We need to install it, since it is not currently a part of dev image
 
 # %%
 import sys
@@ -81,5 +81,77 @@ await start_order_book_socket()
 
 # %%
 msgs[0]
+
+# %% [markdown]
+# # Unicorn-binance-websocket lib approach
+
+# %%
+import sys
+
+# !sudo {sys.executable} -m pip install unicorn-binance-websocket-api
+
+# %%
+import unicorn_binance_websocket_api.manager as bnwam
+import im_v2.common.universe.universe as imvcounun
+from datetime import datetime, timedelta
+import time
+import threading
+import json
+
+# %%
+universe = imvcounun.get_vendor_universe("CCXT", "download", version="v7")
+universe = universe['binance']
+# convert to websocket format
+universe = list(map(lambda x: x.replace("_", "").lower(), universe))
+
+# %%
+bn_websocket_manager = bnwam.BinanceWebSocketApiManager(exchange="binance.com-futures")
+
+# %% [markdown]
+# With regards to airflow we can decide if we want to implement our own timecounter
+# or simply rely on the fact that a single websocket connection lasts 24h.
+# Having our counter will probably make it easier to sync individual downloaders scheduled one after another
+
+# %%
+# Setup callback function
+data = []
+def process_stream_data(websocket_manager: bnwam.BinanceWebSocketApiManager) -> None:
+    end_time = datetime.now() + timedelta(seconds=10)
+    while datetime.now() < end_time:
+        if websocket_manager.is_manager_stopping():
+            break
+        oldest_stream_data_from_stream_buffer = websocket_manager.pop_stream_data_from_stream_buffer()
+        if oldest_stream_data_from_stream_buffer is False:
+            time.sleep(0.01)
+        else:
+            data.append(oldest_stream_data_from_stream_buffer)
+    websocket_manager.stop_manager_with_all_streams()
+
+
+# %% [markdown]
+# ## Single symbol demo
+
+# %%
+markets = universe_single = universe[0:1]
+
+# %%
+freq = 500 # miliseconds
+depth = 5
+channels = [f"depth{depth}@{freq}ms"]
+# There is a limit of maximum 1024 subscriptions calculated as no. of streams * no. of markets
+# output="dict" has to be set, otherwise we get raw data as string
+bn_websocket_manager.create_stream(channels, markets, output="dict")
+
+# %%
+worker_thread = threading.Thread(target=process_stream_data, args=(bn_websocket_manager,))
+worker_thread.start()
+# wait until the worker finishes
+time.sleep(10)
+
+# %% [markdown]
+# ## Received message example
+
+# %% run_control={"marked": true}
+data[0]
 
 # %%
