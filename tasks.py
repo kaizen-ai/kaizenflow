d179c66c71dfdec71381ac5522dee89b2b2b153e
dan <d.yachmenev@crypto-kaizen.com>
2022-10-01 19:35:52 +0000

    checkpoint

--- tasks.py
+++ tasks.py
@@ -99,7 +99,6 @@ from helpers.lib_tasks import (  # isort: skip # noqa: F401  # pylint: disable=u
     traceback,
 )
 
-
 # # TODO(gp): This is due to the coupling between code in linter container and
 # #  the code being linted.
 # try:
