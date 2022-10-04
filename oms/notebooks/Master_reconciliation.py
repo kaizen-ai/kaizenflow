629c6b3f360861f1214bf0d7159cd38adcb90f38
PomazkinG <pomazking@gmail.com>
2022-10-04 16:28:09 +0000

    checkpoint

--- oms/notebooks/Master_reconciliation.py
+++ oms/notebooks/Master_reconciliation.py
@@ -44,32 +44,112 @@ _LOG.info("%s", henv.get_system_signature()[0])
 
 hprint.config_notebook()
 
+
+# %%
+def get_reconciliation_config(date_str: str, asset_class: str) -> cconfig.Config:
+    """
+    Get a reconciliation that is spefic of an asset class.
+    
+    :param date_str: reconciliation date as str, e.g., `20221003`
+    :param asset_class: either `equities` or `crypto`
+    """
+    # Set values for variables that are specific of an asset class.
+    if asset_class == "crypto":
+        # For crypto the TCA part is not implemented yet.
+        run_tca = False
+        #
+        bar_duration = "5"
+        #
+        root_dir = "/shared_data/prod_reconciliation"
+        prod_dir = os.path.join(root_dir, date_str, "prod")
+        data_dict = {
+           "prod_dir": prod_dir,
+           # For crypto we do not have a `candidate` so we just re-use prod.
+           "cand_dir": prod_dir,
+           "sim_dir": os.path.join(root_dir, date_str, "simulation", "system_log_dir"),
+        }
+        #
+        fep_init_dict = {
+            "price_col": "vwap",
+            "prediction_col": "vwap.ret_0.vol_adj_2_hat",
+            "volatility_col": "vwap.ret_0.vol",
+        }
+        quantization = "no_quantization"
+        gmv = 700.0
+    elif asset_class == "equities":
+        run_tca = True
+        #
+        bar_duration = "15T"
+        #
+        root_dir = ""
+        search_str = ""
+        prod_dir_cmd = f"find {root_dir}/{date_str}/prod -name '{search_str}'"
+        _, prod_dir = hsystem.system_to_string(prod_dir_cmd)
+        cand_cmd = f"find {root_dir}/{date_str}/job.candidate.* -name '{search_str}'"
+        _, cand_dir = hsystem.system_to_string(cand_cmd)
+        data_dict = {
+           "prod_dir": prod_dir,
+           "cand_dir": cand_dir,
+           "sim_dir": os.path.join(root_dir, date_str, "system_log_dir"),
+        }
+        #
+        fep_init_dict = {
+            "price_col": "twap",
+            "prediction_col": "prediction",
+            "volatility_col": "garman_klass_vol",
+        }
+        quantization = "nearest_share"
+        gmv = 20000.0
+    else:
+        raise ValueError(f"Unsupported asset class={asset_class}")
+    # Get a config.
+    config_dict = {
+        "meta": {
+            "date_str": date_str,
+            "asset_class": asset_class,
+            "run_tca": run_tca,
+            "bar_duration": bar_duration,
+        },
+        "load_data_config": data_dict,
+        "research_forecast_evaluator_from_prices": {
+            "init": fep_init_dict,
+            "annotate_forecasts_kwargs": {
+                "quantization": quantization,
+                "burn_in_bars": 1,
+                "style": "cross_sectional",
+                "bulk_frac_to_remove": 0.0,
+                "target_gmv": gmv,
+            }
+        }
+    }
+    config = cconfig.Config.from_dict(config_dict)
+    return config
+
+
+# %%
+date_str = "20221003"
+#asset_class = "equities"
+asset_class = "crypto"    
+    
+config = get_reconciliation_config(date_str, asset_class)
+print(config)
+
 # %% [markdown]
 # # Specify data to load
 
 # %% run_control={"marked": true}
-#date_str = "20220928"
-date_str = "20221003"
-root_dir = ""
-search_str = ""
-
-#cmd = f"find {root_dir}/{date_str}/prod -name '{search_str}'"
-#rc, prod_dir = hsystem.system_to_string(cmd)
-prod_dir = "/data/shared/prod_reconciliation/20221003/prod"
-prod_dir = prod_dir.replace("/data/shared/", "/shared_data/")
+prod_dir = config["load_data_config"]["prod_dir"]
+print(prod_dir)
 hdbg.dassert(prod_dir)
 hdbg.dassert_dir_exists(prod_dir)
 
-#cmd = f"find {root_dir}/{date_str}/job.candidate.* -name '{search_str}'"
-#rc, cand_dir = hsystem.system_to_string(cmd)
-cand_dir = "/data/shared/prod_reconciliation/20221003/prod"
-cand_dir = cand_dir.replace("/data/shared/", "/shared_data/")
+cand_dir = config["load_data_config"]["cand_dir"]
+print(cand_dir)
 hdbg.dassert(cand_dir)
 hdbg.dassert_dir_exists(cand_dir)
 
-#sim_dir = os.path.join(f"{root_dir}/{date_str}/system_log_dir")
-sim_dir = "/data/shared/prod_reconciliation/20221003/simulation/system_log_dir"
-sim_dir = sim_dir.replace("/data/shared/", "/shared_data/")
+sim_dir = config["load_data_config"]["sim_dir"]
+print(sim_dir)
 hdbg.dassert(sim_dir)
 hdbg.dassert_dir_exists(sim_dir)
 
@@ -92,8 +172,9 @@ print(sim_portfolio_dir)
 sim_dag_dir = os.path.join(sim_dir, "dag/node_io/node_io.data")
 hdbg.dassert_dir_exists(sim_dag_dir)
 #
-# tca_csv = os.path.join(root_dir, date_str, "tca/sau1_tca.csv")
-# hdbg.dassert_file_exists(tca_csv)
+if config["meta"]["run_tca"]:
+    tca_csv = os.path.join(root_dir, date_str, "tca/sau1_tca.csv")
+    hdbg.dassert_file_exists(tca_csv)
 
 # %%
 portfolio_path_dict = {
@@ -107,10 +188,6 @@ start_timestamp = pd.Timestamp(date_str + " 06:05:00", tz="America/New_York")
 _LOG.info("start_timestamp=%s", start_timestamp)
 end_timestamp = pd.Timestamp(date_str + " 08:00:00", tz="America/New_York")
 _LOG.info("end_timestamp=%s", end_timestamp)
-
-# %%
-#bar_duration = "15T"
-bar_duration = "5T"
 
 
 # %% [markdown]
@@ -302,33 +379,12 @@ prod_cand_dag_corr.min()
 # # Compute research portfolio equivalent
 
 # %%
-fep_dict = {
-    "init": {
-        #"price_col": "twap",
-        #"prediction_col": "prediction",
-        #"volatility_col": "garman_klass_vol",
-        "price_col": "vwap",
-        "prediction_col": "vwap.ret_0.vol_adj_2_hat",
-        "volatility_col": "vwap.ret_0.vol",
-    },
-    "annotate_forecasts_kwargs": {
-        #"quantization": "nearest_share",
-        "quantization": "no_quantization",
-        "burn_in_bars": 3,
-        #
-        "style": "cross_sectional",
-        "bulk_frac_to_remove": 0.0,
-        "target_gmv": 700.0,
-    },
-}
-
-# %%
-fep = dtfmod.ForecastEvaluatorFromPrices(**fep_dict["init"])
+fep = dtfmod.ForecastEvaluatorFromPrices(**config["research_forecast_evaluator_from_prices"]["init"])
 
 # %%
 research_portfolio_df, research_portfolio_stats_df = fep.annotate_forecasts(
     prod_dag_df,
-    **fep_dict["annotate_forecasts_kwargs"],
+    **config["research_forecast_evaluator_from_prices"]["annotate_forecasts_kwargs"],
     compute_extended_stats=True
 )
 
@@ -342,7 +398,7 @@ hpandas.df_to_str(research_portfolio_stats_df, log_level=logging.INFO)
 portfolio_config_dict = {
     "start_timestamp": start_timestamp,
     "end_timestamp": end_timestamp,
-    "freq": bar_duration,
+    "freq": config["meta"]["bar_duration"],
     "normalize_bar_times": True,
 }
 
