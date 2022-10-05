f904eca0e7ebd9b0b6abbb1bd1df9bc38ab189bc
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-10-05 15:33:36 +0000

    CMTask2932: Fix comments

--- oms/filled_orders_reader.py
+++ oms/filled_orders_reader.py
@@ -50,14 +50,15 @@ class FilledOrdersReader:
         :param start_ts: start of time period
         :param end_ts: end of time period
         :param file_format: 'json' or 'csv'
+        :return: list of files falling into given time range
         """
         hdbg.dassert_in(file_format, ["csv", "json"])
+        # Get files of the given file format.
         root_dir = os.path.join(self._root_dir, file_format)
-        # Get files for the given time range.
-        files = os.listdir(root_dir)
-        # Get files for the target account, e.g. only for `***REMOVED***`.
-        files = [f for f in files if str(self._secret_identifier) in f]
-        # Search for filename date patterns, e.g.
+        file_names = os.listdir(root_dir)
+        # Select files for the target account, e.g. only for `***REMOVED***`.
+        file_names = [f for f in file_names if str(self._secret_identifier) in f]
+        # Search for date patterns in file names, e.g.
         #  '20221001-000000_20221004-000000'.
         pattern = re.compile(r"(\d+-\d+)_(\d+-\d+)")
         # Construct a DataFrame of file names and corresponding timestamps.
@@ -65,41 +66,43 @@ class FilledOrdersReader:
         # Select all found time ranges from files.
         start_ts_strings = []
         end_ts_strings = []
-        for file in files:
+        for file in file_names:
+            # Extract start and end of the time range from file name.
+            #  e.g. ('20221001-000000', '20221004-000000')
             file_date_range = re.findall(pattern, file)[0]
             start_ts_as_str = file_date_range[0]
-            end_ts_as_str = file_date_range[0]
+            end_ts_as_str = file_date_range[1]
             start_ts_strings.append(start_ts_as_str)
             end_ts_strings.append(end_ts_as_str)
+        # Construct a dataframe for ease of filtering and convert timestamps.
         file_paths_df = pd.DataFrame(
             data={
-                "file_path": files,
-                "start_ts_string": start_ts_strings,
-                "end_ts_string": end_ts_strings,
+                "file_path": file_names,
+                "start_ts": pd.to_datetime(start_ts_strings, utc=True),
+                "end_ts": pd.to_datetime(end_ts_strings, utc=True),
             }
         )
-        file_paths_df[["start_ts_timestamp", "end_ts_timestamp"]] = file_paths_df[
-            ["start_ts_string", "end_ts_string"]
-        ].apply(pd.to_datetime,utc=True)
-        #
-        if start_ts < file_paths_df["start_ts_timestamp"].min():
+        # Filter data by start_ts.
+        if start_ts < file_paths_df["start_ts"].min():
             _LOG.warning(
                 "Provided start_ts is earlier than the earliest data timestamp: %s",
-                file_paths_df["start_ts_timestamp"].min(),
+                file_paths_df["start_ts"].min(),
             )
         file_paths_df = file_paths_df.loc[
-            file_paths_df["start_ts_timestamp"] >= start_ts
+            file_paths_df["start_ts"] >= start_ts
         ]
-        #
-        if end_ts > file_paths_df["end_ts_timestamp"].max():
+        # Filter data by end_ts.
+        if end_ts > file_paths_df["end_ts"].max():
             _LOG.warning(
                 "Provided end_ts is later than the latest data timestamp: %s",
-                file_paths_df["end_ts_timestamp"].max(),
+                file_paths_df["end_ts"].max(),
             )
         file_paths_df = file_paths_df.loc[
-            file_paths_df["end_ts_timestamp"] <= end_ts
+            file_paths_df["end_ts"] <= end_ts
         ]
-        file_paths = file_paths_df["file_path"]
+        # Extract file paths.
+        file_paths = file_paths_df["file_path"].to_list()
+        # Get absolute paths.
         file_paths = [(os.path.join(root_dir, f)) for f in file_paths]
         return file_paths
 
@@ -110,7 +113,22 @@ class FilledOrdersReader:
         """
         Read a .csv file for filled trades.
 
-        An example of output data: same as `FilledTradesReader.convert_fills_json_to_dataframe`
+        Example of JSON output data: same as `FilledTradesReader.convert_fills_json_to_dataframe`.
+
+        Example of CSV output data:
+        # pylint: disable=line-too-long
+                                            symbol         id       order  side takerOrMaker  price  amount    cost      fees fees_currency  realized_pnl
+        timestamp
+        2022-09-29 16:46:39.509000+00:00  APE/USDT  282773274  5772340563  sell        taker  5.427     5.0  27.135  0.010854          USDT         0.000
+        2022-09-29 16:51:58.567000+00:00  APE/USDT  282775654  5772441841   buy        taker  5.398     6.0  32.388  0.012955          USDT         0.145
+        2022-09-29 16:57:00.267000+00:00  APE/USDT  282779084  5772536135   buy        taker  5.407     3.0  16.221  0.006488          USDT         0.000
+
+        # pylint: enable=line-too-long
+
+        :param start_ts: beginning of time period, tz-aware
+        :param end_ts: end of time period, tz-aware
+        :param file_format: csv or json
+        :return: JSON list of dicts for 'json', formatted DataFrame for 'csv'
         """
         hdbg.dassert_in(file_format, ["json", "csv"])
         # Assert that passed timestamps have timezones.
