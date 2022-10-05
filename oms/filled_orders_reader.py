86c2bc2db483924dc745969c1b29c92a1c80996c
Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>
2022-10-05 14:26:22 +0000

    CMTask2932: Checkpoint

--- oms/filled_orders_reader.py
+++ oms/filled_orders_reader.py
@@ -6,14 +6,14 @@ import oms.filled_orders_reader as ofiorrea
 import logging
 import os
 import re
+from typing import Any, Dict, List, Union
 
 import pandas as pd
 
 import helpers.hdatetime as hdateti
 import helpers.hdbg as hdbg
-import oms.hsecrets as homssec
 import helpers.hio as hio
-from typing import Union, Dict, List, Any
+import oms.hsecrets as homssec
 
 _LOG = logging.getLogger(__name__)
 
@@ -40,7 +40,7 @@ class FilledOrdersReader:
 
     def get_file_names_for_time_period(
         self, start_ts: pd.Timestamp, end_ts: pd.Timestamp, file_format: str
-    ):
+    ) -> List[str]:
         """
         Get files that correspond to the given time period.
 
@@ -53,9 +53,6 @@ class FilledOrdersReader:
         """
         hdbg.dassert_in(file_format, ["csv", "json"])
         root_dir = os.path.join(self._root_dir, file_format)
-        # Change file format to allow reading gzipped csv files.
-        if file_format == "csv":
-            file_format = "csv.gz"
         # Get files for the given time range.
         files = os.listdir(root_dir)
         # Get files for the target account, e.g. only for `***REMOVED***`.
@@ -63,50 +60,58 @@ class FilledOrdersReader:
         # Search for filename date patterns, e.g.
         #  '20221001-000000_20221004-000000'.
         pattern = re.compile(r"(\d+-\d+)_(\d+-\d+)")
-        date_ranges_as_str = []
-        date_ranges_as_timestamps = []
-        # Select all found time ranges from files.
-        for file in files:
-            date_range = re.findall(pattern, file)
-            date_ranges_as_str.extend(date_range)
-
-        # Get files that fit into the given time range.
+        # Construct a DataFrame of file names and corresponding timestamps.
         #
-        # Get start timestamp closest to start_ts.
-        start_ts_in_files = [pd.Timestamp(date_range[0], tz="UTC") for date_range in date_ranges]
-        if min(start_ts_in_files) > start_ts:
-            start_ts_from_file_name = min(start_ts_in_files)
-            _LOG.warning("min_ts is outside available data. Available min_ts=%s", start_ts_from_file_name)
-        else:
-            start_ts_from_file_name = max([ts for ts in start_ts_in_files if ts <= start_ts])
-
-        # Get end timestamp closest to end_ts.
-        end_ts_in_files = [pd.Timestamp(date_range[1], tz="UTC") for date_range in date_ranges]
-        if max(end_ts_in_files) < end_ts:
-            end_ts_from_file_name = max(end_ts_in_files)
-            _LOG.warning("end_ts is outside available data. Available end_ts=%s", end_ts_from_file_name)
-        else:
-            end_ts_from_file_name = min([ts for ts in end_ts_in_files if ts >= end_ts])
-        # Select files closest to the given time boundaries.
-        target_paths = []
-        for date_range in date_ranges:
-            if (
-                date_range[0] >= start_ts_from_file_name
-                and date_range[1] <= end_ts_from_file_name
-            ):
-                path = os.path.join(
-                    root_dir,
-                    f"fills_{date_range[0]}_{date_range[1]}_{self._secret_identifier}.{file_format}",
-                )
-                target_paths.append(path)
-        return target_paths
+        # Select all found time ranges from files.
+        start_ts_strings = []
+        end_ts_strings = []
+        for file in files:
+            file_date_range = re.findall(pattern, file)[0]
+            start_ts_as_str = file_date_range[0]
+            end_ts_as_str = file_date_range[0]
+            start_ts_strings.append(start_ts_as_str)
+            end_ts_strings.append(end_ts_as_str)
+        file_paths_df = pd.DataFrame(
+            data={
+                "file_path": files,
+                "start_ts_string": start_ts_strings,
+                "end_ts_string": end_ts_strings,
+            }
+        )
+        file_paths_df[["start_ts_timestamp", "end_ts_timestamp"]] = file_paths_df[
+            ["start_ts_string", "end_ts_string"]
+        ]
+        #
+        if start_ts < file_paths_df["start_ts_timestamp"].min():
+            file_paths_df = file_paths_df.loc[
+                file_paths_df["start_ts_timestamp"].min()
+            ]
+            _LOG.warning(
+                "Provided start_ts is earlier than the earliest data timestamp: %s",
+                file_paths_df["start_ts_timestamp"].min(),
+            )
+        file_paths_df = file_paths_df.loc[
+            file_paths_df["start_ts_timestamp"] >= start_ts
+        ]
+        #
+        if end_ts > file_paths_df["end_ts_timestamp"].max():
+            file_paths_df = file_paths_df.loc[
+                file_paths_df["end_ts_timestamp"].min()
+            ]
+            _LOG.warning(
+                "Provided end_ts is later than the latest data timestamp: %s",
+                file_paths_df["end_ts_timestamp"].max(),
+            )
+        file_paths_df = file_paths_df.loc[
+            file_paths_df["end_ts_timestamp"] <= end_ts
+        ]
+        file_paths = file_paths_df["file_paths"]
+        file_paths = [(os.path.join(root_dir, f)) for f in file_paths]
+        return file_paths
 
     # TODO(Danya): Update to use with JSON.
     def read_filled_orders(
-        self,
-        start_ts: pd.Timestamp,
-        end_ts: pd.Timestamp,
-        file_format: str
+        self, start_ts: pd.Timestamp, end_ts: pd.Timestamp, file_format: str
     ) -> Union[List[Dict[str, Any]], pd.DataFrame]:
         """
         Read a .csv file for filled trades.
@@ -118,7 +123,9 @@ class FilledOrdersReader:
         hdateti.dassert_has_tz(start_ts)
         hdateti.dassert_has_tz(end_ts)
         # Get files corresponding to the given time period.
-        file_names = self.get_file_names_for_time_period(start_ts, end_ts, file_format)
+        file_names = self.get_file_names_for_time_period(
+            start_ts, end_ts, file_format
+        )
         filled_trades_data = []
         if file_format == "json":
             # Load JSON as a list of dicts.
