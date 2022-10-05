4e3401de49c07e7946cb85de21d237cfd6a333bd
dan <d.yachmenev@crypto-kaizen.com>
2022-10-05 13:59:55 +0000

    linter

--- oms/build_configs.py
+++ oms/build_configs.py
@@ -1,25 +1,20 @@
-import logging
-import os
-from typing import List, Tuple
+"""
+Import as:
 
-import pandas as pd
+import oms.build_configs as obuiconf
+"""
+
+# TODO(Grisha): @Dan Decide where to put the file.
+import os
 
 import core.config as cconfig
-import core.finance as cofinanc
-import core.plotting as coplotti
-import dataflow.model as dtfmod
-import helpers.hdbg as hdbg
-import helpers.henv as henv
-import helpers.hpandas as hpandas
-import helpers.hprint as hprint
 import helpers.hsystem as hsystem
-import oms as oms
 
 
 def get_reconciliation_config(date_str: str, asset_class: str) -> cconfig.Config:
     """
     Get a reconciliation that is specific of an asset class.
-    
+
     :param date_str: reconciliation date as str, e.g., `20221003`
     :param asset_class: either `equities` or `crypto`
     """
@@ -32,12 +27,19 @@ def get_reconciliation_config(date_str: str, asset_class: str) -> cconfig.Config
         #
         root_dir = "/shared_data/prod_reconciliation"
         # TODO(Grisha): probably we should rename to `system_log_dir`.
-        prod_dir = os.path.join(root_dir, date_str, "prod", "system_log_dir_scheduled__2022-10-03T10:00:00+00:00_2hours")
+        prod_dir = os.path.join(
+            root_dir,
+            date_str,
+            "prod",
+            "system_log_dir_scheduled__2022-10-03T10:00:00+00:00_2hours",
+        )
         data_dict = {
-           "prod_dir": prod_dir,
-           # For crypto we do not have a `candidate` so we just re-use prod.
-           "cand_dir": prod_dir,
-           "sim_dir": os.path.join(root_dir, date_str, "simulation", "system_log_dir"),
+            "prod_dir": prod_dir,
+            # For crypto we do not have a `candidate` so we just re-use prod.
+            "cand_dir": prod_dir,
+            "sim_dir": os.path.join(
+                root_dir, date_str, "simulation", "system_log_dir"
+            ),
         }
         #
         fep_init_dict = {
@@ -57,12 +59,14 @@ def get_reconciliation_config(date_str: str, asset_class: str) -> cconfig.Config
         search_str = ""
         prod_dir_cmd = f"find {root_dir}/{date_str}/prod -name '{search_str}'"
         _, prod_dir = hsystem.system_to_string(prod_dir_cmd)
-        cand_cmd = f"find {root_dir}/{date_str}/job.candidate.* -name '{search_str}'"
+        cand_cmd = (
+            f"find {root_dir}/{date_str}/job.candidate.* -name '{search_str}'"
+        )
         _, cand_dir = hsystem.system_to_string(cand_cmd)
         data_dict = {
-           "prod_dir": prod_dir,
-           "cand_dir": cand_dir,
-           "sim_dir": os.path.join(root_dir, date_str, "system_log_dir"),
+            "prod_dir": prod_dir,
+            "cand_dir": cand_dir,
+            "sim_dir": os.path.join(root_dir, date_str, "system_log_dir"),
         }
         #
         fep_init_dict = {
@@ -92,10 +96,9 @@ def get_reconciliation_config(date_str: str, asset_class: str) -> cconfig.Config
                 "style": "cross_sectional",
                 "bulk_frac_to_remove": 0.0,
                 "target_gmv": gmv,
-                "liquidate_at_end_of_day": liquidate_at_end_of_day
-            }
-        }
+                "liquidate_at_end_of_day": liquidate_at_end_of_day,
+            },
+        },
     }
-    
     config = cconfig.Config.from_dict(config_dict)
     return config
