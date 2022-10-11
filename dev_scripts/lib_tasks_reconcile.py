172143075dae5347fd3858f9a982e42251c9e31b
Nina Lee <nina.tru94@gmail.com>
2022-10-11 17:08:25 +0000

    fix linter

--- dev_scripts/lib_tasks_reconcile.py
+++ dev_scripts/lib_tasks_reconcile.py
@@ -77,10 +77,15 @@ def _sanity_check_data(file_path: str) -> None:
 @task
 def reconcile_create_dirs(ctx, run_date=None):  # type: ignore
     """
-    Create dirs for storing reconciliation data.
-
-    Final dirs layout is: ``` data/     shared/ prod_reconciliation/
-    {run_date}/                 prod/ ...                 simulation/
+    Final dirs layout is:
+    ```
+     data/
+         shared/
+             prod_reconciliation/
+                 {run_date}/
+                     prod/
+                     ...
+                     simulation/
     ```
     """
     _ = ctx
