228593031c8a14b7aa07e153845978468e176761
dan <d.yachmenev@crypto-kaizen.com>
2022-10-12 17:01:20 +0000

    checkpoint

--- dev_scripts/lib_tasks_reconcile.py
+++ dev_scripts/lib_tasks_reconcile.py
@@ -77,6 +77,8 @@ def _sanity_check_data(file_path: str) -> None:
 @task
 def reconcile_create_dirs(ctx, run_date=None):  # type: ignore
     """
+    Create dirs for storing reconciliation data.
+    
     Final dirs layout is:
     ```
     data/
