6ab931dca41641deff03b963cf9f45cf43d3bc68
PomazkinG <pomazking@gmail.com>
2022-10-14 14:49:14 +0000

    checkpoint

--- dataflow_amp/system/Cx/Cx_builders.py
+++ dataflow_amp/system/Cx/Cx_builders.py
@@ -288,7 +288,8 @@ def _get_Cx_dag_prod_instance1(
     dag = dag_builder.get_dag(dag_config)
     system = dtfsys.apply_dag_property(dag, system)
     #
-    system = dtfsys.apply_DagRunner_config_for_crypto(system)
+    is_prod = True
+    system = dtfsys.apply_DagRunner_config_for_crypto(system, is_prod)
     # Build Portfolio.
     trading_period_str = dag_builder.get_trading_period(dag_config)
     # TODO(gp): Add a param to get_trading_period to return the int.
