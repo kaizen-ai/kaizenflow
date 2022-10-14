c387c362f05aa2ede859960fb3098cbbcb751222
PomazkinG <pomazking@gmail.com>
2022-10-14 14:51:17 +0000

    checkpoint

--- dataflow_amp/system/Cx/Cx_builders.py
+++ dataflow_amp/system/Cx/Cx_builders.py
@@ -255,7 +255,7 @@ def get_Cx_RealTimeDag_example2(
     system.config["process_forecasts_node_dict"] = cconfig.Config.from_dict(
         process_forecasts_node_dict
     )
-    system = dtfsys.apply_ProcessForecastsNode_config_for_crypto(system)
+    system = dtfsys.apply_ProcessForecastsNode_config_for_crypto(system, is_prod)
     # Append the `ProcessForecastNode`.
     dag = dtfsys.add_ProcessForecastsNode(system, dag)
     return dag
