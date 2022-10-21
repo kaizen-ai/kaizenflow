af0d65d01b0cd708ae79081407fe10efc4c40d1b
Nina Lee <nina.tru94@gmail.com>
2022-10-21 15:01:11 +0000

    CMTask_3068_Fix_get_last_twap_price

--- market_data/abstract_market_data.py
+++ market_data/abstract_market_data.py
@@ -492,7 +492,7 @@ class MarketData(abc.ABC, hobject.PrintableMixin):
         offset = pd.Timedelta(bar_duration)
         start_time = last_end_time - offset
         # TODO(gp): Remove this.
-        start_time = start_time - pd.Timedelta(minutes=1)
+        # start_time = start_time - pd.Timedelta(minutes=1)
         twap_df = self.get_twap_price(
             start_time,
             last_end_time,
