b6cb5eb939792be1a167b9909d8bc98057c7e49d
Nina Lee <68428519+rheenina@users.noreply.github.com>
2022-10-29 01:49:51 +0800

    Cm task3102 add delay analysis (#3103)
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * linter
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    Co-authored-by: PomazkinG <pomazking@gmail.com>

--- oms/notebooks/Master_reconciliation.py
+++ oms/notebooks/Master_reconciliation.py
@@ -32,6 +32,10 @@ import helpers.hdbg as hdbg
 import helpers.henv as henv
 import helpers.hpandas as hpandas
 import helpers.hprint as hprint
+import helpers.hsql as hsql
+import im_v2.ccxt.data.client as icdcl
+import im_v2.common.universe as ivcu
+import im_v2.im_lib_tasks as imvimlita
 import oms as oms
 
 # %%
@@ -95,6 +99,62 @@ _LOG.info("start_timestamp=%s", start_timestamp)
 end_timestamp = pd.Timestamp(date_str + " 08:00:00", tz="America/New_York")
 _LOG.info("end_timestamp=%s", end_timestamp)
 
+
+# %% [markdown]
+# # Data delay analysis
+
+# %%
+# Get the real-time `ImClient`.
+# TODO(Grisha): ideally we should get the values from the config.
+resample_1min = False
+env_file = imvimlita.get_db_env_path("dev")
+connection_params = hsql.get_connection_info_from_env_file(env_file)
+db_connection = hsql.get_connection(*connection_params)
+table_name = "ccxt_ohlcv_futures"
+#
+im_client = icdcl.CcxtSqlRealTimeImClient(
+    resample_1min, db_connection, table_name
+)
+
+# %%
+# Get the universe.
+# TODO(Grisha): get the version from the config.
+vendor = "CCXT"
+mode = "trade"
+version = "v7.1"
+as_full_symbol = True
+full_symbols = ivcu.get_vendor_universe(
+    vendor,
+    mode,
+    version=version,
+    as_full_symbol=as_full_symbol,
+)
+full_symbols
+
+# %%
+# Load the data for the reconciliation date.
+# `ImClient` operates in UTC timezone.
+start_ts = pd.Timestamp(date_str, tz="UTC")
+end_ts = start_ts + pd.Timedelta(days=1)
+columns = None
+filter_data_mode = "assert"
+df = im_client.read_data(
+    full_symbols, start_ts, end_ts, columns, filter_data_mode
+)
+hpandas.df_to_str(df, num_rows=5, log_level=logging.INFO)
+
+# %%
+# TODO(Grisha): move to a lib.
+# Compute delay in seconds.
+df["delta"] = (df["knowledge_timestamp"] - df.index).dt.total_seconds()
+# Plot the delay over assets with the errors bars.
+minimums = df.groupby(by=["full_symbol"]).min()["delta"]
+maximums = df.groupby(by=["full_symbol"]).max()["delta"]
+means = df.groupby(by=["full_symbol"]).mean()["delta"]
+errors = [means - minimums, maximums - means]
+df.groupby(by=["full_symbol"]).mean()["delta"].sort_values(ascending=False).plot(
+    kind="bar", yerr=errors
+)
 
 # %% [markdown]
 # # Compare DAG io
