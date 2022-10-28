cc78d8449f1b9f659dcfa4864df152775ea0bd7b
Nina Lee <nina.tru94@gmail.com>
2022-10-28 10:37:49 +0000

    checkpoint

--- oms/notebooks/Untitled2.py
+++ oms/notebooks/Untitled2.py
@@ -16,13 +16,21 @@
 # %load_ext autoreload
 # %autoreload 2
 
+# %%
+import logging
+
+import pandas as pd
+
+import helpers.hdbg as hdbg
+import helpers.henv as henv
+import helpers.hio as hio
+import helpers.hprint as hprint
 import helpers.hsql as hsql
 import im_v2.ccxt.data.client as icdcl
-import im_v2.common.data.client as icdc
-import im_v2.common.universe.universe_utils as imvcuunut
+import im_v2.common.universe as ivcu
 import im_v2.im_lib_tasks as imvimlita
 
-
+# %%
 resample_1min = False
 env_file = imvimlita.get_db_env_path("dev")
 # Get login info.
@@ -39,23 +47,6 @@ im_client = icdcl.CcxtSqlRealTimeImClient(
 )
 
 # %%
-import logging
-import os
-from typing import Dict
-
-import pandas as pd
-
-import core.config as cconfig
-import core.finance as cofinanc
-import core.plotting as coplotti
-import dataflow.model as dtfmod
-import helpers.hdbg as hdbg
-import helpers.henv as henv
-import helpers.hpandas as hpandas
-import helpers.hpickle as hpickle
-import helpers.hprint as hprint
-import oms as oms
-
 hdbg.init_logger(verbosity=logging.INFO)
 
 _LOG = logging.getLogger(__name__)
@@ -65,8 +56,6 @@ _LOG.info("%s", henv.get_system_signature()[0])
 hprint.config_notebook()
 
 # %%
-import im_v2.common.universe as ivcu
-
 vendor = "CCXT"
 mode = "trade"
 version = "v7.1"
@@ -80,18 +69,20 @@ full_symbols = ivcu.get_vendor_universe(
 full_symbols
 
 # %%
-import pandas as pd
-
 start_ts = pd.Timestamp("2022-10-25 00:00:00-04:00")
 end_ts = pd.Timestamp("2022-10-25 23:59:00-04:00")
 columns = None
 filter_data_mode = "assert"
-df = im_client.read_data(full_symbols, start_ts, end_ts, columns, filter_data_mode)
+df = im_client.read_data(
+    full_symbols, start_ts, end_ts, columns, filter_data_mode
+)
 df["delta"] = (df["knowledge_timestamp"] - df.index).dt.total_seconds()
 df
 
 # %%
-df.groupby(by=["full_symbol"]).max()["delta"].sort_values(ascending=False).plot(kind="bar")
+df.groupby(by=["full_symbol"]).max()["delta"].sort_values(ascending=False).plot(
+    kind="bar"
+)
 
 # %%
 print("min", df["delta"].min().round(2))
@@ -100,10 +91,7 @@ print("std", df["delta"].std().round(2))
 print("max", df["delta"].max().round(2))
 
 # %%
-df["delta"].plot(kind="hist", bins=50, xlim=(0,120))
-
-# %%
-import helpers.hio as hio
+df["delta"].plot(kind="hist", bins=50, xlim=(0, 120))
 
 # %%
 path = "/shared_data/prod_reconciliation/20221013/tca/json/fills_20221012-000000_20221013-000000_binance.preprod.trading.3.json"
