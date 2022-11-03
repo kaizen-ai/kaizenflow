b9929d5c162a366aa9cf2766703e81218db6a9fa
jsmerix <smeriga.juraj@gmail.com>
2022-11-03 16:55:44 +0000

    Add new/update existing preprod DAGs

--- im_v2/airflow/dags/preprod.realtime_db_data_archival_to_s3_fargate.py
+++ im_v2/airflow/dags/preprod.realtime_db_data_archival_to_s3_fargate.py
@@ -1,17 +1,24 @@
-# This is a utility DAG to download bid/ask data daily.
+# This is a utility DAG to archive real time data
+# The DAG fetches data older than a specified timestamp threshold
+# from the specified table(s) and archives to the specified S3
+# location(s). Once the data is archived, it's dropped from the DB table
 
+# IMPORTANT NOTES:
+# Make sure to set correct dag schedule `schedule_interval`` parameter.
+
+# This DAG's configuration deploys tasks to AWS Fargate to offload the EC2s
+# mainly utilized for rt download
 import datetime
 import airflow
 from airflow.contrib.operators.ecs_operator import ECSOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.models import Variable
-from itertools import product
 import copy
 import os
 
 _FILENAME = os.path.basename(__file__)
 
-# This variable will be propagated throughout DAG definition as a prefix to
+# This variable will be propagated throughout DAG definition as a prefix to 
 # names of Airflow configuration variables, allow to switch from test to preprod/prod
 # in one line (in best case scenario).
 _STAGE = _FILENAME.split(".")[0]
@@ -27,13 +34,12 @@ _LAUNCH_TYPE = "fargate"
 assert _LAUNCH_TYPE in ["ec2", "fargate"]
 
 _DAG_ID = _FILENAME.rsplit(".", 1)[0]
-_EXCHANGES = ["binance"]
-_PROVIDERS = ["crypto_chassis"]
-_UNIVERSES = { "crypto_chassis": "v2"}
-_CONTRACTS = ["spot", "futures"]
-_DATA_TYPES = ["bid_ask"]
-_DAG_DESCRIPTION = f"Daily {_DATA_TYPES} data download, contracts:" \
-                + f"{_CONTRACTS}, using {_PROVIDERS} from {_EXCHANGES}."
+# Base name of the db tables to archive, stage will be appended later. 
+_DB_TABLES = ["ccxt_bid_ask_futures_raw"]
+# If _DRY_RUN = True the data is not actually archived/deleted.
+_DRY_RUN = False
+_DAG_DESCRIPTION = f"Realtime data archival to S3 of table(s):" \
+                + f"{_DB_TABLES}."
 _SCHEDULE = Variable.get(f'{_DAG_ID}_schedule')
 
 # Used for container overrides inside DAG task definition.
@@ -47,24 +53,21 @@ _CONTAINER_NAME = f"cmamp{_CONTAINER_SUFFIX}"
 
 ecs_cluster = Variable.get(f'{_STAGE}_ecs_cluster')
 # The naming convention is set such that this value is then reused
-# in log groups, stream prefixes and container names to minimize
+# in log groups, stream prefixes and container names to minimize 
 # convolution and maximize simplicity.
 ecs_task_definition = _CONTAINER_NAME
 
-# Subnets and security group is not needed for EC2 deployment but
+# Subnets and security group is not needed for EC2 deployment but 
 # we keep the configuration header unified for convenience/reusability.
-ecs_subnets = [Variable.get("ecs_subnet1"), Variable.get("ecs_subnet2")]
+ecs_subnets = [Variable.get("ecs_subnet1")]
 ecs_security_group = [Variable.get("ecs_security_group")]
 ecs_awslogs_group = f"/ecs/{ecs_task_definition}"
 ecs_awslogs_stream_prefix = f"ecs/{ecs_task_definition}"
-s3_daily_staged_data_path = f"s3://{Variable.get(f'{_STAGE}_s3_data_bucket')}/{Variable.get('s3_daily_staged_data_bid_ask_folder')}"
+s3_db_archival_data_path = f"s3://{Variable.get(f'{_STAGE}_s3_data_bucket')}/{Variable.get('s3_db_archival_path')}"
 
 # Pass default parameters for the DAG.
 default_args = {
-    "retries": 1,
-    # CryptoChassis might throw
-    # too many request error, wait a couple min to retry.
-    "retry_delay": datetime.timedelta(minutes=5),
+    "retries": 0,
     "email": [Variable.get(f'{_STAGE}_notification_email')],
     "email_on_failure": True if _STAGE in ["prod", "preprod"] else False,
     "email_on_retry": False,
@@ -78,45 +81,35 @@ dag = airflow.DAG(
     max_active_runs=1,
     default_args=default_args,
     schedule_interval=_SCHEDULE,
-    catchup=False,
-    start_date=datetime.datetime(2022, 7, 1, 0, 0, 0),
+    catchup=True,
+    start_date=datetime.datetime(2022, 11, 3, 12, 0, 0),
 )
-
-download_command = [
-    "/app/amp/im_v2/{}/data/extract/download_historical_data.py",
-     "--end_timestamp '{{ execution_date + macros.timedelta(hours=24) - macros.timedelta(minutes=var.value.daily_bid_ask_data_download_delay_min | int)  }}'",
-     "--start_timestamp '{{ execution_date - macros.timedelta(minutes=var.value.daily_bid_ask_data_download_delay_min | int) }}'",
-     "--exchange_id '{}'",
-     "--universe '{}'",
-     "--aws_profile 'ck'",
-     "--data_type '{}'",
-     "--file_format 'parquet'",
-     # The command needs to be executed manually first because --incremental
-     # assumes appending to existing folder.
-     "--incremental",
-     "--contract_type '{}'",
-     "--s3_path '{}{}/{}'"
+    
+archival_command = [
+   "/app/amp/im_v2/ccxt/db/archive_db_data_to_s3.py",
+   "--db_stage 'dev'",
+   "--timestamp '{{ data_interval_end - macros.timedelta(hours=var.value.db_archival_delay_hours | int) }}'",
+   "--db_table '{}'",
+   f"--s3_path '{s3_db_archival_data_path}'",
+   # The command needs to be executed manually first because --incremental 
+   # assumes appending to existing folder.
+   "--incremental"
 ]
 
-start_task = DummyOperator(task_id='start_dag', dag=dag)
-end_download = DummyOperator(task_id='end_dag', dag=dag)
+start_archival = DummyOperator(task_id='start_archival', dag=dag)
+end_archival = DummyOperator(task_id='end_archival', dag=dag)
 
-for provider, exchange, contract, data_type in product(_PROVIDERS, _EXCHANGES, _CONTRACTS, _DATA_TYPES):
+for db_table in _DB_TABLES:
 
+    db_table_with_stage = db_table
+    db_table_with_stage += f"_{_STAGE}" if _STAGE in ["test", "preprod"] else ""
+    
     #TODO(Juraj): Make this code more readable.
     # Do a deepcopy of the bash command list so we can reformat params on each iteration.
-    curr_bash_command = copy.deepcopy(download_command)
-    curr_bash_command[0] = curr_bash_command[0].format(provider)
-    curr_bash_command[3] = curr_bash_command[3].format(exchange)
-    curr_bash_command[4] = curr_bash_command[4].format(_UNIVERSES[provider])
-    curr_bash_command[6] = curr_bash_command[6].format(data_type)
-    curr_bash_command[-2] = curr_bash_command[-2].format(contract)
-    curr_bash_command[-1] = curr_bash_command[-1].format(
-        s3_daily_staged_data_path,
-        # For futures we need to suffix the folder.
-        "-futures" if contract == "futures" else "",
-        provider
-    )
+    curr_bash_command = copy.deepcopy(archival_command)
+    curr_bash_command[3] = curr_bash_command[3].format(db_table_with_stage)
+    if _DRY_RUN:
+        curr_bash_command.append("--dry_run")
 
     kwargs = {}
     kwargs["network_configuration"] = {
@@ -125,9 +118,9 @@ for provider, exchange, contract, data_type in product(_PROVIDERS, _EXCHANGES, _
             "subnets": ecs_subnets,
         },
     }
-
-    downloading_task = ECSOperator(
-        task_id=f"download_{provider}_{exchange}_{contract}",
+    
+    archiving_task = ECSOperator(
+        task_id=f"archive_{db_table_with_stage}",
         dag=dag,
         aws_conn_id=None,
         cluster=ecs_cluster,
@@ -140,14 +133,13 @@ for provider, exchange, contract, data_type in product(_PROVIDERS, _EXCHANGES, _
                     "command": curr_bash_command,
                 }
             ],
-            # For unknown reason bid-ask task needs more resources.
-            "cpu": "512",
-            "memory": "1024"
+            "cpu": "1024",
+            "memory": "4096"
         },
         awslogs_group=ecs_awslogs_group,
         awslogs_stream_prefix=ecs_awslogs_stream_prefix,
-        execution_timeout=datetime.timedelta(minutes=15),
+        execution_timeout=datetime.timedelta(minutes=20),
         **kwargs
     )
-
-    start_task >> downloading_task >> end_download
+    
+    start_archival >> archiving_task >> end_archival
