a254e057671d6437e7fea5cee86518f6bd68fb70
PomazkinG <pomazking@gmail.com>
2022-11-06 17:24:21 +0000

    checkpoint

--- dataflow_amp/system/Cx/Cx_builders.py
+++ dataflow_amp/system/Cx/Cx_builders.py
@@ -165,7 +165,8 @@ def get_ProcessForecastsNode_dict_instance1(
     if is_prod:
         compute_target_positions_kwargs = {
             "bulk_frac_to_remove": 0.0,
-            "target_gmv": 700.0,
+            "target_gmv": 1500.0,
+            #"target_gmv": 700.0,
         }
         root_log_dir = system.config.get("system_log_dir")
     else:
