41a94bc30d8b47e4879653b68f37555dc62a2bfc
Nina Lee <nina.tru94@gmail.com>
2022-11-14 16:23:28 +0000

    remove binance_invoke from cmamp tasks due to invoke failures

--- tasks.py
+++ tasks.py
@@ -98,9 +98,6 @@ from helpers.lib_tasks import (  # isort: skip # noqa: F401  # pylint: disable=u
     run_tests,
     traceback,
 )
-from dev_scripts.lib_tasks_binance import (
-    binance_get_open_positions
-)
 # # TODO(gp): This is due to the coupling between code in linter container and
 # #  the code being linted.
 # try:
