610f7f53ab38a6366da8353f697082c0c7c62c62
jsmerix <smeriga.juraj@gmail.com>
2022-11-14 18:34:19 +0000

    Add suport for db stages

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,14 @@
-from logging.config import fileConfig
+"""
+Import as:
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+import alembic.env as alenv
+"""
 
+import logging
+
+import sqlalchemy
+
+import helpers.hsql as hsql
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -12,7 +18,7 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    logging.config.fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -25,9 +31,20 @@ target_metadata = None
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+# This is a custom added argument to differentiate between db_stages.
+cmd_kwargs = context.get_x_argument(as_dictionary=True)
+if "stage" not in cmd_kwargs:
+    raise Exception(
+        "Argument `stage` not found in the CLI arguments. "
+        "Please verify `alembic` was run with `-x stage=db_stage` "
+        "(e.g. `alembic -x stage=dev upgrade head`)"
+    )
+stage = cmd_kwargs["stage"]
+
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """
+    Run migrations in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -36,7 +53,6 @@ def run_migrations_offline() -> None:
 
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -51,23 +67,24 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+    Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine and associate a
+    connection with the context.
+    """
+    # In order to reuse our API we need to pass this as callable
+    #  into sqlalchemy.
+    conn_creator = lambda _: hsql.get_connection_from_env_file(stage)
+    connectable = sqlalchemy.create_engine(
+        "postgresql+psycopg2://", creator=conn_creator
     )
-
-    with connectable.connect() as connection:
+    with connectable.connect() as conn:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=conn,
+            target_metadata=target_metadata,
+            version_table_schema="public",
         )
-
         with context.begin_transaction():
             context.run_migrations()
 
--- alembic/versions/411c7363a33d_init.py
+++ alembic/versions/411c7363a33d_init.py
@@ -1,17 +1,20 @@
-"""Init
+"""
+Init.
 
 Revision ID: 411c7363a33d
-Revises: 
+Revises:
 Create Date: 2022-11-14 15:04:45.509703+00:00
 
-"""
-import alembic
-import os
-import sqlalchemy as sa
+Import as:
 
+import alembic.versions.411c7363a33d_init as ave41ini
+"""
+import os
+
+import alembic
 
 # revision identifiers, used by Alembic.
-revision = '411c7363a33d'
+revision = "411c7363a33d"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,6 +26,7 @@ init_sql_file = os.path.join(dir_path, "411c7363a33d_init.sql")
 # root of the repo (container)
 with open(init_sql_file) as file:
     alembic.op.execute(file.read())
+
 
 def upgrade() -> None:
     pass
