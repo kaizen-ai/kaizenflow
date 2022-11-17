79276661d4ac751599503e7eb2115c0ef07705e2
jsmerix <smeriga.juraj@gmail.com>
2022-11-17 17:03:47 +0000

    Checkpoint for updating comparison to enable multilevel bid_ask reconciliation

--- im_v2/ccxt/data/extract/compare_realtime_and_historical.py
+++ im_v2/ccxt/data/extract/compare_realtime_and_historical.py
@@ -39,6 +39,7 @@ import helpers.hsql as hsql
 import im_v2.ccxt.data.client as icdcl
 import im_v2.common.universe.full_symbol as imvcufusy
 import im_v2.im_lib_tasks as imvimlita
+import im_v2.common.data.transform.transform_utils as imvcdttrut
 
 _LOG = logging.getLogger(__name__)
 
@@ -71,6 +72,20 @@ def build_data_reconciliation_config_env_var(
 
 # #############################################################################
 
+def get_multilevel_bid_ask_column_names(depth: int = 10) -> List[str]:
+    """
+    Construct list of bid ask column names for multilevel setup.
+    
+    Example for depth = 2
+        [bid_price_l1, bid_size_l1, ask_size_l1, ask_price_l1, bid_price_l2,...]
+    """
+    multilevel_bid_ask_cols = []
+    for i in range(1, depth + 1):
+        bid_ask_cols_level = map(lambda x: f"{x}_l{i}", BID_ASK_COLS)
+        for col in bid_ask_cols_level:
+            multilevel_bid_ask_cols.append(col)
+    return multilevel_bid_ask_cols
+        
 
 def _parse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -216,11 +231,7 @@ class RealTimeHistoricalReconciler:
             "bid_ask": [
                 "timestamp",
                 "full_symbol",
-                "bid_price",
-                "bid_size",
-                "ask_price",
-                "ask_size",
-            ],
+                ] + get_multilevel_bid_ask_column_names(),
         }
         # Get CCXT data.
         self.ccxt_rt = self._get_rt_data()
@@ -325,26 +336,28 @@ class RealTimeHistoricalReconciler:
         _LOG.info("Dataframe length after duplicate rows removed: %s", len(data))
         return data
 
-    @staticmethod
-    def _clean_data_for_orderbook_level(
-        df: pd.DataFrame, level: int = 1
-    ) -> pd.DataFrame:
-        """
-        Specify the order book level in CCXT bid ask data.
-
-        :param df: Data with multiple levels (e.g., bid_price_1, bid_price_2, etc.)
-        :return: Data where specific level has common name (i.e., bid_price)
-        """
-        level_cols = [col for col in df.columns if col.endswith(f"_{level}")]
-        level_cols_cleaned = [elem[:-2] for elem in level_cols]
-        #
-        zip_iterator = zip(level_cols, level_cols_cleaned)
-        col_dict = dict(zip_iterator)
-        #
-        df = df.rename(columns=col_dict)
-        #
-        return df
-
+    
+    #@staticmethod
+    #def _clean_data_for_orderbook_level(
+    #    df: pd.DataFrame, level: int = 1
+    #) -> pd.DataFrame:
+    #    """
+    #    Specify the order book level in CCXT bid ask data.
+#
+    #    :param df: Data with multiple levels (e.g., bid_price_1, bid_price_2, etc.)
+    #    :return: Data where specific level has common name (i.e., bid_price)
+    #    """
+    #    level_cols = [col for col in df.columns if col.endswith(f"_{level}")]
+    #    level_cols_cleaned = [elem[:-2] for elem in level_cols]
+    #    #
+    #    zip_iterator = zip(level_cols, level_cols_cleaned)
+    #    col_dict = dict(zip_iterator)
+    #    #
+    #    df = df.rename(columns=col_dict)
+    #    #
+    #    return df
+    
+    
     @staticmethod
     def _resample_to_1sec(data: pd.DataFrame) -> pd.DataFrame:
         """
@@ -373,18 +386,19 @@ class RealTimeHistoricalReconciler:
         """
         Load and process real time data.
         """
-        # Load real time data from the database.
+        # Load real time data from the datab
+        # ase.
         ccxt_rt = self.ccxt_rt_im_client.read_data(
             self.universe, self.start_ts, self.end_ts, None, "assert"
         )
         ccxt_rt = ccxt_rt.reset_index()
-        if self.data_type == "bid_ask":
-            # CCXT timestamp data goes up to milliseconds, so one needs to round it to minutes.
-            ccxt_rt["timestamp"] = ccxt_rt["timestamp"].apply(
-                lambda x: x.round(freq="T")
-            )
-            # Choose the specific order level (first level by default).
-            ccxt_rt = self._clean_data_for_orderbook_level(ccxt_rt)
+        #if self.data_type == "bid_ask":
+        #    # CCXT timestamp data goes up to milliseconds, so one needs to round it to minutes.
+        #    ccxt_rt["timestamp"] = ccxt_rt["timestamp"].apply(
+        #        lambda x: x.round(freq="T")
+        #    )
+        #    # Choose the specific order level (first level by default).
+        #    ccxt_rt = self._clean_data_for_orderbook_level(ccxt_rt)
         # Remove duplicated columns and reindex real time data.
         _LOG.info("Filter duplicates in real time data")
         ccxt_rt = self._preprocess_data(ccxt_rt)
@@ -569,12 +583,8 @@ class RealTimeHistoricalReconciler:
         data = data.reindex(sorted(data.columns), axis=1)
         # NaNs observation.
         _LOG.info(
-            "Number of observations with NaNs in CryptoChassis = %s",
-            len(data[data["bid_price_cc"].isna()]),
-        )
-        _LOG.info(
-            "Number of observations with NaNs in CCXT = %s",
-            len(data[data["bid_price_ccxt"].isna()]),
+            "Number of observations with NaN in any of the columns.",
+            len(data[data.isna().any(axis=1)]),
         )
         _LOG.info(
             "Number of observations with NaNs for both vendors = %s",
