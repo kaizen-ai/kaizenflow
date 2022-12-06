333078c82b78db97ddadf1fc44519bddc2f9dd1b
Juraj Smeriga <49269742+jsmerix@users.noreply.github.com>
2022-12-06 20:52:18 +0100

    CmTask3342_Add_command_line_arguments_to_identify_datasets_to_the_downloaders (#3354)
    * Mark talos tests as skipped
    
    * Add new cmd line arguments and rename existing, reflect in tests
    
    * Refactor file_format -> data_format
    
    * Fix typo download_entity -> downloading_entity, add support for s3 path building from arguments
    
    * Add common downloader according to surrentum naming convention
    
    * Add bash drective
    
    * Remove dotted downloader file
    
    * Remove deprecated files
    
    * Rename token exchange to exchange_id
    
    * Changed token value pq -> parquet
    
    * Run linter
    
    * Format comment/docstring, fix typos
    
    Co-authored-by: Daniil Tikhomirov <d.tikhomirov@crypto-kaizen.com>

--- im_v2/talos/data/extract/test/test_talos_download_realtime_for_one_exchange.py
+++ im_v2/talos/data/extract/test/test_talos_download_realtime_for_one_exchange.py
@@ -5,17 +5,16 @@ import pandas as pd
 import pytest
 
 import helpers.hdbg as hdbg
-import helpers.henv as henv
 import helpers.hgit as hgit
 import helpers.hsystem as hsystem
 import helpers.hunit_test as hunitest
 import im_v2.talos.utils as imv2tauti
 
-
-@pytest.mark.skipif(
-    not henv.execute_repo_config_code("is_CK_S3_available()"),
-    reason="Run only if CK S3 is available",
-)
+# @pytest.mark.skipif(
+#    not henv.execute_repo_config_code("is_CK_S3_available()"),
+#    reason="Run only if CK S3 is available",
+# )
+@pytest.mark.skip(reason="Talos as a vendor is deprecated.")
 class TestDownloadRealtimeForOneExchangePeriodically1(hunitest.TestCase):
     @pytest.mark.superslow("~40 seconds.")
     def test_amount_of_downloads(self) -> None:
