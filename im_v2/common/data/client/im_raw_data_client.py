2029de0e07c6adc07b091fcb55e0d7865dabfbff
tamriq <tamriqua@gmail.com>
2022-12-07 14:59:03 +0000

    CmTask3331: thin client for im raw data gallery

---
+++ im_v2/common/data/client/im_raw_data_client.py
@@ -0,0 +1,167 @@
+"""
+Thin IM client for loading raw data.
+
+Import as:
+
+import im_v2.common.data.client.im_raw_data_client as imvcdcimrdc
+"""
+
+import logging
+from typing import Any, Dict
+
+import pandas as pd
+import pyarrow as pa
+from pyarrow.parquet import ParquetFile
+
+import data_schema.dataset_schema_utils as dsdascut
+import helpers.hdbg as hdbg
+import helpers.hsql_implementation as hsqlimpl
+import helpers.hs3 as hs3
+import helpers.hsql as hsql
+import im_v2.im_lib_tasks as imvimlita
+
+_LOG = logging.getLogger(__name__)
+
+
+class RawDataReader:
+    """
+    Load the raw data sample from S3 or the DB.
+    """
+
+    def __init__(self, signature: str):
+        """
+        Constructor.
+
+        :param signature: dataset signature,
+          e.g. `bulk.airflow.resampled_1min.pq.bid_ask.spot.v3.crypto_chassis.binance.v1_0_0`
+        """
+        # Validate signature schema.
+        dataset_schema = dsdascut.get_dataset_schema()
+        hdbg.dassert_eq(
+            dsdascut.validate_dataset_signature(signature, dataset_schema), True
+        )
+        self.args = self.parse_signature_to_args(signature, dataset_schema)
+
+    @staticmethod
+    def get_db_connection() -> hsqlimpl.DbConnection:
+        """
+        Connect to the DB.
+        """
+        # Get DB connection.
+        env_file = imvimlita.get_db_env_path("dev")
+        # Connect with the parameters from the env file.
+        connection_params = hsql.get_connection_info_from_env_file(env_file)
+        connection = hsql.get_connection(*connection_params)
+        return connection
+
+    @staticmethod
+    def parse_signature_to_args(
+        signature: str, dataset_schema: Dict[str, Any]
+    ) -> Dict[str, str]:
+        """
+        Parse signature string into key-value mapping according to the schema.
+
+        :param signature: dataset signature to parse,
+          e.g. `bulk.airflow.resampled_1min.pq.bid_ask.spot.v3.crypto_chassis.binance.v1_0_0`
+        :dataset_schema: dataset schema to parse against
+        """
+        token_separator = dataset_schema["token_separator_character"]
+        keys = dataset_schema["dataset_signature"].split(token_separator)
+        values = signature.split(token_separator)
+        args = {keys[i]: values[i] for i in range(len(keys))}
+        return args
+
+    def read_data(self) -> pd.DataFrame:
+        """
+        Load the data sample.
+        """
+        if self.args["download_mode"] == "bulk":
+            # Load the data from S3.
+            data = self.load_parquet()
+        else:
+            # Load the data from DB.
+            data = self.load_db_table()
+        return data
+
+    def load_parquet(self) -> pd.DataFrame:
+        """
+        Load the head of a sample parquet file.
+
+        Currently using `currency_pair=ETH_USDT/year=2022/month=11/data.parquet` location for each data type.
+        """
+        # Build s3 path.
+        s3_pq_file_path = self._build_s3_pq_file_path()
+        _LOG.info(f"Loading the data from `{s3_pq_file_path}` parquet file")
+        aws_profile = "ck"
+        s3fs_ = hs3.get_s3fs(aws_profile)
+        file = s3fs_.open(s3_pq_file_path, "rb")
+        # Load the data.
+        parquet_file = ParquetFile(file)
+        # Get the head of the data.
+        first_ten_rows = next(parquet_file.iter_batches(batch_size=10))
+        df = pa.Table.from_batches([first_ten_rows]).to_pandas()
+        return df
+
+    def load_db_table(self) -> pd.DataFrame:
+        """
+        Load the head of the DB table.
+        """
+        connection = self.get_db_connection()
+        db_name = self._build_db_table_name()
+        _LOG.info(f"Loading the data from `{db_name}` table")
+        query_head = f"SELECT * FROM {db_name} ORDER BY timestamp ASC LIMIT 10"
+        head = hsql.execute_query_to_df(connection, query_head)
+        return head
+
+    def _build_db_table_name(self) -> str:
+        """
+        Build the name of DB table according to the signature arguments.
+        """
+        hdbg.dassert_eq(
+            self.args["vendor"],
+            "ccxt",
+            "Crypto Chassis vendor is not represented in the DB",
+        )
+        vendor = "ccxt"
+        # Get asset type.
+        if self.args["asset_type"] == "futures":
+            data_type = f'{self.args["data_type"]}_futures'
+        else:
+            data_type = self.args["data_type"]
+        # Get action tag.
+        if self.args["action_tag"] == "downloaded_1sec":
+            action_tag = "raw"
+        elif self.args["action_tag"] == "resampled_1min":
+            action_tag = "resampled_1min"
+        else:
+            action_tag = ""
+        if action_tag:
+            db_name = f"{vendor}_{data_type}_{action_tag}"
+        else:
+            # E.g. `ccxt_ohlcv_futures`
+            db_name = f"{vendor}_{data_type}"
+        return db_name
+
+    def _build_s3_pq_file_path(self) -> str:
+        """
+        Get the path to the sample parquet file.
+
+        TODO(Toma): this is a temporary solution, clean it up after data restructuring.
+        """
+        # Use the hardcoded base URL.
+        s3_path_base = "s3://cryptokaizen-data/reorg/daily_staged.airflow.pq"
+        # Use the specific parquet file to load the sample of the data from.
+        pq_file_common_path = (
+            "currency_pair=ETH_USDT/year=2022/month=11/data.parquet"
+        )
+        # Get the full data type, e.g. `bid_ask` or `bid_ask-futures.`
+        if self.args["asset_type"] == "futures":
+            data_type = f'{self.args["data_type"]}-futures'
+        else:
+            data_type = self.args["data_type"]
+        if self.args["vendor"] == "crypto_chassis":
+            vendor = f'{self.args["vendor"]}.{self.args["action_tag"]}'
+        else:
+            vendor = self.args["vendor"]
+        s3_path = f'{s3_path_base}/{data_type}/{vendor}/{self.args["exchange"]}/{pq_file_common_path}'
+        return s3_path
--- im_v2/common/notebooks/Master_raw_data_gallery.py
+++ im_v2/common/notebooks/Master_raw_data_gallery.py
@@ -45,7 +45,7 @@ import helpers.henv as henv
 import helpers.hpandas as hpandas
 import helpers.hparquet as hparque
 import helpers.hprint as hprint
-from im_v2.common.notebooks.master_raw_data_gallery_lib import *
+import im_v2.common.data.client.im_raw_data_client as imvcdcimrdc
 
 # %%
 hdbg.init_logger(verbosity=logging.INFO)
@@ -61,64 +61,60 @@ hprint.config_notebook()
 # # Realtime (the DB data)
 
 # %% [markdown]
-# ## periodic.airflow.downloaded_1hour.csv.ohlcv.futures.1_min.universe_v7_0.ccxt.binance.v1_0
+# ## periodic_daily.airflow.downloaded_1min.csv.ohlcv.futures.v7.ccxt.binance.v1_0_0
 
 # %%
-# Get the real time data from DB.
-ccxt_rt = get_raw_data_from_db(
-    "ccxt_ohlcv_futures", "binance", start_ts=None, end_ts=None
-)
-_LOG.log(log_level, hpandas.df_to_str(ccxt_rt, log_level=log_level))
+signature = "periodic_daily.airflow.downloaded_1min.csv.ohlcv.futures.v7.ccxt.binance.v1_0_0"
+reader = imvcdcimrdc.RawDataReader(signature)
+data = reader.read_data()
+_LOG.log(log_level, hpandas.df_to_str(data, log_level=log_level))
 
 # %% [markdown]
 # # Historical (data updated daily)
 
 # %% [markdown]
-# ## bulk.airflow.downloaded_EOD.parquet.ohlcv.futures.1_min.universe_v7_0.ccxt.binance.v1_0
+# ## bulk.airflow.downloaded_1min.pq.ohlcv.futures.v7.ccxt.binance.v1_0_0
 
 # %%
-s3_path = "s3://cryptokaizen-data/reorg/daily_staged.airflow.pq/ohlcv-futures/ccxt/binance"
-# Load daily data from s3 parquet.
-ccxt_futures_daily = hparque.from_parquet(s3_path, aws_profile="ck")
-_LOG.log(log_level, hpandas.df_to_str(ccxt_futures_daily, log_level=log_level))
+signature = "bulk.airflow.downloaded_1min.pq.ohlcv.futures.v7.ccxt.binance.v1_0_0"
+reader = imvcdcimrdc.RawDataReader(signature)
+data = reader.read_data()
+_LOG.log(log_level, hpandas.df_to_str(data, log_level=log_level))
 
 # %% [markdown]
-# ## bulk.airflow.downloaded_EOD.parquet.bid_ask.futures.1_sec.universe_v3_0.crypto_chassis.binance.v1_0
-
-# %% [markdown]
-# The amount of data is too big to process it all at once, so the data will be loaded separately for each month.
+# ## bulk.airflow.downloaded_1sec.pq.bid_ask.futures.v3.crypto_chassis.binance.v1_0_0
 
 # %%
-s3_path = "s3://cryptokaizen-data/reorg/daily_staged.airflow.pq/bid_ask-futures/crypto_chassis/binance"
-start_ts = "20220627-000000"
-end_ts = "20221130-000000"
-process_s3_data_in_chunks(start_ts, end_ts, s3_path, 3)
+signature = "bulk.airflow.downloaded_1sec.pq.bid_ask.futures.v3.crypto_chassis.binance.v1_0_0"
+reader = imvcdcimrdc.RawDataReader(signature)
+data = reader.read_data()
+_LOG.log(log_level, hpandas.df_to_str(data, log_level=log_level))
 
 # %% [markdown]
-# ## bulk.airflow.downloaded_EOD.parquet.bid_ask.futures.1_min.universe_v3_0.crypto_chassis.binance.v1_0
+# ## bulk.airflow.resampled_1min.pq.bid_ask.futures.v3.crypto_chassis.binance.v1_0_0
 
 # %%
-s3_path = "s3://cryptokaizen-data/reorg/daily_staged.airflow.pq/bid_ask-futures/crypto_chassis.resampled_1min/binance"
-# Load daily data from s3 parquet.
-cc_ba_futures_resampled = hparque.from_parquet(s3_path, aws_profile="ck")
-_LOG.log(
-    log_level, hpandas.df_to_str(cc_ba_futures_resampled, log_level=log_level)
-)
+signature = "bulk.airflow.resampled_1min.pq.bid_ask.futures.v3.crypto_chassis.binance.v1_0_0"
+reader = imvcdcimrdc.RawDataReader(signature)
+data = reader.read_data()
+_LOG.log(log_level, hpandas.df_to_str(data, log_level=log_level))
 
 # %% [markdown]
-# ## bulk.airflow.downloaded_EOD.parquet.bid_ask.spot.1_sec.universe_v3.crypto_chassis.binance.v1_0
+# ## bulk.airflow.downloaded_1sec.pq.bid_ask.spot.v3.crypto_chassis.binance.v1_0_0
 
 # %%
-s3_path = "s3://cryptokaizen-data/reorg/daily_staged.airflow.pq/bid_ask/crypto_chassis/binance"
-start_ts = "20220501-000000"
-end_ts = "20221130-000000"
-process_s3_data_in_chunks(start_ts, end_ts, s3_path, 3)
+signature = "bulk.airflow.downloaded_1sec.pq.bid_ask.spot.v3.crypto_chassis.binance.v1_0_0"
+reader = imvcdcimrdc.RawDataReader(signature)
+data = reader.read_data()
+_LOG.log(log_level, hpandas.df_to_str(data, log_level=log_level))
 
 # %% [markdown]
-# ## bulk.airflow.downloaded_EOD.parquet.bid_ask.spot.1_min.universe_v3_0.crypto_chassis.binance.v1_0
+# ## bulk.airflow.resampled_1min.pq.bid_ask.spot.v3.crypto_chassis.binance.v1_0_0
 
 # %%
-s3_path = "s3://cryptokaizen-data/reorg/daily_staged.airflow.pq/bid_ask/crypto_chassis.resampled_1min/binance"
-# Load daily data from s3 parquet.
-cc_ba_spot_resampled = hparque.from_parquet(s3_path, aws_profile="ck")
-_LOG.log(log_level, hpandas.df_to_str(cc_ba_spot_resampled, log_level=log_level))
+signature = "bulk.airflow.downloaded_1sec.pq.bid_ask.spot.v3.crypto_chassis.binance.v1_0_0"
+reader = imvcdcimrdc.RawDataReader(signature)
+data = reader.read_data()
+_LOG.log(log_level, hpandas.df_to_str(data, log_level=log_level))
+
+# %%
