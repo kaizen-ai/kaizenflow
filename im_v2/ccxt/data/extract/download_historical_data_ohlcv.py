9f3472ab2068ce363ffeaad43b5264de7729fa57
vlady <v.yakovenko@crypto-kaizen.com>
2022-12-07 18:16:03 +0000

    CmTask3324: changes related to PR comments

--- im_v2/ccxt/data/extract/download_historical_data_ohlcv.py
+++ im_v2/ccxt/data/extract/download_historical_data_ohlcv.py
@@ -5,78 +5,68 @@ Download historical data from Binance and save it as CSV locally.
 Use as:
 # Download OHLCV data for binance:
 > im_v2/ccxt/data/extract/download_historical_data_ohlcv.py \
-    --start_timestamp 1670350563000 \
-    --end_timestamp 1670352003000 \
+    --start_timestamp '2022-02-09 10:00:00' \
+    --end_timestamp '2022-02-19' \
     --output_file test1.csv
 """
 import argparse
 import dataclasses
+import csv
 import logging
 from typing import Tuple, Generator
 
 import requests
-import csv
+import pandas as pd
+
+import helpers.hdbg as hdbg
+import helpers.hdatetime as hdateti
 
 
 _LOG = logging.getLogger(__name__)
+
 BASE_URL = "https://api.binance.com/api/v3/klines"
 DEFAULT_INTERVAL = "1m"
 MAX_LINES = 1000
 UNIVERSE = {
-    "CCXT": {
-        "binance": [
-            "ETH_USDT",
-            "BTC_USDT",
-            "SAND_USDT",
-            "ETH_BUSD",
-            "BTC_BUSD",
-            "STORJ_USDT",
-            "GMT_USDT",
-            "AVAX_USDT",
-            "BNB_USDT",
-            "APE_USDT",
-            "MATIC_USDT",
-            "DYDX_USDT",
-            "DOT_USDT",
-            "UNFI_USDT",
-            "LINK_USDT",
-            "XRP_USDT",
-            "CRV_USDT",
-            "RUNE_USDT",
-            "BAKE_USDT",
-            "NEAR_USDT",
-            "FTM_USDT",
-            "WAVES_USDT",
-            "AXS_USDT",
-            "OGN_USDT",
-            "DOGE_USDT",
-            "SOL_USDT",
-            "CTK_USDT"
-        ]
-    }
+    "binance": [
+        "ETH_USDT",
+        "BTC_USDT",
+        "SAND_USDT",
+        "ETH_BUSD",
+        "BTC_BUSD",
+        "STORJ_USDT",
+        "GMT_USDT",
+        "AVAX_USDT",
+        "BNB_USDT",
+        "APE_USDT",
+        "MATIC_USDT",
+        "DYDX_USDT",
+        "DOT_USDT",
+        "UNFI_USDT",
+        "LINK_USDT",
+        "XRP_USDT",
+        "CRV_USDT",
+        "RUNE_USDT",
+        "BAKE_USDT",
+        "NEAR_USDT",
+        "FTM_USDT",
+        "WAVES_USDT",
+        "AXS_USDT",
+        "OGN_USDT",
+        "DOGE_USDT",
+        "SOL_USDT",
+        "CTK_USDT"
+    ]
 }
-
-
-@dataclasses.dataclass
-class OHLCVRow:
-    symbol: str
-    open_time: int
-    open: float
-    high: float
-    low: float
-    close: float
-    volume: float
-
-    def __init__(self, symbol: str, input_list: list):
-        self.symbol = symbol
-        self.open_time = input_list[0]
-        self.open = input_list[1]
-        self.high = input_list[2]
-        self.low = input_list[3]
-        self.close = input_list[4]
-        self.volume = input_list[5]
-
-
+OHLCV_HEADERS = [
+    "symbol",
+    "open_time",
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume"
+]
 
 def _build_url(
         start_time: int,
@@ -99,14 +89,14 @@ def add_download_args(
         "--start_timestamp",
         required=True,
         action="store",
-        type=int,
+        type=str,
         help="Beginning of the downloaded period",
     )
     parser.add_argument(
         "--end_timestamp",
         action="store",
         required=True,
-        type=int,
+        type=str,
         help="End of the downloaded period",
     )
     parser.add_argument(
@@ -147,14 +137,22 @@ def _main(parser: argparse.ArgumentParser) -> None:
     headers = {
         'Content-Type': 'application/json'
     }
+    # Convert timestamps.
+    start_timestamp = pd.Timestamp(args.start_timestamp)
+    start_timestamp_as_unix = hdateti.convert_timestamp_to_unix_epoch(
+        start_timestamp
+    )
+    end_timestamp = pd.Timestamp(args.end_timestamp)
+    end_timestamp_as_unix = hdateti.convert_timestamp_to_unix_epoch(
+        end_timestamp
+    )
     with open(args.output_file, 'w') as output_file:
-        csv_headers = list(OHLCVRow.__annotations__)
-        writer = csv.DictWriter(output_file, fieldnames=csv_headers)
+        writer = csv.DictWriter(output_file, fieldnames=OHLCV_HEADERS)
         writer.writeheader()
-        for symbol in UNIVERSE['CCXT']['binance']:
+        for symbol in UNIVERSE['binance']:
             for start_time, end_time in _split_period_to_days(
-                start_time=args.start_timestamp,
-                end_time=args.end_timestamp
+                start_time=start_timestamp_as_unix,
+                end_time=end_timestamp_as_unix
             ):
                 url = _build_url(
                     start_time=start_time,
@@ -168,15 +166,22 @@ def _main(parser: argparse.ArgumentParser) -> None:
                     headers=headers,
                     data={}
                 )
-                assert response.status_code == 200
-                writer.writerows(
+                hdbg.dassert_eq(response.status_code, 200)
+                data = pd.DataFrame(
                     [
-                        dataclasses.asdict(
-                            OHLCVRow(symbol=symbol, input_list=row)
-                        )
+                        {
+                            "symbol": symbol,
+                            "open_time": row[0],
+                            "open": row[1],
+                            "high": row[2],
+                            "low": row[3],
+                            "close": row[4],
+                            "volume": row[5]
+                        }
                         for row in response.json()
                     ]
                 )
+                data.to_csv(args.output_file, index=False)
 
 
 if __name__ == "__main__":
