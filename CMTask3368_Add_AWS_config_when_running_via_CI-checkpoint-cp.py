2991474a26583f8f59e63073993484445aac3846
Nina Lee <nina.tru94@gmail.com>
2022-12-11 17:02:22 +0000

    checkpoint

--- CMTask3368_Add_AWS_config_when_running_via_CI-checkpoint-cp.py
+++ CMTask3368_Add_AWS_config_when_running_via_CI-checkpoint-cp.py
@@ -22,8 +22,11 @@ import helpers.hs3 as hs3
 
 
 # %%
-def _get_credentials_txt(config: configparser.RawConfigParser, secret_keys: List) -> str:
-    aws_profiles = ["am", "ck"]
+def _generate_config_txt(config: configparser.RawConfigParser, secret_keys: List) -> str:
+    if "region" in secret_keys:
+        aws_profiles = ["profile am", "profile ck"]
+    else:
+        aws_profiles = ["am", "ck"]
     envs = {}
     for profile in aws_profiles:
         tmp = {
@@ -37,56 +40,41 @@ def _get_credentials_txt(config: configparser.RawConfigParser, secret_keys: List
     for profile in aws_profiles:
         for k, v in zip(envs[profile].keys(), envs[profile].values()):
             creds.append("=".join([k, v]))
-    am_creds = "[am]\n".join(creds[:3])
-    ck_creds = "[ck]\n".join(creds[3:])
+    am_creds = "\n".join(creds[:3])
+    am_creds = f"[{aws_profiles[0]}]\n" + am_creds
+    ck_creds = "\n".join(creds[3:])
+    ck_creds = f"[{aws_profiles[1]}]\n" + ck_creds
     txt = "\n\n".join([am_creds, ck_creds])
     return txt
 
 
-def _get_config_txt(config):
-    aws_profiles = ["am", "ck"]
-    envs = {}
-    for profile in aws_profiles:
-        tmp = {
-            profile: {},
-        }
-        for var in config_vars:
-            v = config.get(profile, var)
-            tmp[profile].update({var: v})
-        envs.update(tmp)
-    creds = []
-    for profile in aws_profiles:
-        for k, v in zip(envs[profile].keys(), envs[profile].values()):
-            creds.append("=".join([k, v]))
-    am_creds = "[am]\n".join(creds[:1])
-    ck_creds = "[ck]\n".join(creds[1:])
-    txt = "\n\n".join([am_creds, ck_creds])
-    return txt
-    
-    
 def generate_aws_config() -> None:
-    config_file_path = "~/.aws/config"
-    credentials_file_path = "~/.aws/credentials"
+    config_file_path = ".aws/config"
+    credentials_file_path = ".aws/credentials"
     if os.path.exists(config_file_path) or os.path.exists(credentials_file_path):
         return
     # Get config values to fill "~/.aws/config" file.
-    config_file_name = config_file_path.split("/")[-1]
-    aws_config = hs3._get_aws_config(config_file_name)
-    config_keys = ["region"]
-    txt = _get_config_txt(aws_config, config_keys)
+    file_name = config_file_path.split("/")[-1]
+    credentials = hs3._get_aws_config(file_name)
+    secret_keys = ["region"]
+    txt = _generate_config_txt(credentials, secret_keys)
     # Create config file.
     hio.to_file(config_file_path, txt)
     # Get credentials values to fill "~/.aws/credentials" file.
-    credentials_file_name = credentials_file_path.split("/")[-1]
-    credentials_keys = [
+    file_name = credentials_file_path.split("/")[-1]
+    secret_keys = [
         "aws_access_key_id",
         "aws_secret_access_key",
         "aws_s3_bucket",
     ]
-    credentials_aws_config = hs3._get_aws_config(credentials_file_name)
-    txt = _get_credentials_txt(credentials_aws_config, credentials_keys)    
+    credentials = hs3._get_aws_config(file_name)
+    txt = _generate_config_txt(credentials, secret_keys)    
     # Create credentials file.
     hio.to_file(credentials_file_path, txt)
 
 
 # %%
+generate_aws_config()
+
+# %%
+# !ls  ~/.aws/
