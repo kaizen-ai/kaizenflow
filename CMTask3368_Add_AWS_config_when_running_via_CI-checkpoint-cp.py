9b9cc77652e5a8a48dd63b83a627aaf8b0cc7e9e
Nina Lee <nina.tru94@gmail.com>
2022-12-11 15:55:33 +0000

    checkpoint

--- CMTask3368_Add_AWS_config_when_running_via_CI-checkpoint-cp.py
+++ CMTask3368_Add_AWS_config_when_running_via_CI-checkpoint-cp.py
@@ -18,6 +18,17 @@ import os
 import helpers.hs3 as hs3
 
 # %%
+l = ["[am]", "a", "b", "c", "[ck]", "d", "e", "f"]
+for i in range(4, 9, 4):
+    if i == 4:
+        am = "\n".join(l[:i+1])
+        print(am)
+    else:
+        ck = "\n".join(l[i:])
+        print(ck)
+
+
+# %%
 creds = []
 for profile in aws_profiles:
     tmp = {profile: []}
@@ -50,7 +61,9 @@ def _get_credentials_txt(config) -> str:
     for profile in aws_profiles:
         for k, v in zip(envs[profile].keys(), envs[profile].values()):
             creds.append("=".join([k, v]))
-    txt = "\n".join(creds)
+    am_creds = "[am]\n".join(creds[:4])
+    am_creds = "[ck]\n".join(creds[4:])
+    txt = "\n\n".join([am_creds, ck8_creds])
     return txt
 
 
@@ -68,7 +81,7 @@ def generate_aws_config():
     #
     # Get credentials values to fill "~/.aws/credentials" file.
     credentials_file_name = credentials_file_path.split("/")[-1]
-    credentials_config = hs3._get_aws_config(credentials_file_name)
+    credentials_aws_config = hs3._get_aws_config(credentials_file_name)
     txt = _get_credentials_txt(credentials_config)
     # Here will be a creation of two files.
 
