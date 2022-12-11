a43c7885dbe838d2bf013a722629b28284bf8a22
Nina Lee <nina.tru94@gmail.com>
2022-12-11 16:08:11 +0000

    checkpoint

--- CMTask3368_Add_AWS_config_when_running_via_CI-checkpoint-cp.py
+++ CMTask3368_Add_AWS_config_when_running_via_CI-checkpoint-cp.py
@@ -52,37 +52,54 @@ def _get_credentials_txt(config) -> str:
             profile: {},
         }
         for var in credentials_vars:
-            config.get(profile, var)
+            v = config.get(profile, var)
             tmp[profile].update({var: v})
         envs.update(tmp)
     creds = []
-    # TODO(Nina): Factor out this piece of code to make created
-    # file look like as original one. Look the output in the cell above.
     for profile in aws_profiles:
         for k, v in zip(envs[profile].keys(), envs[profile].values()):
             creds.append("=".join([k, v]))
-    am_creds = "[am]\n".join(creds[:4])
-    am_creds = "[ck]\n".join(creds[4:])
-    txt = "\n\n".join([am_creds, ck8_creds])
+    am_creds = "[am]\n".join(creds[:3])
+    ck_creds = "[ck]\n".join(creds[3:])
+    txt = "\n\n".join([am_creds, ck_creds])
     return txt
 
 
+def _get_config_txt(config):
+    config_vars = ["region"]
+    aws_profiles = ["am", "ck"]
+    envs = {}
+    for profile in aws_profiles:
+        tmp = {
+            profile: {},
+        }
+        for var in config_vars:
+            v = config.get(profile, var)
+            tmp[profile].update({var: v})
+        envs.update(tmp)
+    creds = []
+    for profile in aws_profiles:
+        for k, v in zip(envs[profile].keys(), envs[profile].values()):
+            creds.append("=".join([k, v]))
+    am_creds = "[am]\n".join(creds[:1])
+    ck_creds = "[ck]\n".join(creds[1:])
+    txt = "\n\n".join([am_creds, ck_creds])
+    return txt
+    
+    
 def generate_aws_config():
     config_file_path = "~/.aws/config"
     credentials_file_path = "~/.aws/credentials"
     if os.path.exists(config_file_path) or os.path.exists(credentials_file_path):
         return
-    # Get config values to fill "~/.aws/credentials" file.
+    # Get config values to fill "~/.aws/config" file.
     config_file_name = config_file_path.split("/")[-1]
-    config_vars = ["region"]
-    # Here will be a call of a function to create txt for config.
-    # It's going to be mostly the same as `_get_credentials_txt`
-    # so need to share the common code or unify "_get_credentials_txt".
-    #
+    aws_config = hs3._get_aws_config(config_file_name)
+    txt = _get_config_txt(aws_config)
     # Get credentials values to fill "~/.aws/credentials" file.
     credentials_file_name = credentials_file_path.split("/")[-1]
     credentials_aws_config = hs3._get_aws_config(credentials_file_name)
-    txt = _get_credentials_txt(credentials_config)
+    txt = _get_credentials_txt(credentials_aws_config)
     # Here will be a creation of two files.
 
 
