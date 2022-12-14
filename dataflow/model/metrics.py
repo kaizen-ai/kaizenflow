a53da1239809e43c108c2dad622267408dc78635
Nina Lee <nina.tru94@gmail.com>
2022-12-14 17:38:32 +0000

    checkpoint

--- dataflow/model/metrics.py
+++ dataflow/model/metrics.py
@@ -10,7 +10,11 @@ import helpers.hdbg as hdbg
 
 
 def convert_to_metrics_format(
-    predict_df: pd.DataFrame, *, asset_id_column_name: str = "asset_id"
+    predict_df: pd.DataFrame,
+    y_column_name: str,
+    y_hat_column_name: str,
+    *,
+    asset_id_column_name: str = "asset_id"
 ) -> pd.DataFrame:
     """
     Transform a predict_df (i.e. output of DataFlow pipeline) into a
@@ -23,9 +27,13 @@ def convert_to_metrics_format(
        - the outermost corresponding to "feature" (which can be inputs, outputs,
            and internal nodes)
 
-    # TODO(Grisha): @Nina add a snippet.
     E.g.,
     ```
+                              close                            ... close.ret_0
+                              1030828978 1182743717 1464553467 ... 1030828978 1182743717 1464553467
+    end_ts
+    2022-08-31 20:00:00-04:00  NaN        NaN        NaN       ...  NaN        NaN        NaN
+    2022-08-31 20:05:00-04:00  NaN        NaN        NaN       ...  NaN        NaN        NaN
     ...
     ```
 
@@ -33,15 +41,24 @@ def convert_to_metrics_format(
        - is indexed by the pair (end timestamp, asset id)
        - has all the features as columns
 
-    # TODO(Grisha): @Nina add a snippet.
     E.g.,
     ```
+                                            close        ...  close.ret_0
+    end_ts                      asset_id
+    2022-08-31 20:00:00-04:00   1030828978  0.6696       ...  NaN
+                                1182743717  20016.4000   ...  NaN
+                                1464553467  1551.9500    ...  NaN
     ...
     ```
     """
     hdbg.dassert_eq(2, len(predict_df.columns.levels))
-    # TODO(Grisha): @Nina drop rows where y or y_hat is NaN.
+    # Drop NaNs.
     metrics_df = predict_df.stack()
+    condition = (metrics_df[y_column_name].isna()) | (
+        metrics_df[y_hat_column_name].isna()
+    )
+    metrics_df = metrics_df[condition]
+    #
     metrics_df.index.names = [metrics_df.index.names[0], asset_id_column_name]
     hdbg.dassert_eq(2, len(metrics_df.index.levels))
     return metrics_df
