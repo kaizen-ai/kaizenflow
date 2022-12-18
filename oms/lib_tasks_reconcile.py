440beb0f4c38e5b5af7bb677bef0de56cc5d9401
PomazkinG <pomazking@gmail.com>
2022-12-18 18:05:10 +0000

    checkpoint

--- oms/lib_tasks_reconcile.py
+++ oms/lib_tasks_reconcile.py
@@ -102,9 +102,10 @@ def _resolve_target_dir(
     Return the target dir name to store reconcilation results.
 
     If a dir name is not specified by a user then use prod reconcilation
-    dir on the shared disk with the corresponding run date subdir.
+    dir on the shared disk with the corresponding dag_builder_name and
+    run date subdirs.
 
-    E.g., "/shared_data/prod_reconciliation/20221101".
+    E.g., "/shared_data/prod_reconciliation/C1b/20221101".
 
     # TODO(Grisha): use `root_dir` everywhere, for a date specific dir use `dst_dir`.
     :param start_timestamp_as_str: string representation of the reconcile
@@ -377,7 +378,9 @@ def reconcile_copy_prod_data(
     hdbg.dassert_in(stage, ("local", "test", "preprod", "prod"))
     hdbg.dassert_in(mode, ("scheduled", "manual"))
     if prod_data_source_dir is None:
-        prod_data_source_dir = f"/shared_data/ecs/{stage}/system_reconciliation/{dag_builder_name}"
+        run_date = omreconc.get_run_date(start_timestamp_as_str)
+        # TODO(Grisha): use `os.path.join` instead of string concationation.
+        prod_data_source_dir = f"/shared_data/ecs/{stage}/system_reconciliation/{dag_builder_name}/{run_date}"
     hs3.dassert_path_exists(prod_data_source_dir, aws_profile)
     _ = ctx
     target_dir = _resolve_target_dir(
@@ -687,19 +690,21 @@ def reconcile_run_all(
         dst_dir=dst_dir,
         prevent_overwriting=prevent_overwriting,
     )
-    reconcile_dump_tca_data(
-        ctx,
-        dag_builder_name,
-        start_timestamp_as_str=start_timestamp_as_str,
-        dst_dir=dst_dir,
-        prevent_overwriting=prevent_overwriting,
-    )
+    # TODO(Grisha): do we need TSA?
+    # reconcile_dump_tca_data(
+    #     ctx,
+    #     dag_builder_name,
+    #     start_timestamp_as_str=start_timestamp_as_str,
+    #     dst_dir=dst_dir,
+    #     prevent_overwriting=prevent_overwriting,
+    # )
     #
     if run_notebook:
         reconcile_run_notebook(
             ctx,
             dag_builder_name,
             start_timestamp_as_str=start_timestamp_as_str,
+            end_timestamp_as_str=end_timestamp_as_str,
             dst_dir=dst_dir,
             prevent_overwriting=prevent_overwriting,
         )
