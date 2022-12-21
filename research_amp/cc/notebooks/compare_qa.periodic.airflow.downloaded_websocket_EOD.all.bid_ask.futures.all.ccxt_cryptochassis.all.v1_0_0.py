0f703cb97858417d4562035fc7ea5ffb831c791e
vlady <v.yakovenko@crypto-kaizen.com>
2022-12-21 09:34:09 +0000

    CmTask3235: remove exchange_id cleanup

--- research_amp/cc/notebooks/compare_qa.periodic.airflow.downloaded_websocket_EOD.all.bid_ask.futures.all.ccxt_cryptochassis.all.v1_0_0.py
+++ research_amp/cc/notebooks/compare_qa.periodic.airflow.downloaded_websocket_EOD.all.bid_ask.futures.all.ccxt_cryptochassis.all.v1_0_0.py
@@ -208,10 +208,6 @@ if resample_1min:
     # TODO(Danya): Function as-is has VWAP and TWAP modes and removes the `full_symbol` column.
     ccxt_df = imvcdttrut.resample_bid_ask_data_to_1min(ccxt_df, mode="VWAP")
     cc_df = imvcdttrut.resample_bid_ask_data_to_1min(ccxt_df, mode="VWAP")
-    #
-    ccxt_df = ccxt_df.drop("exchange_id", axis=1)
-    cc_df = cc_df.drop("exchange_id", axis=1)
-
 # %% [markdown]
 # # Analysis
 
