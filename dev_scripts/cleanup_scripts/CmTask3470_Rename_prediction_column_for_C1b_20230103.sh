3fb86f0cc45836e90ba951558d8e9537738dfdeb
Nina Lee <68428519+rheenina@users.noreply.github.com>
2023-01-04 19:20:28 +0500

    Cm task3470 rename prediction column for c1b (#3471)
    * CmTask3470_Rename_prediction_column_for_C1b
    
    * checkpoint
    
    * checkpoint
    
    * rename

---
+++ dev_scripts/cleanup_scripts/CmTask3470_Rename_prediction_column_for_C1b_20230103.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+dir_names="amp/dev_scripts/cleanup_scripts dev_scripts/cleanup_scripts"
+
+replace_text.py \
+    --old "vwap.ret_0.vol_adj_2_hat" \
+    --new "vwap.ret_0.vol_adj.lag_-2.hat" \
+    --exclude_dirs "$dir_names"
