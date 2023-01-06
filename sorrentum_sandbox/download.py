babbe72739b7cad4f596f10782f0b9ae7a6980ce
Vladimir Yakovenko <v.yakovenko@sxope.com>
2023-01-06 19:03:18 +0700

    CmTask3458: fix missed import

--- sorrentum_sandbox/download.py
+++ sorrentum_sandbox/download.py
@@ -5,7 +5,7 @@ import sorrentum_sandbox.download as sinsadow
 """
 
 import abc
-from typing import Any
+from typing import Any, Optional
 
 import pandas as pd
 # #############################################################################
