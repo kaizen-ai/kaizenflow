92c1b8e399079cf7fba9cb9cc4264547268bce10
cpuerta-reagg <49662397+psweet@users.noreply.github.com>
2023-02-27 11:22:36 -0500

    Python Objects for Alpha Vantage
    Ticker class now can initialize empty and use class methods to load data directly from API.

--- alpha_vantage/main.py
+++ alpha_vantage/main.py
@@ -1,7 +1,11 @@
-from api import AlphaVantage
+from models.ticker import Ticker
+from models.time_series import TimeInterval, DataType
 
-AlphaVantage.get_intraday_for("AAPL")
-# AlphaVantage.get_daily_for("AAPL")
-# AlphaVantage.get_weekly_for("AAPL")
-# AlphaVantage.get_monthly_for("AAPL")
-# AlphaVantage.get_sentiment_for("AAPL")
+# First initilize the class stock you want.
+# Setting get_name to True burns an extra API call.
+Apple = Ticker("AAPL", get_name=True)
+
+# Then request the data you want using the DataType ENUM. Time Interval is only used for intraday.
+Apple.get_data(data_type=DataType.INTRADAY, time_interval=TimeInterval.THIRTY)
+
+print(Apple)
---
+++ alpha_vantage/models/ticker.py
@@ -0,0 +1,58 @@
+from typing import List
+from api import AlphaVantage
+from models.time_series import TimeSeriesData, TimeInterval, DataType
+
+class Ticker:
+    def __init__(
+                self,
+                ticker: str,
+                get_name: bool = False,
+                time_series_data: List[TimeSeriesData] = None
+                ) -> None:
+        self.ticker = ticker
+
+        self.name = ticker
+        if get_name:
+            self.name = AlphaVantage.get_name_for(ticker)
+
+        self.time_series_data = time_series_data
+        
+        self.last_updated = None
+        self.last_open = None
+        self.last_close = None
+
+        if self.time_series_data:
+            last = self.time_series_data[0]
+
+            self.last_updated = last.date
+            self.last_open = last.open
+            self.last_close = last.close
+    
+    def __repr__(self) -> str:
+        return f"""
+        Name: {self.name} | {self.ticker}
+        Last Updated: {self.last_updated}
+        Last Open: {self.last_open}
+        Last Close: {self.last_close}
+        Datapoints: {len(self.time_series_data) if self.time_series_data else "No Data"}
+        """
+    
+    def get_data(self, data_type: DataType, time_interval: TimeInterval = None):
+        """
+        Requests and loads the specified data type using Alpha Vantage.
+
+        Parameters:
+        data_type: DataType - Data type requested (eg. DataType.DAILY)
+        """
+        fn = AlphaVantage.get_method(data_type)
+        self.time_series_data = fn(self.ticker, interval=time_interval)
+        self.update()
+
+    def update(self):
+        """Updates object attributes based on latest information if available."""
+        if self.time_series_data:
+            last = self.time_series_data[0]
+
+            self.last_updated = last.date
+            self.last_open = last.open
+            self.last_close = last.close
