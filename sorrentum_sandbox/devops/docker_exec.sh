bbefe0da92783b2f2873ae4433f7fc2e0e968fc9
saggese <saggese@gmail.com>
2023-03-05 10:03:26 -0500

    Reorg files

--- sorrentum_sandbox/devops/docker_exec.sh
+++ sorrentum_sandbox/devops/docker_exec.sh
@@ -3,4 +3,4 @@
 IMAGE_NAME="airflow_cont"
 
 CONTAINER_ID=$(docker container ls | grep $IMAGE_NAME | awk '{print $1}')
-docker exec -it $CONTAINER_ID bash
+docker exec -u postgres -it $CONTAINER_ID bash
