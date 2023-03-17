3ef7577d39de767e221ad9b83bba3d646d19ba17
samarth9008 <samarthpatel90082gmail.com>
2023-03-17 18:09:57 -0400

    TimeLock Test Scripts

--- defi/tokens/test/test_time_lock.py
+++ defi/tokens/test/test_time_lock.py
@@ -2,15 +2,14 @@ from brownie import MockERC20, TokenTimelock, accounts, reverts, chain
 import pytest
 
 
-
-@pytest.fixture(scope="module")
-def mock_token(MockERC20, accounts):
-    return MockERC20.deploy("Mock Token", "MCK", 100,1000, {'from': accounts[0], "gas_price": "60 gwei"})
-
-
 @pytest.fixture(scope="module")
 def time_lock(mock_token, TokenTimelock, accounts):
     return TokenTimelock.deploy(mock_token.address, {"from": accounts[0], "gas_price": "60 gwei"})
+
+
+@pytest.fixture(scope="module")
+def mock_token(MockERC20, accounts):
+    return MockERC20.deploy("Mock Token", "MCK", 1000,10000, {'from': accounts[1], "gas_price": "60 gwei"})
 
 
 @pytest.fixture(autouse=True)
@@ -19,7 +18,7 @@ def shared_setup(fn_isolation):
 
 
 def test_addParticipant_requireAmount(time_lock, accounts): 
-    participant = accounts[1]
+    participant = accounts[2]
     amount = 10000
     release_TimeStamp =  1677064547
     time_lock.addParticipant(participant, amount, release_TimeStamp, {"from": accounts[0], "gas_price": "60 gwei"})
@@ -28,7 +27,7 @@ def test_addParticipant_requireAmount(time_lock, accounts):
     
 
 def test_addParticipant(time_lock, accounts): 
-    participant = accounts[1]
+    participant = accounts[2]
     amount = 10000
     release_TimeStamp =  1677064547
     time_lock.addParticipant(participant, amount, release_TimeStamp, {"from": accounts[0], "gas_price": "60 gwei"})
@@ -39,13 +38,13 @@ def test_addAmount_requireAmount(time_lock, accounts):
         time_lock.addAmount(accounts[1], 1000, {"from": accounts[0], "gas_price": "60 gwei"})   
 
 def test_addAmount(time_lock, accounts):
-    participant = accounts[1]
+    participant = accounts[2]
     amount = 10000
     release_TimeStamp =  1677064547
     time_lock.addParticipant(participant, amount, release_TimeStamp, {"from": accounts[0], "gas_price": "60 gwei"})
     new_amount = 1000
     final_amount = amount+new_amount
-    time_lock.addAmount(accounts[1], new_amount, {"from": accounts[0], "gas_price": "60 gwei"})
+    time_lock.addAmount(accounts[2], new_amount, {"from": accounts[0], "gas_price": "60 gwei"})
     assert time_lock.getTokensAmountLocked(participant, {"from": participant}) == final_amount
 
 def test_getReleaseTimestamp(time_lock, accounts):
@@ -53,7 +52,7 @@ def test_getReleaseTimestamp(time_lock, accounts):
         time_lock.getReleaseTimestamp(accounts[1], {"from": accounts[1]})
 
 def test_releaseTokens_releaseTimestamp(time_lock, accounts, mock_token):
-    participant = accounts[1]
+    participant = accounts[2]
     amount = 3* 10**18
     release_TimeStamp =  1677064547
     time_lock.addParticipant(participant, amount, release_TimeStamp, {"from": accounts[0], "gas_price": "60 gwei"})
@@ -62,12 +61,27 @@ def test_releaseTokens_releaseTimestamp(time_lock, accounts, mock_token):
         time_lock.releaseTokens({"from": accounts[1], "gas_price": "60 gwei"})
 
 
-""" def test_releaseTokens(time_lock, accounts, mock_token):
-    participant = accounts[1]
+def test_releaseTokens(time_lock, accounts, mock_token):
+    participant = accounts[2]
     amount = 3* 10**18
     release_TimeStamp =  1677064547
     time_lock.addParticipant(participant, amount, release_TimeStamp, {"from": accounts[0], "gas_price": "60 gwei"})
     chain.sleep(release_TimeStamp - chain.time() + 1)
-    #mock_token.approve(accounts[1], amount, {"from": accounts[0], "gas_price": "60 gwei"})
-    time_lock.releaseTokens({"from": accounts[1], "gas_price": "60 gwei"}) """
-    
+    mock_token.transfer(time_lock.address, amount, {"from": accounts[1], "gas_price": "60 gwei"})
+    time_lock.releaseTokens({"from": accounts[2], "gas_price": "60 gwei"})
+    assert mock_token.balanceOf(accounts[2]) == amount
+
+def test_withDrawTokens_ownerCheck(time_lock, accounts):
+    with reverts():
+        time_lock.withdrawTokens(5, {"from": accounts[2], "gas_price": "60 gwei"})
+
+def test_withDrawTokens(time_lock, accounts, mock_token):
+    amount = 3* 10**18
+    mock_token.transfer(time_lock.address, amount, {"from": accounts[1], "gas_price": "60 gwei"})
+    time_lock.withdrawTokens(amount, {"from": accounts[0], "gas_price": "60 gwei"})
+    assert mock_token.balanceOf(accounts[0]) == amount 
+
+def test_contractBalance(time_lock, accounts, mock_token):
+    amount = 3* 10**18
+    mock_token.transfer(time_lock.address, amount, {"from": accounts[1], "gas_price": "60 gwei"})
+    assert time_lock.getContractBalance({"from": accounts[0]}) == amount 
