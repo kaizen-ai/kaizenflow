4cf3212e0ee4a81b84806b4f6128cba0cd72e81d
dan <d.yachmenev@crypto-kaizen.com>
2023-03-23 18:41:15 +0000

    checkpoint

--- defi/dao_swap/twap_vwap_adapter.py
+++ defi/dao_swap/twap_vwap_adapter.py
@@ -5,6 +5,7 @@ import defi.dao_swap.twap_vwap_adapter as ddstvwad
 """
 
 import json
+from typing import Any, Dict, List, Tuple
 
 import numpy as np
 import requests
@@ -12,40 +13,32 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# TODO(Dan): Replace with a company API. Now a dummy is used.
-_API_KEY = "bef8f874d2ad2e08d1e0eb91bad9c53399b8ca55016ba01101f4eeb855abf70f"
 
-
-def _get_price_volume_data():
+def _get_price_volume_data() -> Tuple[List[int], List[int]]:
     """
-    Query price and volume data from the CryptoCompare API.
+    Query price and volume data from the CoinGecko API.
     """
     # Get parameters from the Chainlink node request.
     symbol = request.json["symbol"]
     start_time = request.json["start_time"]
     end_time = request.json["end_time"]
-    request.json["time_interval"]
-    # Query the CryptoCompare API for price data within the specified time range.
+    time_interval = request.json["time_interval"]
+    # Query the CoinGecko API for price data within the specified time range.
     response = requests.get(
-        f"https://min-api.cryptocompare.com/data/v2/histohour?fsym={symbol}&tsym=USD&limit=2000&aggregate=1&toTs={end_time}&api_key={_API_KEY}"
+        f"https://api.coingecko.com/api/v3/coins/{symbol}/market_chart/range?vs_currency=eth&from={start_time}&to={end_time}&interval={time_interval}"
     )
-    price_data = json.loads(response.text)["Data"]["Data"]
+    price_data = json.loads(response.text)["prices"]
     # Get price and volume data.
-    prices = [
-        float(entry["close"])
-        for entry in price_data
-        if entry["time"] >= start_time
-    ]
-    volumes = [
-        float(entry["volumefrom"])
-        for entry in price_data
-        if entry["time"] >= start_time
-    ]
+    prices = [float(entry[1]) for entry in price_data]
+    volumes = [float(entry[2]) for entry in price_data]
+    # Convert prices to WEI.
+    eth_to_wei = 10**18
+    prices = [int(price * eth_to_wei) for price in prices]
     return prices, volumes
 
 
 @app.route("/get_twap", methods=["POST"])
-def get_twap():
+def get_twap() -> Dict[str, Any]:
     """
     Get TWAP for the Chainlink node.
     """
@@ -61,7 +54,7 @@ def get_twap():
 
 
 @app.route("/get_vwap", methods=["POST"])
-def get_vwap():
+def get_vwap() -> Dict[str, Any]:
     """
     Get VWAP for the Chainlink node.
     """
