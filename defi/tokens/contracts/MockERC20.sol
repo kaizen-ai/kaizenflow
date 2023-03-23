4c6fb5de93011d56365e25b8e497c2d7b1175ccf
samarth9008 <samarthpatel9008@gmail.com>
2023-03-23 13:04:51 -0400

    Cmtask51: Natspec Format

--- defi/tokens/contracts/MockERC20.sol
+++ defi/tokens/contracts/MockERC20.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.8.4;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title A Mock ERC20 contract
+/// @author Samarth
+/// @notice Mock Contract for testing TokenTimeLock contract using brownie scripts
 contract MockERC20 is ERC20, Ownable {
-    uint256 public MAXIMUM_SUPPLY;
+    uint256 public max_supply;
 
     constructor(string memory name, string memory symbol, uint256 initialSupply, uint256 maxSupply) ERC20(name, symbol) {
-        MAXIMUM_SUPPLY = maxSupply * 10**decimals();
+        max_supply = maxSupply * 10**decimals();
         initialSupply = initialSupply * 10**decimals();
         require(initialSupply <= MAXIMUM_SUPPLY, "Premint amount is more than maximum supply");
         _mint(msg.sender, initialSupply);
