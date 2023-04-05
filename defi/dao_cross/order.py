3b683bfa3ea3299bc97b63d7f973f473c11d8bd9
saggese <saggese@gmail.com>
2023-04-05 11:48:48 -0400

    Update

--- defi/dao_cross/order.py
+++ defi/dao_cross/order.py
@@ -22,6 +22,9 @@ class Order:
     Limit order to be used in DaoCross or DaoSwap.
     """
 
+    # TODO(gp): @all Reorg the params to match the white paper order.
+    # (timestamp, action, quantity, base_token, limit_price, quote_token,
+    # deposit_address)
     def __init__(
         self,
         base_token: str,
@@ -29,7 +32,9 @@ class Order:
         action: str,
         quantity: float,
         limit_price: float,
+        # TODO(gp): -> dst_address?
         deposit_address: Union[int, str],
+        # TODO(gp): -> src_address?
         wallet_address: Union[int, str],
         *,
         timestamp: Optional[datetime.datetime] = None,
@@ -37,16 +42,30 @@ class Order:
         """
         Constructor.
 
-        :param base_token: token to express order quantity with
-        :param quote_token: token to express order price with
+        According to the white paper, an order like:
+
+        ```(0xabcd0000, 1678660406, buy, 3.2, ETH, 4.0, BTC, 0xdeadc0de)```
+
+        corresponds to the natural language description:
+        "At timestamp Mon Mar 13 2023 02:33:25 GMT+0000, the user with wallet
+        0xabcd0000 commits to buy up to 3.2 units of ETH in exchange for BTC up
+        to a limit price of 4.0 BTC per ETH with proceeds deposited at 0xdeadc0de"
+
+        :param base_token: token to express order quantity with (e.g., ETH)
+        :param quote_token: token to express order price with (e.g., BTC)
         :param action: order action type
             - "buy": purchase the base token and pay with the quote token
             - "sell": sell the base token and receive the quote token
-        :param quantity: maximum quantity in terms of the base token
-        :param limit_price: limit price in terms of the quote token
-        :param deposit_address: deposit address to implement the order for
-        :param wallet_address: wallet address to implement the order for
-        :param timestamp: time of order execution
+        :param quantity: maximum quantity in terms of the base token (e.g., 3.2)
+        :param limit_price: limit price in terms of the quote token (e.g.,
+            4.0 BTC per ETH)
+        :param deposit_address: deposit address to transfer the result of the swap
+            (e.g., 0xdeadc0de)
+        :param wallet_address: wallet address to implement the order for (e.g.,
+            0xabcd0000)
+        :param timestamp: time of order execution (e.g., "Mon Mar 13 2023
+            02:33:25 GMT+0000")
+            - `None` means the current wall clock time
         """
         self.base_token = base_token
         self.quote_token = quote_token
@@ -72,6 +91,8 @@ class Order:
         return str(self)
 
     def __str__(self) -> str:
+        # TODO(gp): @all add wallet_address
+        # TODO(gp): @all reorder to match the constructor
         ret = (
             "base_token=%s quote_token=%s action=%s quantity=%s limit_price=%s timestamp=%s deposit_address=%s"
             % (
@@ -103,14 +124,14 @@ class Order:
 
     def _takes_precedence(self, other: "Order") -> bool:
         """
-        Compare order to another one according to quantity, price and
-        timestamp. Prioritize orders according to:
+        Compare order to another one according to quantity, price and timestamp.
 
+        Prioritize orders according to:
             1. Quantity - higher quantity comes first in priority
             2. Price - higher limit price breaks quantity ties
             3. Timestamp - earlier timestamp breaks ties in quantity and price
         :param other: order to compare the actual order with
-        :return: "True" if order preceeds the other one, "False" otherwise
+        :return: "True" if this order preceeds the other one, "False" otherwise
         """
         takes_precedence = False
         if self.quantity > other.quantity:
@@ -160,9 +181,11 @@ def get_random_order(seed: Optional[int] = None) -> Order:
 
 def is_active_order(order: Optional[Order]) -> bool:
     """
-    Return whether the order is active or not.
+    Return whether `order` is active or not.
 
     Order is active if it is not empty and its quantity is above 0.
+
+    Since we need to
     """
     if order is None:
         return False
@@ -171,6 +194,7 @@ def is_active_order(order: Optional[Order]) -> bool:
     return True
 
 
+# TODO(gp): I'd make it a static method of Order.
 def action_to_int(action: str) -> int:
     """
     Translate an action to an int.
