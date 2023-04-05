c768a632b82244868349f73d0419b29600f51994
The-Road <ywu12313@umd.edu>
2023-04-04 20:35:15 -0400

    kaiko test

--- sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/read_kaiko.py
+++ sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/read_kaiko.py
@@ -7,11 +7,32 @@ api_key = "1d16b71fdfa506550a8a9cc5faa36fa4"
 kc = kaiko.KaikoClient(api_key=api_key)
 
 # Test : TickTrade data
-ticktrade = kaiko.TickTrades(
-    exchange="cbse",
-    instrument="btc-usd",
-    start_time="2022-1-1",
-    end_time="2022-1-2",
-    client=kc
-)
-print(ticktrade.df)
+def ticktrade():
+    ticktrade = kaiko.TickTrades(
+        exchange="cbse",
+        instrument="btc-usd",
+        start_time="2022-1-1",
+        end_time="2022-1-2",
+        client=kc
+    )
+    return ticktrade
+
+def book_snapshot():
+    book_snapshot = kaiko.OrderBookSnapshots(
+        exchange="cbse",
+        instrument="btc-usd",
+        start_time="2023-3-5",
+        end_time="2023-3-6",
+        client=kc
+    )
+    return book_snapshot
+
+def book_aggregations():
+    book_aggregations = kaiko.OrderBookAggregations(
+        exchange="cbse",
+        instrument="btc-usd",
+        start_time="2023-3-5",
+        end_time="2023-3-6",
+        client=kc
+    )
+    return book_aggregations
