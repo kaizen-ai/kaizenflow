18368c6b75b7dcba1d500cdeaf66ce4d549fb5b0
Grigorii Pomazkin <64330743+PomazkinG@users.noreply.github.com>
2023-04-07 14:40:24 +0400

    Sorr task80 2 base tokens (#108)
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * fix tests

--- defi/dao_cross/test/test_optimize.py
+++ defi/dao_cross/test/test_optimize.py
@@ -4,63 +4,145 @@ from typing import List
 
 import defi.dao_cross.optimize as ddacropt
 import defi.dao_cross.order as ddacrord
+import helpers.hdbg as hdbg
 import helpers.hunit_test as hunitest
 
 _LOG = logging.getLogger(__name__)
 
 
+# TODO(Grisha): split into multiple test classes and
+# add more unit tests.
+# TODO(Grisha): factor out common idioms.
 class TestRunSolver1(hunitest.TestCase):
     """
     Run the solver using toy orders.
     """
 
     @staticmethod
-    def get_test_orders(
-        limit_price_buy_1: float,
-        limit_price_sell_1: float,
+    def get_test_btc_orders(
+        limit_prices: List[float],
     ) -> List[ddacrord.Order]:
         """
-        Get toy orders for the unit tests.
+        Get toy BTC orders for the unit tests.
 
-        :param limit_price_buy_1: limit price for the 1st buy order
-        :param limit_price_sell_1: limit price for the 1st sell order
-        :return: buy and sell orders
+        :param limit_prices: limit in prices: quote token per base token
+        :return: buy and sell orders with the base token "BTC"
         """
-        # Set dummy variables.
+        hdbg.dassert_eq(len(limit_prices), 4)
+        # Set the common variables.
         base_token = "BTC"
         quote_token = "ETH"
+        buy_action = "buy"
+        sell_action = "sell"
         timestamp = None
         deposit_address = 1
         wallet_address = 1
         # Genereate buy orders.
-        buy_action = "buy"
-        #
         quantity = 4
-        order_1 = ddacrord.Order(
+        order_0 = ddacrord.Order(
             base_token,
             quote_token,
             buy_action,
             quantity,
-            limit_price_buy_1,
+            limit_prices[0],
             timestamp,
             deposit_address,
             wallet_address,
         )
         #
         quantity = 5
-        limit_price = 5
-        order_2 = ddacrord.Order(
+        order_1 = ddacrord.Order(
             base_token,
             quote_token,
             buy_action,
             quantity,
-            limit_price,
+            limit_prices[1],
             timestamp,
             deposit_address,
             wallet_address,
         )
-        # Generate sell orders.
+        # Genereate sell orders.
+        quantity = 6
+        order_2 = ddacrord.Order(
+            base_token,
+            quote_token,
+            sell_action,
+            quantity,
+            limit_prices[2],
+            timestamp,
+            deposit_address,
+            wallet_address,
+        )
+        #
+        quantity = 2
+        order_3 = ddacrord.Order(
+            base_token,
+            quote_token,
+            sell_action,
+            quantity,
+            limit_prices[3],
+            timestamp,
+            deposit_address,
+            wallet_address,
+        )
+        orders = [order_0, order_1, order_2, order_3]
+        return orders
+
+    @staticmethod
+    def get_test_eth_orders(
+        limit_prices: List[float],
+    ) -> List[ddacrord.Order]:
+        """
+        Get toy ETH orders for the unit tests.
+
+        :param limit_prices: limit in prices: quote token per base token
+        :return: buy and sell orders with the base token "ETH"
+        """
+        hdbg.dassert_eq(len(limit_prices), 4)
+        # Set the common variables.
+        base_token = "ETH"
+        quote_token = "BTC"
+        buy_action = "buy"
         sell_action = "sell"
+        timestamp = None
+        deposit_address = 1
+        wallet_address = 1
+        # Genereate buy orders.
+        quantity = 1
+        order_0 = ddacrord.Order(
+            base_token,
+            quote_token,
+            buy_action,
+            quantity,
+            limit_prices[0],
+            timestamp,
+            deposit_address,
+            wallet_address,
+        )
+        #
+        quantity = 9
+        order_1 = ddacrord.Order(
+            base_token,
+            quote_token,
+            buy_action,
+            quantity,
+            limit_prices[1],
+            timestamp,
+            deposit_address,
+            wallet_address,
+        )
+        # Genereate sell orders.
+        quantity = 8
+        order_2 = ddacrord.Order(
+            base_token,
+            quote_token,
+            sell_action,
+            quantity,
+            limit_prices[2],
+            timestamp,
+            deposit_address,
+            wallet_address,
+        )
         #
         quantity = 6
         order_3 = ddacrord.Order(
@@ -68,42 +150,34 @@ class TestRunSolver1(hunitest.TestCase):
             quote_token,
             sell_action,
             quantity,
-            limit_price_sell_1,
+            limit_prices[3],
             timestamp,
             deposit_address,
             wallet_address,
         )
-        #
-        quantity = 1
-        limit_price = 3
-        order_4 = ddacrord.Order(
-            base_token,
-            quote_token,
-            sell_action,
-            quantity,
-            limit_price,
-            timestamp,
-            deposit_address,
-            wallet_address,
-        )
-        orders = [order_1, order_2, order_3, order_4]
+        orders = [order_0, order_1, order_2, order_3]
         return orders
 
     def test1(self) -> None:
         """
         The limit price condition is True for all orders.
         """
-        exchange_rate = 4
-        limit_price_buy_1 = 5
-        limit_price_sell_1 = 3
-        test_orders = self.get_test_orders(limit_price_buy_1, limit_price_sell_1)
-        result = ddacropt.run_solver(test_orders, exchange_rate)
+        # All the limit prices >= clearing price for the buy orders
+        # <= clearing price for the sell orders.
+        btc_limit_prices = [5, 5.5, 3, 2]
+        btc_orders = self.get_test_btc_orders(btc_limit_prices)
+        eth_limit_prices = [0.3, 0.43, 0.1, 0.2]
+        eth_orders = self.get_test_eth_orders(eth_limit_prices)
+        # 
+        orders = btc_orders + eth_orders
+        prices = {"BTC": 2, "ETH": 8}
+        result = ddacropt.run_solver(orders, prices)
         # Check that the solution is found and is different from zero.
-        self.assertEqual(result["problem_objective_value"], 14)
+        self.assertEqual(result["problem_objective_value"], 192)
         # Check the executed quantity values.
         var_values_str = pprint.pformat(result["q_base_asterisk"])
         exp = r"""
-        [2.0, 5.0, 6.0, 1.0]
+        [3.0, 5.0, 6.0, 2.0, 1.0, 9.0, 8.0, 2.0]
         """
         self.assert_equal(var_values_str, exp, fuzzy_match=True)
 
@@ -111,17 +185,16 @@ class TestRunSolver1(hunitest.TestCase):
         """
         The limit price condition is False for a buy order.
         """
-        exchange_rate = 4
-        limit_price_buy_1 = 3
-        limit_price_sell_1 = 3
-        test_orders = self.get_test_orders(limit_price_buy_1, limit_price_sell_1)
-        result = ddacropt.run_solver(test_orders, exchange_rate)
+        prices = {"BTC": 2, "ETH": 8}
+        btc_limit_prices = [5, 2, 3, 2]
+        btc_orders = self.get_test_btc_orders(btc_limit_prices)
+        result = ddacropt.run_solver(btc_orders, prices)
         # Check that the solution is found but it equals zero.
-        self.assertEqual(result["problem_objective_value"], 10)
+        self.assertEqual(result["problem_objective_value"], 16)
         # Check the executed quantity values.
         var_values_str = pprint.pformat(result["q_base_asterisk"])
         exp = r"""
-        [0.0, 5.0, 5.0, 0.0]
+        [4.0, 0.0, 4.0, 0.0]
         """
         self.assert_equal(var_values_str, exp, fuzzy_match=True)
 
@@ -129,16 +202,15 @@ class TestRunSolver1(hunitest.TestCase):
         """
         The limit price condition is False for a sell order.
         """
-        exchange_rate = 4
-        limit_price_buy_1 = 5
-        limit_price_sell_1 = 5
-        test_orders = self.get_test_orders(limit_price_buy_1, limit_price_sell_1)
-        result = ddacropt.run_solver(test_orders, exchange_rate)
+        prices = {"BTC": 2, "ETH": 8}
+        btc_limit_prices = [5, 5.5, 3, 6]
+        btc_orders = self.get_test_btc_orders(btc_limit_prices)
+        result = ddacropt.run_solver(btc_orders, prices)
         # Check that the solution is found but it equals zero.
-        self.assertEqual(result["problem_objective_value"], 2)
+        self.assertEqual(result["problem_objective_value"], 24)
         # Check the executed quantity values.
         var_values_str = pprint.pformat(result["q_base_asterisk"])
         exp = r"""
-        [1.0, 0.0, 0.0, 1.0]
+        [1.0, 5.0, 6.0, 0.0]
         """
         self.assert_equal(var_values_str, exp, fuzzy_match=True)
