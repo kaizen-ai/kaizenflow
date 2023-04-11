d8ee53bb74780543d1a996edb7c5efb1af4577e2
Vishnu Saju <52094332+Vishnusaju24@users.noreply.github.com>
2023-04-11 15:47:52 -0400

    Changes to Scripts

--- sorrentum_sandbox/examples/ml_projects/SorrIssue21_Team2_Implement_sandbox_for_GitHub_2/download_team2.py
+++ sorrentum_sandbox/examples/ml_projects/SorrIssue21_Team2_Implement_sandbox_for_GitHub_2/download_team2.py
@@ -130,18 +130,18 @@ def downloader(pair,target_table,**kwargs):
   
   #Connection String for main DB
   def get_db_connection(query_var) :       
-      connection = psycop.connect(
+	connection = psycop.connect(
           host="host.docker.internal",                                      
           dbname="airflow",
           port=5532,
           user="postgres",
           password="postgres",
       )
-  	drt_cursor=connection.cursor()
-  	drt_cursor.execute(query_var)
-  	data=drt_cursor.fetchall()
-  	connection.close()
-  	return pd.DataFrame(data)
+    drt_cursor=connection.cursor()
+    drt_cursor.execute(query_var)
+    data=drt_cursor.fetchall()
+    connection.close()
+    return pd.DataFrame(data)
 
     #checking for existing rows in Data tables-
   issues_check_query= "SELECT * FROM github_issues"
