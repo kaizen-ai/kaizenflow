f8e7d51d9de0962b13ebd42585f594aa650d9f37
tamriq <tamriqua@gmail.com>
2023-04-13 18:59:22 +0000

    Task79: update test

--- defi/dao_cross/test/test_dao_cross_sol.py
+++ defi/dao_cross/test/test_dao_cross_sol.py
@@ -28,19 +28,21 @@ def daocross(price_oracle, base_token):
     )
 
 
-def test_on_swap_time(daocross, base_token, price_oracle):
+def test_on_swap_time(daocross, base_token):
     # Add some buy orders.
     # Buy 5 tokens for 100000000000 WEI.
     daocross.buyOrder(base_token.address, 5 * 10**18, 100000000000, accounts[1], {"from": accounts[1], "value": 100000000000*5})
     # Buy 3 tokens for 100000000000 WEI.
     daocross.buyOrder(base_token.address, 3 * 10**18, 100000000000, accounts[2], {"from": accounts[2], "value": 100000000000*3})
     # Add some sell orders.
-    #base_token.transfer(accounts[3], 10**18, {"from": accounts[0]})
-    #base_token.transfer(accounts[4], 10**18, {"from": accounts[0]})
-    base_token.approve(daocross, 10**18, {"from": accounts[3]})
-    base_token.approve(daocross, 10**18, {"from": accounts[4]})
-    daocross.sellOrder(base_token.address, 10**18, 1900, accounts[3], {"from": accounts[3]})
-    daocross.sellOrder(base_token.address, 10**18, 2000, accounts[4], {"from": accounts[4]})
+    base_token.transfer(accounts[3], 4 * 10**18, {"from": accounts[0]})
+    base_token.transfer(accounts[4], 3* 10**18, {"from": accounts[0]})
+    #
+    base_token.approve(daocross, 4 * 10**18, {"from": accounts[3]})
+    base_token.approve(daocross, 3 * 10**18, {"from": accounts[4]})
+    #
+    daocross.sellOrder(base_token.address, 4 * 10**18, 100000000000, accounts[3], {"from": accounts[3]})
+    daocross.sellOrder(base_token.address, 3 * 10**18, 100000000000, accounts[4], {"from": accounts[4]})
 
     # Execute the swap
     transfers = daocross.onSwapTime({"from": accounts[0]})
