5ebce9aa418ba6168f40c2a597c6a94988a47eee
Vishnu Saju <52094332+Vishnusaju24@users.noreply.github.com>
2023-04-14 00:12:06 -0400

    Create extracted_api.py

---
+++ sorrentum_sandbox/examples/ml_projects/SorrIssue21_Team2_Implement_sandbox_for_GitHub_2/extracted_api.py
@@ -0,0 +1,20 @@
+##Import requried packages
+import pandas as pd
+import psycopg2 as psycop
+
+
+
+#Connection String for main DB
+def get_db_connection(query_var) :       
+    connection = psycop.connect(
+        host="host.docker.internal",                                      
+        dbname="airflow",
+        port=5532,
+        user="postgres",
+        password="postgres")
+    drt_cursor=connection.cursor()
+    drt_cursor.execute(query_var)
+    data=drt_cursor.fetchall()
+    connection.close()
+    return pd.DataFrame(data)
+
