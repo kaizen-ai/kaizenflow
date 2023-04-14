88acfbc4393cfd544003a928475badc10e741db5
Chih-Hua Chang <fionachang1028@gmail.com>
2023-04-13 22:51:53 -0400

    add comment and modify ipynb file

--- sorrentum_sandbox/examples/ml_projects/Issue26_Team7_Implement_sandbox_for_Chainlink/query_API.py
+++ sorrentum_sandbox/examples/ml_projects/Issue26_Team7_Implement_sandbox_for_Chainlink/query_API.py
@@ -1,7 +1,9 @@
 import psycopg2
 import pandas as pd
 
+# Create a function that takes string input of a SQL query and output a dataframe contain the desired data.
 def query_from_db(query):
+    # Connection to the posrgreSQL in the airflow container.
     connection = psycopg2.connect(
             host="host.docker.internal",
             dbname="airflow",
@@ -9,11 +11,15 @@ def query_from_db(query):
             user="postgres",
             password="postgres",
         )
+    
     cursor = connection.cursor()
     cursor.execute(query)
+
+    # Get the data from the cursor and make a dataframe for it.
     history_rows = cursor.fetchall()
     df = pd.DataFrame(history_rows, columns=[desc[0] for desc in cursor.description])
 
+    # Close the connection,
     cursor.close()
     connection.close()
 
