a296c16ca67efc387970048834e0b34f70d9ed32
dan <d.yachmenev@crypto-kaizen.com>
2023-04-24 15:21:58 +0000

    checkpoint

--- defi/dao_cross/notebooks/SorrTask139_Create_some_pathological_cases_for_supply_demand_curve.py
+++ defi/dao_cross/notebooks/SorrTask139_Create_some_pathological_cases_for_supply_demand_curve.py
@@ -27,6 +27,14 @@ import helpers.hprint as hprint
 import defi.dao_cross.supply_demand as ddcrsede
 
 # %%
+try:
+    import pulp
+except ImportError:
+    # !sudo /bin/bash -c "(source /venv/bin/activate; pip install pulp)"
+    import pulp
+import defi.dao_cross.optimize as ddacropt
+
+# %%
 hdbg.init_logger(verbosity=logging.DEBUG)
 
 _LOG = logging.getLogger(__name__)
@@ -39,33 +47,39 @@ hprint.config_notebook()
 # # Pathological cases
 
 # %% [markdown]
-# ## Get orders for supply and demand curves.
+# ## Get orders and set prices.
 
 # %%
-supply_orders = ddcrsede.get_supply_orders1()
-supply_orders
+supply_orders1 = ddcrsede.get_supply_orders1()
+supply_orders1
 
 # %%
 demand_orders = ddcrsede.get_demand_orders1()
 demand_orders
 
+# %%
+prices = {"BTC": 1, "ETH": 1}
+
 # %% [markdown]
 # ## Multiple intersection points at quantity Q'
 
 # %%
-type_ = "supply"
-supply_curve = ddcrsede.get_curve(supply_orders, type_)
-supply_curve
+supply_curve1 = ddcrsede.get_curve(supply_orders1, "supply")
+supply_curve1
 
 # %%
-type_ = "demand"
-demand_curve = ddcrsede.get_curve(demand_orders, type_)
+demand_curve = ddcrsede.get_curve(demand_orders, "demand")
 demand_curve
 
 # %%
-plt.plot(*zip(*supply_curve))
+plt.plot(*zip(*supply_curve1))
 plt.plot(*zip(*demand_curve))
 plt.show()
+
+# %% run_control={"marked": false}
+all_orders1 = supply_orders1 + demand_orders
+daocross_results1 = ddacropt.run_daocross_solver(all_orders1, prices)
+display(daocross_results1)
 
 # %% [markdown]
 # ## Multiple intersection points at price P'
@@ -73,8 +87,7 @@ plt.show()
 # %%
 quantity_const = 10.0
 supply_orders2 = ddcrsede.get_supply_orders1(quantity_const=quantity_const)
-type_ = "supply"
-supply_curve2 = ddcrsede.get_curve(supply_orders2, type_)
+supply_curve2 = ddcrsede.get_curve(supply_orders2, "supply")
 supply_curve2
 
 # %%
@@ -82,19 +95,34 @@ plt.plot(*zip(*supply_curve2))
 plt.plot(*zip(*demand_curve))
 plt.show()
 
+# %%
+all_orders2 = supply_orders2 + demand_orders
+daocross_results2 = ddacropt.run_daocross_solver(all_orders2, prices)
+display(daocross_results2)
+
 # %% [markdown]
-# ## No intersection
+# ## No intersection, demand is higher
 
 # %%
 quantity_const = 100.0
 supply_orders3 = ddcrsede.get_supply_orders1(quantity_const=quantity_const)
-type_ = "supply"
-supply_curve3 = ddcrsede.get_curve(supply_orders3, type_)
+supply_curve3 = ddcrsede.get_curve(supply_orders3, "supply")
 supply_curve3
 
 # %%
 plt.plot(*zip(*supply_curve3))
 plt.plot(*zip(*demand_curve))
 plt.show()
+
+# %%
+all_orders3 = supply_orders3 + demand_orders
+daocross_results3 = ddacropt.run_daocross_solver(all_orders3, prices)
+display(daocross_results3)
+
+# %% [markdown]
+# # No intersection, demand is lower
+
+# %%
+# TODO(Dan): Implement
 
 # %%
