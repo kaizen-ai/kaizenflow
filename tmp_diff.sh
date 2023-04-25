0ca7f494e70563259c0706bca2e2381b9929247f
KangmingL <58402025+KangmingL@users.noreply.github.com>
2023-04-25 15:56:55 -0400

    Delete tmp_diff.sh

--- tmp_diff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $1 == "wrap" ]]; then
-    cmd='vimdiff -c "windo set wrap"'
-else
-    cmd='vimdiff'
-fi;
-cmd="$cmd defi/dao_cross/test/outcomes/TestMatchOrders1.test3/tmp.final.actual.txt defi/dao_cross/test/outcomes/TestMatchOrders1.test3/tmp.final.expected.txt"
-eval $cmd
