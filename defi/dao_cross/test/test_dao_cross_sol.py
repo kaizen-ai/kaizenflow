ec00d5695562da84db5c970d0ecf80b4e3f4b656
Samarth KaPatel <samarth.kapatel5@gmail.com>
2023-04-27 18:57:11 -0400

    Comments update test_dao_cross_sol.py

--- defi/dao_cross/test/test_dao_cross_sol.py
+++ defi/dao_cross/test/test_dao_cross_sol.py
@@ -30,7 +30,8 @@ def daocross(price_oracle, base_token):
 
 def test_buyOrder_baseToken_address(daocross, base_token):
     """
-    Checks the `require(_baseToken == address(baseToken));` condition.
+    Check if the error is asserted when the token to buy is not equal 
+    to the base token of the contract.
     """
     baseToken = "0x1234567890123456789012345678901234567890"
     quantity =  5 * 10**18
@@ -38,11 +39,11 @@ def test_buyOrder_baseToken_address(daocross, base_token):
     depositAddress = accounts[1]
 
     with reverts():
-         daocross.buyOrder(baseToken, quantity, limitPrice, depositAddress, {"from": accounts[1], "value": 100000000000*5, "gas_price": "60 gwei"})
+         daocross.buyOrder(baseToken, quantity, limitPrice, depositAddress, {"from": accounts[1], "value": limitPrice*5, "gas_price": "60 gwei"})
     
 def test_buyOrder_passedEther(daocross, base_token):
     """
-    Checks the `require(msg.value >= fullPrice)` condition.
+    Check if the ethers passed with function call is greater than or equal to full price.
     """
     quantity =  5 * 10**18
     limitPrice = 100000000000
@@ -53,18 +54,19 @@ def test_buyOrder_passedEther(daocross, base_token):
 
 def test_buyOrder_orderArray(daocross, base_token):
     """
-    Checks the length of order array after creating the orders.
+    Check if the length of order array is increased after creating the buy orders.
     Buy 5 tokens for 100000000000 WEI.
     Buy 2.5 tokens for 100000000000 WEI.
     """
-    daocross.buyOrder(base_token.address, 5 * 10**18, 100000000000, accounts[1], {"from": accounts[1], "value": 100000000000*5, "gas_price": "60 gwei"})
-    daocross.buyOrder(base_token.address, 2.5 * 10**18, 100000000000, accounts[2], {"from": accounts[2], "value": 100000000000*2.5, "gas_price": "60 gwei"})
+    limitPrice = 100000000000
+    daocross.buyOrder(base_token.address, 5 * 10**18, limitPrice, accounts[1], {"from": accounts[1], "value": limitPrice*5, "gas_price": "60 gwei"})
+    daocross.buyOrder(base_token.address, 2.5 * 10**18, limitPrice, accounts[2], {"from": accounts[2], "value": limitPrice*2.5, "gas_price": "60 gwei"})
     assert daocross.orders(0)[0] == 0
     assert daocross.orders(1)[0] == 1
     
 def test_buyOrder_eventEmitted(daocross, base_token):
     """
-    Checks wheather the `newBuyOrder` event was emitted or not.
+    Check whether the newBuyOrder event was emitted or not.
     """
     quantity =  5 * 10**18
     limitPrice = 100000000000
@@ -77,7 +79,8 @@ def test_buyOrder_eventEmitted(daocross, base_token):
 
 def test_sellOrder_baseToken_address(daocross, base_token):
     """
-    Checks the `require(_baseToken == address(baseToken));` condition.
+    Check if the error is asserted when the token to sell is not equal 
+    to the base token of the contract.
     """
     baseToken = "0x1234567890123456789012345678901234567890"
     quantity =  5 * 10**18
@@ -89,7 +92,8 @@ def test_sellOrder_baseToken_address(daocross, base_token):
 
 def test_sellOrder_allowance(daocross, base_token):
     """
-    Checks `require(allowance >= _quantity);` conditon.
+    Check if the allowance by the token seller is greater than
+    or equal to the quantity.
     """
     base_token.transfer(accounts[5], 5* 10**18, {"from": accounts[0], "gas_price": "60 gwei"})
     base_token.approve(daocross, 5 * 10**18, {"from": accounts[5], "gas_price": "60 gwei"})
@@ -103,7 +107,7 @@ def test_sellOrder_allowance(daocross, base_token):
 
 def test_sellOrder_checkBalance(daocross, base_token):
     """
-    Checks the balance of this DaoCross contract after the sell order is placed.
+    Check if the balance of this DaoCross contract is increased after the sell order is placed.
     """
     base_token.transfer(accounts[5], 5* 10**18, {"from": accounts[0], "gas_price": "60 gwei"})
     base_token.approve(daocross, 5 * 10**18, {"from": accounts[5], "gas_price": "60 gwei"})
@@ -118,7 +122,7 @@ def test_sellOrder_checkBalance(daocross, base_token):
 
 def test_sellOrder_orderArray(daocross, base_token):
     """
-    Checks the length of order array after creating the orders.
+    Check if the length of order array is increased after creating the sell orders.
     Sell 5 tokens.
     Sell 2.5 tokens.
     """
@@ -134,7 +138,7 @@ def test_sellOrder_orderArray(daocross, base_token):
     
 def test_sellOrder_eventEmitted(daocross, base_token):
     """
-    Checks wheather the `newBuyOrder` event was emitted or not.
+    Check whether the newSellOrder event was emitted or not.
     """
     base_token.transfer(accounts[5], 5* 10**18, {"from": accounts[0], "gas_price": "60 gwei"})
     base_token.approve(daocross, 5 * 10**18, {"from": accounts[5], "gas_price": "60 gwei"})
@@ -150,14 +154,14 @@ def test_sellOrder_eventEmitted(daocross, base_token):
 
 def test_getChainlinkFeedPrice(daocross):
     """
-    Checks the functionality of `getChainlinkFeedPrice()` fucntion.
+    Check whether the MockV3Aggregator gets the latest price.
     """
     assert daocross.getChainlinkFeedPrice() == 100000000000
 
 def test_onSwapTime_similarQuantity(daocross, base_token):
     """
-    Checks the functionality of token transfers with single order of same quantity.
-    Also checks the functionality of eraseOrders() function.
+    Check whether the token transfers occurs with single order of same quantity.
+    Also check whether the eraseOrders() function deletes the orders from the array.
     """
     
     base_token.transfer(accounts[5], 5* 10**18, {"from": accounts[0], "gas_price": "60 gwei"})
@@ -166,17 +170,17 @@ def test_onSwapTime_similarQuantity(daocross, base_token):
     daocross.buyOrder(base_token.address, 5 * 10**18, 100000000000, accounts[1], {"from": accounts[1], "value": 100000000000*5, "gas_price": "60 gwei"})
     daocross.sellOrder(base_token.address, 5 * 10**18, 100000000000, accounts[5], {"from": accounts[5], "gas_price": "60 gwei"})
     daocross.onSwapTime({"from": accounts[0], "gas_price": "60 gwei"})
-
+    
+    #Check if buyer received the desired amount of tokens.
     assert base_token.balanceOf(accounts[1]) == 5 * 10**18
-    """
-    Since orders array is deleted.
-    """
+ 
+    #Check if the Orders array is deleted after the swapping. 
     with reverts():
         daocross.orders(0)[0] == 0
 
 def test_onSwapTime_sellOrder_with_greater_than_limitPrice(daocross, base_token):
     """
-    Checks the functionality of sell order with Greater than limit Price.
+    Check if the sell order with limit price Greater than latest price is not placed.
     """
     base_token.transfer(accounts[5], 5* 10**18, {"from": accounts[0], "gas_price": "60 gwei"})
     base_token.approve(daocross, 5 * 10**18, {"from": accounts[5], "gas_price": "60 gwei"})
@@ -189,7 +193,7 @@ def test_onSwapTime_sellOrder_with_greater_than_limitPrice(daocross, base_token)
 
 def test_onSwapTime_buyOrder_with_less_than_limitPrice(daocross, base_token):
     """
-    Checks the functionality of buy order with less than limit Price.
+    Check if the buy order with limit Price less than latest price is not placed.
     """
     initialBalance = accounts[6].balance()
 
