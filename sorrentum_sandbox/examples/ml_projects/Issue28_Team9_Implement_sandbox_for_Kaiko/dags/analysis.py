4b3eccb9d6f21b9712366547e9d7d18227a22444
The-Road <ywu12313@umd.edu>
2023-05-04 16:36:05 -0400

    deliverable 4

--- sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/dags/analysis.py
+++ sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/dags/analysis.py
@@ -3,8 +3,8 @@ from airflow.operators.bash import BashOperator
 import datetime
 
 mydag = DAG(
-    dag_id="realtime",
-    description="Download realtime kaiko data",
+    dag_id="analysis",
+    description="Analyse realtime kaiko data",
     tags=["kaiko"],
     start_date=datetime.datetime(2023, 4, 1),
     catchup=True,
@@ -15,7 +15,7 @@ task = BashOperator(
     task_id="download_1d",
     bash_command="""
     cd /opt/airflow
-    python3 download_to_db.py --start_timestamp "{{data_interval_start}}" --end_timestamp "{{data_interval_end}}" --target_table "public.realtime"
+    python3 dask_work.py --date "{{data_interval_start}}" --target_table "public.analysis"
     """
 )
 
--- sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/data_analysis.py
+++ sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/data_analysis.py
@@ -1,10 +1,16 @@
-import pandas as pd
-import numpy as np
+import pandas
+import dask.dataframe as dd
 import load_data
 
-df = load_data.realtime_from_csv()
-print(df)
+def work(date):
+    df = load_data.realtime_from_csv()
+    # use Dask
+    df = dd.from_pandas(df, chunksize=4)
+    df.drop_duplicates(subset=["timestamp", "trade_id", "amount"], keep="last", inplace=True)
+    # select data
+    df['timestamp'] = pandas.to_datetime(df['timestamp'], infer_datetime_format=True)
+    new_table = df[df['timestamp'].dt.date == date.date()]
+    return new_table.compute()
 
-duplicateCase = df[df.duplicated(['timestamp', 'trade_id', 'amount'], keep=False)]
-df.drop_duplicates(subset=["timestamp", "trade_id", "amount"], keep="last", inplace=True)
-print(df)
+if __name__ == "__main__":
+    print(work(pandas.to_datetime("2023-4-1")))
