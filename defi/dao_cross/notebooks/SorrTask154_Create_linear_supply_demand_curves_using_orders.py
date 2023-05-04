8d8e2c8dfaf1d47f5877a3463f5cc9666c531f38
dan <d.yachmenev@crypto-kaizen.com>
2023-05-04 09:11:00 +0000

    SorrTask154: Refine supply demand functions

--- defi/dao_cross/notebooks/SorrTask154_Create_linear_supply_demand_curves_using_orders.py
+++ defi/dao_cross/notebooks/SorrTask154_Create_linear_supply_demand_curves_using_orders.py
@@ -27,6 +27,14 @@ import defi.dao_cross.supply_demand as ddcrsede
 import defi.dao_cross.order as ddacrord
 
 # %%
+try:
+    import pulp
+except ImportError:
+    # !sudo /bin/bash -c "(source /venv/bin/activate; pip install pulp)"
+    import pulp
+import defi.dao_cross.optimize as ddacropt
+
+# %%
 hdbg.init_logger(verbosity=logging.DEBUG)
 
 _LOG = logging.getLogger(__name__)
@@ -36,40 +44,78 @@ _LOG.info("%s", henv.get_system_signature()[0])
 hprint.config_notebook()
 
 # %% [markdown]
-# # Get linear supply / demand orders
+# # Get aggregated supply / demand
 
 # %%
 type_ = "supply"
 alpha = 2.0
 beta = 10.0
 n_orders = 10
-linear_supply_orders = ddcrsede.convert_aggregated_curve_to_limit_orders(type_, alpha, beta, n_orders)
-ddacrord.convert_orders_to_dataframe(linear_supply_orders)
+agg_supply_curve = ddcrsede.get_supply_demand_aggregated_curve(
+    type_, alpha, beta, n_orders
+)
+agg_supply_curve
 
 # %%
 type_ = "demand"
 alpha = -2.0
 beta = 210.0
 n_orders = 10
-linear_demand_orders = ddcrsede.convert_aggregated_curve_to_limit_orders(type_, alpha, beta, n_orders)
-ddacrord.convert_orders_to_dataframe(linear_demand_orders)
+agg_demand_curve = ddcrsede.get_supply_demand_aggregated_curve(
+    type_, alpha, beta, n_orders
+)
+agg_demand_curve
+
+# %%
+plt.plot(agg_supply_curve)
+plt.plot(agg_demand_curve)
+plt.show()
+
+# %%
+agg_supply_orders = ddcrsede.convert_aggregated_curve_to_limit_orders(agg_supply_curve)
+agg_supply_orders_df = ddacrord.convert_orders_to_dataframe(agg_supply_orders)
+agg_supply_orders_df
+
+# %%
+agg_demand_orders = ddcrsede.convert_aggregated_curve_to_limit_orders(agg_demand_curve)
+agg_demand_orders_df = ddacrord.convert_orders_to_dataframe(agg_demand_orders)
+agg_demand_orders_df
 
 # %% [markdown]
-# # Get curves
+# # Implement DaoCross and DaoSwap
+
+# %%
+agg_all_orders = agg_supply_orders + agg_demand_orders
+prices = {"BTC": 1, "ETH": 50}
+
+# %%
+daocross_results = ddacropt.run_daocross_solver(agg_all_orders, prices)
+display(daocross_results)
+
+# %%
+daoswap_results = ddacropt.run_daoswap_solver(agg_all_orders)
+display(daoswap_results)
+
+# %% [markdown]
+# # Sanity check with discrete supply / demand
 
 # %%
 type_ = "supply"
-supply_curve = ddcrsede.get_supply_demand_curve(type_, linear_supply_orders)
-supply_curve
+descrete_supply_curve = ddcrsede.get_supply_demand_discrete_curve(
+    type_, agg_supply_orders_dfagg_supply_orders_df
+)
+descrete_supply_curve
 
 # %%
 type_ = "demand"
-demand_curve = ddcrsede.get_supply_demand_curve(type_, linear_demand_orders)
-demand_curve
+descrete_demand_curve = ddcrsede.get_supply_demand_discrete_curve(
+    type_, agg_demand_orders_df
+)
+descrete_demand_curve
 
 # %%
-plt.plot(*zip(*supply_curve))
-plt.plot(*zip(*demand_curve))
+plt.plot(descrete_supply_curve)
+plt.plot(descrete_demand_curve)
 plt.show()
 
 # %%
--- defi/dao_cross/supply_demand.py
+++ defi/dao_cross/supply_demand.py
@@ -8,6 +8,8 @@ import logging
 import random
 from typing import List, Tuple
 
+import pandas as pd
+
 import helpers.hdbg as hdbg
 import defi.dao_cross.order as ddacrord
 
@@ -20,10 +22,8 @@ _LOG = logging.getLogger(__name__)
 # #############################################################################
 
 
-def get_curve_orders(
-    type_: str,
-    quantities: List[int],
-    limit_prices: List[int],
+def convert_discrete_curve_to_limit_orders(
+    discrete_curve: pd.Series,
     *,
     quantity_scale: float = 1.0,
     quantity_const: float = 0.0,
@@ -34,9 +34,7 @@ def get_curve_orders(
     """
     Get a list of orders that represent supply or demand.
 
-    :param type_: curve type
-    :param quantities: coordinates on quantity axis
-    :param limit_prices: coordinates on limit prices axis
+    :param discrete_curve: discrete curve coordinates
     :param quantity_scale: coef to multiply quantities
     :param quantity_const: const to change quantities
     :param limit_price_scale: coef to multiply limit prices
@@ -51,18 +49,31 @@ def get_curve_orders(
     base_token = "BTC"
     quote_token = "ETH"
     # Get order action based on the curve type.
+    # TODO(Dan): Add asserts for the curve.
+    type_ = discrete_curve.name
+    hdbg.dassert_in(type_, ["supply", "demand"])
     if type_ == "supply":
         action = "sell"
-    elif type_ == "demand":
-        action = "buy"
+        ascending = True
     else:
-        raise ValueError("Invalid type_='%s'" % type_)
+        action = "buy"
+        ascending = False
+    # Invert coordinates, so prices are indices and quantities are values.
+    # Sort by prices with respect to the curve type.
+    inverted_discrete_curve = pd.Series(
+        index=discrete_curve.values,
+        data=discrete_curve.index,
+        name=discrete_curve.name,
+    ).sort_index(ascending=ascending)
+    # Order quantities are distances between the curve quantities.
+    # Order limit prices correspond to the curve limit prices.
+    order_data = inverted_discrete_curve.drop_duplicates().diff().dropna()
     # Generate orders.
     orders = []
-    for quantity, limit_price in zip(quantities, limit_prices):
+    for p, q in order_data.items():
         # Adjust quantities and prices by the passed params.
-        quantity = quantity * quantity_scale + quantity_const
-        limit_price = limit_price * limit_price_scale + limit_price_const
+        q = q * quantity_scale + quantity_const
+        p = p * limit_price_scale + limit_price_const
         # Generate random addresses.
         deposit_address = random.randint(1, 10)
         wallet_address = deposit_address
@@ -70,9 +81,9 @@ def get_curve_orders(
         order = ddacrord.Order(
             timestamp,
             action,
-            quantity,
+            q,
             base_token,
-            limit_price,
+            p,
             quote_token,
             deposit_address,
             wallet_address,
@@ -81,61 +92,69 @@ def get_curve_orders(
     return orders
 
 
-def get_supply_demand_curve(
-    type_: str, orders: List[ddacrord.Order],
-) -> List[Tuple[int, int]]:
+def get_supply_demand_discrete_curve(
+    type_: str,
+    orders_df: pd.DataFrame,
+) -> pd.Series:
     """
-    Get coordinates of dots that represent a curve of the specified type.
+    Get a discrete curve of the specified type from the orders data.
 
     :param type_: curve type
-    :param orders: orders that represent the curve
-    :return: coordinates of dots that belong to curve
+    :param orders_df: orders data
+    :return: coordinates of dots that belong to the curve
     """
-    hdbg.dassert_in(type_, ["demand", "supply"])
-    # Extract quantity and price from the passed orders for sorting.
-    orders_info = [
-        (order.quantity, order.limit_price,) for order in orders
-    ]
-    # Sort orders by limit price with respect to the curve type.
-    # Supply curve is monotonically increasing, orders are in ascending order.
-    # Demand curve is monotonically decreasing, orders are in descending order.
-    reverse = type_ == "demand"
-    orders_info = sorted(orders_info, key=lambda x: x[1], reverse=reverse)
-    # Set amount of quantity that has already entered the market.
-    quantity_on_market = 0
-    dots = []
-    for order_info in orders_info:
-        # Order quantity is a distance between the dot with this order
-        # and quantity on market before it.
-        quantity = order_info[0] + quantity_on_market
-        price = order_info[1]
-        # Add a dot that connects order dots on a broken curve.
-        dot1 = (quantity_on_market, price)
-        dots.append(dot1)
-        # Add a dot with order data.
-        dot2 = (quantity, price)
-        dots.append(dot2)
-        # Update quantity on market.
-        quantity_on_market = quantity_on_market + order_info[0]
-    # Add last line of the curve:
+    # TODO(Dan): Add asserts for orders compatibility.
     if type_ == "supply":
+        orders_df = orders_df[orders_df["action"]=="sell"]
+        ascending = True
         # Extend supply curve with a straight line up at the max quantity.
-        last_dot = (dots[-1][0], dots[-1][1] * 1.25)
+        last_limit_price_mpl = 1.25
     elif type_ == "demand":
+        orders_df = orders_df[orders_df["action"]=="buy"]
+        ascending = False
         # Extend demand curve with a straight line down until zero quantity.
-        last_dot = (dots[-1][0], 0)
+        last_limit_price_mpl = 0.0
     else:
         raise ValueError("Invalid type_='%s'" % type_)
-    dots.append(last_dot)
+    orders_df = orders_df[["limit_price", "quantity"]]
+    # Sort orders by limit price with respect to the curve type.
+    # Supply curve is increasing, orders are in ascending order.
+    # Demand curve is decreasing, orders are in descending order.
+    orders_df = orders_df.sort_values(by="limit_price", ascending=ascending)
+    # Set amount of quantity that has already entered the market.
+    quantity_on_market = 0
+    dots_x = []
+    dots_y = []
+    for _, row in orders_df.iterrows():
+        price = row["limit_price"]
+        order_quantity = row["quantity"]
+        # Add a dot that connects order dots on a discrete curve.
+        dots_x.append(quantity_on_market)
+        dots_y.append(price)
+        # Add a dot with order data.
+        # Order quantity is a distance between the dot with this order
+        # and quantity on market before it.
+        dot_x = order_quantity + quantity_on_market
+        dots_x.append(dot_x)
+        dots_y.append(price)
+        # Update quantity on market.
+        quantity_on_market = quantity_on_market + order_quantity
+    # Add last line of the curve:
+    last_quantity = dots_x[-1]
+    last_limit_price = dots_y[-1] * last_limit_price_mpl
+    dots_x.append(last_quantity)
+    dots_y.append(last_limit_price)
+    # Build a series from curve coordinates.
+    dots = pd.Series(data=dots_y, index=dots_x, name=type_)
     return dots
 
 
 # #############################################################################
-# Linear supply/demand curves.
+# Aggregated supply/demand curves.
 # #############################################################################
 
 
-def convert_aggregated_curve_to_limit_orders(
+def get_supply_demand_aggregated_curve(
     type_: str,
     alpha: float,
     beta: float,
@@ -143,9 +162,9 @@ def convert_aggregated_curve_to_limit_orders(
     *,
     max_quantity: float = 100.0,
     seed: int = 42,
-) -> List[ddacrord.Order]:
+) -> pd.Series:
     """
-    Convert aggregated linear curve to limit orders.
+    Get aggregated curve from the passed linear function formula.
 
     Formula: limit_price = alpha * quantity + beta
 
@@ -155,7 +174,7 @@ def convert_aggregated_curve_to_limit_orders(
     :param n_orders: number of orders to generate
     :param max_quantity: max quantity that a seller can provide
     :param seed: seed for random sampling
-    :return: orders that represent linear curve od a specified type
+    :return: aggregated curve coordinates
     """
     random.seed(seed)
     if type_ == "supply":
@@ -187,28 +206,24 @@ def convert_aggregated_curve_to_limit_orders(
     # Sort quantities in ascending order to represent a curve.
     quantities = sorted(quantities)
     # Sort limit prices with respect to the curve type.
-    # Supply curve is monotonically increasing, limit prices are in ascending order.
-    # Demand curve is monotonically decreasing, limit prices are in descending order.
+    # Supply curve is increasing, limit prices are in ascending order.
+    # Demand curve is decreasing, limit prices are in descending order.
     reverse = type_ == "demand"
     limit_prices = sorted(limit_prices, reverse=reverse)
-    # Get orders.
-    orders = _get_orders_from_dots(type_, quantities, limit_prices)
-    return orders
+    # Put aggregated curve coodinates in a series.
+    agg_curve = pd.Series(data=limit_prices, index=quantities, name=type_)
+    return agg_curve
 
 
-def _get_orders_from_dots(
-    type_: str,
-    quantities: List[float],
-    limit_prices: List[float],
+def convert_aggregated_curve_to_limit_orders(
+    agg_curve: pd.Series,
     *,
     seed: int = 42,
 ) -> List[ddacrord.Order]:
     """
-    Get orders from linear curve dots coordinates.
+    Get orders from aggregated curve dots coordinates.
 
-    :param type_: curve type
-    :param quantities: coordinates on quantity axis
-    :param limit_prices: coordinates on limit prices axis
+    :param agg_curve: aggregated curve coordinates
     :param seed: seed for random sampling
     :return: orders that represent demand or supply curve
     """
@@ -219,16 +234,17 @@ def _get_orders_from_dots(
     base_token = "BTC"
     quote_token = "ETH"
     # Get order action based on the curve type.
+    # TODO(Dan): Add asserts for curve.
+    type_ = agg_curve.name
+    hdbg.dassert_in(type_, ["supply", "demand"])
     if type_ == "supply":
         action = "sell"
-    elif type_ == "demand":
-        action = "buy"
     else:
-        raise ValueError("Invalid type_='%s'" % type_)
+        action = "buy"
     # Set amount of quantity that has already entered the market.
     quantity_on_market = 0.0
     orders = []
-    for q, p in zip(quantities, limit_prices):
+    for q, p in agg_curve.items():
         # Order quantity is a distance between the dot with this order
         # and quantity on market before it.
         order_q = q - quantity_on_market
