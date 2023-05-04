a1866903f13347273669ef882669e035247e86b7
The-Road <ywu12313@umd.edu>
2023-05-04 17:01:24 -0400

    dask work

--- sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/data_analysis.py
+++ sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/data_analysis.py
@@ -1,6 +1,7 @@
 import pandas
 import dask.dataframe as dd
 import load_data
+from dask.distributed import LocalCluster, Client
 
 def work(date):
     df = load_data.realtime_from_csv()
@@ -11,6 +12,12 @@ def work(date):
     df['timestamp'] = pandas.to_datetime(df['timestamp'], infer_datetime_format=True)
     new_table = df[df['timestamp'].dt.date == date.date()]
     return new_table.compute()
+
+def dask_work(date):
+    cluster = LocalCluster()
+    client = Client(cluster)
+    future = client.submit(work, date)
+    return future.result()
 
 if __name__ == "__main__":
     print(work(pandas.to_datetime("2023-4-1")))
