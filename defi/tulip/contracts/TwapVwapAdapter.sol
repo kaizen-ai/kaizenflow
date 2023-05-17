2f7e24b74124797e5421feb40dc1aa8b055ff83c
tamriq <tamriqua@gmail.com>
2023-05-17 14:37:08 +0000

    SorrTask150: integrate adapter prices to daoCross contract

--- defi/tulip/contracts/TwapVwapAdapter.sol
+++ defi/tulip/contracts/TwapVwapAdapter.sol
@@ -5,12 +5,13 @@ import "../node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 contract TwapVwapAdapter is ChainlinkClient {
     using Chainlink for Chainlink.Request;
-
-    uint256 public price;
-    uint256 private fee;
     address private oracle;
     bytes32 private jobIdTwap;
     bytes32 private jobIdVwap;
+
+    uint256 private fee;
+    mapping(bytes32 => uint256) requestIdToResult;
+ 
 
     
     constructor() {
@@ -22,26 +23,30 @@ contract TwapVwapAdapter is ChainlinkClient {
         fee = 0.1 * 1*10**18;
     }
     
-    function requestTwap(string memory _symbol, string memory _startTimestamp, string memory _endTimestamp) public returns (bytes32 requestId) {
+    function requestTwap(string memory _symbol, int256 _startTimestamp, int256 _endTimestamp) public returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobIdTwap, address(this), this.fulfill.selector);
         // Set the parameters.
         request.add("symbol", _symbol);
-        request.add("start_time", _startTimestamp);
-        request.add("end_time", _endTimestamp);
+        request.addInt("start_time", _startTimestamp);
+        request.addInt("end_time", _endTimestamp);
         return sendChainlinkRequestTo(oracle, request, fee);
     }
 
-    function requestVwap(string memory _symbol, string memory _startTimestamp, string memory _endTimestamp) public returns (bytes32 requestId) {
+    function requestVwap(string memory _symbol, int256 _startTimestamp, int256 _endTimestamp) public returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobIdVwap, address(this), this.fulfill.selector);
         // Set the parameters.
         request.add("symbol", _symbol);
-        request.add("start_time", _startTimestamp);
-        request.add("end_time", _endTimestamp);
+        request.addInt("start_time", _startTimestamp);
+        request.addInt("end_time", _endTimestamp);
         return sendChainlinkRequestTo(oracle, request, fee);
     }
     
     function fulfill(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId) {
-        price = _price;
+        requestIdToResult[_requestId] = _price;
+    }
+
+    function getResult(bytes32 _requestId) public view returns (uint256) {
+        return requestIdToResult[_requestId];
     }
     
     function stringToBytes32(string memory source) private pure returns (bytes32 result) {
