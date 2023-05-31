0fa62ec6a624b276179f6877625364b0b84f98eb
dchoi127 <dchoi127@terpmail.umd.edu>
2023-05-31 13:34:39 -0400

    Fixing formatting/organization
    Minor touch ups

--- im_v2/common/data/transform/convert_pq_to_csv.py
+++ im_v2/common/data/transform/convert_pq_to_csv.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 """
-Convert data from Parquet to CSV.
+Convert file from Parquet to CSV.
 
-Usage sample:
+Usage example:
 > im_v2/common/data/transform/convert_pq_to_csv.py \
-    --pq_file_path '~/Users/d_folder/pq_files/pq_file_to_convert.parquet' \
-    --dst_dir '~/Users/d_folder/csv_files' \
-    --aws_profile 'ck'
+    --pq_file_path im_v2/common/data/file_name.pq \
+    --dst_dir im_v2/common/notebooks
 
-Note that dst_dir is optional. If not given, script will save file in \
-same directory that pq_file_path is located in.
 
-Note that pq_file_path can be s3 or local. Destination dir can be absolute or relative path 
+Note that pq_file_path can be s3 or local. Destination dir can be absolute or relative path.
 """
 
 import argparse
@@ -27,6 +24,7 @@ _LOG = logging.getLogger(__name__)
 
 
 def _parse() -> argparse.ArgumentParser:
+    hdbg.init_logger(use_exec_path=True)
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
@@ -38,41 +36,45 @@ def _parse() -> argparse.ArgumentParser:
         help="Input Parquet file to convert to CSV format",
     )
     parser.add_argument(
-        "--aws_profile",
-        action="store",
-        required=False,
-        default=None,
-        type=str,
-        help="The AWS profile to use for `.aws/credentials` or for env vars",
-    )
-    parser.add_argument(
         "--dst_dir",
         action="store",
         type=str,
         required=False,
         default=None,
-        help="Destination dir where to save converted CSV file. \
-        If not specified, uses the same dir where input file is located",
+        help="""
+        Destination dir where to save converted CSV file.
+        If not specified, uses the same dir where input file is located
+        """,
+    )
+    parser.add_argument(
+        "--aws_profile",
+        action="store",
+        required=False,
+        default=None,
+        type=str,
+        help="The AWS profile to convert file located at s3 filesystem.",
     )
     parser = hparser.add_verbosity_arg(parser)
     return parser
+
 
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
     dst_dir = args.dst_dir
     aws_profile = args.aws_profile
     pq_file_path = args.pq_file_path
-    hdbg.init_logger(use_exec_path= True)
-    if dst_dir is not None:
-        hs3.dassert_path_exists(dst_dir, aws_profile = aws_profile)
-    hs3.dassert_path_exists(pq_file_path, aws_profile = aws_profile)
-    df = hparque.from_parquet(pq_file_path, aws_profile = aws_profile)
+    hs3.dassert_path_exists(pq_file_path, aws_profile=aws_profile)
     if not dst_dir:
         dst_dir = os.path.dirname(pq_file_path)
+    hs3.dassert_path_exists(dst_dir, aws_profile=aws_profile)
+    # Make a path to CSV file
     file_name = os.path.basename(pq_file_path).split(".")[-2]
     csv_file_path = os.path.join(dst_dir, file_name + ".csv")
+    # Convert the file.
+    df = hparque.from_parquet(pq_file_path, aws_profile=aws_profile)
     _LOG.debug("Saving CSV file at %s", csv_file_path)
     df.to_csv(csv_file_path)
+
 
 if __name__ == "__main__":
     _main(_parse())
