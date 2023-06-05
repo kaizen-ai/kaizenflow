570429eafda34c62cc33f753b6b0211eae4c3c7e
Vishnu Saju <52094332+Vishnusaju24@users.noreply.github.com>
2023-06-05 16:26:00 -0400

    Made new scripts and deleted old ones

---
+++ dev_scripts/cleanup_scripts/rename_pytestraises.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+script_name="cmamp/dev_scripts/cleanup_scripts/rename_pytestraises.sh"
+
+
+/cmamp/dev_scripts/replace_text.py \
+    --old "self.assertRaises" \
+    --new "self.assertRaises" \
+    --exclude_files $script_name \
+
+## Remove unused imports from affected files.
+#invoke lint -m --only-format
+
+
