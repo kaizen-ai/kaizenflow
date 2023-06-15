be863f3e284b166756e94955b0d469ee7d8f0c58
christyanthony <christyanthony14@gmail.com>
2023-06-15 00:14:17 -0400

    commit on recent changes

--- dev_scripts/cleanup_scripts/Rename_to_multi_line_cmd.sh
+++ dev_scripts/cleanup_scripts/Rename_to_multi_line_cmd.sh
@@ -3,6 +3,6 @@
 dir_name="/amp/dev_scripts/cleanup_scripts dev_scripts/cleanup_scripts"
 
 replace_text.py \
-   --old "to_multi_line_cmd" \
+   --old "_to_multi_line_cmd" \
    --new "to_multi_line_cmd" \
    --exclude_dirs "$dir_name"
