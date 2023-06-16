9a07a97d83a72d11bb468486538d8e7e22633b65
zli00185 <zli00185@terpmail.umd.edu>
2023-06-16 23:35:52 +0800

    remove curr_file and exclude_dirs command

--- dev_scripts/cleanup_scripts/SorrTask298_rename_get_docker_cmd_to_get_docker_run_cmd.sh
+++ dev_scripts/cleanup_scripts/SorrTask298_rename_get_docker_cmd_to_get_docker_run_cmd.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -xe
 
-curr_file="/amp/dev_scripts/cleanup_scripts dev_scripts/cleanup_scripts"
 
 replace_text.py \
     --old "_get_docker_run_cmd" \
-    --new "_get_docker_run_cmd" \
-    --exclude_dirs "$curr_file"
+    --new "_get_docker_run_cmd" 
+
 
 
 
