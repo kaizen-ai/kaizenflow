ec575f889517842fdf98dbcb7407e01da5d219bb
christyanthony <christyanthony14@gmail.com>
2023-06-19 19:34:27 -0400

    Remove unwanted log files, txt files, temporary files and OS generated files

--- tmp_diff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $1 == "wrap" ]]; then
-    cmd='vimdiff -c "windo set wrap"'
-else
-    cmd='vimdiff'
-fi;
-cmd="$cmd helpers/test/outcomes/TestDryRunTasks1.test_docker_ps/tmp.final.actual.txt helpers/test/outcomes/TestDryRunTasks1.test_docker_ps/tmp.final.expected.txt"
-eval $cmd
