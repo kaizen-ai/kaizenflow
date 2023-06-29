129cae36605e6900a4ba5191402b6653b39bb9c1
hhxjqm <67122725+hhxjqm@users.noreply.github.com>
2023-06-29 14:58:08 -0400

    Update test_hintrospection.py

--- helpers/test/test_hintrospection.py
+++ helpers/test/test_hintrospection.py
@@ -196,7 +196,7 @@ def dummy_function() -> None:
 class TestGetFunctionFromString1(hunitest.TestCase):
     def test1(self) -> None:
         """
-        Test if the function is being found and extracted correctly from a string.
+        Test function extraction correctly from string.
         """
         func_str = "helpers.test.test_hintrospection.dummy_function"
         act = hintros.get_function_from_string(func_str)
