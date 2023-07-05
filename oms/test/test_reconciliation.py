c8db37664d52dd2551feac71d1830a353a8177ea
hhxjqm <hhxjqm@gmail.com>
2023-07-05 04:19:15 -0400

    Update comment
    Signed-off-by: hhxjqm <hhxjqm@gmail.com>

--- oms/test/test_reconciliation.py
+++ oms/test/test_reconciliation.py
@@ -6,8 +6,16 @@ import oms.reconciliation as omreconc
 _LOG = logging.getLogger(__name__)
 
 
-class TestGetRunDate(hunitest.TestCase):
+# #############################################################################
+# TestGetRunDate1
+# #############################################################################
+
+
+class TestGetRunDate1(hunitest.TestCase):
     def test1(self) -> None:
+        """
+        Test that function is correctly extracted from a start timestamp.
+        """
         start_timestamp = "20231013_064500"
         act = omreconc.get_run_date(start_timestamp)
         exp = "20231013"
