c52a375962b6b3f6e19582462697a6db8a0ed496
dchoi127 <dchoi127@terpmail.umd.edu>
2023-07-10 11:31:30 -0400

    Changed approach
    copied similar approach to `amp/im_v2/common/notebooks/test/test_master_raw_data_gallery_notebook.py`

--- core/plotting/notebooks/test/test_gallery_notebook.py
+++ core/plotting/notebooks/test/test_gallery_notebook.py
@@ -3,41 +3,31 @@ import logging
 import pytest
 
 import core.config as cconfig
-import helpers.hsystem as hsystem
-import helpers.hunit_test as hunitest
-import research_amp.cc.algotrading as ramccalg
+import dev_scripts.notebooks as dsnrn
 
 _LOG = logging.getLogger(__name__)
 
-
-class TestSorrTask393Notebook(hunitest.TestCase):
-    @pytest.mark.slow
+@pytest.mark.superslow("~40 sec.")
+class TestGalleryNotebook(dsnrn.Test_Run_Notebook_TestCase):
     def test_run_notebook(self) -> None:
         """
         Test if gallery notebook runs end-to-end with no errors.
         """
         notebook_path = "core/plotting/notebooks/gallery_notebook.ipynb"
         config_func_path = "core.plotting.notebooks.test.test_gallery_notebook.build_config_list()"
-        dst_dir = "core/plotting/notebooks/test/outcomes"
-        #  Build the command with parameters.
-        cmd = f"""run_notebook.py \
-            --notebook {notebook_path} \
-            --config_builder "{config_func_path}" \
-            --dst_dir {dst_dir} \
-            --num_threads 1
-        """
         # Run the notebook.
-        hsystem.system(cmd)
+        self._test_run_notebook(notebook_path, config_func_path)
 
 
 def build_config_list() -> cconfig.ConfigList:
     """
-    Build the config to run the notebook.
+    Simple config builder for the test.
     """
-    config = ramccalg.get_default_config()
-    config["id"] = "0"
-    configs = [config]
-    config_list = cconfig.ConfigList(configs)
+    # We want to execute the notebook as it is, but config builder needs
+    # a config from the caller, which we ignore for now
+    config = {}
+    config = cconfig.Config()
+    config_list = cconfig.ConfigList([config])
     return config_list
 
 
