e87c61ad927aa59c7b1c9363af6235013b0efb76
zli00185 <zli00185@terpmail.umd.edu>
2023-07-15 03:00:31 +0800

    skeleton code

--- core/plotting/test/test_misc_plotting.py
+++ core/plotting/test/test_misc_plotting.py
@@ -17,21 +17,8 @@ class Test_plot_timeseries_distribution(unittest.TestCase):
         ]
         value = [1.1,1.2,1.3,1.4]
         srs = pd.Series(value, index = idx)
-        type = ["hour"]
+        type = ["hour","time"]
         cplmiplo.plot_timeseries_distribution(srs,type)
         
        #pytest core/plotting/test/test_misc_plotting.py::Test_plot_timeseries_distribution::test1
-    def test2(self)-> None:
-        """
-        Check time period
-        """
-        idx = [
-            pd.Timestamp("2000-01-01 9:01"),
-            pd.Timestamp("2000-01-01 9:02"),
-            pd.Timestamp("2000-01-01 9:03"),
-            pd.Timestamp("2000-01-01 9:04"),
-        ]
-        value = [0,0,0,0]
-        srs = pd.Series(value, index = idx)
-        type = []
-        cplmiplo.plot_timeseries_distribution(srs,type)
+ 
