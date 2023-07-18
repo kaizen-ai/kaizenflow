b0d400d37f1de57b8d131b2f5645fad552684445
dchoi127 <93415557+dchoi127@users.noreply.github.com>
2023-07-18 14:18:11 -0400

    PR for SorrTask395_Unit_test_plotting_gallery_notebook (#402)
    * Unit Test
    
    Completed unit test using dummy config builder from research_amp.cc.algotrading. Ran the unit test and it completed successfully.
    
    * Delete config.txt
    
    * Changed approach
    
    copied similar approach to `amp/im_v2/common/notebooks/test/test_master_raw_data_gallery_notebook.py`
    
    * Delete config.pkl
    
    * Static function/notebook changes
    
    changed implemented
    
    * Helper func
    
    created separate helper func
    
    * Calling helper in notebook
    
    Updated code in notebook to utilize helper in getting the series
    
    * Update gallery_notebook.py
    
    white space
    
    * Updates
    
    Implemented changes.
    
    * Delete run_notebook.0.log
    
    * Changing Unit testing notebook approach
    
    Changed structure of where test lies, as well as changing the notebookpath
    
    * linter
    
    ---------
    
    Co-authored-by: dan <d.yachmenev@crypto-kaizen.com>

--- core/plotting/test/test_plots.py
+++ core/plotting/test/test_plots.py
@@ -14,6 +14,17 @@ class Test_plots(unittest.TestCase):
     """
     Run smoke tests for plotting functions.
     """
+    @staticmethod
+    def get_plot_histograms_and_lagged_scatterplot1() -> pd.Series:
+        """
+        Get a random Gaussian data series for plotting histograms and lagged
+        scatterplot.
+        """
+        rng = np.random.default_rng(seed=0)
+        samples = rng.normal(size=100)
+        index = pd.date_range(start="2023-01-01", periods=len(samples), freq="D")
+        srs = pd.Series(samples, index=index)
+        return srs
 
     @staticmethod
     def get_plot_time_series_by_period1() -> pd.Series:
@@ -31,13 +42,9 @@ class Test_plots(unittest.TestCase):
         Smoke test for `plot_histograms_and_lagged_scatterplot()`.
         """
         # Set inputs.
-        seq = np.concatenate(
-            [np.random.uniform(-1, 1, 100), np.random.choice([5, 10], 100)]
-        )
-        index = pd.date_range(start="2023-01-01", periods=len(seq), freq="D")
-        srs = pd.Series(seq, index=index)
+        srs = self.get_plot_histograms_and_lagged_scatterplot1()
         lag = 7
-        # TODO(Dan): Remove after integration with `cmamp`
+        # TODO(Dan): Remove after integration with `cmamp`. Changes from Cm #4722 are not in `sorrentum` yet.
         figsize = (20, 20)
         # Plot.
         cpvistte.plot_histograms_and_lagged_scatterplot(srs, lag, figsize=figsize)
