cbd7e7f7b48b951978b2fe80202f81ad825663c5
Dylan-Celli <cellidylan@gmail.com>
2023-08-03 08:08:47 -0400

    Updating with PR comments and linting files

--- core/plotting/notebooks/gallery_notebook.py
+++ core/plotting/notebooks/gallery_notebook.py
@@ -28,17 +28,15 @@
 # %% run_control={"marked": false}
 import logging
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
+import core.plotting.misc_plotting as cplmiplo
 import core.plotting.visual_stationarity_test as cpvistte
-import core.plotting.misc_plotting as cpmiscplt
-
 import helpers.hdbg as hdbg
 import helpers.henv as henv
 import helpers.hprint as hprint
-
 
 # %% [markdown]
 # # Configure Logger
@@ -60,12 +58,14 @@ hprint.config_notebook()
 
 # %%
 # Set inputs.
-seq = np.concatenate([np.random.uniform(-1, 1, 100), np.random.choice([5, 10], 100)])
+seq = np.concatenate(
+    [np.random.uniform(-1, 1, 100), np.random.choice([5, 10], 100)]
+)
 index = pd.date_range(start="2023-01-01", periods=len(seq), freq="D")
 srs = pd.Series(seq, index=index)
 lag = 7
 # TODO(Dan): Remove after integration with `cmamp`
-figsize = (20,20)
+figsize = (20, 20)
 # Plot.
 cpvistte.plot_histograms_and_lagged_scatterplot(srs, lag, figsize=figsize)
 
@@ -73,7 +73,7 @@ cpvistte.plot_histograms_and_lagged_scatterplot(srs, lag, figsize=figsize)
 # ## `plot_projection()`
 
 # %%
-# Calling plot_projection to test that it's basic functionality works
+# Calling plot_projection to test that it's basic functionality works.
 data = [
     [1, 1, 0, 1],
     [0, 1, 0, 1],
@@ -81,24 +81,16 @@ data = [
     [1, 1, 1, 1],
     [1, 1, 1, 1],
 ]
-df = pd.DataFrame(data)
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-cpmiscplt.plot_projection(df, special_values = [0], ax = ax)
-
-# %%
-# Calling plot_projection with the scatter plot mode enabled
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-cpmiscplt.plot_projection(df, special_values = [1], mode = "scatter", ax = ax)
-
-# %%
 index = pd.date_range(start="2023-01-01", periods=len(data), freq="D")
-srs = pd.DataFrame(data, index=index)
+df = pd.DataFrame(data, index=index)
 fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-cpmiscplt.plot_projection(srs, special_values = [0], mode = "scatter", ax = ax)
+ax = fig.add_axes([0, 0, 1, 1])
+cplmiplo.plot_projection(df, special_values=[0], ax=ax)
 
 # %%
+# Calling plot_projection with the scatter plot mode enabled.
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+cplmiplo.plot_projection(df, special_values=[0], mode="scatter", ax=ax)
 
 # %%
--- core/plotting/test/test_plots.py
+++ core/plotting/test/test_plots.py
@@ -1,12 +1,12 @@
 import logging
 import unittest
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
+import core.plotting.misc_plotting as cplmiplo
 import core.plotting.visual_stationarity_test as cpvistte
-import core.plotting.misc_plotting as cpmiscplt
 
 _LOG = logging.getLogger(__name__)
 
@@ -17,12 +17,14 @@ class Test_plots(unittest.TestCase):
         Smoke test for `plot_histograms_and_lagged_scatterplot()`.
         """
         # Set inputs.
-        seq = np.concatenate([np.random.uniform(-1, 1, 100), np.random.choice([5, 10], 100)])
+        seq = np.concatenate(
+            [np.random.uniform(-1, 1, 100), np.random.choice([5, 10], 100)]
+        )
         index = pd.date_range(start="2023-01-01", periods=len(seq), freq="D")
         srs = pd.Series(seq, index=index)
         lag = 7
         # TODO(Dan): Remove after integration with `cmamp`
-        figsize = (20,20)
+        figsize = (20, 20)
         # Plot.
         cpvistte.plot_histograms_and_lagged_scatterplot(srs, lag, figsize=figsize)
 
@@ -40,7 +42,5 @@ class Test_plots(unittest.TestCase):
         index = pd.date_range(start="2023-01-01", periods=len(data), freq="D")
         srs = pd.Series(data, index=index)
         fig = plt.figure()
-        ax = fig.add_axes([0,0,1,1])
-        cpmiscplt.plot_projection(srs, special_values = [0], mode = "scatter", ax = ax)
-
-
+        ax = fig.add_axes([0, 0, 1, 1])
+        cplmiplo.plot_projection(srs, special_values=[0], mode="scatter", ax=ax)
