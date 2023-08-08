4a143250349e88630c0436e2d765cb74a9d87f84
hhxjqm <hhxjqm@gmail.com>
2023-08-08 17:28:16 +0800

    update
    Signed-off-by: hhxjqm <hhxjqm@gmail.com>

--- dev_scripts/process_telegram_collaborator.py
+++ dev_scripts/process_telegram_collaborator.py
@@ -8,6 +8,7 @@ import telegram
 from dotenv import load_dotenv
 
 import helpers.hdbg as hdbg
+import helpers.hparser as hparser
 
 load_dotenv()
 bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -15,7 +16,7 @@ _LOG = logging.getLogger(__name__)
 _TELEGRAM_API = "https://api.telegram.org/bot"
 
 
-def _get_invite_link(group_id) -> str:
+def _get_invite_link(group_id: str) -> str:
     """
     Create a invate link of the Telegram group.
     """
@@ -34,7 +35,10 @@ def _get_invite_link(group_id) -> str:
         )
     return None
 
-async def _invite_collaborator(bot, user_id, group_id) -> None:
+
+async def _invite_collaborator(
+    bot: telegram.Bot, user_id: str, group_id: str
+) -> None:
     """
     Invite a collaborator to Telegram.
     """
@@ -44,7 +48,9 @@ async def _invite_collaborator(bot, user_id, group_id) -> None:
     hdbg.dassert_is_not(message, None)
 
 
-async def _remove_collaborator(bot, user_id, group_id) -> None:
+async def _remove_collaborator(
+    bot: telegram.Bot, user_id: str, group_id: str
+) -> None:
     """
     Remove a collaborator from Telegram.
     """
@@ -56,26 +62,31 @@ def _parse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--action",
+        type=str,
         choices=["add", "remove", "unban"],
         required=True,
         help="Action to perform: add or remove",
     )
-    parser.add_argument("--username", required=True, help="Username of the user")
+    parser.add_argument(
+        "--username", type=str, required=True, help="Username of the user"
+    )
     parser.add_argument(
         "--groupid",
         type=str,
         required=True,
         help="Id of the group to add to",
     )
+    hparser.add_verbosity_arg(parser)
     return parser
 
 
-async def main(parser: argparse.ArgumentParser) -> None:
+async def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
     action = args.action
     username = args.username
     groupid = args.groupid
     bot = telegram.Bot(token=bot_token)
+    hdbg.init_logger(verbosity=args.log_level, use_exec_path=True)
     if action == "add":
         await _invite_collaborator(bot, username, groupid)
     elif action == "remove":
@@ -86,4 +97,4 @@ async def main(parser: argparse.ArgumentParser) -> None:
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(_parse()))
+    loop.run_until_complete(_main(_parse()))
