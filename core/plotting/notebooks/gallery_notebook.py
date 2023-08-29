5b2f7ebea6ca1dc25f2571fe8ccca624d11f03fa
Dylan-Celli <cellidylan@gmail.com>
2023-08-29 08:30:41 -0400

    Rerunning notebook for output

--- core/plotting/notebooks/gallery_notebook.py
+++ core/plotting/notebooks/gallery_notebook.py
@@ -188,7 +188,7 @@ cplmiplo.plot_spectrum(signal=test_df, axes=axes_flat)
 df = cptetepl.Test_plots.get_plot_projection1()
 
 # %%
-special_values = [1]
+special_values = [0]
 cplmiplo.plot_projection(df, special_values=special_values)
 
 # %%
@@ -197,7 +197,5 @@ fig = plt.figure()
 ax = fig.add_axes([0, 0, 1, 1])
 mode = "scatter"
 cplmiplo.plot_projection(df, mode=mode, ax=ax)
-
-
 
 # %%
