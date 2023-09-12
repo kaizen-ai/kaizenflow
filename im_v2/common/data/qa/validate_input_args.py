9d3f13708b9d973ff10e506053c86e3f100987f3
Sameep Pote <85573083+Sameep2808@users.noreply.github.com>
2023-09-12 13:03:27 -0400

    Sorr task538 centralize input validation when running etl scripts (#552)
    * Creating validate_input_args file and transfering the function #538
    
    * Modifying comments and import libraries #538
    
    * Fixing the comments #538
    
    * Adding validate function and moving init extractor back #538
    
    * Fixing if-else #538
    
    * Fixing if-else in download_bulk #538
    
    * Fixing the test fail #538
    
    ---------
    
    Co-authored-by: Juraj Smeriga <49269742+jsmerix@users.noreply.github.com>

---
+++ im_v2/common/data/qa/validate_input_args.py
@@ -0,0 +1,31 @@
+"""
+Centralize checking logical dependencies between values of various arguments
+for ETL scripts.
+
+Import as:
+
+import im_v2.common.data.qa.validate_input_args as imvcdqviar
+"""
+import argparse
+
+import helpers.hdbg as hdbg
+
+
+def validate_vendor_arg(vendor: str, args: argparse.Namespace) -> None:
+    """
+    Validate vendor args.
+
+    :param vendor: vendor name
+    :param args: ETL script args
+    :return: vendor data extractor
+    """
+    hdbg.dassert_in(
+        vendor,
+        ["binance", "ccxt", "crypto_chassis"],
+        msg=f"Vendor {vendor} is not supported.",
+    )
+    if vendor == "crypto_chassis":
+        if not args.get("universe_part"):
+            raise RuntimeError(
+                f"--universe_part argument is mandatory for {vendor}"
+            )
