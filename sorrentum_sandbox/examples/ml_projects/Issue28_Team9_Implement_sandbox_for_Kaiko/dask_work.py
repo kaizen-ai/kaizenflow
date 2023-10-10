fad23115faf482619763d3f05a9ba2e0ff136d56
Samarth KaPatel <samarth.kapatel5@gmail.com>
2023-10-10 10:06:11 -0400

    Kaiko_Deliverable_4 (#222)
    * Create test1
    
    * Remove test1
    
    * test
    
    * test
    
    * test docker
    
    * I upload a new file
    
    * 111
    
    * create 3 .sh files
    
    * Save to db connection
    
    * get connection with db
    
    * database
    
    * get connection with kaiko database
    
    * get connection with kaiko database
    
    * kaiko database connection
    
    * Issue28_Team9_Implement_sandbox_for_Kaiko
    
    * Issue28_Team9_Implement_sandbox_for_Kaiko
    
    * Update
    
    * Update
    
    * Remove copy of helpers. One can run Docker from one level app and tweak PYTHONPATH to pick app helpers
    
    * try airflow
    
    * kaiko test
    
    * kaiko test
    
    * download kaiko source code
    
    * fix download_kaiko
    
    * fix download_to_csv
    
    * fix download_to_db
    
    * Issue28 team9 implement sandbox for kaiko
    
    * data set
    
    * fix all database bugs
    
    * jupyter_notebook
    
    * fix download_to_db.py
    
    * fix download_to_db.py
    
    * try airflow
    
    * analysis
    
    * jupyter_notebook analysis
    
    * finish deliver3
    
    analysis
    
    jupyter_notebook analysis
    
    finish deliver3
    
    finish deliver3
    
    jupyter_notebook analysis
    
    * Jupyter Notebook
    
    * fix realtime ipynb
    
    * fix realtime ipynb
    
    * Create new branch
    
    * python file
    
    * python files
    
    * deliverable 4
    
    * dask work
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/helpers directory
    
    * Delete sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/data directory
    
    ---------
    
    Co-authored-by: Road <112990763+The-Road@users.noreply.github.com>
    Co-authored-by: The-Road <ywu12313@umd.edu>
    Co-authored-by: wangyula47 <wangyula47@gmail.com>
    Co-authored-by: JeremyWigton <jeremywigton@gmail.com>
    Co-authored-by: T22sri <tpavulur@umd.edu>
    Co-authored-by: saggese <saggese@gmail.com>
    Co-authored-by: The-Road <yzwu2000@163.com>

--- sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/dask_work.py
+++ sorrentum_sandbox/examples/ml_projects/Issue28_Team9_Implement_sandbox_for_Kaiko/dask_work.py
@@ -1,21 +1,13 @@
-"""
-Import as:
-
-import sorrentum_sandbox.examples.ml_projects.Issue28_Team9_Implement_sandbox_for_Kaiko.download_to_db as ssempitisfkdtd
-"""
-
-# kaiko
-
-
 import argparse
 import logging
 
-import db_kaiko as sisebidb
-import download_kaiko as sisebido
 import pandas as pd
 
 import helpers.hdbg as hdbg
 import helpers.hparser as hparser
+import db_kaiko as sisebidb
+import common.download as ssandown
+import data_analysis
 
 _LOG = logging.getLogger(__name__)
 
@@ -27,18 +19,11 @@ def _add_download_args(
     Add the command line options for exchange download.
     """
     parser.add_argument(
-        "--start_timestamp",
+        "--date",
         required=True,
         action="store",
         type=str,
         help="Beginning of the loaded period, e.g. 2022-02-09 10:00:00+00:00",
-    )
-    parser.add_argument(
-        "--end_timestamp",
-        action="store",
-        required=True,
-        type=str,
-        help="End of the loaded period, e.g. 2022-02-10 10:00:00+00:00",
     )
     parser.add_argument(
         "--target_table",
@@ -47,7 +32,6 @@ def _add_download_args(
         type=str,
         help="Name of the db table to save data into",
     )
-
     return parser
 
 def _parse() -> argparse.ArgumentParser:
@@ -64,16 +48,12 @@ def _parse() -> argparse.ArgumentParser:
 def _main(parser: argparse.ArgumentParser) -> None:
     args = parser.parse_args()
     # Load data.
-    start_timestamp = pd.Timestamp(args.start_timestamp)
-    end_timestamp = pd.Timestamp(args.end_timestamp)
-
-    downloader = sisebido.KaikoDownloader()
-
-    raw_data = downloader.download(start_timestamp, end_timestamp)
+    date = pd.Timestamp(args.date)
+    analysis_data = data_analysis.work(date)
     # Save data to DB.
     db_conn = sisebidb.get_db_connection()
     saver = sisebidb.PostgresDataFrameSaver(db_conn)
-    saver.save(raw_data, args.target_table)
+    saver.save(ssandown.RawData(analysis_data), args.target_table)
 
 
 if __name__ == "__main__":
