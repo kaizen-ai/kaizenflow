6d4cfd87157adc2a303e718fbcabca2500b94a09
Shabarish Nair <shabarishnair2020@gmail.com>
2023-11-13 16:40:09 -0500

    Created notebook and associated .py files

---
+++ linkedin/notebooks/retrieve_email_from_linkedin_profile.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Description
+#
+# This notebook is used for prototyping, testing, and researching different services that are able to retrieve emails from a LinkedIn profile. The goal is to optimize speed. Emails can be either personal or professional.
+
+# %%
