4b742d6507ef6db8aa5c3e42ba40dbe5675ff007
yiyunlei <im.yiyun.lei@gmail.com>
2023-11-15 03:16:01 -0500

    Resolve reviews

--- linkedin/extract_email/email_extraction.py
+++ linkedin/extract_email/email_extraction.py
@@ -2,34 +2,40 @@
 
 """
 Extract emails from LinkedIn profile URLs listed in the Excel sheet.
+
+Usage:
+    python email_extraction.py
+    Change the data_file_path, data_sheet_name, linkedin_username, and
+    linkedin_password in the code.
 """
 import random
-import subprocess
 import time
+from typing import List
 
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from tqdm import tqdm
 
-# Install selenium and openpyxl in the virtual environment.
-install_code = subprocess.call(
-    'sudo /bin/bash -c "(source /venv/bin/activate; pip install selenium)"',
-    shell=True,
-)
-install_code = subprocess.call(
-    'sudo /bin/bash -c "(source /venv/bin/activate; pip install openpyxl)"',
-    shell=True,
-)
+import helpers.hsystem as hsystem
 
 
-def setup_selenium_linkedin(username, password):
+def setup_selenium_linkedin(username: str, password: str) -> webdriver.Chrome:
+    """
+    Login to LinkedIn with Selenium.
+
+    :param username: LinkedIn username
+    :param password: LinkedIn password
+    :return: Selenium webdriver
+    """
     options = webdriver.ChromeOptions()
-    # Comment this line and send code if manual verification needed for the first time.
+    # Comment this line and send code if manual verification needed for the
+    # first time.
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.linkedin.com")
     time.sleep(5)
+    #
     username_input = driver.find_element(By.ID, "session_key")
     username_input.send_keys(username)
     password_input = driver.find_element(By.ID, "session_password")
@@ -38,7 +44,14 @@ def setup_selenium_linkedin(username, password):
     return driver
 
 
-def get_email_from_url(driver, url):
+def get_email_from_url(driver: webdriver.Chrome, url: str) -> str:
+    """
+    Get email from LinkedIn profile URL.
+
+    :param driver: Selenium webdriver
+    :param url: LinkedIn profile URL
+    :return: email
+    """
     driver.get(url + "/overlay/contact-info/")
     time.sleep(random.randrange(2, 4))
     try:
@@ -51,7 +64,14 @@ def get_email_from_url(driver, url):
     return email
 
 
-def get_email_list(url_list, username, password):
+def get_email_list(url_list: List, username: str, password: str) -> List[str]:
+    """
+    Get email list from the list of LinkedIn profile URLs.
+
+    :param url_list: list of LinkedIn profile URLs
+    :param username: LinkedIn username
+    :param password: LinkedIn password
+    """
     driver = setup_selenium_linkedin(username, password)
     email_list = []
     for url in tqdm(
@@ -64,20 +84,42 @@ def get_email_list(url_list, username, password):
     return email_list
 
 
-def add_email_to_sheet(filepath, sheet_name, username, password):
-    sheet_df = pd.read_excel(filepath, sheet_name=sheet_name)
-    url_list = list(sheet_df["profileUrl"])
-    email_list = get_email_list(url_list, username, password)
+def add_email_to_sheet(
+    email_list: List[str], filepath: str, sheet_name: str
+) -> None:
+    """
+    Add email list to the Excel sheet.
+
+    :param email_list: List of emails
+    :param filepath: Path to the Excel file
+    :param sheet_name: Name of the sheet in the Excel file
+    :return: None
+    """
     sheet_df["email"] = email_list
     with pd.ExcelWriter(filepath, mode="a", if_sheet_exists="replace") as writer:
         sheet_df.to_excel(writer, sheet_name=sheet_name, index=False)
 
 
 if __name__ == "__main__":
-    # LinkedIn username and password here
-    li_username = "your_username"
-    li_password = "your_password"
-    # This is the excel to be read.
-    li_filepath = "Search3-VCs_1st_degree_from_GP.LIn.xlsx"
-    li_sheet_name = "Sheet1"
-    add_email_to_sheet(li_filepath, li_sheet_name, li_username, li_password)
+    # Install selenium and openpyxl in the virtual environment.
+    cmd = 'sudo /bin/bash -c "(source /venv/bin/activate; pip install selenium)"'
+    hsystem.system(cmd, suppress_output=False, log_level="echo")
+    cmd = 'sudo /bin/bash -c "(source /venv/bin/activate; pip install openpyxl)"'
+    hsystem.system(cmd, suppress_output=False, log_level="echo")
+    #
+    # Source data file.
+    data_file_path = (
+        "linkedin/extract_email/Search3-VCs_1st_degree_from_GP.LIn.xlsx"
+    )
+    # Default sheet name is "Sheet1".
+    data_sheet_name = "ExportSearch"
+    # Add your LinkedIn username and password here.
+    linkedIn_username = "your username"
+    linkedIn_password = "your password"
+    #
+    sheet_df = pd.read_excel(data_file_path, sheet_name=data_sheet_name)
+    profile_url_list = list(sheet_df["profileUrl"])
+    email_list = get_email_list(
+        profile_url_list, linkedIn_username, linkedIn_password
+    )
+    add_email_to_sheet(email_list, data_file_path, data_sheet_name)
