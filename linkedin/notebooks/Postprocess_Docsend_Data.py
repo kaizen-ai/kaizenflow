a98887ef58bde5f285ccc90e60da55e74fc83ee8
Shabarish Nair <shabarishnair2020@gmail.com>
2023-12-09 18:31:48 -0500

    Added ability to get top 10 emails that viewed the presentation

--- linkedin/notebooks/Postprocess_Docsend_Data.py
+++ linkedin/notebooks/Postprocess_Docsend_Data.py
@@ -15,20 +15,66 @@
 # %% [markdown]
 # # Description
 #
-# This notebook was used for prototyping ways to postprocess docsend data that has been exported to a csv in GSheets.
+# This notebook was used for prototyping ways to postprocess docsend data that has been exported to a Google Sheet in GDrive.
+#
+# - getting the docsend data using this zapier connection might be good if used properly in conjunction with the GSheets API: https://zapier.com/apps/docsend/integrations/google-sheets/175058/create-google-sheet-rows-for-new-visits-in-docsend
+
+# %% [markdown]
+# ## Instructions
+#
+# - Run pip install gspread-pandas in terminal
+# - Choose between the GSheets and CSV methods
+#
+#
+# ### Method 1 - GSheets
+# - Follow the directions here for *Client Credentials* and follow the *Service Account Route* : https://gspread-pandas.readthedocs.io/en/latest/getting_started.html
+#     - Remember to save your credentials to the gspread_pandas folder
+# - Get the email of your service account
+#     - This can be found under Service Accounts header in the Credentials Tab of your project
+# - Share your Google Sheets file with the service account email
+# - Rename the gsheets_name variable to the name of the file
+# - Run notebook with Google Sheets mode
+#
+# ### Method 2 - CSV Download
+# - Download the CSV to the current directory(the place where this notebook exists)
+# - Rename the csv_file_name variable to the file name
 
 # %%
 #imports 
 
 import sys
-sys.path.append('../../helpers')
-
-from hgoogle_file_api import get_credentials, get_gdrive_service
-#import helpers.google_file_api as hgofiapi
+import pandas as pd
+import numpy as np
+from gspread_pandas import Spread, Client
+import gspread
 
 # %%
+## Names and external imports
+gsheets_name = "Kaizen - VC presentation - v1.5-export"
+csv_file_name = "docsend_data.csv"
 
-credentials = get_credentials()
-gdrive_service = get_gdrive_service()
+# %%
+## load data
+if (gsheets_name != ""):
+    spread = Spread(gsheets_name)    
+    data = spread.sheets[0].get_values()
+    headers = data.pop(0)
+    df#clean data#clean data = pd.DataFrame(data, columns=headers)
+else:
+    df = pd.read_csv(csv_file_name)
 
+# %%
+## clean data
+df['Duration'] = pd.to_timedelta(df['Duration'])
+df = df.replace(r'^\s*$', np.nan, regex=True)
+df
 
+# %%
+## compute data
+sorted_by_duration_df = df.sort_values(by='Duration', ascending=False)
+sorted_by_duration_df = sorted_by_duration_df.dropna(subset=['Email'])
+top_ten_emails = sorted_by_duration_df['Email'].unique()[:10]
+
+# %%
+## show data
+print(top_ten_emails)
