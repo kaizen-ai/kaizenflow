c4d1051e4f99c8460713e641a17b7212f3d8b7b9
Samarth <samarth.kapatel5@gmail.com>
2024-01-22 18:13:14 +0000

    update

--- helpers/hgit.py
+++ helpers/hgit.py
@@ -540,8 +540,8 @@ def _get_repo_short_to_full_name(include_host_name: bool) -> Dict[str, str]:
     """
     # From short name to long name.
     repo_map = {
-        "amp": "alphamatic/amp",
-        "dev_tools": "alphamatic/dev_tools",
+        "amp": "sorrentum/sorrentum",
+        "dev_tools": "sorrentum/dev_tools",
     }
     if include_host_name:
         host_name = "github.com"
