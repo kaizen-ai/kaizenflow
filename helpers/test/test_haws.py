cdb104ce8245165791f67b69f34ad1ed5d823f04
tkpratardan <krishna.pratardan@gmail.com>
2024-02-29 10:08:11 -0500

    Fixing inconsistencies

--- helpers/test/test_haws.py
+++ helpers/test/test_haws.py
@@ -1,9 +1,9 @@
-from moto import mock_s3
-import boto3
 import logging
-import pytest
-import io
 import os
+
+import boto3
+import pytest
+from moto import mock_s3
 
 import helpers.haws as haws
 import helpers.hunit_test as hunitest
@@ -12,7 +12,6 @@ _LOG = logging.getLogger(__name__)
 
 
 class test_get_service_resource(hunitest.TestCase):
-
     @pytest.fixture(autouse=True, scope="class")
     def aws_credentials(self):
         """
@@ -29,19 +28,17 @@ class test_get_service_resource(hunitest.TestCase):
         """
         Test that get_service_resource correctly retrieves an S3 resource.
         """
-        aws_profile = "__mock__"  
+        aws_profile = "__mock__"
         service_name = "s3"
 
-       #Creating a mock s3 bucket. 
-        s3 = boto3.resource('s3')
-        s3.create_bucket(Bucket='my-test-bucket')
+        # Creating a mock s3 bucket.
+        s3 = boto3.resource("s3")
+        s3.create_bucket(Bucket="my-test-bucket")
 
-        
         s3_resource = haws.get_service_resource(aws_profile, service_name)
 
         buckets = list(s3_resource.buckets.all())
         bucket_names = [bucket.name for bucket in buckets]
 
         # Assert
-        self.assertIn('my-test-bucket', bucket_names)
-
+        self.assertIn("my-test-bucket", bucket_names)
