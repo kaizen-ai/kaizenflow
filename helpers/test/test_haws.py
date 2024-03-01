f6a9ab6c2996f516273d4fe43d84e9e3a47a78bb
nehalR777 <nehalrajesh10@gmail.com>
2024-03-01 13:45:39 -0500

    Fix errors (mocking)

--- helpers/test/test_haws.py
+++ helpers/test/test_haws.py
@@ -4,6 +4,7 @@ import os
 import boto3
 import pytest
 from moto import mock_ecs
+from unittest.mock import patch
 
 import helpers.haws as haws
 import helpers.hunit_test as hunitest
@@ -24,18 +25,20 @@ class Test_update_task_definition(hunitest.TestCase):
         os.environ["MOCK_AWS_DEFAULT_REGION"] = "us-east-1"
 
     @mock_ecs
-    def test1(self) -> None:
+    @patch('helpers.haws.get_ecs_client')
+    def test1(self, mock_get_ecs_client) -> None:
         # Mock data
         task_definition_name = "my-task-definition"
         old_image_url = "old_image_url"
         new_image_url = "new_image_url"
         region = "us-east-1"
 
-        # Create ECS client using boto3
-        ecs_client = boto3.client("ecs", region_name=region)
+        # Mock the return value of get_ecs_client
+        mock_client = boto3.client("ecs", region_name=region)
+        mock_get_ecs_client.return_value = mock_client
 
         # Create a mock task definition
-        ecs_client.register_task_definition(
+        mock_client.register_task_definition(
             family=task_definition_name,
             containerDefinitions=[
                 {"name": "my-container", "image": old_image_url}
@@ -53,7 +56,7 @@ class Test_update_task_definition(hunitest.TestCase):
         )
 
         # Check if the task definition is updated
-        task_description = ecs_client.describe_task_definition(
+        task_description = mock_client.describe_task_definition(
             taskDefinition=task_definition_name
         )
         updated_image_url = task_description["taskDefinition"][
