80b3d7360f85ddcd405ca2da87aa1fc84f96b168
nehalR777 <nehalrajesh10@gmail.com>
2024-03-01 22:12:34 -0500

    Revert changes to the code

--- helpers/test/test_haws.py
+++ helpers/test/test_haws.py
@@ -12,17 +12,28 @@ import helpers.hunit_test as hunitest
 _LOG = logging.getLogger(__name__)
 
 
-class TestUpdateTaskDefinition(hunitest.TestCase):
+class Test_update_task_definition(hunitest.TestCase):
+    @pytest.fixture(autouse=True, scope="class")
+    def aws_credentials(self) -> None:
+        """
+        Mocked AWS Credentials for moto.
+        """
+        os.environ["MOCK_AWS_ACCESS_KEY_ID"] = "testing"
+        os.environ["MOCK_AWS_SECRET_ACCESS_KEY"] = "testing"
+        os.environ["MOCK_AWS_SECURITY_TOKEN"] = "testing"
+        os.environ["MOCK_AWS_SESSION_TOKEN"] = "testing"
+        os.environ["MOCK_AWS_DEFAULT_REGION"] = "us-east-1"
+
     @mock_ecs
     @patch('helpers.haws.get_ecs_client')
-    def test_update_task_definition(self, mock_get_ecs_client) -> None:
+    def test1(self, mock_get_ecs_client) -> None:
         # Mock data
         task_definition_name = "my-task-definition"
         old_image_url = "old_image_url"
         new_image_url = "new_image_url"
         region = "us-east-1"
 
-        # Create a mock ECS client
+        # Mock the return value of get_ecs_client
         mock_client = boto3.client("ecs", region_name=region)
         mock_get_ecs_client.return_value = mock_client
 
@@ -39,7 +50,7 @@ class TestUpdateTaskDefinition(hunitest.TestCase):
             memory="512",
         )
 
-        # Call the function under test
+        # Update task definition
         haws.update_task_definition(
             task_definition_name, new_image_url, region=region
         )
@@ -48,5 +59,7 @@ class TestUpdateTaskDefinition(hunitest.TestCase):
         task_description = mock_client.describe_task_definition(
             taskDefinition=task_definition_name
         )
-        updated_image_url = task_description["taskDefinition"]["containerDefinitions"][0]["image"]
-        assert updated_image_url == new_image_url, f"Expected image URL: {new_image_url}, Actual image URL: {updated_image_url}"
+        updated_image_url = task_description["taskDefinition"][
+            "containerDefinitions"
+        ][0]["image"]
+        self.assertEqual(updated_image_url, new_image_url)
