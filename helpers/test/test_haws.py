dc84a4e426ab368819954ce18d30f4077c131703
tkpratardan <krishna.pratardan@gmail.com>
2024-03-07 23:22:19 -0500

    Initial Commit

--- helpers/test/test_haws.py
+++ helpers/test/test_haws.py
@@ -28,11 +28,11 @@ class Test_get_service_client(hunitest.TestCase):
         aws_profile = "__mock__"
         service_name = "s3"
         region = "us-east-1"
-        # Create mock client for s3
+        # Create mock client for s3.
         client = haws.get_service_client(
             aws_profile=aws_profile, service_name=service_name, region=region
         )
         # Check that the returned client is for the S3 service.
         hdbg.dassert_eq(client.meta.service_model.service_name, "s3")
-        # Check for region
+        # Check for region.
         hdbg.dassert_eq(client.meta.region_name, region)
