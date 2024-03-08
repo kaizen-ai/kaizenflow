8e595bf92b000099dee8ba562c37e71272447874
samarth9008 <samarthpatel9008@gmail.com>
2024-03-07 19:33:25 -0500

    update

--- sorrentum_sandbox/devops/init_airflow_setup.sh
+++ sorrentum_sandbox/devops/init_airflow_setup.sh
@@ -6,19 +6,27 @@ AIRFLOW_USER=airflow
 AIRFLOW_PASSWORD=airflow
 AIRFLOW_USER_EMAIL=airflow@airflow.com
 
-CONTAINER_NAME="airflow_cont"
+WEBSERVER_CONTAINER_NAME="airflow_cont"
+SCHEDULER_CONTAINER_NAME="airflow_scheduler_cont"
 
-echo "# Initializing Airflow DB setup"
+echo "# Initializing Airflow DB setup in webserver"
 docker exec \
     -ti \
-    $CONTAINER_NAME \
+    $WEBSERVER_CONTAINER_NAME \
+    airflow db migrate && \
+    echo "Initialized airflow DB"
+
+echo "# Initializing Airflow DB setup in scheduler"
+docker exec \
+    -ti \
+    $SCHEDULER_CONTAINER_NAME \
     airflow db migrate && \
     echo "Initialized airflow DB"
 
 echo "# Setting up Admin user with username $AIRFLOW_USER"
 docker exec \
     -ti \
-    $CONTAINER_NAME \
+    $WEBSERVER_CONTAINER_NAME \
     airflow users create \
       --role Admin \
       --username $AIRFLOW_USER --password $AIRFLOW_PASSWORD \
