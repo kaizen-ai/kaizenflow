5a96776894f0d083cdc60583450b02f16253af78
nehalR777 <nehalrajesh10@gmail.com>
2024-03-10 01:15:59 -0500

    regex , git root changes pushed

--- dev_scripts/jackdoc.py
+++ dev_scripts/jackdoc.py
@@ -14,40 +14,54 @@ import dev_scripts.jackdoc as jackdoc
 import argparse
 import logging
 import os
+import subprocess
 import urllib.parse
+import re
 
 _LOG = logging.getLogger(__name__)
 
-# Define the root directory where Markdown files are located
-DOCS_DIR = "/src/sorrentum1/docs"
+# Define the relative path to the docs directory
+DOCS_DIR = "docs"
 
 
-def _parse() -> argparse.ArgumentParser:
+def _parse():
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
-    parser.add_argument("search_term", help="Input to search in Markdown files")
+    parser.add_argument("search_term", help="Input to search in Markdown files (can be a regex)")
     return parser
 
-def _main(parser: argparse.ArgumentParser) -> None:
+def _get_git_root():
+    try:
+        # Run git command to get the root directory of the repository
+        git_root = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode().strip()
+        return git_root
+    except subprocess.CalledProcessError:
+        raise RuntimeError("Not a Git repository or Git is not installed.")
+
+
+def _main(parser):
     args = parser.parse_args()
     search_term = args.search_term
 
+    git_root = _get_git_root()
+    docs_path = os.path.join(git_root, DOCS_DIR)
+
     found_in_files = []
-    for root, dirs, files in os.walk(DOCS_DIR):
+    for root, dirs, files in os.walk(docs_path):
         for file in files:
             if file.endswith(".md"):
                 md_file = os.path.join(root, file)
                 with open(md_file, 'r') as f:
                     lines = f.readlines()
                     for line_num, line in enumerate(lines, start=1):
-                        if search_term in line:
+                        if re.search(search_term, line):
                             found_in_files.append((md_file, line_num))
 
     if found_in_files:
         print("Input found in the following Markdown files:")
         for file_path, line_num in found_in_files:
-            url = f"https://github.com/sorrentum/sorrentum/blob/master/docs/{file_path[len(DOCS_DIR) + 1:]}?plain=1#L{line_num}"
+            url = "{}/{}#L{}".format(git_root, file_path[len(git_root) + 1:], line_num)
             print(url)
     else:
         print("Input not found in any Markdown files.")
