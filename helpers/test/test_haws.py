01716fa9bf398a1635b44d250fd0f3d6548e6da0
Swathi Baskaran <46857476+11swathi@users.noreply.github.com>
2024-03-11 08:39:41 -0400

    SorrTask696_unit_test_get_task_definition_image_url (#711)
    * Unit test for get_task_definition_image_url
    
    * Chnages after review
    
    * Chnages after review
    
    * Updated type hint
    
    * Updated type hint
    
    * Updated type hint
    
    * Changed type hints
    
    * Changed type hints
    
    * Changed assert
    
    * Resolved merge conflicts
    
    ---------
    
    Co-authored-by: Swathi Baskaran <swathibaskaran@Swathis-MacBook-Pro.local>
    Co-authored-by: Swathi Baskaran <swathibaskaran@wireless-10-105-92-196.umd.edu>
    Co-authored-by: GP Saggese <saggese@gmail.com>

--- helpers/test/test_haws.py
+++ helpers/test/test_haws.py
@@ -90,6 +90,42 @@ class Test_get_service_resource(hunitest.TestCase):
         hdbg.dassert_in("my-test-bucket", bucket_names)
 
 
+class Test_get_task_definition_image_url(hunitest.TestCase):
+    @pytest.fixture(autouse=True, scope="class")
+    def aws_credentials(self) -> None:
+        """
+        Mocked AWS credentials for moto.
+        """
+        os.environ["MOCK_AWS_ACCESS_KEY_ID"] = "testing"
+        os.environ["MOCK_AWS_SECRET_ACCESS_KEY"] = "testing"
+        os.environ["MOCK_AWS_SECURITY_TOKEN"] = "testing"
+        os.environ["MOCK_AWS_SESSION_TOKEN"] = "testing"
+        os.environ["MOCK_AWS_DEFAULT_REGION"] = "us-east-1"
+
+    @mock_ecs
+    @umock.patch("helpers.haws.get_service_client")
+    def test1(self, mock_get_service_client: umock.Mock) -> None:
+        """
+        Test that `get_task_definition_image_url` retrieves correct image URL.
+        """
+        # Mock data.
+        task_definition_name = "my-task-definition"
+        mock_image_url = "old_image_url"
+        region = "us-east-1"
+        # Mock the return value of `get_service_client`.
+        mock_client = boto3.client("ecs", region_name=region)
+        mock_get_service_client.return_value = mock_client
+        # Create a mock task definition.
+        mock_client.register_task_definition(
+            family=task_definition_name,
+            containerDefinitions=[
+                {"name": "my-container", "image": mock_image_url}
+            ],
+        )
+        image_url = haws.get_task_definition_image_url(task_definition_name)
+        self.assertEqual(image_url, mock_image_url)
+
+
 class Test_update_task_definition(hunitest.TestCase):
     @pytest.fixture(autouse=True, scope="class")
     def aws_credentials(self) -> None:
