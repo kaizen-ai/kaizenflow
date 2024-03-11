3c9aba1ad53ff39c56feafaead2ce545f37a3fa0
Nehal Pillai <nehalrajesh10@gmail.com>
2024-03-11 08:12:18 -0400

    SorrTask697_Unit_test_update_task_definition (#709)
    * Added unit test case for update task definition
    
    * code changes
    
    * Changes made to the code
    
    * Review changes made
    
    * minor correction
    
    * minor improvements
    
    * Review changes made
    
    * Changes made as per review
    
    * Review changes made
    
    * type hints added
    
    * Linter check
    
    * Make changes to code
    
    * Resoved changes
    
    * Made changes to use self assert
    
    * merge conflicts resolved
    
    * update test definition
    
    * resolve conflicts
    
    ---------
    
    Co-authored-by: tkpratardan <krishna.pratardan@gmail.com>
    Co-authored-by: GP Saggese <saggese@gmail.com>

--- helpers/test/test_haws.py
+++ helpers/test/test_haws.py
@@ -1,9 +1,11 @@
 import os
+import unittest.mock as umock
 from typing import Optional
 
 import boto3
 import pytest
-from moto import mock_s3
+from botocore.client import BaseClient
+from moto import mock_ecs, mock_s3
 
 import helpers.haws as haws
 import helpers.hdbg as hdbg
@@ -86,3 +88,55 @@ class Test_get_service_resource(hunitest.TestCase):
         bucket_names = [bucket.name for bucket in buckets]
         # Check.
         hdbg.dassert_in("my-test-bucket", bucket_names)
+
+
+class Test_update_task_definition(hunitest.TestCase):
+    @pytest.fixture(autouse=True, scope="class")
+    def aws_credentials(self) -> None:
+        """
+        Mocked AWS credentials for moto.
+        """
+        os.environ["MOCK_AWS_ACCESS_KEY_ID"] = "testing"
+        os.environ["MOCK_AWS_SECRET_ACCESS_KEY"] = "testing"
+        os.environ["MOCK_AWS_SECURITY_TOKEN"] = "testing"
+        os.environ["MOCK_AWS_SESSION_TOKEN"] = "testing"
+        os.environ["MOCK_AWS_DEFAULT_REGION"] = "us-east-1"
+
+    @mock_ecs
+    @umock.patch("helpers.haws.get_ecs_client")
+    def test1(self, mock_get_ecs_client: BaseClient) -> None:
+        """
+        Test updating a task definition with a new image URL.
+        """
+        # Mock data.
+        task_definition_name = "my-task-definition"
+        old_image_url = "old_image_url"
+        new_image_url = "new_image_url"
+        region = "us-east-1"
+        # Mock the return value of `get_ecs_client`.
+        mock_client = boto3.client("ecs", region_name=region)
+        mock_get_ecs_client.return_value = mock_client
+        # Create a mock task definition.
+        mock_client.register_task_definition(
+            family=task_definition_name,
+            containerDefinitions=[
+                {"name": "my-container", "image": old_image_url}
+            ],
+            executionRoleArn="__mock__",
+            networkMode="bridge",
+            requiresCompatibilities=["EC2"],
+            cpu="256",
+            memory="512",
+        )
+        # Update task definition.
+        haws.update_task_definition(
+            task_definition_name, new_image_url, region=region
+        )
+        # Check if the task definition is updated.
+        task_description = mock_client.describe_task_definition(
+            taskDefinition=task_definition_name
+        )
+        updated_image_url = task_description["taskDefinition"][
+            "containerDefinitions"
+        ][0]["image"]
+        self.assertEqual(updated_image_url, new_image_url)
