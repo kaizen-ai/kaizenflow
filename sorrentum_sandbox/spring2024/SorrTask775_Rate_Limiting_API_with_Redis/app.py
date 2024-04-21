a28d527ea32ec09b45be074aa5f1b292584e6d0a
Bodarnn <103896552+Bodarnn@users.noreply.github.com>
2024-04-20 21:27:19 -0400

    Update app.py

--- sorrentum_sandbox/spring2024/SorrTask775_Rate_Limiting_API_with_Redis/app.py
+++ sorrentum_sandbox/spring2024/SorrTask775_Rate_Limiting_API_with_Redis/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -7,17 +7,18 @@ app = Flask(__name__)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    storage_uri="memory://"
+    storage_uri='redis://redis:6379'
+    # storage_uri='memory://'
 )
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/api")
-@limiter.limit("3/second")
+@app.route('/api')
+@limiter.limit('3/second')
 def api():
-    return "Hello World!"
+    return jsonify({'foo': 'bar', 'baz': 'qux'})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
