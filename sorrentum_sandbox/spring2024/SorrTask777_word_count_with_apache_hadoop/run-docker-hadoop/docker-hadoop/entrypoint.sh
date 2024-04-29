54513fb0ea4adc2a9a8c04776e1fe6847f3a9a28
Catherine Ripley <katieripley@wireless-10-104-229-131.umd.edu>
2024-04-29 18:58:40 -0400

    fixed JAVA_HOME env

--- sorrentum_sandbox/spring2024/SorrTask777_word_count_with_apache_hadoop/run-docker-hadoop/docker-hadoop/entrypoint.sh
+++ sorrentum_sandbox/spring2024/SorrTask777_word_count_with_apache_hadoop/run-docker-hadoop/docker-hadoop/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Set Java and Hadoop environment variables
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
 export HADOOP_INSTALL=/usr/local/hadoop
 
 # Update PATH to include Java and Hadoop binaries
@@ -10,9 +10,11 @@ export PATH=$JAVA_HOME/bin:$HADOOP_INSTALL/bin:$HADOOP_INSTALL/sbin:$PATH
 
 # Verify Java executable path
 echo "Java executable path: $(which java)"
+echo "Java version: $(java -version)"
 
 # Verify Hadoop installation directory
 echo "Hadoop installation directory: $HADOOP_INSTALL"
+echo "Hadoop version: $(hadoop version)"
 
 # Start HDFS or YARN if needed
 if [ "$1" = 'hdfs' ]; then
