3c449add45a9389001760c71ac6eea94e9ab2470
Rick <rpowell07@gmail.com>
2024-05-01 18:32:00 -0400

    updating all files

--- sorrentum_sandbox/spring2024/SorrTask774_Graph_traversal_with_Gremlin/README.md
+++ sorrentum_sandbox/spring2024/SorrTask774_Graph_traversal_with_Gremlin/README.md
@@ -4,8 +4,6 @@ This is where I put all my notes about my Spring 2024 DATA605 Final Project, Gra
 
 **Project Description:** Begin by installing Apache TinkerPop's Gremlin Python library (gremlinpython). Next, create a sample graph structure using Gremlin's graph traversal API. Define vertices and edges, and establish connections between them to form a graph. Then, use Gremlin queries to traverse the graph, retrieving vertices, edges, and their properties. Experiment with various traversal strategies and filtering techniques to explore different aspects of the graph.
 
-Does anyone really know how Docker works? I *believe* the answer may be no.
-
 These images from DockerHub might be needed. Figure it out!
 docker pull tinkerpop/gremlin-server
 docker pull tinkerpop/gremlin-console
@@ -15,3 +13,21 @@ docker build -t rick-gremlin .
 
 and then run our docker container:
 docker run -d -p 8182:8182 rick-gremlin
+
+Actual steps to run:
+docker-compose build
+docker-compose up
+
+docker system prune --force --all --volumes
+
+Now the gremlin-python-1 is running before the gremlin-server-1, but I can click run again, and it will work properly. I need to check if there is a way to have the gremlin-server-1 run first, because my docker-compose does say that gremlin-python depends_on gremlin-server, but it doesn't appear to be working as I expected it to.
+
+I am running into an issue where I am able to create a gremlin server inside the docker, but when I run the container, the python file parts are not working. I believe it has something to do with the fact I'm using the gremlin server instead of the console? But I would love to be able to run the code and get back the proper results instead of just:
+
+2024-04-29 19:08:56 Vertices: [['V']]
+2024-04-29 19:08:56 Edges: [['E']]
+2024-04-29 19:08:56 People known by Alpha: [['V'], ['has', 'name', 'Alpha'], ['out', 'knows']]
+2024-04-29 19:08:56 People known by Beta: [['V'], ['has', 'name', 'Beta'], ['out', 'knows']]
+2024-04-29 19:08:56 People: [['V'], ['hasLabel', 'person']]
+
+I have created a list of vertices that I can call individually using print, but I can not get the gremlinpython functions to work.
--- sorrentum_sandbox/spring2024/SorrTask774_Graph_traversal_with_Gremlin/gremlin.py
+++ sorrentum_sandbox/spring2024/SorrTask774_Graph_traversal_with_Gremlin/gremlin.py
@@ -1,39 +1,88 @@
+from gremlin_python import statics
 from gremlin_python.structure.graph import Graph
-from gremlin_python.process.traversal import T
+import nest_asyncio 
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.anonymous_traversal import traversal
+from gremlin_python.process.graph_traversal import __
 
-graph = Graph()
-g = graph.traversal()
+nest_asyncio.apply()  # to support gremlin event loop in jupyter lab
 
-# Add vertices (adding .next() is in all examples, but the codes does not work with it)
-v1 = g.addV('person').property('name', 'Alpha')
-v2 = g.addV('person').property('name', 'Beta')
-v3 = g.addV('person').property('name', 'Delta')
+# set the graph traversal from the local machine:
+connection = DriverRemoteConnection("ws://gremlin-server:8182/gremlin", "g")  # Connect it to your local server
+g = traversal().withRemote(connection)
 
-# Print the list of Vertices to test
-print('Vertices: ', v1, '&', v2, '&', v3)
-'''# Add edges (assuming no weight property for now)
-e1 = g.V(v1).addE('knows').to(v2)
-e2 = g.V(v1).addE('knows').to(v3)
+# Add vertices
+v1 = g.addV('person').property('name', 'Alpha').property('age', 22).next()
+v2 = g.addV('person').property('name', 'Beta').property('age', 34).next()
+v3 = g.addV('person').property('name', 'Gamma').property('age', 28).next()
+v4 = g.addV('person').property('name', 'Delta').property('age', 40).next()
+v5 = g.addV('person').property('name', 'Epsilon').property('age', 45).next()
+v6 = g.addV('dog').property('name', 'Zeta').property('age', 5).next()
+v7 = g.addV('dog').property('name', 'Eta').property('age', 2).next()
+v8 = g.addV('dog').property('name', 'Theta').property('age', 9).next()
+
+# Add edges with weights
+e1 = g.V(v1).addE('knows').to(__.V(v2)).property('weight', 5).next()
+e2 = g.V(v1).addE('knows').to(__.V(v3)).property('weight', 3).next()
+e3 = g.V(v1).addE('knows').to(__.V(v5)).property('weight', 2).next()
+e4 = g.V(v2).addE('knows').to(__.V(v3)).property('weight', 7).next()
+e5 = g.V(v4).addE('knows').to(__.V(v5)).property('weight', 4).next()
+e6 = g.V(v4).addE('knows').to(__.V(v6)).property('weight', 6).next()
+e7 = g.V(v5).addE('knows').to(__.V(v1)).property('weight', 9).next()
+e8 = g.V(v5).addE('knows').to(__.V(v1)).property('weight', 10).next()
+e9 = g.V(v5).addE('knows').to(__.V(v2)).property('weight', 8).next()
+e10 = g.V(v5).addE('knows').to(__.V(v3)).property('weight', 6).next()
+e11 = g.V(v5).addE('knows').to(__.V(v4)).property('weight', 3).next()
+e12 = g.V(v5).addE('knows').to(__.V(v6)).property('weight', 5).next()
 
 # Traverse the graph to get all vertices
-vertices = g.V().valueMap(True).toList()
+vertices = g.V().toList()
 print("Vertices:", vertices)
 
+# Traverse the graph to get the names of all vertices
+names = g.V().values('name').toList()
+print("Vertex names:")
+# Print the names of all vertices
+for name in names:
+    print(name)
+
 # Traverse the graph to get all edges
-edges = g.E().valueMap().toList()
+edges = g.E().toList()
 print("Edges:", edges)
 
-# Example traversal: Find all people known by Alice
-result = g.V().has('name', 'Alice').out('knows').valueMap().toList()
-print("People known by Alice:", result)
+# Example traversal: Find all people known by Alpha
+result = g.V().has('name', 'Alpha').out('knows').toList()
+print("People known by Alpha:", result)
+
+# Example traversal: Find all people known by Beta
+result = g.V().has('name', 'Beta').out('knows').toList()
+print("People known by Beta:", result)
+
+# Example traversal: Find all people known by Gamma
+result = g.V().has('name', 'Gamma').out('knows').toList()
+print("People known by Gamma:", result)
+
+# Example traversal: Find all people known by Delta
+result = g.V().has('name', 'Delta').out('knows').toList()
+print("People known by Delta:", result)
+
+# Example traversal: Find all people known by Epsilon
+result = g.V().has('name', 'Epsilon').out('knows').toList()
+print("People known by Epsilon:", result)
 
 # Example traversal: Find all vertices of type 'person'
-people = g.V().hasLabel('person').valueMap().toList()
-print("People:", people)
+people = g.V().hasLabel('person').toList()
+print("List of People:", people)
 
-# Example traversal: Find all edges with weight greater than 5
-# Adjust this according to your actual data model
-# high_weight_edges = g.E().has('weight', T.gt(5)).valueMap().toList()
-# print("Edges with weight > 5:", high_weight_edges)'''
+# Traverse the graph to get all vertices with age greater than 30
+# age_gt_30 = g.V().has('age', __.gt(30)).toList()
+# print("People above the age of 30:", age_gt_30)
 
-print('Oh Hell yes! This is working.')
+# Traverse the graph to get all edges with weight greater than 5 (Figure out how traversal T works)
+# high_weight_edges = g.E().has('weight', T.gt(5))
+# print("Edges with weight > 5:", high_weight_edges)
+
+# Close the connection
+connection.close()
+
+print('**TEST** This is working.')
