db87c64b9b3aa9f5df1b885b24e758e9b43e4377
Swathi Baskaran <swathibaskaran@Swathis-MacBook-Pro.local>
2024-05-06 16:39:52 -0400

    Cleaned the code

--- sorrentum_sandbox/spring2024/chatbot/app.py
+++ sorrentum_sandbox/spring2024/chatbot/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import redis
 
-# Initialize Redis client
+# Initialize Redis client.
 redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
@@ -19,32 +19,25 @@ def index():
 def chat():
     msg = request.form["msg"]
     input_text = msg
-
-    # Check if the user query exists in Redis cache
+    # Check if the user query exists in Redis cache.
     if redis_client.exists(input_text):
-        # If yes, retrieve the response from Redis
+        # If yes, retrieve the response from Redis.
         return redis_client.get(input_text).decode('utf-8')
     else:
-        # If not, generate a response and store it in Redis
+        # If not, generate a response and store it in Redis.
         response = get_chat_response(input_text)
         redis_client.set(input_text, response)
         return response
 
 def get_chat_response(text):
-    # Let's chat for 5 lines
     for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        # encode the new user input, add the eos_token and return a tensor in Pytorch.
         new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
-
-        # append the new user input tokens to the chat history
+        # append the new user input tokens to the chat history.
         bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-        # generated a response while limiting the total chat history to 1000 tokens
+        # generated a response while limiting the total chat history to 1000 tokens.
         chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-        # decode and return the response
-        response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        
+        response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)        
     return response
 
 if __name__ == '__main__':
