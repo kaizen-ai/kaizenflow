56607ebcdd64144817f3c5caaf5ef4518f725b31
Shigure-kai-2 <qq1205159534@outlook.com>
2024-05-10 22:54:19 -0400

    Fix bug and add random forest

--- sorrentum_sandbox/projects/SorrTask822_Regression_Analysis_with_Apache_Spark_MLlib/MLlib.py
+++ sorrentum_sandbox/projects/SorrTask822_Regression_Analysis_with_Apache_Spark_MLlib/MLlib.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.ml.regression import LinearRegression
-from pyspark.ml.regression import RandomForestRegressionModel
-from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.feature import VectorAssembler
 from ucimlrepo import fetch_ucirepo
 import numpy as np
@@ -28,18 +28,31 @@ data = assembler.transform(data)
 # Split the train set and test set using the RandomSplit function
 train, test = data.randomSplit([0.7, 0.3])
 
-# Importing LR model from the Spark library
-LR = LinearRegression(featuresCol='features', labelCol='class')
+# Importing Logistic Regression model from the Spark library
+LR = LogisticRegression(featuresCol='features', labelCol='class')
 
 # Train the model
-model = LR.fit(train)
+model_lr = LR.fit(train)
 
 # Get the evaluation of model
-eva = model.evaluate(test)
+eva = model_lr.evaluate(test)
 
-# Output the MSE and r2
-print(eva.meanSquaredError)
-print(eva.r2)
+# Get the result of data
+result = eva.predictions
+
+# Output the accuracy
+print("The accuracy for logistic regression is %f.\n" % eva.accuracy)
+
+# Importing Random Forest model
+RF = RandomForestClassifier(featuresCol='features', labelCol='class', numTrees=50)
+
+# Train the model
+model_rf = RF.fit(train)
 
 # Get the prediction of test set label
-pred = model.transform(test)
+prediction = model_rf.transform(test)
+evaluator = MulticlassClassificationEvaluator(
+    labelCol='class', metricName='accuracy'
+)
+accuracy = evaluator.evaluate(prediction)
+print("The accuracy for Random Forest is %f.\n" % accuracy)
