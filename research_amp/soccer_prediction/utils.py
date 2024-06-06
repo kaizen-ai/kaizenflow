779a69f34115afcae53ea99d85ec44b7af88c936
Krishna P Taduri <40231735+tkpratardan@users.noreply.github.com>
2024-06-06 18:05:54 -0400

    SorrTask1020_compute_hit_rate (#1025)
    * Initial commit
    
    * checkpoint
    
    * change

--- research_amp/soccer_prediction/utils.py
+++ research_amp/soccer_prediction/utils.py
@@ -20,6 +20,7 @@ def download_data_from_s3(
     bucket_name: str,
     dataset_path: str,
     local_path: str,
+    file_format: str = ".txt",
     logging_level: int = logging.INFO,
 ) -> None:
     """
@@ -46,8 +47,8 @@ def download_data_from_s3(
         hdbg.dassert_eq(0, len(objects), msg, s3_path)
     for obj in bucket.objects.filter(Prefix=dataset_path):
         key = obj.key
-        # Select the files that end with `.txt` format.
-        if key.endswith(".txt"):
+        # Select the files that end with specified format.
+        if key.endswith(file_format):
             local_file_path = os.path.join(local_path, os.path.basename(key))
             _LOG.log(logging_level, f"Downloading {key} to {local_file_path}")
             bucket.download_file(key, local_file_path)
@@ -95,3 +96,28 @@ def load_data_to_dataframe(
     _LOG.log(logging_level, "Data loaded into dataframes.")
     # Return the dictionary of the dataframes.
     return dataframes
+
+def save_data_to_s3(
+        df: pd.DataFrame(),
+        bucket_name: str,
+        s3_path: str,
+        file_name: str,
+        index: bool = False,
+        **kwargs: Any,
+        ) -> None:
+    """
+    Saves the give dataframe in S3  location as a `.csv` file.
+
+    :param:
+    """
+    # Check if given dataframe is empty.
+    if df.empty:
+        raise ValueError("The given dataframe is empty.")
+    # Save the dataframe a csv file in local directory. 
+    file_path = file_name + ".csv"
+    df.to_csv(file_path, index=index)
+    # Initiate S3 session.
+    s3 = haws.get_service_resource(aws_profile="ck", service_name="s3")
+    # Upload the file to S3.
+    save_path = s3_path + "/" + file_path
+    s3.Bucket(bucket_name).upload_file(file_path, save_path)
