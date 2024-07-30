7b2544c9c285d47e73773cf4a0337ec7f47df7ff
jayati1397 <jayatipatel1397@gmail.com>
2024-07-30 15:49:05 +0000

    update

--- helpers/test/test_dbg.py
+++ helpers/test/test_dbg.py
@@ -114,7 +114,6 @@ class Test_dassert_eq1(hunitest.TestCase):
 
 # TODO(gp): Break it in piece.
 class Test_dassert_misc1(hunitest.TestCase):
-
     # dassert_in
 
     def test_in1(self) -> None:
@@ -660,7 +659,9 @@ class Test_dassert_related_params2(hunitest.TestCase):
         """
         self.assert_equal(act, exp, purify_text=True, fuzzy_match=True)
 
+
 # #############################################################################
+
 
 class Test_dassert_all_attributes_are_same1(hunitest.TestCase):
     def test1(self) -> None:
@@ -714,10 +715,11 @@ class Test_dassert_all_attributes_are_same1(hunitest.TestCase):
         list_ = [Obj(1, 2), Obj(1, 2)]
         hdbg.dassert_all_attributes_are_same(list_, "b")
 
+
 # #############################################################################
 
-class Test_dassert_is_integer(hunitest.TestCase):
 
+class Test_dassert_is_integer(hunitest.TestCase):
     def test1(self) -> None:
         """
         Test that the function do not raise the exception with integer values.
@@ -727,14 +729,16 @@ class Test_dassert_is_integer(hunitest.TestCase):
 
     def test2(self) -> None:
         """
-        Test that the function do not raise the exception with float values that represent an integer.
+        Test that the function do not raise the exception with float values
+        that represent an integer.
         """
         val = 5.0
         hdbg.dassert_is_integer(val)
 
     def test3(self) -> None:
         """
-        Test that the function raises an exception for float values that do not represent an integer.
+        Test that the function raises an exception for float values that do not
+        represent an integer.
         """
         val = 5.5
         with self.assertRaises(AssertionError) as cm:
@@ -749,7 +753,8 @@ class Test_dassert_is_integer(hunitest.TestCase):
 
     def test4(self) -> None:
         """
-        Test that the function raises an exception for non-integer and non-float types.
+        Test that the function raises an exception for non-integer and non-
+        float types.
         """
         val = "5"
         with self.assertRaises(AssertionError) as cm:
