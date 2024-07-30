f1db6f2cdef887db610447b14d46018f07b16084
jayati1397 <jayatipatel1397@gmail.com>
2024-07-30 15:54:08 +0000

    linter update

--- helpers/test/test_dbg.py
+++ helpers/test/test_dbg.py
@@ -114,7 +114,6 @@ class Test_dassert_eq1(hunitest.TestCase):
 
 # TODO(gp): Break it in piece.
 class Test_dassert_misc1(hunitest.TestCase):
-
     # dassert_in
 
     def test_in1(self) -> None:
@@ -660,7 +659,9 @@ class Test_dassert_related_params2(hunitest.TestCase):
         """
         self.assert_equal(act, exp, purify_text=True, fuzzy_match=True)
 
+
 # #############################################################################
+
 
 class Test_dassert_all_attributes_are_same1(hunitest.TestCase):
     def test1(self) -> None:
@@ -714,21 +715,24 @@ class Test_dassert_all_attributes_are_same1(hunitest.TestCase):
         list_ = [Obj(1, 2), Obj(1, 2)]
         hdbg.dassert_all_attributes_are_same(list_, "b")
 
+
 # #############################################################################
 
+
 class Test_dassert_lt(hunitest.TestCase):
-    
     def test1(self) -> None:
         """
-        Test that the function doesn't raise an exception if first value is less than second value.
+        Test that the function doesn't raise an exception if first value is
+        less than second value.
         """
         val1 = 1
         val2 = 2
         hdbg.dassert_lt(val1, val2)
-    
+
     def test2(self) -> None:
         """
-        Test that the function raises an exception if first value is equal to second value.
+        Test that the function raises an exception if first value is equal to
+        second value.
         """
         val1 = 2
         val2 = 2
@@ -741,10 +745,11 @@ class Test_dassert_lt(hunitest.TestCase):
         """
         # check.
         self.assert_equal(act, exp, fuzzy_match=True)
-    
+
     def test3(self) -> None:
         """
-       Test that the function raises an exception if first value is greater than second value.
+        Test that the function raises an exception if first value is greater
+        than second value.
         """
         val1 = 3
         val2 = 2
@@ -760,7 +765,8 @@ class Test_dassert_lt(hunitest.TestCase):
 
     def test4(self) -> None:
         """
-        Test that the function doesn't raise an exception when we pass string inputs.
+        Test that the function doesn't raise an exception when we pass string
+        inputs.
         """
         val1 = "a"
         val2 = "b"
@@ -768,7 +774,8 @@ class Test_dassert_lt(hunitest.TestCase):
 
     def test5(self) -> None:
         """
-        Test that the function raises an exception where first value is greater than second value with floats.
+        Test that the function raises an exception where first value is greater
+        than second value with floats.
         """
         val1 = 2.0
         val2 = 1.0
